f10af1d701240595f75671d77aa98dac04114307
Anton Verburg <info@tec-v.nl>
2015-09-24 07:59:37 -0700

    Be smarter when to show group toggle

--- src/cards/ha-domain-card.js
+++ src/cards/ha-domain-card.js
@@ -1,5 +1,6 @@
 import Polymer from '../polymer';
 import { moreInfoActions } from '../util/home-assistant-js-instance';
+import canToggle from '../util/can-toggle';
 
 require('../components/ha-card');
 require('../components/entity/ha-entity-toggle');
@@ -31,5 +32,14 @@ export default new Polymer({
     ev.stopPropagation();
     const entityId = ev.model.item.entityId;
     this.async(() => moreInfoActions.selectEntity(entityId), 1);
+  },
+
+  showGroupToggle(groupEntity, states) {
+    if (!groupEntity || groupEntity.state !== 'on' && groupEntity.state !== 'off') {
+      return false;
+    }
+
+    // only show if we can toggle 2+ entities in group
+    return states.reduce((sum, state) => sum + canToggle(state.entityId), 0) > 1;
   },
 });
