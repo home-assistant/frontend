59fec6468d65e4b2df3f038bb392aaafb22b26d8
Anton Verburg <info@tec-v.nl>
2015-10-25 18:59:25 -0700

    Move minify script

---
+++ script/minify.js
@@ -0,0 +1,17 @@
+var minify = require('html-minifier');
+var fs = require('fs');
+
+var html = fs.readFileSync('build/frontend.vulcan.html').toString();
+
+var minifiedHtml = minify.minify(html, {
+  customAttrAssign: [/\$=/],
+  removeComments: true,
+  removeCommentsFromCDATA: true,
+  removeCDATASectionsFromCDATA: true,
+  collapseWhitespace: true,
+  removeScriptTypeAttributes: true,
+  removeStyleLinkTypeAttributes: true,
+  minifyJS: true,
+});
+
+fs.writeFileSync('build/frontend.html', minifiedHtml);
