5ea83557bb4c81cc137b6f4a5b8d5f0920bcd821
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2015-11-07 00:55:00 -0800

    Migrate light to use RGB instead of XY color

--- src/components/entity/state-badge.js
+++ src/components/entity/state-badge.js
@@ -1,7 +1,5 @@
 import Polymer from '../../polymer';
 
-import xyBriToRgb from '../../util/xybri-to-rgb';
-
 require('./ha-state-icon');
 
 export default new Polymer({
@@ -20,11 +18,8 @@ export default new Polymer({
   updateIconColor(newVal) {
     // for domain light, set color of icon to light color if available
     if (newVal.domain === 'light' && newVal.state === 'on' &&
-       newVal.attributes.brightness && newVal.attributes.xy_color) {
-      const rgb = xyBriToRgb(newVal.attributes.xy_color[0],
-                             newVal.attributes.xy_color[1],
-                             newVal.attributes.brightness);
-      this.$.icon.style.color = 'rgb(' + rgb.map(Math.floor).join(',') + ')';
+       newVal.attributes.rgb_color) {
+      this.$.icon.style.color = 'rgb(' + newVal.attributes.rgb_color.join(',') + ')';
     } else {
       this.$.icon.style.color = null;
     }
