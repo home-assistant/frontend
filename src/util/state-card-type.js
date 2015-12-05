04a7ce241c8c4d4f15afa61d1565641dda0d12e8
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2015-12-05 00:13:38 -0800

    Add rollershutter UI

--- src/util/state-card-type.js
+++ src/util/state-card-type.js
@@ -1,7 +1,7 @@
 import canToggle from './can-toggle';
 
 const DOMAINS_WITH_CARD = [
-  'thermostat', 'configurator', 'scene', 'media_player'];
+  'thermostat', 'configurator', 'scene', 'media_player', 'rollershutter'];
 
 export default function stateCardType(state) {
   if (DOMAINS_WITH_CARD.indexOf(state.domain) !== -1) {
