e3f58ea779e7cad410263d72d558c40be9c2e92b
John Arild Berentsen <turbokongen@hotmail.com>
2016-02-13 09:06:19 +0100

    Unit fix

--- src/util/state-icon.js
+++ src/util/state-icon.js
@@ -17,8 +17,7 @@ export default function stateIcon(state) {
     if (unit === temperatureUnits.UNIT_TEMP_C ||
         unit === temperatureUnits.UNIT_TEMP_F) {
       return 'mdi:thermometer';
-    } else if (unit === 'Mouse' ||
-               unit === 'Mice') {
+    } else if (unit === 'Mice') {
       return 'mdi:mouse-variant';
     }
   }
