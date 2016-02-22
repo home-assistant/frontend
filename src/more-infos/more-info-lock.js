bee75620fec43bad4f44574e6e9550da7ff1d2ec
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2016-02-21 23:09:11 -0800

    Clean up and fix lock more info

--- src/more-infos/more-info-lock.js
+++ src/more-infos/more-info-lock.js
@@ -15,57 +15,23 @@ export default new Polymer({
       type: String,
       value: '',
     },
-    unlockButtonVisible: {
-      type: Boolean,
-      value: false,
-    },
-    lockButtonVisible: {
-      type: Boolean,
-      value: false,
-    },
-    codeInputVisible: {
-      type: Boolean,
-      value: false,
-    },
-    codeInputEnabled: {
-      type: Boolean,
-      value: false,
-    },
-    codeFormat: {
-      type: String,
-      value: '',
-    },
-    codeValid: {
-      type: Boolean,
-      computed: 'validateCode(enteredCode, codeFormat)',
-    },
   },
+
   handleUnlockTap() {
     this.callService('unlock', { code: this.enteredCode });
   },
+
   handleLockTap() {
     this.callService('lock', { code: this.enteredCode });
   },
-  validateCode(code, format) {
-    const re = new RegExp(format);
-    if (format === null) {
-      return true;
-    }
-    return re.test(code);
-  },
+
   stateObjChanged(newVal) {
     if (newVal) {
-      this.codeFormat = newVal.attributes.code_format;
-      this.codeInputVisible = this.codeFormat !== null;
-      this.codeInputEnabled = (
-        newVal.state === 'locked' ||
-	newVal.state === 'unlocked');
-      this.unlockButtonVisible = (
-	newVal.state === 'locked');
-      this.lockButtonVisible = newVal.state === 'unlocked';
+      this.isLocked = newVal.state === 'locked';
     }
     this.async(() => this.fire('iron-resize'), 500);
   },
+
   callService(service, data) {
     const serviceData = data || {};
     serviceData.entity_id = this.stateObj.entityId;
