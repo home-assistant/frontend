16d2eed5fcac83876c6cd9dbc4ad5f93b35782e9
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2016-05-08 23:36:53 -0700

    Clean up more info hvac

--- src/more-infos/more-info-hvac.js
+++ src/more-infos/more-info-hvac.js
@@ -4,7 +4,15 @@ import Polymer from '../polymer';
 import attributeClassNames from '../util/attribute-class-names';
 
 const { serviceActions } = hass;
-const ATTRIBUTE_CLASSES = ['away_mode', 'aux_heat'];
+const ATTRIBUTE_CLASSES = [
+  'away_mode',
+  'aux_heat',
+  'temperature',
+  'humidity',
+  'operation_list',
+  'fan_list',
+  'swing_list',
+];
 
 export default new Polymer({
   is: 'more-info-hvac',
@@ -15,63 +23,23 @@ export default new Polymer({
       observer: 'stateObjChanged',
     },
 
-    tempMin: {
-      type: Number,
-    },
-
-    tempMax: {
-      type: Number,
-    },
-
-    humMin: {
-      type: Number,
-    },
-
-    humMax: {
-      type: Number,
-    },
-
-    targetTemperatureSliderValue: {
-      type: Number,
-    },
-
-    targetHumiditySliderValue: {
-      type: Number,
-    },
-
-    operationMode: {
-      type: String,
-      value: '',
-    },
-
     operationIndex: {
       type: Number,
-      value: 0,
+      value: -1,
       observer: 'handleOperationmodeChanged',
-    },
-
-    fanMode: {
-      type: String,
-      value: '',
     },
 
     fanIndex: {
       type: Number,
-      value: 0,
+      value: -1,
       observer: 'handleFanmodeChanged',
-    },
-
-    swingMode: {
-      type: String,
-      value: '',
     },
 
     swingIndex: {
       type: Number,
-      value: 0,
+      value: -1,
       observer: 'handleSwingmodeChanged',
     },
-
 
     awayToggleChecked: {
       type: Boolean,
@@ -83,150 +51,110 @@ export default new Polymer({
   },
 
   stateObjChanged(newVal) {
-    this.targetTemperatureSliderValue = newVal.attributes.temperature;
-    this.targetHumiditySliderValue = newVal.attributes.humidity;
-    this.fanMode = newVal.attributes.fan;
-    this.swingMode = newVal.attributes.swing_mode;
-    this.operationMode = newVal.attributes.operation_mode;
     this.awayToggleChecked = newVal.attributes.away_mode === 'on';
     this.auxheatToggleChecked = newVal.attributes.aux_heat === 'on';
 
-    this.tempMin = newVal.attributes.min_temp;
-    this.tempMax = newVal.attributes.max_temp;
-    this.humMin = newVal.attributes.min_humidity;
-    this.humMax = newVal.attributes.max_humidity;
-
-    if (newVal.attributes.fan_list !== null) {
-      this.fanIndex = newVal.attributes.fan_list.indexOf(this.fanMode);
+    if (newVal.attributes.fan_list) {
+      this.fanIndex = newVal.attributes.fan_list.indexOf(
+        newVal.attributes.fan_mode);
+    } else {
+      this.fanIndex = -1;
     }
 
-    if (newVal.attributes.operation_list !== null) {
-      this.operationIndex = newVal.attributes.operation_list.indexOf(this.operationMode);
+    if (newVal.attributes.operation_list) {
+      this.operationIndex = newVal.attributes.operation_list.indexOf(
+        newVal.attributes.operation_mode);
+    } else {
+      this.operationIndex = -1;
     }
 
-    if (newVal.attributes.swing_list !== null) {
-      this.swingIndex = newVal.attributes.swing_list.indexOf(this.swingMode);
+    if (newVal.attributes.swing_list) {
+      this.swingIndex = newVal.attributes.swing_list.indexOf(
+        newVal.attributes.swing_mode);
+    } else {
+      this.swingIndex = -1;
     }
+
+    this.async(() => this.fire('iron-resize'), 500);
   },
 
   computeClassNames(stateObj) {
-    return attributeClassNames(stateObj, ATTRIBUTE_CLASSES);
+    return `more-info-hvac ${attributeClassNames(stateObj, ATTRIBUTE_CLASSES)}`;
   },
 
   targetTemperatureSliderChanged(ev) {
-    serviceActions.callService('hvac', 'set_temperature', {
-      entity_id: this.stateObj.entityId,
-      temperature: ev.target.value,
-    });
+    const temperature = ev.target.value;
+
+    if (temperature === this.stateObj.attributes.temperature) return;
+
+    this.callServiceHelper('set_temperature', { temperature });
   },
 
   targetHumiditySliderChanged(ev) {
-    serviceActions.callService('hvac', 'set_humidity', {
-      entity_id: this.stateObj.entityId,
-      humidity: ev.target.value,
-    });
+    const humidity = ev.target.value;
+
+    if (humidity === this.stateObj.attributes.humidity) return;
+
+    this.callServiceHelper('set_humidity', { humidity });
   },
 
   awayToggleChanged(ev) {
+    const oldVal = this.stateObj.attributes.away_mode === 'on';
     const newVal = ev.target.checked;
 
-    if (newVal && this.stateObj.attributes.away_mode === 'off') {
-      this.service_set_away(true);
-    } else if (!newVal && this.stateObj.attributes.away_mode === 'on') {
-      this.service_set_away(false);
-    }
+    if (oldVal === newVal) return;
+
+    this.callServiceHelper('set_away_mode', { away_mode: newVal });
   },
 
   auxToggleChanged(ev) {
+    const oldVal = this.stateObj.attributes.aux_heat === 'on';
     const newVal = ev.target.checked;
-    if (newVal && this.stateObj.attributes.aux_heat === 'off') {
-      this.service_set_aux_heat(true);
-    } else if (!newVal && this.stateObj.attributes.aux_heat === 'on') {
-      this.service_set_aux_heat(false);
-    }
+
+    if (oldVal === newVal) return;
+
+    this.callServiceHelper('set_aux_heat', { aux_heat: newVal });
   },
 
   handleFanmodeChanged(fanIndex) {
     // Selected Option will transition to '' before transitioning to new value
-    if (!this.stateObj
-        || this.stateObj.attributes.fan_list === undefined
-        || fanIndex >= this.stateObj.attributes.fan_list.length
-    ) {
-      return;
-    }
+    if (fanIndex === '' || fanIndex === -1) return;
 
     const fanInput = this.stateObj.attributes.fan_list[fanIndex];
-    if (fanInput === this.stateObj.attributes.fan) {
-      return;
-    }
+    if (fanInput === this.stateObj.attributes.fan_mode) return;
 
-    serviceActions.callService('hvac', 'set_fan_mode', {
-      entity_id: this.stateObj.entityId, fan: fanInput })
-
-    .then(() => this.stateObjChanged(this.stateObj));
+    this.callServiceHelper('set_fan_mode', { fan_mode: fanInput });
   },
 
   handleOperationmodeChanged(operationIndex) {
     // Selected Option will transition to '' before transitioning to new value
-    if (!this.stateObj
-        || this.stateObj.attributes.operation_list === undefined
-        || operationIndex >= this.stateObj.attributes.operation_list.length
-    ) {
-      return;
-    }
+    if (operationIndex === '' || operationIndex === -1) return;
 
     const operationInput = this.stateObj.attributes.operation_list[operationIndex];
-    if (operationInput === this.stateObj.attributes.operation_mode) {
-      return;
-    }
+    if (operationInput === this.stateObj.attributes.operation_mode) return;
 
-    serviceActions.callService('hvac', 'set_operation_mode', {
-      entity_id: this.stateObj.entityId, operation_mode: operationInput })
-
-    .then(() => this.stateObjChanged(this.stateObj));
+    this.callServiceHelper('set_operation_mode', { operation_mode: operationInput });
   },
 
   handleSwingmodeChanged(swingIndex) {
     // Selected Option will transition to '' before transitioning to new value
-    if (!this.stateObj
-        || this.stateObj.attributes.swing_list === undefined
-        || swingIndex >= this.stateObj.attributes.swing_list.length
-    ) {
-      return;
-    }
+    if (swingIndex === '' || swingIndex === -1) return;
 
     const swingInput = this.stateObj.attributes.swing_list[swingIndex];
-    if (swingInput === this.stateObj.attributes.swing_mode) {
-      return;
-    }
+    if (swingInput === this.stateObj.attributes.swing_mode) return;
 
-    serviceActions.callService('hvac', 'set_swing_mode', {
-      entity_id: this.stateObj.entityId, swing_mode: swingInput })
-
-    .then(() => this.stateObjChanged(this.stateObj));
+    this.callServiceHelper('set_swing_mode', { swing_mode: swingInput });
   },
 
-  service_set_away(awayMode) {
-    // We call stateChanged after a successful call to re-sync the toggle
+  callServiceHelper(service, data) {
+    // We call stateChanged after a successful call to re-sync the inputs
     // with the state. It will be out of sync if our service call did not
     // result in the entity to be turned on. Since the state is not changing,
     // the resync is not called automatic.
-    serviceActions.callService(
-      'hvac', 'set_away_mode',
-      { away_mode: awayMode, entity_id: this.stateObj.entityId })
-
-    .then(() => this.stateObjChanged(this.stateObj));
-  },
-
-  service_set_aux_heat(auxheatMode) {
-    // We call stateChanged after a successful call to re-sync the toggle
-    // with the state. It will be out of sync if our service call did not
-    // result in the entity to be turned on. Since the state is not changing,
-    // the resync is not called automatic.
-    serviceActions.callService(
-      'hvac', 'set_aux_heat',
-      { aux_heat: auxheatMode, entity_id: this.stateObj.entityId })
-
-    .then(() => this.stateObjChanged(this.stateObj));
+    /* eslint-disable no-param-reassign */
+    data.entity_id = this.stateObj.entityId;
+    /* eslint-enable no-param-reassign */
+    serviceActions.callService('hvac', service, data)
+      .then(() => this.stateObjChanged(this.stateObj));
   },
 });
