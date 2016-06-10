007de0860a74c48c931c0df48462684fb81af060
Anton Verburg <info@tec-v.nl>
2016-06-09 23:25:38 -0700

    Clean up state summaries

--- src/state-summary/state-card-content.js
+++ src/state-summary/state-card-content.js
@@ -3,18 +3,7 @@ import Polymer from '../polymer';
 import stateCardType from '../util/state-card-type';
 import dynamicContentUpdater from '../util/dynamic-content-updater';
 
-import './state-card-configurator';
-import './state-card-display';
-import './state-card-hvac';
-import './state-card-input_select';
-import './state-card-input_slider';
-import './state-card-media_player';
-import './state-card-scene';
-import './state-card-script';
-import './state-card-rollershutter';
-import './state-card-thermostat';
-import './state-card-toggle';
-import './state-card-weblink';
+import '../components/state-info';
 
 export default new Polymer({
   is: 'state-card-content',
