63c73c6d8e86dade84781dd725614b1fb95f1be1
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2017-01-29 17:13:53 -0800

    Fix login

--- src/app-core.js
+++ src/app-core.js
@@ -3,23 +3,26 @@ import * as HAWS from 'home-assistant-js-websocket';
 window.HAWS = HAWS;
 window.HASS_DEMO = __DEMO__;
 
-function init(password) {
+const init = window.createHassConnection = function (password) {
   const proto = window.location.protocol === 'https:' ? 'wss' : 'ws';
   const url = `${proto}://${window.location.host}/api/websocket`;
   const options = {};
-  if (password) {
+  if (password !== undefined) {
     options.authToken = password;
   }
+
   return HAWS.createConnection(url, options)
     .then(function (conn) {
       HAWS.subscribeEntities(conn);
       HAWS.subscribeConfig(conn);
       return conn;
     });
-}
+};
 
 if (window.noAuth) {
   window.hassConnection = init();
+} else if (window.localStorage.authToken) {
+  window.hassConnection = init(window.localStorage.authToken);
 } else {
   window.hassConnection = null;
 }
