cf2a71d3ebb3fb840cf3a4b4a63caa1421be37db
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2017-05-11 23:55:09 -0700

    Automation editor JS is a temp build artifact

--- rollup/automation.js
+++ rollup/automation.js
@@ -3,6 +3,6 @@ import config from './base-config';
 export default Object.assign({}, config, {
   entry: 'panels/automation/editor.js',
   targets: [
-    { dest: 'build/editor.js', format: 'iife' },
+    { dest: 'build-temp/editor.js', format: 'iife' },
   ],
 });
