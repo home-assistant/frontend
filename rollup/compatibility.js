76e7d9e56c1b675dd530c6cb4475ca656c41851d
Adam Mills <adam@armills.info>
2017-07-12 21:56:31 -0400

    Big cleanup of gulpfile

--- rollup/compatibility.js
+++
@@ -1,8 +0,0 @@
-import config from './base-config';
-
-export default Object.assign({}, config, {
-  entry: 'src/compatibility.js',
-  targets: [
-    { dest: 'build/compatibility.js', format: 'iife' },
-  ],
-});
