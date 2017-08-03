3e6df407c3506a33d1605e930adc127919d43e1a
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2017-08-02 21:19:26 -0700

    Wrap it up

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,20 +1,3 @@
 var requireDir = require('require-dir');
 
 requireDir('./gulp/tasks/');
-
-/*
-
-TODO
-
-Add build for Hass.io panel.
-
-      Target: './build-temp/hassio-main.html',
-
-      stripExcludes: [
-        'bower_components/font-roboto/roboto.html',
-        'bower_components/paper-styles/color.html',
-        'bower_components/polymer/polymer.html',
-        'bower_components/iron-meta/iron-meta.html',
-      ]
-
-*/
