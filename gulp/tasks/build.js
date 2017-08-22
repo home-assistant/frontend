dce94cac24acd5c94f77e5d9c530b86df690e37b
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2017-08-21 22:38:21 -0700

    Fix Hass.io build script

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,11 +1,6 @@
-const babel = require('gulp-babel');
-const uglify = require('gulp-uglify');
-const { gulp: cssSlam } = require('css-slam');
 const gulp = require('gulp');
 const filter = require('gulp-filter');
-const htmlMinifier = require('gulp-html-minifier');
-const gulpif = require('gulp-if');
-const { PolymerProject, HtmlSplitter } = require('polymer-build');
+const { PolymerProject, } = require('polymer-build');
 const {
   composeStrategies,
   generateShellMergeStrategy,
@@ -15,25 +10,11 @@ const rename = require('gulp-rename');
 
 const polymerConfig = require('../../polymer');
 
-function minifyStream(stream) {
-  const sourcesHtmlSplitter = new HtmlSplitter();
-  return stream
-    .pipe(sourcesHtmlSplitter.split())
-    .pipe(gulpif(/[^app]\.js$/, babel({
-      sourceType: 'script',
-      presets: [
-        ['es2015', { modules: false }]
-      ]
-    })))
-    .pipe(gulpif(/\.js$/, uglify({ sourceMap: false })))
-    .pipe(gulpif(/\.css$/, cssSlam()))
-    .pipe(gulpif(/\.html$/, cssSlam()))
-    .pipe(gulpif(/\.html$/, htmlMinifier({
-      collapseWhitespace: true,
-      removeComments: true
-    })))
-    .pipe(sourcesHtmlSplitter.rejoin());
-}
+const minifyStream = require('../common/transform').minifyStream;
+const {
+  stripImportsStrategy,
+  stripAllButEntrypointStrategy
+} = require('../common/strategy');
 
 function renamePanel(path) {
   // Rename panels to be panels/* and not their subdir
@@ -49,49 +30,14 @@ function renamePanel(path) {
   }
 }
 
-/**
- * Polymer build strategy to strip imports, even if explictely imported
- */
-function generateStripStrategy(urls) {
-  return (bundles) => {
-    for (const bundle of bundles) {
-      for (const url of urls) {
-        bundle.stripImports.add(url);
-      }
-    }
-    return bundles;
-  };
-}
-
-/**
- * Polymer build strategy to strip everything but the entrypoints
- * for bundles that match a specific entry point.
- */
-function stripAllButEntrypoint(entryPoint) {
-  return (bundles) => {
-    for (const bundle of bundles) {
-      if (bundle.entrypoints.size === 1 &&
-          bundle.entrypoints.has(entryPoint)) {
-        for (const file of bundle.files) {
-          if (!bundle.entrypoints.has(file)) {
-            bundle.stripImports.add(file);
-          }
-        }
-      }
-    }
-    return bundles;
-  };
-}
-
-
 gulp.task('build', ['ru_all'], () => {
   const strategy = composeStrategies([
     generateShellMergeStrategy(polymerConfig.shell),
-    generateStripStrategy([
+    stripImportsStrategy([
       'bower_components/font-roboto/roboto.html',
       'bower_components/paper-styles/color.html',
     ]),
-    stripAllButEntrypoint('panels/hassio/ha-panel-hassio.html')
+    stripAllButEntrypointStrategy('panels/hassio/ha-panel-hassio.html')
   ]);
   const project = new PolymerProject(polymerConfig);
 
