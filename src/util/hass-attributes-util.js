5bdbc49dc25dfff079e76504529d7905523e5d9a
Andrey <andrey-git@users.noreply.github.com>
2017-08-29 19:44:05 +0300

    Improvements for customize config panel (#415)
    * Don't save empty json values. Allow per-domain attributes.
    
    * Fix typo

--- src/util/hass-attributes-util.js
+++ src/util/hass-attributes-util.js
@@ -24,27 +24,27 @@ window.hassAttributeUtil.TYPE_TO_TAG = {
 // 1) Any key of this object won't be shown in more-info window.
 // 2) Any key which has value other than undefined will appear in customization
 //    config according to its value.
-// TODO: Allow per-domain config, as some attributes are only relevant for some
-// domains.
-window.hassAttributeUtil.LOGIC_STATE_ATTRIBUTES = {
-  entity_picture: undefined,
-  friendly_name: { type: 'string', description: 'Name' },
-  icon: { type: 'icon' },
-  emulated_hue: { type: 'boolean' },
-  emulated_hue_name: { type: 'string' },
-  haaska_hidden: undefined,
-  haaska_name: undefined,
-  homebridge_hidden: { type: 'boolean' },
-  homebridge_name: { type: 'string' },
-  supported_features: undefined,
-  attribution: undefined,
-  custom_ui_state_card: { type: 'string' },
-  device_class: {
-    type: 'array',
-    options: window.hassAttributeUtil.DOMAIN_DEVICE_CLASS,
-    description: 'Device class' },
-  hidden: { type: 'boolean', description: 'Hide from UI' },
-  assumed_state: { type: 'boolean' },
-  initial_state: { type: 'string' },
-  unit_of_measurement: { type: 'string' },
-};
+window.hassAttributeUtil.LOGIC_STATE_ATTRIBUTES =
+  window.hassAttributeUtil.LOGIC_STATE_ATTRIBUTES || {
+    entity_picture: undefined,
+    friendly_name: { type: 'string', description: 'Name' },
+    icon: { type: 'icon' },
+    emulated_hue: { type: 'boolean' },
+    emulated_hue_name: { type: 'string' },
+    haaska_hidden: undefined,
+    haaska_name: undefined,
+    homebridge_hidden: { type: 'boolean' },
+    homebridge_name: { type: 'string' },
+    supported_features: undefined,
+    attribution: undefined,
+    custom_ui_state_card: { type: 'string' },
+    device_class: {
+      type: 'array',
+      options: window.hassAttributeUtil.DOMAIN_DEVICE_CLASS,
+      description: 'Device class',
+      domains: ['binary_sensor', 'cover'] },
+    hidden: { type: 'boolean', description: 'Hide from UI' },
+    assumed_state: { type: 'boolean' },
+    initial_state: { type: 'string' },
+    unit_of_measurement: { type: 'string' },
+  };
