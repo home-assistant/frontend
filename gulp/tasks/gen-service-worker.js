c1c955256e187dafaf8c8a72a204792a2ce06333
Anton Verburg <info@tec-v.nl>
2017-10-12 02:36:04 -0400

    Only generate fingerprints for non-dev SW (#453)

--- gulp/tasks/gen-service-worker.js
+++ gulp/tasks/gen-service-worker.js
@@ -13,6 +13,7 @@ TODO:
 */
 var gulp = require('gulp');
 var crypto = require('crypto');
+var file = require('gulp-file');
 var fs = require('fs');
 var path = require('path');
 var swPrecache = require('sw-precache');
@@ -53,48 +54,50 @@ function md5(filename) {
 }
 
 gulp.task('gen-service-worker', () => {
-  // Create fingerprinted versions of our dependencies.
-  staticFingerprinted.forEach(fn => {
-    var parts = path.parse(fn);
-    var hash = md5(rootDir + '/' + parts.name + parts.ext);
-    var url = '/static/' + parts.name + '-' + hash + parts.ext;
-    var fpath = rootDir + '/' + parts.name + parts.ext;
-    dynamicUrlToDependencies[url] = [fpath];
-  });
+  var genPromise = null;
+  if (DEV) {
+    var devBase = 'console.warn("Service worker caching disabled in development")';
+    genPromise = Promise.resolve(devBase);
+  } else {
+    // Create fingerprinted versions of our dependencies.
+    staticFingerprinted.forEach(fn => {
+      var parts = path.parse(fn);
+      var hash = md5(rootDir + '/' + parts.name + parts.ext);
+      var url = '/static/' + parts.name + '-' + hash + parts.ext;
+      var fpath = rootDir + '/' + parts.name + parts.ext;
+      dynamicUrlToDependencies[url] = [fpath];
+    });
 
-  panelsFingerprinted.forEach(panel => {
-    var fpath = panelDir + '/ha-panel-' + panel + '.html';
-    var hash = md5(fpath);
-    var url = '/frontend/panels/' + panel + '-' + hash + '.html';
-    dynamicUrlToDependencies[url] = [fpath];
-  });
+    panelsFingerprinted.forEach(panel => {
+      var fpath = panelDir + '/ha-panel-' + panel + '.html';
+      var hash = md5(fpath);
+      var url = '/frontend/panels/' + panel + '-' + hash + '.html';
+      dynamicUrlToDependencies[url] = [fpath];
+    });
 
-  var options = {
-    navigateFallback: '/',
-    navigateFallbackWhitelist: [/^((?!(static|api|local|service_worker.js|manifest.json)).)*$/],
-    dynamicUrlToDependencies: dynamicUrlToDependencies,
-    staticFileGlobs: [
-      rootDir + '/icons/favicon.ico',
-      rootDir + '/icons/favicon-192x192.png',
-      rootDir + '/webcomponents-lite.min.js',
-      rootDir + '/fonts/roboto/Roboto-Light.ttf',
-      rootDir + '/fonts/roboto/Roboto-Medium.ttf',
-      rootDir + '/fonts/roboto/Roboto-Regular.ttf',
-      rootDir + '/fonts/roboto/Roboto-Bold.ttf',
-      rootDir + '/images/card_media_player_bg.png',
-    ],
-    stripPrefix: '..',
-    replacePrefix: 'static',
-    verbose: true,
-  };
+    var options = {
+      navigateFallback: '/',
+      navigateFallbackWhitelist: [/^((?!(static|api|local|service_worker.js|manifest.json)).)*$/],
+      dynamicUrlToDependencies: dynamicUrlToDependencies,
+      staticFileGlobs: [
+        rootDir + '/icons/favicon.ico',
+        rootDir + '/icons/favicon-192x192.png',
+        rootDir + '/webcomponents-lite.min.js',
+        rootDir + '/fonts/roboto/Roboto-Light.ttf',
+        rootDir + '/fonts/roboto/Roboto-Medium.ttf',
+        rootDir + '/fonts/roboto/Roboto-Regular.ttf',
+        rootDir + '/fonts/roboto/Roboto-Bold.ttf',
+        rootDir + '/images/card_media_player_bg.png',
+      ],
+      stripPrefix: '..',
+      replacePrefix: 'static',
+      verbose: true,
+    };
 
-  var devBase = 'console.warn("Service worker caching disabled in development")';
+    genPromise = swPrecache.generate(options);
+  }
 
   var swHass = fs.readFileSync(path.resolve(__dirname, '../service-worker.js.tmpl'), 'UTF-8');
-
-  var genPromise = DEV ? Promise.resolve(devBase) : swPrecache.generate(options);
-
-  genPromise = genPromise.then(swString => swString + '\n' + swHass);
 
   // Fix this
   // if (!DEV) {
@@ -102,9 +105,7 @@ gulp.task('gen-service-worker', () => {
   //     swString => uglifyJS.minify(swString, { fromString: true }).code);
   // }
 
-  genPromise.then(swString => {
-    fs.writeFileSync(path.resolve(config.build_dir, 'service_worker.js'), swString);
-  });
-
-  return genPromise;
+  return genPromise.then(swString => swString + '\n' + swHass)
+    .then(swString => file('service_worker.js', swString)
+      .pipe(gulp.dest(config.build_dir)));
 });
