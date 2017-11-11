3f27c31525d8c58f7bcc5ae6f1a610216b932be9
Anton Verburg <info@tec-v.nl>
2017-11-11 13:30:14 -0800

    Pre-construct frontend index.html (#609)
    * Pre-construct frontend index.html
    
    * Only preload things that matter
    
    * Fix entry point in dev mode
    
    * Template Service worker url
    
    * Update referenced service worker

--- gulp/config.js
+++ gulp/config.js
@@ -1,7 +1,8 @@
 var path = require('path');
 
 module.exports = {
-  static_dir: path.resolve(__dirname, '../..'),
   polymer_dir: path.resolve(__dirname, '..'),
   build_dir: path.resolve(__dirname, '../build'),
+  output: path.resolve(__dirname, '../hass_frontend'),
+  output_es5: path.resolve(__dirname, '../hass_frontend_es5'),
 };
