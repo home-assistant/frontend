3fd8a7eadf59ae656ff64e2f84646c0774442dfa
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2017-11-10 22:39:38 -0800

    Version bump to 20171110.0

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(name='home-assistant-frontend',
-      version='20171106.0',
+      version='20171110.0',
       description='The Home Assistant frontend',
       url='https://github.com/home-assistant/home-assistant-polymer',
-      author='Paulus Schoutsen',
-      author_email='Paulus@PaulusSchoutsen.nl',
+      author='The Home Assistant Authors',
+      author_email='hello@home-assistant.io',
       license='Apache License 2.0',
       packages=find_packages(include=['hass_frontend', 'hass_frontend_es5', 'hass_frontend.*', 'hass_frontend_es5.*']),
       include_package_data=True,
