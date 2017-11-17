216073ff9b304b7fa0ac9ba5a1e93a2084d2d527
Anton Verburg <info@tec-v.nl>
2017-11-16 22:02:53 -0800

    Version bump to 20171117.0

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(name='home-assistant-frontend',
-      version='20171115.0',
+      version='20171117.0',
       description='The Home Assistant frontend',
       url='https://github.com/home-assistant/home-assistant-polymer',
       author='The Home Assistant Authors',
       author_email='hello@home-assistant.io',
       license='Apache License 2.0',
-      packages=find_packages(include=['hass_frontend', 'hass_frontend_es5', 'hass_frontend.*', 'hass_frontend_es5.*']),
+      packages=find_packages(include=[
+          'hass_frontend',
+          'hass_frontend_es5',
+          'hass_frontend.*',
+          'hass_frontend_es5.*'
+      ]),
       include_package_data=True,
       zip_safe=False)
