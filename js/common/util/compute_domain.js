e3509d71823ff5e42a4dcf4849a98c5bb6cb391c
Adam Mills <adam@armills.info>
2017-11-19 00:02:33 -0500

    Don't store computed domains on stateObj

--- js/common/util/compute_domain.js
+++ js/common/util/compute_domain.js
@@ -1,9 +1,5 @@
 import { extractDomain } from 'home-assistant-js-websocket';
 
 export default function computeDomain(stateObj) {
-  if (!stateObj._domain) {
-    stateObj._domain = extractDomain(stateObj.entity_id);
-  }
-
-  return stateObj._domain;
+  return extractDomain(stateObj.entity_id);
 }
