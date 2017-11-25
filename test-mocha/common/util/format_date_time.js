81fde1779041fd8bda5261b7cef436bb24e02068
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2017-11-24 21:01:15 -0800

    Add eslint mocha options (#671)
    * Add eslint mocha options
    
    * Please hound
    
    * Remove deprecated --compiler option

--- test-mocha/common/util/format_date_time.js
+++ test-mocha/common/util/format_date_time.js
@@ -2,19 +2,19 @@ import formatDateTime from '../../../js/common/util/format_date_time';
 
 const assert = require('assert');
 
-describe('formatDateTime', function() {
+describe('formatDateTime', () => {
   const dateObj = new Date(
     2017, 10, 18,
     11, 12, 13, 1400,
   );
 
-  it('Formats English date times', function() {
+  it('Formats English date times', () => {
     assert.strictEqual(formatDateTime(dateObj, 'en'), 'November 18, 2017, 11:12 AM');
   });
 
   // Node only contains intl support for english formats. This test at least ensures
   // the fallback to a different locale
-  it('Formats other date times', function() {
+  it('Formats other date times', () => {
     assert.strictEqual(formatDateTime(dateObj, 'fr'), '2017 M11 18 11:12');
   });
 });
