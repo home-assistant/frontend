10ddb7faace0e681301e0bd08b6c0e529474523a
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2017-11-26 17:20:55 -0800

    Minify once, reduce build warnings (#692)
    * Minify once, reduce build warnings
    
    * Remove rollup-plugin-uglify
    
    * address comment

--- gulp/tasks/rollup.js
+++ gulp/tasks/rollup.js
@@ -1,14 +1,73 @@
 const gulp = require('gulp');
 const rollupEach = require('gulp-rollup-each');
-const rollupConfig = require('../../rollup.config');
-const rollupConfigEs6 = require('../../rollup.config-es6');
+const commonjs = require('rollup-plugin-commonjs');
+const nodeResolve = require('rollup-plugin-node-resolve');
+const replace = require('rollup-plugin-replace');
+const babel = require('rollup-plugin-babel');
+const uglify = require('../common/gulp-uglify.js');
+
+const DEV = !!JSON.parse(process.env.BUILD_DEV || 'true');
+const DEMO = !!JSON.parse(process.env.BUILD_DEMO || 'false');
+
+function getRollupInputOptions(es6) {
+  const babelOpts = {
+    babelrc: false,
+    plugins: [
+      'external-helpers',
+      'transform-object-rest-spread',
+      [
+        'transform-react-jsx',
+        {
+          pragma: 'h'
+        }
+      ],
+    ]
+  };
+
+  if (!es6) {
+    babelOpts.presets = [
+      [
+        'es2015',
+        {
+          modules: false
+        }
+      ]
+    ];
+  }
+
+  return {
+    plugins: [
+      babel(babelOpts),
+
+      nodeResolve({
+        jsnext: true,
+        main: true,
+      }),
+
+      commonjs(),
+
+      replace({
+        values: {
+          __DEV__: JSON.stringify(DEV),
+          __DEMO__: JSON.stringify(DEMO),
+          __BUILD__: JSON.stringify(es6 ? 'latest' : 'es5'),
+        },
+      }),
+    ],
+  };
+}
+
+const rollupOutputOptions = {
+  format: 'iife',
+  exports: 'none',
+};
 
 gulp.task('run_rollup_es5', () => gulp.src([
   'js/core.js',
   'js/compatibility.js',
   'demo_data/demo_data.js',
 ])
-  .pipe(rollupEach(rollupConfig, rollupConfig))
+  .pipe(rollupEach(getRollupInputOptions(/* es6= */ false), rollupOutputOptions))
   .pipe(gulp.dest('build-temp-es5')));
 
 gulp.task('run_rollup', () => gulp.src([
@@ -18,7 +77,7 @@ gulp.task('run_rollup', () => gulp.src([
   'js/script-editor/script-editor.js',
   'demo_data/demo_data.js',
 ])
-  .pipe(rollupEach(rollupConfigEs6, rollupConfigEs6))
+  .pipe(rollupEach(getRollupInputOptions(/* es6= */ true), rollupOutputOptions))
   .pipe(gulp.dest('build-temp')));
 
 gulp.task('ru_all_es5', ['run_rollup_es5'], () => {
@@ -26,6 +85,7 @@ gulp.task('ru_all_es5', ['run_rollup_es5'], () => {
     'build-temp-es5/core.js',
     'build-temp-es5/compatibility.js',
   ])
+    .pipe(uglify(/* es6= */ false, { sourceMap: false }))
     .pipe(gulp.dest('build-es5/'));
 });
 
@@ -33,6 +93,7 @@ gulp.task('ru_all', ['run_rollup'], () => {
   gulp.src([
     'build-temp/core.js',
   ])
+    .pipe(uglify(/* es6= */ true, { sourceMap: false }))
     .pipe(gulp.dest('build/'));
 });
 
