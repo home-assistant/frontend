326fa0036597a72720cbafd4819ccd6ab1400844
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2017-11-26 17:47:36 -0800

    Version bump to 20171127.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='home-assistant-frontend',
-      version='20171126.0',
+      version='20171127.0',
       description='The Home Assistant frontend',
       url='https://github.com/home-assistant/home-assistant-polymer',
       author='The Home Assistant Authors',
