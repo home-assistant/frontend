71de28c73c78f7dd7d591e8736f82a86af4ab6e0
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2017-11-30 11:38:21 -0800

    Merge script and automation editor JS (#693)
    * Merge script and automation editor JS
    
    * Also consolidate the files

--- gulp/tasks/rollup.js
+++ gulp/tasks/rollup.js
@@ -68,16 +68,17 @@ gulp.task('run_rollup_es5', () => gulp.src([
   'demo_data/demo_data.js',
 ])
   .pipe(rollupEach(getRollupInputOptions(/* es6= */ false), rollupOutputOptions))
+  .on('error', err => console.error(err.message))
   .pipe(gulp.dest('build-temp-es5')));
 
 gulp.task('run_rollup', () => gulp.src([
   'js/core.js',
-  'js/automation-editor/automation-editor.js',
+  'js/panel-config/panel-config.js',
   'js/util.js',
-  'js/script-editor/script-editor.js',
   'demo_data/demo_data.js',
 ])
   .pipe(rollupEach(getRollupInputOptions(/* es6= */ true), rollupOutputOptions))
+  .on('error', err => console.error(err.message))
   .pipe(gulp.dest('build-temp')));
 
 gulp.task('ru_all_es5', ['run_rollup_es5'], () => {
