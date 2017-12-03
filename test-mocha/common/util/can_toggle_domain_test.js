0bfa3e3fa8185fa6724ef86a62df3f7397db8b83
Adam Mills <adam@armills.info>
2017-12-03 12:50:19 -0500

    Add tests for canToggleDomain

---
+++ test-mocha/common/util/can_toggle_domain_test.js
@@ -0,0 +1,39 @@
+import canToggleDomain from '../../../js/common/util/can_toggle_domain';
+
+const assert = require('assert');
+
+describe('canToggleDomain', () => {
+  const hass = {
+    config: {
+      services: {
+        light: {
+          turn_on: null, // Service keys only need to be present for test
+          turn_off: null,
+        },
+        lock: {
+          lock: null,
+          unlock: null,
+        },
+        sensor: {
+          custom_service: null,
+        },
+      },
+    },
+  };
+
+  it('Detects lights toggle', () => {
+    assert.strictEqual(canToggleDomain(hass, 'light'), true);
+  });
+
+  it('Detects locks toggle', () => {
+    assert.strictEqual(canToggleDomain(hass, 'lock'), true);
+  });
+
+  it('Detects sensors do not toggle', () => {
+    assert.strictEqual(canToggleDomain(hass, 'sensor'), false);
+  });
+
+  it('Detects binary sensors do not toggle', () => {
+    assert.strictEqual(canToggleDomain(hass, 'binary_sensor'), undefined);
+  });
+});
