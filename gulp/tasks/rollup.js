cdd8f31866dc855a644f44b6c032743124dc0136
andrey-git <andrey-git@users.noreply.github.com>
2018-01-27 08:55:53 +0200

    Throw if version wasn't found

--- gulp/tasks/rollup.js
+++ gulp/tasks/rollup.js
@@ -9,7 +9,11 @@ const uglify = require('../common/gulp-uglify.js');
 
 const DEV = !!JSON.parse(process.env.BUILD_DEV || 'true');
 const DEMO = !!JSON.parse(process.env.BUILD_DEMO || 'false');
-const VERSION = fs.readFileSync('setup.py', 'utf8').match(/\d{4}[^']*/)[0] || '0';
+const version = fs.readFileSync('setup.py', 'utf8').match(/\d{8}[^']*/);
+if (!version) {
+  throw Error('Version not found');
+}
+const VERSION = version[0];
 
 function getRollupInputOptions(es6) {
   const babelOpts = {
