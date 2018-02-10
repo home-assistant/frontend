c6030e6edce2207ee13a3b33cde60663367e9d38
Boyi C <cby@cby.me>
2018-02-10 14:39:15 +0800

    Replace Google Charts with Chart.js (#429)
    * chartjs test
    
    * [WIP] Modified for Chart.js
    
    * Tweaking styles ( tooltips and lines )
    
    * Almost done
    TODO:
    Change tooltips to HTML tag
    Improve color function
    
    * More work on Tooltips
    
    * Improve update logic
    Fix linting
    
    * resolve conflict
    
    * [WIP]
    Create new tooltip mode hack.
    Add axis padding to top and botton to prevent axis cutoff
    
    * TODO: cleanup
    
    * FIXME: tooltip in history graph not working correctly
    reorganize some code
    
    * fix build problem
    
    * Fix color and tooltip issue
    Fix label max width for timeline chart
    
    * update dep
    
    * Fix strange color after build due to `uglify` bug with reference the minified version.
    Make line chart behavior more similar to Google Charts.
    Make the chart honor to `unknown` and other state by manually calculate point value.
    
    * fix bugs
    
    * Remove label for only one data in timeline chart.
    Fix bug for infinite loop in some cases
    
    * Add HTML legend to chart.
    
    * Fix isSingleDevice bug due to calculation.
    Add isSingleDevice property support.
    
    * fix for lint
    
    * Replace innerHTML code with polymer node.
    
    * Replace tooltip with HTML code
    
    * fix tooltip style
    
    * move default tooltip mode to plugin
    
    * LINTING
    
    * fix
    Move localize history data to Timeline Chart.
    Fix timeline static color.
    Rework on chart resize.
    
    * Bug fix:
    Chart may disappear on some case.
    Timeline chart calculation issue.
    Change timeline chart hidden logic.
    
    * fix tooltip
    rework for resize event
    
    * lint
    
    * element
    
    * Replace `var` to `let`.
    Move import and ChartJs injection code to `ha-chart-scripts.html`.
    
    * lint: convert more let to const
    
    * fix font
    fix undef
    
    * update bower.json
    
    * move
    
    * Load chart code on demand

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -36,6 +36,7 @@ function build(es6) {
     stripImportsStrategy([
       'bower_components/font-roboto/roboto.html',
       'bower_components/paper-styles/color.html',
+      'src/resources/ha-chart-scripts.html',
     ]),
     stripAllButEntrypointStrategy('panels/hassio/ha-panel-hassio.html')
   ]);
