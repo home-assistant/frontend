a2974ef544084eddcad591ec67a638de637c71e8
Anton Verburg <info@tec-v.nl>
2018-05-10 14:25:36 -0400

    Add an authorize page for authentication (#1147)
    * Use authorize page if auth provider
    
    * Add webcomponent polyfill
    
    * More fixes
    
    * ES5 fix
    
    * Lint
    
    * Use redirect_uri
    
    * upgrade uglify to fix tests?
    
    * Update browsers used for testing

---
+++ gulp/tasks/auth.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+const replace = require('gulp-batch-replace');
+const rename = require('gulp-rename');
+
+const config = require('../config');
+const minifyStream = require('../common/transform').minifyStream;
+const {
+  bundledStreamFromHTML,
+} = require('../common/html');
+
+const es5Extra = "<script src='/frontend_es5/custom-elements-es5-adapter.js'></script>";
+
+async function buildAuth(es6) {
+  let stream = await bundledStreamFromHTML('src/authorize.html');
+  stream = stream.pipe(replace([['<!--EXTRA_SCRIPTS-->', es6 ? '' : es5Extra]]));
+
+  return minifyStream(stream, /* es6= */ es6)
+    .pipe(rename('authorize.html'))
+    .pipe(gulp.dest(es6 ? config.output : config.output_es5));
+}
+
+gulp.task('authorize-es5', () => buildAuth(/* es6= */ false));
+gulp.task('authorize', () => buildAuth(/* es6= */ true));
