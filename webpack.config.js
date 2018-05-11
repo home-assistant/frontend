0e5eca3287fbea9e714c689941b9724f652bbc46
Paulus Schoutsen <balloob@gmail.com>
2018-05-10 23:41:37 -0400

    Update webpack config to build authorize.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,12 @@ module.exports = {
   mode: 'development',
   entry: {
     index: './src/home-assistant.js',
+    authorize: './src/auth/ha-authorize.js'
   },
   plugins: [],
   output: {
     filename: '[name].bundle.js',
     chunkFilename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist/webpack')
+    path: path.resolve(__dirname, 'build/webpack')
   }
 };
