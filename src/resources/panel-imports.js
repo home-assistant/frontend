acdd709dc372f8253baaf56a2eb36ec857b51121
Anton Verburg <info@tec-v.nl>
2018-05-16 21:33:39 -0400

    Remove unused import helper

--- src/resources/panel-imports.js
+++
@@ -1,4 +0,0 @@
-import '@polymer/app-layout/app-header-layout/app-header-layout.js';
-import '@polymer/app-layout/app-header/app-header.js';
-import '@polymer/app-layout/app-toolbar/app-toolbar.js';
-
