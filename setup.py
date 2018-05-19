1eda9155fd3b3d1e07ffc71468cbd4785fc1fd8f
Paulus Schoutsen <balloob@gmail.com>
2018-05-19 10:37:43 -0400

    Version bump to 20180519.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='home-assistant-frontend',
-      version='20180518.1',
+      version='20180519.0',
       description='The Home Assistant frontend',
       url='https://github.com/home-assistant/home-assistant-polymer',
       author='The Home Assistant Authors',
