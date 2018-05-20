4a3870990ff4e7bc03f5523877dc773ed45d7ebd
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2018-05-20 08:39:24 -0400

    Convert cover and media player models to classes (#1197)

--- src/util/cover-model.js
+++ src/util/cover-model.js
@@ -1,117 +1,110 @@
-export default function CoverEntity(hass, stateObj) {
-  this.hass = hass;
-  this.stateObj = stateObj;
-}
-
-function addGetter(name, getter) {
-  Object.defineProperty(
-    CoverEntity.prototype, name,
-    { get: getter }
-  );
-}
-
-addGetter('isFullyOpen', function () {
-  if (this.stateObj.attributes.current_position !== undefined) {
-    return this.stateObj.attributes.current_position === 100;
-  }
-  return this.stateObj.state === 'open';
-});
-
-addGetter('isFullyClosed', function () {
-  if (this.stateObj.attributes.current_position !== undefined) {
-    return this.stateObj.attributes.current_position === 0;
-  }
-  return this.stateObj.state === 'closed';
-});
-
-addGetter('isFullyOpenTilt', function () {
-  return this.stateObj.attributes.current_tilt_position === 100;
-});
-
-addGetter('isFullyClosedTilt', function () {
-  return this.stateObj.attributes.current_tilt_position === 0;
-});
-
-/* eslint-disable no-bitwise */
-
-addGetter('supportsOpen', function () {
-  return (this.stateObj.attributes.supported_features & 1) !== 0;
-});
-
-addGetter('supportsClose', function () {
-  return (this.stateObj.attributes.supported_features & 2) !== 0;
-});
-
-addGetter('supportsSetPosition', function () {
-  return (this.stateObj.attributes.supported_features & 4) !== 0;
-});
-
-addGetter('supportsStop', function () {
-  return (this.stateObj.attributes.supported_features & 8) !== 0;
-});
-
-addGetter('supportsOpenTilt', function () {
-  return (this.stateObj.attributes.supported_features & 16) !== 0;
-});
-
-addGetter('supportsCloseTilt', function () {
-  return (this.stateObj.attributes.supported_features & 32) !== 0;
-});
-
-addGetter('supportsStopTilt', function () {
-  return (this.stateObj.attributes.supported_features & 64) !== 0;
-});
-
-addGetter('supportsSetTiltPosition', function () {
-  return (this.stateObj.attributes.supported_features & 128) !== 0;
-});
-
-addGetter('isTiltOnly', function () {
-  var supportsCover = this.supportsOpen || this.supportsClose || this.supportsStop;
-  var supportsTilt = this.supportsOpenTilt || this.supportsCloseTilt || this.supportsStopTilt;
-  return supportsTilt && !supportsCover;
-});
-
 /* eslint-enable no-bitwise */
+export default class CoverEntity {
+  constructor(hass, stateObj) {
+    this.hass = hass;
+    this.stateObj = stateObj;
+    this._attr = stateObj.attributes;
+    this._feat = this._attr.supported_features;
+  }
 
-Object.assign(CoverEntity.prototype, {
-  openCover: function () {
+  get isFullyOpen() {
+    if (this._attr.current_position !== undefined) {
+      return this._attr.current_position === 100;
+    }
+    return this.stateObj.state === 'open';
+  }
+
+  get isFullyClosed() {
+    if (this._attr.current_position !== undefined) {
+      return this._attr.current_position === 0;
+    }
+    return this.stateObj.state === 'closed';
+  }
+
+  get isFullyOpenTilt() {
+    return this._attr.current_tilt_position === 100;
+  }
+
+  get isFullyClosedTilt() {
+    return this._attr.current_tilt_position === 0;
+  }
+
+  /* eslint-disable no-bitwise */
+
+  get supportsOpen() {
+    return (this._feat & 1) !== 0;
+  }
+
+  get supportsClose() {
+    return (this._feat & 2) !== 0;
+  }
+
+  get supportsSetPosition() {
+    return (this._feat & 4) !== 0;
+  }
+
+  get supportsStop() {
+    return (this._feat & 8) !== 0;
+  }
+
+  get supportsOpenTilt() {
+    return (this._feat & 16) !== 0;
+  }
+
+  get supportsCloseTilt() {
+    return (this._feat & 32) !== 0;
+  }
+
+  get supportsStopTilt() {
+    return (this._feat & 64) !== 0;
+  }
+
+  get supportsSetTiltPosition() {
+    return (this._feat & 128) !== 0;
+  }
+
+  get isTiltOnly() {
+    var supportsCover = this.supportsOpen || this.supportsClose || this.supportsStop;
+    var supportsTilt = this.supportsOpenTilt || this.supportsCloseTilt || this.supportsStopTilt;
+    return supportsTilt && !supportsCover;
+  }
+
+  openCover() {
     this.callService('open_cover');
-  },
+  }
 
-  closeCover: function () {
+  closeCover() {
     this.callService('close_cover');
-  },
+  }
 
-  stopCover: function () {
+  stopCover() {
     this.callService('stop_cover');
-  },
+  }
 
-  openCoverTilt: function () {
+  openCoverTilt() {
     this.callService('open_cover_tilt');
-  },
+  }
 
-  closeCoverTilt: function () {
+  closeCoverTilt() {
     this.callService('close_cover_tilt');
-  },
+  }
 
-  stopCoverTilt: function () {
+  stopCoverTilt() {
     this.callService('stop_cover_tilt');
-  },
+  }
 
-  setCoverPosition: function (position) {
-    this.callService('set_cover_position', { position: position });
-  },
+  setCoverPosition(position) {
+    this.callService('set_cover_position', { position });
+  }
 
-  setCoverTiltPosition: function (tiltPosition) {
+  setCoverTiltPosition(tiltPosition) {
     this.callService('set_cover_tilt_position', { tilt_position: tiltPosition });
-  },
+  }
 
   // helper method
 
-  callService: function (service, data) {
-    var serviceData = data || {};
-    serviceData.entity_id = this.stateObj.entity_id;
-    this.hass.callService('cover', service, serviceData);
-  },
-});
+  callService(service, data = {}) {
+    data.entity_id = this.stateObj.entity_id;
+    this.hass.callService('cover', service, data);
+  }
+}
