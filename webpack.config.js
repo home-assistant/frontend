5fc64b42f452fc846c59e77bc6a3a774721b57ae
Thibault Cohen <titilambert@gmail.com>
2018-05-31 10:31:55 -0400

    Use preact-compat instead of react

--- webpack.config.js
+++ webpack.config.js
@@ -162,6 +162,16 @@ function createConfig(isProdBuild, latestBuild) {
       chunkFilename: chunkFilename,
       path: path.resolve(__dirname, buildPath),
       publicPath,
+    },
+    resolve: {
+      alias: {
+        'react': 'preact-compat',
+        'react-dom': 'preact-compat',
+        // Not necessary unless you consume a module using `createClass`
+        'create-react-class': 'preact-compat/lib/create-react-class',
+        // Not necessary unless you consume a module requiring `react-dom-factories`
+        'react-dom-factories': 'preact-compat/lib/react-dom-factories'
+      }
     }
   }
 }
