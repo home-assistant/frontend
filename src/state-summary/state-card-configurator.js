f917b5b395796bcf2a2aa48768ab50dd226922fb
Anton Verburg <info@tec-v.nl>
2018-06-01 15:55:00 +0200

    Localize ha-relative-time (#1241)
    * Localize relative time
    
    * Add mixin
    
    * Pass hass to ha-relative-time
    
    * Remove debug
    
    * Add hass
    
    * Add hass everywhere
    
    * Remove value from translation file
    
    * Add quotes to attributes
    
    * Lint
    
    * Fix in hassio file
    
    * Use current placeholder syntax
    
    * Use correct syntax
    
    * Move to ui.components

--- src/state-summary/state-card-configurator.js
+++ src/state-summary/state-card-configurator.js
@@ -33,12 +33,13 @@ class StateCardConfigurator extends PolymerElement {
 
   static get stateInfoTemplate() {
     return html`
-    <state-info state-obj="[[stateObj]]" in-dialog="[[inDialog]]"></state-info>
+    <state-info hass="[[hass]]" state-obj="[[stateObj]]" in-dialog="[[inDialog]]"></state-info>
 `;
   }
 
   static get properties() {
     return {
+      hass: Object,
       stateObj: Object,
       inDialog: {
         type: Boolean,
