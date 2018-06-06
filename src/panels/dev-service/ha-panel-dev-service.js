efbde5db1c3d00be26860cfe55c544723c8e766d
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2018-06-05 21:20:34 -0400

    Remove no longer needed blocks (#1262)

--- src/panels/dev-service/ha-panel-dev-service.js
+++ src/panels/dev-service/ha-panel-dev-service.js
@@ -12,293 +12,291 @@ import '../../components/ha-service-picker.js';
 import '../../resources/ha-style.js';
 import '../../util/app-localstorage-document.js';
 
-{
-  const ERROR_SENTINEL = {};
-  class HaPanelDevService extends PolymerElement {
-    static get template() {
-      return html`
-    <style include='ha-style'>
-      :host {
-        -ms-user-select: initial;
-        -webkit-user-select: initial;
-        -moz-user-select: initial;
-      }
+const ERROR_SENTINEL = {};
+class HaPanelDevService extends PolymerElement {
+  static get template() {
+    return html`
+  <style include='ha-style'>
+    :host {
+      -ms-user-select: initial;
+      -webkit-user-select: initial;
+      -moz-user-select: initial;
+    }
 
-      .content {
-        padding: 16px;
-      }
+    .content {
+      padding: 16px;
+    }
 
-      .ha-form {
-        margin-right: 16px;
-        max-width: 400px;
-      }
+    .ha-form {
+      margin-right: 16px;
+      max-width: 400px;
+    }
 
-      .description {
-        margin-top: 24px;
-        white-space: pre-wrap;
-      }
+    .description {
+      margin-top: 24px;
+      white-space: pre-wrap;
+    }
 
-      .header {
-        @apply --paper-font-title;
-      }
+    .header {
+      @apply --paper-font-title;
+    }
 
-      .attributes th {
-        text-align: left;
-      }
+    .attributes th {
+      text-align: left;
+    }
 
-      .attributes tr {
-        vertical-align: top;
-      }
+    .attributes tr {
+      vertical-align: top;
+    }
 
-      .attributes tr:nth-child(odd) {
-        background-color: var(--table-row-background-color,#eee)
-      }
+    .attributes tr:nth-child(odd) {
+      background-color: var(--table-row-background-color,#eee)
+    }
 
-      .attributes tr:nth-child(even) {
-        background-color: var(--table-row-alternative-background-color,#eee)
-      }
+    .attributes tr:nth-child(even) {
+      background-color: var(--table-row-alternative-background-color,#eee)
+    }
 
-      .attributes td:nth-child(3) {
-        white-space: pre-wrap;
-        word-break: break-word;
-      }
+    .attributes td:nth-child(3) {
+      white-space: pre-wrap;
+      word-break: break-word;
+    }
 
-      pre {
-        margin: 0;
-      }
+    pre {
+      margin: 0;
+    }
 
-      h1 {
-        white-space: normal;
-      }
+    h1 {
+      white-space: normal;
+    }
 
-      td {
-        padding: 4px;
-      }
+    td {
+      padding: 4px;
+    }
 
-      .error {
-        color: var(--google-red-500);
-      }
-    </style>
+    .error {
+      color: var(--google-red-500);
+    }
+  </style>
 
-    <app-header-layout has-scrolling-region>
-      <app-header slot="header" fixed>
-        <app-toolbar>
-          <ha-menu-button narrow='[[narrow]]' show-menu='[[showMenu]]'></ha-menu-button>
-          <div main-title>Services</div>
-        </app-toolbar>
-      </app-header>
+  <app-header-layout has-scrolling-region>
+    <app-header slot="header" fixed>
+      <app-toolbar>
+        <ha-menu-button narrow='[[narrow]]' show-menu='[[showMenu]]'></ha-menu-button>
+        <div main-title>Services</div>
+      </app-toolbar>
+    </app-header>
 
-      <app-localstorage-document
-        key='panel-dev-service-state-domain-service'
-        data='{{domainService}}'>
-      </app-localstorage-document>
-      <app-localstorage-document
-        key='[[_computeServicedataKey(domainService)]]'
-        data='{{serviceData}}'>
-      </app-localstorage-document>
+    <app-localstorage-document
+      key='panel-dev-service-state-domain-service'
+      data='{{domainService}}'>
+    </app-localstorage-document>
+    <app-localstorage-document
+      key='[[_computeServicedataKey(domainService)]]'
+      data='{{serviceData}}'>
+    </app-localstorage-document>
 
-      <div class='content'>
-        <p>
-          The service dev tool allows you to call any available service in Home Assistant.
-        </p>
+    <div class='content'>
+      <p>
+        The service dev tool allows you to call any available service in Home Assistant.
+      </p>
 
-        <div class='ha-form'>
-          <ha-service-picker
+      <div class='ha-form'>
+        <ha-service-picker
+          hass='[[hass]]'
+          value='{{domainService}}'
+        ></ha-service-picker>
+        <template is='dom-if' if='[[_computeHasEntity(_attributes)]]'>
+          <ha-entity-picker
             hass='[[hass]]'
-            value='{{domainService}}'
-          ></ha-service-picker>
-          <template is='dom-if' if='[[_computeHasEntity(_attributes)]]'>
-            <ha-entity-picker
-              hass='[[hass]]'
-              value='[[_computeEntityValue(parsedJSON)]]'
-              on-change='_entityPicked'
-              disabled='[[!validJSON]]'
-              domain-filter='[[_computeEntityDomainFilter(_domain)]]'
-              allow-custom-entity
-            ></ha-entity-picker>
-          </template>
-          <paper-textarea
-            always-float-label
-            label='Service Data (JSON, optional)'
-            value='{{serviceData}}'
-          ></paper-textarea>
-          <paper-button
-            on-click='_callService'
-            raised
+            value='[[_computeEntityValue(parsedJSON)]]'
+            on-change='_entityPicked'
             disabled='[[!validJSON]]'
-          >Call Service</paper-button>
-          <template is='dom-if' if='[[!validJSON]]'>
-              <span class='error'>Invalid JSON</span>
-          </template>
-        </div>
-
-        <template is='dom-if' if='[[!domainService]]'>
-          <h1>Select a service to see the description</h1>
+            domain-filter='[[_computeEntityDomainFilter(_domain)]]'
+            allow-custom-entity
+          ></ha-entity-picker>
         </template>
-
-        <template is='dom-if' if='[[domainService]]'>
-          <template is='dom-if' if='[[!_description]]'>
-            <h1>No description is available</h1>
-          </template>
-          <template is='dom-if' if='[[_description]]'>
-            <h3>[[_description]]</h3>
-
-            <table class='attributes'>
-              <tr>
-                <th>Parameter</th>
-                <th>Description</th>
-                <th>Example</th>
-              </tr>
-              <template is='dom-if' if='[[!_attributes.length]]'>
-                <tr><td colspan='3'>This service takes no parameters.</td></tr>
-              </template>
-              <template is='dom-repeat' items='[[_attributes]]' as='attribute'>
-                <tr>
-                  <td><pre>[[attribute.key]]</pre></td>
-                  <td>[[attribute.description]]</td>
-                  <td>[[attribute.example]]</td>
-                </tr>
-              </template>
-            </table>
-          </template>
+        <paper-textarea
+          always-float-label
+          label='Service Data (JSON, optional)'
+          value='{{serviceData}}'
+        ></paper-textarea>
+        <paper-button
+          on-click='_callService'
+          raised
+          disabled='[[!validJSON]]'
+        >Call Service</paper-button>
+        <template is='dom-if' if='[[!validJSON]]'>
+            <span class='error'>Invalid JSON</span>
         </template>
       </div>
 
-    </app-header-layout>
-      `;
-    }
+      <template is='dom-if' if='[[!domainService]]'>
+        <h1>Select a service to see the description</h1>
+      </template>
 
-    static get properties() {
-      return {
-        hass: {
-          type: Object,
-        },
+      <template is='dom-if' if='[[domainService]]'>
+        <template is='dom-if' if='[[!_description]]'>
+          <h1>No description is available</h1>
+        </template>
+        <template is='dom-if' if='[[_description]]'>
+          <h3>[[_description]]</h3>
 
-        narrow: {
-          type: Boolean,
-          value: false,
-        },
+          <table class='attributes'>
+            <tr>
+              <th>Parameter</th>
+              <th>Description</th>
+              <th>Example</th>
+            </tr>
+            <template is='dom-if' if='[[!_attributes.length]]'>
+              <tr><td colspan='3'>This service takes no parameters.</td></tr>
+            </template>
+            <template is='dom-repeat' items='[[_attributes]]' as='attribute'>
+              <tr>
+                <td><pre>[[attribute.key]]</pre></td>
+                <td>[[attribute.description]]</td>
+                <td>[[attribute.example]]</td>
+              </tr>
+            </template>
+          </table>
+        </template>
+      </template>
+    </div>
 
-        showMenu: {
-          type: Boolean,
-          value: false,
-        },
+  </app-header-layout>
+    `;
+  }
 
-        domainService: {
-          type: String,
-          observer: '_domainServiceChanged',
-        },
+  static get properties() {
+    return {
+      hass: {
+        type: Object,
+      },
 
-        _domain: {
-          type: String,
-          computed: '_computeDomain(domainService)',
-        },
+      narrow: {
+        type: Boolean,
+        value: false,
+      },
 
-        _service: {
-          type: String,
-          computed: '_computeService(domainService)',
-        },
+      showMenu: {
+        type: Boolean,
+        value: false,
+      },
 
-        serviceData: {
-          type: String,
-          value: '',
-        },
+      domainService: {
+        type: String,
+        observer: '_domainServiceChanged',
+      },
 
-        parsedJSON: {
-          type: Object,
-          computed: '_computeParsedServiceData(serviceData)'
-        },
+      _domain: {
+        type: String,
+        computed: '_computeDomain(domainService)',
+      },
 
-        validJSON: {
-          type: Boolean,
-          computed: '_computeValidJSON(parsedJSON)',
-        },
+      _service: {
+        type: String,
+        computed: '_computeService(domainService)',
+      },
 
-        _attributes: {
-          type: Array,
-          computed: '_computeAttributesArray(hass, _domain, _service)',
-        },
+      serviceData: {
+        type: String,
+        value: '',
+      },
 
-        _description: {
-          type: String,
-          computed: '_computeDescription(hass, _domain, _service)',
-        },
-      };
-    }
+      parsedJSON: {
+        type: Object,
+        computed: '_computeParsedServiceData(serviceData)'
+      },
 
-    _domainServiceChanged() {
-      this.serviceData = '';
-    }
+      validJSON: {
+        type: Boolean,
+        computed: '_computeValidJSON(parsedJSON)',
+      },
 
-    _computeAttributesArray(hass, domain, service) {
-      const serviceDomains = hass.config.services;
-      if (!(domain in serviceDomains)) return [];
-      if (!(service in serviceDomains[domain])) return [];
+      _attributes: {
+        type: Array,
+        computed: '_computeAttributesArray(hass, _domain, _service)',
+      },
 
-      const fields = serviceDomains[domain][service].fields;
-      return Object.keys(fields).map(function (field) {
-        return Object.assign({ key: field }, fields[field]);
-      });
-    }
+      _description: {
+        type: String,
+        computed: '_computeDescription(hass, _domain, _service)',
+      },
+    };
+  }
 
-    _computeDescription(hass, domain, service) {
-      const serviceDomains = hass.config.services;
-      if (!(domain in serviceDomains)) return undefined;
-      if (!(service in serviceDomains[domain])) return undefined;
-      return serviceDomains[domain][service].description;
-    }
+  _domainServiceChanged() {
+    this.serviceData = '';
+  }
 
-    _computeServicedataKey(domainService) {
-      return `panel-dev-service-state-servicedata.${domainService}`;
-    }
+  _computeAttributesArray(hass, domain, service) {
+    const serviceDomains = hass.config.services;
+    if (!(domain in serviceDomains)) return [];
+    if (!(service in serviceDomains[domain])) return [];
 
-    _computeDomain(domainService) {
-      return domainService.split('.', 1)[0];
-    }
+    const fields = serviceDomains[domain][service].fields;
+    return Object.keys(fields).map(function (field) {
+      return Object.assign({ key: field }, fields[field]);
+    });
+  }
 
-    _computeService(domainService) {
-      return domainService.split('.', 2)[1] || null;
-    }
+  _computeDescription(hass, domain, service) {
+    const serviceDomains = hass.config.services;
+    if (!(domain in serviceDomains)) return undefined;
+    if (!(service in serviceDomains[domain])) return undefined;
+    return serviceDomains[domain][service].description;
+  }
 
-    _computeParsedServiceData(serviceData) {
-      try {
-        return serviceData ? JSON.parse(serviceData) : {};
-      } catch (err) {
-        return ERROR_SENTINEL;
-      }
-    }
+  _computeServicedataKey(domainService) {
+    return `panel-dev-service-state-servicedata.${domainService}`;
+  }
 
-    _computeValidJSON(parsedJSON) {
-      return parsedJSON !== ERROR_SENTINEL;
-    }
+  _computeDomain(domainService) {
+    return domainService.split('.', 1)[0];
+  }
 
-    _computeHasEntity(attributes) {
-      return attributes.some(attr => attr.key === 'entity_id');
-    }
+  _computeService(domainService) {
+    return domainService.split('.', 2)[1] || null;
+  }
 
-    _computeEntityValue(parsedJSON) {
-      return parsedJSON === ERROR_SENTINEL ? '' : parsedJSON.entity_id;
-    }
-
-    _computeEntityDomainFilter(domain) {
-      return domain === 'homeassistant' ? null : domain;
-    }
-
-    _callService() {
-      if (this.parsedJSON === ERROR_SENTINEL) {
-        // eslint-disable-next-line
-        alert(`Error parsing JSON: ${this.serviceData}`);
-      }
-
-      this.hass.callService(this._domain, this._service, this.parsedJSON);
-    }
-
-    _entityPicked(ev) {
-      this.serviceData = JSON.stringify(Object.assign({}, this.parsedJSON, {
-        entity_id: ev.target.value
-      }), null, 2);
+  _computeParsedServiceData(serviceData) {
+    try {
+      return serviceData ? JSON.parse(serviceData) : {};
+    } catch (err) {
+      return ERROR_SENTINEL;
     }
   }
 
-  customElements.define('ha-panel-dev-service', HaPanelDevService);
+  _computeValidJSON(parsedJSON) {
+    return parsedJSON !== ERROR_SENTINEL;
+  }
+
+  _computeHasEntity(attributes) {
+    return attributes.some(attr => attr.key === 'entity_id');
+  }
+
+  _computeEntityValue(parsedJSON) {
+    return parsedJSON === ERROR_SENTINEL ? '' : parsedJSON.entity_id;
+  }
+
+  _computeEntityDomainFilter(domain) {
+    return domain === 'homeassistant' ? null : domain;
+  }
+
+  _callService() {
+    if (this.parsedJSON === ERROR_SENTINEL) {
+      // eslint-disable-next-line
+      alert(`Error parsing JSON: ${this.serviceData}`);
+    }
+
+    this.hass.callService(this._domain, this._service, this.parsedJSON);
+  }
+
+  _entityPicked(ev) {
+    this.serviceData = JSON.stringify(Object.assign({}, this.parsedJSON, {
+      entity_id: ev.target.value
+    }), null, 2);
+  }
 }
+
+customElements.define('ha-panel-dev-service', HaPanelDevService);
