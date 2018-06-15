54072e5e1d90b43db92b730fd30fbaa41c6c6ce3
Thibault Cohen <titilambert@gmail.com>
2018-06-15 11:17:46 -0400

    Add scheduler panel (#1146)
    * Add scheduler panel
    
    * Rename scheduler to calendar
    
    * Continue
    
    * First working version
    
    * Fix lint errors
    
    * Fix comments
    
    * Fix calendar display
    
    * Continue fixing comments
    
    * Continue fixing comments 2
    
    * Continue fixing comments 3
    
    * Fix UI to work with the new PRs
    
    * Use preact-compat instead of react
    
    * Fix tests
    
    * Move css file in a panels specific folder
    
    * Fix tests
    
    * Add Event details dialog
    
    * Improve date format
    
    * Remove event details
    
    * Continue
    
    * Continue calendar
    
    * continue
    
    * Continue
    
    * Cleaning
    
    * Fix PR comments
    
    * Lint
    
    * Clean useless import and css
    
    * Fix PR comments
    
    * Fix PR comments
    
    * Fix PR comments
    
    * Clean Event handler
    
    * Fix week view
    
    * Continue

---
+++ src/panels/calendar/ha-big-calendar.js
@@ -0,0 +1,83 @@
+import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+import { PolymerElement } from '@polymer/polymer/polymer-element.js';
+
+/* eslint-disable */
+import { render } from 'react-dom';
+import React from 'react';
+/* eslint-enable */
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
+import EventsMixin from '../../mixins/events-mixin.js';
+
+import '../../resources/ha-style.js';
+
+BigCalendar.setLocalizer(BigCalendar.momentLocalizer(moment));
+
+const DEFAULT_VIEW = 'month';
+
+class HaBigCalendar extends EventsMixin(PolymerElement) {
+  static get template() {
+    return html`
+      <link rel="stylesheet" href="/static/panels/calendar/react-big-calendar.css">
+      <style>
+        div#root {
+          height: 100%;
+          width: 100%;
+        }
+      </style>
+      <div id="root"></div>`;
+  }
+
+  update(events) {
+    const allViews = BigCalendar.Views.values;
+
+    const BCElement = React.createElement(
+      BigCalendar,
+      { events: events,
+        views: allViews,
+        popup: true,
+        onNavigate: (date, viewName) => this.fire('navigate', { date, viewName }),
+        onView: viewName => this.fire('view', { viewName }),
+        eventPropGetter: this.setEventStyle,
+        defaultView: this.defaultView,
+        defaultDate: new Date(),
+      }
+    );
+    render(BCElement, this.$.root);
+  }
+
+  setEventStyle(event) {
+    // https://stackoverflow.com/questions/34587067/change-color-of-react-big-calendar-events
+    const newStyle = {};
+    if (event.color) {
+      newStyle.backgroundColor = event.color;
+    }
+    return { style: newStyle };
+  }
+
+  static get properties() {
+    return {
+      dateUpdated: Object,
+
+      viewUpdated: Object,
+
+      defaultView: {
+        type: String,
+        value: DEFAULT_VIEW
+      },
+
+      defaultDate: {
+        type: Object,
+        value: new Date()
+      },
+
+      events: {
+        type: Array,
+        observer: 'update',
+      },
+
+    };
+  }
+}
+
+customElements.define('ha-big-calendar', HaBigCalendar);
