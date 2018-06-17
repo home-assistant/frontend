ff1008b15a7e9c2c374ab64881f820a20fa96a2d
Anton Verburg <info@tec-v.nl>
2018-06-16 22:32:05 -0400

    Cleanup deps (#1288)
    * Upgrade uglify-js
    
    * Remove unused deps
    
    * Add views

--- src/panels/experimental-ui/ha-panel-experimental-ui.js
+++ src/panels/experimental-ui/ha-panel-experimental-ui.js
@@ -2,35 +2,33 @@ import '@polymer/app-layout/app-header-layout/app-header-layout.js';
 import '@polymer/app-layout/app-header/app-header.js';
 import '@polymer/app-layout/app-toolbar/app-toolbar.js';
 import '@polymer/paper-icon-button/paper-icon-button.js';
+import '@polymer/paper-tabs/paper-tab.js';
+import '@polymer/paper-tabs/paper-tabs.js';
+import '@polymer/iron-icon/iron-icon.js';
 
 import { html } from '@polymer/polymer/lib/utils/html-tag.js';
 import { PolymerElement } from '@polymer/polymer/polymer-element.js';
 
-import './hui-view.js';
+import '../../layouts/hass-loading-screen.js';
+import './hui-root.js';
 
 class ExperimentalUI extends PolymerElement {
   static get template() {
     return html`
-    <style include='ha-style'>
-      app-header-layout {
-        height: 100%;
-      }
-    </style>
-    <app-header-layout>
-      <app-header slot="header" fixed>
-        <app-toolbar>
-          <ha-menu-button narrow='[[narrow]]' show-menu='[[showMenu]]'></ha-menu-button>
-          <div main-title>Experimental UI</div>
-          <paper-icon-button icon='hass:refresh' on-click='_fetchConfig'></paper-icon-button>
-        </app-toolbar>
-      </app-header>
-
-      <hui-view
-        hass='[[hass]]'
-        config='[[_curView]]'
-        columns='[[_columns]]'
-      ></hui-view>
-    </app-header-layout>
+      <template is='dom-if' if='[[!_config]]' restamp>
+        <hass-loading-screen
+          narrow="[[narrow]]"
+          show-menu="[[showMenu]]"
+        ></hass-loading-screen>
+      </template>
+      <template is='dom-if' if='[[_config]]' restamp>
+        <hui-root
+          hass='[[hass]]'
+          config='[[_config]]'
+          columns='[[_columns]]'
+          on-config-refresh='_fetchConfig'
+        ></hui-view>
+      </template>
     `;
   }
 
@@ -56,10 +54,7 @@ class ExperimentalUI extends PolymerElement {
       _config: {
         type: Object,
         value: null,
-        observer: '_configChanged',
       },
-
-      _curView: Object
     };
   }
 
@@ -84,12 +79,6 @@ class ExperimentalUI extends PolymerElement {
   _fetchConfig() {
     this.hass.connection.sendMessagePromise({ type: 'frontend/experimental_ui' })
       .then((conf) => { this._config = conf.result; });
-  }
-
-  _configChanged(config) {
-    if (!config) return;
-    // Currently hardcode to first view.
-    this._curView = config.views[0];
   }
 }
 
