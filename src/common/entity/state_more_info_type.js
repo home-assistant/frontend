c5b2bbb345affd66d0b181885f1a6938e3a240e9
c727 <crusader727@gmx.de>
2018-06-18 16:07:58 +0200

    Exp. UI: add support for more info dialog (#1294)
    * Exp. UI: add support for more info dialog
    
    * Sort items
    
    * Rename HIDE_MORE_INFO to DOMAINS_HIDE_MORE_INFO

--- src/common/entity/state_more_info_type.js
+++ src/common/entity/state_more_info_type.js
@@ -1,14 +1,5 @@
 import computeStateDomain from './compute_state_domain.js';
-
-const DOMAINS_WITH_MORE_INFO = [
-  'alarm_control_panel', 'automation', 'camera', 'climate', 'configurator',
-  'cover', 'fan', 'group', 'history_graph', 'light', 'lock', 'media_player', 'script',
-  'sun', 'updater', 'vacuum', 'input_datetime', 'weather'
-];
-
-const HIDE_MORE_INFO = [
-  'input_select', 'scene', 'input_number', 'input_text'
-];
+import { DOMAINS_HIDE_MORE_INFO, DOMAINS_WITH_MORE_INFO } from '../const.js';
 
 export default function stateMoreInfoType(stateObj) {
   const domain = computeStateDomain(stateObj);
@@ -16,7 +7,7 @@ export default function stateMoreInfoType(stateObj) {
   if (DOMAINS_WITH_MORE_INFO.includes(domain)) {
     return domain;
   }
-  if (HIDE_MORE_INFO.includes(domain)) {
+  if (DOMAINS_HIDE_MORE_INFO.includes(domain)) {
     return 'hidden';
   }
   return 'default';
