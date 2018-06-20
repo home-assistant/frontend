78ce2daf2e3cb9c012d2563ad14379d9cfb5eb0d
c727 <crusader727@gmx.de>
2018-06-20 21:08:01 +0200

    Exp. UI: Add glance card (#1305)
    * Exp. UI: Add vertical entities card
    
    * Remove iron-icon import
    
    * Add light
    
    * Change name
    
    * Flex
    
    * Fix CSS
    
    * Rebuild view to work with themes
    
    * Lint
    
    * Default title Home Assistant
    
    * Tweaks

--- src/panels/lovelace/hui-root.js
+++ src/panels/lovelace/hui-root.js
@@ -62,22 +62,27 @@ class HUIRoot extends EventsMixin(PolymerElement) {
         </div>
       </app-header>
 
-      <hui-view
-        hass='[[hass]]'
-        config='[[_computeViewConfig(config.views, _curView)]]'
-        columns='[[columns]]'
-      ></hui-view>
+      <span id='view'></span>
     </app-header-layout>
     `;
   }
 
   static get properties() {
     return {
-      hass: Object,
       narrow: Boolean,
       showMenu: Boolean,
-      config: Object,
-      columns: Number,
+      hass: {
+        type: Object,
+        observer: '_hassChanged',
+      },
+      config: {
+        type: Object,
+        observer: '_configChanged',
+      },
+      columns: {
+        type: Number,
+        observer: '_columnsChanged',
+      },
 
       _curView: {
         type: Number,
@@ -86,8 +91,13 @@ class HUIRoot extends EventsMixin(PolymerElement) {
     };
   }
 
+  ready() {
+    super.ready();
+    this._selectView(0);
+  }
+
   _computeTitle(config) {
-    return config.title || 'Experimental UI';
+    return config.name || 'Home Assistant';
   }
 
   _computeTabsHidden(views) {
@@ -98,17 +108,46 @@ class HUIRoot extends EventsMixin(PolymerElement) {
     return view.tab_title || view.name || 'Unnamed View';
   }
 
-  _computeViewConfig(views, _curView) {
-    return views[_curView];
-  }
-
   _handleRefresh() {
     this.fire('config-refresh');
   }
 
   _handleViewSelected(ev) {
-    this._curView = ev.detail.selected;
+    this._selectView(ev.detail.selected);
   }
+
+  _selectView(viewIndex) {
+    this._curView = viewIndex;
+
+    // Recreate a new element to clear the applied themes.
+    const root = this.$.view;
+    if (root.lastChild) {
+      root.removeChild(root.lastChild);
+    }
+    const view = document.createElement('hui-view');
+    view.setProperties({
+      hass: this.hass,
+      config: this.config.views[this._curView],
+      columns: this.columns,
+    });
+    root.appendChild(view);
+  }
+
+  _hassChanged(hass) {
+    if (!this.$.view.lastChild) return;
+    this.$.view.lastChild.hass = hass;
+  }
+
+  _configChanged() {
+    // On config change, recreate the view from scratch.
+    this._selectView(this._curView);
+  }
+
+  _columnsChanged(columns) {
+    if (!this.$.view.lastChild) return;
+    this.$.view.lastChild.columns = columns;
+  }
+
 
   /**
    * Scroll to a specific y coordinate.
