18b52b53cba0c71db2525fc7ac8426376094a35b
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2018-06-21 10:43:24 -0400

    Upgrade deps (#1311)
    * Update polyfills
    
    * Upgrade deps

--- hassio/webpack.config.js
+++ hassio/webpack.config.js
@@ -57,7 +57,7 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: [
-              ['es2015', { modules: false }]
+              [require('babel-preset-env').default, { modules: false }]
             ],
             plugins: [
               // Only support the syntax, Webpack will handle it.
