b58e52800c0c621a8875614ae24e2b9bf97667d8
Eu <covrig.catalin@gmail.com>
2018-06-24 10:26:31 +0200

    Update hui-iframe-card.js

--- src/panels/lovelace/cards/hui-iframe-card.js
+++ src/panels/lovelace/cards/hui-iframe-card.js
@@ -1,7 +1,7 @@
 import { html } from '@polymer/polymer/lib/utils/html-tag.js';
 import { PolymerElement } from '@polymer/polymer/polymer-element.js';
 
-class IframeCard extends PolymerElement {
+class HuiIframeCard extends PolymerElement {
   static get template() {
     return html`
       <style>
@@ -15,36 +15,11 @@ class IframeCard extends PolymerElement {
           width: 100%;
           border-radius: 2px;
         }
-        .title {
-          @apply --paper-font-common-nowrap;
-          position: relative;
-          left: 0;
-          right: 0;
-          bottom: 0;
-          padding: 10px;
-          line-height: 10px;
-          border-bottom-left-radius: 2px;
-          border-bottom-right-radius: 2px;
-          display: flex;
-          justify-content: space-between;
-        }
-        .title:empty {
-          display: none;
-        }
       </style>
       <ha-card>
-        <iframe height="[[config.height]]" src="[[config.url]]" frameborder="0"></iframe>
-        <div class="title" style="background-color:[[config.title-background-color]];color:[[config.title-color]];font-size:[[config.title-font-size]];font-weight:[[config.title-font-weight]];">[[config.title]]</div>
+        <iframe id="ifr" height="[[config.height]]" src="[[config.url]]" frameborder="0"></iframe>
       </ha-card>
     `;
   }
-  static get properties() {
-    return {
-      hass: Object,
-      config: {
-        type: Object,
-      },
-    };
-  }
 }
-customElements.define('hui-iframe-card', IframeCard);
+customElements.define('hui-iframe-card', HuiIframeCard);
