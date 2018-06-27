c6781faacd436c5d3a83ab358de5fcb39c58ef7d
Paulus Schoutsen <balloob@gmail.com>
2018-06-27 14:04:02 -0400

    Do not redraw unnecessarily

--- src/panels/lovelace/hui-root.js
+++ src/panels/lovelace/hui-root.js
@@ -149,8 +149,8 @@ class HUIRoot extends NavigateMixin(EventsMixin(PolymerElement)) {
     if (index !== this._curView) {
       const id = this.config.views[index].id || index;
       this.navigate(`/lovelace/${id}`);
+      this._selectView(index);
     }
-    this._selectView(index);
   }
 
   _selectView(viewIndex) {
