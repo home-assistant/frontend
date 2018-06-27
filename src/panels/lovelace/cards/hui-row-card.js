06a07fc0f0fed43274a451cadf2fbc95eaccb8cd
c727 <11984118+c727@users.noreply.github.com>
2018-06-28 00:10:06 +0200

    Add row card, also broken CSS

---
+++ src/panels/lovelace/cards/hui-row-card.js
@@ -0,0 +1,58 @@
+import createCardElement from '../common/create-card-element.js';
+import createErrorConfig from '../common/create-error-card-config.js';
+
+class HuiRowCard extends HTMLElement {
+  constructor() {
+    super();
+    this._elements = [];
+  }
+
+  getCardSize() {
+    return 7;
+  }
+
+  set config(config) {
+    this._elements = [];
+    const root = this;
+
+    while (root.lastChild) {
+      root.removeChild(root.lastChild);
+    }
+
+    if (config && config.cards && Array.isArray(config.cards)) {
+      const style = document.createElement('style');
+      style.innerHTML = `
+        .container {
+          display: flex;
+        }
+        div:last-child {
+          margin: 16px;
+        }
+      `;
+      root.appendChild(style);
+      const container = document.createElement('div');
+      container.classList.add('container');
+      root.appendChild(container);
+
+      const elements = [];
+      config.cards.forEach((card) => {
+        const element = createCardElement(card);
+        elements.push(element);
+        container.appendChild(element);
+      });
+      this._elements = elements;
+    } else {
+      const error = 'Card config incorrect.';
+      const element = createCardElement(createErrorConfig(error, config));
+      root.appendChild(element);
+    }
+  }
+
+  set hass(hass) {
+    this._elements.forEach((element) => {
+      element.hass = hass;
+    });
+  }
+}
+
+customElements.define('hui-row-card', HuiRowCard);
