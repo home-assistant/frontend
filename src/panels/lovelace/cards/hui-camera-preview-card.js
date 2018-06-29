7f2abc7e7ae886ef2dac1e75b8bd2a21b148602b
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2018-06-29 15:35:46 -0400

    Random fixes (#1369)
    * Random fixes
    
    * weather card to use legacy wrapper
    
    * Lint

--- src/panels/lovelace/cards/hui-camera-preview-card.js
+++ src/panels/lovelace/cards/hui-camera-preview-card.js
@@ -1,54 +1,14 @@
-import { PolymerElement } from '@polymer/polymer/polymer-element.js';
-
 import '../../../cards/ha-camera-card.js';
 
-import validateEntityConfig from '../common/validate-entity-config.js';
+import LegacyWrapperCard from './hui-legacy-wrapper-card.js';
 
-class HuiCameraPreviewCard extends PolymerElement {
-  static get properties() {
-    return {
-      hass: {
-        type: Object,
-        observer: '_hassChanged'
-      },
-    };
+class HuiCameraPreviewCard extends LegacyWrapperCard {
+  constructor() {
+    super('ha-camera-card', 'camera');
   }
 
   getCardSize() {
     return 4;
-  }
-
-  setConfig(config) {
-    if (!validateEntityConfig(config, 'camera')) {
-      throw new Error('Error in card configuration.');
-    }
-
-    this._config = config;
-    this._entityId = null;
-
-    if (this.lastChild) {
-      this.removeChild(this.lastChild);
-    }
-
-    const entityId = config.entity;
-    if (!(entityId in this.hass.states)) {
-      return;
-    }
-
-    const element = document.createElement('ha-camera-card');
-    element.stateObj = this.hass.states[entityId];
-    element.hass = this.hass;
-    this.appendChild(element);
-    this._entityId = entityId;
-  }
-
-  _hassChanged(hass) {
-    const entityId = this._entityId;
-    if (entityId && entityId in hass.states) {
-      const element = this.lastChild;
-      element.stateObj = hass.states[entityId];
-      element.hass = hass;
-    }
   }
 }
 
