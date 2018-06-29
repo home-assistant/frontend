199d21d9382f6ea757a43e188509cdc566303ec5
c727 <11984118+c727@users.noreply.github.com>
2018-06-29 03:32:33 +0200

    Love: more cleanup

--- src/panels/lovelace/components/hui-entities-toggle.js
+++ src/panels/lovelace/components/hui-entities-toggle.js
@@ -3,8 +3,8 @@ import { PolymerElement } from '@polymer/polymer/polymer-element.js';
 import '@polymer/paper-toggle-button/paper-toggle-button.js';
 
 import canToggleState from '../../../common/entity/can_toggle_state.js';
-import computeDomain from '../../../common/entity/compute_domain.js';
-import { STATES_ON } from '../../../common/const.js';
+import turnOnOffEntities from '../common/entity/turn-on-off-entities.js'
+import { STATES_OFF } from '../../../common/const.js';
 
 class HuiEntitiesToggle extends PolymerElement {
   static get template() {
@@ -38,46 +38,18 @@ class HuiEntitiesToggle extends PolymerElement {
     };
   }
 
-  _computeToggleEntities(hass, entities) {
-    return entities.filter(entity => (entity in hass.states ?
-      canToggleState(hass, hass.states[entity]) : false));
+  _computeToggleEntities(hass, entityIds) {
+    return entityIds.filter(entityId => (entityId in hass.states ?
+      canToggleState(hass, hass.states[entityId]) : false));
   }
 
-  _computeIsChecked(hass, entities) {
-    return entities.some(entity => STATES_ON.includes(hass.states[entity].state));
+  _computeIsChecked(hass, entityIds) {
+    return entityIds.some(entityId => !STATES_OFF.includes(hass.states[entityId].state));
   }
 
   _callService(ev) {
     const turnOn = ev.target.checked;
-    const toCall = {};
-
-    this.entities.forEach((entity) => {
-      if ((STATES_ON.includes(this.hass.states[entity].state)) !== turnOn) {
-        const stateDomain = computeDomain(entity);
-        const serviceDomain = stateDomain === 'lock' || stateDomain === 'cover' ?
-          stateDomain : 'homeassistant';
-
-        if (!(serviceDomain in toCall)) toCall[serviceDomain] = [];
-        toCall[serviceDomain].push(entity);
-      }
-    });
-
-    Object.keys(toCall).forEach((domain) => {
-      let service;
-      switch (domain) {
-        case 'lock':
-          service = turnOn ? 'unlock' : 'lock';
-          break;
-        case 'cover':
-          service = turnOn ? 'open_cover' : 'close_cover';
-          break;
-        default:
-          service = turnOn ? 'turn_on' : 'turn_off';
-      }
-
-      const entities = toCall[domain];
-      this.hass.callService(domain, service, { entity_id: entities });
-    });
+    turnOnOffEntities(hass, this._toggleEntities, turnOn);
   }
 }
 
