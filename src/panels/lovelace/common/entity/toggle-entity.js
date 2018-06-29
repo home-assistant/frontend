048b0d78b781933d046ec35a7e53d3a8c157280c
c727 <crusader727@gmx.de>
2018-06-29 04:30:17 +0200

    Love: more cleanup (#1361)
    * Love: more cleanup
    
    * Fix
    
    * Lint

---
+++ src/panels/lovelace/common/entity/toggle-entity.js
@@ -0,0 +1,7 @@
+import { STATES_OFF } from '../../../../common/const.js';
+import turnOnOffEntity from './turn-on-off-entity.js';
+
+export default function toggleEntity(hass, entityId) {
+  const turnOn = STATES_OFF.includes(hass.states[entityId].state);
+  turnOnOffEntity(hass, entityId, turnOn);
+}
