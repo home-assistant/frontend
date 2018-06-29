7de27ef9239762f16c66c361a79b7003461296c8
Paulus Schoutsen <balloob@gmail.com>
2018-06-29 15:19:14 -0400

    weather card to use legacy wrapper

--- src/panels/lovelace/cards/hui-weather-forecast-card.js
+++ src/panels/lovelace/cards/hui-weather-forecast-card.js
@@ -1,46 +1,14 @@
-import { PolymerElement } from '@polymer/polymer/polymer-element.js';
+import '../../../cards/ha-camera-card.js';
 
-import '../../../cards/ha-weather-card.js';
+import LegacyWrapperCard from './hui-legacy-wrapper-card.js';
 
-import validateEntityConfig from '../common/validate-entity-config.js';
-
-class HuiWeatherForecastCard extends PolymerElement {
-  static get properties() {
-    return {
-      hass: {
-        type: Object,
-        observer: '_hassChanged'
-      },
-    };
+class HuiWeatherForecastCard extends LegacyWrapperCard {
+  constructor() {
+    super('ha-weather-card', 'weather');
   }
 
   getCardSize() {
     return 4;
-  }
-
-  setConfig(config) {
-    if (!validateEntityConfig(config, 'weather')) {
-      throw new Error('Error in card configuration.');
-    }
-
-    this._entityId = null;
-
-    if (this.lastChild) {
-      this.removeChild(this.lastChild);
-    }
-
-    const element = document.createElement('ha-weather-card');
-    this.appendChild(element);
-    this._entityId = config.entity;
-  }
-
-  _hassChanged(hass) {
-    const entityId = this._entityId;
-    if (entityId in hass.states) {
-      const element = this.lastChild;
-      element.stateObj = hass.states[entityId];
-      element.hass = hass;
-    }
   }
 }
 
