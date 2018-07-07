d96c5f6bdefe9c2a27124587e113bbd739073863
c727 <crusader727@gmx.de>
2018-07-07 13:25:06 +0200

    Don't modify stateObj (#1410)
    * Don't modify stateObj
    
    * Lint

--- src/common/entity/compute_state_domain.js
+++ src/common/entity/compute_state_domain.js
@@ -1,9 +1,5 @@
 import computeDomain from './compute_domain.js';
 
 export default function computeStateDomain(stateObj) {
-  if (!stateObj._domain) {
-    stateObj._domain = computeDomain(stateObj.entity_id);
-  }
-
-  return stateObj._domain;
+  return computeDomain(stateObj.entity_id);
 }
