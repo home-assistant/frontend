b4860f0492603815cbb9c92a271f2a01a41b283e
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2018-07-09 18:24:49 +0200

    Use IndieAuth for client ID (#1427)
    * Use IndieAuth for client ID
    
    * Lint

--- src/common/auth/refresh_token.js
+++ src/common/auth/refresh_token.js
@@ -1,12 +1,10 @@
 export default function refreshAccessToken(clientId, refreshToken) {
   const data = new FormData();
+  data.append('client_id', clientId);
   data.append('grant_type', 'refresh_token');
   data.append('refresh_token', refreshToken);
   return fetch('/auth/token', {
     method: 'POST',
-    headers: {
-      authorization: `Basic ${btoa(clientId)}`
-    },
     body: data,
   }).then((resp) => {
     if (!resp.ok) throw new Error('Unable to fetch tokens');
