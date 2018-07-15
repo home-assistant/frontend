93e9e2e4405d1dac36efc177c13e3fab7288c28f
c727 <11984118+c727@users.noreply.github.com>
2018-07-15 20:16:25 +0200

    Glance: Replace turn-on with toggle for scenes

--- src/panels/lovelace/common/entity/toggle-entity.js
+++ src/panels/lovelace/common/entity/toggle-entity.js
@@ -2,6 +2,7 @@ import { STATES_OFF } from '../../../../common/const.js';
 import turnOnOffEntity from './turn-on-off-entity.js';
 
 export default function toggleEntity(hass, entityId) {
-  const turnOn = STATES_OFF.includes(hass.states[entityId].state);
+  const turnOn = entityId.split('.', 1)[0] === 'scene' ?
+    true : STATES_OFF.includes(hass.states[entityId].state);
   turnOnOffEntity(hass, entityId, turnOn);
 }
