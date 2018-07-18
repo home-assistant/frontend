418613a71379427713611cf9fa3b1f8cf49fdecd
c727 <crusader727@gmx.de>
2018-07-18 17:25:13 +0200

    Separate row entity elements for Lovelace (#1461)
    * Separate row entity elements for Lovelace
    
    * Update
    
    * Update
    
    * Add fan
    
    * Add support for last changed
    
    * Feedback
    
    * Feedback2
    
    * Update
    
    * Update
    
    * Fix timer
    
    * Update
    
    * Lint

---
+++ src/panels/lovelace/entity-rows/hui-scene-entity-row.js
@@ -0,0 +1,56 @@
+import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+import { PolymerElement } from '@polymer/polymer/polymer-element.js';
+import '@polymer/paper-button/paper-button.js';
+
+import '../components/hui-generic-entity-row.js';
+
+import LocalizeMixin from '../../../mixins/localize-mixin.js';
+
+/*
+ * @appliesMixin LocalizeMixin
+ */
+class HuiSceneEntityRow extends LocalizeMixin(PolymerElement) {
+  static get template() {
+    return html`
+      <style>
+        paper-button {
+          color: var(--primary-color);
+          font-weight: 500;
+          margin: 0;
+        }
+      </style>
+      <hui-generic-entity-row
+        hass="[[hass]]"
+        config="[[_config]]"
+      >
+        <paper-button on-click="_callService">
+          [[localize('ui.card.scene.activate')]]
+        </paper-button>
+      </hui-generic-entity-row>
+    `;
+  }
+
+  static get properties() {
+    return {
+      hass: Object,
+      _config: Object
+    };
+  }
+
+  _computeStateObj(states, entityId) {
+    return states && entityId in states ? states[entityId] : null;
+  }
+
+  setConfig(config) {
+    if (!config || !config.entity) {
+      throw new Error('Entity not configured.');
+    }
+    this._config = config;
+  }
+
+  _callService(ev) {
+    ev.stopPropagation();
+    this.hass.callService('scene', 'turn_on', { entity_id: this._config.entity });
+  }
+}
+customElements.define('hui-scene-entity-row', HuiSceneEntityRow);
