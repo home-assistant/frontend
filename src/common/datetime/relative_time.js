76a490a1a06e70042b0ab77053526616beb6975e
c727 <11984118+c727@users.noreply.github.com>
2018-07-18 05:03:11 +0200

    Use separate duration for relative time

--- src/common/datetime/relative_time.js
+++ src/common/datetime/relative_time.js
@@ -9,7 +9,7 @@ const tests = [
   7, 'day',
 ];
 
-export default function relativeTime(dateObj) {
+export default function relativeTime(dateObj, localize) {
   let delta = (new Date() - dateObj) / 1000;
   const tense = delta >= 0 ? 'past' : 'future';
   delta = Math.abs(delta);
@@ -17,20 +17,14 @@ export default function relativeTime(dateObj) {
   for (let i = 0; i < tests.length; i += 2) {
     if (delta < tests[i]) {
       delta = Math.floor(delta);
-      return {
-        tense,
-        value: delta,
-        unit: tests[i + 1]
-      };
+      const time = localize(`ui.components.relative_time.duration.${tests[i + 1]}`, 'count', delta);
+      return localize(`ui.components.relative_time.${tense}`, 'time', time);
     }
 
     delta /= tests[i];
   }
 
   delta = Math.floor(delta);
-  return {
-    tense,
-    value: delta,
-    unit: 'week'
-  };
+  const time = localize('ui.components.relative_time.duration.week', 'count', delta);
+  return localize(`ui.components.relative_time.${tense}`, 'time', time);
 }
