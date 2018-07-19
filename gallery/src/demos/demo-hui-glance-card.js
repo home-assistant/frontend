49d66e63c28a32cfafa6a214ad2d86adce03f43b
c727 <crusader727@gmx.de>
2018-07-19 22:57:51 +0200

    Add more features to gallery (#1477)
    * Add more features to gallery
    
    * Feedback
    
    * Fix margin
    
    * Use array-style config
    
    * Split into DemoCards and DemoCard
    
    * Move padding
    
    * Use min-height

--- gallery/src/demos/demo-hui-glance-card.js
+++ gallery/src/demos/demo-hui-glance-card.js
@@ -1,77 +1,146 @@
 import { html } from '@polymer/polymer/lib/utils/html-tag.js';
 import { PolymerElement } from '@polymer/polymer/polymer-element.js';
 
-import '../../../src/panels/lovelace/cards/hui-glance-card.js';
-
-import HomeAssistant from '../data/hass.js';
-import demoStates from '../data/demo_dump.js';
+import '../components/demo-cards.js';
 
 const CONFIGS = [
   {
-    entities: [
-      'binary_sensor.movement_backyard',
-      'light.bed_light',
-      'binary_sensor.basement_floor_wet',
-      'sensor.outside_temperature',
-      'light.ceiling_lights',
-      'switch.ac',
-      'lock.kitchen_door'
-    ],
-    type: 'glance',
-    title: 'Glance card sample'
+    heading: 'Basic example',
+    config: `
+- type: glance
+  entities:
+    - device_tracker.demo_paulus
+    - media_player.living_room
+    - sun.sun
+    - cover.kitchen_window
+    - light.kitchen_lights
+    - lock.kitchen_door
+    - light.ceiling_lights
+    `
   },
   {
-    entities: [
-      'binary_sensor.movement_backyard',
-      'light.bed_light',
-      'binary_sensor.basement_floor_wet',
-      'sensor.outside_temperature',
-      'light.ceiling_lights',
-      'switch.ac',
-      'lock.kitchen_door'
-    ],
-    type: 'glance',
+    heading: 'With title',
+    config: `
+- type: glance
+  title: This is glance
+  entities:
+    - device_tracker.demo_paulus
+    - media_player.living_room
+    - sun.sun
+    - cover.kitchen_window
+    - light.kitchen_lights
+    - lock.kitchen_door
+    - light.ceiling_lights
+    `
   },
   {
-    entities: [
-      'lock.kitchen_door'
-    ],
-    type: 'glance',
+    heading: 'Custom column width',
+    config: `
+- type: glance
+  column_width: calc(100% / 7)
+  entities:
+    - device_tracker.demo_paulus
+    - media_player.living_room
+    - sun.sun
+    - cover.kitchen_window
+    - light.kitchen_lights
+    - lock.kitchen_door
+    - light.ceiling_lights
+    `
   },
-
+  {
+    heading: 'No name',
+    config: `
+- type: glance
+  show_name: false
+  entities:
+    - device_tracker.demo_paulus
+    - media_player.living_room
+    - sun.sun
+    - cover.kitchen_window
+    - light.kitchen_lights
+    - lock.kitchen_door
+    - light.ceiling_lights
+    `
+  },
+  {
+    heading: 'No state',
+    config: `
+- type: glance
+  show_state: false
+  entities:
+    - device_tracker.demo_paulus
+    - media_player.living_room
+    - sun.sun
+    - cover.kitchen_window
+    - light.kitchen_lights
+    - lock.kitchen_door
+    - light.ceiling_lights
+    `
+  },
+  {
+    heading: 'No name and no state',
+    config: `
+- type: glance
+  show_name: false
+  show_state: false
+  entities:
+    - device_tracker.demo_paulus
+    - media_player.living_room
+    - sun.sun
+    - cover.kitchen_window
+    - light.kitchen_lights
+    - lock.kitchen_door
+    - light.ceiling_lights
+    `
+  },
+  {
+    heading: 'Custom name',
+    config: `
+- type: glance
+  entities:
+    - entity: device_tracker.demo_paulus
+      name: ¯\\_(ツ)_/¯
+    - media_player.living_room
+    - sun.sun
+    - cover.kitchen_window
+    - light.kitchen_lights
+    - lock.kitchen_door
+    - light.ceiling_lights
+    `
+  },
+  {
+    heading: 'Custom tap action',
+    config: `
+- type: glance
+  entities:
+    - entity: lock.kitchen_door
+      tap_action: toggle
+    - entity: light.ceiling_lights
+      tap_action: turn-on
+    - device_tracker.demo_paulus
+    - media_player.living_room
+    - sun.sun
+    - cover.kitchen_window
+    - light.kitchen_lights
+    `
+  },
 ];
 
 class DemoPicEntity extends PolymerElement {
   static get template() {
     return html`
-      <style>
-        #root {
-        }
-        hui-glance-card {
-          width: 400px;
-          display: inline-block;
-          margin-left: 20px;
-          margin-top: 20px;
-        }
-      </style>
-      <div id='root'>
-      </div>
+      <demo-cards type="hui-glance-card" configs="[[_configs]]"></demo-cards>
     `;
   }
 
-  ready() {
-    super.ready();
-
-    const root = this.$.root;
-    const hass = new HomeAssistant();
-    hass.states = demoStates;
-    console.log(demoStates);
-    CONFIGS.forEach((config) => {
-      const el = document.createElement('hui-glance-card');
-      el.setConfig(config);
-      el.hass = hass;
-      root.appendChild(el);
-    });
+  static get properties() {
+    return {
+      _configs: {
+        type: Object,
+        value: CONFIGS
+      }
+    };
   }
 }
 
