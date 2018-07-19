dbebd1308ca0fa111003227657d4b87c890656e7
c727 <11984118+c727@users.noreply.github.com>
2018-07-19 19:47:59 +0200

    Add more features to gallery

--- gallery/src/demos/demo-hui-glance-card.js
+++ gallery/src/demos/demo-hui-glance-card.js
@@ -8,37 +8,132 @@ import demoStates from '../data/demo_dump.js';
 
 const CONFIGS = [
   {
-    entities: [
-      'binary_sensor.movement_backyard',
-      'light.bed_light',
-      'binary_sensor.basement_floor_wet',
-      'sensor.outside_temperature',
-      'light.ceiling_lights',
-      'switch.ac',
-      'lock.kitchen_door'
-    ],
-    type: 'glance',
-    title: 'Glance card sample'
+    heading: 'Basic example',
+    yaml: `- type: entities
+    title: Entities card sample
+    show_header_toggle: true
+    entities:
+      - entity: alarm_control_panel.alarm
+        name: Alarm Panel
+      - device_tracker.demo_paulus
+      - switch.decorative_lights
+      - group.all_lights
+      - group.all_locks`,
+    json: {
+      entities: [
+        'binary_sensor.movement_backyard',
+        'light.bed_light',
+        'binary_sensor.basement_floor_wet',
+        'sensor.outside_temperature',
+        'light.ceiling_lights',
+        'switch.ac',
+        'lock.kitchen_door'
+      ],
+      type: 'glance',
+      title: 'Glance card sample'
+    }
   },
   {
-    entities: [
-      'binary_sensor.movement_backyard',
-      'light.bed_light',
-      'binary_sensor.basement_floor_wet',
-      'sensor.outside_temperature',
-      'light.ceiling_lights',
-      'switch.ac',
-      'lock.kitchen_door'
-    ],
-    type: 'glance',
-  },
-  {
-    entities: [
-      'lock.kitchen_door'
-    ],
-    type: 'glance',
-  },
+    heading: 'Without title',
+    yaml: `- type: entities
+    title: Entities card sample
+    show_header_toggle: true
+    entities:
+      - entity: alarm_control_panel.alarm
+        name: Alarm Panel
+      - device_tracker.demo_paulus
+      - switch.decorative_lights
+      - group.all_lights
+      - group.all_locks`,
+    json: {
+      entities: [
+        'binary_sensor.movement_backyard',
+        'light.bed_light',
+        'binary_sensor.basement_floor_wet',
+        'sensor.outside_temperature',
+        'light.ceiling_lights',
+        'switch.ac',
+        'lock.kitchen_door'
+      ],
+      type: 'glance',
+    }
+  }
 
+  ,{
+    heading: 'Without title',
+    yaml: `- type: entities
+    title: Entities card sample
+    show_header_toggle: true
+    entities:
+      - entity: alarm_control_panel.alarm
+        name: Alarm Panel
+      - device_tracker.demo_paulus
+      - switch.decorative_lights
+      - group.all_lights
+      - group.all_locks`,
+    json: {
+      entities: [
+        'binary_sensor.movement_backyard',
+        'light.bed_light',
+        'binary_sensor.basement_floor_wet',
+        'sensor.outside_temperature',
+        'light.ceiling_lights',
+        'switch.ac',
+        'lock.kitchen_door'
+      ],
+      type: 'glance',
+    }
+  },
+  {
+    heading: 'Without title',
+    yaml: `- type: entities
+    title: Entities card sample
+    show_header_toggle: true
+    entities:
+      - entity: alarm_control_panel.alarm
+        name: Alarm Panel
+      - device_tracker.demo_paulus
+      - switch.decorative_lights
+      - group.all_lights
+      - group.all_locks`,
+    json: {
+      entities: [
+        'binary_sensor.movement_backyard',
+        'light.bed_light',
+        'binary_sensor.basement_floor_wet',
+        'sensor.outside_temperature',
+        'light.ceiling_lights',
+        'switch.ac',
+        'lock.kitchen_door'
+      ],
+      type: 'glance',
+    }
+  },
+  {
+    heading: 'Without title',
+    yaml: `- type: entities
+    title: Entities card sample
+    show_header_toggle: true
+    entities:
+      - entity: alarm_control_panel.alarm
+        name: Alarm Panel
+      - device_tracker.demo_paulus
+      - switch.decorative_lights
+      - group.all_lights
+      - group.all_locks`,
+    json: {
+      entities: [
+        'binary_sensor.movement_backyard',
+        'light.bed_light',
+        'binary_sensor.basement_floor_wet',
+        'sensor.outside_temperature',
+        'light.ceiling_lights',
+        'switch.ac',
+        'lock.kitchen_door'
+      ],
+      type: 'glance',
+    }
+  }
 ];
 
 class DemoPicEntity extends PolymerElement {
@@ -46,15 +141,23 @@ class DemoPicEntity extends PolymerElement {
     return html`
       <style>
         #root {
+          display: flex;
+          flex-wrap: wrap;
+        }
+        div {
+          flex: 0 1 600px;
+          padding: 8px;
+        }
+        h2 {
+          margin: 0;
+          color: #03a9f4;
         }
         hui-glance-card {
-          width: 400px;
-          display: inline-block;
           margin-left: 20px;
           margin-top: 20px;
         }
       </style>
-      <div id='root'>
+      <div id="root">
       </div>
     `;
   }
@@ -66,11 +169,19 @@ class DemoPicEntity extends PolymerElement {
     const hass = new HomeAssistant();
     hass.states = demoStates;
     console.log(demoStates);
-    CONFIGS.forEach((config) => {
+    CONFIGS.forEach((item) => {
+      const container = document.createElement('div');
+      const heading = document.createElement('h2');
+      heading.innerText = item.heading;
+      container.appendChild(heading);
       const el = document.createElement('hui-glance-card');
-      el.setConfig(config);
+      el.setConfig(item.json);
       el.hass = hass;
-      root.appendChild(el);
+      container.appendChild(el);
+      const yaml = document.createElement('pre');
+      yaml.innerText = item.yaml;
+      container.appendChild(yaml);
+      root.appendChild(container);
     });
   }
 }
