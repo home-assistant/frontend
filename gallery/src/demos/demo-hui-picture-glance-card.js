77de8814290973889aabf32104ac794e384e4cb3
c727 <11984118+c727@users.noreply.github.com>
2018-07-20 02:56:17 +0200

    Add more gallery cards

--- gallery/src/demos/demo-hui-picture-glance-card.js
+++ gallery/src/demos/demo-hui-picture-glance-card.js
@@ -1,91 +1,92 @@
 import { html } from '@polymer/polymer/lib/utils/html-tag.js';
 import { PolymerElement } from '@polymer/polymer/polymer-element.js';
 
-import '../../../src/panels/lovelace/cards/hui-picture-glance-card.js';
-
-import HomeAssistant from '../data/hass.js';
-import demoStates from '../data/demo_dump.js';
+import '../components/demo-cards.js';
 
 const CONFIGS = [
   {
-    type: 'picture-glance',
-    image: 'https://images.pexels.com/photos/276724/pexels-photo-276724.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=240&w=495',
-    title: 'Picture glance',
-    entities: [
-      'switch.decorative_lights',
-      'light.ceiling_lights',
-      'binary_sensor.movement_backyard',
-      'binary_sensor.basement_floor_wet',
-    ]
+    heading: 'Title, dialog, toggle',
+    config: `
+- type: picture-glance
+  image: https://images.pexels.com/photos/276724/pexels-photo-276724.jpeg
+  title: Living room
+  entities:
+    - switch.decorative_lights
+    - light.ceiling_lights
+    - binary_sensor.movement_backyard
+    - binary_sensor.basement_floor_wet
+    `
   },
   {
-    type: 'picture-glance',
-    image: 'https://images.pexels.com/photos/276724/pexels-photo-276724.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=240&w=495',
-    entities: [
-      'switch.decorative_lights',
-      'light.ceiling_lights',
-      'binary_sensor.movement_backyard',
-      'binary_sensor.basement_floor_wet',
-    ]
+    heading: 'Title, dialog, no toggle',
+    config: `
+- type: picture-glance
+  image: https://images.pexels.com/photos/276724/pexels-photo-276724.jpeg
+  title: Living room
+  entities:
+    - binary_sensor.movement_backyard
+    - binary_sensor.basement_floor_wet
+    `
   },
   {
-    type: 'picture-glance',
-    image: 'https://images.pexels.com/photos/276724/pexels-photo-276724.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=240&w=495',
-    title: 'Picture glance',
-    entities: [
-      'switch.decorative_lights',
-      'light.ceiling_lights',
-    ]
+    heading: 'Title, no dialog, toggle',
+    config: `
+- type: picture-glance
+  image: https://images.pexels.com/photos/276724/pexels-photo-276724.jpeg
+  title: Living room
+  entities:
+    - switch.decorative_lights
+    - light.ceiling_lights
+    `
   },
   {
-    type: 'picture-glance',
-    image: 'https://images.pexels.com/photos/276724/pexels-photo-276724.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=240&w=495',
-    entities: [
-      'switch.decorative_lights',
-      'light.ceiling_lights',
-    ]
+    heading: 'No title, dialog, toggle',
+    config: `
+- type: picture-glance
+  image: https://images.pexels.com/photos/276724/pexels-photo-276724.jpeg
+  entities:
+    - switch.decorative_lights
+    - light.ceiling_lights
+    - binary_sensor.movement_backyard
+    - binary_sensor.basement_floor_wet
+    `
   },
   {
-    type: 'picture-glance',
-    image: 'https://images.pexels.com/photos/276724/pexels-photo-276724.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=240&w=495',
-    entities: [
-      'binary_sensor.movement_backyard',
-      'binary_sensor.basement_floor_wet',
-    ]
+    heading: 'No title, dialog, no toggle',
+    config: `
+- type: picture-glance
+  image: https://images.pexels.com/photos/276724/pexels-photo-276724.jpeg
+  entities:
+    - binary_sensor.movement_backyard
+    - binary_sensor.basement_floor_wet
+    `
+  },
+  {
+    heading: 'No title, no dialog, toggle',
+    config: `
+- type: picture-glance
+  image: https://images.pexels.com/photos/276724/pexels-photo-276724.jpeg
+  entities:
+    - switch.decorative_lights
+    - light.ceiling_lights
+    `
   },
 ];
 
 class DemoPicGlance extends PolymerElement {
   static get template() {
     return html`
-      <style>
-        #root {
-          padding: 10px;
-        }
-        hui-picture-glance-card {
-          width: 400px;
-          display: inline-block;
-          margin: 10px;
-        }
-      </style>
-      <div id='root'>
-      </div>
+      <demo-cards configs="[[_configs]]"></demo-cards>
     `;
   }
 
-  ready() {
-    super.ready();
-
-    const root = this.$.root;
-    const hass = new HomeAssistant();
-    hass.states = demoStates;
-    console.log(demoStates);
-    CONFIGS.forEach((config) => {
-      const el = document.createElement('hui-picture-glance-card');
-      el.setConfig(config);
-      el.hass = hass;
-      root.appendChild(el);
-    });
+  static get properties() {
+    return {
+      _configs: {
+        type: Object,
+        value: CONFIGS
+      }
+    };
   }
 }
 
