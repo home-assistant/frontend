8f3c0decc195ff35ce7358151e63820dd6b4963b
Jerad Meisner <jerad.meisner@gmail.com>
2018-07-20 03:04:11 -0700

    Add custom elements to picture-elements. (#1482)

--- src/panels/lovelace/common/create-hui-element.js
+++ src/panels/lovelace/common/create-hui-element.js
@@ -5,8 +5,10 @@ import '../elements/hui-state-badge-element.js';
 import '../elements/hui-state-icon-element.js';
 import '../elements/hui-state-label-element.js';
 
+import fireEvent from '../../../common/dom/fire_event.js';
 import createErrorCardConfig from './create-error-card-config.js';
 
+const CUSTOM_TYPE_PREFIX = 'custom:';
 const ELEMENT_TYPES = new Set([
   'icon',
   'image',
@@ -36,6 +38,21 @@ function _createErrorElement(error, config) {
 export default function createHuiElement(config) {
   if (!config || typeof config !== 'object' || !config.type) {
     return _createErrorElement('No element type configured.', config);
+  }
+
+  if (config.type.startsWith(CUSTOM_TYPE_PREFIX)) {
+    const tag = config.type.substr(CUSTOM_TYPE_PREFIX.length);
+
+    if (customElements.get(tag)) {
+      return _createElement(tag, config);
+    }
+
+    const element = _createErrorElement(`Custom element doesn't exist: ${tag}.`, config);
+
+    customElements.whenDefined(tag)
+      .then(() => fireEvent(element, 'rebuild-view'));
+
+    return element;
   }
 
   if (!ELEMENT_TYPES.has(config.type)) {
