aa51e637c791dbcc2f38c5a5a4cd8a2075e3aced
c727 <crusader727@gmx.de>
2018-07-20 03:22:16 +0200

    Add more gallery cards (#1481)
    * Add more gallery cards
    
    * Lint

---
+++ gallery/src/demos/demo-hui-entities-card.js
@@ -0,0 +1,96 @@
+import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+import { PolymerElement } from '@polymer/polymer/polymer-element.js';
+
+import '../components/demo-cards.js';
+
+const CONFIGS = [
+  {
+    heading: 'Basic',
+    config: `
+- type: entities
+  entities:
+    - scene.romantic_lights
+    - device_tracker.demo_paulus
+    - cover.kitchen_window
+    - group.kitchen
+    - lock.kitchen_door
+    - light.bed_light
+    - light.non_existing
+    `
+  },
+  {
+    heading: 'With title, toggle-able',
+    config: `
+- type: entities
+  entities:
+    - scene.romantic_lights
+    - device_tracker.demo_paulus
+    - cover.kitchen_window
+    - group.kitchen
+    - lock.kitchen_door
+    - light.bed_light
+  title: Random group
+    `
+  },
+  {
+    heading: 'With title, toggle = false',
+    config: `
+- type: entities
+  entities:
+    - scene.romantic_lights
+    - device_tracker.demo_paulus
+    - cover.kitchen_window
+    - group.kitchen
+    - lock.kitchen_door
+    - light.bed_light
+  title: Random group
+  show_header_toggle: false
+    `
+  },
+  {
+    heading: 'With title, cant\'t toggle',
+    config: `
+- type: entities
+  entities:
+    - device_tracker.demo_paulus
+  title: Random group
+    `
+  },
+  {
+    heading: 'Custom name, secondary info',
+    config: `
+- type: entities
+  entities:
+    - entity: scene.romantic_lights
+      name: ¯\\_(ツ)_/¯
+    - entity: device_tracker.demo_paulus
+      secondary_info: entity-id
+    - entity: cover.kitchen_window
+      secondary_info: last-changed
+    - group.kitchen
+    - lock.kitchen_door
+    - light.bed_light
+  title: Random group
+  show_header_toggle: false
+    `
+  },
+];
+
+class DemoEntities extends PolymerElement {
+  static get template() {
+    return html`
+      <demo-cards configs="[[_configs]]"></demo-cards>
+    `;
+  }
+
+  static get properties() {
+    return {
+      _configs: {
+        type: Object,
+        value: CONFIGS
+      }
+    };
+  }
+}
+
+customElements.define('demo-hui-entities-card', DemoEntities);
---
+++ gallery/src/demos/demo-hui-entity-filter-card.js
@@ -0,0 +1,61 @@
+import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+import { PolymerElement } from '@polymer/polymer/polymer-element.js';
+
+import '../components/demo-cards.js';
+
+const CONFIGS = [
+  {
+    heading: 'Basic',
+    config: `
+- type: entity-filter
+  entities:
+    - device_tracker.demo_anne_therese
+    - device_tracker.demo_home_boy
+    - device_tracker.demo_paulus
+    - light.bed_light
+    - light.ceiling_lights
+    - light.kitchen_lights
+  state_filter:
+    - "on"
+    - not_home
+    `
+  },
+  {
+    heading: 'With card config',
+    config: `
+- type: entity-filter
+  entities:
+    - device_tracker.demo_anne_therese
+    - device_tracker.demo_home_boy
+    - device_tracker.demo_paulus
+    - light.bed_light
+    - light.ceiling_lights
+    - light.kitchen_lights
+  state_filter:
+    - "on"
+    - not_home
+  card:
+    type: glance
+    show_state: false
+    `
+  },
+];
+
+class DemoFilter extends PolymerElement {
+  static get template() {
+    return html`
+      <demo-cards configs="[[_configs]]"></demo-cards>
+    `;
+  }
+
+  static get properties() {
+    return {
+      _configs: {
+        type: Object,
+        value: CONFIGS
+      }
+    };
+  }
+}
+
+customElements.define('demo-hui-entity-filter-card', DemoFilter);
