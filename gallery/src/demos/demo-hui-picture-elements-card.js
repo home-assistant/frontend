dfe6041b2b2a00b899b8799b478f0913a8402f49
arsaboo <arsaboo@gmx.com>
2018-07-21 11:34:46 -0400

    Cleanup

--- gallery/src/demos/demo-hui-picture-elements-card.js
+++ gallery/src/demos/demo-hui-picture-elements-card.js
@@ -46,63 +46,9 @@ const CONFIGS = [
         --iron-icon-fill-color: rgba(50, 50, 50, .75)
     `
   },
-  {
-    heading: 'Title, dialog, no toggle',
-    config: `
-- type: picture-glance
-  image: https://images.pexels.com/photos/276724/pexels-photo-276724.jpeg
-  title: Living room
-  entities:
-    - binary_sensor.movement_backyard
-    - binary_sensor.basement_floor_wet
-    `
-  },
-  {
-    heading: 'Title, no dialog, toggle',
-    config: `
-- type: picture-glance
-  image: https://images.pexels.com/photos/276724/pexels-photo-276724.jpeg
-  title: Living room
-  entities:
-    - switch.decorative_lights
-    - light.ceiling_lights
-    `
-  },
-  {
-    heading: 'No title, dialog, toggle',
-    config: `
-- type: picture-glance
-  image: https://images.pexels.com/photos/276724/pexels-photo-276724.jpeg
-  entities:
-    - switch.decorative_lights
-    - light.ceiling_lights
-    - binary_sensor.movement_backyard
-    - binary_sensor.basement_floor_wet
-    `
-  },
-  {
-    heading: 'No title, dialog, no toggle',
-    config: `
-- type: picture-glance
-  image: https://images.pexels.com/photos/276724/pexels-photo-276724.jpeg
-  entities:
-    - binary_sensor.movement_backyard
-    - binary_sensor.basement_floor_wet
-    `
-  },
-  {
-    heading: 'No title, no dialog, toggle',
-    config: `
-- type: picture-glance
-  image: https://images.pexels.com/photos/276724/pexels-photo-276724.jpeg
-  entities:
-    - switch.decorative_lights
-    - light.ceiling_lights
-    `
-  },
 ];
 
-class DemoPicGlance extends PolymerElement {
+class DemoPicElements extends PolymerElement {
   static get template() {
     return html`
       <demo-cards configs="[[_configs]]"></demo-cards>
