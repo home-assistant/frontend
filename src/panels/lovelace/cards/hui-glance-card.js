68bedc0b95fcf87eb0c06b64fb2a33bc1cf4749e
c727 <11984118+c727@users.noreply.github.com>
2018-07-23 23:17:47 +0200

    Lint

--- src/panels/lovelace/cards/hui-glance-card.js
+++ src/panels/lovelace/cards/hui-glance-card.js
@@ -6,7 +6,6 @@ import computeStateName from '../../../common/entity/compute_state_name.js';
 import processConfigEntities from '../common/process-config-entities';
 
 import toggleEntity from '../common/entity/toggle-entity.js';
-import turnOnOffEntity from '../common/entity/turn-on-off-entity.js';
 
 import '../../../components/entity/state-badge.js';
 import '../../../components/ha-card.js';
@@ -127,7 +126,7 @@ class HuiGlanceCard extends LocalizeMixin(EventsMixin(PolymerElement)) {
       case 'toggle':
         toggleEntity(this.hass, entityId);
         break;
-      case 'call-service':
+      case 'call-service': {
         const [domain, service] = config.service.split('.', 2);
         const serviceData = Object.assign(
           {}, { entity_id: entityId },
@@ -135,6 +134,7 @@ class HuiGlanceCard extends LocalizeMixin(EventsMixin(PolymerElement)) {
         );
         this.hass.callService(domain, service, serviceData);
         break;
+      }
       default:
         this.fire('hass-more-info', { entityId });
     }
