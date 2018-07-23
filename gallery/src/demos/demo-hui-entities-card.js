54725bb4b983f15841f3d501c5b6e742554ab573
Anton Verburg <info@tec-v.nl>
2018-07-23 10:02:33 +0200

    Add row divider for entities card (#1503)
    * Add row divider for entities card
    
    * Add gallery demo
    
    * Change hr to div, less attributes needed

--- gallery/src/demos/demo-hui-entities-card.js
+++ gallery/src/demos/demo-hui-entities-card.js
@@ -121,6 +121,11 @@ const CONFIGS = [
       url: http://google.com/
       icon: mdi:google
       name: Google
+    - type: divider
+    - type: divider
+      style:
+        height: 16px
+        background: center / contain url("https://d33wubrfki0l68.cloudfront.net/075995fe17a5351e2699b2dd878652ec4f1d8654/8bfdd/demo/favicon-192x192.png")
     `
   },
 ];
