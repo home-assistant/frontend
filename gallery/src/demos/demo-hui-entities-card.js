690252a6ba70e3caa54310713232538ab274b629
Jerad Meisner <jerad.meisner@gmail.com>
2018-07-25 01:40:21 -0700

    Added row entity for climate entities. (#1494)
    * Added row entity for climate entities.
    
    * Revert to old climate state style.
    
    * Added climate entity to entities-card in gallery.

--- gallery/src/demos/demo-hui-entities-card.js
+++ gallery/src/demos/demo-hui-entities-card.js
@@ -38,6 +38,24 @@ const ENTITIES = [
     battery: 71,
     friendly_name: 'Paulus'
   }),
+  getEntity('climate', 'ecobee', 'auto', {
+    current_temperature: 73,
+    min_temp: 45,
+    max_temp: 95,
+    temperature: null,
+    target_temp_high: 75,
+    target_temp_low: 70,
+    fan_mode: 'Auto Low',
+    fan_list: ['On Low', 'On High', 'Auto Low', 'Auto High', 'Off'],
+    operation_mode: 'auto',
+    operation_list: ['heat', 'cool', 'auto', 'off'],
+    hold_mode: 'home',
+    swing_mode: 'Auto',
+    swing_list: ['Auto', '1', '2', '3', 'Off'],
+    unit_of_measurement: '°F',
+    friendly_name: 'Ecobee',
+    supported_features: 1014
+  })
 ];
 
 const CONFIGS = [
@@ -53,6 +71,7 @@ const CONFIGS = [
     - lock.kitchen_door
     - light.bed_light
     - light.non_existing
+    - climate.ecobee
     `
   },
   {
@@ -66,6 +85,7 @@ const CONFIGS = [
     - group.kitchen
     - lock.kitchen_door
     - light.bed_light
+    - climate.ecobee
   title: Random group
     `
   },
@@ -80,6 +100,7 @@ const CONFIGS = [
     - group.kitchen
     - lock.kitchen_door
     - light.bed_light
+    - climate.ecobee
   title: Random group
   show_header_toggle: false
     `
@@ -108,6 +129,7 @@ const CONFIGS = [
       icon: mdi:home-assistant
     - lock.kitchen_door
     - light.bed_light
+    - climate.ecobee
   title: Random group
   show_header_toggle: false
     `
