3f097ced2f9ce3a197dc0bf63f00ad409d5ff9c4
Paulus Schoutsen <balloob@gmail.com>
2018-08-10 17:33:30 +0200

    Also split out more info dialog

--- src/layouts/app/more-info-mixin.js
+++ src/layouts/app/more-info-mixin.js
@@ -1,11 +1,29 @@
+import { afterNextRender } from '@polymer/polymer/lib/utils/render-status.js';
+
 export default superClass =>
   class extends superClass {
     ready() {
       super.ready();
       this.addEventListener('hass-more-info', e => this._handleMoreInfo(e));
+
+      // Load it once we are having the initial rendering done.
+      afterNextRender(null, () =>
+        import(/* webpackChunkName: "more-info-dialog" */ '../../dialogs/ha-more-info-dialog.js'));
     }
 
-    _handleMoreInfo(ev) {
+    async _handleMoreInfo(ev) {
+      if (!this.__moreInfoEl) {
+        this.__moreInfoEl = document.createElement('ha-more-info-dialog');
+        this.shadowRoot.appendChild(this.__moreInfoEl);
+      }
       this._updateHass({ moreInfoEntityId: ev.detail.entityId });
+    }
+
+    hassChanged(hass, oldHass) {
+      super.hassChanged(hass, oldHass);
+
+      if (this.__moreInfoEl) {
+        this.__moreInfoEl.hass = hass;
+      }
     }
   };
