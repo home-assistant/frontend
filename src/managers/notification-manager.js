63c7c55843736ed5f8a23a3e30d24d9702ea472d
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2018-08-13 15:48:54 +0200

    Cleanups (#1566)
    * Cleanup core.js
    
    * Add ha-toast
    
    * Lint
    
    * Remove unused import

--- src/managers/notification-manager.js
+++ src/managers/notification-manager.js
@@ -1,8 +1,9 @@
-import '@polymer/paper-toast/paper-toast.js';
 import { html } from '@polymer/polymer/lib/utils/html-tag.js';
 import { PolymerElement } from '@polymer/polymer/polymer-element.js';
 
 import LocalizeMixin from '../mixins/localize-mixin.js';
+
+import '../components/ha-toast.js';
 
 class NotificationManager extends LocalizeMixin(PolymerElement) {
   static get template() {
@@ -13,8 +14,17 @@ class NotificationManager extends LocalizeMixin(PolymerElement) {
       }
     </style>
 
-    <paper-toast id="toast" no-cancel-on-outside-click="[[_cancelOnOutsideClick]]"></paper-toast>
-    <paper-toast id="connToast" duration="0" text="[[localize('ui.notification_toast.connection_lost')]]" opened="[[connectionLost]]"></paper-toast>
+    <ha-toast
+      id="toast"
+      no-cancel-on-outside-click="[[_cancelOnOutsideClick]]"
+    ></ha-toast>
+
+    <ha-toast
+      id="connToast"
+      duration="0"
+      text="[[localize('ui.notification_toast.connection_lost')]]"
+      opened="[[connectionLost]]"
+    ></ha-toast>
 `;
   }
 
@@ -60,28 +70,6 @@ class NotificationManager extends LocalizeMixin(PolymerElement) {
 
   computeConnectionLost(wasConnected, hass) {
     return wasConnected && hass && !hass.connected;
-  }
-
-  constructor() {
-    super();
-    this.handleWindowChange = this.handleWindowChange.bind(this);
-    this._mediaq = window.matchMedia('(max-width: 599px)');
-    this._mediaq.addListener(this.handleWindowChange);
-  }
-
-  connectedCallback() {
-    super.connectedCallback();
-    this.handleWindowChange(this._mediaq);
-  }
-
-  disconnectedCallback() {
-    super.disconnectedCallback();
-    this._mediaq.removeListener(this.handleWindowChange);
-  }
-
-  handleWindowChange(ev) {
-    this.$.toast.classList.toggle('fit-bottom', ev.matches);
-    this.$.connToast.classList.toggle('fit-bottom', ev.matches);
   }
 
   showDialog({ message }) {
