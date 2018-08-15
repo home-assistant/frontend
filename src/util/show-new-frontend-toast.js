6eca0ab16385cdeb3e9fa1716823fbbc7930e480
Anton Verburg <info@tec-v.nl>
2018-08-15 10:04:52 +0200

    Use ha toast for frontend notification

--- src/util/show-new-frontend-toast.js
+++ src/util/show-new-frontend-toast.js
@@ -1,8 +1,8 @@
-import '@polymer/paper-toast/paper-toast.js';
+import '@polymer/ha-toast/ha-toast.js';
 import '@polymer/paper-button/paper-button.js';
 
 export default (installingWorker) => {
-  const toast = document.createElement('paper-toast');
+  const toast = document.createElement('ha-toast');
   toast.opened = true;
   toast.text = 'A new version of the frontend is available.';
   toast.duration = 0;
