28e1610075fe5a5250af0bea6560490b13b7853b
Jason Hunter <hunterjm@gmail.com>
2018-08-15 04:45:36 -0400

    Media Player: Standardize computePlaybackControlIcon (#1571)
    * standardize computePlaybackControlIcon and add play/pause icon for UNKNOWN state
    
    * fix linting errors

--- src/dialogs/more-info/controls/more-info-media_player.js
+++ src/dialogs/more-info/controls/more-info-media_player.js
@@ -194,8 +194,13 @@ class MoreInfoMediaPlayer extends LocalizeMixin(EventsMixin(PolymerElement)) {
   computePlaybackControlIcon(playerObj) {
     if (playerObj.isPlaying) {
       return playerObj.supportsPause ? 'hass:pause' : 'hass:stop';
+    } else if (playerObj.hasMediaControl || playerObj.isOff || playerObj.isIdle) {
+      if (playerObj.hasMediaControl && playerObj.supportsPause && !playerObj.isPaused) {
+        return 'hass:play-pause';
+      }
+      return playerObj.supportsPlay ? 'hass:play' : null;
     }
-    return playerObj.supportsPlay ? 'hass:play' : null;
+    return '';
   }
 
   computeHidePowerButton(playerObj) {
