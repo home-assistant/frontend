fe024eb941bf1cd3c4207664d626dc4c9e5f7fda
squidwardy <vilanyib@gmail.com>
2018-08-21 15:41:22 +0200

    Update load-custom-panel.js

--- src/util/custom-panel/load-custom-panel.js
+++ src/util/custom-panel/load-custom-panel.js
@@ -16,13 +16,11 @@ export default function loadCustomPanel(panelConfig) {
     return Promise.all(toLoad).then(([{ importHrefPromise }]) =>
       importHrefPromise(panelConfig.html_url));
   } else if (panelConfig.js_url) {
-    if (panelConfig.js_is_module) {
-      return loadModule(panelConfig.js_url)
-    }
     if (!(panelConfig.js_url in JS_CACHE)) {
       JS_CACHE[panelConfig.js_url] = loadJS(panelConfig.js_url);
     }
     return JS_CACHE[panelConfig.js_url];
-  }
+  } else if (panelConfig.module_url) {
+    return loadModule(panelConfig.module_url);
   return Promise.reject('No valid url found in panel config.');
 }
