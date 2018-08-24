cafcec38bfd825b22ee3bda69ecbb630cce50384
Glenn Waters <gwwaters@gmail.com>
2018-08-24 10:34:17 -0400

    Update more-info-alarm_control_panel.js
    Disarm button visible on arming state.

--- src/dialogs/more-info/controls/more-info-alarm_control_panel.js
+++ src/dialogs/more-info/controls/more-info-alarm_control_panel.js
@@ -135,8 +135,7 @@ class MoreInfoAlarmControlPanel extends LocalizeMixin(EventsMixin(PolymerElement
         _codeFormat: newVal.attributes.code_format,
         _armVisible: state === 'disarmed',
         _disarmVisible: this._armedStates.includes(state) ||
-          state === 'pending' || state === 'triggered' ||
-          state === 'arming' || state === 'disarming'
+          state === 'pending' || state === 'triggered' || state === 'arming'
       };
       props._inputEnabled = props._disarmVisible || props._armVisible;
       this.setProperties(props);
