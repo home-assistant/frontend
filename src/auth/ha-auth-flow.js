18255b29445dca4e19b2eeafbd36ae3a20056dea
Anton Verburg <info@tec-v.nl>
2018-08-27 22:10:15 +0200

    Authorize onboarding (#1603)
    * Tweak onboarding + authorize
    
    * Authorize/Onboarding pimp
    
    * More tweaks
    
    * Comments

--- src/auth/ha-auth-flow.js
+++ src/auth/ha-auth-flow.js
@@ -2,46 +2,64 @@ import { PolymerElement } from '@polymer/polymer/polymer-element.js';
 import '@polymer/paper-button/paper-button.js';
 import { html } from '@polymer/polymer/lib/utils/html-tag.js';
 import '../components/ha-form.js';
-import EventsMixin from '../mixins/events-mixin.js';
+import LocalizeLiteMixin from '../mixins/localize-lite-mixin.js';
 
-/*
- * @appliesMixin EventsMixin
- */
-class HaAuthFlow extends EventsMixin(PolymerElement) {
+class HaAuthFlow extends LocalizeLiteMixin(PolymerElement) {
   static get template() {
     return html`
     <style>
+      :host {
+        /* So we can set min-height to avoid jumping during loading */
+        display: block;
+      }
       .action {
-        margin: 32px 0;
+        margin: 24px 0 8px;
         text-align: center;
       }
     </style>
-    <template is="dom-if" if="[[_equals(_state, &quot;loading&quot;)]]">
-      Please wait
-    </template>
-    <template is="dom-if" if="[[_equals(_state, &quot;error&quot;)]]">
-      Something went wrong
-    </template>
-    <template is="dom-if" if="[[_equals(_state, &quot;step&quot;)]]">
-      <template is="dom-if" if="[[_equals(_step.type, &quot;abort&quot;)]]">
-        Aborted
+    <form>
+      <template is="dom-if" if="[[_equals(_state, &quot;loading&quot;)]]">
+      [[localize('ui.panel.page-authorize.form.working')]]:
       </template>
-      <template is="dom-if" if="[[_equals(_step.type, &quot;create_entry&quot;)]]">
-        Success!
+      <template is="dom-if" if="[[_equals(_state, &quot;error&quot;)]]">
+      [[localize('ui.panel.page-authorize.form.unknown_error')]]:
       </template>
-      <template is="dom-if" if="[[_equals(_step.type, &quot;form&quot;)]]">
-        <ha-form data="{{_stepData}}" schema="[[_step.data_schema]]" error="[[_step.errors]]"></ha-form>
+      <template is="dom-if" if="[[_equals(_state, &quot;step&quot;)]]">
+        <template is="dom-if" if="[[_equals(_step.type, &quot;abort&quot;)]]">
+          [[localize('ui.panel.page-authorize.abort_intro')]]:
+          <ha-markdown content="[[_computeStepAbortedReason(localize, _step)]]"></ha-markdown>
+        </template>
+
+        <template is="dom-if" if="[[_equals(_step.type, &quot;form&quot;)]]">
+          <template is="dom-if" if="[[_computeStepDescription(localize, _step)]]">
+            <ha-markdown content="[[_computeStepDescription(localize, _step)]]" allow-svg></ha-markdown>
+          </template>
+
+          <ha-form
+            data="{{_stepData}}"
+            schema="[[_step.data_schema]]"
+            error="[[_step.errors]]"
+            compute-label="[[_computeLabelCallback(localize, _step)]]"
+            compute-error="[[_computeErrorCallback(localize, _step)]]"
+          ></ha-form>
+        </template>
+        <div class='action'>
+          <paper-button
+            raised
+            on-click='_handleSubmit'
+          >[[_computeSubmitCaption(_step.type)]]</paper-button>
+        </div>
       </template>
-      <div class='action'>
-        <paper-button raised on-click="_handleSubmit">[[_computeSubmitCaption(_step.type)]]</paper-button>
-      </div>
-    </template>
+    </form>
 `;
   }
 
   static get properties() {
     return {
-      authProvider: Object,
+      authProvider: {
+        type: Object,
+        observer: '_providerChanged',
+      },
       clientId: String,
       redirectUri: String,
       oauth2State: String,
@@ -53,11 +71,14 @@ class HaAuthFlow extends EventsMixin(PolymerElement) {
         type: Object,
         value: () => ({}),
       },
-      _step: Object,
+      _step: {
+        type: Object,
+        notify: true,
+      }
     };
   }
 
-  async ready() {
+  ready() {
     super.ready();
 
     this.addEventListener('keypress', (ev) => {
@@ -67,28 +88,45 @@ class HaAuthFlow extends EventsMixin(PolymerElement) {
     });
   }
 
-  connectedCallback() {
-    super.connectedCallback();
+  async _providerChanged(newProvider, oldProvider) {
+    if (oldProvider && this._step && this._step.type === 'form') {
+      fetch(`/auth/login_flow/${this._step.flow_id}`, {
+        method: 'DELETE',
+        credentials: 'same-origin',
+      }).catch(() => {});
+    }
 
-    fetch('/auth/login_flow', {
-      method: 'POST',
-      credentials: 'same-origin',
-      body: JSON.stringify({
-        client_id: this.clientId,
-        handler: [this.authProvider.type, this.authProvider.id],
-        redirect_uri: this.redirectUri,
-      })
-    }).then((response) => {
-      if (!response.ok) throw new Error();
-      return response.json();
-    }).then(step => this.setProperties({
-      _step: step,
-      _state: 'step',
-    })).catch((err) => {
+    try {
+      const response = await fetch('/auth/login_flow', {
+        method: 'POST',
+        credentials: 'same-origin',
+        body: JSON.stringify({
+          client_id: this.clientId,
+          handler: [newProvider.type, newProvider.id],
+          redirect_uri: this.redirectUri,
+        })
+      });
+
+      const step = await response.json();
+      this._updateStep(step);
+    } catch (err) {
       // eslint-disable-next-line
       console.error('Error starting auth flow', err);
       this._state = 'error';
-    });
+    }
+  }
+
+  _updateStep(step) {
+    const props = {
+      _step: step,
+      _state: 'step',
+    };
+
+    if (this._step && step.step_id !== this._step.step_id) {
+      props._stepData = {};
+    }
+
+    this.setProperties(props);
   }
 
   _equals(a, b) {
@@ -99,25 +137,52 @@ class HaAuthFlow extends EventsMixin(PolymerElement) {
     return stepType === 'form' ? 'Next' : 'Start over';
   }
 
-  _handleSubmit() {
+  _computeStepAbortedReason(localize, step) {
+    return localize(`ui.panel.page-authorize.form.providers.${step.handler[0]}.abort.${step.reason}`);
+  }
+
+  _computeStepDescription(localize, step) {
+    const args = [`ui.panel.page-authorize.form.providers.${step.handler[0]}.step.${step.step_id}.description`];
+    const placeholders = step.description_placeholders || {};
+    Object.keys(placeholders).forEach((key) => {
+      args.push(key);
+      args.push(placeholders[key]);
+    });
+    return localize(...args);
+  }
+
+  _computeLabelCallback(localize, step) {
+    // Returns a callback for ha-form to calculate labels per schema object
+    return schema => localize(`ui.panel.page-authorize.form.providers.${step.handler[0]}.step.${step.step_id}.data.${schema.name}`);
+  }
+
+  _computeErrorCallback(localize, step) {
+    // Returns a callback for ha-form to calculate error messages
+    return error => localize(`ui.panel.page-authorize.form.providers.${step.handler[0]}.error.${error}`);
+  }
+
+  async _handleSubmit() {
     if (this._step.type !== 'form') {
-      this.fire('reset');
+      this._providerChanged(this.authProvider, null);
       return;
     }
     this._state = 'loading';
+    // To avoid a jumping UI.
+    this.style.setProperty('min-height', `${this.offsetHeight}px`);
 
     const postData = Object.assign({}, this._stepData, {
       client_id: this.clientId,
     });
 
-    fetch(`/auth/login_flow/${this._step.flow_id}`, {
-      method: 'POST',
-      credentials: 'same-origin',
-      body: JSON.stringify(postData)
-    }).then((response) => {
-      if (!response.ok) throw new Error();
-      return response.json();
-    }).then((newStep) => {
+    try {
+      const response = await fetch(`/auth/login_flow/${this._step.flow_id}`, {
+        method: 'POST',
+        credentials: 'same-origin',
+        body: JSON.stringify(postData)
+      });
+
+      const newStep = await response.json();
+
       if (newStep.type === 'create_entry') {
         // OAuth 2: 3.1.2 we need to retain query component of a redirect URI
         let url = this.redirectUri;
@@ -136,20 +201,14 @@ class HaAuthFlow extends EventsMixin(PolymerElement) {
         document.location = url;
         return;
       }
-
-      const props = {
-        _step: newStep,
-        _state: 'step',
-      };
-      if (newStep.step_id !== this._step.step_id) {
-        props._stepData = {};
-      }
-      this.setProperties(props);
-    }).catch((err) => {
+      this._updateStep(newStep);
+    } catch (err) {
       // eslint-disable-next-line
-      console.error('Error loading auth providers', err);
+      console.error('Error submitting step', err);
       this._state = 'error-loading';
-    });
+    } finally {
+      this.style.setProperty('min-height', '');
+    }
   }
 }
 customElements.define('ha-auth-flow', HaAuthFlow);
--- src/auth/ha-authorize.js
+++ src/auth/ha-authorize.js
@@ -1,76 +1,73 @@
-import '@polymer/iron-flex-layout/iron-flex-layout-classes.js';
 import '@polymer/polymer/lib/elements/dom-if.js';
 import '@polymer/polymer/lib/elements/dom-repeat.js';
 import { html } from '@polymer/polymer/lib/utils/html-tag.js';
 import { PolymerElement } from '@polymer/polymer/polymer-element.js';
 
-import '../auth/ha-auth-flow.js';
-import '../auth/ha-pick-auth-provider.js';
+import '../components/ha-markdown.js';
 
-class HaAuthorize extends PolymerElement {
+import LocalizeLiteMixin from '../mixins/localize-lite-mixin.js';
+
+import '../auth/ha-auth-flow.js';
+
+class HaAuthorize extends LocalizeLiteMixin(PolymerElement) {
   static get template() {
     return html`
-    <style include="iron-flex iron-positioning"></style>
     <style>
-      .content {
-        padding: 20px 16px;
-        max-width: 360px;
-        margin: 0 auto;
+      ha-markdown a {
+        color: var(--primary-color);
       }
-
-      .header {
-        text-align: center;
-        font-size: 1.96em;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        font-weight: 300;
+      ha-markdown p:last-child{
+        margin-bottom: 0;
       }
-
-      .header img {
-        margin-right: 16px;
+      ha-pick-auth-provider {
+        display: block;
+        margin-top: 48px;
       }
-
     </style>
-    <div class="content layout vertical fit">
-      <div class='header'>
-        <img src="/static/icons/favicon-192x192.png" height="52">
-        Home Assistant
-      </div>
 
-      <p>Logging in to <b>[[clientId]]</b>.</p>
+    <template is="dom-if" if="[[!_authProviders]]">
+      <p>[[localize('ui.panel.page-authorize.initializing')]]</p>
+    </template>
 
-      <template is="dom-if" if="[[_authProvider]]">
-        <ha-auth-flow
-          client-id="[[clientId]]"
-          redirect-uri="[[redirectUri]]"
-          oauth2-state="[[oauth2State]]"
-          auth-provider="[[_authProvider]]"
-          on-reset="_handleReset"
-        ></ha-auth-flow>
-      </template>
-      <template is="dom-if" if="[[!_authProvider]]">
+    <template is="dom-if" if="[[_authProviders]]">
+      <ha-markdown content='[[_computeIntro(localize, clientId, _authProvider)]]'></ha-markdown>
+
+      <ha-auth-flow
+        resources="[[resources]]"
+        client-id="[[clientId]]"
+        redirect-uri="[[redirectUri]]"
+        oauth2-state="[[oauth2State]]"
+        auth-provider="[[_authProvider]]"
+        step="{{step}}"
+      ></ha-auth-flow>
+
+      <template is="dom-if" if="[[_computeMultiple(_authProviders)]]">
         <ha-pick-auth-provider
+          resources="[[resources]]"
           client-id="[[clientId]]"
+          auth-providers="[[_computeInactiveProvders(_authProvider, _authProviders)]]"
           on-pick="_handleAuthProviderPick"
         ></ha-pick-auth-provider>
       </template>
-    </div>
+    </template>
 `;
   }
 
   static get properties() {
     return {
-      _authProvider: {
-        type: String,
-        value: null,
-      },
+      _authProvider: String,
+      _authProviders: Array,
       clientId: String,
       redirectUri: String,
       oauth2State: String,
+      translationFragment: {
+        type: String,
+        value: 'page-authorize',
+      }
     };
   }
-  ready() {
+
+  async ready() {
     super.ready();
     const query = {};
     const values = location.search.substr(1).split('&');
@@ -85,12 +82,49 @@ class HaAuthorize extends PolymerElement {
     if (query.redirect_uri) props.redirectUri = query.redirect_uri;
     if (query.state) props.oauth2State = query.state;
     this.setProperties(props);
+
+    import(/* webpackChunkName: "pick-auth-provider" */ '../auth/ha-pick-auth-provider.js');
+
+    // Fetch auth providers
+    try {
+      const response = await window.providersPromise;
+      const authProviders = await response.json();
+
+      if (authProviders.length === 0) {
+        alert('No auth providers returned. Unable to finish login.');
+        return;
+      }
+
+      this.setProperties({
+        _authProviders: authProviders,
+        _authProvider: authProviders[0],
+      });
+    } catch (err) {
+      // eslint-disable-next-line
+      console.error('Error loading auth providers', err);
+      this._state = 'error-loading';
+    }
   }
-  _handleAuthProviderPick(ev) {
+
+  _computeMultiple(array) {
+    return array && array.length > 1;
+  }
+
+  async _handleAuthProviderPick(ev) {
     this._authProvider = ev.detail;
   }
-  _handleReset() {
-    this._authProvider = null;
+
+  _computeInactiveProvders(curProvider, providers) {
+    return providers.filter(prv =>
+      prv.type !== curProvider.type || prv.id !== curProvider.id);
+  }
+
+  _computeIntro(localize, clientId, authProvider) {
+    return (
+      localize('ui.panel.page-authorize.authorizing_client', 'clientId', clientId) +
+      '\n\n' +
+      localize('ui.panel.page-authorize.logging_in_with', 'authProviderName', authProvider.name)
+    );
   }
 }
 customElements.define('ha-authorize', HaAuthorize);
