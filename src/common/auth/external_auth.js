d8f5ce3f8c5200a7e81ca7b62135d6d98c15844a
Paulus Schoutsen <paulus@home-assistant.io>
2018-09-07 19:43:03 +0200

    Update external_auth.js

--- src/common/auth/external_auth.js
+++ src/common/auth/external_auth.js
@@ -27,7 +27,7 @@ export default class ExternalAuth extends Auth {
     // Allow promise to set resolve on window object.
     await 0;
 
-    const callbackPayload = { callback: CALLBACK_METHOD }
+    const callbackPayload = { callback: CALLBACK_METHOD };
 
     if (window.externalApp) {
       window.externalApp.getExternalAuth(callbackPayload);
