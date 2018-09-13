a4738087153622eb75b7d030a93a176030bc47a1
Randell L Hodges <rhodges@taxfodder.com>
2018-09-13 13:58:15 -0500

    added a comment on what is supported

--- src/common/util/parse-aspect-ratio.js
+++ src/common/util/parse-aspect-ratio.js
@@ -1,4 +1,6 @@
 export default function parseAspectRatio(input) {
+  // Handle 16x9, 16:9, 1.78:1, 1.78
+  // Ignore everything else
   try {
     if (input) {
       let arr = input.replace(':', 'x').split('x');
