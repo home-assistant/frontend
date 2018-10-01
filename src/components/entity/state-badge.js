7d9448d423721006c1cf0fe65c9ac9317029d4e2
Charles Garwood <cgarwood@gmail.com>
2018-09-30 21:49:44 -0400

    Set input_boolean icon color to active color when on

--- src/components/entity/state-badge.js
+++ src/components/entity/state-badge.js
@@ -26,11 +26,12 @@ class StateBadge extends PolymerElement {
     }
 
     /* Color the icon if light or sun is on */
-    ha-icon[data-domain=light][data-state=on],
-    ha-icon[data-domain=switch][data-state=on],
     ha-icon[data-domain=binary_sensor][data-state=on],
     ha-icon[data-domain=fan][data-state=on],
-    ha-icon[data-domain=sun][data-state=above_horizon] {
+    ha-icon[data-domain=input_boolean][data-state=on],
+    ha-icon[data-domain=light][data-state=on],
+    ha-icon[data-domain=sun][data-state=above_horizon],
+    ha-icon[data-domain=switch][data-state=on] {
       color: var(--paper-item-icon-active-color, #FDD835);
     }
 
