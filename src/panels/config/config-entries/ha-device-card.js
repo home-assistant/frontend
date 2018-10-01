3f8bd91826ddba8c23b05ac29cf11561e439da41
Jason Hu <awaregit@gmail.com>
2018-10-01 00:49:41 -0700

    Lint

--- src/panels/config/config-entries/ha-device-card.js
+++ src/panels/config/config-entries/ha-device-card.js
@@ -136,7 +136,7 @@ class HaDeviceCard extends EventsMixin(LocalizeMixin(PolymerElement)) {
   _computeDeviceName(devices, deviceId) {
     const device = devices.find(dev => dev.id === deviceId);
     return device ? device.name
-        : this.localize('ui.panel.config.integrations.config_entry.device_unavailable');
+      : this.localize('ui.panel.config.integrations.config_entry.device_unavailable');
   }
 
   _openMoreInfo(ev) {
