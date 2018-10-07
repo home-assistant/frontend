d52f9876f2424f1a72c3e34ea94dfd9da0d40ede
Franck Nijhof <frenck@frenck.nl>
2018-10-07 15:30:22 +0200

    :shirt: Really now..

--- hassio/src/addon-store/hassio-addon-repository.js
+++ hassio/src/addon-store/hassio-addon-repository.js
@@ -50,14 +50,14 @@ class HassioAddonRepository extends NavigateMixin(PolymerElement) {
   sortAddons(a, b) {
     var addonA = a.name.toUpperCase();
     var addonB = b.name.toUpperCase();
-    
+
     if (addonA < addonB) {
       return -1;
     } else if (addonA > addonB) {
       return 1;
+    } else {
+      return 0;
     }
-
-    return 0;
   }
 
   computeIcon(addon) {
