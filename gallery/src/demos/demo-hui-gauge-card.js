e7b8b9cd0a68328b461283ee69a0b93f57bcd968
Zack Arnett <arnett.zackary@gmail.com>
2018-10-11 04:30:56 -0400

    Adding Gauge Card to Lovelace (#1742)
    * Commiting Only needed Files. Adds Gallery Entry
    
    * Adding Attribute current_temperature to gallery entry config
    
    * Fixing code from review and updating gallery
    
    * Updating Gallery to show errors
    
    * Resolving Reviews and updating gallery
    
    * Deleting unused line
    
    * Minor changes
    
    * Address my own comments.

---
+++ gallery/src/demos/demo-hui-gauge-card.js
@@ -0,0 +1,83 @@
+import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+import { PolymerElement } from '@polymer/polymer/polymer-element.js';
+
+import '../components/demo-cards.js';
+
+const CONFIGS = [
+  {
+    heading: 'Basic example',
+    config: `
+- type: gauge
+  entity: sensor.brightness
+    `
+  },
+  {
+    heading: 'With title',
+    config: `
+- type: gauge
+  title: Humidity
+  entity: sensor.outside_humidity
+    `
+  },
+  {
+    heading: 'Custom Unit of Measurement',
+    config: `
+- type: gauge
+  entity: sensor.outside_temperature
+  unit_of_measurement: C
+    `
+  },
+  {
+    heading: 'Setting Severity Levels',
+    config: `
+- type: gauge
+  entity: sensor.brightness
+  severity:
+    red: 32
+    green: 0
+    yellow: 23
+    `
+  },
+  {
+    heading: 'Setting Min and Max Values',
+    config: `
+- type: gauge
+  entity: sensor.brightness
+  min: 0
+  max: 38
+    `
+  },
+  {
+    heading: 'Invalid Entity',
+    config: `
+- type: gauge
+  entity: sensor.invalid_entity
+    `
+  },
+  {
+    heading: 'Non-Numeric Value',
+    config: `
+- type: gauge
+  entity: plant.bonsai
+    `
+  },
+];
+
+class DemoGaugeEntity extends PolymerElement {
+  static get template() {
+    return html`
+      <demo-cards configs="[[_configs]]"></demo-cards>
+    `;
+  }
+
+  static get properties() {
+    return {
+      _configs: {
+        type: Object,
+        value: CONFIGS
+      }
+    };
+  }
+}
+
+customElements.define('demo-hui-gauge-card', DemoGaugeEntity);
