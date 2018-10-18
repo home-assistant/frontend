3e42189400ce5b0a07d5675ec0d2e610d4cb6927
Timmo <contact@timmo.xyz>
2018-10-18 20:21:01 +0100

    :hammer: add attributes to dev-state

--- src/panels/dev-state/ha-panel-dev-state.js
+++ src/panels/dev-state/ha-panel-dev-state.js
@@ -87,14 +87,28 @@ class HaPanelDevState extends EventsMixin(PolymerElement) {
             hass="[[hass]]"
             value="{{_entityId}}"
             allow-custom-entity
+            autocapitalize='none'
+            autocomplete='off'
+            autocorrect='off'
+            spellcheck='false'
           ></ha-entity-picker>
           <paper-input
             label="State"
             required
+            autocapitalize='none'
+            autocomplete='off'
+            autocorrect='off'
+            spellcheck='false'
             value='{{_state}}'
             class='state-input'
           ></paper-input>
-          <paper-textarea label="State attributes (JSON, optional)" value='{{_stateAttributes}}'></paper-textarea>
+          <paper-textarea 
+            label="State attributes (JSON, optional)"
+            autocapitalize='none'
+            autocomplete='off'
+            autocorrect='off'
+            spellcheck='false'
+            value='{{_stateAttributes}}'></paper-textarea>
           <paper-button on-click='handleSetState' raised>Set State</paper-button>
         </div>
 
