e00ddfad3822b5ee654eb2cba09783a7c33a368d
Anton Verburg <info@tec-v.nl>
2018-10-23 10:53:42 +0200

    Add HomeAssistant object hass to LovelaceCard interface (#1843)

--- src/panels/lovelace/types.ts
+++ src/panels/lovelace/types.ts
@@ -1,8 +1,11 @@
+import { HomeAssistant } from "../../types.js";
+
 export interface LovelaceConfig {
   type: string;
 }
 
-export interface LovelaceCard {
+export interface LovelaceCard extends HTMLElement {
+  hass?: HomeAssistant;
   getCardSize(): number;
   setConfig(config: LovelaceConfig): void;
 }
