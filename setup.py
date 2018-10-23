ea47c7580535179609d18571580763d7a4870b56
Paulus Schoutsen <balloob@gmail.com>
2018-10-23 14:01:51 +0200

    Update version to 20181023.0

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
 from setuptools import setup, find_packages
 
-setup(name='home-assistant-frontend',
-      version='20181021.0',
-      description='The Home Assistant frontend',
-      url='https://github.com/home-assistant/home-assistant-polymer',
-      author='The Home Assistant Authors',
-      author_email='hello@home-assistant.io',
-      license='Apache License 2.0',
-      packages=find_packages(include=[
-          'hass_frontend',
-          'hass_frontend_es5',
-          'hass_frontend.*',
-          'hass_frontend_es5.*'
-      ]),
-      install_requires=[
-          'user-agents==1.1.0',
-      ],
-      include_package_data=True,
-      zip_safe=False)
+setup(
+    name="home-assistant-frontend",
+    version="20181023.0",
+    description="The Home Assistant frontend",
+    url="https://github.com/home-assistant/home-assistant-polymer",
+    author="The Home Assistant Authors",
+    author_email="hello@home-assistant.io",
+    license="Apache License 2.0",
+    packages=find_packages(
+        include=[
+            "hass_frontend",
+            "hass_frontend_es5",
+            "hass_frontend.*",
+            "hass_frontend_es5.*",
+        ]
+    ),
+    install_requires=["user-agents==1.1.0"],
+    include_package_data=True,
+    zip_safe=False,
+)
