9da78ba1ca5c7caf9ef6682fc1853e6ad8d3bd34
Anton Verburg <info@tec-v.nl>
2018-10-26 13:40:53 -0400

    Prettier fixes (#1864)

--- src/panels/lovelace/elements/types.ts
+++ src/panels/lovelace/elements/types.ts
@@ -1,12 +1,12 @@
 import { HomeAssistant } from "../../../types";
 
 export interface LovelaceElementConfig {
-    type: string;
-    entity?: string;
-    style: object;
+  type: string;
+  entity?: string;
+  style: object;
 }
 
 export interface LovelaceElement extends HTMLElement {
-    hass?: HomeAssistant;
-    setConfig(config: LovelaceElementConfig): void;
+  hass?: HomeAssistant;
+  setConfig(config: LovelaceElementConfig): void;
 }
