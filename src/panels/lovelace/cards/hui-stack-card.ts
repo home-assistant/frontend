6767dbd0cd4756c05731c3296f6056753209b63d
Anton Verburg <info@tec-v.nl>
2018-10-26 02:27:57 -0500

    Convert hui-horizontal-stack-card to TypeScript/LitElement (#1851)
    * Convert horizontal-stack to TypeScript/LitElement
    
    Base stack-card class for horizontal and vertical stack cards to extend as their code overlaps a lot. Not sure if it should be in `common` or not
    
    * rename locale hass variable
    
    * Address review comments
    
    * Abstract getCardSize

---
+++ src/panels/lovelace/cards/hui-stack-card.ts
@@ -0,0 +1,65 @@
+import { html, LitElement } from "@polymer/lit-element";
+import { TemplateResult } from "lit-html";
+
+import createCardElement from "../common/create-card-element.js";
+
+import { LovelaceCard, LovelaceConfig } from "../types";
+import { HomeAssistant } from "../../../types";
+
+interface Config extends LovelaceConfig {
+    cards: LovelaceConfig[];
+}
+
+export default abstract class HuiStackCard extends LitElement
+  implements LovelaceCard {
+  protected config?: Config;
+  protected _cards?: LovelaceCard[];
+  private _hass?: HomeAssistant;
+
+  static get properties() {
+    return {
+      config: {},
+    };
+  }
+
+  set hass(hass: HomeAssistant) {
+    this._hass = hass;
+
+    if (!this._cards) {
+      return;
+    }
+
+    for (const element of this._cards) {
+      element.hass = this._hass;
+    }
+  }
+
+  public abstract getCardSize(): number;
+
+  public setConfig(config: Config): void {
+    if (!config || !config.cards || !Array.isArray(config.cards)) {
+      throw new Error("Card config incorrect");
+    }
+    this.config = config;
+    this._cards = config.cards.map((card) => {
+      const element = createCardElement(card) as LovelaceCard;
+      element.hass = this._hass;
+      return element;
+    });
+  }
+
+  protected render(): TemplateResult {
+    if (!this.config) {
+      return html``;
+    }
+
+    return html`
+      ${this.renderStyle()}
+      <div id="root">
+        ${this._cards}
+      </div>
+    `;
+  }
+
+  protected abstract renderStyle(): TemplateResult;
+}
