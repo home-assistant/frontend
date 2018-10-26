1185d1f52b5d89e81dab1e5ee4e88c390e6968c6
Paulus Schoutsen <balloob@gmail.com>
2018-10-26 19:36:23 +0200

    Extract navigate to function

--- src/mixins/navigate-mixin.js
+++ src/mixins/navigate-mixin.js
@@ -1,5 +1,5 @@
 import { dedupingMixin } from "@polymer/polymer/lib/utils/mixin.js";
-import EventsMixin from "./events-mixin";
+import { navigate } from "../common/navigate";
 
 /*
  * @polymerMixin
@@ -8,13 +8,8 @@ import EventsMixin from "./events-mixin";
 export default dedupingMixin(
   (superClass) =>
     class extends EventsMixin(superClass) {
-      navigate(path, replace = false) {
-        if (replace) {
-          history.replaceState(null, null, path);
-        } else {
-          history.pushState(null, null, path);
-        }
-        this.fire("location-changed");
+      navigate(...args) {
+        navigate(this, ...args);
       }
     }
 );
