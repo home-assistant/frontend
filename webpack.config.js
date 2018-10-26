ae52dca6512c4c16a796e4c06efa5e9a7ab5cd42
Paulus Schoutsen <paulus@home-assistant.io>
2018-10-26 23:22:45 +0200

    Fix version extract (#1866)

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const zopfli = require("@gfx/zopfli");
 const translationMetadata = require("./build-translations/translationMetadata.json");
 const { babelLoaderConfig } = require("./config/babel.js");
 
-const version = fs.readFileSync("setup.py", "utf8").match(/\d{8}[^']*/);
+const version = fs.readFileSync("setup.py", "utf8").match(/\d{8}\.\d+/);
 if (!version) {
   throw Error("Version not found");
 }
