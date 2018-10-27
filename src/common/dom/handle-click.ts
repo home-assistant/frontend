7b7bf16213481df7c5f4ec2a0db2836d456af570
Anton Verburg <info@tec-v.nl>
2018-10-27 16:51:44 -0500

    Implemented navigate function
    Fix linting error

--- src/common/dom/handle-click.ts
+++ src/common/dom/handle-click.ts
@@ -1,6 +1,7 @@
 import { HomeAssistant } from "../../types";
 import { LovelaceElementConfig } from "../../panels/lovelace/elements/types";
 import { fireEvent } from "../dom/fire_event.js";
+import { navigate } from "../../common/navigate";
 import toggleEntity from "../../../src/panels/lovelace/common/entity/toggle-entity";
 
 export const handleClick = (
@@ -24,7 +25,7 @@ export const handleClick = (
       fireEvent(node, "hass-more-info", { entityId: config.entity });
       break;
     case "navigate":
-      // this.navigate(config.navigation_path); // TODO wait for balloob's navigate function
+      navigate(node, config.navigation_path ? config.navigation_path : "");
       break;
     case "toggle":
       toggleEntity(hass, config.entity);
@@ -32,11 +33,10 @@ export const handleClick = (
     case "call-service": {
       if (config.service) {
         const [domain, service] = config.service.split(".", 2);
-        const serviceData = Object.assign(
-          {},
-          { entity_id: config.entity },
-          config.service_data
-        );
+        const serviceData = {
+          entity_id: config.entity,
+          ...config.service_data,
+        };
         hass.callService(domain, service, serviceData);
       }
     }
