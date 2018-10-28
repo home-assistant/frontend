517290a32f8ba98dabda87c2b45c1447f4a7a2e9
Paulus Schoutsen <balloob@gmail.com>
2018-10-28 09:41:48 +0100

    Handle no operation mode

--- src/types.ts
+++ src/types.ts
@@ -4,6 +4,7 @@ import {
   Auth,
   Connection,
   MessageBase,
+  HassEntity,
 } from "home-assistant-js-websocket";
 
 export interface Credential {
@@ -85,3 +86,27 @@ export interface HomeAssistant {
   sendWS: (msg: MessageBase) => Promise<void>;
   callWS: <T>(msg: MessageBase) => Promise<T>;
 }
+
+export type ClimateEntity = HassEntity & {
+  attributes: {
+    current_temperature: number;
+    min_temp: number;
+    max_temp: number;
+    temperature: number;
+    target_temp_step?: number;
+    target_temp_high?: number;
+    target_temp_low?: number;
+    target_humidity?: number;
+    target_humidity_low?: number;
+    target_humidity_high?: number;
+    fan_mode?: string;
+    fan_list?: string[];
+    operation_mode?: string;
+    operation_list?: string[];
+    hold_mode?: string;
+    swing_mode?: string;
+    swing_list?: string[];
+    away_mode?: "on" | "off";
+    aux_heat?: "on" | "off";
+  };
+};
