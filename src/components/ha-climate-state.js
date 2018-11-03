ba21b718bee8f0f3843d27667f9bfea99d3d51b8
Michael Scherer <mic@n49e08.de>
2018-11-03 20:28:35 +0000

    better comparsion

--- src/components/ha-climate-state.js
+++ src/components/ha-climate-state.js
@@ -104,7 +104,7 @@ class HaClimateState extends LocalizeMixin(PolymerElement) {
   }
 
   _hasKnownState(state) {
-    return state != 'unknown';
+    return state !== 'unknown';
   }
 
   _localizeState(state) {
