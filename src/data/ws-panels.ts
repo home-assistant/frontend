9639252eaf2e45d672a11e8fc38ccb303e25c087
Paulus Schoutsen <balloob@gmail.com>
2018-11-03 22:33:04 +0100

    Migrate ws collections to TS

--- src/data/ws-panels.ts
+++ src/data/ws-panels.ts
@@ -4,7 +4,7 @@ export const subscribePanels = (conn, onChange) =>
   createCollection(
     "_pnl",
     (conn_) => conn_.sendMessagePromise({ type: "get_panels" }),
-    null,
+    undefined,
     conn,
     onChange
   );
--- src/data/ws-user.ts
+++ src/data/ws-user.ts
@@ -1,4 +1,10 @@
 import { createCollection, getUser } from "home-assistant-js-websocket";
 
 export const subscribeUser = (conn, onChange) =>
-  createCollection("_usr", (conn_) => getUser(conn_), null, conn, onChange);
+  createCollection(
+    "_usr",
+    (conn_) => getUser(conn_),
+    undefined,
+    conn,
+    onChange
+  );
