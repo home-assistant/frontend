851d8c0e94f4c3956b84ab69511aa9ba7ee9e4cc
Paulus Schoutsen <paulus@home-assistant.io>
2018-11-05 09:59:19 +0100

    Split up cloud card (#1983)
    * Split up cloud card
    
    * Fix quotes

---
+++ src/panels/config/cloud/data.ts
@@ -0,0 +1,17 @@
+import { HomeAssistant } from "../../../types";
+import { SubscriptionInfo } from "./types";
+
+export const fetchSubscriptionInfo = (hass: HomeAssistant) =>
+  hass.callWS<SubscriptionInfo>({ type: "cloud/subscription" });
+
+export const updatePref = (
+  hass: HomeAssistant,
+  prefs: {
+    google_enabled?: boolean;
+    alexa_enabled?: boolean;
+  }
+) =>
+  hass.callWS({
+    type: "cloud/update_prefs",
+    ...prefs,
+  });
