fb0011dcc075fd2c833b395c168294d38b9ae0ba
Ian Richardson <iantrich@gmail.com>
2018-11-06 03:47:24 -0600

    New Card: Shopping List (#1970)
    * New Card: Shopping List
    
    Following features:
    - Add item
    - Edit item
    - Complete item
    - Clear items
    
    * Address Travis complaint
    
    * Addressed review comments
    
    * Update translation variable name
    
    * Line up input row text
    
    * Taking MVP to heart
    
    Addressed review comments and scaled this back to just get a simple shopping list card out there and we can discuss/debate how best to add the additional pieces with smaller PRs
    
    * Remove calling connected in set hass

---
+++ src/data/shopping-list.ts
@@ -0,0 +1,28 @@
+import { HomeAssistant } from "../types";
+
+export interface ShoppingListItem {
+  id: number;
+  name: string;
+  complete: boolean;
+}
+
+export const fetchItems = (hass: HomeAssistant): Promise<ShoppingListItem[]> =>
+  hass.callApi("GET", "shopping_list");
+
+export const saveEdit = (
+  hass: HomeAssistant,
+  itemId: number,
+  name: string
+): Promise<ShoppingListItem> =>
+  hass.callApi("POST", "shopping_list/item/" + itemId, {
+    name,
+  });
+
+export const completeItem = (
+  hass: HomeAssistant,
+  itemId: number,
+  complete: boolean
+): Promise<void> =>
+  hass.callApi("POST", "shopping_list/item/" + itemId, {
+    complete,
+  });
