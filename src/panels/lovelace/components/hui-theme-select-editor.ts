1884d8f76e1694554353a6946a9cc36cab92c23d
Zack Arnett <arnett.zackary@gmail.com>
2018-11-06 20:57:40 -0500

    Glance Card Editor

---
+++ src/panels/lovelace/components/hui-theme-select-editor.ts
@@ -0,0 +1,66 @@
+import { html, LitElement, PropertyDeclarations } from "@polymer/lit-element";
+import "@polymer/paper-button/paper-button";
+import { TemplateResult } from "lit-html";
+
+import { HomeAssistant } from "../../../types";
+import { fireEvent } from "../../../common/dom/fire_event";
+import { hassLocalizeLitMixin } from "../../../mixins/lit-localize-mixin";
+
+export class HuiThemeSelectionEditor extends hassLocalizeLitMixin(LitElement) {
+  protected hass?: HomeAssistant;
+  private value?: string;
+
+  static get properties(): PropertyDeclarations {
+    return {
+      hass: {},
+      value: {},
+    };
+  }
+
+  protected render(): TemplateResult {
+    const themes = ["Backend-selected", "default"].concat(
+      Object.keys(this.hass!.themes.themes).sort()
+    );
+    return html`
+      ${this.renderStyle()}
+      <paper-dropdown-menu
+        label="${this.localize("ui.panel.profile.themes.dropdown_label")}"
+        dynamic-align
+        @value-changed="${this._changed}"
+      >
+        <paper-listbox
+          slot="dropdown-content"
+          selected="${this.value || "Backend-selected"}"
+          attr-for-selected="theme"
+        >
+          ${themes.map((theme) => {
+            return html`<paper-item .theme=${theme}>${theme}</paper-item>`;
+          })}
+        </paper-listbox>
+      </paper-dropdown-menu>
+    `;
+  }
+
+  private renderStyle(): TemplateResult {
+    return html`
+      <style>
+        paper-dropdown-menu {
+          width: 100%;
+        }
+      </style>
+    `;
+  }
+
+  private _changed(ev): void {
+    this.value = ev.target.value;
+    fireEvent(this, "change");
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "hui-theme-select-editor": HuiThemeSelectionEditor;
+  }
+}
+
+customElements.define("hui-theme-select-editor", HuiThemeSelectionEditor);
--- src/panels/lovelace/editor/hui-glance-card-editor.ts
+++ src/panels/lovelace/editor/hui-glance-card-editor.ts
@@ -1,15 +1,20 @@
 import { html, LitElement, PropertyDeclarations } from "@polymer/lit-element";
 import { TemplateResult } from "lit-html";
 import "@polymer/paper-checkbox/paper-checkbox";
+import "@polymer/paper-dropdown-menu/paper-dropdown-menu";
+import "@polymer/paper-item/paper-item";
+import "@polymer/paper-listbox/paper-listbox";
 
+import processConfigEntities from "../common/process-config-entities";
 import { hassLocalizeLitMixin } from "../../../mixins/lit-localize-mixin";
 import { HomeAssistant } from "../../../types";
 import { LovelaceCardEditor } from "../types";
 import { fireEvent } from "../../../common/dom/fire_event";
-import { Config } from "../cards/hui-glance-card";
+import { Config, EntityConfig } from "../cards/hui-glance-card";
 
 import "../../../components/entity/state-badge";
 import "../../../components/entity/ha-entity-picker";
+import "../components/hui-theme-select-editor";
 import "../../../components/ha-card";
 import "../../../components/ha-icon";
 
@@ -17,16 +22,21 @@ export class HuiGlanceCardEditor extends hassLocalizeLitMixin(LitElement)
   implements LovelaceCardEditor {
   public hass?: HomeAssistant;
   private _config?: Config;
+  private _configEntities?: EntityConfig[];
 
   static get properties(): PropertyDeclarations {
     return {
       hass: {},
       _config: {},
+      _configEntities: {},
     };
   }
 
   public setConfig(config: Config): void {
     this._config = { type: "glance", ...config };
+    const entities = processConfigEntities(config.entities);
+
+    this._configEntities = entities;
   }
 
   protected render(): TemplateResult {
@@ -35,26 +45,92 @@ export class HuiGlanceCardEditor extends hassLocalizeLitMixin(LitElement)
     }
 
     return html`
+      ${this.renderStyle()}
       <paper-input
         label="Title"
         value="${this._config!.title}"
-        .configValue="${"title"}"
+        .configValue=${"title"}
+        .type=${"input"}
         @value-changed="${this._valueChanged}"
-      ></paper-input
-      ><br />
+      ></paper-input>
+      <hui-theme-select-editor
+        .hass=${this.hass}
+        .value=${this._config!.theme}
+        .type=${"input"}
+        .configValue=${"theme"}
+        @change=${this._valueChanged}
+      ></hui-theme-select-editor><br>
+      <paper-input
+        label="Columns"
+        value="${this._config!.columns || ""}"
+        .configValue=${"columns"}
+        .type=${"input"}
+        @value-changed="${this._valueChanged}"
+      ></paper-input>
+      <h4>Entities</h4>
+      <div class="entities">
+        ${this._configEntities!.map((entityConf, index) => {
+          return this.renderEntity(entityConf, index!);
+        })}
+      </div><br>
+      <paper-button
+        noink
+        raised
+        @click=${this.addEntity}
+      >Add Entity</paper-button>
+      <br><br>
       <paper-checkbox
         ?checked="${this._config!.show_name !== false}"
-        .configValue="${"show_name"}"
+        .configValue=${"show_name"}
+        .type=${"checkbox"}
         @change="${this._valueChanged}"
         >Show Entity's Name?</paper-checkbox
       ><br /><br />
       <paper-checkbox
         ?checked="${this._config!.show_state !== false}"
-        .configValue="${"show_state"}"
+        .configValue=${"show_state"}
+        .type=${"checkbox"}
         @change="${this._valueChanged}"
         >Show Entity's State Text?</paper-checkbox
       ><br />
     `;
+  }
+
+  private renderStyle(): TemplateResult {
+    return html`
+      <style>
+        .entities {
+          padding-left: 20px;
+        }
+      </style>
+    `;
+  }
+
+  private renderEntity(
+    entityConf: EntityConfig,
+    index: number
+  ): TemplateResult {
+    return html`
+      <ha-entity-picker
+        .hass="${this.hass}"
+        .value="${entityConf.entity || entityConf}"
+        .type=${"entity"}
+        .index="${index}"
+        @change="${this._valueChanged}"
+      ></ha-entity-picker>
+    `;
+  }
+
+  private addEntity() {
+    const newConfig = this._config!;
+    const newConfigEntites = this._configEntities!;
+
+    newConfigEntites.push(newConfigEntites[0].entity);
+    newConfig.entities = newConfigEntites;
+
+    fireEvent(this, "config-changed", {
+      config: newConfig,
+    });
   }
 
   private _valueChanged(ev: MouseEvent): void {
@@ -63,12 +139,30 @@ export class HuiGlanceCardEditor extends hassLocalizeLitMixin(LitElement)
     }
 
     const target = ev.target! as any;
+    let newConfig = this._config;
+    const newConfigEntites = this._configEntities!;
 
-    const newValue =
-      target.checked !== undefined ? target.checked : target.value;
+    switch (target.type) {
+      case "input":
+        newConfig = { ...this._config, [target.configValue]: target.value };
+        break;
+
+      case "checkbox":
+        newConfig = { ...this._config, [target.configValue]: target.checked };
+        break;
+
+      case "entity":
+        if (target.value === "") {
+          newConfigEntites.splice(target.index, 1);
+        } else {
+          newConfigEntites[target.index].entity = target.value;
+        }
+        newConfig.entities = newConfigEntites;
+        break;
+    }
 
     fireEvent(this, "config-changed", {
-      config: { ...this._config, [target.configValue]: newValue },
+      config: newConfig,
     });
   }
 }
