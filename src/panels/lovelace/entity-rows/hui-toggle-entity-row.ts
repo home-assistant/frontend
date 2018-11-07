d35ff436b5a68b0518fa6ceda009face42d92c87
Ian Richardson <iantrich@gmail.com>
2018-11-07 02:30:05 -0600

    Convert hui-toggle-entity-row to TypeScript/LitElement (#1939)
    * Convert hui-toggle-entity-row to TypeScript/LitElement
    
    * Properly set generic-entity properties
    
    * Addressed review comments
    
    * Address review comments

---
+++ src/panels/lovelace/entity-rows/hui-toggle-entity-row.ts
@@ -0,0 +1,78 @@
+import { html, LitElement, PropertyDeclarations } from "@polymer/lit-element";
+import { TemplateResult } from "lit-html";
+
+import "../components/hui-generic-entity-row";
+import "../../../components/entity/ha-entity-toggle";
+import "./hui-error-entity-row";
+
+import computeStateDisplay from "../../../common/entity/compute_state_display";
+import { hassLocalizeLitMixin } from "../../../mixins/lit-localize-mixin";
+import { HomeAssistant } from "../../../types";
+import { EntityRow, EntityConfig } from "./types";
+
+class HuiToggleEntityRow extends hassLocalizeLitMixin(LitElement)
+  implements EntityRow {
+  public hass?: HomeAssistant;
+  private _config?: EntityConfig;
+
+  static get properties(): PropertyDeclarations {
+    return {
+      hass: {},
+      _config: {},
+    };
+  }
+
+  public setConfig(config: EntityConfig): void {
+    if (!config) {
+      throw new Error("Configuration error");
+    }
+    this._config = config;
+  }
+
+  protected render(): TemplateResult {
+    if (!this._config || !this.hass) {
+      return html``;
+    }
+
+    const stateObj = this.hass.states[this._config.entity];
+
+    if (!stateObj) {
+      return html`
+        <hui-error-entity-row
+          .entity=${this._config.entity}
+        ></hui-error-entity-row>`;
+    }
+
+    return html`
+      <hui-generic-entity-row
+        .hass=${this.hass}
+        .config=${this._config}
+      >
+        ${
+          stateObj.state === "on" || stateObj.state === "off"
+            ? html`
+            <ha-entity-toggle
+              .hass=${this.hass}
+              .stateObj=${stateObj}
+            ></ha-entity-toggle>`
+            : html`
+            <div>
+              ${computeStateDisplay(
+                this.localize,
+                stateObj,
+                this.hass!.language
+              )}
+            </div>`
+        }
+      </hui-generic-entity-row>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "hui-toggle-entity-row": HuiToggleEntityRow;
+  }
+}
+
+customElements.define("hui-toggle-entity-row", HuiToggleEntityRow);
