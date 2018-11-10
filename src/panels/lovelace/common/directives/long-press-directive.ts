da069bd7e4afc3a17a5217384bc04871ddaebffe
Paulus Schoutsen <paulus@home-assistant.io>
2018-11-10 20:36:25 +0100

    Upgrade Lit (#2032)

--- src/panels/lovelace/common/directives/long-press-directive.ts
+++ src/panels/lovelace/common/directives/long-press-directive.ts
@@ -134,7 +134,6 @@ export const longPressBind = (element: LongPressElement) => {
   longpress.bind(element);
 };
 
-export const longPress = () =>
-  directive((part: PropertyPart) => {
-    longPressBind(part.committer.element);
-  });
+export const longPress = directive(() => (part: PropertyPart) => {
+  longPressBind(part.committer.element);
+});
