e76bd6378125e2ee1d8965d3f76a1ede071d4482
Ian Richardson <iantrich@gmail.com>
2018-11-12 22:13:57 -0600

    Remove isValidObject function
    This is not used in the codebase and can be removed unless anyone can think of a reason to keep it around for future use.

--- src/panels/lovelace/common/is-valid-object.js
+++
@@ -1,9 +0,0 @@
-// Check if given obj is a JS object and optionally contains all required keys
-export default function isValidObject(obj, requiredKeys = []) {
-  return (
-    obj &&
-    typeof obj === "object" &&
-    !Array.isArray(obj) &&
-    requiredKeys.every((k) => k in obj)
-  );
-}
