276729bb14255351689312cb760a40b9a00337d7
Ian Richardson <iantrich@gmail.com>
2018-11-16 11:46:16 -0600

    Attempt to not be dumb :)

--- src/panels/lovelace/common/compute-notifications.ts
+++ src/panels/lovelace/common/compute-notifications.ts
@@ -1,8 +1,8 @@
-import { HassEntities } from "home-assistant-js-websocket";
+import { HassEntities, HassEntity } from "home-assistant-js-websocket";
 
 import computeDomain from "../../../common/entity/compute_domain";
 
-export const computeNotifications = (states: HassEntities[]): string[] => {
+export const computeNotifications = (states: HassEntities): HassEntity[] => {
   return Object.keys(states)
     .filter((entityId) => computeDomain(entityId) === "configurator")
     .map((entityId) => states[entityId]);
