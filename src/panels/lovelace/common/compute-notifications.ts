82f8f5e99aaca08ef57c98b4a969efcfee95dd1e
Anton Verburg <info@tec-v.nl>
2018-11-18 07:50:21 -0600

    Convert compute functions to TypeScript (#2055)
    * Convert compute functions to TypeScript
    
    * Address review comment
    
    * Address Travis complaint
    
    * Attempt to not be dumb :)

--- src/panels/lovelace/common/compute-notifications.ts
+++ src/panels/lovelace/common/compute-notifications.ts
@@ -1,7 +1,9 @@
+import { HassEntities, HassEntity } from "home-assistant-js-websocket";
+
 import computeDomain from "../../../common/entity/compute_domain";
 
-export default function computeNotifications(states) {
+export const computeNotifications = (states: HassEntities): HassEntity[] => {
   return Object.keys(states)
     .filter((entityId) => computeDomain(entityId) === "configurator")
     .map((entityId) => states[entityId]);
-}
+};
