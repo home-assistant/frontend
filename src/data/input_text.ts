7b513252f780b87e59cdd286bb865f41ce99ca0f
Ian Richardson <iantrich@gmail.com>
2018-11-20 06:09:45 -0600

    Convert hui-input-text-entity-row to TypeScript/LitElement (#2050)
    * Convert hui-input-text-entity-row to TypeScript/LitElement
    
    * Address review comments
    
    * Address review comments
    
    Does anyone know why line 74 seemingly doesn't fire when the value hasn't changed? I think we should blur regardless if the value changed or not on "enter" but only seems to work when setValue is called
    
    * Return promise from call service

---
+++ src/data/input_text.ts
@@ -0,0 +1,7 @@
+import { HomeAssistant } from "../types";
+
+export const setValue = (hass: HomeAssistant, entity: string, value: string) =>
+  hass.callService("input_text", "set_value", {
+    value,
+    entity_id: entity,
+  });
