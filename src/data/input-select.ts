3271ecc1d92091b11d28b0580c918c555b230030
Paulus Schoutsen <balloob@gmail.com>
2018-11-20 12:30:30 +0100

    Return callService promise

--- src/data/input-select.ts
+++ src/data/input-select.ts
@@ -4,9 +4,8 @@ export const setOption = (
   hass: HomeAssistant,
   entity: string,
   option: string
-) => {
+) =>
   hass.callService("input_select", "select_option", {
     option,
     entity_id: entity,
   });
-};
