30779b7546844625d49f475ac75a7cfc477ae0fb
Zack Arnett <arnett.zackary@gmail.com>
2018-11-20 22:17:45 -0500

    first commit

--- src/panels/lovelace/components/hui-entity-editor.ts
+++ src/panels/lovelace/components/hui-entity-editor.ts
@@ -32,13 +32,32 @@ export class HuiEntityEditor extends LitElement {
         ${
           this.entities.map((entityConf, index) => {
             return html`
-              <ha-entity-picker
-                .hass="${this.hass}"
-                .value="${entityConf.entity}"
-                .index="${index}"
-                @change="${this._valueChanged}"
-                allow-custom-entity
-              ></ha-entity-picker>
+              <div class="entity">
+                <ha-entity-picker
+                  .hass="${this.hass}"
+                  .value="${entityConf.entity}"
+                  .index="${index}"
+                  @change="${this._valueChanged}"
+                  allow-custom-entity
+                ></ha-entity-picker>
+                <paper-checkbox>Options</paper-checkbox>
+                <div class="options">
+                  <paper-input
+                    label="Name"
+                    .value="${entityConf.name || ""}"
+                    .configValue="${"name"}"
+                    .index="${index}"
+                    @value-changed="${this._optionChanged}"
+                  ></paper-input>
+                  <paper-input
+                    label="icon"
+                    .value="${entityConf.icon || ""}"
+                    .configValue="${"icon"}"
+                    .index="${index}"
+                    @value-changed="${this._optionChanged}"
+                  ></paper-input>
+                </div>
+              </div>
             `;
           })
         }
@@ -71,14 +90,49 @@ export class HuiEntityEditor extends LitElement {
     fireEvent(this, "change", { entities: newConfigEntities });
   }
 
+  private _optionChanged(ev: Event): void {
+    const target = ev.target! as any;
+    const newConfigEntities = this.entities!.concat();
+    newConfigEntities[target.index] = {
+      ...newConfigEntities[target.index!],
+      [target.configValue]: target.value,
+    };
+
+    fireEvent(this, "change", { entities: newConfigEntities });
+  }
+
   private renderStyle(): TemplateResult {
     return html`
       <style>
         .entities {
           padding-left: 20px;
         }
+        .entity {
+          display: flex;
+          align-items: flex-end;
+          flex-direction: row;
+          flex-wrap: wrap;
+        }
         paper-button {
           margin: 8px 0;
+        }
+        ha-entity-picker {
+          flex: 3;
+        }
+        .entity paper-checkbox {
+          flex: 1;
+          padding: 0 0 8px 16px;
+        }
+        .options {
+          display: none;
+          width: 100%;
+        }
+        .options > * {
+          flex: 1;
+          padding-right: 4px;
+        }
+        .entity paper-checkbox[checked] ~ .options {
+          display: flex;
         }
       </style>
     `;
