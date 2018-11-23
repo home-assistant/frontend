b32eae32d931185458ce735653b2c88aa77ea3aa
Simon Holzmayer <simon@holzmayer.me>
2018-11-23 11:52:48 +0100

    add "for" input field to numeric_state in trigger editor (#2081)

--- src/panels/config/js/trigger/numeric_state.js
+++ src/panels/config/js/trigger/numeric_state.js
@@ -24,7 +24,18 @@ export default class NumericStateTrigger extends Component {
   /* eslint-disable camelcase */
   render({ trigger, hass, localize }) {
     const { value_template, entity_id, below, above } = trigger;
+    let trgFor = trigger.for;
 
+    if (trgFor && (trgFor.hours || trgFor.minutes || trgFor.seconds)) {
+      // If the trigger was defined using the yaml dict syntax, convert it to
+      // the equivalent string format
+      let { hours = 0, minutes = 0, seconds = 0 } = trgFor;
+      hours = hours.toString();
+      minutes = minutes.toString().padStart(2, "0");
+      seconds = seconds.toString().padStart(2, "0");
+
+      trgFor = `${hours}:${minutes}:${seconds}`;
+    }
     return (
       <div>
         <ha-entity-picker
@@ -55,6 +66,14 @@ export default class NumericStateTrigger extends Component {
           )}
           name="value_template"
           value={value_template}
+          onvalue-changed={this.onChange}
+        />
+        <paper-input
+          label={localize(
+            "ui.panel.config.automation.editor.triggers.type.state.for"
+          )}
+          name="for"
+          value={trgFor}
           onvalue-changed={this.onChange}
         />
       </div>
