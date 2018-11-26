8a14f4f59169de7deb2211f92d03bb3d64ee4e3f
Paulus Schoutsen <balloob@gmail.com>
2018-11-26 12:17:36 +0100

    Convert authorize page to lit

---
+++ src/mixins/localize-lite-base-mixin.ts
@@ -0,0 +1,44 @@
+/**
+ * Lite base mixin to add localization without depending on the Hass object.
+ */
+import { localizeBaseMixin } from "./localize-base-mixin";
+import { getTranslation } from "../util/hass-translation";
+
+/**
+ * @polymerMixin
+ */
+export const localizeLiteBaseMixin = (superClass) =>
+  class extends localizeBaseMixin(superClass) {
+    protected _initializeLocalizeLite() {
+      if (this.resources) {
+        return;
+      }
+
+      if (!this.translationFragment) {
+        // In dev mode, we will issue a warning if after a second we are still
+        // not configured correctly.
+        if (__DEV__) {
+          setTimeout(
+            () =>
+              !this.resources &&
+              // tslint:disable-next-line
+              console.error(
+                "Forgot to pass in resources or set translationFragment for",
+                this.nodeName
+              ),
+            1000
+          );
+        }
+        return;
+      }
+
+      this._updateResources();
+    }
+
+    private async _updateResources() {
+      const { language, data } = await getTranslation(this.translationFragment);
+      this.resources = {
+        [language]: data,
+      };
+    }
+  };
