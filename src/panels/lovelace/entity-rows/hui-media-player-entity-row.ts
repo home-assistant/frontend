88142324ffb6fdc47b544706bcb5019a934f0aa2
Ian Richardson <iantrich@gmail.com>
2018-11-29 13:27:34 -0600

    Extract media-player constants to src/data

--- src/panels/lovelace/entity-rows/hui-media-player-entity-row.ts
+++ src/panels/lovelace/entity-rows/hui-media-player-entity-row.ts
@@ -8,11 +8,12 @@ import { hassLocalizeLitMixin } from "../../../mixins/lit-localize-mixin";
 import { EntityRow, EntityConfig } from "./types";
 import { HomeAssistant } from "../../../types";
 import { HassEntity } from "home-assistant-js-websocket";
-
-const SUPPORT_PAUSE = 1;
-const SUPPORT_NEXT_TRACK = 32;
-const SUPPORTS_PLAY = 16384;
-const OFF_STATES = ["off", "idle"];
+import {
+  SUPPORTS_PLAY,
+  SUPPORT_NEXT_TRACK,
+  OFF_STATES,
+  SUPPORT_PAUSE,
+} from "../../../data/media-player";
 
 class HuiMediaPlayerEntityRow extends hassLocalizeLitMixin(LitElement)
   implements EntityRow {
