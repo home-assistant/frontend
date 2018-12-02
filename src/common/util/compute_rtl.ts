2c329ebd9eeec1c78e86c1f2773a99bc098e976b
Ronen Hayun <ronen.hayun@weizmann.ac.il>
2018-12-02 11:08:48 +0200

    Replace var to const

--- src/common/util/compute_rtl.ts
+++ src/common/util/compute_rtl.ts
@@ -1,7 +1,7 @@
 import { HassEntity } from "home-assistant-js-websocket";
 
 export default function computeRTL(stateObj: HassEntity) {
-  var lang = stateObj.selectedLanguage || stateObj.language;
+  const lang = stateObj.selectedLanguage || stateObj.language;
   if (stateObj.translationMetadata.translations[lang]) {
     return stateObj.translationMetadata.translations[lang].isRTL || false;
   }
