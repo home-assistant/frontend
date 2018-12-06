1bb352a3508fa6f1f21f14085c7f2d77d79699d0
Anton Verburg <info@tec-v.nl>
2018-12-06 14:10:37 +0100

    Code split jQuery roundslider (#2197)

--- src/resources/jquery.roundslider.js
+++ src/resources/jquery.roundslider.js
@@ -1,7 +1,11 @@
 import { html } from "@polymer/lit-element";
-import "./jquery";
+// jQuery import should come before plugin import
+import { jQuery as jQuery_ } from "./jquery";
 import "round-slider";
+// eslint-disable-next-line
 import roundSliderCSS from "round-slider/dist/roundslider.min.css";
+
+export const jQuery = jQuery_;
 
 export const roundSliderStyle = html`
   <style>
