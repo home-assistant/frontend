47de0e9156f659d07b26550f47fbfac7b11dab9d
Ian Richardson <iantrich@gmail.com>
2018-12-07 22:54:18 -0600

    UI Editor for `alarm-panel` card
    Still need to get states sorted out

--- src/panels/lovelace/cards/hui-alarm-panel-card.js
+++ src/panels/lovelace/cards/hui-alarm-panel-card.js
@@ -21,7 +21,21 @@ const Icons = {
   triggered: "hass:bell-ring",
 };
 
+export const Config = {
+  title: "",
+  entity: "",
+  states: "",
+};
+
 class HuiAlarmPanelCard extends LocalizeMixin(EventsMixin(PolymerElement)) {
+  static async getConfigElement() {
+    await import("../editor/config-elements/hui-alarm-panel-card-editor");
+    return document.createElement("hui-alarm-panel-card-editor");
+  }
+  static getStubConfig() {
+    return { states: ["arm_home", "arm_away"] };
+  }
+
   static get template() {
     return html`
     <style>
