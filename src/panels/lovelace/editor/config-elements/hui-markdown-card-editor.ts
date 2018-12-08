d7e59935015062e97d7f5d871d9e8e63d0fb396e
Ian Richardson <iantrich@gmail.com>
2018-12-07 21:39:27 -0600

    UI Editor for `markdown` card

---
+++ src/panels/lovelace/editor/config-elements/hui-markdown-card-editor.ts
@@ -0,0 +1,97 @@
+import { html, LitElement, PropertyDeclarations } from "@polymer/lit-element";
+import { TemplateResult } from "lit-html";
+import { struct } from "../../common/structs/struct";
+import "@polymer/paper-input/paper-input";
+import "@polymer/paper-input/paper-textarea";
+
+import { EntitiesEditorEvent, EditorTarget } from "../types";
+import { hassLocalizeLitMixin } from "../../../../mixins/lit-localize-mixin";
+import { HomeAssistant } from "../../../../types";
+import { LovelaceCardEditor } from "../../types";
+import { fireEvent } from "../../../../common/dom/fire_event";
+import { Config } from "../../cards/hui-glance-card";
+import { configElementStyle } from "./config-elements-style";
+
+const cardConfigStruct = struct({
+  type: "string",
+  id: "string|number",
+  title: "string?",
+  content: "string",
+});
+
+export class HuiMarkdownCardEditor extends hassLocalizeLitMixin(LitElement)
+  implements LovelaceCardEditor {
+  public hass?: HomeAssistant;
+  private _config?: Config;
+
+  public setConfig(config: Config): void {
+    config = cardConfigStruct(config);
+
+    this._config = { type: "markdown", ...config };
+  }
+
+  static get properties(): PropertyDeclarations {
+    return { hass: {}, _config: {} };
+  }
+
+  get _title(): string {
+    return this._config!.title || "";
+  }
+
+  get _content(): string {
+    return this._config!.content || "";
+  }
+
+  protected render(): TemplateResult {
+    if (!this.hass) {
+      return html``;
+    }
+
+    return html`
+      ${configElementStyle}
+      <div class="card-config">
+        <paper-input
+          label="Title"
+          value="${this._title}"
+          .configValue="${"title"}"
+          @value-changed="${this._valueChanged}"
+        ></paper-input>
+        <paper-textarea
+          label="Content"
+          value="${this._content}"
+          .configValue="${"content"}"
+          @value-changed="${this._valueChanged}"
+          autocapitalize="none"
+          autocomplete="off"
+          spellcheck="false"
+        ></paper-textarea>
+      </div>
+    `;
+  }
+
+  private _valueChanged(ev: EntitiesEditorEvent): void {
+    if (!this._config || !this.hass) {
+      return;
+    }
+    const target = ev.target! as EditorTarget;
+
+    if (this[`_${target.configValue}`] === target.value) {
+      return;
+    }
+    if (target.configValue) {
+      this._config = {
+        ...this._config,
+        [target.configValue!]: target.value,
+      };
+    }
+    fireEvent(this, "config-changed", { config: this._config });
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "hui-markdown-card-editor": HuiMarkdownCardEditor;
+  }
+}
+
+customElements.define("hui-markdown-card-editor", HuiMarkdownCardEditor);
