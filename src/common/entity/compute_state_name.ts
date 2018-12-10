4077783f5575c3316ff815daa1b1aa9c88ee84f1
Bram Kragten <mail@bramkragten.nl>
2018-12-10 15:01:55 +0100

    remove returning on undefined in statename

--- src/common/entity/compute_state_name.ts
+++ src/common/entity/compute_state_name.ts
@@ -6,9 +6,6 @@ type CachedDisplayEntity = HassEntity & {
 };
 
 export default function computeStateName(stateObj: HassEntity) {
-  if (!stateObj) {
-    return;
-  }
   const state = stateObj as CachedDisplayEntity;
 
   if (state._entityDisplay === undefined) {
