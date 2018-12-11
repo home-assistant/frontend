c0ce9345ba7fb901a4246e5f78de4add0fbcdb72
Anton Verburg <info@tec-v.nl>
2018-12-11 19:38:57 +0100

    Add move card to view (#2262)
    * Add move card to view
    
    * Fix style
    
    * Fix style and tests
    
    * last style change
    
    * update tests

--- test-mocha/panels/lovelace/editor/config-util.spec.ts
+++ test-mocha/panels/lovelace/editor/config-util.spec.ts
@@ -1,6 +1,9 @@
 import * as assert from "assert";
 
-import { swapCard } from "../../../../src/panels/lovelace/editor/config-util";
+import {
+  swapCard,
+  moveCard,
+} from "../../../../src/panels/lovelace/editor/config-util";
 import { LovelaceConfig } from "../../../../src/data/lovelace";
 
 describe("swapCard", () => {
@@ -50,5 +53,79 @@ describe("swapCard", () => {
       ],
     };
     assert.deepEqual(expected, result);
+  });
+});
+
+describe("moveCard", () => {
+  it("move a card to an empty view", () => {
+    const config: LovelaceConfig = {
+      views: [
+        {},
+        {
+          cards: [{ type: "card1" }, { type: "card2" }],
+        },
+      ],
+    };
+
+    const result = moveCard(config, [1, 0], [0]);
+    const expected = {
+      views: [
+        {
+          cards: [{ type: "card1" }],
+        },
+        {
+          cards: [{ type: "card2" }],
+        },
+      ],
+    };
+    assert.deepEqual(expected, result);
+  });
+
+  it("move a card to different view", () => {
+    const config: LovelaceConfig = {
+      views: [
+        {
+          cards: [{ type: "v1-c1" }, { type: "v1-c2" }],
+        },
+        {
+          cards: [{ type: "v2-c1" }, { type: "v2-c2" }],
+        },
+      ],
+    };
+
+    const result = moveCard(config, [1, 0], [0]);
+    const expected = {
+      views: [
+        {
+          cards: [{ type: "v1-c1" }, { type: "v1-c2" }, { type: "v2-c1" }],
+        },
+        {
+          cards: [{ type: "v2-c2" }],
+        },
+      ],
+    };
+    assert.deepEqual(expected, result);
+  });
+
+  it("move a card to the same view", () => {
+    const config: LovelaceConfig = {
+      views: [
+        {
+          cards: [{ type: "v1-c1" }, { type: "v1-c2" }],
+        },
+        {
+          cards: [{ type: "v2-c1" }, { type: "v2-c2" }],
+        },
+      ],
+    };
+
+    const result = function() {
+      moveCard(config, [1, 0], [1]);
+    };
+    assert.throws(
+      result,
+      Error,
+      "You can not move a card to the view it is in."
+    );
   });
 });
