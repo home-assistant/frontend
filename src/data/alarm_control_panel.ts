ae2cc588e6dbc53f6b9320d54e3042513351c822
Zack Arnett <arnett.zackary@gmail.com>
2018-12-12 15:08:15 -0500

    Update actions

--- src/data/alarm_control_panel.ts
+++ src/data/alarm_control_panel.ts
@@ -3,7 +3,12 @@ import { HomeAssistant } from "../types";
 export const callAlarmAction = (
   hass: HomeAssistant,
   entity: string,
-  action: string,
+  action:
+    | "arm_away"
+    | "arm_home"
+    | "arm_night"
+    | "arm_custom_bypass"
+    | "disarm",
   code: string
 ) => {
   hass!.callService("alarm_control_panel", "alarm_" + action, {
