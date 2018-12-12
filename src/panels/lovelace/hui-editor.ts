4507ddd59712e3e1798be9e0dc348f6cfa58db55
Zack Arnett <arnett.zackary@gmail.com>
2018-12-12 14:59:35 -0500

    Add Notification if saved is done

--- src/panels/lovelace/hui-editor.ts
+++ src/panels/lovelace/hui-editor.ts
@@ -1,4 +1,5 @@
 import { LitElement, html } from "@polymer/lit-element";
+import { classMap } from "lit-html/directives/classMap";
 import { TemplateResult } from "lit-html";
 import yaml from "js-yaml";
 
@@ -7,9 +8,12 @@ import "@polymer/app-layout/app-header/app-header";
 import "@polymer/app-layout/app-toolbar/app-toolbar";
 import "@polymer/paper-button/paper-button";
 import "@polymer/paper-icon-button/paper-icon-button";
+import "@polymer/paper-spinner/paper-spinner";
 
 import { Lovelace } from "./types";
 import { hassLocalizeLitMixin } from "../../mixins/lit-localize-mixin";
+
+import "../../components/ha-icon";
 
 const TAB_INSERT = "  ";
 
@@ -17,10 +21,12 @@ class LovelaceFullConfigEditor extends hassLocalizeLitMixin(LitElement) {
   public lovelace?: Lovelace;
   public closeEditor?: () => void;
   private _haStyle?: DocumentFragment;
+  private _saving?: boolean;
 
   static get properties() {
     return {
       lovelace: {},
+      _saving: {},
     };
   }
 
@@ -35,7 +41,12 @@ class LovelaceFullConfigEditor extends hassLocalizeLitMixin(LitElement) {
               @click="${this.closeEditor}"
             ></paper-icon-button>
             <div main-title>Edit Config</div>
-            <paper-button @click="${this._handleSave}">Save</paper-button>
+            <paper-button @click="${this._handleSave}"
+              >Save<ha-icon class="${classMap({
+                saved: this._saving! === false,
+                "save-button": true,
+              })}" icon="hass:check"></ha-icon></paper-spinner
+            ></paper-button>
           </app-toolbar>
         </app-header>
         <div class="content">
@@ -112,11 +123,32 @@ class LovelaceFullConfigEditor extends hassLocalizeLitMixin(LitElement) {
           font-family: "Courier New", Courier, monospace;
           padding: 8px;
         }
+
+        .save-button {
+          opacity: 0;
+        }
+
+        .saved {
+          animation-name: opacityOn;
+          animation-duration: 1.5s;
+          animation-delay: 0.5s;
+          animation-fill-mode: forwards;
+        }
+
+        @keyframes opacityOn {
+          0% {
+            opacity: 0;
+          }
+          100% {
+            opacity: 1;
+          }
+        }
       </style>
     `;
   }
 
-  private _handleSave() {
+  private async _handleSave() {
+    this._saving = true;
     let value;
     try {
       value = yaml.safeLoad(this.textArea.value);
@@ -125,7 +157,8 @@ class LovelaceFullConfigEditor extends hassLocalizeLitMixin(LitElement) {
       return;
     }
 
-    this.lovelace!.saveConfig(value);
+    await this.lovelace!.saveConfig(value);
+    this._saving = false;
   }
 
   private get textArea(): HTMLTextAreaElement {
