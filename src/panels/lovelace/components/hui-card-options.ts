f03267be2dd77edc3d717716fa20396ca47fd96f
Zack Arnett <arnett.zackary@gmail.com>
2018-12-13 09:23:02 -0500

    Updates box shadow on card options

--- src/panels/lovelace/components/hui-card-options.ts
+++ src/panels/lovelace/components/hui-card-options.ts
@@ -31,7 +31,7 @@ export class HuiCardOptions extends hassLocalizeLitMixin(LitElement) {
           padding: 5px 8px;
           background: var(--paper-card-background-color, white);
           box-shadow: rgba(0, 0, 0, 0.14) 0px 2px 2px 0px,
-            rgba(0, 0, 0, 0.12) 0px 1px 5px 0px,
+            rgba(0, 0, 0, 0.12) 0px 1px 5px -4px,
             rgba(0, 0, 0, 0.2) 0px 3px 1px -2px;
           display: flex;
         }
