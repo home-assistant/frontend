b3b9ca9c3ff461c1b8229d7c8d536ec3021595fd
Paulus Schoutsen <paulus@home-assistant.io>
2018-12-13 23:36:01 +0100

    Fix setting aspect ratio in percentage (#2289)
    * Fix setting aspect ratio in percentage
    
    * Use endsWith
    
    * Fix invalid test

--- src/common/util/parse-aspect-ratio.ts
+++ src/common/util/parse-aspect-ratio.ts
@@ -1,24 +1,30 @@
-export default function parseAspectRatio(input) {
-  // Handle 16x9, 16:9, 1.78x1, 1.78:1, 1.78
-  // Ignore everything else
-  function parseOrThrow(num) {
-    const parsed = parseFloat(num);
-    if (isNaN(parsed)) {
-      throw new Error(`${num} is not a number`);
-    }
-    return parsed;
+// Handle 16x9, 16:9, 1.78x1, 1.78:1, 1.78
+// Ignore everything else
+const parseOrThrow = (num) => {
+  const parsed = parseFloat(num);
+  if (isNaN(parsed)) {
+    throw new Error(`${num} is not a number`);
+  }
+  return parsed;
+};
+
+export default function parseAspectRatio(input: string) {
+  if (!input) {
+    return null;
   }
   try {
-    if (input) {
-      const arr = input.replace(":", "x").split("x");
-      if (arr.length === 0) {
-        return null;
-      }
-
-      return arr.length === 1
-        ? { w: parseOrThrow(arr[0]), h: 1 }
-        : { w: parseOrThrow(arr[0]), h: parseOrThrow(arr[1]) };
+    if (input.endsWith("%")) {
+      return { w: 100, h: parseOrThrow(input.substr(0, input.length - 1)) };
     }
+
+    const arr = input.replace(":", "x").split("x");
+    if (arr.length === 0) {
+      return null;
+    }
+
+    return arr.length === 1
+      ? { w: parseOrThrow(arr[0]), h: 1 }
+      : { w: parseOrThrow(arr[0]), h: parseOrThrow(arr[1]) };
   } catch (err) {
     // Ignore the error
   }
