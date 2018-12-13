20a11c204d5fe0b2ce497342ad130a036668be4e
Ian Richardson <iantrich@gmail.com>
2018-12-12 21:32:56 -0600

    Address review comments

--- src/panels/lovelace/editor/config-elements/hui-light-card-editor.ts
+++ src/panels/lovelace/editor/config-elements/hui-light-card-editor.ts
@@ -67,7 +67,7 @@ export class HuiLightCardEditor extends hassLocalizeLitMixin(LitElement)
             .hass="${this.hass}"
             .value="${this._entity}"
             .configValue=${"entity"}
-            domain-filter=${"light"}
+            domain-filter="light"
             @change="${this._valueChanged}"
             allow-custom-entity
           ></ha-entity-picker>
