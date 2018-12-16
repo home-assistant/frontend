966407ef8a3d7374d8cc7c931ca8a2da609a21a3
Paulus Schoutsen <paulus@home-assistant.io>
2018-12-16 19:52:13 +0100

    Fix undefined on plant/weather card (#2339)

--- src/cards/ha-plant-card.js
+++ src/cards/ha-plant-card.js
@@ -119,7 +119,7 @@ class HaPlantCard extends EventsMixin(PolymerElement) {
   }
 
   computeTitle(stateObj) {
-    return this.config.name || computeStateName(stateObj);
+    return (this.config && this.config.name) || computeStateName(stateObj);
   }
 
   computeAttributes(data) {
