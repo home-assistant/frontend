f775ea83af21d04938eda9fd77b29f41c5a0671b
Ian Richardson <iantrich@gmail.com>
2018-12-15 23:41:18 -0600

    :sparkles: UI Editor for `media-control` card

--- src/panels/lovelace/cards/hui-media-control-card.js
+++ src/panels/lovelace/cards/hui-media-control-card.js
@@ -2,7 +2,20 @@ import "../../../cards/ha-media_player-card";
 
 import LegacyWrapperCard from "./hui-legacy-wrapper-card";
 
+export const Config = {
+  entity: "",
+};
+
 class HuiMediaControlCard extends LegacyWrapperCard {
+  static async getConfigElement() {
+    await import("../editor/config-elements/hui-media-control-card-editor");
+    return document.createElement("hui-media-control-card-editor");
+  }
+
+  static getStubConfig() {
+    return {};
+  }
+
   constructor() {
     super("ha-media_player-card", "media_player");
   }
