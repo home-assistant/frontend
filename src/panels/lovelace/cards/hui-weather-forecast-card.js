fb1a4e9dd79e7570cc8121fccb3f9adba651b7fe
Ian Richardson <iantrich@gmail.com>
2018-12-18 07:26:34 -0600

    UI Editor for `weather-forecast` card (#2285)
    * :sparkles: UI Editor for `weather-forecast` card
    
    * Missed name
    
    * Fix typo
    
    * Address review comments
    
    * Remove unused optional configs with no defaults
    
    * cleanup
    
    * Name chunk

--- src/panels/lovelace/cards/hui-weather-forecast-card.js
+++ src/panels/lovelace/cards/hui-weather-forecast-card.js
@@ -2,7 +2,22 @@ import "../../../cards/ha-weather-card";
 
 import LegacyWrapperCard from "./hui-legacy-wrapper-card";
 
+// should be interface when converted to TS
+export const Config = {
+  entity: "",
+  name: "",
+};
+
 class HuiWeatherForecastCard extends LegacyWrapperCard {
+  static async getConfigElement() {
+    await import(/* webpackChunkName: "hui-weather-forecast-card-editor" */ "../editor/config-elements/hui-weather-forecast-card-editor");
+    return document.createElement("hui-weather-forecast-card-editor");
+  }
+
+  static getStubConfig() {
+    return {};
+  }
+
   constructor() {
     super("ha-weather-card", "weather");
   }
