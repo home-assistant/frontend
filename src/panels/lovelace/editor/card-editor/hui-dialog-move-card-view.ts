998a39240ce8176356b04532064a5e8af70138d3
Anton Verburg <info@tec-v.nl>
2018-12-19 07:08:17 -0500

    Update UI and Add Move Card to a Dialogue (#2282)
    * Update UI
    
    * REmove useless import
    
    * Add Notification if saved is done
    
    * Move Register Dialog
    
    * Updates from suggestions
    
    * Updates box shadow on card options
    
    * Update color and viewselection
    
    * Add pointer
    
    * Update Cursor
    
    * Update check next to save
    
    * Comment Updates
    
    * Text area

---
+++ src/panels/lovelace/editor/card-editor/hui-dialog-move-card-view.ts
@@ -0,0 +1,106 @@
+import { html, LitElement, PropertyDeclarations } from "@polymer/lit-element";
+import { TemplateResult } from "lit-html";
+import "@polymer/paper-dialog/paper-dialog";
+import "@polymer/paper-item/paper-item";
+// tslint:disable-next-line:no-duplicate-imports
+import { PaperDialogElement } from "@polymer/paper-dialog/paper-dialog";
+
+import { hassLocalizeLitMixin } from "../../../../mixins/lit-localize-mixin";
+import { moveCard } from "../config-util";
+import { MoveCardViewDialogParams } from "./show-move-card-view-dialog";
+
+export class HuiDialogMoveCardView extends hassLocalizeLitMixin(LitElement) {
+  private _params?: MoveCardViewDialogParams;
+
+  static get properties(): PropertyDeclarations {
+    return {
+      _params: {},
+    };
+  }
+
+  public async showDialog(params: MoveCardViewDialogParams): Promise<void> {
+    this._params = params;
+    await this.updateComplete;
+  }
+
+  protected render(): TemplateResult {
+    if (!this._params) {
+      return html``;
+    }
+    return html`
+      <style>
+        paper-item {
+          margin: 8px;
+          cursor: pointer;
+        }
+        paper-item[active] {
+          color: var(--primary-color);
+        }
+        paper-item[active]:before {
+          border-radius: 4px;
+          position: absolute;
+          top: 0;
+          right: 0;
+          bottom: 0;
+          left: 0;
+          pointer-events: none;
+          content: "";
+          background-color: var(--primary-color);
+          opacity: 0.12;
+          transition: opacity 15ms linear;
+          will-change: opacity;
+        }
+      </style>
+      <paper-dialog
+        with-backdrop
+        opened
+        @opened-changed="${this._openedChanged}"
+      >
+        <h2>Choose view to move card</h2>
+        ${
+          this._params!.lovelace!.config.views.map((view, index) => {
+            return html`
+              <paper-item
+                ?active="${this._params!.path![0] === index}"
+                @click="${this._moveCard}"
+                .index="${index}"
+                >${view.title}</paper-item
+              >
+            `;
+          })
+        }
+      </paper-dialog>
+    `;
+  }
+
+  private get _dialog(): PaperDialogElement {
+    return this.shadowRoot!.querySelector("paper-dialog")!;
+  }
+
+  private _moveCard(e: Event): void {
+    const newView = (e.currentTarget! as any).index;
+    const path = this._params!.path!;
+    if (newView === path[0]) {
+      return;
+    }
+
+    const lovelace = this._params!.lovelace!;
+
+    lovelace.saveConfig(moveCard(lovelace.config, path, [newView!]));
+    this._dialog.close();
+  }
+
+  private _openedChanged(ev: MouseEvent) {
+    if (!(ev.detail as any).value) {
+      this._params = undefined;
+    }
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "hui-dialog-move-card-view": HuiDialogMoveCardView;
+  }
+}
+
+customElements.define("hui-dialog-move-card-view", HuiDialogMoveCardView);
--- src/panels/lovelace/hui-editor.ts
+++ src/panels/lovelace/hui-editor.ts
@@ -1,4 +1,5 @@
 import { LitElement, html } from "@polymer/lit-element";
+import { classMap } from "lit-html/directives/classMap";
 import { TemplateResult } from "lit-html";
 import yaml from "js-yaml";
 
@@ -7,9 +8,12 @@ import "@polymer/app-layout/app-header/app-header";
 import "@polymer/app-layout/app-toolbar/app-toolbar";
 import "@polymer/paper-button/paper-button";
 import "@polymer/paper-icon-button/paper-icon-button";
+import "@polymer/paper-spinner/paper-spinner";
 
 import { Lovelace } from "./types";
 import { hassLocalizeLitMixin } from "../../mixins/lit-localize-mixin";
+
+import "../../components/ha-icon";
 
 const TAB_INSERT = "  ";
 
@@ -17,10 +21,14 @@ class LovelaceFullConfigEditor extends hassLocalizeLitMixin(LitElement) {
   public lovelace?: Lovelace;
   public closeEditor?: () => void;
   private _haStyle?: DocumentFragment;
+  private _saving?: boolean;
+  private _changed?: boolean;
 
   static get properties() {
     return {
       lovelace: {},
+      _saving: {},
+      _changed: {},
     };
   }
 
@@ -36,6 +44,9 @@ class LovelaceFullConfigEditor extends hassLocalizeLitMixin(LitElement) {
             ></paper-icon-button>
             <div main-title>Edit Config</div>
             <paper-button @click="${this._handleSave}">Save</paper-button>
+            <ha-icon class="save-button ${classMap({
+              saved: this._saving! === false && !this._changed!,
+            })}" icon="hass:check">
           </app-toolbar>
         </app-header>
         <div class="content">
@@ -44,6 +55,7 @@ class LovelaceFullConfigEditor extends hassLocalizeLitMixin(LitElement) {
             autocorrect="off"
             autocapitalize="off"
             spellcheck="false"
+            @input="${this._yamlChanged}"
           ></textarea>
         </div>
       </app-header-layout>
@@ -90,11 +102,12 @@ class LovelaceFullConfigEditor extends hassLocalizeLitMixin(LitElement) {
         app-header-layout {
           height: 100vh;
         }
-        app-toolbar {
-          background-color: #455a64;
-        }
         paper-button {
           font-size: 16px;
+        }
+        app-toolbar {
+          background-color: var(--dark-background-color, #455a64);
+          color: var(--dark-text-color);
         }
 
         .content {
@@ -112,20 +125,46 @@ class LovelaceFullConfigEditor extends hassLocalizeLitMixin(LitElement) {
           font-family: "Courier New", Courier, monospace;
           padding: 8px;
         }
+
+        .save-button {
+          opacity: 0;
+          margin-left: -16px;
+          margin-top: -4px;
+          transition: opacity 1.5s;
+        }
+
+        .saved {
+          opacity: 1;
+        }
       </style>
     `;
   }
 
-  private _handleSave() {
+  private async _handleSave() {
+    this._saving = true;
     let value;
     try {
       value = yaml.safeLoad(this.textArea.value);
     } catch (err) {
       alert(`Unable to parse YAML: ${err}`);
+      this._saving = false;
       return;
     }
 
-    this.lovelace!.saveConfig(value);
+    try {
+      await this.lovelace!.saveConfig(value);
+    } catch (err) {
+      alert(`Unable to save YAML: ${err}`);
+    }
+    this._saving = false;
+    this._changed = false;
+  }
+
+  private _yamlChanged() {
+    if (this._changed) {
+      return;
+    }
+    this._changed = true;
   }
 
   private get textArea(): HTMLTextAreaElement {
