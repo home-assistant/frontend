18f2dda14023b8d3fc253a69e79c7d91b6831b0b
Anton Verburg <info@tec-v.nl>
2019-01-03 23:46:14 +0200

    Allow empty name (#2388)

--- src/common/entity/compute_state_name.ts
+++ src/common/entity/compute_state_name.ts
@@ -2,5 +2,6 @@ import { HassEntity } from "home-assistant-js-websocket";
 import computeObjectId from "./compute_object_id";
 
 export default (stateObj: HassEntity): string =>
-  stateObj.attributes.friendly_name ||
-  computeObjectId(stateObj.entity_id).replace(/_/g, " ");
+  stateObj.attributes.friendly_name === undefined
+    ? computeObjectId(stateObj.entity_id).replace(/_/g, " ")
+    : stateObj.attributes.friendly_name || "";
