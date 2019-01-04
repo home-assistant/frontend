fb2c2911b3f096c90dbb63e6cd74d561be3d8ca4
David Mulcahey <david.mulcahey@icloud.com>
2019-01-04 12:06:00 -0500

    convert to lit / ts

---
+++ src/panels/config/zha/ha-config-zha.ts
@@ -0,0 +1,79 @@
+import { html, LitElement, PropertyDeclarations } from "@polymer/lit-element";
+import { TemplateResult } from "lit-html";
+import "@polymer/app-layout/app-header/app-header";
+import "@polymer/app-layout/app-toolbar/app-toolbar";
+import "@polymer/paper-icon-button/paper-icon-button";
+import { HomeAssistant } from "../../../types";
+
+import "../../../layouts/ha-app-layout";
+import "../../../resources/ha-style";
+
+import "./zha-network";
+
+export class HaConfigZha extends LitElement {
+  public hass?: HomeAssistant;
+  public isWide?: boolean;
+  private _haStyle?: DocumentFragment;
+  private _ironFlex?: DocumentFragment;
+
+  static get properties(): PropertyDeclarations {
+    return {
+      hass: {},
+      isWide: {},
+    };
+  }
+
+  protected render(): TemplateResult {
+    return html`
+      ${this.renderStyle()}
+      <ha-app-layout has-scrolling-region="">
+        <app-header slot="header" fixed="">
+          <app-toolbar>
+            <paper-icon-button
+              icon="hass:arrow-left"
+              @click="${this._onBackTapped}"
+            ></paper-icon-button>
+          </app-toolbar>
+        </app-header>
+
+        <zha-network
+          id="zha-network"
+          .is-wide="${this.isWide}"
+          .hass="${this.hass}"
+        ></zha-network>
+      </ha-app-layout>
+    `;
+  }
+
+  private renderStyle(): TemplateResult {
+    if (!this._haStyle) {
+      this._haStyle = document.importNode(
+        (document.getElementById("ha-style")!
+          .children[0] as HTMLTemplateElement).content,
+        true
+      );
+    }
+    if (!this._ironFlex) {
+      this._ironFlex = document.importNode(
+        (document.getElementById("iron-flex")!
+          .children[0] as HTMLTemplateElement).content,
+        true
+      );
+    }
+    return html`
+      ${this._ironFlex} ${this._haStyle}
+    `;
+  }
+
+  private _onBackTapped() {
+    history.back();
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "ha-config-zha": HaConfigZha;
+  }
+}
+
+customElements.define("ha-config-zha", HaConfigZha);
