a081336f2f6eb30e4e63cbf85d8c2fa3ad686d0a
David Mulcahey <david.mulcahey@icloud.com>
2019-01-04 11:43:29 -0500

    cleanup

--- src/panels/config/zha/ha-config-zha.js
+++ src/panels/config/zha/ha-config-zha.js
@@ -8,47 +8,11 @@ import "../../../layouts/ha-app-layout";
 import "../../../resources/ha-style";
 
 import "./zha-network";
-import "./zha-node";
 
 class HaConfigZha extends PolymerElement {
   static get template() {
     return html`
-      <style include="iron-flex ha-style ha-form-style">
-        .content {
-          margin-top: 24px;
-        }
-
-        .node-info {
-          margin-left: 16px;
-        }
-
-        .help-text {
-          padding-left: 24px;
-          padding-right: 24px;
-        }
-
-        paper-card {
-          display: block;
-          margin: 0 auto;
-          max-width: 600px;
-        }
-
-        ha-service-description {
-          display: block;
-          color: grey;
-        }
-
-        [hidden] {
-          display: none;
-        }
-
-        .toggle-help-icon {
-          position: absolute;
-          top: 6px;
-          right: 0;
-          color: var(--primary-color);
-        }
-      </style>
+      <style include="iron-flex ha-style"></style>
       <ha-app-layout has-scrolling-region="">
         <app-header slot="header" fixed="">
           <app-toolbar>
@@ -56,7 +20,6 @@ class HaConfigZha extends PolymerElement {
               icon="hass:arrow-left"
               on-click="_backTapped"
             ></paper-icon-button>
-            <div main-title="">[[localize('ui.panel.config.zha.caption')]]</div>
           </app-toolbar>
         </app-header>
 
@@ -73,23 +36,7 @@ class HaConfigZha extends PolymerElement {
     return {
       hass: Object,
       isWide: Boolean,
-
-      config: {
-        type: Array,
-        value: function() {
-          return [];
-        },
-      },
-
-      showHelp: {
-        type: Boolean,
-        value: false,
-      },
     };
-  }
-
-  toggleHelp() {
-    this.showHelp = !this.showHelp;
   }
 
   _backTapped() {
