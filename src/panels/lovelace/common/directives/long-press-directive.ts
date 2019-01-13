7173b16ef3b6929d63e2f3437fec6493305b263b
Paulus Schoutsen <paulus@home-assistant.io>
2019-01-13 09:15:09 -0800

    Fix long press directive (#2464)

--- src/panels/lovelace/common/directives/long-press-directive.ts
+++ src/panels/lovelace/common/directives/long-press-directive.ts
@@ -183,7 +183,6 @@ export const longPressBind = (element: LongPressElement) => {
   longpress.bind(element);
 };
 
-export const longPress = () =>
-  directive((part: PropertyPart) => {
-    longPressBind(part.committer.element);
-  });
+export const longPress = directive(() => (part: PropertyPart) => {
+  longPressBind(part.committer.element);
+});
