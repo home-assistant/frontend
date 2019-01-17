ad7ad8b35eb07900b7c56c6be924e84bb24a1851
Bram Kragten <mail@bramkragten.nl>
2019-01-17 20:07:05 +0100

    Deep clone config (#2496)

--- src/panels/lovelace/common/create-card-element.ts
+++ src/panels/lovelace/common/create-card-element.ts
@@ -1,3 +1,5 @@
+import deepClone from "deep-clone-simple";
+
 import { fireEvent } from "../../../common/dom/fire_event";
 
 import "../cards/hui-alarm-panel-card";
@@ -68,7 +70,7 @@ const _createElement = (
 ): LovelaceCard | HuiErrorCard => {
   const element = document.createElement(tag) as LovelaceCard;
   try {
-    element.setConfig(config);
+    element.setConfig(deepClone(config));
   } catch (err) {
     // tslint:disable-next-line
     console.error(tag, err);
