9299d548babc2e719bc3a59f88d4aab0142ba420
Paulus Schoutsen <paulus@home-assistant.io>
2019-01-27 10:40:46 -0800

    Convert common dir to TS (#2580)
    * Convert common dir to TS
    
    * Lint
    
    * Update setup-leaflet-map.ts

--- src/common/dom/load_resource.ts
+++ src/common/dom/load_resource.ts
@@ -1,9 +1,13 @@
 // Load a resource and get a promise when loading done.
 // From: https://davidwalsh.name/javascript-loader
 
-function _load(tag, url, type) {
+const _load = (
+  tag: "link" | "script" | "img",
+  url: string,
+  type?: "module"
+) => {
   // This promise will be used by Promise.all to determine success or failure
-  return new Promise(function(resolve, reject) {
+  return new Promise((resolve, reject) => {
     const element = document.createElement(tag);
     let attr = "src";
     let parent = "body";
@@ -15,14 +19,14 @@ function _load(tag, url, type) {
     // Need to set different attributes depending on tag type
     switch (tag) {
       case "script":
-        element.async = true;
+        (element as HTMLScriptElement).async = true;
         if (type) {
-          element.type = type;
+          (element as HTMLScriptElement).type = type;
         }
         break;
       case "link":
-        element.type = "text/css";
-        element.rel = "stylesheet";
+        (element as HTMLLinkElement).type = "text/css";
+        (element as HTMLLinkElement).rel = "stylesheet";
         attr = "href";
         parent = "head";
     }
@@ -31,9 +35,9 @@ function _load(tag, url, type) {
     element[attr] = url;
     document[parent].appendChild(element);
   });
-}
+};
 
-export const loadCSS = (url) => _load("link", url);
-export const loadJS = (url) => _load("script", url);
-export const loadImg = (url) => _load("img", url);
-export const loadModule = (url) => _load("script", url, "module");
+export const loadCSS = (url: string) => _load("link", url);
+export const loadJS = (url: string) => _load("script", url);
+export const loadImg = (url: string) => _load("img", url);
+export const loadModule = (url: string) => _load("script", url, "module");
--- src/common/dom/scroll-to-target.ts
+++ src/common/dom/scroll-to-target.ts
@@ -11,23 +11,23 @@ export default function scrollToTarget(element, target) {
   // the scroll event will trigger _updateScrollState directly,
   // However, _updateScrollState relies on the previous `scrollTop` to update the states.
   // Calling _updateScrollState will ensure that the states are synced correctly.
-  var top = 0;
-  var scroller = target;
-  var easingFn = function easeOutQuad(t, b, c, d) {
+  const top = 0;
+  const scroller = target;
+  const easingFn = function easeOutQuad(t, b, c, d) {
     /* eslint-disable no-param-reassign, space-infix-ops, no-mixed-operators */
     t /= d;
     return -c * t * (t - 2) + b;
     /* eslint-enable no-param-reassign, space-infix-ops, no-mixed-operators */
   };
-  var animationId = Math.random();
-  var duration = 200;
-  var startTime = Date.now();
-  var currentScrollTop = scroller.scrollTop;
-  var deltaScrollTop = top - currentScrollTop;
+  const animationId = Math.random();
+  const duration = 200;
+  const startTime = Date.now();
+  const currentScrollTop = scroller.scrollTop;
+  const deltaScrollTop = top - currentScrollTop;
   element._currentAnimationId = animationId;
   (function updateFrame() {
-    var now = Date.now();
-    var elapsedTime = now - startTime;
+    const now = Date.now();
+    const elapsedTime = now - startTime;
     if (elapsedTime > duration) {
       scroller.scrollTop = top;
     } else if (element._currentAnimationId === animationId) {
