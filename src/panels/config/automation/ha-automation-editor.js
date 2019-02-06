2f8ed421f357bedeb81813d6ff644943026736de
Paulus Schoutsen <paulus@home-assistant.io>
2019-02-06 11:01:52 -0800

    Update ha-automation-editor.js

--- src/panels/config/automation/ha-automation-editor.js
+++ src/panels/config/automation/ha-automation-editor.js
@@ -249,6 +249,7 @@ class HaAutomationEditor extends LocalizeMixin(NavigateMixin(PolymerElement)) {
   async _updateComponent() {
     if (this._renderScheduled || !this.hass || !this.config) return;
     this._renderScheduled = true;
+
     await 0;
 
     if (!this._renderScheduled) return;
