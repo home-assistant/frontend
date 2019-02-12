31440cbad0949ac60ee7ee64a04eb7f578b64263
Ian Richardson <iantrich@gmail.com>
2019-02-12 00:33:57 -0600

    Convert notification-button to Lit/TS and add badge

---
+++ src/panels/lovelace/components/notifications/hui-notifications-button.ts
@@ -0,0 +1,57 @@
+import {
+  html,
+  LitElement,
+  TemplateResult,
+  css,
+  CSSResult,
+  property,
+} from "lit-element";
+import "@polymer/paper-badge/paper-badge";
+import "@polymer/paper-icon-button/paper-icon-button";
+import { fireEvent } from "../../../../common/dom/fire_event";
+
+class HuiNotificationsButton extends LitElement {
+  @property() public notifications?: string[];
+  @property() public open?: boolean;
+
+  protected render(): TemplateResult | void {
+    return html`
+      <paper-icon-button
+        icon="hass:bell"
+        @click="${this._clicked}"
+      ></paper-icon-button>
+      ${this.notifications
+        ? html`
+            <paper-badge label="${this.notifications.length}"></paper-badge>
+          `
+        : ""}
+    `;
+  }
+
+  static get styles(): CSSResult[] {
+    return [
+      css`
+        :host {
+          position: relative;
+        }
+        paper-badge {
+          left: 23px !important;
+          top: 0px !important;
+        }
+      `,
+    ];
+  }
+
+  private _clicked() {
+    this.open = true;
+    fireEvent(this, "open-changed");
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "hui-notifications-button": HuiNotificationsButton;
+  }
+}
+
+customElements.define("hui-notifications-button", HuiNotificationsButton);
