9940352b66b4297ec666c696d87703feb0791a7f
Paulus Schoutsen <paulus@home-assistant.io>
2019-02-25 11:53:46 -0800

    Convert ha-menu-button to TS (#2825)
    * Convert ha-menu-button to TS
    
    * Address comments
    
    * Fix icon searcher

---
+++ src/components/ha-menu-button.ts
@@ -0,0 +1,44 @@
+import "@polymer/paper-icon-button/paper-icon-button";
+import {
+  property,
+  TemplateResult,
+  LitElement,
+  html,
+  customElement,
+} from "lit-element";
+
+import { fireEvent } from "../common/dom/fire_event";
+
+@customElement("ha-menu-button")
+class HaMenuButton extends LitElement {
+  @property({ type: Boolean })
+  public showMenu = false;
+
+  @property({ type: Boolean })
+  public hassio = false;
+
+  protected render(): TemplateResult | void {
+    return html`
+      <paper-icon-button
+        .icon=${this.hassio ? "hassio:menu" : "hass:menu"}
+        @click=${this._toggleMenu}
+      ></paper-icon-button>
+    `;
+  }
+
+  // We are not going to use ShadowDOM as we're rendering a single element
+  // without any CSS used.
+  protected createRenderRoot(): Element | ShadowRoot {
+    return this;
+  }
+
+  private _toggleMenu(): void {
+    fireEvent(this, this.showMenu ? "hass-close-menu" : "hass-open-menu");
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "ha-menu-button": HaMenuButton;
+  }
+}
