b6008cf9d6c5e9c519d8dc307e4327268313031e
Paulus Schoutsen <balloob@gmail.com>
2019-03-05 12:08:58 -0800

    Swap out babel-minify for terser

--- config/webpack.js
+++ config/webpack.js
@@ -1,6 +1,6 @@
 const webpack = require("webpack");
 const path = require("path");
-const BabelMinifyPlugin = require("babel-minify-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports.resolve = {
   extensions: [".ts", ".js", ".json", ".tsx"],
@@ -29,32 +29,15 @@ module.exports.plugins = [
   ),
 ];
 
-module.exports.optimization = {
+module.exports.optimization = (latestBuild) => ({
   minimizer: [
-    // Took options from Polymer build tool
-    // https://github.com/Polymer/tools/blob/master/packages/build/src/js-transform.ts
-    new BabelMinifyPlugin(
-      {
-        // Disable the minify-constant-folding plugin because it has a bug relating
-        // to invalid substitution of constant values into export specifiers:
-        // https://github.com/babel/minify/issues/820
-        evaluate: false,
-
-        // TODO(aomarks) Find out why we disabled this plugin.
-        simplifyComparisons: false,
-
-        // Prevent removal of things that babel thinks are unreachable, but sometimes
-        // gets wrong: https://github.com/Polymer/tools/issues/724
-        deadcode: false,
-
-        // Disable the simplify plugin because it can eat some statements preceeding
-        // loops. https://github.com/babel/minify/issues/824
-        simplify: false,
-
-        // This is breaking ES6 output. https://github.com/Polymer/tools/issues/261
-        mangle: false,
+    new TerserPlugin({
+      cache: true,
+      parallel: true,
+      extractComments: true,
+      terserOptions: {
+        ecma: latestBuild ? undefined : 5,
       },
-      {}
-    ),
+    }),
   ],
-};
+});
