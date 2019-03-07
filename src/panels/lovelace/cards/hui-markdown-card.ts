288f439aa8fbaf080ab0e6eb5d3e08fb09b554ac
Ian Richardson <iantrich@gmail.com>
2019-03-07 12:52:39 -0600

    Cleanup cards (#2870)
    * Cleanup cards
    
    * Update hui-iframe-card.ts
    
    * address review comments
    
    * Apply paper-styles
    
    https://github.com/PolymerElements/paper-styles/blob/v3.0.1/classes/typography.js
    
    I didn't find `paper-font-common-nowrap` in anything after v1.0.0 but did end up applying what I found here: https://chromium.googlesource.com/chromium/src/+/923ede74ebc57633583795a84b33e994f7dcd4fa/third_party/polymer/v1_0/components/paper-styles/typography.html
    
    * Added comments on paper-style usage

--- src/panels/lovelace/cards/hui-markdown-card.ts
+++ src/panels/lovelace/cards/hui-markdown-card.ts
@@ -1,8 +1,11 @@
 import {
   html,
   LitElement,
-  PropertyDeclarations,
   TemplateResult,
+  customElement,
+  property,
+  css,
+  CSSResult,
 } from "lit-element";
 import { classMap } from "lit-html/directives/class-map";
 
@@ -17,22 +20,18 @@ export interface Config extends LovelaceCardConfig {
   title?: string;
 }
 
+@customElement("hui-markdown-card")
 export class HuiMarkdownCard extends LitElement implements LovelaceCard {
   public static async getConfigElement(): Promise<LovelaceCardEditor> {
     await import(/* webpackChunkName: "hui-markdown-card-editor" */ "../editor/config-elements/hui-markdown-card-editor");
     return document.createElement("hui-markdown-card-editor");
   }
+
   public static getStubConfig(): object {
     return { content: " " };
   }
 
-  private _config?: Config;
-
-  static get properties(): PropertyDeclarations {
-    return {
-      _config: {},
-    };
-  }
+  @property() private _config?: Config;
 
   public getCardSize(): number {
     return this._config!.content.split("\n").length;
@@ -52,7 +51,6 @@ export class HuiMarkdownCard extends LitElement implements LovelaceCard {
     }
 
     return html`
-      ${this.renderStyle()}
       <ha-card .header="${this._config.title}">
         <ha-markdown
           class="markdown ${classMap({
@@ -64,35 +62,40 @@ export class HuiMarkdownCard extends LitElement implements LovelaceCard {
     `;
   }
 
-  private renderStyle(): TemplateResult {
-    return html`
-      <style>
-        :host {
-          @apply --paper-font-body1;
-        }
-        ha-markdown {
-          display: block;
-          padding: 0 16px 16px;
-          -ms-user-select: initial;
-          -webkit-user-select: initial;
-          -moz-user-select: initial;
-        }
-        .markdown.no-header {
-          padding-top: 16px;
-        }
-        ha-markdown > *:first-child {
-          margin-top: 0;
-        }
-        ha-markdown > *:last-child {
-          margin-bottom: 0;
-        }
-        ha-markdown a {
-          color: var(--primary-color);
-        }
-        ha-markdown img {
-          max-width: 100%;
-        }
-      </style>
+  static get styles(): CSSResult {
+    return css`
+      :host {
+        /* start paper-font-headline style */
+        font-family: "Roboto", "Noto", sans-serif;
+        -webkit-font-smoothing: antialiased; /* OS X subpixel AA bleed bug */
+        text-rendering: optimizeLegibility;
+        font-size: 24px;
+        font-weight: 400;
+        letter-spacing: -0.012em;
+        /* end paper-font-headline style */
+      }
+      ha-markdown {
+        display: block;
+        padding: 0 16px 16px;
+        -ms-user-select: initial;
+        -webkit-user-select: initial;
+        -moz-user-select: initial;
+      }
+      .markdown.no-header {
+        padding-top: 16px;
+      }
+      ha-markdown > *:first-child {
+        margin-top: 0;
+      }
+      ha-markdown > *:last-child {
+        margin-bottom: 0;
+      }
+      ha-markdown a {
+        color: var(--primary-color);
+      }
+      ha-markdown img {
+        max-width: 100%;
+      }
     `;
   }
 }
@@ -102,5 +105,3 @@ declare global {
     "hui-markdown-card": HuiMarkdownCard;
   }
 }
-
-customElements.define("hui-markdown-card", HuiMarkdownCard);
