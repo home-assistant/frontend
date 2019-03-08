148cd48c83f296281183fe99bb499688d90abd4d
Jason Hu <awarecan@users.noreply.github.com>
2019-03-08 02:49:58 -0800

    Save user language setting to backend (#2784)
    * Save user language setting to backend
    
    * Remove hass.selectedLanguage
    
    * Lint
    
    * Address code review comment
    
    * Refactoring translation
    
    * Code review
    
    * Add back selectedLanguage and local app storage
    
    * Move getTranslations to data/frontend.ts
    
    * Fix mock hass
    
    * Rewrite translations-mixin
    
    * revert no need changes
    
    * Final tweak

--- src/types.ts
+++ src/types.ts
@@ -117,8 +117,14 @@ export interface HomeAssistant {
   panelUrl: string;
 
   // i18n
+  // current effective language, in that order:
+  //   - backend saved user selected lanugage
+  //   - language in local appstorage
+  //   - browser language
+  //   - english (en)
   language: string;
-  selectedLanguage?: string;
+  // local stored language, keep that name for backward compability
+  selectedLanguage: string;
   resources: Resources;
   localize: LocalizeFunc;
   translationMetadata: {
