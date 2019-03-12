4065f36bac825e5549da7ae24ae03a8a08953d70
Paulus Schoutsen <balloob@gmail.com>
2019-03-11 22:23:59 -0700

    Fix area translations and error message

--- src/panels/config/area_registry/dialog-area-registry-detail.ts
+++ src/panels/config/area_registry/dialog-area-registry-detail.ts
@@ -123,7 +123,7 @@ class DialogAreaDetail extends LitElement {
       }
       this._params = undefined;
     } catch (err) {
-      this._error = err;
+      this._error = err.message || "Unknown error";
     } finally {
       this._submitting = false;
     }
