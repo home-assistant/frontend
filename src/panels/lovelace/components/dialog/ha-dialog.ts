0793a3b45d0cf71d8f573d9906f49d4acc738a16
Yosi Levy <yosilevy@gmail.com>
2019-03-24 15:16:26 +0200

    Improved docs

--- src/panels/lovelace/components/dialog/ha-dialog.ts
+++ src/panels/lovelace/components/dialog/ha-dialog.ts
@@ -3,15 +3,15 @@ import { mixinBehaviors } from "@polymer/polymer/lib/legacy/class";
 import { HaIronFocusablesHelper } from "./ha-iron-focusables-helper.js";
 
 const paperDialogClass = customElements.get("paper-dialog");
-// as Constructor<PaperIconButtonElement>;
 
+// behavior that will override existing iron-overlay-behavior and call the fixed implementation
 const haTabFixBehaviorImpl = {
   get _focusableNodes() {
     return HaIronFocusablesHelper.getTabbableNodes(this);
   },
 };
 
-// define a class that uses the haTabFixBehaviorImpl behvaior to override the method which fails
+// paper-dialog that uses the haTabFixBehaviorImpl behvaior
 export class HaPaperDialog extends mixinBehaviors(
   [haTabFixBehaviorImpl],
   paperDialogClass
