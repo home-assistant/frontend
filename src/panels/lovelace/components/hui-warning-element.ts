58119bf4c695664091fd8692c718d5cb976f55bc
Anton Verburg <info@tec-v.nl>
2019-03-24 01:10:55 -0500

    warning-element (#3006)
    * warning-element
    
    * add warning-element to picture-glance
    
    * add glance-card

---
+++ src/panels/lovelace/components/hui-warning-element.ts
@@ -0,0 +1,36 @@
+import {
+  html,
+  LitElement,
+  TemplateResult,
+  CSSResult,
+  css,
+  customElement,
+  property,
+} from "lit-element";
+
+import "../../../components/ha-icon";
+
+@customElement("hui-warning-element")
+export class HuiWarningElement extends LitElement {
+  @property() public label?: string;
+
+  protected render(): TemplateResult | void {
+    return html`
+      <ha-icon icon="hass:alert" .title="${this.label}"></ha-icon>
+    `;
+  }
+
+  static get styles(): CSSResult {
+    return css`
+      ha-icon {
+        color: #fce588;
+      }
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "hui-warning-element": HuiWarningElement;
+  }
+}
