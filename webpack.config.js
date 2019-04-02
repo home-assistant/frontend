849f93719101d36e12657713a2a192199867e01a
Paulus Schoutsen <balloob@gmail.com>
2019-04-02 11:58:05 -0700

    Fix var name

--- webpack.config.js
+++ webpack.config.js
@@ -160,7 +160,7 @@ function createConfig(isProdBuild, latestBuild) {
           /hass-icons.js$/,
           /\.chunk\.js$/,
         ],
-        templatedURLs: {
+        templatedUrls: {
           [`/static/translations/${
             translationMetadata["translations"]["en"]["fingerprints"]["en"]
           }`]: "build-translations/output/en.json",
