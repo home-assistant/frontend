f1a1af5146387b62beae003b2f05347aabc4ba67
Robbie Trencheny <me@robbiet.us>
2019-04-20 00:45:59 -0700

    Only allow whitelisted haptics

--- src/util/haptics.ts
+++ src/util/haptics.ts
@@ -4,5 +4,22 @@
 
 import { fireEvent } from "../common/dom/fire_event";
 
-export const forwardHaptic = (el: HTMLElement, hapticType: string) =>
+// Allowed types are from iOS HIG.
+// https://developer.apple.com/design/human-interface-guidelines/ios/user-interaction/feedback/#haptics
+// Implementors on platforms other than iOS should attempt to match the patterns (shown in HIG) as closely as possible.
+const ALLOWED_HAPTIC_TYPES = [
+  "success",
+  "warning",
+  "failure",
+  "light",
+  "medium",
+  "heavy",
+  "selection",
+];
+
+export const forwardHaptic = (el: HTMLElement, hapticType: string) => {
+  if (ALLOWED_HAPTIC_TYPES.indexOf(hapticType) === -1) {
+    throw new Error(`Unknown haptic type: ${hapticType}`);
+  }
   fireEvent(el, "haptic", { hapticType });
+};
