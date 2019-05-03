1ed8aedcdcd08e2dbc134a42ce394e4c1ffc39d6
Anton Verburg <info@tec-v.nl>
2019-05-02 22:41:58 -0700

    Migrate demo to gulp (#3152)
    * Migrate demo to gulp
    
    * Tweak demo migration to gulp
    
    * Feature detect demo
    
    * Gen icons

--- build-scripts/gulp/clean.js
+++ build-scripts/gulp/clean.js
@@ -3,3 +3,4 @@ const gulp = require("gulp");
 const config = require("../paths");
 
 gulp.task("clean", () => del([config.root, config.build_dir]));
+gulp.task("clean-demo", () => del([config.demo_root, config.build_dir]));
