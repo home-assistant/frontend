adbe2e703e8e614f05087c04967c1f91f952f9b8
Paulus Schoutsen <balloob@gmail.com>
2019-05-12 21:28:25 -0700

    Pimp script editor (#3192)
    * Pimp script editor
    
    * Use property for define
    
    * Show toasts

--- src/data/script.ts
+++ src/data/script.ts
@@ -1,5 +1,14 @@
+import { HomeAssistant } from "../types";
+import computeObjectId from "../common/entity/compute_object_id";
+
 export interface EventAction {
   event: string;
   event_data?: { [key: string]: any };
   event_data_template?: { [key: string]: any };
 }
+
+export const triggerScript = (
+  hass: HomeAssistant,
+  entityId: string,
+  variables?: {}
+) => hass.callService("script", computeObjectId(entityId), variables);
