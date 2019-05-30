4d8b7b465a51031c10ea1bc36d9edd077013552f
Paulus Schoutsen <balloob@gmail.com>
2019-05-30 08:41:32 -0700

    Dynamic update panels (#3227)

--- src/data/ws-panels.ts
+++ src/data/ws-panels.ts
@@ -1,14 +1,25 @@
 import { createCollection, Connection } from "home-assistant-js-websocket";
 import { Panels } from "../types";
 
+const fetchPanels = (conn) =>
+  conn.sendMessagePromise({
+    type: "get_panels",
+  });
+
+const subscribeUpdates = (conn, store) =>
+  conn.subscribeEvents(
+    () => fetchPanels(conn).then((panels) => store.setState(panels, true)),
+    "panels_updated"
+  );
+
 export const subscribePanels = (
   conn: Connection,
   onChange: (panels: Panels) => void
 ) =>
   createCollection<Panels>(
     "_pnl",
-    () => conn.sendMessagePromise({ type: "get_panels" }),
-    undefined,
+    fetchPanels,
+    subscribeUpdates,
     conn,
     onChange
   );
