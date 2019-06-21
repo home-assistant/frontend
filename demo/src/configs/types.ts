becb9517f240e64449efcb4f3bff4c76f65a4b36
Paulus Schoutsen <balloob@gmail.com>
2019-06-20 23:40:56 -0700

    Update demo (#3292)
    * Update demo
    
    * Kill homebridge hidden
    
    * Add missing air translation
    
    * Hide demo card with localStorage.hide_demo_card
    
    * adjust size of demo card if hidden
    
    * disable lint rule

--- demo/src/configs/types.ts
+++ demo/src/configs/types.ts
@@ -1,12 +1,13 @@
 import { LovelaceConfig } from "../../../src/data/lovelace";
 import { Entity } from "../../../src/fake_data/entity";
+import { LocalizeFunc } from "../../../src/common/translations/localize";
 
 export interface DemoConfig {
   index?: number;
   name: string;
   authorName: string;
   authorUrl: string;
-  lovelace: () => LovelaceConfig;
-  entities: () => Entity[];
+  lovelace: (localize: LocalizeFunc) => LovelaceConfig;
+  entities: (localize: LocalizeFunc) => Entity[];
   theme: () => { [key: string]: string } | null;
 }
