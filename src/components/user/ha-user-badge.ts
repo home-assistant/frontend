bac0e2bace05594a81686e74c90e393ce6142bf8
Paulus Schoutsen <balloob@gmail.com>
2019-06-27 15:23:05 -0700

    experimental sidebar (#3306)
    * experimental sidebar
    
    * Change default docked sidebar to true
    
    * remove delay
    
    * Push things down
    
    * Speed up animation
    
    * Always open on big screens
    
    * Move things around
    
    * Final tweaks
    
    * Lint
    
    * Don't open on hover logo

--- src/components/user/ha-user-badge.ts
+++ src/components/user/ha-user-badge.ts
@@ -7,7 +7,6 @@ import {
   property,
   customElement,
 } from "lit-element";
-import { classMap } from "lit-html/directives/class-map";
 import { User } from "../../data/user";
 import { CurrentUser } from "../../types";
 
@@ -33,24 +32,23 @@ class StateBadge extends LitElement {
 
   protected render(): TemplateResult | void {
     const user = this.user;
-
     const initials = user ? computeInitials(user.name) : "?";
-
     return html`
-      <div
-        class="${classMap({
-          "profile-badge": true,
-          long: initials.length > 2,
-        })}"
-      >
-        ${initials}
-      </div>
+      ${initials}
     `;
+  }
+
+  protected updated(changedProps) {
+    super.updated(changedProps);
+    this.toggleAttribute(
+      "long",
+      (this.user ? computeInitials(this.user.name) : "?").length > 2
+    );
   }
 
   static get styles(): CSSResult {
     return css`
-      .profile-badge {
+      :host {
         display: inline-block;
         box-sizing: border-box;
         width: 40px;
@@ -63,7 +61,7 @@ class StateBadge extends LitElement {
         overflow: hidden;
       }
 
-      .profile-badge.long {
+      :host([long]) {
         font-size: 80%;
       }
     `;
