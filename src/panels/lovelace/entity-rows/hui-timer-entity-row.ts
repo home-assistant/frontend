06b9c9cab4b1c2dce1e5561fb818336a72a1d879
Anton Verburg <info@tec-v.nl>
2019-07-06 00:22:29 +0200

    Fix timer stops updating when changing views in LL (#3248)
    * Fix timer stops updating when changing views in LL
    
    * Test if _config exists in connectedCallback()
    
    * Update hui-timer-entity-row.ts

--- src/panels/lovelace/entity-rows/hui-timer-entity-row.ts
+++ src/panels/lovelace/entity-rows/hui-timer-entity-row.ts
@@ -40,6 +40,16 @@ class HuiTimerEntityRow extends LitElement {
     this._clearInterval();
   }
 
+  public connectedCallback(): void {
+    super.connectedCallback();
+    if (this._config && this._config.entity) {
+      const stateObj = this.hass!.states[this._config!.entity];
+      if (stateObj) {
+        this._startInterval(stateObj);
+      }
+    }
+  }
+
   protected render(): TemplateResult | void {
     if (!this._config || !this.hass) {
       return html``;
