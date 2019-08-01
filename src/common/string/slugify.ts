2fda2ee7420c05bf7e7a964520b007cb35c222e1
Paulus Schoutsen <balloob@gmail.com>
2019-08-01 13:33:28 -0700

    Suggest a view path when user enters a title when creating a view (#3448)
    * Suggest a view path when user enters a title when creating a view
    
    * Lint

---
+++ src/common/string/slugify.ts
@@ -0,0 +1,19 @@
+// https://gist.github.com/hagemann/382adfc57adbd5af078dc93feef01fe1
+export const slugify = (value: string) => {
+  const a =
+    "àáäâãåăæąçćčđďèéěėëêęğǵḧìíïîįłḿǹńňñòóöôœøṕŕřßşśšșťțùúüûǘůűūųẃẍÿýźžż·/_,:;";
+  const b =
+    "aaaaaaaaacccddeeeeeeegghiiiiilmnnnnooooooprrsssssttuuuuuuuuuwxyyzzz------";
+  const p = new RegExp(a.split("").join("|"), "g");
+
+  return value
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, "-") // Replace spaces with -
+    .replace(p, (c) => b.charAt(a.indexOf(c))) // Replace special characters
+    .replace(/&/g, "-and-") // Replace & with 'and'
+    .replace(/[^\w\-]+/g, "") // Remove all non-word characters
+    .replace(/\-\-+/g, "-") // Replace multiple - with single -
+    .replace(/^-+/, "") // Trim - from start of text
+    .replace(/-+$/, ""); // Trim - from end of text
+};
