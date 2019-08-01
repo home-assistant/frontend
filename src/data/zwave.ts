703a99a51bac5d84e1284722c70cddb2a21cd593
Charles Garwood <cgarwood@gmail.com>
2019-08-01 11:32:20 -0400

    Convert zwave-node-config to lit/ts (#3434)
    * Convert zwave-node-config to lit/ts
    
    * Add translations
    
    * lint round 1
    
    * lint round 2
    
    * . all the properties and fix missing types
    
    * Clean up bad prettier auto-style
    
    * set header property instead of attribute

--- src/data/zwave.ts
+++ src/data/zwave.ts
@@ -11,6 +11,34 @@ export interface ZWaveValue {
   poll_intensity: number;
 }
 
+export interface ZWaveConfigItem {
+  key: number;
+  value: {
+    data: any;
+    data_items: any[];
+    help: string;
+    label: string;
+    max: number;
+    min: number;
+    type: string;
+  };
+}
+
+export interface ZWaveConfigServiceData {
+  node_id: number;
+  parameter: number;
+  value: number | string;
+}
+
+export interface ZWaveNode {
+  attributes: ZWaveAttributes;
+}
+
+export interface ZWaveAttributes {
+  node_id: number;
+  wake_up_interval?: number;
+}
+
 export const ZWAVE_NETWORK_STATE_STOPPED = 0;
 export const ZWAVE_NETWORK_STATE_FAILED = 1;
 export const ZWAVE_NETWORK_STATE_STARTED = 5;
@@ -26,3 +54,6 @@ export const fetchNetworkStatus = (
 
 export const fetchValues = (hass: HomeAssistant, nodeId: number) =>
   hass.callApi<ZWaveValue[]>("GET", `zwave/values/${nodeId}`);
+
+export const fetchNodeConfig = (hass: HomeAssistant, nodeId: number) =>
+  hass.callApi<ZWaveConfigItem[]>("GET", `zwave/config/${nodeId}`);
