d841cc92ef462c473ce1ae74668c644fbfe30297
Paulus Schoutsen <balloob@gmail.com>
2019-08-24 12:55:40 -0700

    Slightly simplify markdown API

--- src/resources/markdown_worker.ts
+++ src/resources/markdown_worker.ts
@@ -2,13 +2,9 @@ import marked from "marked";
 // @ts-ignore
 import filterXSS from "xss";
 
-export const renderMarkdown = async (
-  content: string,
-  markedOptions: object
-) => {
-  return filterXSS(marked(content, markedOptions), {
+export const renderMarkdown = (content: string, markedOptions: object) =>
+  filterXSS(marked(content, markedOptions), {
     onIgnoreTag(tag, html) {
       return ["svg", "path", "ha-icon"].indexOf(tag) !== -1 ? html : null;
     },
   });
-};
