c3b2f1d42118c605e93d81f1e9e801d548f3b663
David Mulcahey <david.mulcahey@me.com>
2019-08-25 13:04:55 -0400

    add mixin for zha device info dialog

---
+++ src/state/zha-dialog-mixin.ts
@@ -0,0 +1,28 @@
+import { Constructor, LitElement } from "lit-element";
+
+import { HassBaseEl } from "./hass-base-mixin";
+
+import { showZHADeviceInfoDialog } from "../dialogs/zha-device-info-dialog/show-dialog-zha-device-info";
+
+declare global {
+  // for fire event
+  interface HASSDomEvents {
+    "zha-show-device-dialog": {
+      ieee: string;
+    };
+  }
+}
+
+export default (superClass: Constructor<LitElement & HassBaseEl>) =>
+  class extends superClass {
+    protected firstUpdated(changedProps) {
+      super.firstUpdated(changedProps);
+      this.addEventListener("zha-show-device-dialog", (e) =>
+        this._handleShowZHADeviceInfoDialog(e)
+      );
+    }
+
+    private async _handleShowZHADeviceInfoDialog(ev) {
+      showZHADeviceInfoDialog(ev.target, ev.detail);
+    }
+  };
