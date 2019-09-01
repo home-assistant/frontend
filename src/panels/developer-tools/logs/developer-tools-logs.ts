bc51d8c0312b32f2aad832cae9bf4e2b840e818b
Bram Kragten <mail@bramkragten.nl>
2019-09-01 14:42:18 +0200

    Split Logs from info tab in dev tools

---
+++ src/panels/developer-tools/logs/developer-tools-logs.ts
@@ -0,0 +1,54 @@
+import {
+  LitElement,
+  html,
+  CSSResult,
+  css,
+  TemplateResult,
+  property,
+} from "lit-element";
+
+import { HomeAssistant } from "../../../types";
+import { haStyle } from "../../../resources/styles";
+
+import "../logs/system-log-card";
+import "../logs/error-log-card";
+
+class HaPanelDevLogs extends LitElement {
+  @property() public hass!: HomeAssistant;
+
+  protected render(): TemplateResult | void {
+    const hass = this.hass;
+
+    return html`
+      <div class="content">
+        <system-log-card .hass=${this.hass}></system-log-card>
+        <error-log-card .hass=${this.hass}></error-log-card>
+      </div>
+    `;
+  }
+
+  static get styles(): CSSResult[] {
+    return [
+      haStyle,
+      css`
+        :host {
+          -ms-user-select: initial;
+          -webkit-user-select: initial;
+          -moz-user-select: initial;
+        }
+
+        .content {
+          direction: ltr;
+        }
+      `,
+    ];
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "developer-tools-logs": HaPanelDevLogs;
+  }
+}
+
+customElements.define("developer-tools-logs", HaPanelDevLogs);
