8537c8a21711da302d5af4e5e9bdad659d353715
Bram Kragten <mail@bramkragten.nl>
2019-09-04 00:57:54 +0200

    Add MQTT subscribe to dev tools (#3589)
    * Add mqtt subscribe to dev tools
    
    * Update mqtt-subscribe-card.ts
    
    * Comments
    
    * type

---
+++ src/panels/developer-tools/mqtt/developer-tools-mqtt.ts
@@ -0,0 +1,126 @@
+import {
+  LitElement,
+  customElement,
+  TemplateResult,
+  html,
+  property,
+  CSSResultArray,
+  css,
+} from "lit-element";
+import "@material/mwc-button";
+import "@polymer/paper-input/paper-input";
+import "@polymer/paper-input/paper-textarea";
+
+import { HomeAssistant } from "../../../types";
+
+import { haStyle } from "../../../resources/styles";
+import "../../../components/ha-card";
+import "./mqtt-subscribe-card";
+
+@customElement("developer-tools-mqtt")
+class HaPanelDevMqtt extends LitElement {
+  @property() public hass?: HomeAssistant;
+
+  @property() private topic = "";
+
+  @property() private payload = "";
+
+  private inited: boolean = false;
+
+  protected firstUpdated() {
+    if (localStorage && localStorage["panel-dev-mqtt-topic"]) {
+      this.topic = localStorage["panel-dev-mqtt-topic"];
+    }
+    if (localStorage && localStorage["panel-dev-mqtt-payload"]) {
+      this.payload = localStorage["panel-dev-mqtt-payload"];
+    }
+    this.inited = true;
+  }
+
+  protected render(): TemplateResult {
+    return html`
+      <div class="content">
+        <ha-card header="Publish a packet">
+          <div class="card-content">
+            <paper-input
+              label="topic"
+              .value=${this.topic}
+              @value-changed=${this._handleTopic}
+            ></paper-input>
+
+            <paper-textarea
+              always-float-label
+              label="Payload (template allowed)"
+              .value="${this.payload}"
+              @value-changed=${this._handlePayload}
+            ></paper-textarea>
+          </div>
+          <div class="card-actions">
+            <mwc-button @click=${this._publish}>Publish</mwc-button>
+          </div>
+        </ha-card>
+
+        <mqtt-subscribe-card .hass=${this.hass}></mqtt-subscribe-card>
+      </div>
+    `;
+  }
+
+  private _handleTopic(ev: CustomEvent) {
+    this.topic = ev.detail.value;
+    if (localStorage && this.inited) {
+      localStorage["panel-dev-mqtt-topic"] = this.topic;
+    }
+  }
+
+  private _handlePayload(ev: CustomEvent) {
+    this.payload = ev.detail.value;
+    if (localStorage && this.inited) {
+      localStorage["panel-dev-mqtt-payload"] = this.payload;
+    }
+  }
+
+  private _publish(): void {
+    if (!this.hass) {
+      return;
+    }
+    this.hass.callService("mqtt", "publish", {
+      topic: this.topic,
+      payload_template: this.payload,
+    });
+  }
+
+  static get styles(): CSSResultArray {
+    return [
+      haStyle,
+      css`
+        :host {
+          -ms-user-select: initial;
+          -webkit-user-select: initial;
+          -moz-user-select: initial;
+        }
+
+        .content {
+          padding: 24px 0 32px;
+          max-width: 600px;
+          margin: 0 auto;
+          direction: ltr;
+        }
+
+        mwc-button {
+          background-color: white;
+        }
+
+        mqtt-subscribe-card {
+          display: block;
+          margin: 16px auto;
+        }
+      `,
+    ];
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "developer-tools-mqtt": HaPanelDevMqtt;
+  }
+}
