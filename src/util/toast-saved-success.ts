e09e70229511b859a94cb43763220d8c075d154f
Bram Kragten <mail@bramkragten.nl>
2019-09-03 19:31:47 +0200

    Rename

--- src/util/toast-saved-success.ts
+++ src/util/toast-saved-success.ts
@@ -1,7 +1,7 @@
 import { showToast } from "./toast";
 import { HomeAssistant } from "../types";
 
-export const showSuccesToast = (el: HTMLElement, hass: HomeAssistant) =>
+export const showSaveSuccessToast = (el: HTMLElement, hass: HomeAssistant) =>
   showToast(el, {
     message: hass!.localize("ui.common.successfully_saved"),
   });
