880d52b802887680700c1de17fce590fe401ecb9
Ian Richardson <iantrich@gmail.com>
2019-09-04 20:45:09 -0500

    Address review comments

--- src/dialogs/more-info/controls/more-info-timer.ts
+++ src/dialogs/more-info/controls/more-info-timer.ts
@@ -6,32 +6,63 @@ import {
   css,
   property,
   PropertyValues,
+  customElement,
 } from "lit-element";
 import "@material/mwc-button";
 
 import { HomeAssistant } from "../../../types";
+import { TimerEntity } from "../../../data/timer";
 
+@customElement("more-info-timer")
 class MoreInfoTimer extends LitElement {
   @property() public hass!: HomeAssistant;
-  @property() public stateObj?: any; // TODO
+
+  @property() public stateObj?: TimerEntity;
 
   protected render(): TemplateResult | void {
-    if (!this.stateObj) {
+    if (!this.hass || !this.stateObj) {
       return html``;
     }
-
-    const actions = ["start", "pause", "cancel", "finish"];
 
     return html`
       <ha-attributes .stateObj="${this.stateObj}"></ha-attributes>
       <div class="actions">
-        ${actions.map((state) => {
-          return html`
-            <mwc-button .action="${state}" @click="${this._handleActionClick}">
-              ${state}
-            </mwc-button>
-          `;
-        })}
+        ${this.stateObj.state === "idle" || this.stateObj.state === "paused"
+          ? html`
+              <mwc-button
+                .action="${"start"}"
+                @click="${this._handleActionClick}"
+              >
+                ${this.hass!.localize("ui.card.timer.actions.start")}
+              </mwc-button>
+            `
+          : ""}
+        ${this.stateObj.state === "active"
+          ? html`
+              <mwc-button
+                .action="${"pause"}"
+                @click="${this._handleActionClick}"
+              >
+                ${this.hass!.localize("ui.card.timer.actions.pause")}
+              </mwc-button>
+            `
+          : ""}
+        ${this.stateObj.state === "active" || this.stateObj.state === "paused"
+          ? html`
+              <mwc-button
+                .action="${"cancel"}"
+                @click="${this._handleActionClick}"
+              >
+                ${this.hass!.localize("ui.card.timer.actions.cancel")}
+              </mwc-button>
+              <mwc-button
+                .action="${"finish"}"
+                @click="${this._handleActionClick}"
+              >
+                ${this.hass!.localize("ui.card.timer.actions.finish")}
+              </mwc-button>
+            `
+          : ""}
       </div>
     `;
   }
@@ -45,7 +76,9 @@ class MoreInfoTimer extends LitElement {
 
   private _handleActionClick(e: MouseEvent): void {
     const action = (e.currentTarget as any).action;
-    this.hass.callService("timer", action, undefined);
+    this.hass.callService("timer", action, {
+      entity_id: this.stateObj!.entity_id,
+    });
   }
 
   static get styles(): CSSResult {
@@ -61,4 +94,8 @@ class MoreInfoTimer extends LitElement {
   }
 }
 
-customElements.define("more-info-timer", MoreInfoTimer);
+declare global {
+  interface HTMLElementTagNameMap {
+    "more-info-timer": MoreInfoTimer;
+  }
+}
