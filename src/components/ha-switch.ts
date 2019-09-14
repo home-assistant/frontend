7aa4c8b8ca70c8942e7eefba5e3bb3cdc27ad4fe
Ian Richardson <iantrich@gmail.com>
2019-09-13 22:51:49 -0500

    apply not-checked theming
    Thanks @bramkragten!

--- src/components/ha-switch.ts
+++ src/components/ha-switch.ts
@@ -1,5 +1,6 @@
-import { Constructor, customElement } from "lit-element";
+import { Constructor, customElement, CSSResult, css } from "lit-element";
 import "@material/mwc-switch";
+import { style } from "@material/mwc-switch/mwc-switch-css";
 // tslint:disable-next-line
 import { Switch } from "@material/mwc-switch";
 // tslint:disable-next-line
@@ -10,6 +11,22 @@ export class HaSwitch extends MwcSwitch {
   protected firstUpdated() {
     super.firstUpdated();
     this.style.setProperty("--mdc-theme-secondary", "var(--primary-color)");
+  }
+
+  protected static get styles(): CSSResult[] {
+    return [
+      style,
+      css`
+        .mdc-switch:not(.mdc-switch--checked) .mdc-switch__thumb {
+          background-color: var(--paper-toggle-button-unchecked-button-color);
+          border-color: var(--paper-toggle-button-unchecked-button-color);
+        }
+        .mdc-switch:not(.mdc-switch--checked) .mdc-switch__track {
+          background-color: var(--paper-toggle-button-unchecked-bar-color;
+          border-color: var(--paper-toggle-button-unchecked-bar-color);
+        }
+      `,
+    ];
   }
 }
 
