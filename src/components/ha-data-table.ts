eb1f7e7315ef2cf0050e281d56447d56f4d56106
Bram Kragten <mail@bramkragten.nl>
2019-09-17 07:44:52 +0200

    paper-card > card

--- src/components/ha-data-table.ts
+++ src/components/ha-data-table.ts
@@ -427,7 +427,7 @@ export class HaDataTable extends BaseElement {
       }
 
       .mdc-data-table {
-        background-color: var(--card-background-color, white);
+        background-color: var(--card-background-color);
         border-radius: 4px;
         border-width: 1px;
         border-style: solid;
