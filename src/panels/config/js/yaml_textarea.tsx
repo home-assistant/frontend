1b31dc4ca64aa0a2f7fe429233184a032af7e60c
Bram Kragten <mail@bramkragten.nl>
2019-10-02 18:46:14 +0200

    Catch errors in constructor

--- src/panels/config/js/yaml_textarea.tsx
+++ src/panels/config/js/yaml_textarea.tsx
@@ -2,7 +2,7 @@ import { h, Component } from "preact";
 import yaml from "js-yaml";
 import "../../../components/ha-textarea";
 
-const isEmpty = (obj) => {
+const isEmpty = (obj: object) => {
   for (const key in obj) {
     if (obj.hasOwnProperty(key)) {
       return false;
@@ -14,12 +14,20 @@ const isEmpty = (obj) => {
 export default class YAMLTextArea extends Component<any, any> {
   constructor(props) {
     super(props);
-    this.state = {
-      isvalid: true,
-      value:
+
+    let value: string | undefined;
+    try {
+      value =
         props.value && !isEmpty(props.value)
           ? yaml.safeDump(props.value)
-          : undefined,
+          : undefined;
+    } catch (err) {
+      alert(`There is an error in the YAML: ${err}`);
+    }
+
+    this.state = {
+      isvalid: true,
+      value,
     };
 
     this.onChange = this.onChange.bind(this);
