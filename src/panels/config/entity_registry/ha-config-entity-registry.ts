f1ce3ec271d9098cb88bb4298e023ca6ddefd997
Bram Kragten <mail@bramkragten.nl>
2019-10-08 16:03:38 +0200

    Migrate entity registry to data-table

--- src/panels/config/entity_registry/ha-config-entity-registry.ts
+++ src/panels/config/entity_registry/ha-config-entity-registry.ts
@@ -6,9 +6,6 @@ import {
   CSSResult,
   property,
 } from "lit-element";
-import "@polymer/paper-item/paper-icon-item";
-import "@polymer/paper-item/paper-item";
-import "@polymer/paper-item/paper-item-body";
 
 import { HomeAssistant } from "../../../types";
 import {
@@ -18,7 +15,7 @@ import {
 } from "../../../data/entity_registry";
 import "../../../layouts/hass-subpage";
 import "../../../layouts/hass-loading-screen";
-import "../../../components/ha-card";
+import "../../../components/data-table/ha-data-table";
 import "../../../components/ha-icon";
 import "../../../components/ha-switch";
 import { domainIcon } from "../../../common/entity/domain_icon";
@@ -30,11 +27,14 @@ import {
   loadEntityRegistryDetailDialog,
 } from "./show-dialog-entity-registry-detail";
 import { UnsubscribeFunc } from "home-assistant-js-websocket";
-import { compare } from "../../../common/string/compare";
-import { classMap } from "lit-html/directives/class-map";
 // tslint:disable-next-line
 import { HaSwitch } from "../../../components/ha-switch";
 import memoize from "memoize-one";
+// tslint:disable-next-line
+import {
+  DataTabelColumnContainer,
+  RowClickedEvent,
+} from "../../../components/data-table/ha-data-table";
 
 class HaConfigEntityRegistry extends LitElement {
   @property() public hass!: HomeAssistant;
@@ -45,9 +45,62 @@ class HaConfigEntityRegistry extends LitElement {
 
   private _filteredEntities = memoize(
     (entities: EntityRegistryEntry[], showDisabled: boolean) =>
-      showDisabled
+      (showDisabled
         ? entities
         : entities.filter((entity) => !Boolean(entity.disabled_by))
+      ).map((entry) => {
+        const state = this.hass!.states[entry.entity_id];
+        return {
+          ...entry,
+          icon: state
+            ? stateIcon(state)
+            : domainIcon(computeDomain(entry.entity_id)),
+          name:
+            computeEntityRegistryName(this.hass!, entry) ||
+            this.hass!.localize("state.default.unavailable"),
+          enabled: !entry.disabled_by,
+          id: entry.entity_id,
+        };
+      })
+  );
+
+  private _columns = memoize(
+    (): DataTabelColumnContainer => {
+      return {
+        icon: {
+          title: "",
+          template: (icon) => html`
+            <ha-icon slot="item-icon" .icon=${icon}></ha-icon>
+          `,
+        },
+        name: {
+          title: "Name",
+          sortable: true,
+          filterable: true,
+          direction: "asc",
+        },
+        entity_id: {
+          title: "Entity id",
+          sortable: true,
+          filterable: true,
+        },
+        platform: {
+          title: "Platform",
+          sortable: true,
+          filterable: true,
+        },
+        enabled: {
+          title: "Enabled",
+          sortable: true,
+          template: (enabled) => html`
+            <ha-icon
+              slot="item-icon"
+              icon=${enabled ? "ha:check-circle-outline" : "ha:cancel"}
+            ></ha-icon>
+          `,
+        },
+      };
+    }
   );
 
   public disconnectedCallback() {
@@ -90,55 +143,19 @@ class HaConfigEntityRegistry extends LitElement {
               )}
             </a>
           </span>
-          <ha-card>
-            <paper-item>
-              <ha-switch
-                ?checked=${this._showDisabled}
-                @change=${this._showDisabledChanged}
-                >${this.hass.localize(
-                  "ui.panel.config.entity_registry.picker.show_disabled"
-                )}</ha-switch
-              ></paper-item
-            >
-            ${this._filteredEntities(this._entities, this._showDisabled).map(
-              (entry) => {
-                const state = this.hass!.states[entry.entity_id];
-                return html`
-                  <paper-icon-item
-                    @click=${this._openEditEntry}
-                    .entry=${entry}
-                    class=${classMap({ "disabled-entry": !!entry.disabled_by })}
-                  >
-                    <ha-icon
-                      slot="item-icon"
-                      .icon=${state
-                        ? stateIcon(state)
-                        : domainIcon(computeDomain(entry.entity_id))}
-                    ></ha-icon>
-                    <paper-item-body two-line>
-                      <div class="name">
-                        ${computeEntityRegistryName(this.hass!, entry) ||
-                          `(${this.hass!.localize(
-                            "state.default.unavailable"
-                          )})`}
-                      </div>
-                      <div class="secondary entity-id">
-                        ${entry.entity_id}
-                      </div>
-                    </paper-item-body>
-                    <div class="platform">
-                      ${entry.platform}
-                      ${entry.disabled_by
-                        ? html`
-                            <br />(disabled)
-                          `
-                        : ""}
-                    </div>
-                  </paper-icon-item>
-                `;
-              }
-            )}
-          </ha-card>
+          <ha-switch
+            ?checked=${this._showDisabled}
+            @change=${this._showDisabledChanged}
+            >${this.hass.localize(
+              "ui.panel.config.entity_registry.picker.show_disabled"
+            )}</ha-switch
+          >
+          <ha-data-table
+            .columns=${this._columns()}
+            .data=${this._filteredEntities(this._entities, this._showDisabled)}
+            @row-click=${this._openEditEntry}
+          >
+          </ha-data-table>
         </ha-config-section>
       </hass-subpage>
     `;
@@ -155,9 +172,7 @@ class HaConfigEntityRegistry extends LitElement {
       this._unsubEntities = subscribeEntityRegistry(
         this.hass.connection,
         (entities) => {
-          this._entities = entities.sort((ent1, ent2) =>
-            compare(ent1.entity_id, ent2.entity_id)
-          );
+          this._entities = entities;
         }
       );
     }
@@ -167,8 +182,14 @@ class HaConfigEntityRegistry extends LitElement {
     this._showDisabled = (ev.target as HaSwitch).checked;
   }
 
-  private _openEditEntry(ev: MouseEvent): void {
-    const entry = (ev.currentTarget! as any).entry;
+  private _openEditEntry(ev: CustomEvent): void {
+    const entryId = (ev.detail as RowClickedEvent).id;
+    const entry = this._entities!.find(
+      (entity) => entity.entity_id === entryId
+    );
+    if (!entry) {
+      return;
+    }
     showEntityRegistryDetailDialog(this, {
       entry,
     });
@@ -179,23 +200,10 @@ class HaConfigEntityRegistry extends LitElement {
       a {
         color: var(--primary-color);
       }
-      ha-card {
+      ha-data-table {
         margin-bottom: 24px;
+        margin-top: 0px;
         direction: ltr;
-      }
-      paper-icon-item {
-        cursor: pointer;
-        color: var(--primary-text-color);
-      }
-      ha-icon {
-        margin-left: 8px;
-      }
-      .platform {
-        text-align: right;
-        margin: 0 0 0 8px;
-      }
-      .disabled-entry {
-        color: var(--secondary-text-color);
       }
     `;
   }
