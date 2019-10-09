8d8203b255901ab704507ad444e9db2c777fc5f2
shbatm <support@shbatm.com>
2019-10-09 12:08:31 -0500

    Add width property to light card to fix #3964

--- src/panels/lovelace/cards/hui-light-card.ts
+++ src/panels/lovelace/cards/hui-light-card.ts
@@ -216,6 +216,7 @@ export class HuiLightCard extends LitElement implements LovelaceCard {
           left: 50%;
           transform: translate(-50%);
           font-size: var(--name-font-size);
+          width: 90%;
         }
 
         .brightness {
