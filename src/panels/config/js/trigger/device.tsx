631584c62615731875b4dcbe67e27686776740e3
Erik <erik@montnemery.com>
2019-10-09 07:36:08 +0200

    Allow device triggers to specify extra fields

--- src/panels/config/js/trigger/device.tsx
+++ src/panels/config/js/trigger/device.tsx
@@ -2,21 +2,29 @@ import { h, Component } from "preact";
 
 import "../../../../components/device/ha-device-picker";
 import "../../../../components/device/ha-device-trigger-picker";
+import "../../../../components/ha-form";
+
+import { fetchDeviceTriggerCapabilities } from "../../../../data/device_automation";
 
 export default class DeviceTrigger extends Component<any, any> {
   constructor() {
     super();
     this.devicePicked = this.devicePicked.bind(this);
     this.deviceTriggerPicked = this.deviceTriggerPicked.bind(this);
-    this.state = { device_id: undefined };
+    this._extraFieldsChanged = this._extraFieldsChanged.bind(this);
+    this.state = { device_id: undefined, capabilities: undefined };
   }
 
   public devicePicked(ev) {
-    this.setState({ device_id: ev.target.value });
+    this.setState({ ...this.state, device_id: ev.target.value });
   }
 
-  public deviceTriggerPicked(ev) {
+  public async deviceTriggerPicked(ev) {
     const deviceTrigger = ev.target.value;
+    const capabilities = deviceTrigger.domain
+      ? await fetchDeviceTriggerCapabilities(this.props.hass, deviceTrigger)
+      : null;
+    this.setState({ ...this.state, capabilities });
     this.props.onChange(this.props.index, deviceTrigger);
   }
 
@@ -24,6 +32,16 @@ export default class DeviceTrigger extends Component<any, any> {
   public render({ trigger, hass }, { device_id }) {
     if (device_id === undefined) {
       device_id = trigger.device_id;
+    }
+    let extraFieldsData = {};
+    if (this.state.capabilities && this.state.capabilities.extra_fields) {
+      this.state.capabilities.extra_fields.forEach(
+        (item) =>
+          (extraFieldsData = {
+            ...extraFieldsData,
+            [item.name]: this.props.trigger[item.name],
+          })
+      );
     }
 
     return (
@@ -41,8 +59,64 @@ export default class DeviceTrigger extends Component<any, any> {
           hass={hass}
           label="Trigger"
         />
+        {this.state.capabilities && this.state.capabilities.extra_fields && (
+          <ha-form
+            data={extraFieldsData}
+            onData-changed={this._extraFieldsChanged}
+            schema={this.state.capabilities.extra_fields}
+            // error={step.errors} // TODO
+            computeLabel={this._extraFieldsComputeLabelCallback(hass.localize)}
+            computeSuffix={this._extraFieldsComputeSuffixCallback()}
+            // computeError={this._extraFieldsComputeErrorCallback} // TODO
+          />
+        )}
       </div>
     );
+  }
+
+  public componentDidMount() {
+    const hass = this.props.hass;
+    const trigger = this.props.trigger;
+
+    if (!this.state.capabilities && trigger.domain) {
+      fetchDeviceTriggerCapabilities(hass, trigger).then((capabilities) => {
+        this.setState({ ...this.state, capabilities });
+      });
+    }
+  }
+
+  private _extraFieldsChanged(ev) {
+    if (!ev.detail.path) {
+      return;
+    }
+    const item = ev.detail.path.replace("data.", "");
+    const value = ev.detail.value || undefined;
+
+    this.props.onChange(this.props.index, {
+      ...this.props.trigger,
+      [item]: value,
+    });
+  }
+
+  private _extraFieldsComputeLabelCallback(localize) {
+    // Returns a callback for ha-form to calculate labels per schema object
+    return (schema) =>
+      localize(
+        `ui.panel.config.automation.editor.triggers.type.device.extra_fields.${
+          schema.name
+        }`
+      ) || schema.name;
+  }
+
+  private _extraFieldsComputeSuffixCallback() {
+    // Returns a callback for ha-form to calculate suffixes per schema object
+    return (schema) => {
+      let description = "";
+      if (schema.description) {
+        description = schema.description.unit_of_measurement || "";
+      }
+      return description;
+    };
   }
 }
 
