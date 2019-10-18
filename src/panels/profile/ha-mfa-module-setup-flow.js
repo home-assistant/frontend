00f2d36cb5b8c2e2446fd0682b7fef51534159d5
Bram Kragten <mail@bramkragten.nl>
2019-10-18 16:08:34 +0200

    Migrate ha-form to lit (#4000)
    * Migrate ha-form to lit
    
    * Fix import path
    
    * Update
    
    * add default, change suffix, fix import
    
    * Fix select

--- src/panels/profile/ha-mfa-module-setup-flow.js
+++ src/panels/profile/ha-mfa-module-setup-flow.js
@@ -5,7 +5,7 @@ import { html } from "@polymer/polymer/lib/utils/html-tag";
 import { PolymerElement } from "@polymer/polymer/polymer-element";
 
 import "../../components/dialog/ha-paper-dialog";
-import "../../components/ha-form";
+import "../../components/ha-form/ha-form";
 import "../../components/ha-markdown";
 import "../../resources/ha-style";
 
