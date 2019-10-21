5504abc84d79eda8475fd8351c1c716cedec1164
Drake Loud <louddrm@gmail.com>
2019-10-21 13:05:51 -0700

    updated view config title

--- src/panels/lovelace/editor/view-editor/hui-edit-view.ts
+++ src/panels/lovelace/editor/view-editor/hui-edit-view.ts
@@ -88,13 +88,13 @@ export class HuiEditView extends LitElement {
   }
 
   private get _viewConfigTitle(): string {
-    if (!this._config || !this._config.title || this._config.title === "") {
+    if (!this._config || !this._config.title) {
       return this.hass!.localize(
         "ui.panel.lovelace.editor.edit_view.header"
       );;
     }
 
-    return this.hass!.localize("ui.panel.lovelace.editor.edit_view.header_name", this._config.title)
+    return this.hass!.localize("ui.panel.lovelace.editor.edit_view.header_name", "name", this._config.title)
   }
 
   protected render(): TemplateResult | void {
