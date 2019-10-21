56cacb89bba636e3182e9e86853b71d1869fbadf
Drake Loud <louddrm@gmail.com>
2019-10-21 13:49:47 -0700

    Prettier updates

--- src/panels/lovelace/editor/view-editor/hui-edit-view.ts
+++ src/panels/lovelace/editor/view-editor/hui-edit-view.ts
@@ -89,12 +89,12 @@ export class HuiEditView extends LitElement {
 
   private get _viewConfigTitle(): string {
     if (!this._config || !this._config.title) {
-      return this.hass!.localize("ui.panel.lovelace.editor.edit_view.header");;
+      return this.hass!.localize("ui.panel.lovelace.editor.edit_view.header");
     }
 
     return this.hass!.localize(
-      "ui.panel.lovelace.editor.edit_view.header_name", 
-      "name", 
+      "ui.panel.lovelace.editor.edit_view.header_name",
+      "name",
       this._config.title
     );
   }
