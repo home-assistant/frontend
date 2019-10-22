49d0f2359b31b3553f64f4fca0bd65857297d8c6
Ian Richardson <iantrich@gmail.com>
2019-10-22 14:43:34 -0500

    add sensor card to first demo (#4035)
    * add sensor card to first demo
    
    * Change to temperature sensor

--- demo/src/configs/arsaboo/lovelace.ts
+++ demo/src/configs/arsaboo/lovelace.ts
@@ -447,6 +447,11 @@ export const demoLovelaceArsaboo: DemoConfig["lovelace"] = (localize) => ({
           ],
         },
         {
+          type: "sensor",
+          entity: "sensor.study_temp",
+          graph: "line",
+        },
+        {
           type: "entities",
           title: "Doorbell",
           show_header_toggle: false,
