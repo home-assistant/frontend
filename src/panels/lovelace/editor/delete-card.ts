d5c33d06eef1af16f1afce9a5c18c3e750c1e493
Timmo <contact@timmo.xyz>
2019-10-22 20:20:42 +0100

    Confirmation Dialog (#4053)
    * :sparkles: Add confirmation dialog
    
    * :sparkles: Add confirmation dialog to service calls
    
    * :hammer: Change returned value
    
    * :sparkles: Add confirmation dialog to integration remove
    
    * :sparkles: Add to entity registry
    
    * :sparkles: Add to delete card and add translation
    
    * :sparkles: Add to views deletion
    
    * :hammer: Remove async
    
    * :hammer: Fix min-width for smaller screens
    
    * :hammer: Remove async
    
    * Fix wrong merge
    
    * Update ha-config-entry-page.ts

--- src/panels/lovelace/editor/delete-card.ts
+++ src/panels/lovelace/editor/delete-card.ts
@@ -1,16 +1,22 @@
 import { Lovelace } from "../types";
 import { deleteCard } from "./config-util";
+import { showConfirmationDialog } from "../../../dialogs/confirmation/show-dialog-confirmation";
+import { HomeAssistant } from "../../../types";
 
 export async function confDeleteCard(
+  element: HTMLElement,
+  hass: HomeAssistant,
   lovelace: Lovelace,
   path: [number, number]
 ): Promise<void> {
-  if (!confirm("Are you sure you want to delete this card?")) {
-    return;
-  }
-  try {
-    await lovelace.saveConfig(deleteCard(lovelace.config, path));
-  } catch (err) {
-    alert(`Deleting failed: ${err.message}`);
-  }
+  showConfirmationDialog(element, {
+    text: hass.localize("ui.panel.lovelace.cards.confirm_delete"),
+    confirm: async () => {
+      try {
+        await lovelace.saveConfig(deleteCard(lovelace.config, path));
+      } catch (err) {
+        alert(`Deleting failed: ${err.message}`);
+      }
+    },
+  });
 }
