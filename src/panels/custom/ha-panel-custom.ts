2eb07b43bf508ac9cfb4baee2078199735f212f4
springstan <floorb@gmx.de>
2019-10-24 16:42:27 +0200

    Translated custom panel confirm message

--- src/panels/custom/ha-panel-custom.ts
+++ src/panels/custom/ha-panel-custom.ts
@@ -78,13 +78,23 @@ export class HaPanelCustom extends UpdatingElement {
       !["localhost", "127.0.0.1", location.hostname].includes(tempA.hostname)
     ) {
       if (
-        !confirm(`Do you trust the external panel "${config.name}" at "${
-          tempA.href
-        }"?
-
-It will have access to all data in Home Assistant.
-
-(Check docs for the panel_custom component to hide this message)`)
+        !confirm(
+          this.hass.localize(
+            "ui.panel.custom.external_panel.question_trust",
+            "name",
+            config.name,
+            "link",
+            tempA.href
+          ) +
+            "\n\n" +
+            this.hass.localize(
+              "ui.panel.custom.external_panel.complete_access"
+            ) +
+            "\n\n" +
+            "(" +
+            this.hass.localize("ui.panel.custom.external_panel.hide_message") +
+            ")"
+        )
       ) {
         return;
       }
