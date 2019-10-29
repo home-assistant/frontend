53453280bb46b11a79386d4f7561502486fd147d
Bram Kragten <mail@bramkragten.nl>
2019-10-29 14:57:05 +0100

    Added animation when listening and we should talk back right? :'-)

--- src/dialogs/voice-command-dialog/ha-voice-command-dialog.ts
+++ src/dialogs/voice-command-dialog/ha-voice-command-dialog.ts
@@ -110,11 +110,20 @@ export class HaVoiceCommandDialog extends LitElement {
             ${SpeechRecognition
               ? html`
                   <span suffix="" slot="suffix">
+                    ${this.results
+                      ? html`
+                          <div class="bouncer">
+                            <div class="double-bounce1"></div>
+                            <div class="double-bounce2"></div>
+                          </div>
+                        `
+                      : ""}
                     <paper-icon-button
                       .active=${Boolean(this.results)}
                       icon="hass:microphone"
                       @click=${this._toggleListening}
-                    ></paper-icon-button>
+                    >
+                    </paper-icon-button>
                   </span>
                 `
               : ""}
@@ -212,6 +221,13 @@ export class HaVoiceCommandDialog extends LitElement {
         who: "hass",
         text: response.speech.plain.speech,
       });
+      if (speechSynthesis) {
+        const speech = new SpeechSynthesisUtterance(
+          response.speech.plain.speech
+        );
+        speech.lang = "en-US";
+        speechSynthesis.speak(speech);
+      }
     } catch {
       this._conversation.slice(-1).pop()!.error = true;
       this.requestUpdate();
@@ -347,6 +363,50 @@ export class HaVoiceCommandDialog extends LitElement {
 
         .messages {
           max-height: 68vh;
+        }
+      }
+
+      .bouncer {
+        width: 40px;
+        height: 40px;
+        position: absolute;
+        top: 0;
+      }
+      .double-bounce1,
+      .double-bounce2 {
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        background-color: var(--primary-color);
+        opacity: 0.2;
+        position: absolute;
+        top: 0;
+        left: 0;
+        -webkit-animation: sk-bounce 2s infinite ease-in-out;
+        animation: sk-bounce 2s infinite ease-in-out;
+      }
+      .double-bounce2 {
+        -webkit-animation-delay: -1s;
+        animation-delay: -1s;
+      }
+      @-webkit-keyframes sk-bounce {
+        0%,
+        100% {
+          -webkit-transform: scale(0);
+        }
+        50% {
+          -webkit-transform: scale(1);
+        }
+      }
+      @keyframes sk-bounce {
+        0%,
+        100% {
+          transform: scale(0);
+          -webkit-transform: scale(0);
+        }
+        50% {
+          transform: scale(1);
+          -webkit-transform: scale(1);
         }
       }
     `;
