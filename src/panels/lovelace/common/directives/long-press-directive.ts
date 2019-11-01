4eb028c734ea089a033e54228aa02fe2a47a0f43
Ian Richardson <iantrich@gmail.com>
2019-11-01 10:39:53 -0500

    enter is registering as double_tap (#4161)
    * enter is registering as double_tap
    
    * Update long-press-directive.ts
    
    * Update long-press-directive.ts

--- src/panels/lovelace/common/directives/long-press-directive.ts
+++ src/panels/lovelace/common/directives/long-press-directive.ts
@@ -123,7 +123,7 @@ class LongPress extends HTMLElement implements LongPress {
       if (this.held) {
         element.dispatchEvent(new Event("ha-hold"));
       } else if (options.hasDoubleClick) {
-        if ((ev as MouseEvent).detail === 1) {
+        if ((ev as MouseEvent).detail === 1 || ev.type === "keyup") {
           this.dblClickTimeout = window.setTimeout(() => {
             element.dispatchEvent(new Event("ha-click"));
           }, 250);
