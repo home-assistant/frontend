ab22df5e7bab4c869f3dfbff753f812e28296c41
springstan <46536646+springstan@users.noreply.github.com>
2019-11-01 16:07:18 +0100

    Translated unused entities, device registry page and domain toggler dialog (#4141)
    * Translated device registry and domain toggler dialog
    
    * Translated unused entities page
    
    * Extracted unused_entities keys and created new section for domain_toggler
    
    * Moved unknow_error and area_picker_label into devices section

--- src/dialogs/domain-toggler/dialog-domain-toggler.ts
+++ src/dialogs/domain-toggler/dialog-domain-toggler.ts
@@ -37,7 +37,9 @@ class DomainTogglerDialog extends LitElement {
         opened
         @opened-changed=${this._openedChanged}
       >
-        <h2>Toggle Domains</h2>
+        <h2>
+          ${this.hass.localize("ui.dialogs.domain_toggler.title")}
+        </h2>
         <div>
           ${domains.map(
             (domain) =>
