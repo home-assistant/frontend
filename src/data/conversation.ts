dd44c33902266c7cc59f0f39f9073ed3fde8a97c
Anton Verburg <info@tec-v.nl>
2019-11-10 20:30:41 +0100

    Add attribution and onboarding to voice (#4190)
    * Add attribution and onboarding to voice
    
    * Align with backend changes
    
    * Layout + switch to ws for process
    
    * Don't mutate window
    
    * Move speechRecognition
    
    * Add border
    
    * Update ha-voice-command-dialog.ts

--- src/data/conversation.ts
+++ src/data/conversation.ts
@@ -7,10 +7,33 @@ interface ProcessResults {
   };
 }
 
+export interface AgentInfo {
+  attribution?: { name: string; url: string };
+  onboarding?: { text: string; url: string };
+}
+
 export const processText = (
   hass: HomeAssistant,
   text: string,
   // tslint:disable-next-line: variable-name
   conversation_id: string
 ): Promise<ProcessResults> =>
-  hass.callApi("POST", "conversation/process", { text, conversation_id });
+  hass.callWS({
+    type: "conversation/process",
+    text,
+    conversation_id,
+  });
+
+export const getAgentInfo = (hass: HomeAssistant): Promise<AgentInfo> =>
+  hass.callWS({
+    type: "conversation/agent/info",
+  });
+
+export const setConversationOnboarding = (
+  hass: HomeAssistant,
+  value: boolean
+): Promise<boolean> =>
+  hass.callWS({
+    type: "conversation/onboarding/set",
+    shown: value,
+  });
