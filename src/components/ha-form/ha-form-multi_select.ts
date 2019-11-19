cfd169d1e6d04d61feff6f443fe5463300754c82
Bram Kragten <mail@bramkragten.nl>
2019-11-19 15:52:35 +0100

    Add multi select component

---
+++ src/components/ha-form/ha-form-multi_select.ts
@@ -0,0 +1,90 @@
+import {
+  customElement,
+  LitElement,
+  html,
+  property,
+  TemplateResult,
+  query,
+} from "lit-element";
+import {
+  HaFormElement,
+  HaFormMultiSelectSchema,
+  HaFormMultiSelectData,
+} from "./ha-form";
+import { fireEvent } from "../../common/dom/fire_event";
+
+import "@polymer/paper-dropdown-menu/paper-dropdown-menu";
+import "@polymer/paper-listbox/paper-listbox";
+import "@polymer/paper-item/paper-icon-item";
+import "@polymer/paper-checkbox/paper-checkbox";
+
+@customElement("ha-form-multi_select")
+export class HaFormMultiSelect extends LitElement implements HaFormElement {
+  @property() public schema!: HaFormMultiSelectSchema;
+  @property() public data!: HaFormMultiSelectData;
+  @property() public label!: string;
+  @property() public suffix!: string;
+  @property() private _init = false;
+  @query("paper-dropdown-menu") private _input?: HTMLElement;
+
+  public focus() {
+    if (this._input) {
+      this._input.focus();
+    }
+  }
+
+  protected render(): TemplateResult {
+    return html`
+      <paper-listbox
+        multi
+        attr-for-selected="item-value"
+        .selectedValues=${this.data}
+        @selected-items-changed=${this._valueChanged}
+      >
+        ${this.schema.options!.map((item) => {
+          const value = this._optionValue(item);
+          return html`
+            <paper-icon-item .itemValue=${value}>
+              <paper-checkbox
+                .checked=${this.data.includes(value)}
+                slot="item-icon"
+              ></paper-checkbox>
+              ${this._optionLabel(item)}
+            </paper-icon-item>
+          `;
+        })}
+      </paper-listbox>
+    `;
+  }
+
+  private _optionValue(item) {
+    return Array.isArray(item) ? item[0] : item;
+  }
+
+  private _optionLabel(item) {
+    return Array.isArray(item) ? item[1] : item;
+  }
+
+  private _valueChanged(ev: CustomEvent) {
+    if (!ev.detail.value || !this._init) {
+      // ignore first call because that is the init of the component
+      this._init = true;
+      return;
+    }
+
+    fireEvent(
+      this,
+      "value-changed",
+      {
+        value: ev.detail.value.map((element) => element.itemValue),
+      },
+      { bubbles: false }
+    );
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "ha-form-multi_select": HaFormMultiSelect;
+  }
+}
--- src/components/ha-form/ha-form.ts
+++ src/components/ha-form/ha-form.ts
@@ -12,6 +12,7 @@ import "./ha-form-integer";
 import "./ha-form-float";
 import "./ha-form-boolean";
 import "./ha-form-select";
+import "./ha-form-multi_select";
 import "./ha-form-positive_time_period_dict";
 import { fireEvent } from "../../common/dom/fire_event";
 import { dynamicElement } from "../../common/dom/dynamic-element-directive";
@@ -22,6 +23,7 @@ export type HaFormSchema =
   | HaFormFloatSchema
   | HaFormBooleanSchema
   | HaFormSelectSchema
+  | HaFormMultiSelectSchema
   | HaFormTimeSchema;
 
 export interface HaFormBaseSchema {
@@ -41,6 +43,11 @@ export interface HaFormIntegerSchema extends HaFormBaseSchema {
 
 export interface HaFormSelectSchema extends HaFormBaseSchema {
   type: "select";
+  options?: string[];
+}
+
+export interface HaFormMultiSelectSchema extends HaFormBaseSchema {
+  type: "multi_select";
   options?: string[];
 }
 
@@ -71,6 +78,7 @@ export type HaFormData =
   | HaFormFloatData
   | HaFormBooleanData
   | HaFormSelectData
+  | HaFormMultiSelectData
   | HaFormTimeData;
 
 export type HaFormStringData = string;
@@ -78,6 +86,7 @@ export type HaFormIntegerData = number;
 export type HaFormFloatData = number;
 export type HaFormBooleanData = boolean;
 export type HaFormSelectData = string;
+export type HaFormMultiSelectData = string[];
 export interface HaFormTimeData {
   hours?: number;
   minutes?: number;
