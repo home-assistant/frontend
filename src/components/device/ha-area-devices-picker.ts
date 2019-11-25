de9129f962d1f185bfbd30542515fed9b68969b5
Bram Kragten <mail@bramkragten.nl>
2019-11-25 17:36:18 +0100

    WIP: Area/multiple devices and name support

--- src/components/device/ha-area-devices-picker.ts
+++ src/components/device/ha-area-devices-picker.ts
@@ -12,16 +12,17 @@ import {
   CSSResult,
   customElement,
   property,
+  PropertyValues,
 } from "lit-element";
 import { UnsubscribeFunc } from "home-assistant-js-websocket";
 import { SubscribeMixin } from "../../mixins/subscribe-mixin";
+import "./ha-devices-picker";
 
 import { HomeAssistant } from "../../types";
 import { fireEvent } from "../../common/dom/fire_event";
 import {
   DeviceRegistryEntry,
   subscribeDeviceRegistry,
-  computeDeviceName,
 } from "../../data/device_registry";
 import { compare } from "../../common/string/compare";
 import { PolymerChangedEvent } from "../../polymer-types";
@@ -36,42 +37,60 @@ import {
 } from "../../data/entity_registry";
 import { computeDomain } from "../../common/entity/compute_domain";
 
-interface Device {
-  name: string;
-  area: string;
-  id: string;
+interface DevicesByArea {
+  [areaId: string]: AreaDevices;
 }
 
-const rowRenderer = (root: HTMLElement, _owner, model: { item: Device }) => {
+interface AreaDevices {
+  id?: string;
+  name: string;
+  devices: string[];
+}
+
+const rowRenderer = (
+  root: HTMLElement,
+  _owner,
+  model: { item: AreaDevices }
+) => {
   if (!root.firstElementChild) {
     root.innerHTML = `
     <style>
       paper-item {
+        width: 100%;
         margin: -10px 0;
         padding: 0;
       }
+      paper-icon-button {
+        float: right;
+      }
+      .devices {
+        display: none;
+      }
+      .devices.visible {
+        display: block;
+      }
     </style>
     <paper-item>
-      <paper-item-body two-line="">    
+      <paper-item-body two-line="">
         <div class='name'>[[item.name]]</div>
-        <div secondary>[[item.area]]</div>
+        <div secondary>[[item.devices.length]] devices</div>
       </paper-item-body>
     </paper-item>
     `;
   }
-
   root.querySelector(".name")!.textContent = model.item.name!;
-  root.querySelector("[secondary]")!.textContent = model.item.area!;
+  root.querySelector(
+    "[secondary]"
+  )!.textContent = `${model.item.devices.length.toString()} devices`;
 };
 
-@customElement("ha-device-picker")
-export class HaDevicePicker extends SubscribeMixin(LitElement) {
+@customElement("ha-area-devices-picker")
+export class HaAreaDevicesPicker extends SubscribeMixin(LitElement) {
   @property() public hass!: HomeAssistant;
   @property() public label?: string;
   @property() public value?: string;
-  @property() public devices?: DeviceRegistryEntry[];
-  @property() public areas?: AreaRegistryEntry[];
-  @property() public entities?: EntityRegistryEntry[];
+  @property() public area?: string;
+  @property() public devices?: string[];
   /**
    * Show only devices with entities from specific domains.
    * @type {Array}
@@ -95,6 +114,12 @@ export class HaDevicePicker extends SubscribeMixin(LitElement) {
   public includeDeviceClasses?: string[];
   @property({ type: Boolean })
   private _opened?: boolean;
+  @property() private _areaPicker = true;
+  @property() private _devices?: DeviceRegistryEntry[];
+  @property() private _areas?: AreaRegistryEntry[];
+  @property() private _entities?: EntityRegistryEntry[];
+  private _selectedDevices: string[] = [];
+  private _filteredDevices: DeviceRegistryEntry[] = [];
 
   private _getDevices = memoizeOne(
     (
@@ -104,7 +129,7 @@ export class HaDevicePicker extends SubscribeMixin(LitElement) {
       includeDomains: this["includeDomains"],
       excludeDomains: this["excludeDomains"],
       includeDeviceClasses: this["includeDeviceClasses"]
-    ): Device[] => {
+    ): AreaDevices[] => {
       if (!devices.length) {
         return [];
       }
@@ -118,11 +143,6 @@ export class HaDevicePicker extends SubscribeMixin(LitElement) {
           deviceEntityLookup[entity.device_id] = [];
         }
         deviceEntityLookup[entity.device_id].push(entity);
-      }
-
-      const areaLookup: { [areaId: string]: AreaRegistryEntry } = {};
-      for (const area of areas) {
-        areaLookup[area.area_id] = area;
       }
 
       let inputDevices = [...devices];
@@ -171,65 +191,116 @@ export class HaDevicePicker extends SubscribeMixin(LitElement) {
         });
       }
 
-      const outputDevices = inputDevices.map((device) => {
-        return {
-          id: device.id,
-          name: computeDeviceName(
-            device,
-            this.hass,
-            deviceEntityLookup[device.id]
-          ),
-          area: device.area_id ? areaLookup[device.area_id].name : "No area",
-        };
-      });
-      if (outputDevices.length === 1) {
-        return outputDevices;
+      this._filteredDevices = inputDevices;
+
+      const areaLookup: { [areaId: string]: AreaRegistryEntry } = {};
+      for (const area of areas) {
+        areaLookup[area.area_id] = area;
       }
-      return outputDevices.sort((a, b) => compare(a.name || "", b.name || ""));
+
+      const devicesByArea: DevicesByArea = {};
+
+      for (const device of inputDevices) {
+        const areaId = device.area_id;
+        if (areaId) {
+          if (!(areaId in devicesByArea)) {
+            devicesByArea[areaId] = {
+              id: areaId,
+              name: areaLookup[areaId].name,
+              devices: [],
+            };
+          }
+          devicesByArea[areaId].devices.push(device.id);
+        }
+      }
+
+      const sorted = Object.keys(devicesByArea)
+        .sort((a, b) =>
+          compare(devicesByArea[a].name || "", devicesByArea[b].name || "")
+        )
+        .map((key) => devicesByArea[key]);
+
+      return sorted;
     }
   );
 
   public hassSubscribe(): UnsubscribeFunc[] {
     return [
       subscribeDeviceRegistry(this.hass.connection!, (devices) => {
-        this.devices = devices;
+        this._devices = devices;
       }),
       subscribeAreaRegistry(this.hass.connection!, (areas) => {
-        this.areas = areas;
+        this._areas = areas;
       }),
       subscribeEntityRegistry(this.hass.connection!, (entities) => {
-        this.entities = entities;
+        this._entities = entities;
       }),
     ];
   }
 
+  protected updated(changedProps: PropertyValues) {
+    if (changedProps.has("area") && this.area) {
+      this._areaPicker = true;
+      this.value = this.area;
+    } else if (changedProps.has("devices") && this.devices) {
+      this._areaPicker = false;
+      const filteredDeviceIds = this._filteredDevices.map(
+        (device) => device.id
+      );
+      const selectedDevices = this.devices.filter((device) =>
+        filteredDeviceIds.includes(device)
+      );
+      this._setValue(selectedDevices);
+    }
+  }
+
   protected render(): TemplateResult | void {
-    if (!this.devices || !this.areas || !this.entities) {
+    if (!this._devices || !this._areas || !this._entities) {
       return;
     }
-    const devices = this._getDevices(
-      this.devices,
-      this.areas,
-      this.entities,
+    const areas = this._getDevices(
+      this._devices,
+      this._areas,
+      this._entities,
       this.includeDomains,
       this.excludeDomains,
       this.includeDeviceClasses
     );
+    if (!this._areaPicker || areas.length === 0) {
+      return html`
+        <ha-devices-picker
+          @value-changed=${this._devicesPicked}
+          .hass=${this.hass}
+          .includeDomains=${this.includeDomains}
+          .includeDeviceClasses=${this.includeDeviceClasses}
+          .value=${this._selectedDevices}
+          .pickDeviceLabel=${`Add ${this.label} device`}
+          .pickedDeviceLabel=${`${this.label} device`}
+        ></ha-devices-picker>
+        ${areas.length > 0
+          ? html`
+              <mwc-button @click=${this._switchPicker}
+                >Choose an area</mwc-button
+              >
+            `
+          : ""}
+      `;
+    }
     return html`
       <vaadin-combo-box-light
         item-value-path="id"
         item-id-path="id"
         item-label-path="name"
-        .items=${devices}
+        .items=${areas}
         .value=${this._value}
         .renderer=${rowRenderer}
         @opened-changed=${this._openedChanged}
-        @value-changed=${this._deviceChanged}
+        @value-changed=${this._areaPicked}
       >
         <paper-input
           .label=${this.label === undefined && this.hass
             ? this.hass.localize("ui.components.device-picker.device")
-            : this.label}
+            : `${this.label} in area`}
           class="input"
           autocapitalize="none"
           autocomplete="off"
@@ -252,7 +323,7 @@ export class HaDevicePicker extends SubscribeMixin(LitElement) {
                 </paper-icon-button>
               `
             : ""}
-          ${devices.length > 0
+          ${areas.length > 0
             ? html`
                 <paper-icon-button
                   aria-label=${this.hass.localize(
@@ -268,34 +339,53 @@ export class HaDevicePicker extends SubscribeMixin(LitElement) {
             : ""}
         </paper-input>
       </vaadin-combo-box-light>
+      <mwc-button @click=${this._switchPicker}
+        >Choose individual devices</mwc-button
+      >
     `;
   }
 
   private _clearValue(ev: Event) {
     ev.stopPropagation();
-    this._setValue("");
+    this._setValue([]);
   }
 
   private get _value() {
-    return this.value || "";
+    return this.value || [];
   }
 
   private _openedChanged(ev: PolymerChangedEvent<boolean>) {
     this._opened = ev.detail.value;
   }
 
-  private _deviceChanged(ev: PolymerChangedEvent<string>) {
-    const newValue = ev.detail.value;
+  private async _switchPicker() {
+    this._areaPicker = !this._areaPicker;
+  }
 
-    if (newValue !== this._value) {
-      this._setValue(newValue);
+  private async _areaPicked(ev: PolymerChangedEvent<string>) {
+    const value = ev.detail.value;
+    let selectedDevices = [];
+    const target = ev.target as any;
+    if (target.selectedItem) {
+      selectedDevices = target.selectedItem.devices;
+    }
+
+    if (value !== this._value || this._selectedDevices !== selectedDevices) {
+      this._setValue(selectedDevices, value);
     }
   }
 
-  private _setValue(value: string) {
+  private _devicesPicked(ev: CustomEvent) {
+    ev.stopPropagation();
+    const selectedDevices = ev.detail.value;
+    this._setValue(selectedDevices);
+  }
+
+  private _setValue(selectedDevices: string[], value = "") {
     this.value = value;
+    this._selectedDevices = selectedDevices;
     setTimeout(() => {
-      fireEvent(this, "value-changed", { value });
+      fireEvent(this, "value-changed", { value: selectedDevices });
       fireEvent(this, "change");
     }, 0);
   }
@@ -317,6 +407,6 @@ export class HaDevicePicker extends SubscribeMixin(LitElement) {
 
 declare global {
   interface HTMLElementTagNameMap {
-    "ha-device-picker": HaDevicePicker;
+    "ha-area-devices-picker": HaAreaDevicesPicker;
   }
 }
--- src/panels/config/automation/thingtalk/ha-thingtalk-placeholders.ts
+++ src/panels/config/automation/thingtalk/ha-thingtalk-placeholders.ts
@@ -6,8 +6,11 @@ import {
   customElement,
   css,
   CSSResult,
-  query,
+  PropertyValues,
 } from "lit-element";
+
+import "../../../../components/device/ha-area-devices-picker";
+
 import { HomeAssistant } from "../../../../types";
 import { PolymerChangedEvent } from "../../../../polymer-types";
 import { fireEvent } from "../../../../common/dom/fire_event";
@@ -17,8 +20,15 @@ import { SubscribeMixin } from "../../../../mixins/subscribe-mixin";
 import { subscribeEntityRegistry } from "../../../../data/entity_registry";
 import { computeDomain } from "../../../../common/entity/compute_domain";
 import { HassEntity } from "home-assistant-js-websocket";
-import { HaDevicePicker } from "../../../../components/device/ha-device-picker";
 import { getPath, applyPatch } from "../../../../common/util/patch";
+import {
+  subscribeAreaRegistry,
+  AreaRegistryEntry,
+} from "../../../../data/area_registry";
+import {
+  subscribeDeviceRegistry,
+  DeviceRegistryEntry,
+} from "../../../../data/device_registry";
 
 declare global {
   // for fire event
@@ -28,7 +38,23 @@ declare global {
 }
 
 export interface PlaceholderValues {
-  [key: string]: { [index: number]: { [key: string]: string } };
+  [key: string]: {
+    [index: number]: {
+      [index: number]: { device_id?: string; entity_id?: string };
+    };
+  };
+}
+
+export interface ExtraInfo {
+  [key: string]: {
+    [index: number]: {
+      [index: number]: {
+        area_id?: string;
+        device_id?: string;
+        manualEntity: boolean;
+      };
+    };
+  };
 }
 
 interface DeviceEntitiesLookup {
@@ -43,9 +69,11 @@ export class ThingTalkPlaceholders extends SubscribeMixin(LitElement) {
   @property() public placeholders!: PlaceholderContainer;
   @property() private _error?: string;
   private _deviceEntityLookup: DeviceEntitiesLookup = {};
-  private _manualEntities: PlaceholderValues = {};
+  @property() private _extraInfo: ExtraInfo = {};
   @property() private _placeholderValues: PlaceholderValues = {};
-  @query("#device-entity-picker") private _deviceEntityPicker?: HaDevicePicker;
+  private _devices?: DeviceRegistryEntry[];
+  private _areas?: AreaRegistryEntry[];
+  private _search = false;
 
   public hassSubscribe() {
     return [
@@ -66,12 +94,28 @@ export class ThingTalkPlaceholders extends SubscribeMixin(LitElement) {
           }
         }
       }),
+      subscribeDeviceRegistry(this.hass.connection!, (devices) => {
+        this._devices = devices;
+        this._searchNames();
+      }),
+      subscribeAreaRegistry(this.hass.connection!, (areas) => {
+        this._areas = areas;
+        this._searchNames();
+      }),
     ];
+  }
+
+  protected updated(changedProps: PropertyValues) {
+    if (changedProps.has("placeholders")) {
+      this._search = true;
+      this._searchNames();
+    }
   }
 
   protected render(): TemplateResult | void {
     return html`
       <ha-paper-dialog
+        modal
         with-backdrop
         .opened=${this.opened}
         @opened-changed="${this._openedChanged}"
@@ -93,55 +137,66 @@ export class ThingTalkPlaceholders extends SubscribeMixin(LitElement) {
                 </h3>
                 ${placeholders.map((placeholder) => {
                   if (placeholder.fields.includes("device_id")) {
+                    const extraInfo = getPath(this._extraInfo, [
+                      type,
+                      placeholder.index,
+                    ]);
                     return html`
-                      <ha-device-picker
+                      <ha-area-devices-picker
                         .type=${type}
                         .placeholder=${placeholder}
-                        @change=${this._devicePicked}
+                        @value-changed=${this._devicePicked}
                         .hass=${this.hass}
+                        .area=${extraInfo ? extraInfo.area_id : undefined}
+                        .devices=${extraInfo && extraInfo.device_ids
+                          ? extraInfo.device_ids
+                          : undefined}
                         .includeDomains=${placeholder.domains}
                         .includeDeviceClasses=${placeholder.device_classes}
                         .label=${this._getLabel(
                           placeholder.domains,
                           placeholder.device_classes
                         )}
-                      ></ha-device-picker>
-                      ${(getPath(this._placeholderValues, [
-                        type,
-                        placeholder.index,
-                        "device_id",
-                      ]) &&
-                        placeholder.fields.includes("entity_id") &&
-                        getPath(this._placeholderValues, [
-                          type,
-                          placeholder.index,
-                          "entity_id",
-                        ]) === undefined) ||
-                      getPath(this._manualEntities, [
-                        type,
-                        placeholder.index,
-                        "manual",
-                      ]) === true
+                      ></ha-area-devices-picker>
+                      ${extraInfo && extraInfo.manualEntity
                         ? html`
-                            <ha-entity-picker
-                              id="device-entity-picker"
-                              .type=${type}
-                              .placeholder=${placeholder}
-                              @change=${this._entityPicked}
-                              .includeDomains=${placeholder.domains}
-                              .includeDeviceClasses=${placeholder.device_classes}
-                              .hass=${this.hass}
-                              .label=${this._getLabel(
-                                placeholder.domains,
-                                placeholder.device_classes
-                              )}
-                              .entityFilter=${(state: HassEntity) =>
-                                this._deviceEntityLookup[
-                                  this._placeholderValues[type][
-                                    placeholder.index
-                                  ].device_id
-                                ].includes(state.entity_id)}
-                            ></ha-entity-picker>
+                            <h3>
+                              One or more devices have more than one matching
+                              entity, please pick the one you want to use.
+                            </h3>
+                            ${Object.keys(extraInfo.manualEntity).map(
+                              (idx) => html`
+                                <ha-entity-picker
+                                  id="device-entity-picker"
+                                  .type=${type}
+                                  .placeholder=${placeholder}
+                                  .index=${idx}
+                                  @change=${this._entityPicked}
+                                  .includeDomains=${placeholder.domains}
+                                  .includeDeviceClasses=${placeholder.device_classes}
+                                  .hass=${this.hass}
+                                  .label=${`${this._getLabel(
+                                    placeholder.domains,
+                                    placeholder.device_classes
+                                  )} of device ${this._getDeviceName(
+                                    getPath(this._placeholderValues, [
+                                      type,
+                                      placeholder.index,
+                                      idx,
+                                      "device_id",
+                                    ])
+                                  )}`}
+                                  .entityFilter=${(state: HassEntity) => {
+                                    const devId = this._placeholderValues[type][
+                                      placeholder.index
+                                    ][idx].device_id;
+                                    return this._deviceEntityLookup[
+                                      devId
+                                    ].includes(state.entity_id);
+                                  }}
+                                ></ha-entity-picker>
+                              `
+                            )}
                           `
                         : ""}
                     `;
@@ -189,17 +244,72 @@ export class ThingTalkPlaceholders extends SubscribeMixin(LitElement) {
     `;
   }
 
+  private _getDeviceName(deviceId: string): string {
+    if (!this._devices) {
+      return "";
+    }
+    const foundDevice = this._devices.find((device) => device.id === deviceId);
+    if (!foundDevice) {
+      return "";
+    }
+    return foundDevice.name_by_user || foundDevice.name || "";
+  }
+
+  private _searchNames() {
+    if (this._search && this._areas && this._devices) {
+      this._search = false;
+      Object.entries(this.placeholders).forEach(([type, placeholders]) =>
+        placeholders.forEach((placeholder) => {
+          if (placeholder.name) {
+            const name = placeholder.name;
+            const foundArea = this._areas!.find((area) =>
+              area.name.toLowerCase().includes(name)
+            );
+            if (foundArea) {
+              applyPatch(
+                this._extraInfo,
+                [type, placeholder.index, "area_id"],
+                foundArea.area_id
+              );
+              this.requestUpdate("_extraInfo");
+              return;
+            }
+            const foundDevices = this._devices!.filter((device) => {
+              const deviceName = device.name_by_user || device.name;
+              if (!deviceName) {
+                return false;
+              }
+              return deviceName.toLowerCase().includes(name);
+            });
+            if (foundDevices.length) {
+              applyPatch(
+                this._extraInfo,
+                [type, placeholder.index, "device_ids"],
+                foundDevices.map((device) => device.id)
+              );
+              this.requestUpdate("_extraInfo");
+            }
+          }
+        })
+      );
+    }
+  }
+
   private get _isDone(): boolean {
     return Object.entries(this.placeholders).every(([type, placeholders]) =>
       placeholders.every((placeholder) =>
-        placeholder.fields.every(
-          (field) =>
-            getPath(this._placeholderValues, [
-              type,
-              placeholder.index,
-              field,
-            ]) !== undefined
-        )
+        placeholder.fields.every((field) => {
+          const entries: {
+            [key: number]: { device_id?: string; entity_id?: string };
+          } = getPath(this._placeholderValues, [type, placeholder.index]);
+          if (!entries) {
+            return false;
+          }
+          const values = Object.values(entries);
+          return values.every(
+            (entry) => entry[field] !== undefined && entry[field] !== ""
+          );
+        })
       )
     );
   }
@@ -212,76 +322,87 @@ export class ThingTalkPlaceholders extends SubscribeMixin(LitElement) {
     }`;
   }
 
-  private _devicePicked(ev: Event): void {
+  private _devicePicked(ev: CustomEvent): void {
+    const value: string[] = ev.detail.value;
+    if (!value) {
+      return;
+    }
     const target = ev.target as any;
     const placeholder = target.placeholder as Placeholder;
-    const value = target.value;
     const type = target.type;
-    applyPatch(
-      this._placeholderValues,
-      [type, placeholder.index, "device_id"],
-      value
-    );
+
+    if (!value.length && this._placeholderValues[type]) {
+      delete this._placeholderValues[type][placeholder.index];
+      delete this._extraInfo[type][placeholder.index];
+      this.requestUpdate("_placeholderValues");
+      return;
+    }
+
+    if (!value.length) {
+      return;
+    }
+
+    value.forEach((deviceId, index) => {
+      applyPatch(
+        this._placeholderValues,
+        [type, placeholder.index, index, "device_id"],
+        deviceId
+      );
+    });
+
     if (!placeholder.fields.includes("entity_id")) {
       return;
     }
-    if (value === "") {
-      delete this._placeholderValues[type][placeholder.index].entity_id;
-      if (this._deviceEntityPicker) {
-        this._deviceEntityPicker.value = undefined;
-      }
-      applyPatch(
-        this._manualEntities,
-        [type, placeholder.index, "manual"],
-        false
-      );
-      this.requestUpdate("_placeholderValues");
-      return;
-    }
-    const devEntities = this._deviceEntityLookup[value];
-    const entities = devEntities.filter((eid) => {
-      if (placeholder.device_classes) {
-        const stateObj = this.hass.states[eid];
-        if (!stateObj) {
-          return false;
+
+    applyPatch(this._extraInfo, [type, placeholder.index], undefined);
+
+    value.forEach((deviceId, index) => {
+      const devEntities = this._deviceEntityLookup[deviceId];
+
+      const entities = devEntities.filter((eid) => {
+        if (placeholder.device_classes) {
+          const stateObj = this.hass.states[eid];
+          if (!stateObj) {
+            return false;
+          }
+          return (
+            placeholder.domains.includes(computeDomain(eid)) &&
+            stateObj.attributes.device_class &&
+            placeholder.device_classes.includes(
+              stateObj.attributes.device_class
+            )
+          );
         }
-        return (
-          placeholder.domains.includes(computeDomain(eid)) &&
-          stateObj.attributes.device_class &&
-          placeholder.device_classes.includes(stateObj.attributes.device_class)
+        return placeholder.domains.includes(computeDomain(eid));
+      });
+      if (entities.length === 0) {
+        // Should not happen because we filter the device picker on domain
+        this._error = `No ${placeholder.domains
+          .map((domain) => this.hass.localize(`domain.${domain}`))
+          .join(", ")} entities found in this device.`;
+      } else if (entities.length === 1) {
+        applyPatch(
+          this._placeholderValues,
+          [type, placeholder.index, index, "entity_id"],
+          entities[0]
         );
+        this.requestUpdate("_placeholderValues");
+      } else {
+        delete this._placeholderValues[type][placeholder.index][index]
+          .entity_id;
+        applyPatch(
+          this._extraInfo,
+          [type, placeholder.index, "manualEntity", index],
+          true
+        );
+        this.requestUpdate("_placeholderValues");
       }
-      return placeholder.domains.includes(computeDomain(eid));
     });
-    if (entities.length === 0) {
-      // Should not happen because we filter the device picker on domain
-      this._error = `No ${placeholder.domains
-        .map((domain) => this.hass.localize(`domain.${domain}`))
-        .join(", ")} entities found in this device.`;
-    } else if (entities.length === 1) {
-      applyPatch(
-        this._placeholderValues,
-        [type, placeholder.index, "entity_id"],
-        entities[0]
-      );
-      applyPatch(
-        this._manualEntities,
-        [type, placeholder.index, "manual"],
-        false
-      );
-      this.requestUpdate("_placeholderValues");
-    } else {
-      delete this._placeholderValues[type][placeholder.index].entity_id;
-      if (this._deviceEntityPicker) {
-        this._deviceEntityPicker.value = undefined;
-      }
-      applyPatch(
-        this._manualEntities,
-        [type, placeholder.index, "manual"],
-        true
-      );
-      this.requestUpdate("_placeholderValues");
-    }
+
+    fireEvent(
+      this.shadowRoot!.querySelector("ha-paper-dialog")! as HTMLElement,
+      "iron-resize"
+    );
   }
 
   private _entityPicked(ev: Event): void {
@@ -289,9 +410,10 @@ export class ThingTalkPlaceholders extends SubscribeMixin(LitElement) {
     const placeholder = target.placeholder as Placeholder;
     const value = target.value;
     const type = target.type;
+    const index = target.index || 0;
     applyPatch(
       this._placeholderValues,
-      [type, placeholder.index, "entity_id"],
+      [type, placeholder.index, index, "entity_id"],
       value
     );
     this.requestUpdate("_placeholderValues");
