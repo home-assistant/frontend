656b8831677829023a8ece757f88ed3066c3d5c3
Bram Kragten <mail@bramkragten.nl>
2019-11-27 22:57:47 +0100

    Conditional chaining

--- src/entrypoints/core.ts
+++ src/entrypoints/core.ts
@@ -25,9 +25,7 @@ declare global {
 
 const isExternal =
   window.externalApp ||
-  (window.webkit &&
-    window.webkit.messageHandlers &&
-    window.webkit.messageHandlers.getExternalAuth) ||
+  window.webkit?.messageHandlers?.getExternalAuth ||
   location.search.includes("external_auth=1");
 
 const authProm = isExternal
