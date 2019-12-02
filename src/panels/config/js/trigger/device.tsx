bf4a320ac267008621dda8a89c447ab61ee13972
Bram Kragten <mail@bramkragten.nl>
2019-12-02 10:40:16 +0100

    prettier

--- src/panels/config/js/trigger/device.tsx
+++ src/panels/config/js/trigger/device.tsx
@@ -121,9 +121,7 @@ export default class DeviceTrigger extends AutomationComponent {
     // Returns a callback for ha-form to calculate labels per schema object
     return (schema) =>
       localize(
-        `ui.panel.config.automation.editor.triggers.type.device.extra_fields.${
-          schema.name
-        }`
+        `ui.panel.config.automation.editor.triggers.type.device.extra_fields.${schema.name}`
       ) || schema.name;
   }
 }
