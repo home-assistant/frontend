f333ef996a5ea5902e03552404a1500d1850b7a0
Marius <33354141+Mariusthvdb@users.noreply.github.com>
2019-12-09 06:58:41 +0100

    change tp partly-cloudy
    following discussion on home-assistant/home-assistant#29569 this aims use partly-cloudy all through HA's weather integrations and backend. Will Pr all integrations and documentation accordingly

--- src/cards/ha-weather-card.js
+++ src/cards/ha-weather-card.js
@@ -286,7 +286,7 @@ class HaWeatherCard extends LocalizeMixin(EventsMixin(PolymerElement)) {
       hail: "hass:weather-hail",
       lightning: "hass:weather-lightning",
       "lightning-rainy": "hass:weather-lightning-rainy",
-      partlycloudy: "hass:weather-partly-cloudy",
+      "partly-cloudy": "hass:weather-partly-cloudy",
       pouring: "hass:weather-pouring",
       rainy: "hass:weather-rainy",
       snowy: "hass:weather-snowy",
