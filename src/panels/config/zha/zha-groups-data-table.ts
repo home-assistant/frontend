de653e1f7b8eace0492c7c72fc4a25b22e17a04c
David F. Mulcahey <david.mulcahey@me.com>
2019-12-23 10:46:34 -0500

    Add Zigbee group viewing to ZHA config panel (#4365)
    * add ability to view zigbee groups
    
    * review comments
    
    * remove selectable until used

---
+++ src/panels/config/zha/zha-groups-data-table.ts
@@ -0,0 +1,85 @@
+import "../../../components/data-table/ha-data-table";
+import "../../../components/entity/ha-state-icon";
+
+import memoizeOne from "memoize-one";
+
+import {
+  LitElement,
+  html,
+  TemplateResult,
+  property,
+  customElement,
+} from "lit-element";
+import { HomeAssistant } from "../../../types";
+// tslint:disable-next-line
+import { DataTableColumnContainer } from "../../../components/data-table/ha-data-table";
+// tslint:disable-next-line
+import { ZHAGroup, ZHADevice } from "../../../data/zha";
+import { formatAsPaddedHex } from "./functions";
+
+export interface GroupRowData extends ZHAGroup {
+  group?: GroupRowData;
+  id?: number;
+}
+
+@customElement("zha-groups-data-table")
+export class ZHAGroupsDataTable extends LitElement {
+  @property() public hass!: HomeAssistant;
+  @property() public narrow = false;
+  @property() public groups: ZHAGroup[] = [];
+
+  private _columns = memoizeOne(
+    (narrow: boolean): DataTableColumnContainer =>
+      narrow
+        ? {
+            name: {
+              title: "Group",
+              sortable: true,
+              filterable: true,
+              direction: "asc",
+            },
+          }
+        : {
+            name: {
+              title: this.hass.localize("ui.panel.config.zha.groups.groups"),
+              sortable: true,
+              filterable: true,
+              direction: "asc",
+            },
+            group_id: {
+              title: this.hass.localize("ui.panel.config.zha.groups.group_id"),
+              template: (groupId: number) => {
+                return html`
+                  ${formatAsPaddedHex(groupId)}
+                `;
+              },
+              sortable: true,
+            },
+            members: {
+              title: this.hass.localize("ui.panel.config.zha.groups.members"),
+              template: (members: ZHADevice[]) => {
+                return html`
+                  ${members.length}
+                `;
+              },
+              sortable: true,
+            },
+          }
+  );
+
+  protected render(): TemplateResult {
+    return html`
+      <ha-data-table
+        .columns=${this._columns(this.narrow)}
+        .data=${this.groups}
+        .id=${"group_id"}
+      ></ha-data-table>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "zha-groups-data-table": ZHAGroupsDataTable;
+  }
+}
