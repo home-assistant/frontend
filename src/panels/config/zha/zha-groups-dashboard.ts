9fce34b840464c03e9e4974d86ca725c19a21146
David F. Mulcahey <david.mulcahey@me.com>
2019-12-23 10:46:34 -0500

    Add Zigbee group viewing to ZHA config panel (#4365)
    * add ability to view zigbee groups
    
    * review comments
    
    * remove selectable until used

---
+++ src/panels/config/zha/zha-groups-dashboard.ts
@@ -0,0 +1,77 @@
+import "../../../layouts/hass-subpage";
+import "./zha-groups-data-table";
+
+import {
+  LitElement,
+  html,
+  TemplateResult,
+  property,
+  customElement,
+  CSSResult,
+  css,
+} from "lit-element";
+import { HomeAssistant } from "../../../types";
+import { ZHAGroup, fetchGroups } from "../../../data/zha";
+import { sortZHAGroups } from "./functions";
+
+@customElement("zha-groups-dashboard")
+export class ZHAGroupsDashboard extends LitElement {
+  @property() public hass!: HomeAssistant;
+  @property() public narrow = false;
+  @property() public _groups!: ZHAGroup[];
+
+  public connectedCallback(): void {
+    super.connectedCallback();
+    this._fetchGroups();
+  }
+
+  protected render(): TemplateResult {
+    return html`
+      <hass-subpage
+        header=${this.hass.localize(
+          "ui.panel.config.zha.groups.zha_zigbee_groups"
+        )}
+      >
+        <div class="content">
+          <zha-groups-data-table
+            .hass=${this.hass}
+            .narrow=${this.narrow}
+            .groups=${this._groups}
+            class="table"
+          ></zha-groups-data-table>
+        </div>
+      </hass-subpage>
+    `;
+  }
+
+  private async _fetchGroups() {
+    this._groups = (await fetchGroups(this.hass!)).sort(sortZHAGroups);
+  }
+
+  static get styles(): CSSResult[] {
+    return [
+      css`
+        .content {
+          padding: 4px;
+        }
+        zha-groups-data-table {
+          width: 100%;
+        }
+        .button {
+          float: right;
+        }
+
+        .table {
+          height: 200px;
+          overflow: auto;
+        }
+      `,
+    ];
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "zha-groups-dashboard": ZHAGroupsDashboard;
+  }
+}
