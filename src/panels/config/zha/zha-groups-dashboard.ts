ff0b1881e2ebbfc8d0357edd55db33536ec89c81
David F. Mulcahey <david.mulcahey@me.com>
2019-12-24 08:12:02 -0500

    Add Zigbee group removal to the ZHA config panel (#4376)
    * add remove groups function
    
    * add ability to remove groups
    
    * translations
    
    * review comments
    
    * review comments
    
    * review comments

--- src/panels/config/zha/zha-groups-dashboard.ts
+++ src/panels/config/zha/zha-groups-dashboard.ts
@@ -9,20 +9,37 @@ import {
   customElement,
   CSSResult,
   css,
+  PropertyValues,
 } from "lit-element";
 import { HomeAssistant } from "../../../types";
-import { ZHAGroup, fetchGroups } from "../../../data/zha";
+import { ZHAGroup, fetchGroups, removeGroups } from "../../../data/zha";
 import { sortZHAGroups } from "./functions";
+import { SelectionChangedEvent } from "../../../components/data-table/ha-data-table";
+import "@material/mwc-button";
+import "@polymer/paper-spinner/paper-spinner";
 
 @customElement("zha-groups-dashboard")
 export class ZHAGroupsDashboard extends LitElement {
   @property() public hass!: HomeAssistant;
   @property() public narrow = false;
-  @property() public _groups!: ZHAGroup[];
+  @property() public _groups?: ZHAGroup[];
+  @property() private _processingRemove: boolean = false;
+  @property() private _selectedGroupsToRemove: number[] = [];
+  private _firstUpdatedCalled: boolean = false;
 
   public connectedCallback(): void {
     super.connectedCallback();
-    this._fetchGroups();
+    if (this.hass && this._firstUpdatedCalled) {
+      this._fetchGroups();
+    }
+  }
+
+  protected firstUpdated(changedProperties: PropertyValues): void {
+    super.firstUpdated(changedProperties);
+    if (this.hass) {
+      this._fetchGroups();
+    }
+    this._firstUpdatedCalled = true;
   }
 
   protected render(): TemplateResult {
@@ -33,12 +50,41 @@ export class ZHAGroupsDashboard extends LitElement {
         )}
       >
         <div class="content">
-          <zha-groups-data-table
-            .hass=${this.hass}
-            .narrow=${this.narrow}
-            .groups=${this._groups}
-            class="table"
-          ></zha-groups-data-table>
+          ${this._groups
+            ? html`
+                <zha-groups-data-table
+                  .hass=${this.hass}
+                  .narrow=${this.narrow}
+                  .groups=${this._groups}
+                  .selectable=${true}
+                  @selection-changed=${this._handleRemoveSelectionChanged}
+                  class="table"
+                ></zha-groups-data-table>
+              `
+            : html`
+                <paper-spinner
+                  active
+                  alt=${this.hass!.localize("ui.common.loading")}
+                ></paper-spinner>
+              `}
+        </div>
+        <div class="paper-dialog-buttons">
+          <mwc-button
+            ?disabled="${!this._selectedGroupsToRemove.length ||
+              this._processingRemove}"
+            @click="${this._removeGroup}"
+            class="button"
+          >
+            <paper-spinner
+              ?active="${this._processingRemove}"
+              alt=${this.hass!.localize(
+                "ui.panel.config.zha.groups.removing_groups"
+              )}
+            ></paper-spinner>
+            ${this.hass!.localize(
+              "ui.panel.config.zha.groups.remove_groups"
+            )}</mwc-button
+          >
         </div>
       </hass-subpage>
     `;
@@ -46,6 +92,27 @@ export class ZHAGroupsDashboard extends LitElement {
 
   private async _fetchGroups() {
     this._groups = (await fetchGroups(this.hass!)).sort(sortZHAGroups);
+  }
+
+  private _handleRemoveSelectionChanged(ev: CustomEvent): void {
+    const changedSelection = ev.detail as SelectionChangedEvent;
+    const groupId = Number(changedSelection.id);
+    if (changedSelection.selected) {
+      this._selectedGroupsToRemove.push(groupId);
+    } else {
+      const index = this._selectedGroupsToRemove.indexOf(groupId);
+      if (index !== -1) {
+        this._selectedGroupsToRemove.splice(index, 1);
+      }
+    }
+    this._selectedGroupsToRemove = [...this._selectedGroupsToRemove];
+  }
+
+  private async _removeGroup(): Promise<void> {
+    this._processingRemove = true;
+    this._groups = await removeGroups(this.hass, this._selectedGroupsToRemove);
+    this._selectedGroupsToRemove = [];
+    this._processingRemove = false;
   }
 
   static get styles(): CSSResult[] {
@@ -60,10 +127,27 @@ export class ZHAGroupsDashboard extends LitElement {
         .button {
           float: right;
         }
-
         .table {
           height: 200px;
           overflow: auto;
+        }
+        mwc-button paper-spinner {
+          width: 14px;
+          height: 14px;
+          margin-right: 20px;
+        }
+        paper-spinner {
+          display: none;
+        }
+        paper-spinner[active] {
+          display: block;
+        }
+        .paper-dialog-buttons {
+          align-items: flex-end;
+          padding: 8px;
+        }
+        .paper-dialog-buttons .warning {
+          --mdc-theme-primary: var(--google-red-500);
         }
       `,
     ];
