1048eebb74c88125ac903b3f03e59773554d7ef3
David F. Mulcahey <david.mulcahey@me.com>
2020-01-08 12:35:21 -0500

    Rework the ZHA config panel (#4415)
    * convert zha config panel to tabs
    
    * add spacer to prevent combobox from hitting bottom
    
    * break clusters out into their own section
    
    * cleanup buttons
    
    * remove header
    
    * make devices default tab
    
    * convert from tabs to a list view
    
    * convert to table on dashboard
    
    * fix anchor on mobile safari
    
    * cleanup CSS to fix display on mobile
    
    * cleanup card css
    
    * more css cleanup
    
    * fix group page
    
    * remove translations changes
    
    * Update src/panels/config/zha/zha-clusters.ts
    
    Co-Authored-By: Bram Kragten <mail@bramkragten.nl>
    
    * Update src/panels/config/zha/zha-config-dashboard.ts
    
    Co-Authored-By: Bram Kragten <mail@bramkragten.nl>
    
    * Update src/panels/config/zha/zha-device-page.ts
    
    Co-Authored-By: Bram Kragten <mail@bramkragten.nl>
    
    * Update src/panels/config/zha/zha-groups-dashboard.ts
    
    Co-Authored-By: Bram Kragten <mail@bramkragten.nl>
    
    * review comments
    
    * fix dangling quote after commit suggestion
    
    * css cleanup
    
    * remove flex rules
    
    * remove flex rules
    
    * css  cleanup
    
    * remove dialog per review comments
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>

--- src/panels/config/zha/zha-config-dashboard-router.ts
+++ src/panels/config/zha/zha-config-dashboard-router.ts
@@ -1,29 +1,34 @@
-import "../../../layouts/hass-loading-screen";
-
-import { customElement, property } from "lit-element";
-
 import {
   HassRouterPage,
   RouterOptions,
 } from "../../../layouts/hass-router-page";
+import { customElement, property } from "lit-element";
 import { HomeAssistant } from "../../../types";
 
-@customElement("zha-config-panel")
-class ZHAConfigPanel extends HassRouterPage {
+@customElement("zha-config-dashboard-router")
+class ZHAConfigDashboardRouter extends HassRouterPage {
   @property() public hass!: HomeAssistant;
   @property() public isWide!: boolean;
   @property() public narrow!: boolean;
 
   protected routerOptions: RouterOptions = {
-    defaultPage: "configuration",
+    defaultPage: "dashboard",
     cacheAll: true,
     preloadAll: true,
+    showLoading: true,
     routes: {
-      configuration: {
-        tag: "ha-config-zha",
+      dashboard: {
+        tag: "zha-config-dashboard",
         load: () =>
           import(
-            /* webpackChunkName: "zha-configuration-page" */ "./ha-config-zha"
+            /* webpackChunkName: "zha-config-dashboard" */ "./zha-config-dashboard"
+          ),
+      },
+      device: {
+        tag: "zha-device-page",
+        load: () =>
+          import(
+            /* webpackChunkName: "zha-devices-page" */ "./zha-device-page"
           ),
       },
       add: {
@@ -62,12 +67,14 @@ class ZHAConfigPanel extends HassRouterPage {
     el.narrow = this.narrow;
     if (this._currentPage === "group") {
       el.groupId = this.routeTail.path.substr(1);
+    } else if (this._currentPage === "device") {
+      el.ieee = this.routeTail.path.substr(1);
     }
   }
 }
 
 declare global {
   interface HTMLElementTagNameMap {
-    "zha-config-panel": ZHAConfigPanel;
+    "zha-config-dashboard-router": ZHAConfigDashboardRouter;
   }
 }
