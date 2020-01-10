1e3ca4077e914397e3f2ec29ad8049e0e62c473b
Marius <33354141+Mariusthvdb@users.noreply.github.com>
2020-01-10 14:07:39 +0100

    Update src/panels/lovelace/components/hui-generic-entity-row.ts
    Co-Authored-By: Bram Kragten <mail@bramkragten.nl>

--- src/panels/lovelace/components/hui-generic-entity-row.ts
+++ src/panels/lovelace/components/hui-generic-entity-row.ts
@@ -114,7 +114,7 @@ class HuiGenericEntityRow extends LitElement {
                     ></ha-relative-time>
                   `
                 : this.hass.localize(
-                    "ui.panel.lovelace.cards.entities.never_triggered"
+                    "ui.panel.lovelace.card.entities.never_triggered"
                   )
               : ""}
           </div>
