a4c72ca0a543e92d539a923ef4a8ef9b1a326d4d
Anton Verburg <info@tec-v.nl>
2020-01-22 15:01:02 -0600

    ♻️ convert more-info-script to LitElement/TypeScript (#4545)
    * ♻️ convert more-info-script to Lit/TypeScript
    
    * ♻️ convert more-info-script to LitElement/TypeScript
    
    * 👌 use relative time

---
+++ src/dialogs/more-info/controls/more-info-script.ts
@@ -0,0 +1,46 @@
+import {
+  LitElement,
+  html,
+  TemplateResult,
+  property,
+  customElement,
+} from "lit-element";
+import { HassEntity } from "home-assistant-js-websocket";
+
+import { HomeAssistant } from "../../../types";
+
+import "../../../components/ha-relative-time";
+
+@customElement("more-info-script")
+class MoreInfoScript extends LitElement {
+  @property() public hass!: HomeAssistant;
+  @property() public stateObj?: HassEntity;
+
+  protected render(): TemplateResult | void {
+    if (!this.hass || !this.stateObj) {
+      return html``;
+    }
+
+    return html`
+      <div>
+        ${this.hass.localize(
+          "ui.dialogs.more_info_control.script.last_triggered"
+        )}:
+        ${this.stateObj.attributes.last_triggered
+          ? html`
+              <ha-relative-time
+                .hass=${this.hass}
+                .datetime=${this.stateObj.attributes.last_triggered}
+              ></ha-relative-time>
+            `
+          : this.hass.localize("ui.components.relative_time.never")}
+      </div>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "more-info-script": MoreInfoScript;
+  }
+}
