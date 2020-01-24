bd75b8ec58602fa600d7b2c363e21f98d0e2a1ea
Ian Richardson <iantrich@gmail.com>
2020-01-23 22:28:35 -0600

    ⏪ revert fix for card editor overflow menu selection

--- src/panels/lovelace/components/hui-card-options.ts
+++ src/panels/lovelace/components/hui-card-options.ts
@@ -62,7 +62,6 @@ export class HuiCardOptions extends LitElement {
             horizontal-align="right"
             vertical-align="bottom"
             vertical-offset="40"
-            close-on-activate
           >
             <paper-icon-button
               icon="hass:dots-vertical"
