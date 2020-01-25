da050fafc3192ff6132234be226a5c39c91a0a86
Timmo <contact@timmo.xyz>
2020-01-25 16:48:29 +0000

    Merge Confirmation Dialogs into Alert, Confirmation or Prompt (#4114)
    * :sparkles: Add alert dialog
    
    :hammer: Add alert dialogs
    
    :hammer: add more
    
    :hammer: Add more
    
    :hammer: Enhance check
    
    Co-Authored-By: Bram Kragten <mail@bramkragten.nl>
    
    :fire: Combine confirmation into alert dialog
    
    :hammer: Replace confirmation calls with alert dialog
    
    :pencil2: Remove 3
    
    :hammer: Add prompt logic
    
    Rename to generic
    
    Rename and add new params for alert, confirmation and prompt
    
    Renames and prop changes
    
    Rename and props
    
    Cleanup
    
    Setup prompt
    
    Wording
    
    Use text for prompt
    
    Add prompts and confirmation on delete user
    
    Rename
    
    Rename
    
    Rename imports
    
    Fix parms change
    
    Only use default for confirmation
    
    Co-Authored-By: Bram Kragten <mail@bramkragten.nl>
    
    Update src/dialogs/generic/dialog-box.ts
    
    Co-Authored-By: Bram Kragten <mail@bramkragten.nl>
    
    Update
    
    change text
    
    Add autofocus
    
    Merge show dialogs into one generic
    
    Add automation delete confirmation
    
    Modal
    
    Remove deleted file
    
    Add delete donfirm to script
    
    Fix error with tslint
    
    Fix from rebase
    
    Fix from rebase
    
    Fix from rebase
    
    * 🔧 Split dialog functions
    
    * 🔧 Fix from rebase
    
    * 🔧 More fixes
    
    * 🔧 Fix
    
    * 🔧 Apply suggestions from code review
    
    Co-Authored-By: Bram Kragten <mail@bramkragten.nl>
    
    * 🔧 Update from suggestion
    
    * 🔧 Renames and cleanup
    
    * 🔧 Camelcase
    
    Co-Authored-By: Bram Kragten <mail@bramkragten.nl>
    
    * 🔧 camel case
    
    Co-Authored-By: Bram Kragten <mail@bramkragten.nl>
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>

--- src/panels/config/automation/ha-automation-editor.ts
+++ src/panels/config/automation/ha-automation-editor.ts
@@ -25,7 +25,10 @@ import {
   Trigger,
 } from "../../../data/automation";
 import { Action } from "../../../data/script";
-import { showConfirmationDialog } from "../../../dialogs/confirmation/show-dialog-confirmation";
+import {
+  showAlertDialog,
+  showConfirmationDialog,
+} from "../../../dialogs/generic/show-dialog-box";
 import "../../../layouts/ha-app-layout";
 import { haStyle } from "../../../resources/styles";
 import { HomeAssistant } from "../../../types";
@@ -65,7 +68,7 @@ export class HaAutomationEditor extends LitElement {
                       "ui.panel.config.automation.picker.delete_automation"
                     )}"
                     icon="hass:delete"
-                    @click=${this._delete}
+                    @click=${this._deleteConfirm}
                   ></paper-icon-button>
                 `}
           </app-toolbar>
@@ -252,17 +255,18 @@ export class HaAutomationEditor extends LitElement {
             this._config = config;
           },
           (resp) => {
-            alert(
-              resp.status_code === 404
-                ? this.hass.localize(
-                    "ui.panel.config.automation.editor.load_error_not_editable"
-                  )
-                : this.hass.localize(
-                    "ui.panel.config.automation.editor.load_error_unknown",
-                    "err_no",
-                    resp.status_code
-                  )
-            );
+            showAlertDialog(this, {
+              text:
+                resp.status_code === 404
+                  ? this.hass.localize(
+                      "ui.panel.config.automation.editor.load_error_not_editable"
+                    )
+                  : this.hass.localize(
+                      "ui.panel.config.automation.editor.load_error_unknown",
+                      "err_no",
+                      resp.status_code
+                    ),
+            });
             history.back();
           }
         );
@@ -326,8 +330,8 @@ export class HaAutomationEditor extends LitElement {
         text: this.hass!.localize(
           "ui.panel.config.automation.editor.unsaved_confirm"
         ),
-        confirmBtnText: this.hass!.localize("ui.common.yes"),
-        cancelBtnText: this.hass!.localize("ui.common.no"),
+        confirmText: this.hass!.localize("ui.common.yes"),
+        dismissText: this.hass!.localize("ui.common.no"),
         confirm: () => history.back(),
       });
     } else {
@@ -335,14 +339,18 @@ export class HaAutomationEditor extends LitElement {
     }
   }
 
+  private async _deleteConfirm() {
+    showConfirmationDialog(this, {
+      text: this.hass.localize(
+        "ui.panel.config.automation.picker.delete_confirm"
+      ),
+      confirmText: this.hass!.localize("ui.common.yes"),
+      dismissText: this.hass!.localize("ui.common.no"),
+      confirm: () => this._delete(),
+    });
+  }
+
   private async _delete() {
-    if (
-      !confirm(
-        this.hass.localize("ui.panel.config.automation.picker.delete_confirm")
-      )
-    ) {
-      return;
-    }
     await deleteAutomation(this.hass, this.automation.attributes.id!);
     history.back();
   }
