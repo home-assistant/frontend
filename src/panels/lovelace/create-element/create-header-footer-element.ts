e1fd28e1ee1dfd7adc132070f3237466823a2361
Anton Verburg <info@tec-v.nl>
2020-01-27 00:34:08 -0800

    Add buttons header-footer (#4601)
    * Add buttons header-footer
    
    * Simplify
    
    * Update src/panels/lovelace/header-footer/hui-buttons-header-footer.ts
    
    Co-Authored-By: Ian Richardson <iantrich@gmail.com>
    
    * Address comments
    
    Co-authored-by: Ian Richardson <iantrich@gmail.com>

--- src/panels/lovelace/create-element/create-header-footer-element.ts
+++ src/panels/lovelace/create-element/create-header-footer-element.ts
@@ -1,8 +1,9 @@
 import "../header-footer/hui-picture-header-footer";
+import "../header-footer/hui-buttons-header-footer";
 import { LovelaceHeaderFooterConfig } from "../header-footer/types";
 import { createLovelaceElement } from "./create-element-base";
 
-const SPECIAL_TYPES = new Set(["picture"]);
+const SPECIAL_TYPES = new Set(["picture", "buttons"]);
 
 export const createHeaderFooterElement = (config: LovelaceHeaderFooterConfig) =>
   createLovelaceElement("header-footer", config, SPECIAL_TYPES);
