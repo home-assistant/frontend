54f756e3fdfd2cae9595082ccba1f4e330590cec
Bram Kragten <mail@bramkragten.nl>
2020-01-30 17:15:46 +0100

    Fix flex issues on google and alexa settings page

--- src/panels/config/cloud/alexa/cloud-alexa.ts
+++ src/panels/config/cloud/alexa/cloud-alexa.ts
@@ -337,17 +337,13 @@ class CloudAlexa extends LitElement {
         padding: 0 8px;
       }
       .content {
-        display: flex;
-        flex-wrap: wrap;
-        padding: 4px;
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+        grid-gap: 8px 8px;
+        padding: 8px;
       }
       ha-switch {
         clear: both;
-      }
-      ha-card {
-        margin: 4px;
-        width: 300px;
-        flex-grow: 1;
       }
       .card-content {
         padding-bottom: 12px;
