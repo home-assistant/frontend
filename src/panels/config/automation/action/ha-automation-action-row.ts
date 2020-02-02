fd2c1ceb97a400dac99c256da51b52146d6fa3ca
Timmo <contact@timmo.xyz>
2020-02-02 13:07:28 +0000

    Swap automation confirmation dialogs

--- src/panels/config/automation/action/ha-automation-action-row.ts
+++ src/panels/config/automation/action/ha-automation-action-row.ts
@@ -16,6 +16,7 @@ import { dynamicElement } from "../../../../common/dom/dynamic-element-directive
 import { fireEvent } from "../../../../common/dom/fire_event";
 import "../../../../components/ha-card";
 import { HomeAssistant } from "../../../../types";
+import { showConfirmationDialog } from "../../../../dialogs/generic/show-dialog-box";
 
 import { Action } from "../../../../data/script";
 
@@ -206,15 +207,16 @@ export default class HaAutomationActionRow extends LitElement {
   }
 
   private _onDelete() {
-    if (
-      confirm(
-        this.hass.localize(
-          "ui.panel.config.automation.editor.actions.delete_confirm"
-        )
-      )
-    ) {
-      fireEvent(this, "value-changed", { value: null });
-    }
+    showConfirmationDialog(this, {
+      text: this.hass.localize(
+        "ui.panel.config.automation.editor.actions.delete_confirm"
+      ),
+      dismissText: this.hass.localize("ui.common.no"),
+      confirmText: this.hass.localize("ui.common.yes"),
+      confirm: () => {
+        fireEvent(this, "value-changed", { value: null });
+      },
+    });
   }
 
   private _typeChanged(ev: CustomEvent) {
