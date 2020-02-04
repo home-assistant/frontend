f1a1654371828535f31ebc7b85fa563390c75a8d
Joakim Sørensen <joasoe@gmail.com>
2020-02-04 12:36:09 +0100

    Use icon image where available (#4721)
    * Use iconimg where available
    
    * Adjust margin for icon
    
    * remove log
    
    * Fix property casing
    
    * Add blue topbar, and generalize properties
    
    * Inline checks
    
    * inline property functions
    
    * Limit compute
    
    * Linting
    
    * lovercase const
    
    * Review comments and move update dot to line
    
    * Add roboto font to hassio
    
    * Fix update and stopped styles colliding
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>

--- hassio/src/resources/hassio-style.ts
+++ hassio/src/resources/hassio-style.ts
@@ -17,11 +17,11 @@ export const hassioStyle = css`
     font-weight: var(--paper-font-headline_-_font-weight);
     letter-spacing: var(--paper-font-headline_-_letter-spacing);
     line-height: var(--paper-font-headline_-_line-height);
-    padding-left: 16px;
+    padding-left: 8px;
   }
   .description {
     margin-top: 4px;
-    padding-left: 16px;
+    padding-left: 8px;
   }
   .card-group {
     display: grid;
