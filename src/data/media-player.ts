da83965b88282980dae2450cf9207aea06bf987c
Zack Arnett <arnett.zackary@gmail.com>
2020-02-11 13:11:25 -0500

    Update Media Card to check for supported features

--- src/data/media-player.ts
+++ src/data/media-player.ts
@@ -3,8 +3,17 @@ import { HomeAssistant } from "../types";
 import { timeCachePromiseFunc } from "../common/util/time-cache-function-promise";
 
 export const SUPPORT_PAUSE = 1;
+export const SUPPORT_VOLUME_SET = 4;
+export const SUPPORT_VOLUME_MUTE = 8;
+export const SUPPORT_PREVIOUS_TRACK = 16;
 export const SUPPORT_NEXT_TRACK = 32;
+export const SUPPORT_TURN_ON = 128;
+export const SUPPORT_TURN_OFF = 256;
+export const SUPPORT_PLAY_MEDIA = 512;
+export const SUPPORT_VOLUME_BUTTONS = 1024;
+export const SUPPORT_SELECT_SOURCE = 2048;
 export const SUPPORTS_PLAY = 16384;
+export const SUPPORT_SELECT_SOUND_MODE = 65536;
 export const OFF_STATES = ["off", "idle"];
 
 export interface MediaPlayerThumbnail {
