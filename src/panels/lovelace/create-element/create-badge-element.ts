c0a62aa7e77226e026f98d2562be526769c9bec4
Paulus Schoutsen <balloob@gmail.com>
2020-02-13 21:13:48 -0800

    Add option to lazy load cards (#4857)
    * Add option to lazy load cards
    
    * Lazy load header/footer elements
    
    * Lazy load rows
    
    * Clean up params
    
    * Rename last var

--- src/panels/lovelace/create-element/create-badge-element.ts
+++ src/panels/lovelace/create-element/create-badge-element.ts
@@ -1,10 +1,19 @@
-import "../badges/hui-entity-filter-badge";
 import "../badges/hui-state-label-badge";
 
 import { LovelaceBadgeConfig } from "../../../data/lovelace";
 import { createLovelaceElement } from "./create-element-base";
 
-const BADGE_TYPES = new Set(["entity-filter", "error", "state-label"]);
+const ALWAYS_LOADED_TYPES = new Set(["error", "state-label"]);
+const LAZY_LOAD_TYPES = {
+  "entity-filter": () => import("../badges/hui-entity-filter-badge"),
+};
 
 export const createBadgeElement = (config: LovelaceBadgeConfig) =>
-  createLovelaceElement("badge", config, BADGE_TYPES, undefined, "state-label");
+  createLovelaceElement(
+    "badge",
+    config,
+    ALWAYS_LOADED_TYPES,
+    LAZY_LOAD_TYPES,
+    undefined,
+    "state-label"
+  );
