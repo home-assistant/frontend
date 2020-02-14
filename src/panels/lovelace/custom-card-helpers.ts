e8c9118e216c36b59262b315dc0251a492c7274b
Ian Richardson <iantrich@gmail.com>
2020-02-13 21:03:25 -0600

    address comments

--- src/panels/lovelace/custom-card-helpers.ts
+++ src/panels/lovelace/custom-card-helpers.ts
@@ -1,11 +1,5 @@
-import { createRowElement as createRowElement_ } from "./create-element/create-row-element";
-import { createCardElement as createCardElement_ } from "./create-element/create-card-element";
-import { createBadgeElement as createBadgeElement_ } from "./create-element/create-badge-element";
-import { createHeaderFooterElement as createHeaderFooterElement_ } from "./create-element/create-header-footer-element";
-import { createHuiElement as createHuiElement_ } from "./create-element/create-hui-element";
-
-export const createRowElement = createRowElement_;
-export const createCardElement = createCardElement_;
-export const createBadgeElement = createBadgeElement_;
-export const createHeaderFooterElement = createHeaderFooterElement_;
-export const createHuiElement = createHuiElement_;
+export { createRowElement } from "./create-element/create-row-element";
+export { createCardElement } from "./create-element/create-card-element";
+export { createBadgeElement } from "./create-element/create-badge-element";
+export { createHeaderFooterElement } from "./create-element/create-header-footer-element";
+export { createHuiElement } from "./create-element/create-hui-element";
