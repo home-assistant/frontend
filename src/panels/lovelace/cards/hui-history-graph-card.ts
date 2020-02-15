cfd359802328ac35e7c7d4db26e2ace1c727964b
Zack Arnett <arnett.zackary@gmail.com>
2020-02-15 11:29:05 -0500

    Conversion to TS

--- src/panels/lovelace/cards/hui-history-graph-card.ts
+++ src/panels/lovelace/cards/hui-history-graph-card.ts
@@ -1,99 +1,169 @@
-import { html } from "@polymer/polymer/lib/utils/html-tag";
-import { PolymerElement } from "@polymer/polymer/polymer-element";
+import {
+  html,
+  LitElement,
+  TemplateResult,
+  customElement,
+  property,
+  css,
+  CSSResult,
+  PropertyValues,
+} from "lit-element";
+import { classMap } from "lit-html/directives/class-map";
 
 import "../../../components/ha-card";
 import "../../../components/state-history-charts";
 import "../../../data/ha-state-history-data";
 
+import { getRecentWithCache, CacheConfig } from "../../../data/cached-history";
 import { processConfigEntities } from "../common/process-config-entities";
+import { HomeAssistant } from "../../../types";
+import { HistoryGraphCardConfig } from "./types";
+import { LovelaceCard } from "../types";
+import { EntityConfig } from "../entity-rows/types";
 
-class HuiHistoryGraphCard extends PolymerElement {
-  static async getConfigElement() {
+@customElement("hui-history-graph-card")
+export class HuiHistoryGraphCard extends LitElement implements LovelaceCard {
+  public static async getConfigElement() {
     await import(
       /* webpackChunkName: "hui-history-graph-card-editor" */ "../editor/config-elements/hui-history-graph-card-editor"
     );
     return document.createElement("hui-history-graph-card-editor");
   }
 
-  static getStubConfig() {
+  public static getStubConfig() {
     return { entities: [] };
   }
 
-  static get template() {
-    return html`
-      <style>
-        .content {
-          padding: 16px;
-        }
-        [header] .content {
-          padding-top: 0;
-        }
-      </style>
+  @property() public hass?: HomeAssistant;
+  @property() public _stateHistory?: any;
+  @property() private _config?: HistoryGraphCardConfig;
+  private _configEntities?: EntityConfig[];
+  private _names: { [key: string]: string } = {};
+  private _cacheConfig?: CacheConfig;
+  private _interval?: number;
 
-      <ha-card header$="[[_config.title]]">
-        <div class="content">
-          <ha-state-history-data
-            hass="[[hass]]"
-            filter-type="recent-entity"
-            entity-id="[[_entities]]"
-            data="{{_stateHistory}}"
-            is-loading="{{_stateHistoryLoading}}"
-            cache-config="[[_cacheConfig]]"
-          ></ha-state-history-data>
+  public getCardSize(): number {
+    return 4;
+  }
+
+  public setConfig(config: HistoryGraphCardConfig): void {
+    if (!config.entities) {
+      throw new Error("Entities must be defined");
+    }
+
+    if (config.entities && !Array.isArray(config.entities)) {
+      throw new Error("Entities need to be an array");
+    }
+
+    this._config = { theme: "default", ...config };
+    this._configEntities = config.entities
+      ? processConfigEntities(config.entities)
+      : [];
+
+    const _entities: string[] = [];
+
+    this._configEntities.forEach((entity) => {
+      _entities.push(entity.entity);
+      if (entity.name) {
+        this._names[entity.entity] = entity.name;
+      }
+    });
+
+    this._cacheConfig = {
+      cacheKey: _entities.join(),
+      hoursToShow: config.hours_to_show || 24,
+      refresh: config.refresh_interval || 0,
+    };
+  }
+
+  public disconnectedCallback(): void {
+    super.disconnectedCallback();
+    this._clearInterval();
+  }
+
+  protected updated(changedProps: PropertyValues) {
+    super.updated(changedProps);
+    if (!this._config || !this.hass || !this._cacheConfig) {
+      return;
+    }
+
+    const oldConfig = changedProps.get("_config") as
+      | HistoryGraphCardConfig
+      | undefined;
+
+    if (oldConfig !== this._config) {
+      this._getStateHistory();
+      this._clearInterval();
+    }
+
+    if (!this._interval && this._cacheConfig.refresh) {
+      this._interval = window.setInterval(() => {
+        this._getStateHistory();
+      }, this._cacheConfig.refresh * 1000);
+    }
+  }
+
+  protected render(): TemplateResult {
+    if (!this.hass || !this._config) {
+      return html``;
+    }
+
+    return html`
+      <ha-card .header="${this._config.title}">
+        <div
+          class="${classMap({
+            content: true,
+            "has-header": !!this._config.title,
+          })}"
+        >
           <state-history-charts
-            hass="[[hass]]"
-            history-data="[[_stateHistory]]"
-            is-loading-data="[[_stateHistoryLoading]]"
-            names="[[_names]]"
-            up-to-now
-            no-single
+            .hass=${this.hass}
+            .historyData=${this._stateHistory}
+            .names=${this._names}
+            .upToNow=${true}
+            .noSingle=${true}
           ></state-history-charts>
         </div>
       </ha-card>
     `;
   }
 
-  static get properties() {
-    return {
-      hass: Object,
-      _config: Object,
-      _names: Object,
-      _entities: Array,
-
-      _stateHistory: Object,
-      _stateHistoryLoading: Boolean,
-      _cacheConfig: Object,
-    };
+  private _getStateHistory(): void {
+    getRecentWithCache(
+      this.hass!,
+      this._cacheConfig!.cacheKey,
+      this._cacheConfig!,
+      this.hass!.localize,
+      this.hass!.language
+    ).then((stateHistory) => {
+      this._stateHistory = {
+        ...this._stateHistory,
+        ...stateHistory,
+      };
+    });
   }
 
-  getCardSize() {
-    return 4;
+  private _clearInterval(): void {
+    if (this._interval) {
+      window.clearInterval(this._interval);
+      this._interval = undefined;
+    }
   }
 
-  setConfig(config) {
-    const entities = processConfigEntities(config.entities);
-
-    this._config = config;
-
-    const _entities = [];
-    const _names = {};
-    entities.forEach((entity) => {
-      _entities.push(entity.entity);
-      if (entity.name) {
-        _names[entity.entity] = entity.name;
+  static get styles(): CSSResult {
+    return css`
+      .content {
+        padding: 16px;
       }
-    });
-
-    this.setProperties({
-      _cacheConfig: {
-        cacheKey: _entities.join(),
-        hoursToShow: config.hours_to_show || 24,
-        refresh: config.refresh_interval || 0,
-      },
-      _entities,
-      _names,
-    });
+      .has-header {
+        padding-top: 0;
+      }
+    `;
   }
 }
 
-customElements.define("hui-history-graph-card", HuiHistoryGraphCard);
+declare global {
+  interface HTMLElementTagNameMap {
+    "hui-history-graph-card": HuiHistoryGraphCard;
+  }
+}
