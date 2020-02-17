906e0fedf310603685040053bb2418fde62abc11
Paulus Schoutsen <balloob@gmail.com>
2020-02-17 13:56:04 -0800

    Add safe mode support

---
+++ src/panels/lovelace/cards/hui-safe-mode-card.ts
@@ -0,0 +1,68 @@
+import {
+  html,
+  LitElement,
+  TemplateResult,
+  customElement,
+  css,
+  CSSResult,
+  property,
+} from "lit-element";
+import "@material/mwc-button";
+
+import "../../../components/ha-card";
+
+import { LovelaceCard } from "../types";
+import { HomeAssistant } from "../../../types";
+
+@customElement("hui-safe-mode-card")
+export class HuiSafeModeCard extends LitElement implements LovelaceCard {
+  @property() public hass?: HomeAssistant;
+
+  public getCardSize(): number {
+    return 3;
+  }
+
+  public setConfig(_config: any): void {}
+
+  protected render(): TemplateResult {
+    return html`
+      <ha-card
+        .header=${this.hass!.localize(
+          "ui.panel.lovelace.cards.safe-mode.header"
+        )}
+      >
+        <div class="card-content">
+          ${this.hass!.localize(
+            "ui.panel.lovelace.cards.safe-mode.description"
+          )}
+        </div>
+        <div class="card-actions">
+          <a href="/developer-tools/logs">
+            <mwc-button>
+              ${this.hass!.localize(
+                "ui.panel.lovelace.cards.safe-mode.show_errors"
+              )}
+            </mwc-button>
+          </a>
+        </div>
+      </ha-card>
+    `;
+  }
+
+  static get styles(): CSSResult {
+    return css`
+      ha-card {
+        --ha-card-header-color: var(--primary-color);
+      }
+      .card-actions a {
+        text-decoration: none;
+      }
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "hui-safe-mode-card": HuiSafeModeCard;
+  }
+}
