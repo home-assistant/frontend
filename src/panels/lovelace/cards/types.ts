e5dbf184ba112afa291aea0cdaf39f53c3a03ba0
Ian Richardson <iantrich@gmail.com>
2020-02-17 08:26:45 -0600

    add state_color option to glance, button and state-icon (#4854)
    * add state_color option to glance, button and state-icon
    
    * address comments
    
    * address comments

--- src/panels/lovelace/cards/types.ts
+++ src/panels/lovelace/cards/types.ts
@@ -57,6 +57,7 @@ export interface ButtonCardConfig extends LovelaceCardConfig {
   tap_action?: ActionConfig;
   hold_action?: ActionConfig;
   double_tap_action?: ActionConfig;
+  state_color?: boolean;
 }
 
 export interface EntityFilterCardConfig extends LovelaceCardConfig {
@@ -102,6 +103,7 @@ export interface GlanceConfigEntity extends ConfigEntity {
   show_last_changed?: boolean;
   image?: string;
   show_state?: boolean;
+  state_color?: boolean;
 }
 
 export interface GlanceCardConfig extends LovelaceCardConfig {
@@ -112,6 +114,7 @@ export interface GlanceCardConfig extends LovelaceCardConfig {
   theme?: string;
   entities: ConfigEntity[];
   columns?: number;
+  state_color?: boolean;
 }
 
 export interface IframeCardConfig extends LovelaceCardConfig {
