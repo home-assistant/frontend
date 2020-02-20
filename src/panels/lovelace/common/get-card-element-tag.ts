c72dab2abdd8a5ddf81c3936172f515e770584c5
Paulus Schoutsen <balloob@gmail.com>
2020-02-20 00:55:42 -0800

    Add rebuild support to editor preview (#4932)
    * Add rebuild support to editor preview
    
    * getLovelaceCardClass function added
    
    * Use error class
    
    * Tiny cleanup
    
    * Misplaced comment

--- src/panels/lovelace/common/get-card-element-tag.ts
+++
@@ -1,7 +0,0 @@
-const CUSTOM_TYPE_PREFIX = "custom:";
-
-export function getCardElementTag(type: string): string {
-  return type.startsWith(CUSTOM_TYPE_PREFIX)
-    ? type.substr(CUSTOM_TYPE_PREFIX.length)
-    : `hui-${type}-card`;
-}
