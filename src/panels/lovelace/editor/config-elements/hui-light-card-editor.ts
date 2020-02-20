96d442e5d8e2c4068febc32bad3ea685efa43ee9
Bram Kragten <mail@bramkragten.nl>
2020-02-20 17:31:54 +0100

    Oops

--- src/panels/lovelace/editor/config-elements/hui-light-card-editor.ts
+++ src/panels/lovelace/editor/config-elements/hui-light-card-editor.ts
@@ -131,7 +131,6 @@ export class HuiLightCardEditor extends LitElement
         };
       }
     }
-    console.log(this._config);
     fireEvent(this, "config-changed", { config: this._config });
   }
 }
