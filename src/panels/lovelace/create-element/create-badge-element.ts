f02bb67485dcc51e14d1f08142c5c7881ca6fbc4
Paulus Schoutsen <balloob@gmail.com>
2020-02-26 00:40:54 -0800

    Static import all the LL cards etc (#4987)

--- src/panels/lovelace/create-element/create-badge-element.ts
+++ src/panels/lovelace/create-element/create-badge-element.ts
@@ -3,9 +3,18 @@ import "../badges/hui-state-label-badge";
 import { LovelaceBadgeConfig } from "../../../data/lovelace";
 import { createLovelaceElement } from "./create-element-base";
 
-const ALWAYS_LOADED_TYPES = new Set(["error", "state-label"]);
+// lazy load imports
+import "../badges/hui-entity-filter-badge";
+
+const ALWAYS_LOADED_TYPES = new Set([
+  "error",
+  "state-label",
+
+  // lazy load types
+  "entity-filter",
+]);
 const LAZY_LOAD_TYPES = {
-  "entity-filter": () => import("../badges/hui-entity-filter-badge"),
+  // "entity-filter": () => import("../badges/hui-entity-filter-badge"),
 };
 
 export const createBadgeElement = (config: LovelaceBadgeConfig) =>
