6712aa5412a2960568faf16c3da2c6cd44f9a0ac
Thomas Lovén <thomasloven@gmail.com>
2020-02-27 10:46:09 +0100

    Address review comments

--- src/panels/lovelace/editor/config-elements/hui-stack-card-editor.ts
+++ src/panels/lovelace/editor/config-elements/hui-stack-card-editor.ts
@@ -7,7 +7,7 @@ import {
   CSSResult,
   css,
 } from "lit-element";
-import { classMap } from "lit-html/directives/class-map";
+import "@polymer/paper-tabs";
 
 import { struct } from "../../common/structs/struct";
 import { HomeAssistant } from "../../../../types";
@@ -29,8 +29,7 @@ export class HuiAlarmPanelCardEditor extends LitElement
   @property() private _selectedCard: number = 0;
 
   public setConfig(config: StackCardConfig): void {
-    config = cardConfigStruct(config);
-    this._config = config;
+    this._config = cardConfigStruct(config);
   }
 
   protected render(): TemplateResult {
@@ -39,84 +38,73 @@ export class HuiAlarmPanelCardEditor extends LitElement
     }
     const selected = this._selectedCard!;
     const numcards = this._config.cards.length;
-    const start = Math.max(0, Math.min(numcards - 5, selected - 2));
-    const end = Math.min(numcards, start + 5);
 
     return html`
       <div class="card-config">
         <div class="toolbar">
-          <div id="tabbar">
-            ${Array.from(Array(end - start).keys()).map((i) => {
-              const isSide =
-                (i === 0 && start > 0) || (i === 4 && end < numcards);
-              const num = i + start;
-              const isSelected = num === selected;
+          <paper-tabs
+            selected="${selected}"
+            scrollable
+            @iron-select=${(ev) => (this._selectedCard = ev.target.selected)}
+          >
+            ${this._config.cards.map((_, i) => {
               return html`
-                <div
-                  class="tab ${classMap({
-                    active: isSelected,
-                  })}"
-                  @click="${() => {
-                    this._selectedCard = num;
-                  }}"
-                >
-                  ${isSide ? "..." : num + 1}
-                  <mwc-ripple></mwc-ripple>
-                </div>
+                <paper-tab>
+                  ${i + 1}
+                </paper-tab>
               `;
             })}
-          </div>
-
-          <div
+          </paper-tabs>
+          <paper-tabs
             id="add-card"
-            class="tab ${classMap({
-              active: selected === numcards,
-            })}"
-            @click="${() => {
-              this._selectedCard = numcards;
-            }}"
+            selected=${selected === numcards ? "0" : undefined}
           >
-            <ha-icon icon="hass:plus"></ha-icon>
-            <mwc-ripple></mwc-ripple>
-          </div>
+            <paper-tab
+              @click="${() => (this._selectedCard = numcards)}"
+            >
+              <ha-icon icon="hass:plus"></ha-icon>
+            </paper-tab>
+          <paper-tabs>
         </div>
 
         <div id="editor">
-          ${selected < numcards
-            ? html`
-                <div id="card-options">
-                  <paper-icon-button
-                    title="Move card before"
-                    icon="hass:arrow-left"
-                    ?disabled=${selected === 0}
-                    @click=${this._handleMoveBefore}
-                  ></paper-icon-button>
+          ${
+            selected < numcards
+              ? html`
+                  <div id="card-options">
+                    <paper-icon-button
+                      title="Move card before"
+                      icon="hass:arrow-left"
+                      ?disabled=${selected === 0}
+                      @click=${this._handleMoveBefore}
+                    ></paper-icon-button>
 
-                  <paper-icon-button
-                    title="Move card after"
-                    icon="hass:arrow-right"
-                    ?disabled=${selected === numcards - 1}
-                    @click=${this._handleMoveAfter}
-                  ></paper-icon-button>
+                    <paper-icon-button
+                      title="Move card after"
+                      icon="hass:arrow-right"
+                      ?disabled=${selected === numcards - 1}
+                      @click=${this._handleMoveAfter}
+                    ></paper-icon-button>
 
-                  <paper-icon-button
-                    icon="hass:delete"
-                    @click=${this._handleDeleteCard}
-                  ></paper-icon-button>
-                </div>
+                    <paper-icon-button
+                      icon="hass:delete"
+                      @click=${this._handleDeleteCard}
+                    ></paper-icon-button>
+                  </div>
 
-                <hui-card-editor
-                  .hass=${this.hass}
-                  .value="${this._config.cards[selected]}"
-                  @config-changed="${this._handleConfigChanged}"
-                ></hui-card-editor>
-              `
-            : html`
-                <hui-card-picker
-                  .hass="${this.hass}"
-                  @config-changed="${this._handleCardPicked}"
-                ></hui-card-picker>
-              `}
+                  <hui-card-editor
+                    .hass=${this.hass}
+                    .value="${this._config.cards[selected]}"
+                    @config-changed="${this._handleConfigChanged}"
+                  ></hui-card-editor>
+                `
+              : html`
+                  <hui-card-picker
+                    .hass="${this.hass}"
+                    @config-changed="${this._handleCardPicked}"
+                  ></hui-card-picker>
+                `
+          }
         </div>
       </div>
     `;
@@ -180,37 +168,19 @@ export class HuiAlarmPanelCardEditor extends LitElement
     return css`
       .toolbar {
         display: flex;
-        font-size: 20px;
-        height: 64px;
-        border-bottom: 1px solid var(--divider-color);
-        box-sizing: border-box;
+        --paper-tabs-selection-bar-color: var(--primary-color);
+        --paper-tab-ink: var(--primary-color);
       }
-      #tabbar {
+      paper-tabs {
         display: flex;
         font-size: 14px;
         flex-grow: 1;
       }
-      #tabbar.scroll_left {
-        color: green;
-      }
-      .tab {
-        padding: 0 16px;
-        display: flex;
-        flex-direction: column;
-        text-align: center;
-        align-items: center;
-        justify-content: center;
-        height: 64px;
-        cursor: pointer;
-      }
-      .tab.active,
-      #add-card.active {
-        color: var(--primary-color);
-        border-bottom: 2px solid var(--primary-color);
-      }
       #add-card {
+        max-width: 32px;
         padding: 0;
       }
+
       #card-options {
         display: flex;
         justify-content: flex-end;
@@ -218,13 +188,13 @@ export class HuiAlarmPanelCardEditor extends LitElement
       }
 
       #editor {
-        box-shadow: var(
-          --ha-card-box-shadow,
-          0 2px 2px 0 rgba(0, 0, 0, 0.14),
-          0 1px 5px 0 rgba(0, 0, 0, 0.12),
-          0 3px 1px -2px rgba(0, 0, 0, 0.2)
-        );
-        padding: 8px;
+        border: 1px solid var(--divider-color);
+        padding: 12px;
+      }
+      @media (max-width: 450px) {
+        #editor {
+          margin: 0 -12px;
+        }
       }
     `;
   }
