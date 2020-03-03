aa2e632df328231c3ba5d0f46a30eeaa1ab8bf3b
Zack Arnett <arnett.zackary@gmail.com>
2020-03-03 14:53:55 -0500

    Card Picker with Previews of cards (#4975)
    * Card Picker with Previews of cards
    
    * Getting Preview Async - Using dialogs entities
    
    * Create generic getElement - filter entities before
    
    * lint
    
    * Add entities back to Picker. Set Qualifier
    
    * Style Updates
    
    * Move setup of filtered cards to connected
    
    * style updates
    
    * Dont pull entities if noEntity config
    
    * Move all config logic to getConfig
    
    * Style Update - Remove Manual process
    
    * lint
    
    * Accounting for ll-rebuild for async cards
    
    * Style Updates - Use GetStubConfig for most
    
    * Lint
    
    * Filter entities with function - style - no preview
    
    * Iframe rename and description
    
    * Move getstubconfig to helper - update spinner
    
    * Style for themes
    
    * Move entities to be calc once
    
    * Should update
    
    * oops
    
    * TSC
    
    * Comments

--- src/panels/lovelace/cards/hui-weather-forecast-card.ts
+++ src/panels/lovelace/cards/hui-weather-forecast-card.ts
@@ -24,6 +24,8 @@ import { fireEvent } from "../../../common/dom/fire_event";
 import { toggleAttribute } from "../../../common/dom/toggle_attribute";
 import { applyThemesOnElement } from "../../../common/dom/apply_themes_on_element";
 import { actionHandler } from "../common/directives/action-handler-directive";
+import { LovelaceConfig } from "../../../data/lovelace";
+import { findEntities } from "../common/find-entites";
 
 const cardinalDirections = [
   "N",
@@ -71,8 +73,25 @@ class HuiWeatherForecastCard extends LitElement implements LovelaceCard {
     );
     return document.createElement("hui-weather-forecast-card-editor");
   }
-  public static getStubConfig(): object {
-    return { entity: "" };
+
+  public static getStubConfig(
+    hass: HomeAssistant,
+    lovelaceConfig: LovelaceConfig,
+    entities?: string[],
+    entitiesFill?: string[]
+  ): object {
+    const includeDomains = ["weather"];
+    const maxEntities = 1;
+    const foundEntities = findEntities(
+      hass,
+      lovelaceConfig,
+      maxEntities,
+      entities,
+      entitiesFill,
+      includeDomains
+    );
+
+    return { entity: foundEntities[0] || "" };
   }
 
   @property() public hass?: HomeAssistant;
