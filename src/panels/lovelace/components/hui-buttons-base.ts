029be75564e0dece76b12b8315d14c42c63e72d7
Ian Richardson <iantrich@gmail.com>
2020-03-06 00:05:02 -0600

    add name option to buttons row

--- src/panels/lovelace/components/hui-buttons-base.ts
+++ src/panels/lovelace/components/hui-buttons-base.ts
@@ -22,6 +22,7 @@ import { hasAction } from "../common/has-action";
 import { ActionHandlerEvent } from "../../../data/lovelace";
 import { handleAction } from "../common/handle-action";
 import { EntitiesCardEntityConfig } from "../cards/types";
+import { computeStateName } from "../../../common/entity/compute_state_name";
 
 @customElement("hui-buttons-base")
 export class HuiButtonsBase extends LitElement {
@@ -31,9 +32,12 @@ export class HuiButtonsBase extends LitElement {
 
   set hass(hass: HomeAssistant) {
     this._hass = hass;
+    const entitiesShowingIcons = this.configEntities?.filter(
+      (entity) => entity.show_icon !== false
+    );
     this._badges.forEach((badge, index: number) => {
       badge.hass = hass;
-      badge.stateObj = hass.states[this.configEntities![index].entity];
+      badge.stateObj = hass.states[entitiesShowingIcons![index].entity];
     });
   }
 
@@ -46,22 +50,32 @@ export class HuiButtonsBase extends LitElement {
         }
 
         return html`
-          <div>
-            <state-badge
-              title=${computeTooltip(this._hass!, entityConf)}
-              @action=${this._handleAction}
-              .actionHandler=${actionHandler({
-                hasHold: hasAction(entityConf.hold_action),
-                hasDoubleClick: hasAction(entityConf.double_tap_action),
-              })}
-              .config=${entityConf}
-              .hass=${this._hass}
-              .stateObj=${stateObj}
-              .overrideIcon=${entityConf.icon}
-              .overrideImage=${entityConf.image}
-              stateColor
-              tabindex="0"
-            ></state-badge>
+          <div
+            @action=${this._handleAction}
+            .actionHandler=${actionHandler({
+              hasHold: hasAction(entityConf.hold_action),
+              hasDoubleClick: hasAction(entityConf.double_tap_action),
+            })}
+            .config=${entityConf}
+            tabindex="0"
+          >
+            ${entityConf.show_icon !== false
+              ? html`
+                  <state-badge
+                    title=${computeTooltip(this._hass!, entityConf)}
+                    _hass=${this._hass}
+                    .stateObj=${stateObj}
+                    .overrideIcon=${entityConf.icon}
+                    .overrideImage=${entityConf.image}
+                    stateColor
+                  ></state-badge>
+                `
+              : ""}
+            <span>
+              ${entityConf.show_name || entityConf.name
+                ? entityConf.name || computeStateName(stateObj)
+                : ""}
+            </span>
             <mwc-ripple unbounded></mwc-ripple>
           </div>
         `;
@@ -88,8 +102,10 @@ export class HuiButtonsBase extends LitElement {
       .missing {
         color: #fce588;
       }
-      state-badge {
+      div {
         cursor: pointer;
+        align-items: center;
+        display: inline-flex;
       }
     `;
   }
