fd7bb1fa2c3341a8eb41c67f52f1c7638d62ec8b
Ian Richardson <iantrich@gmail.com>
2020-03-06 21:13:56 -0600

    explicit show_name

--- src/panels/lovelace/components/hui-buttons-base.ts
+++ src/panels/lovelace/components/hui-buttons-base.ts
@@ -72,7 +72,7 @@ export class HuiButtonsBase extends LitElement {
                 `
               : ""}
             <span>
-              ${entityConf.show_name || entityConf.name
+              ${entityConf.show_name
                 ? entityConf.name || computeStateName(stateObj)
                 : ""}
             </span>
