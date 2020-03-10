906f417436918609e53449508494f286d1330ae8
Paulus Schoutsen <balloob@gmail.com>
2020-03-10 15:18:13 -0700

    Lazy load our elements once again (#5124)

--- src/panels/lovelace/create-element/create-badge-element.ts
+++ src/panels/lovelace/create-element/create-badge-element.ts
@@ -3,18 +3,9 @@ import "../badges/hui-state-label-badge";
 import { LovelaceBadgeConfig } from "../../../data/lovelace";
 import { createLovelaceElement } from "./create-element-base";
 
-// lazy load imports
-import "../badges/hui-entity-filter-badge";
-
-const ALWAYS_LOADED_TYPES = new Set([
-  "error",
-  "state-label",
-
-  // lazy load types
-  "entity-filter",
-]);
+const ALWAYS_LOADED_TYPES = new Set(["error", "state-label"]);
 const LAZY_LOAD_TYPES = {
-  // "entity-filter": () => import("../badges/hui-entity-filter-badge"),
+  "entity-filter": () => import("../badges/hui-entity-filter-badge"),
 };
 
 export const createBadgeElement = (config: LovelaceBadgeConfig) =>
