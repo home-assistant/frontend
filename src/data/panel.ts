6fec47fdadc232d627e34cbc936e7d0e98bc1b54
Timmo <contact@timmo.xyz>
2020-03-16 18:35:08 +0000

    guard clause

--- src/data/panel.ts
+++ src/data/panel.ts
@@ -6,17 +6,18 @@ export const getPanelTitle = (hass: HomeAssistant): string | undefined => {
   const panel = Object.values(hass.panels).find(
     (p: PanelInfo): boolean => p.url_path === hass.panelUrl
   );
-  if (panel) {
-    const defaultPanel =
-      hass.panels[localStorage.defaultPage || DEFAULT_PANEL] ||
-      hass.panels[DEFAULT_PANEL];
-    title =
-      panel.url_path === "profile"
-        ? hass.localize("panel.profile")
-        : hass.localize(`panel.${panel.title}`) ||
-          panel.title ||
-          defaultPanel.title ||
-          hass.localize("panel.states");
+  if (!panel) {
+    return;
   }
+  const defaultPanel =
+    hass.panels[localStorage.defaultPage || DEFAULT_PANEL] ||
+    hass.panels[DEFAULT_PANEL];
+  title =
+    panel.url_path === "profile"
+      ? hass.localize("panel.profile")
+      : hass.localize(`panel.${panel.title}`) ||
+        panel.title ||
+        defaultPanel.title ||
+        hass.localize("panel.states");
   return title;
 };
