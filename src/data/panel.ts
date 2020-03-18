2741bb8b38c172e06dec60eb10774647423a0b7a
Paulus Schoutsen <balloob@gmail.com>
2020-03-18 12:32:35 -0700

    Fix calculating title if panels not loaded yet (#5262)

--- src/data/panel.ts
+++ src/data/panel.ts
@@ -1,23 +1,35 @@
 import { HomeAssistant, PanelInfo } from "../types";
-import { DEFAULT_PANEL } from "../common/const";
+
+/** Panel to show when no panel is picked. */
+const DEFAULT_PANEL = "lovelace";
+
+export const getDefaultPanelUrlPath = () =>
+  localStorage.defaultPage || DEFAULT_PANEL;
+
+export const getDefaultPanel = (panels: HomeAssistant["panels"]) =>
+  panels[localStorage.defaultPage] || panels[DEFAULT_PANEL];
 
 export const getPanelTitle = (hass: HomeAssistant): string | undefined => {
-  let title: string = "";
+  if (!hass.panels) {
+    return;
+  }
+
   const panel = Object.values(hass.panels).find(
     (p: PanelInfo): boolean => p.url_path === hass.panelUrl
   );
+
   if (!panel) {
     return;
   }
-  const defaultPanel =
-    hass.panels[localStorage.defaultPage || DEFAULT_PANEL] ||
-    hass.panels[DEFAULT_PANEL];
-  title =
-    panel.url_path === "profile"
-      ? hass.localize("panel.profile")
-      : hass.localize(`panel.${panel.title}`) ||
-        panel.title ||
-        defaultPanel.title ||
-        hass.localize("panel.states");
-  return title;
+
+  if (panel.url_path === "profile") {
+    return hass.localize("panel.profile");
+  }
+
+  return (
+    hass.localize(`panel.${panel.title}`) ||
+    panel.title ||
+    // default panel
+    (hass.panels[localStorage.defaultPage] || hass.panels[DEFAULT_PANEL]).title!
+  );
 };
