5ffedadf2a960f55e0f18618ca87ae5e52f8c4df
Olivér Falvai <ofalvai@gmail.com>
2020-03-18 17:00:08 +0100

    Add loading text for long-running integration install step (#4378)
    * Added optional label for dialog-data-entry-flow
    
    * Use correct loading element
    
    * Update src/translations/en.json
    
    Co-Authored-By: Bram Kragten <mail@bramkragten.nl>
    
    * Minor template adjustment
    
    * Revert accidental change of PR templates
    
    * Revert accidental change of PR templates
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>

--- src/dialogs/config-flow/step-flow-loading.ts
+++ src/dialogs/config-flow/step-flow-loading.ts
@@ -5,14 +5,22 @@ import {
   css,
   customElement,
   CSSResult,
+  property,
 } from "lit-element";
 import "@polymer/paper-spinner/paper-spinner-lite";
 
 @customElement("step-flow-loading")
 class StepFlowLoading extends LitElement {
+  @property() public label?: string;
+
   protected render(): TemplateResult {
     return html`
       <div class="init-spinner">
+        ${this.label
+          ? html`
+              <div>${this.label}</div>
+            `
+          : ""}
         <paper-spinner-lite active></paper-spinner-lite>
       </div>
     `;
