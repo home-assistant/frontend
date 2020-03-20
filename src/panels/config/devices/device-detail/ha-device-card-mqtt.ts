c54f2b66dad37e9aa24633b5b9408a8a8afc3a1f
Erik Montnemery <erik@montnemery.com>
2020-03-20 15:53:36 +0100

    Add delete button to MQTT devices' config page (#5117)
    * Add delete button to MQTT devices' config page
    
    * Move delete button to its own card
    
    * Fix review comments
    
    * Fix review comments
    
    * Fix review comments
    
    * Use haStyle instead of haStyleDialog

---
+++ src/panels/config/devices/device-detail/ha-device-card-mqtt.ts
@@ -0,0 +1,43 @@
+import { DeviceRegistryEntry } from "../../../../data/device_registry";
+import { removeMQTTDeviceEntry } from "../../../../data/mqtt";
+import {
+  LitElement,
+  html,
+  customElement,
+  property,
+  TemplateResult,
+  CSSResult,
+} from "lit-element";
+import { showConfirmationDialog } from "../../../../dialogs/generic/show-dialog-box";
+import { HomeAssistant } from "../../../../types";
+import { haStyle } from "../../../../resources/styles";
+
+@customElement("ha-device-card-mqtt")
+export class HaDeviceCardMqtt extends LitElement {
+  @property() public hass!: HomeAssistant;
+  @property() public device!: DeviceRegistryEntry;
+
+  protected render(): TemplateResult {
+    return html`
+      <mwc-button class="warning" @click="${this._confirmDeleteEntry}">
+        ${this.hass.localize("ui.panel.config.devices.delete")}
+      </mwc-button>
+    `;
+  }
+
+  private async _confirmDeleteEntry(): Promise<void> {
+    const confirmed = await showConfirmationDialog(this, {
+      text: this.hass.localize("ui.panel.config.devices.confirm_delete"),
+    });
+
+    if (!confirmed) {
+      return;
+    }
+
+    await removeMQTTDeviceEntry(this.hass!, this.device.id);
+  }
+
+  static get styles(): CSSResult {
+    return haStyle;
+  }
+}
