9fbfdec54641b10ce54b26ea2c57837b6de38c01
Thomas Lovén <thomasloven@gmail.com>
2020-03-31 19:35:51 +0200

    Add some more card helpers.

--- src/panels/lovelace/custom-card-helpers.ts
+++ src/panels/lovelace/custom-card-helpers.ts
@@ -3,3 +3,9 @@ export { createCardElement } from "./create-element/create-card-element";
 export { createBadgeElement } from "./create-element/create-badge-element";
 export { createHeaderFooterElement } from "./create-element/create-header-footer-element";
 export { createHuiElement } from "./create-element/create-hui-element";
+export { actionHandlerBind } from "./common/directives/action-handler-directive";
+export { handleAction } from "./common/handle-action";
+
+export const loadElements = {
+  entityEditor: () => import("./components/hui-entity-editor"),
+};
