9a55ac505eff08f8e369b3560e90261496d7d578
Zack Arnett <arnett.zackary@gmail.com>
2020-04-01 20:19:34 -0400

    Adding insert card and delete undo

--- src/util/toast-deleted-success.ts
+++ src/util/toast-deleted-success.ts
@@ -1,7 +1,15 @@
 import { showToast } from "./toast";
 import { HomeAssistant } from "../types";
 
-export const showDeleteSuccessToast = (el: HTMLElement, hass: HomeAssistant) =>
+export const showDeleteSuccessToast = (
+  el: HTMLElement,
+  hass: HomeAssistant,
+  action: () => void
+) =>
   showToast(el, {
     message: hass!.localize("ui.common.successfully_deleted"),
+    action: {
+      text: "Undo",
+      action,
+    },
   });
