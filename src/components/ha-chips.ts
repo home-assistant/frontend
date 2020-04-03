32fc5cf92b129fd4f7f0a05b8264968163daab1f
Zack Arnett <arnett.zackary@gmail.com>
2020-04-03 19:51:42 -0400

    Add the ability to use input chips to Ha-chips

--- src/components/ha-chips.ts
+++ src/components/ha-chips.ts
@@ -8,33 +8,43 @@ import {
   customElement,
   unsafeCSS,
 } from "lit-element";
+import { classMap } from "lit-html/directives/class-map";
 import { ripple } from "@material/mwc-ripple/ripple-directive";
 // @ts-ignore
 import chipStyles from "@material/chips/dist/mdc.chips.min.css";
 import { fireEvent } from "../common/dom/fire_event";
 
+import "./ha-icon";
+
 declare global {
   // for fire event
   interface HASSDomEvents {
-    "chip-clicked": { index: string };
+    "chip-clicked": { index: number };
   }
 }
 
 @customElement("ha-chips")
 export class HaChips extends LitElement {
   @property() public items = [];
+  @property() public type: "input" | "choice" = "choice";
 
   protected render(): TemplateResult {
     if (this.items.length === 0) {
       return html``;
     }
+
     return html`
-      <div class="mdc-chip-set">
+      <div
+        class="mdc-chip-set ${classMap({
+          "mdc-chip-set--input": this.type === "input",
+          "mdc-chip-set--choice": this.type === "choice",
+        })}"
+      >
         ${this.items.map(
           (item, idx) =>
             html`
               <div class="mdc-chip" .index=${idx} @click=${this._handleClick}>
-                <div class="mdc-chip__ripple" .ripple="${ripple()}"></div>
+                <div class="mdc-chip__ripple" .ripple=${ripple()}></div>
                 <span role="gridcell">
                   <span
                     role="button"
@@ -44,6 +54,19 @@ export class HaChips extends LitElement {
                     <span class="mdc-chip__text">${item}</span>
                   </span>
                 </span>
+                ${this.type === "input"
+                  ? html`
+                      <span role="gridcell">
+                        <ha-icon
+                          class="mdc-chip__icon mdc-chip__icon--trailing"
+                          icon="mdi:close-circle"
+                          tabindex="-1"
+                          role="button"
+                          @click=${this._handleRemove}
+                        ></ha-icon>
+                      </span>
+                    `
+                  : ""}
               </div>
             `
         )}
@@ -51,9 +74,16 @@ export class HaChips extends LitElement {
     `;
   }
 
-  private _handleClick(ev): void {
+  private _handleClick(ev: MouseEvent): void {
     fireEvent(this, "chip-clicked", {
-      index: ev.currentTarget.index,
+      index: (ev.currentTarget as any).index,
+    });
+  }
+
+  private _handleRemove(ev: MouseEvent): void {
+    ev.stopPropagation();
+    fireEvent(this, "chip-clicked", {
+      index: (ev.currentTarget as any).index,
     });
   }
 
