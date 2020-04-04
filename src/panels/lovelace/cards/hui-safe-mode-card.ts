1a41ac35a9d437826444ca207743affdf7cde584
Bram Kragten <mail@bramkragten.nl>
2020-04-04 18:09:02 +0200

    Add error logs to safe mode card

--- src/panels/lovelace/cards/hui-safe-mode-card.ts
+++ src/panels/lovelace/cards/hui-safe-mode-card.ts
@@ -10,7 +10,7 @@ import {
 import "@material/mwc-button";
 
 import "../../../components/ha-card";
-
+import "../../../panels/developer-tools/logs/error-log-card";
 import { LovelaceCard } from "../types";
 import { HomeAssistant } from "../../../types";
 
@@ -37,12 +37,14 @@ export class HuiSafeModeCard extends LitElement implements LovelaceCard {
           ${this.hass!.localize(
             "ui.panel.lovelace.cards.safe-mode.description"
           )}
+
+          <error-log-card .hass=${this.hass}></error-log-card>
         </div>
         <div class="card-actions">
           <a href="/developer-tools/logs">
             <mwc-button>
               ${this.hass!.localize(
-                "ui.panel.lovelace.cards.safe-mode.show_errors"
+                "ui.panel.lovelace.cards.safe-mode.go_to_logs"
               )}
             </mwc-button>
           </a>
