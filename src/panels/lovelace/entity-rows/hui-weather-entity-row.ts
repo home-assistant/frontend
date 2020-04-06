eb094187a78c2c0d06002b5a963ac6f8eeed093f
Bram Kragten <mail@bramkragten.nl>
2020-04-06 17:48:01 +0200

    Update hui-weather-entity-row.ts

--- src/panels/lovelace/entity-rows/hui-weather-entity-row.ts
+++ src/panels/lovelace/entity-rows/hui-weather-entity-row.ts
@@ -70,11 +70,18 @@ class HuiWeatherEntityRow extends LitElement implements LovelaceRow {
       <hui-generic-entity-row .hass=${this.hass} .config=${weatherRowConfig}>
         <div class="attributes">
           <div>
-            ${stateObj.attributes.temperature}
-            ${getWeatherUnit(this.hass, "temperature")}
+            ${UNAVAILABLE_STATES.includes(stateObj.state)
+              ? this.hass.localize(`state.default.${stateObj.state}`) ||
+                stateObj.state
+              : html`
+                  ${stateObj.attributes.temperature}
+                  ${getWeatherUnit(this.hass, "temperature")}
+                `}
           </div>
           <div class="secondary">
-            ${this._getSecondaryAttribute(stateObj)}
+            ${!UNAVAILABLE_STATES.includes(stateObj.state)
+              ? this._getSecondaryAttribute(stateObj)
+              : ""}
           </div>
         </div>
       </hui-generic-entity-row>
