4daccb591ce5c13d92fabafbfd79d0f858bb28e4
Bram Kragten <mail@bramkragten.nl>
2020-04-08 16:25:53 +0200

    Simplify

--- src/dialogs/zha-device-zigbee-signature-dialog/dialog-zha-device-zigbee-info.ts
+++ src/dialogs/zha-device-zigbee-signature-dialog/dialog-zha-device-zigbee-info.ts
@@ -1,12 +1,10 @@
 import {
   LitElement,
   html,
-  css,
   CSSResult,
   TemplateResult,
   customElement,
   property,
-  query,
 } from "lit-element";
 import "../../components/ha-code-editor";
 import "../../components/ha-dialog";
@@ -14,43 +12,28 @@ import "../../components/ha-dialog";
 import { haStyleDialog } from "../../resources/styles";
 import { HomeAssistant } from "../../types";
 import { ZHADeviceZigbeeInfoDialogParams } from "./show-dialog-zha-device-zigbee-info";
-// tslint:disable-next-line: no-duplicate-imports
-import { HaCodeEditor } from "../../components/ha-code-editor";
-import { afterNextRender } from "../../common/util/render-status";
-import { fireEvent } from "../../common/dom/fire_event";
-// tslint:disable-next-line: no-duplicate-imports
-import { HaDialog } from "../../components/ha-dialog";
 
 @customElement("dialog-zha-device-zigbee-info")
 class DialogZHADeviceZigbeeInfo extends LitElement {
   @property() public hass!: HomeAssistant;
-  @property() private _params?: ZHADeviceZigbeeInfoDialogParams;
-  @property() private _error?: string;
   @property() private _signature: any;
-  @query("ha-code-editor") private _codeEditor?: HaCodeEditor;
-  @query("ha-dialog") private _dialog!: HaDialog;
 
   public async showDialog(
     params: ZHADeviceZigbeeInfoDialogParams
   ): Promise<void> {
-    this._params = params;
-    const signature = JSON.parse(JSON.stringify(this._params.device.signature));
-    signature.manufacturer = this._params.device.manufacturer;
-    signature.model = this._params.device.model;
-    this._signature = JSON.stringify(signature, undefined, 2);
-    await this.updateComplete;
-    afterNextRender(() => {
-      if (this._codeEditor?.codemirror) {
-        this._codeEditor.codemirror.refresh();
-      }
-      afterNextRender(() =>
-        fireEvent(this._dialog as HTMLElement, "iron-resize")
-      );
-    });
+    this._signature = JSON.stringify(
+      {
+        ...params.device.signature,
+        manufacturer: params.device.manufacturer,
+        model: params.device.model,
+      },
+      null,
+      2
+    );
   }
 
   protected render(): TemplateResult {
-    if (!this._params) {
+    if (!this._signature) {
       return html``;
     }
 
@@ -63,44 +46,17 @@ class DialogZHADeviceZigbeeInfo extends LitElement {
           `ui.dialogs.zha_device_info.device_signature`
         )}
       >
-        ${this._error
-          ? html`
-              <div class="error">${this._error}</div>
-            `
-          : html`
-              <ha-code-editor
-                mode="yaml"
-                .value=${this._signature}
-                .hass=${this.hass}
-              >
-              </ha-code-editor>
-            `}
+        <ha-code-editor mode="yaml" .value=${this._signature}> </ha-code-editor>
       </ha-dialog>
     `;
   }
 
   private _close(): void {
-    this._params = undefined;
-    this._error = undefined;
     this._signature = undefined;
   }
 
-  static get styles(): CSSResult[] {
-    return [
-      haStyleDialog,
-      css`
-        ha-dialog > * {
-          margin: 0;
-          display: block;
-          padding: 0;
-          min-width: 400px;
-          max-width: 600px;
-        }
-        .error {
-          color: var(--google-red-500);
-        }
-      `,
-    ];
+  static get styles(): CSSResult {
+    return haStyleDialog;
   }
 }
 
