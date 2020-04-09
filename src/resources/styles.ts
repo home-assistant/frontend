c9eff3caf26117123d2f35daf72b4c7385675052
Sicco <sdotter@gmail.com>
2020-04-09 17:55:15 +0200

    Redo change sidebar-background-color...

--- src/resources/styles.ts
+++ src/resources/styles.ts
@@ -7,7 +7,7 @@ export const derivedStyles = {
   "sidebar-text-color": "var(--primary-text-color)",
   "sidebar-header-background-color": "var(--primary-background-color))",
   "sidebar-header-height": "var(--header-height)",
-  "sidebar-background-color": "var(--paper-listbox-background-color)",
+  "sidebar-background-color": "var(--card-background-color)",
   "sidebar-selected-text-color": "var(--primary-color)",
   "sidebar-selected-icon-color": "var(--primary-color)",
   "sidebar-icon-color": "rgba(var(--rgb-primary-text-color), 0.6)",
