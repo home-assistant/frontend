7458124fae7103feaa9f47a97d1c7fb8ef69d3e7
Paulus Schoutsen <balloob@gmail.com>
2020-04-10 04:13:58 -0700

    Add icons to info panel (#5501)
    * Add icons to info panel
    
    * Update src/panels/developer-tools/info/integrations-card.ts
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>

--- src/panels/developer-tools/info/integrations-card.ts
+++ src/panels/developer-tools/info/integrations-card.ts
@@ -30,6 +30,16 @@ class IntegrationsCard extends LitElement {
             ${this._sortedIntegrations(this.hass!.config.components).map(
               (domain) => html`
                 <tr>
+                  <td>
+                    <img
+                      loading="lazy"
+                      src="https://brands.home-assistant.io/_/${domain}/icon.png"
+                      srcset="
+                        https://brands.home-assistant.io/_/${domain}/icon@2x.png 2x
+                      "
+                      referrerpolicy="no-referrer"
+                    />
+                  </td>
                   <td>${domain}</td>
                   <td>
                     <a
@@ -66,6 +76,11 @@ class IntegrationsCard extends LitElement {
       }
       td:first-child {
         padding-left: 0;
+      }
+      img {
+        display: block;
+        max-height: 24px;
+        max-width: 24px;
       }
       a {
         color: var(--primary-color);
