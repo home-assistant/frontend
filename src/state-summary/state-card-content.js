41ee4288369e9f4d3f5ad2d9c71a8606ab30e42f
Anton Verburg <info@tec-V.nl>
2020-04-18 18:16:58 +0200

    Fix state-card-contents

--- src/state-summary/state-card-content.js
+++ src/state-summary/state-card-content.js
@@ -2,6 +2,7 @@
 import { PolymerElement } from "@polymer/polymer/polymer-element";
 import dynamicContentUpdater from "../common/dom/dynamic_content_updater";
 import { stateCardType } from "../common/entity/state_card_type";
+import "./state-card-analog_output";
 import "./state-card-climate";
 import "./state-card-configurator";
 import "./state-card-cover";
