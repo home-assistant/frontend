7132f6e71f09bf0e4f1fe223188da765e8e5478b
Paulus Schoutsen <balloob@gmail.com>
2020-04-21 15:02:00 -0700

    Fix cast translations (#5582)

--- build-scripts/gulp/translations.js
+++ build-scripts/gulp/translations.js
@@ -252,7 +252,26 @@ gulp.task(taskName, function () {
   return gulp
     .src(fullDir + "/*.json")
     .pipe(
-      transform((data) => {
+      transform((data, file) => {
+        // HACK to pull in old state translations for cast
+        if (process.env.IS_CAST) {
+          const legacyTranslationsPath = path.join(
+            "cast/src/translations",
+            file.relative
+          );
+          if (fs.existsSync(legacyTranslationsPath)) {
+            const legacyStrings = JSON.parse(
+              fs.readFileSync(legacyTranslationsPath, "utf-8")
+            );
+            // These 2 translations still exist today.
+            if (legacyStrings.state && "default" in legacyStrings.state) {
+              legacyStrings.default.unknown = data.state.default.unknown;
+              legacyStrings.default.unavailable =
+                data.state.default.unavailable;
+            }
+            data.state = legacyStrings.state;
+          }
+        }
         TRANSLATION_FRAGMENTS.forEach((fragment) => {
           delete data.ui.panel[fragment];
         });
