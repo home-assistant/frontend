cfc21b58466c728d8eba64a28d1e1e8d651003ff
Erik Montnemery <erik@montnemery.com>
2020-04-22 12:01:43 +0200

    Display MQTT debug info (#5375)
    * Add MQTT debug info
    
    * Refactor
    
    * Fix mistake
    
    * Rewrite, improve display.
    
    * Tweak translations
    
    * Add mqtt-payload.ts
    
    * Apply suggestions from code review
    
    Co-Authored-By: Zack Arnett <arnett.zackary@gmail.com>
    
    * Tweak after adding review comments.
    
    * Rewrite to only render the messages when details is opened
    
    * Adapt to core PR #33752
    
    * Address review comments
    
    * Lint
    
    * Lint
    
    * Address review comments
    
    Co-authored-by: Zack Arnett <arnett.zackary@gmail.com>

---
+++ src/dialogs/mqtt-device-debug-info-dialog/mqtt-discovery-payload.ts
@@ -0,0 +1,49 @@
+import {
+  LitElement,
+  html,
+  TemplateResult,
+  customElement,
+  property,
+} from "lit-element";
+import { safeDump } from "js-yaml";
+
+@customElement("mqtt-discovery-payload")
+class MQTTDiscoveryPayload extends LitElement {
+  @property() public payload!: object;
+
+  @property() public showAsYaml = false;
+
+  @property() public summary!: string;
+
+  @property() private _open = false;
+
+  protected render(): TemplateResult {
+    return html`
+      <details @toggle=${this._handleToggle}>
+        <summary>
+          ${this.summary}
+        </summary>
+        ${this._open ? this._renderPayload() : ""}
+      </details>
+    `;
+  }
+
+  private _renderPayload(): TemplateResult {
+    const payload = this.payload;
+    return html`
+      ${this.showAsYaml
+        ? html` <pre>${safeDump(payload)}</pre> `
+        : html` <pre>${JSON.stringify(payload, null, 2)}</pre> `}
+    `;
+  }
+
+  private _handleToggle(ev) {
+    this._open = ev.target.open;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "mqtt-discovery-payload": MQTTDiscoveryPayload;
+  }
+}
