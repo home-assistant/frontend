86806099e83f72a61015ef95cb0f514eccc49e9f
Paulus Schoutsen <balloob@gmail.com>
2020-04-25 16:35:55 -0700

    Type translation stub for future

--- demo/src/stubs/translations.ts
+++ demo/src/stubs/translations.ts
@@ -1,7 +1,10 @@
 import { MockHomeAssistant } from "../../../src/fake_data/provide_hass";
 
 export const mockTranslations = (hass: MockHomeAssistant) => {
-  hass.mockWS("frontend/get_translations", () => ({
-    resources: {},
-  }));
+  hass.mockWS(
+    "frontend/get_translations",
+    (/* msg: {language: string, category: string} */) => {
+      return { resources: {} };
+    }
+  );
 };
