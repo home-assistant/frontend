329f31a143b6bf2260a8121e50b30eb2d773acaf
Jason Knott <24280037+jayknott@users.noreply.github.com>
2020-05-01 12:47:32 -0700

    Added options to Lovelace evaluate filter

--- src/panels/lovelace/common/evaluate-filter.ts
+++ src/panels/lovelace/common/evaluate-filter.ts
@@ -20,7 +20,26 @@ export const evaluateFilter = (stateObj: HassEntity, filter: any): boolean => {
       return state > value;
     case "!=":
       return state !== value;
+    case "in":
+      if (Array.isArray(state) || typeof state === typeof "string") {
+        return state.includes(value);
+      }
+      return false;
+    case "not in":
+      if (Array.isArray(state) || typeof state === typeof "string") {
+        return !state.includes(value);
+      }
+      return false;
     case "regex": {
+      if (
+        typeof state !== typeof "string" &&
+        typeof state !== typeof 1 &&
+        typeof state !== typeof true &&
+        state !== null &&
+        typeof state !== typeof undefined
+      ) {
+        return state.match(JSON.stringify(value));
+      }
       return state.match(value);
     }
     default:
