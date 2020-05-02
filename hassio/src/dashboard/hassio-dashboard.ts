108233f3b89be74aabcf4c24cece731718991421
Joakim Sørensen <joasoe@gmail.com>
2020-05-02 20:19:17 +0200

    Adds mainPage to hass-tabs-subpage (#5724)

--- hassio/src/dashboard/hassio-dashboard.ts
+++ hassio/src/dashboard/hassio-dashboard.ts
@@ -40,6 +40,7 @@ class HassioDashboard extends LitElement {
         .hass=${this.hass}
         .narrow=${this.narrow}
         hassio
+        main-page
         .route=${this.route}
         .tabs=${supervisorTabs}
       >
