fe56b001c46c37f5215b11e1d1433b73d256dcba
Joakim Sørensen <joasoe@gmail.com>
2020-05-02 15:18:57 +0200

    Add reload buttons to store and snapshot (#5714)

--- hassio/src/hassio-panel.ts
+++ hassio/src/hassio-panel.ts
@@ -15,7 +15,29 @@ import "../../src/resources/ha-style";
 import { HomeAssistant, Route } from "../../src/types";
 import "./hassio-panel-router";
 import type { PageNavigation } from "../../src/layouts/hass-tabs-subpage";
-import "../../src/layouts/hass-tabs-subpage";
+
+export const supervisorTabs: PageNavigation[] = [
+  {
+    name: "Dashboard",
+    path: `/hassio/dashboard`,
+    icon: "hassio:view-dashboard",
+  },
+  {
+    name: "Add-on store",
+    path: `/hassio/store`,
+    icon: "hassio:store",
+  },
+  {
+    name: "Snapshots",
+    path: `/hassio/snapshots`,
+    icon: "hassio:backup-restore",
+  },
+  {
+    name: "System",
+    path: `/hassio/system`,
+    icon: "hassio:cogs",
+  },
+];
 
 @customElement("hassio-panel")
 class HassioPanel extends LitElement {
@@ -34,46 +56,16 @@ class HassioPanel extends LitElement {
   @property({ attribute: false }) public hassOsInfo!: HassioHassOSInfo;
 
   protected render(): TemplateResult {
-    const supervisorTabs: PageNavigation[] = [
-      {
-        name: "Dashboard",
-        path: `/hassio/dashboard`,
-        icon: "mdi:view-dashboard",
-      },
-      {
-        name: "Add-on store",
-        path: `/hassio/store`,
-        icon: "mdi:store",
-      },
-      {
-        name: "Snapshots",
-        path: `/hassio/snapshots`,
-        icon: "mdi:backup-restore",
-      },
-      {
-        name: "System",
-        path: `/hassio/system`,
-        icon: "mdi:cogs",
-      },
-    ];
-
     return html`
-      <hass-tabs-subpage
+      <hassio-panel-router
+        .route=${this.route}
         .hass=${this.hass}
         .narrow=${this.narrow}
-        .route=${this.route}
-        .tabs=${supervisorTabs}
-      >
-        <span slot="header">Supervisor</span>
-        <hassio-panel-router
-          .route=${this.route}
-          .hass=${this.hass}
-          .supervisorInfo=${this.supervisorInfo}
-          .hostInfo=${this.hostInfo}
-          .hassInfo=${this.hassInfo}
-          .hassOsInfo=${this.hassOsInfo}
-        ></hassio-panel-router>
-      </hass-tabs-subpage>
+        .supervisorInfo=${this.supervisorInfo}
+        .hostInfo=${this.hostInfo}
+        .hassInfo=${this.hassInfo}
+        .hassOsInfo=${this.hassOsInfo}
+      ></hassio-panel-router>
     `;
   }
 }
