02885e3008b50ad981bde0882d5e546a00e087b2
Bram Kragten <mail@bramkragten.nl>
2020-05-06 12:58:53 +0200

    Bump fecha (#5728)

--- src/common/datetime/format_date.ts
+++ src/common/datetime/format_date.ts
@@ -1,4 +1,4 @@
-import fecha from "fecha";
+import { format } from "fecha";
 import { toLocaleDateStringSupportsOptions } from "./check_options_support";
 
 export const formatDate = toLocaleDateStringSupportsOptions
@@ -8,4 +8,4 @@ export const formatDate = toLocaleDateStringSupportsOptions
         month: "long",
         day: "numeric",
       })
-  : (dateObj: Date) => fecha.format(dateObj, "longDate");
+  : (dateObj: Date) => format(dateObj, "longDate");
