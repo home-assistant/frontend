b69d5e0fa31bc122ed368626d56eaa6ba03b2507
Paulus Schoutsen <balloob@gmail.com>
2020-05-07 09:28:51 -0700

    Revert "Use gulp-terser instead of webpack-terser (#5788)" (#5796)

--- build-scripts/gulp/cast.js
+++ build-scripts/gulp/cast.js
@@ -4,7 +4,6 @@ require("./clean.js");
 require("./translations.js");
 require("./gather-static.js");
 require("./webpack.js");
-require("./minify.js");
 require("./service-worker.js");
 require("./entry-html.js");
 
@@ -33,7 +32,6 @@ gulp.task(
     gulp.parallel("gen-icons-json", "build-translations"),
     "copy-static-cast",
     "webpack-prod-cast",
-    "minify-cast",
     "gen-index-cast-prod"
   )
 );
