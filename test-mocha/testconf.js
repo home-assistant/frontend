67e479f1f73b374dfa968202181ed174588b27fa
EC2 Default User <ec2-user@ip-172-31-56-62.ec2.internal>
2020-05-08 10:31:30 +0000

    More voodoo

--- test-mocha/testconf.js
+++ test-mocha/testconf.js
@@ -1,1 +1,4 @@
-//global.__BACKWARDS_COMPAT__ = false;
+global.__BACKWARDS_COMPAT__ = false;
+
+//Not quite sure why JSDom doesn't just do this for us.
+global.customElements = window.customElements;
