71212a1452c7f8832915102ecff5a4731e80af9f
Bram Kragten <mail@bramkragten.nl>
2020-05-21 13:00:33 +0200

    Fix markdown card crashing the demo (#5962)

--- src/panels/lovelace/common/compute-card-size.ts
+++ src/panels/lovelace/common/compute-card-size.ts
@@ -1,5 +1,5 @@
 import { LovelaceCard } from "../types";
 
 export const computeCardSize = (card: LovelaceCard): number => {
-  return typeof card.getCardSize === "function" ? card.getCardSize() : 1;
+  return typeof card.getCardSize === "function" ? card.getCardSize() : 4;
 };
