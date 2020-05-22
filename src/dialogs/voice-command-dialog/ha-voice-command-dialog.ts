1087a6dc67eb23cd712d38bb3950d08ff9361c60
Bram Kragten <mail@bramkragten.nl>
2020-05-22 14:44:17 +0200

    Fix show password toggle (#5979)

--- src/dialogs/voice-command-dialog/ha-voice-command-dialog.ts
+++ src/dialogs/voice-command-dialog/ha-voice-command-dialog.ts
@@ -176,7 +176,6 @@ export class HaVoiceCommandDialog extends LitElement {
                         `
                       : ""}
                     <ha-icon-button
-                      .active=${Boolean(this.results)}
                       icon="hass:microphone"
                       @click=${this._toggleListening}
                     >
