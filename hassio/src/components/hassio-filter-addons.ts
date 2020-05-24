fcc675dfd048d54c832f029fff25afad37b85c13
Paulus Schoutsen <balloob@gmail.com>
2020-05-24 01:39:07 -0700

    Upgrade Fuse (#6012)

--- hassio/src/components/hassio-filter-addons.ts
+++ hassio/src/components/hassio-filter-addons.ts
@@ -1,13 +1,13 @@
-import * as Fuse from "fuse.js";
+import Fuse from "fuse.js";
 import { HassioAddonInfo } from "../../../src/data/hassio/addon";
 
 export function filterAndSort(addons: HassioAddonInfo[], filter: string) {
-  const options: Fuse.FuseOptions<HassioAddonInfo> = {
+  const options: Fuse.IFuseOptions<HassioAddonInfo> = {
     keys: ["name", "description", "slug"],
-    caseSensitive: false,
+    isCaseSensitive: false,
     minMatchCharLength: 2,
     threshold: 0.2,
   };
   const fuse = new Fuse(addons, options);
-  return fuse.search(filter);
+  return fuse.search(filter).map((result) => result.item);
 }
