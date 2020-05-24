c4010ff03c68eacb1f6adf2c7918a053d12a2130
Paulus Schoutsen <balloob@gmail.com>
2020-05-24 14:44:03 -0700

    Integrate compatibility into the build

--- build-scripts/rollup-plugins/ignore-plugin.js
+++ build-scripts/rollup-plugins/ignore-plugin.js
@@ -1,5 +1,7 @@
 const path = require("path");
 
+const suffix = "?ignore";
+
 module.exports = function (userOptions = {}) {
   // Files need to be absolute paths.
   // This only works if the file has no exports
@@ -14,32 +16,6 @@ module.exports = function (userOptions = {}) {
 
   return {
     name: "ignore",
-    resolveId(importee, importer) {
-      // Only use ignore to intercept imports that we don't control
-      // inside node_module dependencies.
-      if (
-        importee.endsWith("commonjsHelpers.js") ||
-        importee.endsWith("rollupPluginBabelHelpers.js") ||
-        importee.endsWith("?commonjs-proxy") ||
-        !importer ||
-        !importer.includes("/node_modules/")
-      ) {
-        return null;
-      }
-      let fullPath;
-      try {
-        fullPath = importee.startsWith(".")
-          ? path.resolve(importee, importer)
-          : require.resolve(importee);
-      } catch (err) {
-        console.error("Error in ignore plugin", { importee, importer }, err);
-        throw err;
-      }
-
-      return files.some((toIgnorePath) => fullPath.startsWith(toIgnorePath))
-        ? fullPath
-        : null;
-    },
 
     load(id) {
       return files.some((toIgnorePath) => id.startsWith(toIgnorePath))
