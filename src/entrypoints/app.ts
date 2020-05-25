79316b911b5cf00dc4913de23f12fb76e1636b2f
Paulus Schoutsen <balloob@gmail.com>
2020-05-25 09:55:50 -0700

    Split out HTML imports from entrypoint (#6030)

--- src/entrypoints/app.ts
+++ src/entrypoints/app.ts
@@ -2,7 +2,6 @@ import { setPassiveTouchGestures } from "@polymer/polymer/lib/utils/settings";
 import "../resources/roboto";
 import "../resources/ha-style";
 import "../layouts/home-assistant";
-import "../resources/html-import/polyfill";
 import "../util/legacy-support";
 
 setPassiveTouchGestures(true);
@@ -10,3 +9,5 @@ setPassiveTouchGestures(true);
 document.createElement = Document.prototype.createElement;
 
 (window as any).frontendVersion = __VERSION__;
+
+import("../resources/html-import/polyfill");
