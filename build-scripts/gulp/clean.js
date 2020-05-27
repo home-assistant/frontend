1589cdf11f207dbebe97a5401b32a837dc6d00ae
Paulus Schoutsen <balloob@gmail.com>
2020-05-27 13:55:51 -0700

    Fix hassio dev under rollup

--- build-scripts/gulp/clean.js
+++ build-scripts/gulp/clean.js
@@ -1,39 +1,36 @@
 const del = require("del");
 const gulp = require("gulp");
-const config = require("../paths");
+const paths = require("../paths");
 require("./translations");
 
 gulp.task(
   "clean",
   gulp.parallel("clean-translations", function cleanOutputAndBuildDir() {
-    return del([config.app_output_root, config.build_dir]);
+    return del([paths.app_output_root, paths.build_dir]);
   })
 );
 
 gulp.task(
   "clean-demo",
   gulp.parallel("clean-translations", function cleanOutputAndBuildDir() {
-    return del([config.demo_output_root, config.build_dir]);
+    return del([paths.demo_output_root, paths.build_dir]);
   })
 );
 
 gulp.task(
   "clean-cast",
   gulp.parallel("clean-translations", function cleanOutputAndBuildDir() {
-    return del([config.cast_output_root, config.build_dir]);
+    return del([paths.cast_output_root, paths.build_dir]);
   })
 );
 
-gulp.task(
-  "clean-hassio",
-  gulp.parallel("clean-translations", function cleanOutputAndBuildDir() {
-    return del([config.hassio_output_root, config.build_dir]);
-  })
-);
+gulp.task("clean-hassio", function cleanOutputAndBuildDir() {
+  return del([paths.hassio_output_root, paths.build_dir]);
+});
 
 gulp.task(
   "clean-gallery",
   gulp.parallel("clean-translations", function cleanOutputAndBuildDir() {
-    return del([config.gallery_output_root, config.build_dir]);
+    return del([paths.gallery_output_root, paths.build_dir]);
   })
 );
