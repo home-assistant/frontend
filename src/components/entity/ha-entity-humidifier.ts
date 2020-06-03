cc5a32973fde157f6277713cae92ac179e1a188a
Denis Shulyaka <Shulyaka@gmail.com>
2020-06-04 00:53:51 +0300

    LitElement

---
+++ src/components/entity/ha-entity-humidifier.ts
@@ -0,0 +1,61 @@
+import { css, CSSResult, html, TemplateResult } from "lit-element";
+import { HaEntityToggle } from "./ha-entity-toggle";
+
+class HaEntityHumidifier extends HaEntityToggle {
+  protected render(): TemplateResult {
+    if (!this.stateObj) {
+      return super.render();
+    }
+
+    return html`
+      <div class="target">
+        ${this.stateObj.attributes.mode
+          ? html`<span class="state-label">
+              ${this.hass!.localize(
+                `state_attributes.humidifier.mode.${this.stateObj.attributes.mode}`
+              ) || this.stateObj.attributes.mode}
+            </span>`
+          : ""}
+        <div class="unit">
+          ${this.stateObj.attributes.humidity
+            ? html`${this.stateObj.attributes.humidity} %`
+            : ""}
+        </div>
+      </div>
+
+      ${super.render()}
+    `;
+  }
+
+  static get styles(): CSSResult {
+    return css`
+      :host {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+      }
+
+      .target {
+        color: var(--primary-text-color);
+      }
+
+      .current {
+        color: var(--secondary-text-color);
+      }
+
+      .state-label {
+        font-weight: bold;
+        text-transform: capitalize;
+      }
+
+      .unit {
+        display: inline-block;
+        direction: ltr;
+      }
+      ${super.styles}
+    `;
+  }
+}
+
+customElements.define("ha-entity-humidifier", HaEntityHumidifier);
