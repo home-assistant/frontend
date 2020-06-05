bec150eac87b15166fa269196a7a54d567b76e07
Paulus Schoutsen <balloob@gmail.com>
2020-06-05 02:03:11 -0700

    Cleanup of builds (#6106)

--- gallery/webpack.config.js
+++ gallery/webpack.config.js
@@ -1,5 +1,8 @@
 const { createGalleryConfig } = require("../build-scripts/webpack.js");
+const { isProdBuild, isStatsBuild } = require("../build-scripts/env.js");
 
 module.exports = createGalleryConfig({
+  isProdBuild: isProdBuild(),
+  isStatsBuild: isStatsBuild(),
   latestBuild: true,
 });
