2d802cb196f9b058abba591b3ffe2289484f7706
Bram Kragten <mail@bramkragten.nl>
2020-06-20 14:52:56 +0200

    Move MQTT dev tools to integrations (#6189)

--- src/panels/config/devices/device-detail/integration-elements/mqtt/show-dialog-mqtt-device-debug-info.ts
+++ src/panels/config/devices/device-detail/integration-elements/mqtt/show-dialog-mqtt-device-debug-info.ts
@@ -1,5 +1,5 @@
-import { fireEvent } from "../../common/dom/fire_event";
-import { DeviceRegistryEntry } from "../../data/device_registry";
+import { fireEvent } from "../../../../../../common/dom/fire_event";
+import { DeviceRegistryEntry } from "../../../../../../data/device_registry";
 
 export interface MQTTDeviceDebugInfoDialogParams {
   device: DeviceRegistryEntry;
