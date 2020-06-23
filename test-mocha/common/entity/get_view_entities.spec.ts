1c32924db26eccdb91bc15da9d2a908b4fcdf713
Paulus Schoutsen <balloob@gmail.com>
2020-06-23 15:09:54 -0700

    Remove support for deprecated hidden property (#6220)
    * Remove support for deprecated hidden property
    
    * Lint
    
    * Make sure to keep zones out of generated LL

--- test-mocha/common/entity/get_view_entities.spec.ts
+++ test-mocha/common/entity/get_view_entities.spec.ts
@@ -4,7 +4,6 @@ import { getViewEntities } from "../../../src/common/entity/get_view_entities";
 
 import {
   createEntities,
-  createEntity,
   createGroup,
   createView,
   entityMap,
@@ -44,35 +43,6 @@ describe("getViewEntities", () => {
       expectedEntities,
       entityMap(group2.attributes.entity_id.map((ent) => entities[ent]))
     );
-
-    assert.deepEqual(expectedEntities, getViewEntities(entities, view));
-  });
-
-  it("should not include hidden entities inside groups", () => {
-    const visibleEntity = createEntity({ attributes: { hidden: false } });
-    const hiddenEntity = createEntity({ attributes: { hidden: true } });
-    const group1 = createGroup({
-      attributes: {
-        entity_id: [visibleEntity.entity_id, hiddenEntity.entity_id],
-      },
-    });
-
-    const entities = {
-      [visibleEntity.entity_id]: visibleEntity,
-      [hiddenEntity.entity_id]: hiddenEntity,
-      [group1.entity_id]: group1,
-    };
-
-    const view = createView({
-      attributes: {
-        entity_id: [group1.entity_id],
-      },
-    });
-
-    const expectedEntities = {
-      [visibleEntity.entity_id]: visibleEntity,
-      [group1.entity_id]: group1,
-    };
 
     assert.deepEqual(expectedEntities, getViewEntities(entities, view));
   });
