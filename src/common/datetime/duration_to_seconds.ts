685d28229ee90a473e6e3a000427ad8d3734eb90
Bram Kragten <mail@bramkragten.nl>
2020-07-27 19:04:25 +0200

    Fix for displaying time left for values > 24 hour

--- src/common/datetime/duration_to_seconds.ts
+++ src/common/datetime/duration_to_seconds.ts
@@ -1,4 +1,10 @@
 export default function durationToSeconds(duration: string): number {
+  let days = 0;
+  if (duration.includes("day")) {
+    const position = duration.indexOf("day");
+    days = Number(duration.substr(0, position));
+    duration = duration.split(",")[1];
+  }
   const parts = duration.split(":").map(Number);
-  return parts[0] * 3600 + parts[1] * 60 + parts[2];
+  return (days * 24 + parts[0]) * 3600 + parts[1] * 60 + parts[2];
 }
