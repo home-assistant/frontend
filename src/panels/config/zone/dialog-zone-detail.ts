e08b2817bab9610143a176c64fd8ee4aa8ac8844
Bram Kragten <mail@bramkragten.nl>
2020-08-03 02:08:05 +0200

    Move view edit dialog to mwc (#6479)

--- src/panels/config/zone/dialog-zone-detail.ts
+++ src/panels/config/zone/dialog-zone-detail.ts
@@ -99,9 +99,9 @@ class DialogZoneDetail extends LitElement {
     return html`
       <ha-dialog
         open
-        @closed="${this.closeDialog}"
-        scrimClickAction=""
-        escapeKeyAction=""
+        @closed=${this.closeDialog}
+        scrimClickAction
+        escapeKeyAction
         .heading=${createCloseHeading(
           this.hass,
           this._params.entry
