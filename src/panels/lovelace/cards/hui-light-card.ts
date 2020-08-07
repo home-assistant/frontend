b30e467685885b7510381e085dc15b30761f0d9c
Bram Kragten <mail@bramkragten.nl>
2020-08-07 11:43:20 +0200

    Fix z-index light and thermostat more info button (#6561)

--- src/panels/lovelace/cards/hui-light-card.ts
+++ src/panels/lovelace/cards/hui-light-card.ts
@@ -32,6 +32,7 @@ import { hasConfigOrEntityChanged } from "../common/has-changed";
 import { createEntityNotFoundWarning } from "../components/hui-warning";
 import { LovelaceCard, LovelaceCardEditor } from "../types";
 import { LightCardConfig } from "./types";
+import { mdiDotsVertical } from "@mdi/js";
 
 @customElement("hui-light-card")
 export class HuiLightCard extends LitElement implements LovelaceCard {
@@ -101,12 +102,14 @@ export class HuiLightCard extends LitElement implements LovelaceCard {
 
     return html`
       <ha-card>
-        <ha-icon-button
-          icon="hass:dots-vertical"
+        <mwc-icon-button
           class="more-info"
+          label="Open more info"
           @click=${this._handleMoreInfo}
           tabindex="0"
-        ></ha-icon-button>
+        >
+          <ha-svg-icon .path=${mdiDotsVertical}></ha-svg-icon>
+        </mwc-icon-button>
 
         <div class="content">
           <div id="controls">
@@ -284,7 +287,7 @@ export class HuiLightCard extends LitElement implements LovelaceCard {
         right: 0;
         border-radius: 100%;
         color: var(--secondary-text-color);
-        z-index: 25;
+        z-index: 1;
       }
 
       .content {
