ca32ef6a7dc9e90ee7e17d0a612f9affed729361
Bram Kragten <mail@bramkragten.nl>
2020-08-18 23:41:53 +0200

    dragMode move

--- src/dialogs/image-cropper-dialog/image-cropper-dialog.ts
+++ src/dialogs/image-cropper-dialog/image-cropper-dialog.ts
@@ -52,6 +52,7 @@ export class HaImagecropperDialog extends LitElement {
       this._cropper = new Cropper(this._image, {
         aspectRatio: 1,
         viewMode: 1,
+        dragMode: "move",
         ready: () => {
           URL.revokeObjectURL(this._image!.src);
         },
