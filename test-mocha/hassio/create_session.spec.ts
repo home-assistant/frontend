bcc76821a88a5d988262f5307fb36f10198b5e6f
Ludeeus <ludeeus@ludeeus.dev>
2020-08-18 08:14:55 +0000

    Add test

---
+++ test-mocha/hassio/create_session.spec.ts
@@ -0,0 +1,46 @@
+import * as assert from "assert";
+import { createHassioSession } from "../../src/data/hassio/supervisor";
+
+const sessionID = "fhdsu73rh3io4h8f3irhjel8ousafehf8f3yh";
+
+describe("Create hassio session", function () {
+  it("Check session without HTTPS", async function () {
+    // @ts-ignore
+    global.document = {};
+    // @ts-ignore
+    global.location = {};
+    await createHassioSession({
+      // @ts-ignore
+      callApi: async function () {
+        return { data: { session: sessionID } };
+      },
+    });
+    assert.deepEqual(
+      // @ts-ignore
+      global.document.cookie,
+      "ingress_session=fhdsu73rh3io4h8f3irhjel8ousafehf8f3yh;path=/api/hassio_ingress/;SameSite=Strict"
+    );
+  });
+  it("Check session with HTTPS", async function () {
+    // @ts-ignore
+    global.document = {};
+    // @ts-ignore
+    global.location = { protocol: "https:" };
+    await createHassioSession({
+      // @ts-ignore
+      callApi: async function () {
+        return { data: { session: sessionID } };
+      },
+    });
+    assert.deepEqual(
+      // @ts-ignore
+      global.document.cookie,
+      "ingress_session=fhdsu73rh3io4h8f3irhjel8ousafehf8f3yh;path=/api/hassio_ingress/;SameSite=Strict;Secure"
+    );
+  });
+  // Clean up in case they will be used in other tests
+  // @ts-ignore
+  global.document = {};
+  // @ts-ignore
+  global.location = {};
+});
