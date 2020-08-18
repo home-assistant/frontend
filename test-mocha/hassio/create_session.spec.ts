8e01b56e2cd7aa310a23201001b0a9f297e85f3c
Ludeeus <ludeeus@ludeeus.dev>
2020-08-18 08:16:17 +0000

    move cleanup

--- test-mocha/hassio/create_session.spec.ts
+++ test-mocha/hassio/create_session.spec.ts
@@ -37,10 +37,11 @@ describe("Create hassio session", function () {
       global.document.cookie,
       "ingress_session=fhdsu73rh3io4h8f3irhjel8ousafehf8f3yh;path=/api/hassio_ingress/;SameSite=Strict;Secure"
     );
+
+    // Clean up in case they will be used in other tests
+    // @ts-ignore
+    global.document = {};
+    // @ts-ignore
+    global.location = {};
   });
-  // Clean up in case they will be used in other tests
-  // @ts-ignore
-  global.document = {};
-  // @ts-ignore
-  global.location = {};
 });
