bcc0a9fb00ab4bb2bd5ed9f7f9538f5059d7d71a
Paulus Schoutsen <balloob@gmail.com>
2020-08-20 15:34:52 +0200

    Add tag config panel (#6601)
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>

---
+++ src/data/tag.ts
@@ -0,0 +1,57 @@
+import { HomeAssistant } from "../types";
+import { HassEventBase } from "home-assistant-js-websocket";
+
+export const EVENT_TAG_SCANNED = "tag_scanned";
+
+export interface TagScannedEvent extends HassEventBase {
+  event_type: "tag_scanned";
+  data: {
+    tag_id: string;
+    device_id?: string;
+  };
+}
+
+export interface Tag {
+  id: string;
+  name?: string;
+  description?: string;
+  last_scanned?: string;
+}
+
+export interface UpdateTagParams {
+  name?: Tag["name"];
+  description?: Tag["description"];
+}
+
+export const fetchTags = async (hass: HomeAssistant) =>
+  hass.callWS<Tag[]>({
+    type: "tag/list",
+  });
+
+export const createTag = async (
+  hass: HomeAssistant,
+  params: UpdateTagParams,
+  tagId?: string
+) =>
+  hass.callWS<Tag>({
+    type: "tag/create",
+    tag_id: tagId,
+    ...params,
+  });
+
+export const updateTag = async (
+  hass: HomeAssistant,
+  tagId: string,
+  params: UpdateTagParams
+) =>
+  hass.callWS<Tag>({
+    ...params,
+    type: "tag/update",
+    tag_id: tagId,
+  });
+
+export const deleteTag = async (hass: HomeAssistant, tagId: string) =>
+  hass.callWS<void>({
+    type: "tag/delete",
+    tag_id: tagId,
+  });
