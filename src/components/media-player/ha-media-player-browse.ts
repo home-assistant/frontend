cde4ee937bf20201dde9330544933ffb88104c06
Zack Arnett <arnett.zackary@gmail.com>
2020-08-20 20:27:04 -0500

    Visual updates

--- src/components/media-player/ha-media-player-browse.ts
+++ src/components/media-player/ha-media-player-browse.ts
@@ -1,3 +1,7 @@
+import "@material/mwc-button/mwc-button";
+import "@material/mwc-fab/mwc-fab";
+import "@polymer/paper-listbox/paper-listbox";
+import "@polymer/paper-item/paper-item";
 import {
   LitElement,
   html,
@@ -7,14 +11,19 @@ import {
   internalProperty,
   css,
 } from "lit-element";
-import { mdiPlay, mdiFolder } from "@mdi/js";
+import { mdiPlay, mdiFolder, mdiArrowLeft } from "@mdi/js";
 
 import { haStyle } from "../../resources/styles";
-import { MediaPlayerItem, browseMediaPlayer } from "../../data/media-player";
+import { browseMediaPlayer } from "../../data/media-player";
+import { debounce } from "../../common/util/debounce";
+import { installResizeObserver } from "../../panels/lovelace/common/install-resize-observer";
+import type { MediaPlayerItem } from "../../data/media-player";
 import type { HomeAssistant } from "../../types";
 
 import "../ha-circular-progress";
 import "../ha-svg-icon";
+import "../ha-card";
+import "../ha-paper-dropdown-menu";
 
 @customElement("ha-media-player-browse")
 export class HaMediaPlayerBrowse extends LitElement {
@@ -26,9 +35,25 @@ export class HaMediaPlayerBrowse extends LitElement {
 
   @property() public mediaContentType?: string;
 
+  @property({ type: Boolean, attribute: "narrow", reflect: true })
+  private _narrow = false;
+
   @internalProperty() private _loading = false;
 
   @internalProperty() private _mediaPlayerItems: MediaPlayerItem[] = [];
+
+  private _resizeObserver?: ResizeObserver;
+
+  public connectedCallback(): void {
+    super.connectedCallback();
+    this.updateComplete.then(() => this._attachObserver());
+  }
+
+  public disconnectedCallback(): void {
+    if (this._resizeObserver) {
+      this._resizeObserver.disconnect();
+    }
+  }
 
   public render() {
     if (!this._mediaPlayerItems.length) {
@@ -48,51 +73,138 @@ export class HaMediaPlayerBrowse extends LitElement {
         : undefined;
 
     return html`
-      <div class="breadcrumb">
-        <h2>${mostRecentItem.title}</h2>
-        ${previousItem // navigating back to root doesn't work as the root id is a UUID and overload int(10), JJ is aware
-          ? html`
-              <div
-                .previous=${true}
-                .item=${previousItem}
-                @click=${this._navigate}
-              >
-                ${previousItem.title}
-              </div>
-            `
-          : ""}
-      </div>
-      ${mostRecentItem.children?.length
-        ? html`
-            ${mostRecentItem.children.map(
-              (child) => html`
-                <div class="child" @click=${this._navigate} .item=${child}>
-                  <div class="image-icon">
-                    ${child.thumbnail
-                      ? html`<img .src=${child.thumbnail} />` // If no thumbnail, display icon of media type
-                      : child.can_expand
-                      ? html`<ha-svg-icon .path=${mdiFolder}></ha-svg-icon>`
-                      : ""}
-                  </div>
-                  <div class="title-type">
-                    <div class="title">${child.title}</div>
-                    <div class="type">${child.media_content_type}</div>
-                  </div>
-                  ${child.can_play
+      <div class="header">
+        <div class="header-content">
+          ${mostRecentItem.thumbnail
+            ? html`
+                <div
+                  class="img"
+                  style="background-image: url(${mostRecentItem.thumbnail})"
+                >
+                  ${this._narrow && mostRecentItem?.can_play
                     ? html`
-                        <ha-svg-icon
-                          title="Play Media"
-                          .path=${mdiPlay}
+                        <mwc-fab
+                          mini
+                          .item=${mostRecentItem}
                           @click=${this._playMedia}
-                        ></ha-svg-icon>
+                        >
+                          <ha-svg-icon
+                            slot="icon"
+                            path=${mdiPlay}
+                          ></ha-svg-icon>
+                          <!-- Lokalize -->
+                          Play
+                        </mwc-fab>
                       `
                     : ""}
                 </div>
               `
-            )}
-          `
-        : ""}
+            : ""}
+          <div class="header-info">
+            <div class="breadcrumb">
+              ${previousItem
+                ? html`
+                    <div
+                      class="previous-title"
+                      .previous=${true}
+                      .item=${previousItem}
+                      @click=${this._navigate}
+                    >
+                      <ha-svg-icon .path=${mdiArrowLeft}></ha-svg-icon>
+                      ${previousItem.title}
+                    </div>
+                  `
+                : ""}
+              <div class="title">${mostRecentItem.title}</div>
+              <div class="subtitle">
+                <!-- Lokalize -->${mostRecentItem.media_content_type}
+              </div>
+            </div>
+            ${!this._narrow && mostRecentItem?.can_play
+              ? html`
+                  <div class="actions">
+                    <mwc-button
+                      raised
+                      .item=${mostRecentItem}
+                      @click=${this._playMedia}
+                    >
+                      <ha-svg-icon slot="icon" path=${mdiPlay}></ha-svg-icon>
+                      <!-- Lokalize -->
+                      Play
+                    </mwc-button>
+                  </div>
+                `
+              : ""}
+          </div>
+        </div>
+        <div class="media-source">
+          <ha-paper-dropdown-menu .label=${"Media Source"}>
+            <paper-listbox
+              slot="dropdown-content"
+              .selected=${this.entityId}
+              @iron-select=${() => console.log()}
+              attr-for-selected="item-name"
+            >
+              <paper-item .itemName=${this.entityId}
+                >${this.hass.states[this.entityId].attributes
+                  .friendly_name}</paper-item
+              >
+            </paper-listbox>
+          </ha-paper-dropdown-menu>
+        </div>
+      </div>
+      <div class="divider"></div>
+      <div class="children">
+        ${mostRecentItem.children?.length
+          ? html`
+              ${mostRecentItem.children.map(
+                (child) => html`
+                  <div class="child" @click=${this._navigate} .item=${child}>
+                    <div class="ha-card-parent">
+                      <ha-card
+                        style="background-image: url(${child.thumbnail})"
+                      >
+                        ${child.can_expand && !child.thumbnail
+                          ? html`
+                              <ha-svg-icon
+                                class="folder"
+                                .path=${mdiFolder}
+                              ></ha-svg-icon>
+                            `
+                          : ""}
+                      </ha-card>
+                      <div class="ha-card-copy">
+                        ${child.can_play
+                          ? html`
+                              <ha-svg-icon
+                                title="Play Media"
+                                class="play"
+                                .item=${child}
+                                .path=${mdiPlay}
+                                @click=${this._playMedia}
+                              ></ha-svg-icon>
+                            `
+                          : ""}
+                      </div>
+                    </div>
+                    <div class="title-type">
+                      <div class="title">${child.title}</div>
+                      <div class="type">
+                        <!-- Lokalize -->${child.media_content_type}
+                      </div>
+                    </div>
+                  </div>
+                `
+              )}
+            `
+          : ""}
+      </div>
     `;
+  }
+
+  protected firstUpdated(): void {
+    this._measureCard();
+    this._attachObserver();
   }
 
   public updated(changedProps: PropertyValues) {
@@ -109,11 +221,26 @@ export class HaMediaPlayerBrowse extends LitElement {
     this._fetchData(this.mediaContentId, this.mediaContentType);
   }
 
+  private _measureCard() {
+    this._narrow = this.offsetWidth < 600;
+  }
+
+  private async _attachObserver(): Promise<void> {
+    if (!this._resizeObserver) {
+      await installResizeObserver();
+      this._resizeObserver = new ResizeObserver(
+        debounce(() => this._measureCard(), 250, false)
+      );
+    }
+
+    this._resizeObserver.observe(this);
+  }
+
   private async _fetchData(mediaContentId?: string, mediaContentType?: string) {
     const itemData = await browseMediaPlayer(
       this.hass,
       this.entityId,
-      !mediaContentId ? undefined : mediaContentId.toString(), // Change after JJ fixes response to alway be a string (would rather keep it consistent than patch frontend)
+      !mediaContentId ? undefined : mediaContentId,
       mediaContentType
     );
     this._mediaPlayerItems = [...this._mediaPlayerItems, itemData];
@@ -134,8 +261,7 @@ export class HaMediaPlayerBrowse extends LitElement {
 
   private _playMedia(ev: MouseEvent) {
     ev.stopPropagation();
-    const item: MediaPlayerItem = ((ev.currentTarget as HTMLElement)!
-      .parentElement as any).item;
+    const item = (ev.currentTarget as any).item;
     this.hass.callService("media_player", "play_media", {
       entity_id: this.entityId,
       media_content_id: item.media_content_id,
@@ -147,49 +273,190 @@ export class HaMediaPlayerBrowse extends LitElement {
     return [
       haStyle,
       css`
+        :host {
+          display: block;
+          padding: 16px;
+        }
+
+        .header {
+          display: flex;
+          justify-content: space-between;
+        }
+
+        .media-source {
+          display: flex;
+          justify-content: flex-end;
+          flex-direction: column;
+        }
+
+        .header-content {
+          display: flex;
+          flex-wrap: wrap;
+          flex-grow: 1;
+          align-items: flex-start;
+        }
+
+        .header-content .img {
+          height: 200px;
+          width: 200px;
+          margin-right: 16px;
+          background-size: cover;
+        }
+
+        .header-info {
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+          align-self: stretch;
+        }
+
+        .header-info .actions {
+          padding-top: 24px;
+          --mdc-theme-primary: var(--accent-color);
+        }
+
         .breadcrumb {
           display: flex;
           flex-direction: column;
-          padding: 8px 4px;
+          overflow: hidden;
         }
 
-        .breadcrumb h2 {
-          margin-bottom: 0;
+        .breadcrumb .title {
+          font-size: 48px;
+          line-height: 1.2;
+          font-weight: bold;
+          margin: 0;
+          overflow: hidden;
+          text-overflow: ellipsis;
         }
 
-        .breadcrumb div {
+        .breadcrumb .previous-title {
+          font-size: 14px;
+          padding-bottom: 8px;
           color: var(--secondary-text-color);
+          overflow: hidden;
+          text-overflow: ellipsis;
           cursor: pointer;
+          --mdc-icon-size: 14px;
+        }
+
+        .breadcrumb .subtitle {
+          font-size: 16px;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          text-transform: capitalize;
+        }
+
+        .divider {
+          padding: 10px 0;
+        }
+
+        .divider::before {
+          height: 1px;
+          display: block;
+          background-color: var(--divider-color);
+          content: " ";
+        }
+
+        /* ============= Narrow Header ============= */
+
+        :host([narrow]) .header,
+        :host([narrow]) .header-content {
+          flex-direction: column;
+        }
+
+        :host([narrow]) .header-content .img {
+          height: auto;
+          width: 100%;
+          margin-right: 0;
+          padding-bottom: 100%;
+          margin-bottom: 8px;
+          position: relative;
+        }
+
+        :host([narrow]) .header-content .img mwc-fab {
+          position: absolute;
+          bottom: -20px;
+          right: 20px;
+        }
+
+        /* ============= CHILDREN ============= */
+
+        .children {
+          display: grid;
+          grid-template-columns: repeat(
+            auto-fit,
+            var(--media-browse-item-size, 125px)
+          );
+          grid-gap: 16px;
+          margin: 8px 0px;
         }
 
         .child {
           display: flex;
-          padding: 8px 4px;
+          flex-direction: column;
           align-items: center;
           cursor: pointer;
         }
 
-        .child > div {
-          padding: 0 4px;
+        .ha-card-parent {
+          position: relative;
         }
 
-        .child .image-icon {
-          width: 45px;
-          height: 45px;
-          --mdc-icon-size: 45px;
+        ha-card,
+        .ha-card-copy {
+          position: relative;
+          width: var(--media-browse-item-size, 125px);
+          height: var(--media-browse-item-size, 125px);
+          background-size: cover;
+          background-position: center;
         }
 
-        .child .image-icon img {
-          width: 100%;
+        .ha-card-copy {
+          position: absolute;
+          top: 0;
+        }
+
+        .child .folder,
+        .child .play {
+          position: absolute;
+          top: calc(50% - (var(--mdc-icon-size) / 2));
+          left: calc(50% - (var(--mdc-icon-size) / 2));
+          --mdc-icon-size: calc(var(--media-browse-item-size, 125px) * 0.4);
+        }
+
+        .child .folder {
+          color: var(--sidebar-icon-color);
+        }
+
+        .child .play {
+          opacity: 0;
+          transition: all 0.5s;
+        }
+
+        .child .play:hover {
+          color: var(--accent-color);
+        }
+
+        .ha-card-parent:hover > ha-card {
+          opacity: 0.5;
+        }
+
+        .ha-card-parent:hover > .ha-card-copy > .play {
+          opacity: 1;
         }
 
         .child .title-type {
-          max-width: 250px;
           width: 100%;
         }
 
         .child .title {
-          font-size: 18px;
+          font-size: 16px;
+          padding-top: 8px;
+          overflow: hidden;
+          display: -webkit-box;
+          -webkit-box-orient: vertical;
+          -webkit-line-clamp: 2;
         }
 
         .child .type {
