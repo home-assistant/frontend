2c713706941cd2d3bb138a047f1d2f0d89d44ca1
Ludeeus <ludeeus@ludeeus.dev>
2020-08-25 08:31:17 +0000

    Add types

--- src/data/hassio/network.ts
+++ src/data/hassio/network.ts
@@ -1,8 +1,41 @@
 import { HomeAssistant } from "../../types";
 import { hassioApiResultExtractor, HassioResponse } from "./common";
 
+export interface NetworkInterface {
+  gateway: string;
+  id: string;
+  ip_address: string;
+  method: "static" | "dhcp";
+  nameservers: string[];
+  primary: boolean;
+  type: string;
+}
+
+export interface NetworkInterfaces {
+  [key: string]: NetworkInterface;
+}
+
+export interface NetworkInfo {
+  interfaces: NetworkInterfaces;
+}
+
 export const fetchNetworkInfo = async (hass: HomeAssistant) => {
   return hassioApiResultExtractor(
-    await hass.callApi<HassioResponse<void>>("GET", "hassio/network/info")
+    await hass.callApi<HassioResponse<NetworkInfo>>(
+      "GET",
+      "hassio/network/info"
+    )
+  );
+};
+
+export const updateNetworkInterface = async (
+  hass: HomeAssistant,
+  network_interface: string,
+  options: Partial<NetworkInterface>
+) => {
+  await hass.callApi<HassioResponse<NetworkInfo>>(
+    "POST",
+    `hassio/network/interface/${network_interface}/update`,
+    options
   );
 };
