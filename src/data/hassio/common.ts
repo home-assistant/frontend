c580e9d5ede4913a893e0fc4b5bad2167800a478
Joakim Sørensen <joasoe@gmail.com>
2020-09-03 16:38:59 +0200

    hassio-addon-info feedback (#6734)
    * hassio-addon-info feedback
    
    * lint
    
    * init config validation
    
    * better error
    
    * Finish
    
    * sort imports
    
    * Use startup type for watchdog
    
    * Only show error if issue with config
    
    * Adjust

--- src/data/hassio/common.ts
+++ src/data/hassio/common.ts
@@ -5,3 +5,11 @@ export interface HassioResponse<T> {
 
 export const hassioApiResultExtractor = <T>(response: HassioResponse<T>) =>
   response.data;
+
+export const extractApiErrorMessage = (error: any): string => {
+  return typeof error === "object"
+    ? typeof error.body === "object"
+      ? error.body.message || "Unkown error, see logs"
+      : error.body || "Unkown error, see logs"
+    : error;
+};
