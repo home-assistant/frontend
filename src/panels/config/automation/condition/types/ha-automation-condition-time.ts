af0fe76aa32ee99a9ece50dced70c18d87a8e163
Misiu <t.jagusz@gmail.com>
2020-09-04 16:19:49 +0200

    review fixes part2

--- src/panels/config/automation/condition/types/ha-automation-condition-time.ts
+++ src/panels/config/automation/condition/types/ha-automation-condition-time.ts
@@ -18,26 +18,21 @@ import {
   handleChangeEvent,
 } from "../ha-automation-condition-row";
 
-interface Weekday {
-  name: string;
-  order: number;
-}
+const DAYS = {
+  mon: 1,
+  tue: 2,
+  wed: 3,
+  thu: 4,
+  fri: 5,
+  sat: 6,
+  sun: 7,
+};
 
 @customElement("ha-automation-condition-time")
 export class HaTimeCondition extends LitElement implements ConditionElement {
   @property({ attribute: false }) public hass!: HomeAssistant;
 
   @property({ attribute: false }) public condition!: TimeCondition;
-
-  private _days: Weekday[] = [
-    { name: "mon", order: 1 },
-    { name: "tue", order: 2 },
-    { name: "wed", order: 3 },
-    { name: "thu", order: 4 },
-    { name: "fri", order: 5 },
-    { name: "sat", order: 6 },
-    { name: "sun", order: 7 },
-  ];
 
   public static get defaultConfig() {
     return {};
@@ -63,20 +58,20 @@ export class HaTimeCondition extends LitElement implements ConditionElement {
         @value-changed=${this._valueChanged}
       ></paper-input>
 
-      ${this._days.map(
+      ${Object.keys(DAYS).map(
         (day) => html`
           <ha-formfield
             alignEnd
             spaceBetween
             class="flex"
             .label=${this.hass!.localize(
-              `ui.panel.config.automation.editor.conditions.type.time.weekdays.${day.name}`
+              `ui.panel.config.automation.editor.conditions.type.time.weekdays.${day}`
             )}
             .dir=${computeRTLDirection(this.hass!)}
           >
             <ha-switch
-              .day=${day.name}
-              .checked=${weekday ? weekday.includes(day.name) : false}
+              .day=${day}
+              .checked=${weekday?.includes(day)}
               @change=${this._dayValueChanged}
             >
             </ha-switch>
@@ -107,13 +102,7 @@ export class HaTimeCondition extends LitElement implements ConditionElement {
       days = days.filter((d) => d !== day);
     }
 
-    days.sort((a: string, b: string) => {
-      const first: number =
-        this._days.find((d) => d.name === b.toLowerCase())?.order ?? 1;
-      const second: number =
-        this._days.find((d) => d.name === a.toLowerCase())?.order ?? 2;
-      return second - first;
-    });
+    days.sort((a: string, b: string) => DAYS[a] - DAYS[b]);
 
     this.condition.weekday = days;
 
