b3af5bda06ded11ac4038c3208c780448a610728
Charles Garwood <cgarwood@gmail.com>
2020-09-04 16:51:05 -0400

    Move buttons to device-actions

--- src/panels/config/devices/device-detail/integration-elements/ozw/ha-device-actions-ozw.ts
+++ src/panels/config/devices/device-detail/integration-elements/ozw/ha-device-actions-ozw.ts
@@ -14,13 +14,15 @@ import { haStyle } from "../../../../../../resources/styles";
 import { HomeAssistant } from "../../../../../../types";
 import {
   OZWDevice,
-  fetchOZWNodeStatus,
   getIdentifiersFromDevice,
   OZWNodeIdentifiers,
 } from "../../../../../../data/ozw";
+import { showOZWRefreshNodeDialog } from "../../../../integrations/integration-panels/ozw/show-dialog-ozw-refresh-node";
+import { navigate } from "../../../../../../common/navigate";
+import "@material/mwc-button/mwc-button";
 
-@customElement("ha-device-info-ozw")
-export class HaDeviceInfoOzw extends LitElement {
+@customElement("ha-device-actions-ozw")
+export class HaDeviceActionsOzw extends LitElement {
   @property({ attribute: false }) public hass!: HomeAssistant;
 
   @property() public device!: DeviceRegistryEntry;
@@ -43,58 +45,45 @@ export class HaDeviceInfoOzw extends LitElement {
       }
       this.ozw_instance = identifiers.ozw_instance;
       this.node_id = identifiers.node_id;
-
-      this._fetchNodeDetails();
     }
-  }
-
-  protected async _fetchNodeDetails() {
-    this._ozwDevice = await fetchOZWNodeStatus(
-      this.hass,
-      this.ozw_instance,
-      this.node_id
-    );
   }
 
   protected render(): TemplateResult {
-    if (!this._ozwDevice) {
+    if (!this.ozw_instance || !this.node_id) {
       return html``;
     }
     return html`
-      <h4>
-        ${this.hass.localize("ui.panel.config.ozw.device_info.zwave_info")}
-      </h4>
-      <div>
-        ${this.hass.localize("ui.panel.config.ozw.common.node_id")}:
-        ${this._ozwDevice.node_id}
-      </div>
-      <div>
-        ${this.hass.localize("ui.panel.config.ozw.device_info.stage")}:
-        ${this._ozwDevice.node_query_stage}
-      </div>
-      <div>
-        ${this.hass.localize("ui.panel.config.ozw.common.ozw_instance")}:
-        ${this._ozwDevice.ozw_instance}
-      </div>
-      <div>
-        ${this.hass.localize("ui.panel.config.ozw.device_info.node_failed")}:
-        ${this._ozwDevice.is_failed
-          ? this.hass.localize("ui.common.yes")
-          : this.hass.localize("ui.common.no")}
-      </div>
+      <mwc-button @click=${this._nodeDetailsClicked}>
+        ${this.hass.localize("ui.panel.config.ozw.node.button")}
+      </mwc-button>
+      <mwc-button @click=${this._refreshNodeClicked}>
+        ${this.hass.localize("ui.panel.config.ozw.refresh_node.button")}
+      </mwc-button>
     `;
+  }
+
+  private async _refreshNodeClicked() {
+    showOZWRefreshNodeDialog(this, {
+      node_id: this.node_id,
+      ozw_instance: this.ozw_instance,
+    });
+  }
+
+  private async _nodeDetailsClicked() {
+    navigate(
+      this,
+      `/config/ozw/network/${this.ozw_instance}/node/${this.node_id}/dashboard`
+    );
   }
 
   static get styles(): CSSResult[] {
     return [
       haStyle,
       css`
-        h4 {
-          margin-bottom: 4px;
-        }
-        div {
-          word-break: break-all;
-          margin-top: 2px;
+        :host {
+          display: flex;
+          flex-direction: column;
+          align-items: flex-start;
         }
       `,
     ];
