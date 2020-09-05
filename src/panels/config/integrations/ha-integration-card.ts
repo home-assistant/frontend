591e9fa98a0c95286d0a8faa919d501a742df205
Ludeeus <ludeeus@ludeeus.dev>
2020-09-05 21:21:21 +0000

    Add comma

--- src/panels/config/integrations/ha-integration-card.ts
+++ src/panels/config/integrations/ha-integration-card.ts
@@ -184,6 +184,7 @@ export class HaIntegrationCard extends LitElement {
                         >
                       `
                     : ""}
+                  ${devices.length || services.length ? "," : ""}
                   ${services.length
                     ? html`
                         <a
