112b72f089e560599f661243f5eb65bac8b684f5
Tomasz <t.jagusz@gmail.com>
2020-09-08 09:34:17 +0200

    simplify
    Co-authored-by: Zack Barett <zackbarett@hey.com>

--- src/panels/config/automation/condition/types/ha-automation-condition-time.ts
+++ src/panels/config/automation/condition/types/ha-automation-condition-time.ts
@@ -163,10 +163,7 @@ export class HaTimeCondition extends LitElement implements ConditionElement {
 
     const daySwitch = ev.currentTarget as WeekdayHaSwitch;
 
-    let days = this.condition.weekday;
-    if (!days) {
-      days = [];
-    }
+    let days = this.condition.weekday || [];
 
     if (daySwitch.checked) {
       days.push(daySwitch.day);
