be18313aeb3bc41904e3a205a41ce58a1f262321
J. Nick Koston <nick@koston.org>
2020-09-11 14:36:39 -0500

    Show which state changed events a template listeners for in dev tools (#6939)
    * Show which state changed events a template listeners for in dev tools
    
    * Update src/data/ws-templates.ts
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>
    
    * Update src/data/ws-templates.ts
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>
    
    * Update src/panels/developer-tools/template/developer-tools-template.ts
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>
    
    * Update src/panels/developer-tools/template/developer-tools-template.ts
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>
    
    * Update src/panels/developer-tools/template/developer-tools-template.ts
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>
    
    * Update src/panels/developer-tools/template/developer-tools-template.ts
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>
    
    * merge
    
    * Update src/panels/lovelace/cards/hui-markdown-card.ts
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>
    
    * fix string reversal
    
    * Update src/panels/lovelace/cards/hui-markdown-card.ts
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>
    
    * Update src/panels/developer-tools/template/developer-tools-template.ts
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>
    
    * error to string
    
    * cleanup
    
    * cleanup
    
    * no listeners is probably worth warning about as well
    
    * handle unknown error
    
    * fix error alignment in pre
    
    * fix error alignment in pre
    
    * fix error alignment in pre
    
    * fix error alignment in pre
    
    * reformat
    
    * reformat
    
    * reformat
    
    * fix accidential revert
    
    * Update src/panels/developer-tools/template/developer-tools-template.ts
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>
    
    * Update src/panels/developer-tools/template/developer-tools-template.ts
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>
    
    * clear error on success
    
    * tweak to not error if listeners are not returned
    
    * Update src/panels/developer-tools/template/developer-tools-template.ts
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>

--- src/panels/developer-tools/template/developer-tools-template.ts
+++ src/panels/developer-tools/template/developer-tools-template.ts
@@ -13,7 +13,10 @@ import { classMap } from "lit-html/directives/class-map";
 import { debounce } from "../../../common/util/debounce";
 import "../../../components/ha-circular-progress";
 import "../../../components/ha-code-editor";
-import { subscribeRenderTemplate } from "../../../data/ws-templates";
+import {
+  subscribeRenderTemplate,
+  RenderTemplateResult,
+} from "../../../data/ws-templates";
 import { haStyle } from "../../../resources/styles";
 import { HomeAssistant } from "../../../types";
 
@@ -44,11 +47,11 @@ class HaPanelDevTemplate extends LitElement {
 
   @property() public narrow!: boolean;
 
-  @internalProperty() private _error = false;
+  @internalProperty() private _error?: string;
 
   @internalProperty() private _rendering = false;
 
-  @internalProperty() private _processed = "";
+  @internalProperty() private _templateResult?: RenderTemplateResult;
 
   @internalProperty() private _unsubRenderTemplate?: Promise<UnsubscribeFunc>;
 
@@ -139,9 +142,57 @@ class HaPanelDevTemplate extends LitElement {
             .active=${this._rendering}
             size="small"
           ></ha-circular-progress>
-          <pre class="rendered ${classMap({ error: this._error })}">
-${this._processed}</pre
-          >
+
+          <pre
+            class="rendered ${classMap({ error: Boolean(this._error) })}"
+          ><!-- display: block -->${this._error}${this._templateResult
+            ?.result}</pre>
+          ${!this._templateResult?.listeners
+            ? ""
+            : this._templateResult.listeners.all
+            ? html`
+                <span class="all_listeners">
+                  ${this.hass.localize(
+                    "ui.panel.developer-tools.tabs.templates.all_listeners"
+                  )}
+                </span>
+              `
+            : this._templateResult.listeners.domains.length ||
+              this._templateResult.listeners.entities.length
+            ? html`
+                ${this.hass.localize(
+                  "ui.panel.developer-tools.tabs.templates.listeners"
+                )}
+                <ul>
+                  ${this._templateResult.listeners.domains.map(
+                    (domain) =>
+                      html`
+                        <li>
+                          ${this.hass.localize(
+                            "ui.panel.developer-tools.tabs.templates.domain"
+                          )}:
+                          ${domain}
+                        </li>
+                      `
+                  )}
+                  ${this._templateResult.listeners.entities.map(
+                    (entity_id) =>
+                      html`
+                        <li>
+                          ${this.hass.localize(
+                            "ui.panel.developer-tools.tabs.templates.entity"
+                          )}:
+                          ${entity_id}
+                        </li>
+                      `
+                  )}
+                </ul>
+              `
+            : html` <span class="all_listeners">
+                ${this.hass.localize(
+                  "ui.panel.developer-tools.tabs.templates.no_listeners"
+                )}
+              </span>`}
         </div>
       </div>
     `;
@@ -189,6 +240,11 @@ ${this._processed}</pre
           @apply --paper-font-code1;
           clear: both;
           white-space: pre-wrap;
+          background-color: var(--secondary-background-color);
+        }
+
+        .all_listeners {
+          color: var(--warning-color);
         }
 
         .rendered.error {
@@ -210,7 +266,7 @@ ${this._processed}</pre
   private _templateChanged(ev) {
     this._template = ev.detail.value;
     if (this._error) {
-      this._error = false;
+      this._error = undefined;
     }
     this._debounceRender();
   }
@@ -222,7 +278,8 @@ ${this._processed}</pre
       this._unsubRenderTemplate = subscribeRenderTemplate(
         this.hass.connection,
         (result) => {
-          this._processed = result;
+          this._templateResult = result;
+          this._error = undefined;
         },
         {
           template: this._template,
@@ -230,9 +287,10 @@ ${this._processed}</pre
       );
       await this._unsubRenderTemplate;
     } catch (err) {
-      this._error = true;
+      this._error = "Unknown error";
       if (err.message) {
-        this._processed = err.message;
+        this._error = err.message;
+        this._templateResult = undefined;
       }
       this._unsubRenderTemplate = undefined;
     } finally {
