f18913b5a04b4fbfc039f1dc19b485300a7cc33c
J. Nick Koston <nick@koston.org>
2020-09-11 14:36:39 -0500

    Show which state changed events a template listeners for in dev tools (#6939)
    * Show which state changed events a template listeners for in dev tools
    
    * Update src/data/ws-templates.ts
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>
    
    * Update src/data/ws-templates.ts
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>
    
    * Update src/panels/developer-tools/template/developer-tools-template.ts
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>
    
    * Update src/panels/developer-tools/template/developer-tools-template.ts
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>
    
    * Update src/panels/developer-tools/template/developer-tools-template.ts
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>
    
    * Update src/panels/developer-tools/template/developer-tools-template.ts
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>
    
    * merge
    
    * Update src/panels/lovelace/cards/hui-markdown-card.ts
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>
    
    * fix string reversal
    
    * Update src/panels/lovelace/cards/hui-markdown-card.ts
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>
    
    * Update src/panels/developer-tools/template/developer-tools-template.ts
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>
    
    * error to string
    
    * cleanup
    
    * cleanup
    
    * no listeners is probably worth warning about as well
    
    * handle unknown error
    
    * fix error alignment in pre
    
    * fix error alignment in pre
    
    * fix error alignment in pre
    
    * fix error alignment in pre
    
    * reformat
    
    * reformat
    
    * reformat
    
    * fix accidential revert
    
    * Update src/panels/developer-tools/template/developer-tools-template.ts
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>
    
    * Update src/panels/developer-tools/template/developer-tools-template.ts
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>
    
    * clear error on success
    
    * tweak to not error if listeners are not returned
    
    * Update src/panels/developer-tools/template/developer-tools-template.ts
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>

--- src/data/ws-templates.ts
+++ src/data/ws-templates.ts
@@ -1,20 +1,27 @@
 import { Connection, UnsubscribeFunc } from "home-assistant-js-websocket";
 
-interface RenderTemplateResult {
+export interface RenderTemplateResult {
   result: string;
+  listeners: TemplateListeners;
+}
+
+interface TemplateListeners {
+  all: boolean;
+  domains: string[];
+  entities: string[];
 }
 
 export const subscribeRenderTemplate = (
   conn: Connection,
-  onChange: (result: string) => void,
+  onChange: (result: RenderTemplateResult) => void,
   params: {
     template: string;
     entity_ids?: string | string[];
     variables?: object;
   }
 ): Promise<UnsubscribeFunc> => {
-  return conn.subscribeMessage(
-    (msg: RenderTemplateResult) => onChange(msg.result),
-    { type: "render_template", ...params }
-  );
+  return conn.subscribeMessage((msg: RenderTemplateResult) => onChange(msg), {
+    type: "render_template",
+    ...params,
+  });
 };
