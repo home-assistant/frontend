dd37cff8d7bbc2f39a8083af901185fb12b98946
Bram Kragten <mail@bramkragten.nl>
2020-09-16 17:17:47 +0200

    Fix safari issues and move scrollable to own container

--- src/panels/media-browser/ha-panel-media-browser.ts
+++ src/panels/media-browser/ha-panel-media-browser.ts
@@ -137,7 +137,7 @@ class PanelMediaBrowser extends LitElement {
           --mdc-theme-primary: var(--app-header-text-color);
         }
         ha-media-player-browse {
-          height: calc(100vh - 84px);
+          height: calc(100vh - 64px);
         }
         :host([narrow]) app-toolbar mwc-button {
           width: 65px;
