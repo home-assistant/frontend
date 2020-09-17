2e481396184ad80b91d8f253d3997e81a95b9eab
Bram Kragten <mail@bramkragten.nl>
2020-09-17 09:50:30 +0200

    Add comment to PR

--- src/entrypoints/core.ts
+++ src/entrypoints/core.ts
@@ -32,10 +32,10 @@ declare global {
   }
 }
 
+// https://github.com/home-assistant/frontend/pull/7031
 const isSafari14 = /^((?!chrome|android).)*version\/14\.0.*safari/i.test(
   navigator.userAgent
 );
-
 if (isSafari14) {
   const origAttachShadow = window.Element.prototype.attachShadow;
   window.Element.prototype.attachShadow = function (init) {
