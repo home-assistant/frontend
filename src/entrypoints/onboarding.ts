6bb41154d173c7c47fae17e6812e732404dcbda5
Bram Kragten <mail@bramkragten.nl>
2020-09-17 10:17:37 +0200

    Add to login and onboarding

--- src/entrypoints/onboarding.ts
+++ src/entrypoints/onboarding.ts
@@ -1,6 +1,6 @@
 // Compat needs to be first import
-import "../resources/compatibility";
 import "../onboarding/ha-onboarding";
+import "../resources/compatibility";
 import "../resources/ha-style";
 import "../resources/roboto";
 
@@ -8,4 +8,18 @@ declare global {
   interface Window {
     stepsPromise: Promise<Response>;
   }
+}
+
+// https://github.com/home-assistant/frontend/pull/7031
+const isSafari14 = /^((?!chrome|android).)*version\/14\.0.*safari/i.test(
+  navigator.userAgent
+);
+if (isSafari14) {
+  const origAttachShadow = window.Element.prototype.attachShadow;
+  window.Element.prototype.attachShadow = function (init) {
+    if (init && init.delegatesFocus) {
+      delete init.delegatesFocus;
+    }
+    return origAttachShadow.apply(this, [init]);
+  };
 }
