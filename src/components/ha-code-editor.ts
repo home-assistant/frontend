6bf0c55598a437e57303d86008dcb488edddd3ef
Kendell R <KTibow@users.noreply.github.com>
2020-09-18 08:23:41 -0700

    Default colon color to primary text color (#7044)
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>

--- src/components/ha-code-editor.ts
+++ src/components/ha-code-editor.ts
@@ -190,7 +190,7 @@ export class HaCodeEditor extends UpdatingElement {
       }
       
       .cm-s-default .cm-meta {
-        color: var(--codemirror-meta, #000);
+        color: var(--codemirror-meta, var(--primary-text-color));
       }
       
       .cm-s-default .cm-attribute {
