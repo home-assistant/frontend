821a54934f2431b07a416fc0f1686466a202defa
Joakim Sørensen <joasoe@gmail.com>
2020-09-22 21:58:31 +0200

    Adds hassio-system-metrics (#7105)

--- src/data/hassio/common.ts
+++ src/data/hassio/common.ts
@@ -1,6 +1,19 @@
+import { HomeAssistant } from "../../types";
+
 export interface HassioResponse<T> {
   data: T;
   result: "ok";
+}
+
+export interface HassioStats {
+  blk_read: number;
+  blk_write: number;
+  cpu_percent: number;
+  memory_limit: number;
+  memory_percent: number;
+  memory_usage: number;
+  network_rx: number;
+  network_tx: number;
 }
 
 export const hassioApiResultExtractor = <T>(response: HassioResponse<T>) =>
@@ -15,3 +28,15 @@ export const extractApiErrorMessage = (error: any): string => {
 };
 
 export const ignoredStatusCodes = new Set([502, 503, 504]);
+
+export const fetchHassioStats = async (
+  hass: HomeAssistant,
+  container: string
+): Promise<HassioStats> => {
+  return hassioApiResultExtractor(
+    await hass.callApi<HassioResponse<HassioStats>>(
+      "GET",
+      `hassio/${container}/stats`
+    )
+  );
+};
