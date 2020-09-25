927aafcbfa9f329d54d217ef95e52657f8c37c62
Ludeeus <ludeeus@ludeeus.dev>
2020-09-25 14:50:20 +0000

    Get discovery information

---
+++ src/data/discovery.ts
@@ -0,0 +1,17 @@
+export interface DiscoveryInformation {
+  uuid: string;
+  base_url: string | null;
+  external_url: string | null;
+  internal_url: string | null;
+  location_name: string;
+  installation_type: string;
+  requires_api_password: boolean;
+  version: string;
+}
+
+export const fetchDiscoveryInformation = async (): Promise<
+  DiscoveryInformation
+> => {
+  const response = await fetch("/api/discovery_info", { method: "GET" });
+  return await response.json();
+};
