4759c89628517cab3e832f6ce933981508964d7f
Bram Kragten <mail@bramkragten.nl>
2020-09-28 13:29:02 +0200

    Bring back lazy load more info + helper (#7131)

--- src/panels/lovelace/custom-card-helpers.ts
+++ src/panels/lovelace/custom-card-helpers.ts
@@ -3,3 +3,4 @@ export { createCardElement } from "./create-element/create-card-element";
 export { createHeaderFooterElement } from "./create-element/create-header-footer-element";
 export { createHuiElement } from "./create-element/create-hui-element";
 export { createRowElement } from "./create-element/create-row-element";
+export { importMoreInfoControl } from "../../dialogs/more-info/state_more_info_control";
