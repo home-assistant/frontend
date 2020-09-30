86a4a8ba02ffdf22036ee041f9339205a8d546e0
Zack Arnett <arnett.zackary@gmail.com>
2020-09-29 19:41:44 -0500

    icon changes

--- src/common/entity/domain_icon.ts
+++ src/common/entity/domain_icon.ts
@@ -4,14 +4,10 @@ import { HassEntity } from "home-assistant-js-websocket";
  *
  * Optionally pass in a state to influence the domain icon.
  */
-import {
-  DEFAULT_DOMAIN_ICON,
-  FIXED_DEVICE_CLASS_ICONS,
-  FIXED_DOMAIN_ICONS,
-  UNIT_C,
-  UNIT_F,
-} from "../const";
-import { batteryIcon } from "./battery_icon";
+import { DEFAULT_DOMAIN_ICON, FIXED_DOMAIN_ICONS } from "../const";
+import { binarySensorIcon } from "./binary_sensor_icon";
+import { coverIcon } from "./cover_icon";
+import { sensorIcon } from "./sensor_icon";
 
 export const domainIcon = (
   domain: string,
@@ -35,133 +31,11 @@ export const domainIcon = (
           return "hass:bell";
       }
 
-    case "binary_sensor": {
-      const is_off = compareState === "off";
-      switch (stateObj?.attributes.device_class) {
-        case "battery":
-          return is_off ? "hass:battery" : "hass:battery-outline";
-        case "battery_charging":
-          return is_off ? "hass:battery" : "hass:battery-charging";
-        case "cold":
-          return is_off ? "hass:thermometer" : "hass:snowflake";
-        case "connectivity":
-          return is_off ? "hass:server-network-off" : "hass:server-network";
-        case "door":
-          return is_off ? "hass:door-closed" : "hass:door-open";
-        case "garage_door":
-          return is_off ? "hass:garage" : "hass:garage-open";
-        case "power":
-          return is_off ? "hass:power-off" : "hass:power-on";
-        case "gas":
-        case "problem":
-        case "safety":
-        case "smoke":
-          return is_off ? "hass:shield-check" : "hass:alert";
-        case "heat":
-          return is_off ? "hass:thermometer" : "hass:fire";
-        case "light":
-          return is_off ? "hass:brightness-5" : "hass:brightness-7";
-        case "lock":
-          return is_off ? "hass:lock" : "hass:lock-open";
-        case "moisture":
-          return is_off ? "hass:water-off" : "hass:water";
-        case "motion":
-          return is_off ? "hass:walk" : "hass:run";
-        case "occupancy":
-          return is_off ? "hass:home-outline" : "hass:home";
-        case "opening":
-          return is_off ? "hass:square" : "hass:square-outline";
-        case "plug":
-          return is_off ? "hass:power-plug-off" : "hass:power-plug";
-        case "presence":
-          return is_off ? "hass:home-outline" : "hass:home";
-        case "sound":
-          return is_off ? "hass:music-note-off" : "hass:music-note";
-        case "vibration":
-          return is_off ? "hass:crop-portrait" : "hass:vibrate";
-        case "window":
-          return is_off ? "hass:window-closed" : "hass:window-open";
-        default:
-          return is_off ? "hass:radiobox-blank" : "hass:checkbox-marked-circle";
-      }
-    }
+    case "binary_sensor":
+      return binarySensorIcon(compareState, stateObj);
 
-    case "cover": {
-      const open = compareState !== "closed";
-
-      switch (stateObj?.attributes.device_class) {
-        case "garage":
-          switch (compareState) {
-            case "opening":
-              return "hass:arrow-up-box";
-            case "closing":
-              return "hass:arrow-down-box";
-            case "closed":
-              return "hass:garage";
-            default:
-              return "hass:garage-open";
-          }
-        case "gate":
-          switch (compareState) {
-            case "opening":
-            case "closing":
-              return "hass:gate-arrow-right";
-            case "closed":
-              return "hass:gate";
-            default:
-              return "hass:gate-open";
-          }
-        case "door":
-          return open ? "hass:door-open" : "hass:door-closed";
-        case "damper":
-          return open ? "hass:circle" : "hass:circle-slice-8";
-        case "shutter":
-          switch (compareState) {
-            case "opening":
-              return "hass:arrow-up-box";
-            case "closing":
-              return "hass:arrow-down-box";
-            case "closed":
-              return "hass:window-shutter";
-            default:
-              return "hass:window-shutter-open";
-          }
-        case "blind":
-        case "curtain":
-          switch (compareState) {
-            case "opening":
-              return "hass:arrow-up-box";
-            case "closing":
-              return "hass:arrow-down-box";
-            case "closed":
-              return "hass:blinds";
-            default:
-              return "hass:blinds-open";
-          }
-        case "window":
-          switch (compareState) {
-            case "opening":
-              return "hass:arrow-up-box";
-            case "closing":
-              return "hass:arrow-down-box";
-            case "closed":
-              return "hass:window-closed";
-            default:
-              return "hass:window-open";
-          }
-      }
-
-      switch (compareState) {
-        case "opening":
-          return "hass:arrow-up-box";
-        case "closing":
-          return "hass:arrow-down-box";
-        case "closed":
-          return "hass:window-closed";
-        default:
-          return "hass:window-open";
-      }
-    }
+    case "cover":
+      return coverIcon(compareState, stateObj);
 
     case "humidifier":
       return state && state === "off"
@@ -187,19 +61,9 @@ export const domainIcon = (
       }
 
     case "sensor": {
-      const dclass = stateObj?.attributes.device_class;
-
-      if (dclass && dclass in FIXED_DEVICE_CLASS_ICONS) {
-        return FIXED_DEVICE_CLASS_ICONS[dclass];
-      }
-
-      if (dclass === "battery") {
-        return stateObj ? batteryIcon(stateObj) : "hass:battery";
-      }
-
-      const unit = stateObj?.attributes.unit_of_measurement;
-      if (unit === UNIT_C || unit === UNIT_F) {
-        return "hass:thermometer";
+      const icon = sensorIcon(stateObj);
+      if (icon) {
+        return icon;
       }
 
       break;
--- src/common/entity/sensor_icon.ts
+++ src/common/entity/sensor_icon.ts
@@ -1,36 +1,23 @@
 /** Return an icon representing a sensor state. */
 import { HassEntity } from "home-assistant-js-websocket";
-import { UNIT_C, UNIT_F } from "../const";
-import { domainIcon } from "./domain_icon";
+import { FIXED_DEVICE_CLASS_ICONS, UNIT_C, UNIT_F } from "../const";
 import { batteryIcon } from "./battery_icon";
 
-const fixedDeviceClassIcons = {
-  current: "hass:current-ac",
-  energy: "hass:flash",
-  humidity: "hass:water-percent",
-  illuminance: "hass:brightness-5",
-  temperature: "hass:thermometer",
-  pressure: "hass:gauge",
-  power: "hass:flash",
-  power_factor: "hass:angle-acute",
-  signal_strength: "hass:wifi",
-  timestamp: "hass:clock",
-  voltage: "hass:sine-wave",
-};
+export const sensorIcon = (stateObj?: HassEntity): string | undefined => {
+  const dclass = stateObj?.attributes.device_class;
 
-export const sensorIcon = (state: HassEntity) => {
-  const dclass = state.attributes.device_class;
-
-  if (dclass && dclass in fixedDeviceClassIcons) {
-    return fixedDeviceClassIcons[dclass];
+  if (dclass && dclass in FIXED_DEVICE_CLASS_ICONS) {
+    return FIXED_DEVICE_CLASS_ICONS[dclass];
   }
+
   if (dclass === "battery") {
-    return batteryIcon(state);
+    return stateObj ? batteryIcon(stateObj) : "hass:battery";
   }
 
-  const unit = state.attributes.unit_of_measurement;
+  const unit = stateObj?.attributes.unit_of_measurement;
   if (unit === UNIT_C || unit === UNIT_F) {
     return "hass:thermometer";
   }
-  return domainIcon("sensor");
+
+  return undefined;
 };
