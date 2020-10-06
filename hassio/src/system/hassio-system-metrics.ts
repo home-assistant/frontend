b59268770ae28386f2d45aebb0719acd14a20b59
Ryan Meek <25127328+maykar@users.noreply.github.com>
2020-10-06 19:41:22 -0400

    Add title to _renderMetric

--- hassio/src/system/hassio-system-metrics.ts
+++ hassio/src/system/hassio-system-metrics.ts
@@ -88,7 +88,7 @@ class HassioSystemMetrics extends LitElement {
     this._loadData();
   }
 
-  private _renderMetric(description: string, value: number): TemplateResult {
+  private _renderMetric(description: string, value: number, title: string): TemplateResult {
     const roundedValue = roundWithOneDecimal(value);
     return html`<ha-settings-row>
       <span slot="heading">
