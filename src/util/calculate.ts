c956a7a31ea97e9c3e21df7e03fdc5f565c2d4aa
Ryan Meek <25127328+maykar@users.noreply.github.com>
2020-10-06 13:18:11 -0400

    bytesToLargestString to return largest metric of disk space

--- src/util/calculate.ts
+++ src/util/calculate.ts
@@ -21,3 +21,12 @@ export const getValueInPercentage = (
 export const roundWithOneDecimal = (value: number): number => {
   return Math.round(value * 10) / 10;
 };
+
+export const bytesToLargestString = (value: number, decimals = 3): number => {
+    if (bytes === 0) return '0 Bytes';
+    const k = 1024;
+    decimals = decimals < 0 ? 0 : decimals;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(decimals)) + ' ' + sizes[i];
+}
