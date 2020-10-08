69f20d5a73e6d6d475066b344d51c1fa5601de9a
Ian Richardson <iantrich@gmail.com>
2020-10-08 09:37:51 -0500

    Show moon phase icon in state-label-badge (#7194)
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>

--- src/components/entity/ha-state-label-badge.ts
+++ src/components/entity/ha-state-label-badge.ts
@@ -110,7 +110,9 @@ export class HaStateLabelBadge extends LitElement {
         return null;
       case "sensor":
       default:
-        return state.state === UNKNOWN
+        return state.attributes.device_class === "moon__phase"
+          ? null
+          : state.state === UNKNOWN
           ? "-"
           : state.attributes.unit_of_measurement
           ? state.state
@@ -162,7 +164,9 @@ export class HaStateLabelBadge extends LitElement {
           ? "hass:timer-outline"
           : "hass:timer-off-outline";
       default:
-        return null;
+        return state?.attributes.device_class === "moon__phase"
+          ? stateIcon(state)
+          : null;
     }
   }
 
