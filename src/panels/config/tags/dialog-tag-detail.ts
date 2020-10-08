16a236015b894f390e936c15c02fb91a0826ca76
Thomas Lovén <thomasloven@gmail.com>
2020-10-08 16:37:17 +0200

    Display qr code in tag properties (#7092)

--- src/panels/config/tags/dialog-tag-detail.ts
+++ src/panels/config/tags/dialog-tag-detail.ts
@@ -10,6 +10,7 @@ import {
   property,
   TemplateResult,
 } from "lit-element";
+
 import { fireEvent } from "../../../common/dom/fire_event";
 import { createCloseHeading } from "../../../components/ha-dialog";
 import "../../../components/ha-formfield";
@@ -20,6 +21,8 @@ import { HassDialog } from "../../../dialogs/make-dialog-manager";
 import { haStyleDialog } from "../../../resources/styles";
 import { HomeAssistant } from "../../../types";
 import { TagDetailDialogParams } from "./show-dialog-tag-detail";
+
+const QR_LOGO_URL = "/static/icons/favicon-192x192.png";
 
 @customElement("dialog-tag-detail")
 class DialogTagDetail extends LitElement
@@ -36,6 +39,8 @@ class DialogTagDetail extends LitElement
 
   @internalProperty() private _submitting = false;
 
+  @internalProperty() private _qrCode?: TemplateResult;
+
   public showDialog(params: TagDetailDialogParams): void {
     this._params = params;
     this._error = undefined;
@@ -49,6 +54,7 @@ class DialogTagDetail extends LitElement
 
   public closeDialog(): void {
     this._params = undefined;
+    this._qrCode = undefined;
     fireEvent(this, "dialog-closed", { dialog: this.localName });
   }
 
@@ -107,6 +113,36 @@ class DialogTagDetail extends LitElement
                 ></paper-input>`
               : ""}
           </div>
+          ${this._params.entry
+            ? html`
+                <div>
+                  <p>
+                    ${this.hass!.localize(
+                      "ui.panel.config.tags.detail.usage",
+                      "companion_link",
+                      html`<a
+                        href="https://companion.home-assistant.io/"
+                        target="_blank"
+                        rel="noreferrer"
+                        >${this.hass!.localize(
+                          "ui.panel.config.tags.detail.companion_apps"
+                        )}</a
+                      >`
+                    )}
+                  </p>
+                </div>
+
+                <div id="qr">
+                  ${this._qrCode
+                    ? this._qrCode
+                    : html`
+                        <mwc-button @click=${this._generateQR}
+                          >Generate QR code
+                        </mwc-button>
+                      `}
+                </div>
+              `
+            : ``}
         </div>
         ${this._params.entry
           ? html`
@@ -192,12 +228,42 @@ class DialogTagDetail extends LitElement
     }
   }
 
+  private async _generateQR() {
+    const qrcode = await import("qrcode");
+    const canvas = await qrcode.toCanvas(
+      `https://home-assistant.io/tag/${this._params?.entry?.id}`,
+      {
+        width: 180,
+        errorCorrectionLevel: "Q",
+      }
+    );
+    const context = canvas.getContext("2d");
+
+    const imageObj = new Image();
+    imageObj.src = QR_LOGO_URL;
+    await new Promise((resolve) => {
+      imageObj.onload = resolve;
+    });
+    context.drawImage(
+      imageObj,
+      canvas.width / 3,
+      canvas.height / 3,
+      canvas.width / 3,
+      canvas.height / 3
+    );
+
+    this._qrCode = html`<img src=${canvas.toDataURL()}></img>`;
+  }
+
   static get styles(): CSSResult[] {
     return [
       haStyleDialog,
       css`
         a {
           color: var(--primary-color);
+        }
+        #qr {
+          text-align: center;
         }
       `,
     ];
