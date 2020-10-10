8c48f43d8951ee973b8362561e9f614a65a07cda
Paulus Schoutsen <balloob@gmail.com>
2020-10-10 10:12:39 +0000

    Include all compat

--- src/resources/compatibility.ts
+++ src/resources/compatibility.ts
@@ -1,8 +1,4 @@
-import "mdn-polyfills/Array.prototype.includes";
-import "mdn-polyfills/Object.assign";
-import "mdn-polyfills/Object.entries";
-import "mdn-polyfills/Object.values";
-import "mdn-polyfills/String.prototype.padStart";
+import "core-js";
 import "regenerator-runtime/runtime";
 import "unfetch/polyfill";
 // To use comlink under ES5
