53c679eb35bffc21b3e2f03e5637ac5f192031c1
Justin Wong <46082645+uvjustin@users.noreply.github.com>
2020-10-12 21:58:19 +0800

    Check if video el still exists before resize

--- src/components/ha-hls-player.ts
+++ src/components/ha-hls-player.ts
@@ -154,6 +154,7 @@ class HaHLSPlayer extends LitElement {
   }
 
   private _resizeExoPlayer = () => {
+    if (!this._videoEl) return;
     const rect = this._videoEl.getBoundingClientRect();
     this.hass!.auth.external!.fireMessage({
       type: "exoplayer/resize",
