d177463222e597edeb02d9151eb9a20c34a2e0e0
Donnie <donniekarnsinsb@hotmail.com>
2020-10-13 15:26:12 -0700

    Add friendly name to quick bar list and filter (#7306)
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>

--- test-mocha/common/string/sequence_matching.test.ts
+++ test-mocha/common/string/sequence_matching.test.ts
@@ -3,7 +3,7 @@ import { assert } from "chai";
 import { fuzzySequentialMatch } from "../../../src/common/string/sequence_matching";
 
 describe("fuzzySequentialMatch", () => {
-  const entityId = "automation.ticker";
+  const entity = { entity_id: "automation.ticker", friendly_name: "Stocks" };
 
   const shouldMatchEntity = [
     "",
@@ -23,6 +23,9 @@ describe("fuzzySequentialMatch", () => {
     "uoaintce",
     "au.tce",
     "tomaontkr",
+    "s",
+    "stocks",
+    "sks",
   ];
 
   const shouldNotMatchEntity = [
@@ -32,19 +35,27 @@ describe("fuzzySequentialMatch", () => {
     "automation. ticke",
     "1",
     "noitamotua",
+    "autostocks",
+    "stox",
   ];
 
-  describe(`Entity '${entityId}'`, () => {
+  describe(`Entity '${entity.entity_id}'`, () => {
     for (const goodFilter of shouldMatchEntity) {
       it(`matches with '${goodFilter}'`, () => {
-        const res = fuzzySequentialMatch(goodFilter, entityId);
+        const res = fuzzySequentialMatch(goodFilter, [
+          entity.entity_id,
+          entity.friendly_name.toLowerCase(),
+        ]);
         assert.equal(res, true);
       });
     }
 
     for (const badFilter of shouldNotMatchEntity) {
       it(`fails to match with '${badFilter}'`, () => {
-        const res = fuzzySequentialMatch(badFilter, entityId);
+        const res = fuzzySequentialMatch(badFilter, [
+          entity.entity_id,
+          entity.friendly_name,
+        ]);
         assert.equal(res, false);
       });
     }
