85308b7dc798570a9ce189a926816733c5f2dfb0
Philip Allgaier <mail@spacegaier.de>
2020-10-13 21:19:49 +0200

    Proper conversion from JS to TS

--- src/util/hass-attributes-util.ts
+++ src/util/hass-attributes-util.ts
@@ -1,73 +1,72 @@
-const hassAttributeUtil = {};
-
-hassAttributeUtil.DOMAIN_DEVICE_CLASS = {
-  binary_sensor: [
-    "battery",
-    "cold",
-    "connectivity",
-    "door",
-    "garage_door",
-    "gas",
-    "heat",
-    "light",
-    "lock",
-    "moisture",
-    "motion",
-    "moving",
-    "occupancy",
-    "opening",
-    "plug",
-    "power",
-    "presence",
-    "problem",
-    "safety",
-    "smoke",
-    "sound",
-    "vibration",
-    "window",
-  ],
-  cover: [
-    "awning",
-    "blind",
-    "curtain",
-    "damper",
-    "door",
-    "garage",
-    "shade",
-    "shutter",
-    "window",
-  ],
-  humidifier: ["dehumidifier", "humidifier"],
-  sensor: [
-    "battery",
-    "humidity",
-    "illuminance",
-    "temperature",
-    "pressure",
-    "power",
-    "signal_strength",
-    "timestamp",
-  ],
-  switch: ["switch", "outlet"],
-};
-
-hassAttributeUtil.UNKNOWN_TYPE = "json";
-hassAttributeUtil.ADD_TYPE = "key-value";
-
-hassAttributeUtil.TYPE_TO_TAG = {
-  string: "ha-customize-string",
-  json: "ha-customize-string",
-  icon: "ha-customize-icon",
-  boolean: "ha-customize-boolean",
-  array: "ha-customize-array",
-  "key-value": "ha-customize-key-value",
+const hassAttributeUtil = {
+  DOMAIN_DEVICE_CLASS: {
+    binary_sensor: [
+      "battery",
+      "cold",
+      "connectivity",
+      "door",
+      "garage_door",
+      "gas",
+      "heat",
+      "light",
+      "lock",
+      "moisture",
+      "motion",
+      "moving",
+      "occupancy",
+      "opening",
+      "plug",
+      "power",
+      "presence",
+      "problem",
+      "safety",
+      "smoke",
+      "sound",
+      "vibration",
+      "window",
+    ],
+    cover: [
+      "awning",
+      "blind",
+      "curtain",
+      "damper",
+      "door",
+      "garage",
+      "shade",
+      "shutter",
+      "window",
+    ],
+    humidifier: ["dehumidifier", "humidifier"],
+    sensor: [
+      "battery",
+      "humidity",
+      "illuminance",
+      "temperature",
+      "pressure",
+      "power",
+      "signal_strength",
+      "timestamp",
+    ],
+    switch: ["switch", "outlet"],
+  },
+  UNKNOWN_TYPE: "json",
+  ADD_TYPE: "key-value",
+  TYPE_TO_TAG: {
+    string: "ha-customize-string",
+    json: "ha-customize-string",
+    icon: "ha-customize-icon",
+    boolean: "ha-customize-boolean",
+    array: "ha-customize-array",
+    "key-value": "ha-customize-key-value",
+  },
+  LOGIC_STATE_ATTRIBUTES: {},
 };
 
 // Attributes here serve dual purpose:
 // 1) Any key of this object won't be shown in more-info window.
 // 2) Any key which has value other than undefined will appear in customization
 //    config according to its value.
-hassAttributeUtil.LOGIC_STATE_ATTRIBUTES = hassAttributeUtil.LOGIC_STATE_ATTRIBUTES || {
+hassAttributeUtil.LOGIC_STATE_ATTRIBUTES = {
   entity_picture: undefined,
   friendly_name: { type: "string", description: "Name" },
   icon: { type: "icon" },
