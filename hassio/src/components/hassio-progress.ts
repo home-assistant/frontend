e52efb432118c0f387e603562dc4f79e85126441
Sándor Balázs <balazs.sandor@vcc.live>
2020-10-17 22:42:26 +0200

    Replace paper-progress with mwc-linear-progress in hassio

--- hassio/src/components/hassio-progress.ts
+++ hassio/src/components/hassio-progress.ts
@@ -4,78 +4,72 @@ import {
   CSSResult,
   customElement,
   html,
+  internalProperty,
   LitElement,
   property,
   TemplateResult,
 } from "lit-element";
 import { SubscribeMixin } from "../../../src/mixins/subscribe-mixin";
 import { HomeAssistant } from "../../../src/types";
-import "@polymer/paper-progress/paper-progress";
+import "@material/mwc-linear-progress";
 
 interface HassioProgressEvent {
   data: {
     name: string;
-    downloading: { current: number; total: number };
-    extracting: { current: number; total: number };
+    progress: number;
+    buffer: number | null;
   };
 }
 
 @customElement("hassio-progress")
 export class HassioProgress extends SubscribeMixin(LitElement) {
-  @property() public hass!: HomeAssistant;
+  @property({ attribute: false }) public hass!: HomeAssistant;
 
   @property() public name = "";
 
-  @property() private downloaded = 0;
+  @internalProperty() private buffer = 0;
 
-  @property() private progress = 100;
+  @internalProperty() private progress = 1;
 
   protected render(): TemplateResult {
-    if (this.progress === 100) {
+    if (this.progress === 1) {
       return html``;
     }
-    if (this.progress === 0 && this.downloaded === 0) {
-      return html` <paper-progress indeterminate></paper-progress> `;
+    if (this.progress === 0 && this.buffer === 0) {
+      return html` <mwc-linear-progress indeterminate></mwc-linear-progress> `;
     }
     return html`
-      <paper-progress
-        .value=${this.progress}
-        .secondaryProgress=${this.downloaded}
-      ></paper-progress>
+      <mwc-linear-progress
+        .progress=${this.progress}
+        .buffer=${this.buffer}
+      ></mwc-linear-progress>
     `;
   }
 
   public hassSubscribe(): Promise<UnsubscribeFunc>[] {
     return [
-      this.hass!.connection.subscribeEvents(this._update, "hassio_progress"),
+      this.hass.connection.subscribeEvents(this._update, "hassio_progress"),
     ];
   }
 
   private _update = (ev: HassioProgressEvent) => {
     if (ev.data.name === this.name) {
-      this.downloaded =
-        Math.round(
-          (ev.data.downloading.current / ev.data.downloading.total) * 100
-        ) || 0;
-      this.progress =
-        Math.round(
-          (ev.data.extracting.current / ev.data.extracting.total) * 100
-        ) || 0;
+      this.buffer = ev.data.buffer || 0;
+      this.progress = ev.data.progress || 0;
     }
   };
 
   static get styles(): CSSResult[] {
     return [
       css`
-        paper-progress {
+        mwc-linear-progress {
           position: absolute;
           left: 0;
           right: 0;
           bottom: 0;
           width: auto;
 
-          --paper-progress-active-color: var(--primary-color);
-          --paper-progress-secondary-color: var(--light-primary-color);
+          --mdc-linear-progress-buffer-color: var(--light-primary-color);
         }
       `,
     ];
