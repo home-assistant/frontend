cbef9096570d7ac948a115e9bdd0441626a2a895
Daniel Martin Gonzalez <danimart1991@outlook.com>
2020-10-17 22:15:37 +0200

    Add Counter to Helpers (#7346)
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>

--- src/panels/config/helpers/dialog-helper-detail.ts
+++ src/panels/config/helpers/dialog-helper-detail.ts
@@ -22,6 +22,7 @@ import { createInputDateTime } from "../../../data/input_datetime";
 import { createInputNumber } from "../../../data/input_number";
 import { createInputSelect } from "../../../data/input_select";
 import { createInputText } from "../../../data/input_text";
+import { createCounter } from "../../../data/counter";
 import { haStyleDialog } from "../../../resources/styles";
 import { HomeAssistant } from "../../../types";
 import { Helper } from "./const";
@@ -30,6 +31,7 @@ import "./forms/ha-input_datetime-form";
 import "./forms/ha-input_number-form";
 import "./forms/ha-input_select-form";
 import "./forms/ha-input_text-form";
+import "./forms/ha-counter-form";
 
 const HELPERS = {
   input_boolean: createInputBoolean,
@@ -37,6 +39,7 @@ const HELPERS = {
   input_number: createInputNumber,
   input_datetime: createInputDateTime,
   input_select: createInputSelect,
+  counter: createCounter,
 };
 
 @customElement("dialog-helper-detail")
