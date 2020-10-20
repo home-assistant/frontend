1ed71f6bbe6d73acad3f2aae48b5dd6a2fed63e1
Donnie <donniekarnsinsb@hotmail.com>
2020-10-20 08:29:20 -0700

    Use shouldUpdate to avoid calling updated

--- src/state/quick-bar-mixin.ts
+++ src/state/quick-bar-mixin.ts
@@ -4,6 +4,7 @@ import {
   QuickBarParams,
   showQuickBar,
 } from "../dialogs/quick-bar/show-dialog-quick-bar";
+import { HomeAssistant } from "../types";
 
 declare global {
   interface HASSDomEvents {
@@ -17,20 +18,15 @@ export default <T extends Constructor<HassElement>>(superClass: T) =>
   class extends superClass {
     private _isAdmin = false;
 
-    private _haveCheckedAdminStatus = false;
+    private _isRegistered = false;
 
     protected updated(changedProps: PropertyValues) {
-      if (this._haveCheckedAdminStatus) {
-        return;
-      }
-
+      super.updated(changedProps);
       if (changedProps.has("hass")) {
         if (this.hass?.user) {
-          this.isAdmin = this.hass?.user?.is_admin || false;
-          this.haveCheckedAdminStatus = true;
-
-          if (this.isAdmin) {
-            this.registerShortcut();
+          this._isAdmin = this.hass?.user?.is_admin || false;
+          if (this._isAdmin) {
+            this._registerShortcut();
           }
         }
       }
@@ -44,6 +40,17 @@ export default <T extends Constructor<HassElement>>(superClass: T) =>
       }
     }
 
+    // protected shouldUpdate(changedProps: PropertyValues) {
+    //   super.shouldUpdate(changedProps);
+    //   if (changedProps.has("hass")) {
+    //     const oldHass = changedProps.get("hass") as HomeAssistant | undefined;
+    //     if (this.hass?.user !== oldHass?.user) {
+    //       return true;
+    //     }
+    //   }
+    //   return false;
+    // }
+
     private _registerShortcut() {
       document.addEventListener("keydown", (e: KeyboardEvent) => {
         if (this.isOSCtrlKey(e) && e.code === "KeyP") {
@@ -52,7 +59,7 @@ export default <T extends Constructor<HassElement>>(superClass: T) =>
           if (e.shiftKey) {
             eventParams.commandMode = true;
           }
-
+          this._isRegistered = true;
           showQuickBar(this, eventParams);
         }
       });
