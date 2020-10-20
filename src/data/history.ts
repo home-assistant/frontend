8c8151be9273b339fc5f804e7f1593af1d4eebaf
Philip Allgaier <mail@spacegaier.de>
2020-10-20 23:02:59 +0200

    Add entity filter to history panel (#7401)

--- src/data/history.ts
+++ src/data/history.ts
@@ -85,11 +85,14 @@ export const fetchRecent = (
 export const fetchDate = (
   hass: HomeAssistant,
   startTime: Date,
-  endTime: Date
+  endTime: Date,
+  entityId
 ): Promise<HassEntity[][]> => {
   return hass.callApi(
     "GET",
-    `history/period/${startTime.toISOString()}?end_time=${endTime.toISOString()}&minimal_response`
+    `history/period/${startTime.toISOString()}?end_time=${endTime.toISOString()}&minimal_response${
+      entityId ? `&filter_entity_id=${entityId}` : ``
+    }`
   );
 };
 
