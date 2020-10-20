ad409bab474b5cda15ab28c6de8ba711312c170c
Donnie <donniekarnsinsb@hotmail.com>
2020-10-20 09:01:16 -0700

    Add scoring and sorting to sequence matcher (#7367)
    * Replace sequence matcher with VS Code's score-based implementation
    
    * Remove everything not related to fuzzyScore and matchSubstring
    
    * Fix bug when filter length <= 3
    
    * Add licensing and credit to Microsoft
    
    * Remove unnecessary character codes
    
    * Remove old sequence matcher, update tests, fix issue with not finding best score in list of words
    
    * Remove unnecessary sequence precheck, refactor client api to remove array
    
    * Fix issue with score sorting not implemented correctly and thus not actually sorting by score
    
    * Update src/common/string/filter/sequence-matching.ts
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>
    
    * Remove unnecessary string return from fuzzy matcher. Clean up code
    
    * Remove globals from filter. Move sorting logic into matcher
    
    * Update function description, make score property optional.
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>

--- test-mocha/common/string/sequence_matching.test.ts
+++ test-mocha/common/string/sequence_matching.test.ts
@@ -1,34 +1,48 @@
 import { assert } from "chai";
 
-import { fuzzySequentialMatch } from "../../../src/common/string/sequence_matching";
+import {
+  fuzzyFilterSort,
+  fuzzySequentialMatch,
+} from "../../../src/common/string/filter/sequence-matching";
 
 describe("fuzzySequentialMatch", () => {
   const entity = { entity_id: "automation.ticker", friendly_name: "Stocks" };
 
+  const createExpectation: (
+    pattern,
+    expected
+  ) => {
+    pattern: string;
+    expected: string | number | undefined;
+  } = (pattern, expected) => ({
+    pattern,
+    expected,
+  });
+
   const shouldMatchEntity = [
-    "",
-    "automation.ticker",
-    "automation.ticke",
-    "automation.",
-    "au",
-    "automationticker",
-    "tion.tick",
-    "ticker",
-    "automation.r",
-    "tick",
-    "aumatick",
-    "aion.tck",
-    "ioticker",
-    "atmto.ikr",
-    "uoaintce",
-    "au.tce",
-    "tomaontkr",
-    "s",
-    "stocks",
-    "sks",
+    createExpectation("automation.ticker", 138),
+    createExpectation("automation.ticke", 129),
+    createExpectation("automation.", 89),
+    createExpectation("au", 17),
+    createExpectation("automationticker", 107),
+    createExpectation("tion.tick", 18),
+    createExpectation("ticker", 1),
+    createExpectation("automation.r", 89),
+    createExpectation("tick", 1),
+    createExpectation("aumatick", 15),
+    createExpectation("aion.tck", 14),
+    createExpectation("ioticker", 19),
+    createExpectation("atmto.ikr", 1),
+    createExpectation("uoaintce", 1),
+    createExpectation("au.tce", 17),
+    createExpectation("tomaontkr", 9),
+    createExpectation("s", 7),
+    createExpectation("stocks", 48),
+    createExpectation("sks", 7),
   ];
 
   const shouldNotMatchEntity = [
+    "",
     " ",
     "abcdefghijklmnopqrstuvwxyz",
     "automation.tickerz",
@@ -40,24 +54,50 @@ describe("fuzzySequentialMatch", () => {
   ];
 
   describe(`Entity '${entity.entity_id}'`, () => {
-    for (const goodFilter of shouldMatchEntity) {
-      it(`matches with '${goodFilter}'`, () => {
-        const res = fuzzySequentialMatch(goodFilter, [
+    for (const expectation of shouldMatchEntity) {
+      it(`matches '${expectation.pattern}' with return of '${expectation.expected}'`, () => {
+        const res = fuzzySequentialMatch(
+          expectation.pattern,
           entity.entity_id,
-          entity.friendly_name.toLowerCase(),
-        ]);
-        assert.equal(res, true);
+          entity.friendly_name
+        );
+        assert.equal(res, expectation.expected);
       });
     }
 
     for (const badFilter of shouldNotMatchEntity) {
       it(`fails to match with '${badFilter}'`, () => {
-        const res = fuzzySequentialMatch(badFilter, [
+        const res = fuzzySequentialMatch(
+          badFilter,
           entity.entity_id,
-          entity.friendly_name,
-        ]);
-        assert.equal(res, false);
+          entity.friendly_name
+        );
+        assert.equal(res, 0);
       });
     }
+  });
+});
+
+describe("fuzzyFilterSort", () => {
+  const filter = "ticker";
+  const item1 = { text: "automation.ticker", altText: "Stocks", score: 0 };
+  const item2 = { text: "sensor.ticker", altText: "Stocks up", score: 0 };
+  const item3 = {
+    text: "automation.check_router",
+    altText: "Timer Check Router",
+    score: 0,
+  };
+  const itemsBeforeFilter = [item1, item2, item3];
+
+  it(`sorts correctly`, () => {
+    const expectedItemsAfterFilter = [
+      { ...item2, score: 23 },
+      { ...item3, score: 12 },
+      { ...item1, score: 1 },
+    ];
+
+    const res = fuzzyFilterSort(filter, itemsBeforeFilter);
+
+    assert.deepEqual(res, expectedItemsAfterFilter);
   });
 });
