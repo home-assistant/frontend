673649a6034929eb9b366c4679a10961d2bff1e7
Bram Kragten <mail@bramkragten.nl>
2020-10-21 18:36:59 +0200

    Bumped version to 20201021.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="home-assistant-frontend",
-    version="20201001.0",
+    version="20201021.0",
     description="The Home Assistant frontend",
     url="https://github.com/home-assistant/home-assistant-polymer",
     author="The Home Assistant Authors",
