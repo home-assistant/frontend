7f55328c69c6589a6916aa29f3b416f011090348
Florian Gareis <mail@zoker.me>
2020-10-23 16:44:45 +0200

    Change translation keys

--- src/components/ha-push-notifications-toggle.js
+++ src/components/ha-push-notifications-toggle.js
@@ -90,8 +90,8 @@ class HaPushNotificationsToggle extends EventsMixin(PolymerElement) {
       }
 
       const name = await showPromptDialog(this, {
-        title: this.hass.localize("ui.panel.profile.push_notifications.add_device.title"),
-        inputLabel: this.hass.localize("ui.panel.profile.push_notifications.add_device.text"),
+        title: this.hass.localize("ui.panel.profile.push_notifications.add_device_prompt.title"),
+        inputLabel: this.hass.localize("ui.panel.profile.push_notifications.add_device_prompt.input_label"),
       });
       if (name == null) {
         this.pushChecked = false;
