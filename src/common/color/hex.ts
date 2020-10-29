a494080dcdcb988b22ba34fe6900abdd202bc19e
Ryan Meek <25127328+maykar@users.noreply.github.com>
2020-10-29 12:23:28 -0400

    Create hex.ts

---
+++ src/common/color/hex.ts
@@ -0,0 +1,23 @@
+const expand_hex = (hex: string): string => {
+  hex = hex.replace("#", "");
+  if (hex.length === 6) return hex;
+  let result = "";
+  for (const val of hex) {
+    result += val + val;
+  }
+  return result;
+};
+
+export const hexBlend = (color_1: string, color_2: string, blend = 50): string => {
+  let color = "";
+  color_1 = expand_hex(color_1);
+  color_2 = expand_hex(color_2);
+  for (let i = 0; i <= 5; i += 2) {
+    const c1 = parseInt(color_1.substr(i, 2), 16);
+    const c2 = parseInt(color_2.substr(i, 2), 16);
+    let hex = Math.floor(c2 + (c1 - c2) * (blend / 100)).toString(16);
+    while (hex.length < 2) hex = "0" + hex;
+    color += hex;
+  }
+  return `#${color}`;
+};
