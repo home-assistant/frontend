fa0b83dca13caaa413fb7af4baa2a820412963c2
Kendell R <KTibow@users.noreply.github.com>
2020-11-07 08:11:29 -0800

    Change approach a bit

--- src/panels/profile/ha-overflow-menu-row.ts
+++ src/panels/profile/ha-overflow-menu-row.ts
@@ -1,64 +1,51 @@
 import {
-  css,
-  CSSResult,
   customElement,
   html,
   LitElement,
   property,
   TemplateResult,
 } from "lit-element";
-import "../../components/ha-card";
+import { fireEvent } from "../../common/dom/fire_event";
 import "../../components/ha-switch";
-import {
-  CoreFrontendUserData,
-  getOptimisticFrontendUserDataCollection,
-} from "../../data/frontend";
-import { HomeAssistant } from "../../types";
+import type { HaSwitch } from "../../components/ha-switch";
+import type { HomeAssistant } from "../../types";
 import "../../components/ha-settings-row";
 
 @customElement("ha-overflow-menu-row")
-class OverflowMenuRow extends LitElement {
+class HaOverflowMenuRow extends LitElement {
   @property({ attribute: false }) public hass!: HomeAssistant;
 
   @property() public narrow!: boolean;
-
-  @property() public coreUserData?: CoreFrontendUserData;
 
   protected render(): TemplateResult {
     return html`
       <ha-settings-row .narrow=${this.narrow}>
         <span slot="heading">
-          ${this.hass.localize("ui.panel.profile.overflow_menu.title")}
+          ${this.hass.localize("ui.panel.profile.overflow_menu.header")}
         </span>
         <span slot="description">
           ${this.hass.localize("ui.panel.profile.overflow_menu.description")}
         </span>
         <ha-switch
-          .checked=${this.coreUserData && this.coreUserData.hideOverflowMenu}
-          .disabled=${this.coreUserData === undefined}
-          @change=${this._overflowToggled}
+          .checked=${this.hass.enableOverflowMenu}
+          @change=${this._checkedChanged}
         ></ha-switch>
       </ha-settings-row>
     `;
   }
 
-  private async _overflowToggled(ev) {
-    getOptimisticFrontendUserDataCollection(this.hass.connection, "core").save({
-      hideOverflowMenu: ev.currentTarget.checked,
-    });
-  }
+  private async _checkedChanged(ev: Event) {
+    const enabled = (ev.target as HaSwitch).checked;
+    if (enabled === this.hass.enableOverflowMenu) {
+      return;
+    }
 
-  static get styles(): CSSResult {
-    return css`
-      a {
-        color: var(--primary-color);
-      }
-    `;
+    fireEvent(this, "hass-enable-overflow-menu", enabled);
   }
 }
 
 declare global {
   interface HTMLElementTagNameMap {
-    "ha-overflow-menu-row": OverflowMenuRow;
+    "ha-overflow-menu-row": HaOverflowMenuRow;
   }
 }
