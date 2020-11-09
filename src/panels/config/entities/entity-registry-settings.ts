c642960985096704aba9c5d2a312a35a060b033f
Erik Montnemery <erik@montnemery.com>
2020-11-09 19:48:24 +0100

    Improve user experience when enabling a disabled entity (#7580)

--- src/panels/config/entities/entity-registry-settings.ts
+++ src/panels/config/entities/entity-registry-settings.ts
@@ -23,7 +23,10 @@ import {
   removeEntityRegistryEntry,
   updateEntityRegistryEntry,
 } from "../../../data/entity_registry";
-import { showConfirmationDialog } from "../../../dialogs/generic/show-dialog-box";
+import {
+  showAlertDialog,
+  showConfirmationDialog,
+} from "../../../dialogs/generic/show-dialog-box";
 import type { PolymerChangedEvent } from "../../../polymer-types";
 import { haStyle } from "../../../resources/styles";
 import type { HomeAssistant } from "../../../types";
@@ -191,7 +194,27 @@ export class EntityRegistrySettings extends LitElement {
       params.disabled_by = this._disabledBy;
     }
     try {
-      await updateEntityRegistryEntry(this.hass!, this._origEntityId, params);
+      const result = await updateEntityRegistryEntry(
+        this.hass!,
+        this._origEntityId,
+        params
+      );
+      if (result.require_restart) {
+        showAlertDialog(this, {
+          text: this.hass.localize(
+            "ui.dialogs.entity_registry.editor.enabled_restart_confirm"
+          ),
+        });
+      }
+      if (result.reload_delay) {
+        showAlertDialog(this, {
+          text: this.hass.localize(
+            "ui.dialogs.entity_registry.editor.enabled_delay_confirm",
+            "delay",
+            result.reload_delay
+          ),
+        });
+      }
       fireEvent(this as HTMLElement, "close-dialog");
     } catch (err) {
       this._error = err.message || "Unknown error";
