32532ffd28750bc909de4e46a925dcc1480754cd
Hypfer <soeren@soerenbeye.de>
2020-11-10 15:07:09 +0100

    Change brands-url code as requested in review

--- src/util/brands-url.ts
+++ src/util/brands-url.ts
@@ -1,29 +1,9 @@
-const brandsBaseUrl = "https://brands.home-assistant.io/";
-const imageSuffix = ".png";
-
 export const brandsUrl = (
   domain: string,
   type: "icon" | "logo",
   useFallback?: boolean
 ) => {
-  let brandUrl = brandsBaseUrl;
-
-  if (useFallback) {
-    brandUrl += "_/";
-  }
-
-  brandUrl += domain + "/";
-
-  switch (type) {
-    case "icon":
-      brandUrl += "icon";
-      break;
-    case "logo":
-      brandUrl += "logo";
-      break;
-  }
-
-  brandUrl += imageSuffix;
-
-  return brandUrl;
+  return `https://brands.home-assistant.io/${
+    useFallback ? "_/" : ""
+  }${domain}/${type}.png`;
 };
