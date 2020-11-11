4300086a63777d251bd479dd92dcc6c43f581ed2
Donnie <donniekarnsinsb@hotmail.com>
2020-11-11 02:46:53 -0800

    Add navigation commands to quick bar commands (#7380)
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>

---
+++ src/common/config/can_show_page.ts
@@ -0,0 +1,18 @@
+import { isComponentLoaded } from "./is_component_loaded";
+import { PageNavigation } from "../../layouts/hass-tabs-subpage";
+import { HomeAssistant } from "../../types";
+
+export const canShowPage = (hass: HomeAssistant, page: PageNavigation) => {
+  return (
+    (isCore(page) || isLoadedIntegration(hass, page)) &&
+    !hideAdvancedPage(hass, page)
+  );
+};
+
+const isLoadedIntegration = (hass: HomeAssistant, page: PageNavigation) =>
+  !page.component || isComponentLoaded(hass, page.component);
+const isCore = (page: PageNavigation) => page.core;
+const isAdvancedPage = (page: PageNavigation) => page.advancedOnly;
+const userWantsAdvanced = (hass: HomeAssistant) => hass.userData?.showAdvanced;
+const hideAdvancedPage = (hass: HomeAssistant, page: PageNavigation) =>
+  isAdvancedPage(page) && !userWantsAdvanced(hass);
