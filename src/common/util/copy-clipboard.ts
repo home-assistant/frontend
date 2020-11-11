8588f8f711e952ade71baf57b0174303157c986d
Kendell R <KTibow@users.noreply.github.com>
2020-11-10 17:29:49 -0800

    Split out clipboard fallback
    The clipboard fallback triggers if navigator.clipboard is falsey, or if
    an error occurs using navigator.clipboard.
    Thanks for helping improve the code Bram!
    
    Co-Authored-By: Bram Kragten <bramkragten@users.noreply.github.com>

--- src/common/util/copy-clipboard.ts
+++ src/common/util/copy-clipboard.ts
@@ -1,12 +1,16 @@
+const fallbackCopy = (str) => {
+  const el = document.createElement("textarea");
+  el.value = str;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand("copy");
+  document.body.removeChild(el);
+}
+
 export const copyToClipboard = (str) => {
   if (navigator.clipboard) {
-    navigator.clipboard.writeText(str);
+    navigator.clipboard.writeText(str).catch(() => fallbackCopy(str));
   } else {
-    const el = document.createElement("textarea");
-    el.value = str;
-    document.body.appendChild(el);
-    el.select();
-    document.execCommand("copy");
-    document.body.removeChild(el);
+    fallbackCopy(str)
   }
 };
