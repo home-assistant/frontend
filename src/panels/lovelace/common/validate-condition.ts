d224054ccc61b36b7ac85f9ab16dca08ce85eb9f
Ing. Jaroslav Safka <devel@safka.org>
2020-11-14 20:44:18 +0100

    Add above,below,attribute to conditional card
    add conditional card support:
    * above state
    * below state
    * use attribute instedad of the state

--- src/panels/lovelace/common/validate-condition.ts
+++ src/panels/lovelace/common/validate-condition.ts
@@ -1,27 +1,51 @@
 import { HomeAssistant } from "../../../types";
-import { UNAVAILABLE } from "../../../data/entity";
 
 export interface Condition {
   entity: string;
+  attribute?: string;
   state?: string;
   state_not?: string;
+  above?: any;
+  below?: any;
 }
 
 export function checkConditionsMet(
   conditions: Condition[],
   hass: HomeAssistant
 ): boolean {
-  return conditions.every((c) => {
-    const state = hass.states[c.entity]
-      ? hass!.states[c.entity].state
-      : UNAVAILABLE;
+  return conditions.every((condition) => {
+    const stateObj = hass.states[condition.entity];
+    if (!stateObj) {
+      return false;
+    }
 
-    return c.state ? state === c.state : state !== c.state_not;
+    const state = condition.attribute
+      ? stateObj.attributes[condition.attribute]
+      : stateObj.state;
+
+    if (condition.state && state !== condition.state) {
+      return false;
+    }
+    if (condition.state_not && state === condition.state_not) {
+      return false;
+    }
+    if (condition.above != null && state <= condition.above) {
+      return false;
+    }
+    if (condition.below != null && state >= condition.below) {
+      return false;
+    }
+    return true;
   });
 }
 
 export function validateConditionalConfig(conditions: Condition[]): boolean {
   return conditions.every(
-    (c) => ((c.entity && (c.state || c.state_not)) as unknown) as boolean
+    (c) =>
+      ((c.entity &&
+        (c.state ||
+          c.state_not ||
+          c.above != null ||
+          c.below != null)) as unknown) as boolean
   );
 }
