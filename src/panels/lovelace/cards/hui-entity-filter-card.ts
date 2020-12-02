140f85db6810f219ce994313931bc4194cf9b0bc
Philip Allgaier <mail@spacegaier.de>
2020-12-02 16:04:37 +0100

    Allow again empty "entities" for entities card

--- src/panels/lovelace/cards/hui-entity-filter-card.ts
+++ src/panels/lovelace/cards/hui-entity-filter-card.ts
@@ -56,7 +56,6 @@ class EntityFilterCard extends UpdatingElement implements LovelaceCard {
     this._config = config;
     this._baseCardConfig = {
       type: "entities",
-      allow_empty: true,
       entities: [],
       ...this._config.card,
     };
