12fe6667d0cc5ea07f69a6a7fca84505cf03d6c0
Philip Allgaier <mail@spacegaier.de>
2020-12-10 15:55:57 +0100

    Add copy YAML (automation & script) fallback without `navigator.clipboard` (#7900)

--- src/common/util/copy-clipboard.ts
+++ src/common/util/copy-clipboard.ts
@@ -1,8 +1,12 @@
 export const copyToClipboard = (str) => {
-  const el = document.createElement("textarea");
-  el.value = str;
-  document.body.appendChild(el);
-  el.select();
-  document.execCommand("copy");
-  document.body.removeChild(el);
+  if (navigator.clipboard) {
+    navigator.clipboard.writeText(str);
+  } else {
+    const el = document.createElement("textarea");
+    el.value = str;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand("copy");
+    document.body.removeChild(el);
+  }
 };
