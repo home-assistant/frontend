91b2e2c781632f0762c30549c918316e962c825b
Philip Allgaier <mail@spacegaier.de>
2020-12-10 20:37:34 +0100

    Catch navigator.clipboard errors

--- src/common/util/copy-clipboard.ts
+++ src/common/util/copy-clipboard.ts
@@ -1,12 +1,17 @@
-export const copyToClipboard = (str) => {
+export const copyToClipboard = async (str) => {
   if (navigator.clipboard) {
-    navigator.clipboard.writeText(str);
-  } else {
-    const el = document.createElement("textarea");
-    el.value = str;
-    document.body.appendChild(el);
-    el.select();
-    document.execCommand("copy");
-    document.body.removeChild(el);
+    try {
+      await navigator.clipboard.writeText(str);
+      return;
+    } catch {
+      // just continue with the fallback coding below
+    }
   }
+
+  const el = document.createElement("textarea");
+  el.value = str;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand("copy");
+  document.body.removeChild(el);
 };
