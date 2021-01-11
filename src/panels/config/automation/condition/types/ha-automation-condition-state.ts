6865a2db094123fb22f8abdc6b171e8f3b01bcad
Nikfinn99 <nikfinn99@gmail.com>
2021-01-11 15:03:14 +0000

    rename variable to better name

--- src/panels/config/automation/condition/types/ha-automation-condition-state.ts
+++ src/panels/config/automation/condition/types/ha-automation-condition-state.ts
@@ -21,22 +21,22 @@ export class HaStateCondition extends LitElement implements ConditionElement {
 
   protected render() {
     const { entity_id, attribute, state } = this.condition;
-    let trgFor = this.condition.for;
+    let forTime = this.condition.for;
 
     if (
-      trgFor &&
-      ((trgFor as ForDict).hours ||
-        (trgFor as ForDict).minutes ||
-        (trgFor as ForDict).seconds)
+      forTime &&
+      ((forTime as ForDict).hours ||
+        (forTime as ForDict).minutes ||
+        (forTime as ForDict).seconds)
     ) {
       // If the trigger was defined using the yaml dict syntax, convert it to
       // the equivalent string format
-      let { hours = 0, minutes = 0, seconds = 0 } = trgFor as ForDict;
+      let { hours = 0, minutes = 0, seconds = 0 } = forTime as ForDict;
       hours = hours.toString().padStart(2, "0");
       minutes = minutes.toString().padStart(2, "0");
       seconds = seconds.toString().padStart(2, "0");
 
-      trgFor = `${hours}:${minutes}:${seconds}`;
+      forTime = `${hours}:${minutes}:${seconds}`;
     }
 
     return html`
@@ -71,7 +71,7 @@ export class HaStateCondition extends LitElement implements ConditionElement {
           "ui.panel.config.automation.editor.triggers.type.state.for"
         )}
         .name=${"for"}
-        .value=${trgFor}
+        .value=${forTime}
         @value-changed=${this._valueChanged}
       ></paper-input>
     `;
