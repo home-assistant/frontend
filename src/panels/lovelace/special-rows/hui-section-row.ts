f2279a9f2f44866d98c0f9f7d0ca8885fc3fb254
Philip Allgaier <mail@spacegaier.de>
2021-01-14 12:47:54 +0100

    Use correct font color var

--- src/panels/lovelace/special-rows/hui-section-row.ts
+++ src/panels/lovelace/special-rows/hui-section-row.ts
@@ -41,7 +41,7 @@ class HuiSectionRow extends LitElement implements LovelaceRow {
   static get styles(): CSSResult {
     return css`
       .label {
-        color: var(--secondary-text-color);
+        color: var(--primary-text-color);
         margin-left: 8px;
         margin-bottom: 8px;
         margin-top: 16px;
