ed368ddd9d214efd9cacf98e861e2cb729c197f4
Philip Allgaier <mail@spacegaier.de>
2021-01-27 10:46:15 +0100

    Add support for delay values split into parts + millisecond time input (#7886)
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>

--- src/components/ha-form/ha-form.ts
+++ src/components/ha-form/ha-form.ts
@@ -8,6 +8,7 @@ import {
 } from "lit-element";
 import { dynamicElement } from "../../common/dom/dynamic-element-directive";
 import { fireEvent } from "../../common/dom/fire_event";
+import { HaTimeData } from "../ha-time-input";
 import "./ha-form-boolean";
 import "./ha-form-constant";
 import "./ha-form-float";
@@ -71,7 +72,7 @@ export interface HaFormBooleanSchema extends HaFormBaseSchema {
 }
 
 export interface HaFormTimeSchema extends HaFormBaseSchema {
-  type: "time";
+  type: "positive_time_period_dict";
 }
 
 export interface HaFormDataContainer {
@@ -93,11 +94,7 @@ export type HaFormFloatData = number;
 export type HaFormBooleanData = boolean;
 export type HaFormSelectData = string;
 export type HaFormMultiSelectData = string[];
-export interface HaFormTimeData {
-  hours?: number;
-  minutes?: number;
-  seconds?: number;
-}
+export type HaFormTimeData = HaTimeData;
 
 export interface HaFormElement extends LitElement {
   schema: HaFormSchema | HaFormSchema[];
