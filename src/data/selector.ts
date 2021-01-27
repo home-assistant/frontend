a020c421d8170a7930710c5cd7fc68c899df915e
Thomas Lovén <thomasloven@gmail.com>
2021-01-27 11:45:51 +0100

    Add selectors for text and arbitrary objects (#8152)

--- src/data/selector.ts
+++ src/data/selector.ts
@@ -6,7 +6,9 @@ export type Selector =
   | NumberSelector
   | BooleanSelector
   | TimeSelector
-  | ActionSelector;
+  | ActionSelector
+  | StringSelector
+  | ObjectSelector;
 
 export interface EntitySelector {
   entity: {
@@ -81,4 +83,15 @@ export interface TimeSelector {
 export interface ActionSelector {
   // eslint-disable-next-line @typescript-eslint/ban-types
   action: {};
+}
+
+export interface StringSelector {
+  text: {
+    multiline: boolean;
+  };
+}
+
+export interface ObjectSelector {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  object: {};
 }
