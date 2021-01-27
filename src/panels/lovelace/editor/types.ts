5a3d3a50d3470ca2844e81001b8fb78d34fd8193
Philip Allgaier <mail@spacegaier.de>
2021-01-27 18:07:25 +0100

    Add check to prevent undefined access attempts

--- src/panels/lovelace/editor/types.ts
+++ src/panels/lovelace/editor/types.ts
@@ -96,8 +96,14 @@ export interface EditSubElementEvent {
 }
 
 export const actionConfigStruct = dynamic((_value, ctx) => {
-  const test = actionConfigMap[ctx.branch[0][ctx.path[0]].action];
-  return test || actionConfigStructType;
+  if (ctx.branch[0][ctx.path[0]]) {
+    return (
+      actionConfigMap[ctx.branch[0][ctx.path[0]].action] ||
+      actionConfigStructType
+    );
+  }
+
+  return actionConfigStructType;
 });
 
 const actionConfigStructUser = object({
