80917b959daac36beab30518aff876141708e477
posixx <2280400+posixx@users.noreply.github.com>
2021-02-04 19:21:07 +0100

    HA frontend change for alarm panel vacation mode

--- src/panels/lovelace/editor/config-elements/hui-alarm-panel-card-editor.ts
+++ src/panels/lovelace/editor/config-elements/hui-alarm-panel-card-editor.ts
@@ -65,7 +65,7 @@ export class HuiAlarmPanelCardEditor extends LitElement
       return html``;
     }
 
-    const states = ["arm_home", "arm_away", "arm_night", "arm_custom_bypass"];
+    const states = ["arm_home", "arm_away", "arm_night", "arm_vacation", "arm_custom_bypass"];
 
     return html`
       <div class="card-config">
