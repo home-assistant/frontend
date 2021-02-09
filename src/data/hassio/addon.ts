5b4cfc6a6aeb5e5ef2117bbf7ffb02a7a24b4538
Ludeeus <ludeeus@ludeeus.dev>
2021-02-09 10:20:36 +0000

    Revert 8322

--- src/data/hassio/addon.ts
+++ src/data/hassio/addon.ts
@@ -64,7 +64,7 @@ export interface HassioAddonDetails extends HassioAddonInfo {
   privileged: any;
   protected: boolean;
   rating: "1-6";
-  schema: HaFormSchema[] | null;
+  schema: HaFormSchema[];
   services_role: string[];
   slug: string;
   startup: "initialize" | "system" | "services" | "application" | "once";
