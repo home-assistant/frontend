49310737a5eae1a7b5c7b75b1f5c81c65835c640
Kendell R <KTibow@users.noreply.github.com>
2021-02-10 07:42:28 -0800

    Just use the dev version of copy to clipboard

--- src/common/util/copy-clipboard.ts
+++ src/common/util/copy-clipboard.ts
@@ -1,16 +1,17 @@
-const fallbackCopy = (str) => {
+export const copyToClipboard = async (str) => {
+  if (navigator.clipboard) {
+    try {
+      await navigator.clipboard.writeText(str);
+      return;
+    } catch {
+      // just continue with the fallback coding below
+    }
+  }
+
   const el = document.createElement("textarea");
   el.value = str;
   document.body.appendChild(el);
   el.select();
   document.execCommand("copy");
   document.body.removeChild(el);
-}
-
-export const copyToClipboard = (str) => {
-  if (navigator.clipboard) {
-    navigator.clipboard.writeText(str).catch(() => fallbackCopy(str));
-  } else {
-    fallbackCopy(str)
-  }
 };
