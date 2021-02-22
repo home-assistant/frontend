4cff6d52bdba17dec0d72cb7a475f0e40f8e28fe
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2021-02-22 12:06:13 +0100

    Update metadata license string

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     url="https://github.com/home-assistant/home-assistant-polymer",
     author="The Home Assistant Authors",
     author_email="hello@home-assistant.io",
-    license="Apache License 2.0",
+    license="Apache-2.0",
     packages=find_packages(include=["hass_frontend", "hass_frontend.*"]),
     include_package_data=True,
     zip_safe=False,
