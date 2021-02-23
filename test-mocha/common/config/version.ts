bfdf799b471772fa60c987f770f2937c590346a9
Ludeeus <ludeeus@ludeeus.dev>
2021-02-23 16:11:36 +0000

    restructure test

--- test-mocha/common/config/version.ts
+++ test-mocha/common/config/version.ts
@@ -1,30 +1,41 @@
 import { assert } from "chai";
 import { atLeastVersion } from "../../../src/common/config/version";
 
-describe("atLeastVersion", () => {
-  it("'2021.1.1', 2021, 1", () => {
-    assert.isTrue(atLeastVersion("2021.1.1", 2021, 1));
-  });
-  it("'2021.1.1', 2021, 2", () => {
-    assert.isTrue(atLeastVersion("2021.1.1", 2020, 12));
-  });
-  it("'2021.1.1', 2021, 1, 0", () => {
-    assert.isTrue(atLeastVersion("2021.1.1", 2021, 1, 0));
-  });
-  it("'2021.1.1', 2021, 1, 1", () => {
-    assert.isTrue(atLeastVersion("2021.1.1", 2021, 1, 1));
-  });
-  it("'2021.2.4', 0, 113", () => {
-    assert.isTrue(atLeastVersion("2021.2.4", 0, 113));
-  });
+const testTruthyData = [
+  { version: "2021.1.1", major: 2021, minor: 1, patch: 1 },
+  { version: "2021.1.1", major: 2021, minor: 1 },
+  { version: "2021.1.1", major: 2020, minor: 12, patch: 1 },
+  { version: "2021.1.1", major: 2020, minor: 12 },
+  { version: "2021.1.1", major: 2021, minor: 2, patch: 0 },
+  { version: "2021.1.1", major: 2021, minor: 2 },
 
-  it("'2021.2.3', 2021, 2, 4", () => {
-    assert.isFalse(atLeastVersion("2021.2.3", 2021, 2, 4));
-  });
-  it("'0.114.0', 0, 113", () => {
-    assert.isTrue(atLeastVersion("0.114.0", 0, 113));
-  });
-  it("'2021.2.0dev.2323', 2021, 2, 4", () => {
-    assert.isFalse(atLeastVersion("2021.2.0dev.2323", 2021, 2, 1));
-  });
+  { version: "2021.2.4", major: 0, minor: 113, patch: 0 },
+  { version: "2021.2.4", major: 0, minor: 113 },
+
+  { version: "0.114.0", major: 0, minor: 113, patch: 0 },
+  { version: "0.114.0", major: 0, minor: 113 },
+
+  { version: "2021.2.0dev.2323", major: 2021, minor: 2, patch: 0 },
+  { version: "2021.2.0dev.2323", major: 2021, minor: 2 },
+];
+
+const testFalsyData = [
+  { version: "0.114.0", major: 0, minor: 113 },
+  { version: "2021.2.0dev.2323", major: 2021, minor: 2, patch: 0 },
+];
+
+describe("atLeastVersion - Truthy", () => {
+  testTruthyData.forEach((test) =>
+    it(`'${test.version}' >= ${test.major},${test.minor},${test.patch}`, () => {
+      assert.isTrue(atLeastVersion("2021.1.1", 2021, 1));
+    })
+  );
+});
+
+describe("atLeastVersion - Falsy", () => {
+  testFalsyData.forEach((test) =>
+    it(`'${test.version}' >= ${test.major},${test.minor},${test.patch}`, () => {
+      assert.isTrue(atLeastVersion("2021.1.1", 2021, 1));
+    })
+  );
 });
