250c209a1f401e9481bba33215c9daf737326316
Joakim Sørensen <joasoe@gmail.com>
2021-02-23 16:03:58 +0100

    Update src/common/config/version.ts
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>

--- src/common/config/version.ts
+++ src/common/config/version.ts
@@ -8,9 +8,9 @@ export const atLeastVersion = (
 
   return (
     Number(haMajor) > major ||
-    (Number(haMajor) === major && patch === undefined
+    (Number(haMajor) === major && (patch === undefined
       ? Number(haMinor) >= minor
-      : Number(haMinor) > minor) ||
+      : Number(haMinor) > minor)) ||
     (patch !== undefined &&
       Number(haMajor) === major &&
       Number(haMinor) === minor &&
