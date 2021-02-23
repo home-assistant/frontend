39b8ebe7a1aa47e07b66f1259acafceb67511a1e
Ludeeus <ludeeus@ludeeus.dev>
2021-02-23 12:42:34 +0000

    Fix version check

---
+++ test-mocha/common/config/version.ts
@@ -0,0 +1,30 @@
+import { assert } from "chai";
+import { atLeastVersion } from "../../../src/common/config/version";
+
+describe("atLeastVersion", () => {
+  it("'2021.1.1', 2021, 1", () => {
+    assert.isTrue(atLeastVersion("2021.1.1", 2021, 1));
+  });
+  it("'2021.1.1', 2021, 2", () => {
+    assert.isTrue(atLeastVersion("2021.1.1", 2020, 12));
+  });
+  it("'2021.1.1', 2021, 1, 0", () => {
+    assert.isTrue(atLeastVersion("2021.1.1", 2021, 1, 0));
+  });
+  it("'2021.1.1', 2021, 1, 1", () => {
+    assert.isTrue(atLeastVersion("2021.1.1", 2021, 1, 1));
+  });
+  it("'2021.2.4', 0, 113", () => {
+    assert.isTrue(atLeastVersion("2021.2.4", 0, 113));
+  });
+
+  it("'2021.2.3', 2021, 2, 4", () => {
+    assert.isFalse(atLeastVersion("2021.2.3", 2021, 2, 4));
+  });
+  it("'0.114.0', 0, 113", () => {
+    assert.isFalse(atLeastVersion("0.114.0", 0, 113));
+  });
+  it("'2021.2.0dev.2323', 2021, 2, 4", () => {
+    assert.isFalse(atLeastVersion("2021.2.0dev.2323", 2021, 2, 1));
+  });
+});
