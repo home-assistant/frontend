a648e9be4914e992322d7571a7b37bdfc2a6c3d2
Joakim Sørensen <joasoe@gmail.com>
2021-02-23 22:17:22 +0100

    Fix atLeastVersion (#8437)
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>

--- src/common/config/version.ts
+++ src/common/config/version.ts
@@ -5,11 +5,15 @@ export const atLeastVersion = (
   patch?: number
 ): boolean => {
   const [haMajor, haMinor, haPatch] = version.split(".", 3);
+
   return (
     Number(haMajor) > major ||
-    (Number(haMajor) === major && Number(haMinor) >= minor) ||
+    (Number(haMajor) === major && (patch === undefined
+      ? Number(haMinor) >= minor
+      : Number(haMinor) > minor)) ||
     (patch !== undefined &&
-      Number(haMajor) === major && Number(haMinor) === minor &&
+      Number(haMajor) === major &&
+      Number(haMinor) === minor &&
       Number(haPatch) >= patch)
   );
 };
