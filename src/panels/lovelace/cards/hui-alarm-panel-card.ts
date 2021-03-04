2abba7e4458cf1cfb66ff9b8a2190c14e7dd0717
Josh McCarty <josh@joshmccarty.com>
2021-03-04 02:08:16 -0700

    Alarm numeric inputmode (#8521)

--- src/panels/lovelace/cards/hui-alarm-panel-card.ts
+++ src/panels/lovelace/cards/hui-alarm-panel-card.ts
@@ -191,6 +191,9 @@ class HuiAlarmPanelCard extends LitElement implements LovelaceCard {
                 id="alarmCode"
                 .label=${this.hass.localize("ui.card.alarm_control_panel.code")}
                 type="password"
+                .inputmode=${stateObj.attributes.code_format === FORMAT_NUMBER
+                  ? "numeric"
+                  : "text"}
               ></paper-input>
             `}
         ${stateObj.attributes.code_format !== FORMAT_NUMBER
