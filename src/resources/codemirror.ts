0d1c72386e0f8530b9776664e0373b8feb353cca
Bram Kragten <mail@bramkragten.nl>
2021-03-04 16:43:34 +0100

    Bump codemirror to 0.18 (#8546)

--- src/resources/codemirror.ts
+++ src/resources/codemirror.ts
@@ -4,19 +4,24 @@ import { StreamLanguage } from "@codemirror/stream-parser";
 import { jinja2 } from "@codemirror/legacy-modes/mode/jinja2";
 import { yaml } from "@codemirror/legacy-modes/mode/yaml";
 import { indentLess, indentMore } from "@codemirror/commands";
+import { Compartment } from "@codemirror/state";
 
-export { keymap } from "@codemirror/view";
+export { keymap, highlightActiveLine } from "@codemirror/view";
 export { CMEditorView as EditorView };
-export { EditorState, Prec, tagExtension } from "@codemirror/state";
+export { EditorState, Prec } from "@codemirror/state";
 export { defaultKeymap } from "@codemirror/commands";
 export { lineNumbers } from "@codemirror/gutter";
 export { searchKeymap, highlightSelectionMatches } from "@codemirror/search";
 export { history, historyKeymap } from "@codemirror/history";
+export { rectangularSelection } from "@codemirror/rectangular-selection";
 
 export const langs = {
   jinja2: StreamLanguage.define(jinja2),
   yaml: StreamLanguage.define(yaml),
 };
+
+export const langCompartment = new Compartment();
+export const readonlyCompartment = new Compartment();
 
 export const tabKeyBindings: KeyBinding[] = [
   { key: "Tab", run: indentMore },
@@ -27,7 +32,7 @@ export const tabKeyBindings: KeyBinding[] = [
 ];
 
 export const theme = CMEditorView.theme({
-  $: {
+  "&": {
     color: "var(--primary-text-color)",
     backgroundColor:
       "var(--code-editor-background-color, var(--card-background-color))",
@@ -36,27 +41,28 @@ export const theme = CMEditorView.theme({
     maxHeight: "var(--code-mirror-max-height, unset)",
   },
 
-  $scroller: { outline: "none" },
-
-  $content: { caretColor: "var(--secondary-text-color)" },
-
-  $$focused: { outline: "none" },
-
-  "$$focused $cursor": { borderLeftColor: "#var(--secondary-text-color)" },
-  "$$focused $selectionBackground, $selectionBackground": {
+  "&.cm-focused": { outline: "none" },
+  "&.cm-focused .cm-cursor": {
+    borderLeftColor: "#var(--secondary-text-color)",
+  },
+  "&.cm-focused .cm-selectionBackground, .cm-selectionBackground": {
     backgroundColor: "rgba(var(--rgb-primary-color), 0.3)",
   },
 
-  $panels: {
+  ".cm-scroller": { outline: "none" },
+
+  ".cm-content": { caretColor: "var(--secondary-text-color)" },
+
+  ".cm-panels": {
     backgroundColor: "var(--primary-background-color)",
     color: "var(--primary-text-color)",
   },
-  "$panels.top": { borderBottom: "1px solid var(--divider-color)" },
-  "$panels.bottom": { borderTop: "1px solid var(--divider-color)" },
+  ".cm-panels.top": { borderBottom: "1px solid var(--divider-color)" },
+  ".cm-panels.bottom": { borderTop: "1px solid var(--divider-color)" },
 
-  "$panel.search input": { margin: "4px 4px 0" },
+  ".cm-panel.search input": { margin: "4px 4px 0" },
 
-  $button: {
+  ".cm-button": {
     border: "1px solid var(--primary-color)",
     padding: "0px 16px",
     textTransform: "uppercase",
@@ -72,7 +78,7 @@ export const theme = CMEditorView.theme({
     letterSpacing: "var(--mdc-typography-button-letter-spacing, 0.0892857em)",
   },
 
-  $textfield: {
+  ".cm-textfield": {
     padding: "4px 0px 5px",
     borderRadius: "0",
     fontSize: "16px",
@@ -93,20 +99,20 @@ export const theme = CMEditorView.theme({
     },
   },
 
-  $selectionMatch: {
+  ".cm-selectionMatch": {
     backgroundColor: "rgba(var(--rgb-primary-color), 0.1)",
   },
 
-  $searchMatch: {
+  ".cm-searchMatch": {
     backgroundColor: "rgba(var(--rgb-accent-color), .2)",
     outline: "1px solid rgba(var(--rgb-accent-color), .4)",
   },
-  "$searchMatch.selected": {
+  ".cm-searchMatch.selected": {
     backgroundColor: "rgba(var(--rgb-accent-color), .4)",
     outline: "1px solid var(--accent-color)",
   },
 
-  $gutters: {
+  ".cm-gutters": {
     backgroundColor:
       "var(--paper-dialog-background-color, var(--primary-background-color))",
     color: "var(--paper-dialog-color, var(--secondary-text-color))",
@@ -115,15 +121,15 @@ export const theme = CMEditorView.theme({
       "1px solid var(--paper-input-container-color, var(--secondary-text-color))",
     paddingRight: "1px",
   },
-  "$$focused $gutters": {
+  "&.cm-focused cm-gutters": {
     borderRight:
       "2px solid var(--paper-input-container-focus-color, var(--primary-color))",
     paddingRight: "0",
   },
-  "$gutterElementags.lineNumber": { color: "inherit" },
+  ".cm-gutterElementags.lineNumber": { color: "inherit" },
 });
 
-export const highlightStyle = HighlightStyle.define(
+export const highlightStyle = HighlightStyle.define([
   { tag: tags.keyword, color: "var(--codemirror-keyword, #6262FF)" },
   {
     tag: [
@@ -194,5 +200,5 @@ export const highlightStyle = HighlightStyle.define(
   { tag: tags.processingInstruction, color: "var(--secondary-text-color)" },
   { tag: tags.string, color: "var(--codemirror-string, #07a)" },
   { tag: tags.inserted, color: "var(--codemirror-string2, #07a)" },
-  { tag: tags.invalid, color: "var(--error-color)" }
-);
+  { tag: tags.invalid, color: "var(--error-color)" },
+]);
