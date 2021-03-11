ca933a77fd979c0d0095a3624ddbcb660c012dc1
Josh McCarty <josh@joshmccarty.com>
2021-03-11 08:15:14 -0700

    Add tests for formatNumber

---
+++ test-mocha/common/string/format_number.ts
@@ -0,0 +1,33 @@
+import { assert } from "chai";
+
+import { formatNumber } from "../../../src/common/string/format_number";
+import { NumberFormat } from "../../../src/data/translation";
+
+describe("formatNumber", () => {
+  // Node only ships with English support for `Intl`, so we can not test for other number formats here.
+  it("Formats English numbers", () => {
+    assert.strictEqual(
+      formatNumber(1234.5, {
+        language: "en",
+        number_format: NumberFormat.auto,
+      }),
+      "1,234.5"
+    );
+  });
+
+  it("Formats number with options", () => {
+    assert.strictEqual(
+      formatNumber(
+        1234.5,
+        {
+          language: "en",
+          number_format: NumberFormat.auto,
+        },
+        {
+          minimumFractionDigits: 2,
+        }
+      ),
+      "1,234.50"
+    );
+  });
+});
