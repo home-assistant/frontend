cd2404f26a07d43092fda12807edcdbdf8e8ef8c
Bram Kragten <mail@bramkragten.nl>
2021-03-30 02:05:51 +0200

    Add link between trace and editor (#8750)

--- src/panels/config/automation/blueprint-automation-editor.ts
+++ src/panels/config/automation/blueprint-automation-editor.ts
@@ -104,12 +104,21 @@ export class HaBlueprintAutomationEditor extends LitElement {
                       "ui.panel.config.automation.editor.enable_disable"
                     )}
                   </div>
-                  <mwc-button
-                    @click=${this._runActions}
-                    .stateObj=${this.stateObj}
-                  >
-                    ${this.hass.localize("ui.card.automation.trigger")}
-                  </mwc-button>
+                  <div>
+                    <a href="/config/automation/trace/${this.config.id}">
+                      <mwc-button>
+                        ${this.hass.localize(
+                          "ui.panel.config.automation.editor.show_trace"
+                        )}
+                      </mwc-button>
+                    </a>
+                    <mwc-button
+                      @click=${this._runActions}
+                      .stateObj=${this.stateObj}
+                    >
+                      ${this.hass.localize("ui.card.automation.trigger")}
+                    </mwc-button>
+                  </div>
                 </div>
               `
             : ""}
