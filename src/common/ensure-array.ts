3d3c04a5b2fd30497a71cdff3367fb7567008b58
Paulus Schoutsen <balloob@gmail.com>
2021-04-01 20:15:39 +0000

    Fix typing

--- src/common/ensure-array.ts
+++ src/common/ensure-array.ts
@@ -1,6 +1,8 @@
-export const ensureArray = <T>(value: T | T[]): T[] => {
-  if (!value || Array.isArray(value)) {
-    return value as T[];
+export function ensureArray(value: undefined): undefined;
+export function ensureArray<T>(value: T | T[]): T[];
+export function ensureArray(value) {
+  if (value === undefined || Array.isArray(value)) {
+    return value;
   }
   return [value];
-};
+}
