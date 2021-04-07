9f979c7c90ea05d2763ea47c0a4fdb4e045c4b90
Bram Kragten <mail@bramkragten.nl>
2021-04-07 12:25:17 +0200

    Keep root state when replacing, fix subpages for menu button on mobile (#8837)

--- src/panels/config/cloud/ha-config-cloud.ts
+++ src/panels/config/cloud/ha-config-cloud.ts
@@ -128,6 +128,7 @@ class HaConfigCloud extends HassRouterPage {
         hass: this.hass,
         email: this._loginEmail,
         isWide: this.isWide,
+        narrow: this.narrow,
         cloudStatus: this.cloudStatus,
         flashMessage: this._flashMessage,
       });
