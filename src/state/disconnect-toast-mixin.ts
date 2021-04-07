abdf8c52128e23bb80eeb7ac9d58e7156d1da446
J. Nick Koston <nick@koston.org>
2021-04-06 23:34:25 -1000

    type

--- src/state/disconnect-toast-mixin.ts
+++ src/state/disconnect-toast-mixin.ts
@@ -75,7 +75,7 @@ export default <T extends Constructor<HassBaseEl>>(superClass: T) =>
       });
     }
 
-    private _handleMessage(message: any): void {
+    private _handleMessage(message: { string: number }): void {
       if (this.hass!.config.state !== STATE_NOT_RUNNING) {
         return;
       }
