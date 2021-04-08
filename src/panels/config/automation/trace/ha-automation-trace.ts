25e219ca6f7ab79e6229b0a443a038398ba5d3e8
Paulus Schoutsen <balloob@gmail.com>
2021-04-08 13:59:24 -0700

    Pass narrow (#8864)

--- src/panels/config/automation/trace/ha-automation-trace.ts
+++ src/panels/config/automation/trace/ha-automation-trace.ts
@@ -242,6 +242,7 @@ export class HaAutomationTrace extends LitElement {
                     ? html`
                         <ha-automation-trace-logbook
                           .hass=${this.hass}
+                          .narrow=${this.narrow}
                           .logbookEntries=${this._logbookEntries}
                         ></ha-automation-trace-logbook>
                       `
