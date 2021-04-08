2409c82256f9117b2ab0362bff0a145841e336d2
Paulus Schoutsen <balloob@gmail.com>
2021-04-08 18:36:54 +0000

    Fix it weer

--- src/panels/config/automation/trace/ha-automation-trace-logbook.ts
+++ src/panels/config/automation/trace/ha-automation-trace-logbook.ts
@@ -16,16 +16,17 @@ import "../../../logbook/ha-logbook";
 export class HaAutomationTraceLogbook extends LitElement {
   @property({ attribute: false }) public hass!: HomeAssistant;
 
+  @property({ type: Boolean, reflect: true }) public narrow!: boolean;
+
   @property({ attribute: false }) public logbookEntries!: LogbookEntry[];
 
   protected render(): TemplateResult {
-    return html`
-    return logbookEntries.length
+    return this.logbookEntries.length
       ? html`
           <ha-logbook
             relative-time
             .hass=${this.hass}
-            .entries=${logbookEntries}
+            .entries=${this.logbookEntries}
             .narrow=${this.narrow}
           ></ha-logbook>
           <hat-logbook-note></hat-logbook-note>
@@ -33,8 +34,6 @@ export class HaAutomationTraceLogbook extends LitElement {
       : html`<div class="padded-box">
           No Logbook entries found for this step.
         </div>`;
-  }
-    `;
   }
 
   static get styles(): CSSResult[] {
