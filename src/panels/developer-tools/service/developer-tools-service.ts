88ec7a10c923d23d6ecb4284cc14d839835dee40
Bram Kragten <mail@bramkragten.nl>
2021-04-12 23:30:46 +0200

    Show toast with call service error

--- src/panels/developer-tools/service/developer-tools-service.ts
+++ src/panels/developer-tools/service/developer-tools-service.ts
@@ -28,6 +28,7 @@ import { haStyle } from "../../../resources/styles";
 import "../../../styles/polymer-ha-style";
 import { HomeAssistant } from "../../../types";
 import "../../../util/app-localstorage-document";
+import { showToast } from "../../../util/toast";
 
 class HaPanelDevService extends LitElement {
   @property({ attribute: false }) public hass!: HomeAssistant;
@@ -267,11 +268,22 @@ class HaPanelDevService extends LitElement {
     }
   );
 
-  private _callService() {
+  private async _callService() {
     if (!this._serviceData?.service) {
       return;
     }
-    callExecuteScript(this.hass, [this._serviceData]);
+    try {
+      await callExecuteScript(this.hass, [this._serviceData]);
+    } catch (err) {
+      showToast(this, {
+        message:
+          this.hass.localize(
+            "ui.notification_toast.service_call_failed",
+            "service",
+            `${this._serviceData.service}`
+          ) + ` ${err.message}`,
+      });
+    }
   }
 
   private _toggleYaml() {
