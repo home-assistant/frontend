7bef716a74059b7a9890bc84c4250c76e45fca71
Bram Kragten <mail@bramkragten.nl>
2021-04-13 16:54:53 +0200

    Fix time selector + base am/pm on user language

--- src/components/ha-selector/ha-selector-time.ts
+++ src/components/ha-selector/ha-selector-time.ts
@@ -1,12 +1,9 @@
 import { customElement, html, LitElement, property } from "lit-element";
+import memoizeOne from "memoize-one";
 import { fireEvent } from "../../common/dom/fire_event";
 import { TimeSelector } from "../../data/selector";
 import { HomeAssistant } from "../../types";
 import "../paper-time-input";
-
-const test = new Date().toLocaleString();
-const useAMPM = test.includes("AM") || test.includes("PM");
-
 @customElement("ha-selector-time")
 export class HaTimeSelector extends LitElement {
   @property() public hass!: HomeAssistant;
@@ -19,7 +16,14 @@ export class HaTimeSelector extends LitElement {
 
   @property({ type: Boolean }) public disabled = false;
 
+  private _useAmPm = memoizeOne((langauge: string) => {
+    const test = new Date().toLocaleString(langauge);
+    return test.includes("AM") || test.includes("PM");
+  });
+
   protected render() {
+    const useAMPM = this._useAmPm(this.hass.locale.language);
+
     const parts = this.value?.split(":") || [];
     const hours = useAMPM ? parts[0] ?? "12" : parts[0] ?? "0";
 
@@ -42,12 +46,21 @@ export class HaTimeSelector extends LitElement {
 
   private _timeChanged(ev) {
     let value = ev.target.value;
+    const useAMPM = this._useAmPm(this.hass.locale.language);
     if (useAMPM) {
       let hours = Number(ev.target.hour);
       if (ev.target.amPm === "PM") {
         hours += 12;
       }
       value = `${hours}:${ev.target.min}:${ev.target.sec}`;
+    }
+    if (
+      (!this.value &&
+        ((!useAMPM && value === "0:00:00") ||
+          (useAMPM && value === "12:00:00"))) ||
+      value === this.value
+    ) {
+      return;
     }
     fireEvent(this, "value-changed", {
       value,
