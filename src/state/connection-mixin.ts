49795870bfadcc3971f08588dede1447b8f07ea3
J. Nick Koston <nick@koston.org>
2021-04-16 14:39:51 -1000

    tsc

--- src/state/connection-mixin.ts
+++ src/state/connection-mixin.ts
@@ -84,7 +84,7 @@ export const connectionMixin = <T extends Constructor<HassBaseEl>>(
               err.error.code === ERR_CONNECTION_LOST &&
               serviceCallWillDisconnect(domain, service)
             ) {
-              return this;
+              throw err;
             }
             if (__DEV__) {
               // eslint-disable-next-line no-console
