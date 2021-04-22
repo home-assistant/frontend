2291771830eb94e9bde4bfc5298e650b75608608
Philip Allgaier <mail@spacegaier.de>
2021-04-22 23:40:10 +0200

    Ensure 0 does not get formatted to empty string

--- test-mocha/common/string/format_number.ts
+++ test-mocha/common/string/format_number.ts
@@ -15,6 +15,56 @@ describe("formatNumber", () => {
     );
   });
 
+  it("Test format 'none' (keep comma despite language 'en')", () => {
+    assert.strictEqual(
+      formatNumber("1,23", {
+        language: "en",
+        number_format: NumberFormat.none,
+      }),
+      "1,23"
+    );
+  });
+
+  it("Ensure zero is kept for format 'language'", () => {
+    assert.strictEqual(
+      formatNumber(0, {
+        language: "en",
+        number_format: NumberFormat.language,
+      }),
+      "0"
+    );
+  });
+
+  it("Ensure zero is kept for format 'none'", () => {
+    assert.strictEqual(
+      formatNumber(0, {
+        language: "en",
+        number_format: NumberFormat.none,
+      }),
+      "0"
+    );
+  });
+
+  it("Test empty string input for format 'none'", () => {
+    assert.strictEqual(
+      formatNumber("", {
+        language: "en",
+        number_format: NumberFormat.none,
+      }),
+      ""
+    );
+  });
+
+  it("Test empty string input for format 'language'", () => {
+    assert.strictEqual(
+      formatNumber("", {
+        language: "en",
+        number_format: NumberFormat.language,
+      }),
+      "0"
+    );
+  });
+
   it("Formats number with options", () => {
     assert.strictEqual(
       formatNumber(
