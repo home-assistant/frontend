18fa4135cdb792c36810f89c35bedeef289cd5bd
Philip Allgaier <mail@spacegaier.de>
2021-04-25 23:35:17 +0200

    Revert color contrast fixes

--- src/common/dom/apply_themes_on_element.ts
+++ src/common/dom/apply_themes_on_element.ts
@@ -93,7 +93,7 @@ export const applyThemesOnElement = (
       themeRules["light-primary-color"] = rgb2hex(rgbLightPrimaryColor);
       themeRules["dark-primary-color"] = lab2hex(labDarken(labPrimaryColor));
       themeRules["text-primary-color"] =
-        rgbContrast(rgbPrimaryColor, [33, 33, 33]) < 7 ? "#fff" : "#212121";
+        rgbContrast(rgbPrimaryColor, [33, 33, 33]) < 6 ? "#fff" : "#212121";
       themeRules["text-light-primary-color"] =
         rgbContrast(rgbLightPrimaryColor, [33, 33, 33]) < 6
           ? "#fff"
