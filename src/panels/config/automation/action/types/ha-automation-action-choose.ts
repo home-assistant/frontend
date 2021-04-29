48cd399dc3768707bfcf7f19061ca4dce346efac
Bram Kragten <mail@bramkragten.nl>
2021-04-29 17:53:58 +0200

    Handle when choose is not an array (#9028)

--- src/panels/config/automation/action/types/ha-automation-action-choose.ts
+++ src/panels/config/automation/action/types/ha-automation-action-choose.ts
@@ -10,6 +10,7 @@ import {
 } from "lit-element";
 import { html } from "lit-html";
 import { fireEvent } from "../../../../../common/dom/fire_event";
+import { ensureArray } from "../../../../../common/ensure-array";
 import { Condition } from "../../../../../data/automation";
 import { Action, ChooseAction } from "../../../../../data/script";
 import { haStyle } from "../../../../../resources/styles";
@@ -31,7 +32,7 @@ export class HaChooseAction extends LitElement implements ActionElement {
     const action = this.action;
 
     return html`
-      ${(action.choose || []).map(
+      ${(action.choose ? ensureArray(action.choose) : []).map(
         (option, idx) => html`<ha-card>
           <mwc-icon-button
             .idx=${idx}
@@ -101,7 +102,9 @@ export class HaChooseAction extends LitElement implements ActionElement {
     ev.stopPropagation();
     const value = ev.detail.value as Condition[];
     const index = (ev.target as any).idx;
-    const choose = this.action.choose ? [...this.action.choose] : [];
+    const choose = this.action.choose
+      ? [...ensureArray(this.action.choose)]
+      : [];
     choose[index].conditions = value;
     fireEvent(this, "value-changed", {
       value: { ...this.action, choose },
@@ -112,7 +115,9 @@ export class HaChooseAction extends LitElement implements ActionElement {
     ev.stopPropagation();
     const value = ev.detail.value as Action[];
     const index = (ev.target as any).idx;
-    const choose = this.action.choose ? [...this.action.choose] : [];
+    const choose = this.action.choose
+      ? [...ensureArray(this.action.choose)]
+      : [];
     choose[index].sequence = value;
     fireEvent(this, "value-changed", {
       value: { ...this.action, choose },
@@ -120,7 +125,9 @@ export class HaChooseAction extends LitElement implements ActionElement {
   }
 
   private _addOption() {
-    const choose = this.action.choose ? [...this.action.choose] : [];
+    const choose = this.action.choose
+      ? [...ensureArray(this.action.choose)]
+      : [];
     choose.push({ conditions: [], sequence: [] });
     fireEvent(this, "value-changed", {
       value: { ...this.action, choose },
@@ -129,7 +136,9 @@ export class HaChooseAction extends LitElement implements ActionElement {
 
   private _removeOption(ev: CustomEvent) {
     const index = (ev.currentTarget as any).idx;
-    const choose = this.action.choose ? [...this.action.choose] : [];
+    const choose = this.action.choose
+      ? [...ensureArray(this.action.choose)]
+      : [];
     choose.splice(index, 1);
     fireEvent(this, "value-changed", {
       value: { ...this.action, choose },
