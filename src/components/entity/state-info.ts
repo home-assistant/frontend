8f7cc45992c184ebc3a351f661ddf38b41b9a1a0
Bram Kragten <mail@bramkragten.nl>
2021-05-10 22:22:54 +0200

    Align state info in center (#9153)

--- src/components/entity/state-info.ts
+++ src/components/entity/state-info.ts
@@ -73,7 +73,7 @@ class StateInfo extends LitElement {
                 </div>
               </paper-tooltip>
             </div>`
-          : html`<div class="extra-info"><slot> </slot></div>`}
+          : html`<div class="extra-info"><slot></slot></div>`}
       </div>`;
   }
 
@@ -106,6 +106,10 @@ class StateInfo extends LitElement {
 
       .info {
         margin-left: 56px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        height: 100%;
       }
 
       :host([rtl]) .info {
