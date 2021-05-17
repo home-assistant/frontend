b5d50ec3cda940a1ac9026f17b3802d754c0cb32
Paulus Schoutsen <balloob@gmail.com>
2021-05-17 11:19:47 -0700

    Trace fixes (#9192)
    * Show sub-conditions
    
    * Better show errors in timeline
    
    * Add rendered nodes
    
    * Fix some rendering issues with sub-conditions in timeline
    
    * Improve condition rendering

--- src/components/trace/hat-trace-timeline.ts
+++ src/components/trace/hat-trace-timeline.ts
@@ -245,13 +245,15 @@ class ActionRenderer {
     try {
       data = getDataFromPath(this.trace.config, path);
     } catch (err) {
-      this.entries.push(
-        html`Unable to extract path ${path}. Download trace and report as bug`
+      this._renderEntry(
+        path,
+        `Unable to extract path ${path}. Download trace and report as bug`
       );
       return index + 1;
     }
 
-    const isTopLevel = path.split("/").length === 2;
+    const parts = path.split("/");
+    const isTopLevel = parts.length === 2;
 
     if (!isTopLevel && !actionType) {
       this._renderEntry(path, path.replace(/\//g, " "));
@@ -267,7 +269,16 @@ class ActionRenderer {
     }
 
     this._renderEntry(path, describeAction(this.hass, data, actionType));
-    return index + 1;
+
+    let i = index + 1;
+
+    for (; i < this.keys.length; i++) {
+      if (this.keys[i].split("/").length === parts.length) {
+        break;
+      }
+    }
+
+    return i;
   }
 
   private _handleTrigger(index: number, triggerStep: TriggerTraceStep): number {
@@ -303,7 +314,7 @@ class ActionRenderer {
     // +4: executed sequence
 
     const choosePath = this.keys[index];
-    const startLevel = choosePath.split("/").length - 1;
+    const startLevel = choosePath.split("/").length;
 
     const chooseTrace = this._getItem(index)[0] as ChooseActionTraceStep;
     const defaultExecuted = chooseTrace.result?.choice === "default";
