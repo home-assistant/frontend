b294726200c7476eb6ab171b3964d9995d46c21b
Philip Allgaier <mail@spacegaier.de>
2021-05-24 23:27:00 +0200

    Ensure timer row uses correct state translation keys (#9143)
    * Ensure timer row uses correct state translation keys
    
    * Changes from review
    
    * Update src/panels/lovelace/entity-rows/hui-timer-entity-row.ts
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>
    
    * Move logic to data/timer
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>

--- test-mocha/common/entity/timer_time_remaining_test.ts
+++ test-mocha/common/entity/timer_time_remaining_test.ts
@@ -1,7 +1,7 @@
 import { assert } from "chai";
 import * as sinon from "sinon";
 
-import { timerTimeRemaining } from "../../../src/common/entity/timer_time_remaining";
+import { timerTimeRemaining } from "../../../src/data/timer";
 
 describe("timerTimeRemaining", () => {
   it("works with idle timers", () => {
