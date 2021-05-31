72827be1a5a0ef7d77b0ef691b9009dc65cc354d
Bram Kragten <mail@bramkragten.nl>
2021-05-31 11:22:12 +0200

    Fix store auth disappearing

--- src/dialogs/ha-store-auth-card.ts
+++ src/dialogs/ha-store-auth-card.ts
@@ -50,6 +50,9 @@ class HaStoreAuth extends LitElement {
         bottom: 16px;
         right: 16px;
         transition: bottom 0.25s;
+        --ha-card-box-shadow: 0px 3px 5px -1px rgba(0, 0, 0, 0.2),
+          0px 6px 10px 0px rgba(0, 0, 0, 0.14),
+          0px 1px 18px 0px rgba(0, 0, 0, 0.12);
       }
 
       .card-actions {
