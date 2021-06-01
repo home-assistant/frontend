2427d68aa1383e8c7c88e075f276528443471308
Bram Kragten <mail@bramkragten.nl>
2021-06-01 11:39:15 +0200

    Use local version 0.7 of lit-virtualizer (#9321)

---
+++ src/resources/lit-virtualizer/lit-virtualizer.d.ts
@@ -0,0 +1,10 @@
+export { scroll } from "./lib/scroll.js";
+export {
+  Layout1d,
+  layout1d,
+  Layout1dGrid,
+  RangeChangeEvent,
+  scrollerRef,
+} from "./lib/uni-virtualizer/uni-virtualizer.js";
+export { LitVirtualizer } from "./lib/lit-virtualizer.js";
+//# sourceMappingURL=lit-virtualizer.d.ts.map
