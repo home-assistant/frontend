c9ed746f9ce89e46169e17572eff023bf64d3149
Thomas Lovén <thomasloven@gmail.com>
2021-07-30 10:51:21 +0200

    Add needle option to ha-gauge and gauge card (#9637)

--- src/panels/lovelace/editor/config-elements/hui-gauge-card-editor.ts
+++ src/panels/lovelace/editor/config-elements/hui-gauge-card-editor.ts
@@ -1,7 +1,7 @@
 import "@polymer/paper-input/paper-input";
 import { css, CSSResultGroup, html, LitElement, TemplateResult } from "lit";
 import { customElement, property, state } from "lit/decorators";
-import { assert, number, object, optional, string } from "superstruct";
+import { assert, boolean, number, object, optional, string } from "superstruct";
 import { fireEvent } from "../../../../common/dom/fire_event";
 import { computeRTLDirection } from "../../../../common/util/compute_rtl";
 import "../../../../components/ha-formfield";
@@ -23,6 +23,7 @@ const cardConfigStruct = object({
   max: optional(number()),
   severity: optional(object()),
   theme: optional(string()),
+  needle: optional(boolean()),
 });
 
 const includeDomains = ["counter", "input_number", "number", "sensor"];
@@ -139,6 +140,17 @@ export class HuiGaugeCardEditor
         ></paper-input>
         <ha-formfield
           .label=${this.hass.localize(
+            "ui.panel.lovelace.editor.card.gauge.needle_gauge"
+          )}
+          .dir=${computeRTLDirection(this.hass)}
+        >
+          <ha-switch
+            .checked="${this._config!.needle !== undefined}"
+            @change="${this._toggleNeedle}"
+          ></ha-switch
+        ></ha-formfield>
+        <ha-formfield
+          .label=${this.hass.localize(
             "ui.panel.lovelace.editor.card.gauge.severity.define"
           )}
           .dir=${computeRTLDirection(this.hass)}
@@ -210,6 +222,22 @@ export class HuiGaugeCardEditor
         }
       `,
     ];
+  }
+
+  private _toggleNeedle(ev: EntitiesEditorEvent): void {
+    if (!this._config || !this.hass) {
+      return;
+    }
+    if ((ev.target as EditorTarget).checked) {
+      this._config = {
+        ...this._config,
+        needle: true,
+      };
+    } else {
+      this._config = { ...this._config };
+      delete this._config.needle;
+    }
+    fireEvent(this, "config-changed", { config: this._config });
   }
 
   private _toggleSeverity(ev: EntitiesEditorEvent): void {
