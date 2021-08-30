dd9a9b34d162b0c7ec2c37b1f459f5490389322a
Joakim Sørensen <joasoe@gmail.com>
2021-08-30 18:08:48 +0200

    Fix width when there is no data in energy cards (#9888)
    * Fix width when there is no data in energy cards
    
    * right: 0;

--- src/panels/lovelace/cards/energy/hui-energy-gas-graph-card.ts
+++ src/panels/lovelace/cards/energy/hui-energy-gas-graph-card.ts
@@ -321,10 +321,10 @@ export class HuiEnergyGasGraphCard
       }
       .no-data {
         position: absolute;
-        width: 100%;
         height: 100%;
         top: 0;
         left: 0;
+        right: 0;
         display: flex;
         justify-content: center;
         align-items: center;
