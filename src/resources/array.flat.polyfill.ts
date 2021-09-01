0cbac8bb44563b4eaadf6eea288596467d9d7d6c
Bram Kragten <mail@bramkragten.nl>
2021-09-01 16:23:41 +0200

    Polyfill `Array.flat` (#9917)

---
+++ src/resources/array.flat.polyfill.ts
@@ -0,0 +1,26 @@
+/* eslint-disable no-extend-native */
+// @ts-expect-error
+if (!Array.prototype.flat) {
+  Object.defineProperty(Array.prototype, "flat", {
+    configurable: true,
+    writable: true,
+    value: function (...args) {
+      const depth = typeof args[0] === "undefined" ? 1 : Number(args[0]) || 0;
+      const result = [];
+      const forEach = result.forEach;
+
+      const flatDeep = (arr: Array<any>, dpth: number) => {
+        forEach.call(arr, (val) => {
+          if (dpth > 0 && Array.isArray(val)) {
+            flatDeep(val, dpth - 1);
+          } else {
+            result.push(val);
+          }
+        });
+      };
+
+      flatDeep(this, depth);
+      return result;
+    },
+  });
+}
