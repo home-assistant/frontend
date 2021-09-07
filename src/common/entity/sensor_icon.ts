bb8ad1212fce061f6d4d29849eec4f618eb46504
Ville Skyttä <ville.skytta@iki.fi>
2021-09-07 10:21:51 +0300

    Use SENSOR_DEVICE_CLASS_* constants more (#9982)

--- src/common/entity/sensor_icon.ts
+++ src/common/entity/sensor_icon.ts
@@ -2,6 +2,7 @@
 import { HassEntity } from "home-assistant-js-websocket";
 import { FIXED_DEVICE_CLASS_ICONS, UNIT_C, UNIT_F } from "../const";
 import { batteryIcon } from "./battery_icon";
+import { SENSOR_DEVICE_CLASS_BATTERY } from "../../data/sensor";
 
 export const sensorIcon = (stateObj?: HassEntity): string | undefined => {
   const dclass = stateObj?.attributes.device_class;
@@ -10,7 +11,7 @@ export const sensorIcon = (stateObj?: HassEntity): string | undefined => {
     return FIXED_DEVICE_CLASS_ICONS[dclass];
   }
 
-  if (dclass === "battery") {
+  if (dclass === SENSOR_DEVICE_CLASS_BATTERY) {
     return stateObj ? batteryIcon(stateObj) : "hass:battery";
   }
 
