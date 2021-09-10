014ddf1e513875ca13712d4322fb41ec820fdf0e
Ruslan Sayfutdinov <ruslan@sayfutdinov.com>
2021-09-10 14:20:48 +0100

    Use concat

--- src/data/sensor.ts
+++ src/data/sensor.ts
@@ -3,4 +3,4 @@ import { UNAVAILABLE_STATES } from "./entity";
 export const SENSOR_DEVICE_CLASS_BATTERY = "battery";
 export const SENSOR_DEVICE_CLASS_TIMESTAMP = "timestamp";
 export const NOT_SET = "not_set";
-export const VALID_TIMESTAMP_STATES = UNAVAILABLE_STATES + [NOT_SET];
+export const VALID_TIMESTAMP_STATES = UNAVAILABLE_STATES.concat([NOT_SET]);
