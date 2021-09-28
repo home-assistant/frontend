abc4816888c49df0512cf262b7f74819799eee36
Bram Kragten <mail@bramkragten.nl>
2021-09-28 02:56:56 +0200

    Only show entities in energy with `energy` device class (#10076)

--- src/panels/config/energy/dialogs/dialog-energy-battery-settings.ts
+++ src/panels/config/energy/dialogs/dialog-energy-battery-settings.ts
@@ -15,6 +15,7 @@ import "@material/mwc-button/mwc-button";
 import "../../../../components/entity/ha-statistic-picker";
 
 const energyUnits = ["kWh"];
+const energyDeviceClasses = ["energy"];
 
 @customElement("dialog-energy-battery-settings")
 export class DialogEnergyBatterySettings
@@ -65,6 +66,7 @@ export class DialogEnergyBatterySettings
         <ha-statistic-picker
           .hass=${this.hass}
           .includeUnitOfMeasurement=${energyUnits}
+          .includeDeviceClasses=${energyDeviceClasses}
           .value=${this._source.stat_energy_to}
           .label=${`Energy going in to the battery (kWh)`}
           entities-only
@@ -74,6 +76,7 @@ export class DialogEnergyBatterySettings
         <ha-statistic-picker
           .hass=${this.hass}
           .includeUnitOfMeasurement=${energyUnits}
+          .includeDeviceClasses=${energyDeviceClasses}
           .value=${this._source.stat_energy_from}
           .label=${`Energy coming out of the battery (kWh)`}
           entities-only
