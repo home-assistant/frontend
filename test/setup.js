95ba5a47df2995120c42cc96f50ab9f6a4a94024
Bram Kragten <mail@bramkragten.nl>
2021-09-29 21:38:36 +0200

    Create required files for build

--- test/setup.js
+++ test/setup.js
@@ -1,1 +1,11 @@
+const fs = require("fs");
+const path = require("path");
+
 process.env.TZ = "Etc/UTC";
+
+const MDI_OUTPUT_DIR = path.resolve(__dirname, "../build/mdi");
+
+if (!fs.existsSync(MDI_OUTPUT_DIR)) {
+  fs.mkdirSync(MDI_OUTPUT_DIR, { recursive: true });
+  fs.writeFileSync(path.resolve(MDI_OUTPUT_DIR, "iconMetadata.json"), "{}");
+}
