0a9461e6eefd8b9be3b037689ea77b66783f0c19
Paulus Schoutsen <balloob@gmail.com>
2021-09-30 22:26:49 -0700

    Add input styles

--- src/resources/styles.ts
+++ src/resources/styles.ts
@@ -13,6 +13,21 @@ export const darkStyles = {
   "switch-unchecked-track-color": "#9b9b9b",
   "divider-color": "rgba(225, 225, 225, .12)",
   "mdc-ripple-color": "#AAAAAA",
+
+  // TODO: adjust these colors for black.
+  "input-idle-line-color": "rgba(0, 0, 0, 0.42)",
+  "input-hover-line-color": "rgba(0, 0, 0, 0.87)",
+  "input-disabled-line-color": "rgba(0, 0, 0, 0.06)",
+  "input-outlined-idle-border-color": "rgba(0, 0, 0, 0.38)",
+  "input-outlined-hover-border-color": "rgba(0, 0, 0, 0.87)",
+  "input-outlined-disabled-border-color": "rgba(0, 0, 0, 0.06)",
+  "input-fill-color": "rgb(245, 245, 245)",
+  "input-disabled-fill-color": "rgb(250, 250, 250)",
+  "input-ink-color": "rgba(0, 0, 0, 0.87)",
+  "input-label-ink-color": "rgba(0, 0, 0, 0.6)",
+  "input-disabled-ink-color": "rgba(0, 0, 0, 0.37)",
+  "input-dropdown-icon-color": "rgba(0, 0, 0, 0.54)",
+
   "codemirror-keyword": "#C792EA",
   "codemirror-operator": "#89DDFF",
   "codemirror-variable": "#f07178",
@@ -80,6 +95,7 @@ export const derivedStyles = {
   "mdc-theme-text-primary-on-background": "var(--primary-text-color)",
   "mdc-theme-text-secondary-on-background": "var(--secondary-text-color)",
   "mdc-theme-text-icon-on-background": "var(--secondary-text-color)",
+  "mdc-theme-error": "var(--error-color)",
   "app-header-text-color": "var(--text-primary-color)",
   "app-header-background-color": "var(--primary-color)",
   "mdc-checkbox-unchecked-color": "rgba(var(--rgb-primary-text-color), 0.54)",
@@ -90,6 +106,38 @@ export const derivedStyles = {
   "mdc-button-disabled-ink-color": "var(--disabled-text-color)",
   "mdc-button-outline-color": "var(--divider-color)",
   "mdc-dialog-scroll-divider-color": "var(--divider-color)",
+
+  "mdc-text-field-idle-line-color": "var(--input-idle-line-color)",
+  "mdc-text-field-hover-line-color": "var(--input-hover-line-color)",
+  "mdc-text-field-disabled-line-color": "var(--input-disabled-line-color)",
+  "mdc-text-field-outlined-idle-border-color":
+    "var(--input-outlined-idle-border-color)",
+  "mdc-text-field-outlined-hover-border-color":
+    "var(--input-outlined-hover-border-color)",
+  "mdc-text-field-outlined-disabled-border-color":
+    "var(--input-outlined-disabled-border-color)",
+  "mdc-text-field-fill-color": "var(--input-fill-color)",
+  "mdc-text-field-disabled-fill-color": "var(--input-disabled-fill-color)",
+  "mdc-text-field-ink-color": "var(--input-ink-color)",
+  "mdc-text-field-label-ink-color": "var(--input-label-ink-color)",
+  "mdc-text-field-disabled-ink-color": "var(--input-disabled-ink-color)",
+
+  "mdc-select-idle-line-color": "var(--input-idle-line-color)",
+  "mdc-select-hover-line-color": "var(--input-hover-line-color)",
+  "mdc-select-outlined-idle-border-color":
+    "var(--input-outlined-idle-border-color)",
+  "mdc-select-outlined-hover-border-color":
+    "var(--input-outlined-hover-border-color)",
+  "mdc-select-outlined-disabled-border-color":
+    "var(--input-outlined-disabled-border-color)",
+  "mdc-select-fill-color": "var(--input-fill-color)",
+  "mdc-select-disabled-fill-color": "var(--input-disabled-fill-color)",
+  "mdc-select-ink-color": "var(--input-ink-color)",
+  "mdc-select-label-ink-color": "var(--input-label-ink-color)",
+  "mdc-select-disabled-ink-color": "var(--input-disabled-ink-color)",
+  "mdc-select-dropdown-icon-color": "var(--input-dropdown-icon-color)",
+  "mdc-select-disabled-dropdown-icon-color": "var(--input-disabled-ink-color)",
+
   "chip-background-color": "rgba(var(--rgb-primary-text-color), 0.15)",
   // Vaadin
   "material-body-text-color": "var(--primary-text-color)",
