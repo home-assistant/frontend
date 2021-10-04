b01c30050b9dbe2b6ae96e95a72920baa59348ee
Zack Arnett <arnett.zackary@gmail.com>
2021-10-04 12:33:53 -0500

    Add More to Gallery

--- gallery/src/demos/demo-hui-area-card.ts
+++ gallery/src/demos/demo-hui-area-card.ts
@@ -8,7 +8,7 @@ const ENTITIES = [
   getEntity("light", "bed_light", "on", {
     friendly_name: "Bed Light",
   }),
-  getEntity("switch", "ac", "on", {
+  getEntity("switch", "bed_ac", "on", {
     friendly_name: "Ecobee",
   }),
   getEntity("sensor", "bed_temp", "72", {
@@ -16,16 +16,57 @@ const ENTITIES = [
     device_class: "temperature",
     unit_of_measurement: "°F",
   }),
+  getEntity("light", "living_room_light", "off", {
+    friendly_name: "Living Room Light",
+  }),
+  getEntity("fan", "living_room", "on", {
+    friendly_name: "Living Room Fan",
+  }),
+  getEntity("sensor", "office_humidity", "73", {
+    friendly_name: "Office Humidity",
+    device_class: "humidity",
+    unit_of_measurement: "%",
+  }),
+  getEntity("light", "office", "on", {
+    friendly_name: "Office Light",
+  }),
+  getEntity("fan", "second_office", "on", {
+    friendly_name: "Second Office Fan",
+  }),
 ];
 
 // TODO: Update image here
 const CONFIGS = [
   {
-    heading: "Basic example",
+    heading: "Bedroom",
     config: `
 - type: area
   area: bedroom
   image: "https://blogmickey.com/wp-content/uploads/2019/01/Riviera-Resort-One-or-Two-Bedroom-Villa-Bedroom-16x9-1024x576.jpg"
+    `,
+  },
+  {
+    heading: "Living Room",
+    config: `
+- type: area
+  area: living_room
+  image: "https://thirtynineframes.com/wp-content/uploads/2019/01/Youngs_Eames-16x9x1920.jpg"
+    `,
+  },
+  {
+    heading: "Office",
+    config: `
+- type: area
+  area: office
+  image: "https://www.boardandvellum.com/wp-content/uploads/2019/09/16x9-private_offices_vs_open_office_concepts-1242x699.jpg"
+    `,
+  },
+  {
+    heading: "Second Office",
+    config: `
+- type: area
+  area: second_office
+  image: "https://images.wallpaperscraft.com/image/single/office_work_interior_walls_80538_1600x900.jpg"
     `,
   },
 ];
@@ -49,6 +90,18 @@ class DemoArea extends LitElement {
         name: "Bedroom",
         area_id: "bedroom",
       },
+      {
+        name: "Living Room",
+        area_id: "living_room",
+      },
+      {
+        name: "Office",
+        area_id: "office",
+      },
+      {
+        name: "Second Office",
+        area_id: "second_office",
+      },
     ]);
     hass.mockWS("config/device_registry/list", () => []);
     hass.mockWS("config/entity_registry/list", () => [
@@ -58,11 +111,31 @@ class DemoArea extends LitElement {
       },
       {
         area_id: "bedroom",
-        entity_id: "switch.ac",
+        entity_id: "switch.bed_ac",
       },
       {
         area_id: "bedroom",
         entity_id: "sensor.bed_temp",
+      },
+      {
+        area_id: "living_room",
+        entity_id: "light.living_room_light",
+      },
+      {
+        area_id: "living_room",
+        entity_id: "fan.living_room",
+      },
+      {
+        area_id: "office",
+        entity_id: "light.office",
+      },
+      {
+        area_id: "office",
+        entity_id: "sensor.office_humidity",
+      },
+      {
+        area_id: "second_office",
+        entity_id: "fan.second_office",
       },
     ]);
   }
