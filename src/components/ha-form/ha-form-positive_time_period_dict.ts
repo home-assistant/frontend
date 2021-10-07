a839494a1e3f54ef50a5ae705f5825fd7489462a
Paulus Schoutsen <balloob@gmail.com>
2021-10-07 12:21:35 -0700

    Use MWC components for ha-form (#10120)

--- src/components/ha-form/ha-form-positive_time_period_dict.ts
+++ src/components/ha-form/ha-form-positive_time_period_dict.ts
@@ -1,7 +1,7 @@
 import { html, LitElement, TemplateResult } from "lit";
 import { customElement, property, query } from "lit/decorators";
 import "../ha-duration-input";
-import { HaFormElement, HaFormTimeData, HaFormTimeSchema } from "./ha-form";
+import { HaFormElement, HaFormTimeData, HaFormTimeSchema } from "./types";
 
 @customElement("ha-form-positive_time_period_dict")
 export class HaFormTimePeriod extends LitElement implements HaFormElement {
@@ -10,8 +10,6 @@ export class HaFormTimePeriod extends LitElement implements HaFormElement {
   @property() public data!: HaFormTimeData;
 
   @property() public label!: string;
-
-  @property() public suffix!: string;
 
   @query("ha-time-input", true) private _input?: HTMLElement;
 
