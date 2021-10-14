48948d5854bf43cfabeae4a126f65ad8a4c0a01b
Paulus Schoutsen <balloob@gmail.com>
2021-10-14 09:56:51 -0700

    Initial support for entity category (#10266)

--- gallery/src/demos/demo-integration-card.ts
+++ gallery/src/demos/demo-integration-card.ts
@@ -187,6 +187,7 @@ const createEntityRegistryEntries = (
     device_id: "mock-device-id",
     area_id: null,
     disabled_by: null,
+    entity_category: null,
     entity_id: "binary_sensor.updater",
     name: null,
     icon: null,
