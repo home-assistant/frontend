dcb5224f64b7fb10350a10316b08793a4a3fe59a
piitaya <paul.bottein@gmail.com>
2021-10-25 22:10:32 +0200

    improve proxy for backward compatibility

--- src/data/custom_icons.ts
+++ src/data/custom_icons.ts
@@ -24,19 +24,12 @@ if (!("customIcons" in customIconsWindow)) {
 
 // Proxy for backward compatibility with icon sets
 export const customIcons = new Proxy(customIconsWindow.customIcons!, {
-  get: function (obj, prop: string) {
-    return (
-      obj[prop] ??
-      (customIconsets[prop]
-        ? {
-            getIcon: customIconsets[prop],
-          }
-        : undefined)
-    );
-  },
-  has: function (obj, prop: string) {
-    return Boolean(obj[prop] || customIconsets[prop]);
-  },
+  get: (obj, prop: string) =>
+    obj[prop] ??
+    (customIconsets[prop]
+      ? {
+          getIcon: customIconsets[prop],
+        }
+      : undefined),
+  has: (obj, prop: string) => prop in obj || prop in customIconsets,
 });
-
-(window as any).customIconsProxy = customIcons;
