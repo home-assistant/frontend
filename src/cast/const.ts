d5d741ef7a1152ef2c996fb5ffc79193b0588e60
Bram Kragten <mail@bramkragten.nl>
2021-11-17 19:32:15 +0100

    Update Lovelace Cast app ID (#10592)

--- src/cast/const.ts
+++ src/cast/const.ts
@@ -3,5 +3,5 @@ import { CAST_DEV_APP_ID } from "./dev_const";
 // Guard dev mode with `__dev__` so it can only ever be enabled in dev mode.
 export const CAST_DEV = __DEV__ && true;
 
-export const CAST_APP_ID = CAST_DEV ? CAST_DEV_APP_ID : "B12CE3CA";
+export const CAST_APP_ID = CAST_DEV ? CAST_DEV_APP_ID : "A078F6B0";
 export const CAST_NS = "urn:x-cast:com.nabucasa.hast";
