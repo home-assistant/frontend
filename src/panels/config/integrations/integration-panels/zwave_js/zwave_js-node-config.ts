dc1592a83643239b27b796120b24c9f2125500d5
Raman Gupta <7243222+raman325@users.noreply.github.com>
2021-12-06 12:51:52 -0500

    Update zwave_js-node-config.ts

--- src/panels/config/integrations/integration-panels/zwave_js/zwave_js-node-config.ts
+++ src/panels/config/integrations/integration-panels/zwave_js/zwave_js-node-config.ts
@@ -327,10 +327,10 @@ class ZWaveJSNodeConfig extends SubscribeMixin(LitElement) {
     if (!("states" in item.metadata)) {
       return false;
     }
-    if (!(0 in item.metadata.states) || !(1 in item.metadata.states)) {
+    if (Object.keys(item.metadata.states).length !== 2) {
       return false;
     }
-    if (Object.keys(item.metadata.states).length !== 2) {
+    if (!(0 in item.metadata.states) || !(1 in item.metadata.states)) {
       return false;
     }
     if (
