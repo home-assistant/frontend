614aaa88f6baf123546b4ff295832e7c2ead3785
Bram Kragten <mail@bramkragten.nl>
2021-12-11 17:05:45 +0100

    Fix formfield label touch

--- src/components/ha-formfield.ts
+++ src/components/ha-formfield.ts
@@ -1,10 +1,28 @@
 import { Formfield } from "@material/mwc-formfield";
 import { css, CSSResultGroup } from "lit";
 import { customElement } from "lit/decorators";
+import { fireEvent } from "../common/dom/fire_event";
 
 @customElement("ha-formfield")
 // @ts-expect-error
 export class HaFormfield extends Formfield {
+  protected _labelClick() {
+    const input = this.input;
+    if (input) {
+      input.focus();
+      switch (input.tagName) {
+        case "HA-CHECKBOX":
+        case "HA-RADIO":
+          (input as any).checked = !(input as any).checked;
+          fireEvent(input, "change");
+          break;
+        default:
+          input.click();
+          break;
+      }
+    }
+  }
+
   protected static get styles(): CSSResultGroup {
     return [
       Formfield.styles,
