5eb52e3c8b65a541976db5de3719ebaf72b70a8d
Philip Allgaier <mail@spacegaier.de>
2021-12-12 13:25:05 +0100

    Add tooltips and aria-labels to climate modes (#10875)

--- src/panels/lovelace/cards/hui-thermostat-card.ts
+++ src/panels/lovelace/cards/hui-thermostat-card.ts
@@ -19,7 +19,7 @@ import {
   svg,
   TemplateResult,
 } from "lit";
-import { customElement, property, state, query } from "lit/decorators";
+import { customElement, property, query, state } from "lit/decorators";
 import { classMap } from "lit/directives/class-map";
 import { UNIT_F } from "../../../common/const";
 import { applyThemesOnElement } from "../../../common/dom/apply_themes_on_element";
@@ -427,6 +427,7 @@ export class HuiThermostatCard extends LitElement implements LovelaceCard {
         @click=${this._handleAction}
         tabindex="0"
         .path=${modeIcons[mode]}
+        .label=${this.hass!.localize(`component.climate.state._.${mode}`)}
       >
       </ha-icon-button>
     `;
