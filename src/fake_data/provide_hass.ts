453dca394965350d0ab1635a69a27890081565c6
Bram Kragten <mail@bramkragten.nl>
2021-12-20 13:42:52 +0100

    Fix demo loadFragmentTranslation

--- src/fake_data/provide_hass.ts
+++ src/fake_data/provide_hass.ts
@@ -271,7 +271,10 @@ export const provideHass = (
     updateStates,
     updateTranslations,
     addTranslations,
-    loadFragmentTranslation: async (_fragment: string) => hass().localize,
+    loadFragmentTranslation: async (fragment: string) => {
+      await updateTranslations(fragment);
+      return hass().localize;
+    },
     addEntities,
     mockWS(type, callback) {
       wsCommands[type] = callback;
