ac02d85cedd7c0e5b855be5ca4be3c188abf8361
Paulus Schoutsen <balloob@gmail.com>
2022-01-10 15:30:53 -0800

    Bumped version to 20211229.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="home-assistant-frontend",
-    version="20211229.0",
+    version="20211229.1",
     description="The Home Assistant frontend",
     url="https://github.com/home-assistant/frontend",
     author="The Home Assistant Authors",
