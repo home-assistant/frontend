7a929794bbf1e27f6d2cee040bd51916482ae5b3
Franck Nijhof <git@frenck.dev>
2022-01-12 13:11:19 +0100

    Add overflow menu to discovered integration item

--- src/panels/config/integrations/ha-config-flow-card.ts
+++ src/panels/config/integrations/ha-config-flow-card.ts
@@ -1,6 +1,7 @@
-import { css, html, LitElement, TemplateResult } from "lit";
+import { css, CSSResultGroup, html, LitElement, TemplateResult } from "lit";
 import { customElement, property } from "lit/decorators";
 import { classMap } from "lit/directives/class-map";
+import { mdiDotsVertical, mdiOpenInNew } from "@mdi/js";
 import { fireEvent } from "../../../common/dom/fire_event";
 import {
   ATTENTION_SOURCES,
@@ -11,6 +12,7 @@ import {
 import type { IntegrationManifest } from "../../../data/integration";
 import { showConfigFlowDialog } from "../../../dialogs/config-flow/show-dialog-config-flow";
 import { showConfirmationDialog } from "../../../dialogs/generic/show-dialog-box";
+import { haStyle } from "../../../resources/styles";
 import type { HomeAssistant } from "../../../types";
 import type { DataEntryFlowProgressExtended } from "./ha-config-integrations";
 import "./ha-integration-action-card";
@@ -50,17 +52,62 @@ export class HaConfigFlowCard extends LitElement {
             }`
           )}
         ></mwc-button>
-        ${DISCOVERY_SOURCES.includes(this.flow.context.source) &&
-        this.flow.context.unique_id
-          ? html`
-              <mwc-button
-                @click=${this._ignoreFlow}
-                .label=${this.hass.localize(
-                  "ui.panel.config.integrations.ignore.ignore"
+        <ha-button-menu corner="BOTTOM_START">
+          <ha-icon-button
+            slot="trigger"
+            .label=${this.hass.localize("ui.common.menu")}
+            .path=${mdiDotsVertical}
+          ></ha-icon-button>
+          ${this.flow.context.configuration_url
+            ? html` <a
+                href=${this.flow.context.configuration_url.replace(
+                  /^homeassistant:\/\//,
+                  ""
                 )}
-              ></mwc-button>
-            `
-          : ""}
+                rel="noreferrer"
+                target=${this.flow.context.configuration_url.startsWith(
+                  "homeassistant://"
+                )
+                  ? "_self"
+                  : "_blank"}
+              >
+                <mwc-list-item hasMeta>
+                  ${this.hass.localize(
+                    "ui.panel.config.integrations.config_entry.open_configuration_url"
+                  )}<ha-svg-icon
+                    slot="meta"
+                    .path=${mdiOpenInNew}
+                  ></ha-svg-icon>
+                </mwc-list-item>
+              </a>`
+            : ""}
+          ${this.manifest
+            ? html` <a
+                href=${this.manifest.documentation}
+                rel="noreferrer"
+                target="_blank"
+              >
+                <mwc-list-item hasMeta>
+                  ${this.hass.localize(
+                    "ui.panel.config.integrations.config_entry.documentation"
+                  )}<ha-svg-icon
+                    slot="meta"
+                    .path=${mdiOpenInNew}
+                  ></ha-svg-icon>
+                </mwc-list-item>
+              </a>`
+            : ""}
+          ${DISCOVERY_SOURCES.includes(this.flow.context.source) &&
+          this.flow.context.unique_id
+            ? html`
+                <mwc-list-item @click=${this._ignoreFlow}>
+                  ${this.hass.localize(
+                    "ui.panel.config.integrations.ignore.ignore"
+                  )}
+                </mwc-list-item>
+              `
+            : ""}
+        </ha-button-menu>
       </ha-integration-action-card>
     `;
   }
