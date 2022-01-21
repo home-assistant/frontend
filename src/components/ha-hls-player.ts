649e1807fc5521bfa0b07d0b345258e95ef85c2f
Allen Porter <allen.porter@gmail.com>
2022-01-21 07:21:35 -0800

    Update src/components/ha-hls-player.ts
    Co-authored-by: Philip Allgaier <philip.allgaier@gmx.de>

--- src/components/ha-hls-player.ts
+++ src/components/ha-hls-player.ts
@@ -196,7 +196,7 @@ class HaHLSPlayer extends LitElement {
     // once should be OK.
     // The stream count may be incremented multiple times before this function is called to check
     // the count e.g. when loading a page with many streams on it. The race can work in our favor
-    // so we know have a better idea on if we'll use too many browser connections later.
+    // so we now have a better idea on if we'll use too many browser connections later.
     if (HaHLSPlayer.streamCount <= 2) {
       return true;
     }
