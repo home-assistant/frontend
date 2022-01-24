7402b797ae00a59b368cc14a1694dc783da6a9ab
Philip Allgaier <mail@spacegaier.de>
2022-01-24 17:10:44 +0100

    Ensure forecast temperatures are properly positioned + show em-dash when n/a (#9066)
    Co-authored-by: Zack Barett <zackbarett@hey.com>

--- src/panels/lovelace/cards/hui-weather-forecast-card.ts
+++ src/panels/lovelace/cards/hui-weather-forecast-card.ts
@@ -24,6 +24,7 @@ import {
   getWeatherStateIcon,
   getWeatherUnit,
   getWind,
+  isForecastHourly,
   weatherAttrIcons,
   WeatherEntity,
   weatherSVGStyles,
@@ -177,23 +178,15 @@ class HuiWeatherForecastCard extends LitElement implements LovelaceCard {
         : undefined;
     const weather = !forecast || this._config?.show_current !== false;
 
-    let hourly: boolean | undefined;
+    const hourly = isForecastHourly(forecast);
     let dayNight: boolean | undefined;
 
-    if (forecast?.length && forecast?.length > 2) {
-      const date1 = new Date(forecast[1].datetime);
-      const date2 = new Date(forecast[2].datetime);
-      const timeDiff = date2.getTime() - date1.getTime();
+    if (hourly) {
+      const dateFirst = new Date(forecast![0].datetime);
+      const datelast = new Date(forecast![forecast!.length - 1].datetime);
+      const dayDiff = datelast.getTime() - dateFirst.getTime();
 
-      hourly = timeDiff < DAY_IN_MILLISECONDS;
-
-      if (hourly) {
-        const dateFirst = new Date(forecast[0].datetime);
-        const datelast = new Date(forecast[forecast.length - 1].datetime);
-        const dayDiff = datelast.getTime() - dateFirst.getTime();
-
-        dayNight = dayDiff > DAY_IN_MILLISECONDS;
-      }
+      dayNight = dayDiff > DAY_IN_MILLISECONDS;
     }
 
     const weatherStateIcon = getWeatherStateIcon(stateObj.state, this);
@@ -288,69 +281,76 @@ class HuiWeatherForecastCard extends LitElement implements LovelaceCard {
         ${forecast
           ? html`
               <div class="forecast">
-                ${forecast.map(
-                  (item) => html`
-                    <div>
-                      <div>
-                        ${dayNight
-                          ? html`
-                              ${new Date(item.datetime).toLocaleDateString(
-                                this.hass!.language,
-                                { weekday: "short" }
-                              )}
-                              <div class="daynight">
-                                ${item.daytime === undefined || item.daytime
-                                  ? this.hass!.localize("ui.card.weather.day")
-                                  : this.hass!.localize(
-                                      "ui.card.weather.night"
-                                    )}<br />
-                              </div>
-                            `
-                          : hourly
-                          ? html`
-                              ${formatTime(
-                                new Date(item.datetime),
-                                this.hass!.locale
-                              )}
-                            `
-                          : html`
-                              ${new Date(item.datetime).toLocaleDateString(
-                                this.hass!.language,
-                                { weekday: "short" }
-                              )}
-                            `}
-                      </div>
-                      ${item.condition !== undefined && item.condition !== null
-                        ? html`
-                            <div class="forecast-image-icon">
-                              ${getWeatherStateIcon(
-                                item.condition,
-                                this,
-                                !(item.daytime || item.daytime === undefined)
-                              )}
-                            </div>
-                          `
-                        : ""}
-                      ${item.temperature !== undefined &&
-                      item.temperature !== null
-                        ? html`
-                            <div class="temp">
-                              ${formatNumber(
-                                item.temperature,
-                                this.hass!.locale
-                              )}°
-                            </div>
-                          `
-                        : ""}
-                      ${item.templow !== undefined && item.templow !== null
-                        ? html`
-                            <div class="templow">
-                              ${formatNumber(item.templow, this.hass!.locale)}°
-                            </div>
-                          `
-                        : ""}
-                    </div>
-                  `
+                ${forecast.map((item) =>
+                  this._showValue(item.templow) ||
+                  this._showValue(item.temperature)
+                    ? html`
+                        <div>
+                          <div>
+                            ${dayNight
+                              ? html`
+                                  ${new Date(item.datetime).toLocaleDateString(
+                                    this.hass!.language,
+                                    { weekday: "short" }
+                                  )}
+                                  <div class="daynight">
+                                    ${item.daytime === undefined || item.daytime
+                                      ? this.hass!.localize(
+                                          "ui.card.weather.day"
+                                        )
+                                      : this.hass!.localize(
+                                          "ui.card.weather.night"
+                                        )}<br />
+                                  </div>
+                                `
+                              : hourly
+                              ? html`
+                                  ${formatTime(
+                                    new Date(item.datetime),
+                                    this.hass!.locale
+                                  )}
+                                `
+                              : html`
+                                  ${new Date(item.datetime).toLocaleDateString(
+                                    this.hass!.language,
+                                    { weekday: "short" }
+                                  )}
+                                `}
+                          </div>
+                          ${this._showValue(item.condition)
+                            ? html`
+                                <div class="forecast-image-icon">
+                                  ${getWeatherStateIcon(
+                                    item.condition!,
+                                    this,
+                                    !(
+                                      item.daytime || item.daytime === undefined
+                                    )
+                                  )}
+                                </div>
+                              `
+                            : ""}
+                          <div class="temp">
+                            ${this._showValue(item.temperature)
+                              ? html`${formatNumber(
+                                  item.temperature,
+                                  this.hass!.locale
+                                )}°`
+                              : "—"}
+                          </div>
+                          <div class="templow">
+                            ${this._showValue(item.templow)
+                              ? html`${formatNumber(
+                                  item.templow!,
+                                  this.hass!.locale
+                                )}°`
+                              : hourly
+                              ? ""
+                              : "—"}
+                          </div>
+                        </div>
+                      `
+                    : ""
                 )}
               </div>
             `
@@ -400,6 +400,10 @@ class HuiWeatherForecastCard extends LitElement implements LovelaceCard {
       this.removeAttribute("verynarrow");
     }
     this._veryVeryNarrow = card.offsetWidth < 245;
+  }
+
+  private _showValue(item?: any): boolean {
+    return typeof item !== "undefined" && item !== null;
   }
 
   static get styles(): CSSResultGroup {
