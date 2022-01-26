b6847f927305c50c8d4eaab7e600a27c6c058c87
Philip Allgaier <mail@spacegaier.de>
2022-01-26 15:02:51 +0100

    Typing

--- src/panels/config/entities/ha-config-entities.ts
+++ src/panels/config/entities/ha-config-entities.ts
@@ -165,11 +165,11 @@ export class HaConfigEntities extends SubscribeMixin(LitElement) {
   );
 
   private _columns = memoize(
-    (narrow, _language, showDisabled): DataTableColumnContainer => ({
+    (narrow, _language, showDisabled): DataTableColumnContainer<EntityRow> => ({
       icon: {
         title: "",
         type: "icon",
-        template: (_, entry: any) => html`
+        template: (_, entry: EntityRow) => html`
           <ha-state-icon
             .title=${entry.entity?.state}
             slot="item-icon"
@@ -186,7 +186,7 @@ export class HaConfigEntities extends SubscribeMixin(LitElement) {
         direction: "asc",
         grows: true,
         template: narrow
-          ? (name, entity: any) =>
+          ? (name, entity: EntityRow) =>
               html`
                 ${name}<br />
                 <div class="secondary">
@@ -247,7 +247,7 @@ export class HaConfigEntities extends SubscribeMixin(LitElement) {
         sortable: true,
         filterable: true,
         width: "68px",
-        template: (_status, entity: any) =>
+        template: (_status, entity: EntityRow) =>
           entity.unavailable || entity.disabled_by || entity.readonly
             ? html`
                 <div
