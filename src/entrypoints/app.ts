79a8b813afd8860446c4e4478243e0856f044c39
Steve Repsher <steverep@users.noreply.github.com>
2022-01-26 21:28:58 +0000

    Turn off Polymer setting to cancel synthetic click events for all UI entrypoints

--- src/entrypoints/app.ts
+++ src/entrypoints/app.ts
@@ -1,9 +1,13 @@
-import { setPassiveTouchGestures } from "@polymer/polymer/lib/utils/settings";
+import {
+  setPassiveTouchGestures,
+  setCancelSyntheticClickEvents,
+} from "@polymer/polymer/lib/utils/settings";
 import "../layouts/home-assistant";
 import "../resources/ha-style";
 import "../resources/roboto";
 import "../util/legacy-support";
 
 setPassiveTouchGestures(true);
+setCancelSyntheticClickEvents(false);
 
 (window as any).frontendVersion = __VERSION__;
