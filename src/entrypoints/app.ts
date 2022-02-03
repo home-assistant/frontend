a201f640e1f3126cb5612e01d056474bc62eb7b8
Bram Kragten <mail@bramkragten.nl>
2022-02-03 16:33:24 +0100

    Revert "Mobile click accessibility" (#11526)

--- src/entrypoints/app.ts
+++ src/entrypoints/app.ts
@@ -1,11 +1,7 @@
-import {
-  setPassiveTouchGestures,
-  setCancelSyntheticClickEvents,
-} from "@polymer/polymer/lib/utils/settings";
+import { setPassiveTouchGestures } from "@polymer/polymer/lib/utils/settings";
 import "../layouts/home-assistant";
 import "../resources/ha-style";
 import "../resources/roboto";
 import "../util/legacy-support";
 
 setPassiveTouchGestures(true);
-setCancelSyntheticClickEvents(false);
