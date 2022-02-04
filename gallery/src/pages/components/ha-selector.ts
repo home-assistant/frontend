7af1af3c3f95dbd466a81b766e2bd387d83f0cf7
Bram Kragten <mail@bramkragten.nl>
2022-02-04 13:47:17 +0100

    Convert selectors to MWC

--- gallery/src/pages/components/ha-selector.ts
+++ gallery/src/pages/components/ha-selector.ts
@@ -48,10 +48,15 @@ const SCHEMAS: {
       boolean: { name: "Boolean", selector: { boolean: {} } },
       time: { name: "Time", selector: { time: {} } },
       action: { name: "Action", selector: { action: {} } },
-      text: { name: "Text", selector: { text: { multiline: false } } },
+      text: {
+        name: "Text",
+        selector: { text: { multiline: false } },
+      },
       text_multiline: {
         name: "Text multiline",
-        selector: { text: { multiline: true } },
+        selector: {
+          text: { multiline: true },
+        },
       },
       object: { name: "Object", selector: { object: {} } },
       select: {
