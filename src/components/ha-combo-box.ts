4fc4ef4b54373d37cb590cad8b28f70e2dd4d707
Zack <zackbarett@hey.com>
2022-02-07 10:15:29 -0600

    Fix height issue

--- src/components/ha-combo-box.ts
+++ src/components/ha-combo-box.ts
@@ -205,7 +205,7 @@ export class HaComboBox extends LitElement {
       :host {
         display: block;
         width: 100%;
-        margin-top: 4px;
+        margin-top: 12px;
       }
       vaadin-combo-box-light {
         position: relative;
