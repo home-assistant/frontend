dc0330698564209c88c10417a0caf2195e61e099
Zack <zackbarett@hey.com>
2022-02-10 15:04:21 -0600

    memoize

--- src/panels/config/automation/trigger/types/ha-automation-trigger-sun.ts
+++ src/panels/config/automation/trigger/types/ha-automation-trigger-sun.ts
@@ -1,6 +1,7 @@
 import "@polymer/paper-input/paper-input";
 import { html, LitElement } from "lit";
-import { customElement, property, state } from "lit/decorators";
+import { customElement, property } from "lit/decorators";
+import memoizeOne from "memoize-one";
 import { fireEvent } from "../../../../../common/dom/fire_event";
 import "../../../../../components/ha-radio";
 import "../../../../../components/ha-formfield";
@@ -8,6 +9,7 @@ import type { SunTrigger } from "../../../../../data/automation";
 import type { HomeAssistant } from "../../../../../types";
 import { TriggerElement } from "../ha-automation-trigger-row";
 import { HaFormSchema } from "../../../../../components/ha-form/types";
+import { LocalizeFunc } from "../../../../../common/translations/localize";
 
 @customElement("ha-automation-trigger-sun")
 export class HaSunTrigger extends LitElement implements TriggerElement {
@@ -15,7 +17,28 @@ export class HaSunTrigger extends LitElement implements TriggerElement {
 
   @property({ attribute: false }) public trigger!: SunTrigger;
 
-  @state() private _schema?: HaFormSchema[];
+  private _schema = memoizeOne((localize: LocalizeFunc) => [
+      {
+        name: "event",
+        type: "select",
+        required: true,
+        options: [
+          [
+            "sunrise",
+            localize(
+              "ui.panel.config.automation.editor.triggers.type.sun.sunrise"
+            ),
+          ],
+          [
+            "sunset",
+            localize(
+              "ui.panel.config.automation.editor.triggers.type.sun.sunset"
+            ),
+          ],
+        ],
+      },
+      { name: "offset", selector: { text: {} } },
+    ]);
 
   public static get defaultConfig() {
     return {
@@ -24,39 +47,10 @@ export class HaSunTrigger extends LitElement implements TriggerElement {
     };
   }
 
-  protected firstUpdated(): void {
-    if (!this.hass) {
-      return;
-    }
-
-    this._schema = [
-      {
-        name: "event",
-        type: "select",
-        required: true,
-        options: [
-          [
-            "sunrise",
-            this.hass.localize(
-              "ui.panel.config.automation.editor.triggers.type.sun.sunrise"
-            ),
-          ],
-          [
-            "sunset",
-            this.hass.localize(
-              "ui.panel.config.automation.editor.triggers.type.sun.sunset"
-            ),
-          ],
-        ],
-      },
-      { name: "offset", selector: { text: {} } },
-    ];
-  }
-
   protected render() {
     return html`
       <ha-form
-        .schema=${this._schema}
+        .schema=${this._schema(this.hass.localize)}
         .data=${this.trigger}
         .hass=${this.hass}
         .computeLabel=${this._computeLabelCallback}
