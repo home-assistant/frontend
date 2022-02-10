50a44afb9e926e76ecbee10037c6e92193fa2284
Zack <zackbarett@hey.com>
2022-02-10 13:17:50 -0600

    Conversion of Ha trigger

--- src/panels/config/automation/trigger/types/ha-automation-trigger-homeassistant.ts
+++ src/panels/config/automation/trigger/types/ha-automation-trigger-homeassistant.ts
@@ -1,11 +1,10 @@
+import "../../../../../components/ha-form/ha-form";
 import { css, html, LitElement } from "lit";
-import { customElement, property } from "lit/decorators";
+import { customElement, property, state } from "lit/decorators";
 import { fireEvent } from "../../../../../common/dom/fire_event";
-import type { HaRadio } from "../../../../../components/ha-radio";
 import type { HassTrigger } from "../../../../../data/automation";
 import type { HomeAssistant } from "../../../../../types";
-import "../../../../../components/ha-formfield";
-import "../../../../../components/ha-radio";
+import { HaFormSchema } from "../../../../../components/ha-form/types";
 
 @customElement("ha-automation-trigger-homeassistant")
 export class HaHassTrigger extends LitElement {
@@ -13,55 +12,69 @@ export class HaHassTrigger extends LitElement {
 
   @property({ attribute: false }) public trigger!: HassTrigger;
 
+  @state() private _schema?: HaFormSchema[];
+
   public static get defaultConfig() {
     return {
       event: "start" as HassTrigger["event"],
     };
   }
 
+  protected firstUpdated(): void {
+    if (!this.hass) {
+      return;
+    }
+
+    this._schema = [
+      {
+        name: "event",
+        type: "select",
+        required: true,
+        options: [
+          [
+            "start",
+            this.hass.localize(
+              "ui.panel.config.automation.editor.triggers.type.homeassistant.start"
+            ),
+          ],
+          [
+            "shutdown",
+            this.hass.localize(
+              "ui.panel.config.automation.editor.triggers.type.homeassistant.shutdown"
+            ),
+          ],
+        ],
+      },
+    ];
+  }
+
   protected render() {
-    const { event } = this.trigger;
     return html`
-      <label id="eventlabel">
-        ${this.hass.localize(
-          "ui.panel.config.automation.editor.triggers.type.homeassistant.event"
-        )}
-        <ha-formfield
-          .label=${this.hass.localize(
-            "ui.panel.config.automation.editor.triggers.type.homeassistant.start"
-          )}
-        >
-          <ha-radio
-            name="event"
-            value="start"
-            .checked=${event === "start"}
-            @change=${this._radioGroupPicked}
-          ></ha-radio>
-        </ha-formfield>
-        <ha-formfield
-          .label=${this.hass.localize(
-            "ui.panel.config.automation.editor.triggers.type.homeassistant.shutdown"
-          )}
-        >
-          <ha-radio
-            name="event"
-            value="shutdown"
-            .checked=${event === "shutdown"}
-            @change=${this._radioGroupPicked}
-          ></ha-radio>
-        </ha-formfield>
-      </label>
+      <ha-form
+        .schema=${this._schema}
+        .data=${this.trigger}
+        .hass=${this.hass}
+        .computeLabel=${this._computeLabelCallback}
+        @value-changed=${this._valueChanged}
+      ></ha-form>
     `;
   }
 
-  private _radioGroupPicked(ev) {
+  private _valueChanged(ev: CustomEvent): void {
     ev.stopPropagation();
-    fireEvent(this, "value-changed", {
-      value: {
-        ...this.trigger,
-        event: (ev.target as HaRadio).value,
-      },
-    });
+    const newTrigger = ev.detail.value;
+    Object.keys(newTrigger).forEach((key) =>
+      newTrigger[key] === undefined || newTrigger[key] === ""
+        ? delete newTrigger[key]
+        : {}
+    );
+    fireEvent(this, "value-changed", { value: newTrigger });
+  }
+
+  private _computeLabelCallback(schema: HaFormSchema): string {
+    return this.hass.localize(
+      `ui.panel.config.automation.editor.triggers.type.geo_location.${schema.name}`
+    );
   }
 
   static styles = css`
