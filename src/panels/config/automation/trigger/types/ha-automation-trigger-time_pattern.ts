986f4649554196a502d3b305cc21bf8e7c6d6840
Zack <zackbarett@hey.com>
2022-02-10 13:43:51 -0600

    Update Value changed

--- src/panels/config/automation/trigger/types/ha-automation-trigger-time_pattern.ts
+++ src/panels/config/automation/trigger/types/ha-automation-trigger-time_pattern.ts
@@ -35,11 +35,6 @@ export class HaTimePatternTrigger extends LitElement implements TriggerElement {
   private _valueChanged(ev: CustomEvent): void {
     ev.stopPropagation();
     const newTrigger = ev.detail.value;
-    Object.keys(newTrigger).forEach((key) =>
-      newTrigger[key] === undefined || newTrigger[key] === ""
-        ? delete newTrigger[key]
-        : {}
-    );
     fireEvent(this, "value-changed", { value: newTrigger });
   }
 
