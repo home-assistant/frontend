baac1d8ff08899e18a358b3f29b367f2160fd725
Zack <zackbarett@hey.com>
2022-02-10 13:08:16 -0600

    Geo Location Converstion

--- src/panels/config/automation/trigger/types/ha-automation-trigger-geo_location.ts
+++ src/panels/config/automation/trigger/types/ha-automation-trigger-geo_location.ts
@@ -2,7 +2,6 @@ import "../../../../../components/ha-form/ha-form";
 import { css, html, LitElement } from "lit";
 import { customElement, property, state } from "lit/decorators";
 import { fireEvent } from "../../../../../common/dom/fire_event";
-import "../../../../../components/entity/ha-entity-picker";
 import { HaFormSchema } from "../../../../../components/ha-form/types";
 import type { GeoLocationTrigger } from "../../../../../data/automation";
 import type { HomeAssistant } from "../../../../../types";
@@ -13,7 +12,7 @@ export class HaGeolocationTrigger extends LitElement {
 
   @property({ attribute: false }) public trigger!: GeoLocationTrigger;
 
-  @state() private _schema?: HaFormSchema;
+  @state() private _schema?: HaFormSchema[];
 
   public static get defaultConfig() {
     return {
@@ -57,7 +56,6 @@ export class HaGeolocationTrigger extends LitElement {
     if (!this._schema) {
       return html``;
     }
-    const { source, zone, event } = this.trigger;
 
     return html`
       <ha-form
@@ -67,49 +65,6 @@ export class HaGeolocationTrigger extends LitElement {
         .computeLabel=${this._computeLabelCallback}
         @value-changed=${this._valueChanged}
       ></ha-form>
-      <paper-input
-        .label=${this.hass.localize(
-          "ui.panel.config.automation.editor.triggers.type.geo_location.source"
-        )}
-        name="source"
-        .value=${source}
-        @value-changed=${this._valueChanged}
-      ></paper-input>
-      <ha-entity-picker
-        .label=${this.hass.localize(
-          "ui.panel.config.automation.editor.triggers.type.geo_location.zone"
-        )}
-        .value=${zone}
-        .hass=${this.hass}
-        allow-custom-entity
-      ></ha-entity-picker>
-      <label>
-        ${this.hass.localize(
-          "ui.panel.config.automation.editor.triggers.type.geo_location.event"
-        )}
-        <ha-formfield
-          .label=${this.hass.localize(
-            "ui.panel.config.automation.editor.triggers.type.geo_location.enter"
-          )}
-        >
-          <ha-radio
-            name="event"
-            value="enter"
-            .checked=${event === "enter"}
-          ></ha-radio>
-        </ha-formfield>
-        <ha-formfield
-          .label=${this.hass.localize(
-            "ui.panel.config.automation.editor.triggers.type.geo_location.leave"
-          )}
-        >
-          <ha-radio
-            name="event"
-            value="leave"
-            .checked=${event === "leave"}
-          ></ha-radio>
-        </ha-formfield>
-      </label>
     `;
   }
 
