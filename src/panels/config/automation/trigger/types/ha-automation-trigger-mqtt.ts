1407e4158c8c6546a057506e553be12aa9f5c769
Zack <zackbarett@hey.com>
2022-02-10 11:55:51 -0600

    Convert

--- src/panels/config/automation/trigger/types/ha-automation-trigger-mqtt.ts
+++ src/panels/config/automation/trigger/types/ha-automation-trigger-mqtt.ts
@@ -1,12 +1,15 @@
-import "@polymer/paper-input/paper-input";
 import { html, LitElement } from "lit";
 import { customElement, property } from "lit/decorators";
+import { fireEvent } from "../../../../../common/dom/fire_event";
+import { HaFormSchema } from "../../../../../components/ha-form/types";
 import { MqttTrigger } from "../../../../../data/automation";
 import { HomeAssistant } from "../../../../../types";
-import {
-  handleChangeEvent,
-  TriggerElement,
-} from "../ha-automation-trigger-row";
+import { TriggerElement } from "../ha-automation-trigger-row";
+
+const SCHEMA = [
+  { name: "topic", selector: { text: {} } },
+  { name: "payload", required: false, selector: { text: {} } },
+];
 
 @customElement("ha-automation-trigger-mqtt")
 export class HaMQTTTrigger extends LitElement implements TriggerElement {
@@ -19,29 +22,32 @@ export class HaMQTTTrigger extends LitElement implements TriggerElement {
   }
 
   protected render() {
-    const { topic, payload } = this.trigger;
     return html`
-      <paper-input
-        .label=${this.hass.localize(
-          "ui.panel.config.automation.editor.triggers.type.mqtt.topic"
-        )}
-        name="topic"
-        .value=${topic}
+      <ha-form
+        .schema=${SCHEMA}
+        .data=${this.trigger}
+        .hass=${this.hass}
+        .computeLabel=${this._computeLabelCallback}
         @value-changed=${this._valueChanged}
-      ></paper-input>
-      <paper-input
-        .label=${this.hass.localize(
-          "ui.panel.config.automation.editor.triggers.type.mqtt.payload"
-        )}
-        name="payload"
-        .value=${payload}
-        @value-changed=${this._valueChanged}
-      ></paper-input>
+      ></ha-form>
     `;
   }
 
   private _valueChanged(ev: CustomEvent): void {
-    handleChangeEvent(this, ev);
+    ev.stopPropagation();
+    const newTrigger = ev.detail.value;
+    Object.keys(newTrigger).forEach((key) =>
+      newTrigger[key] === undefined || newTrigger[key] === ""
+        ? delete newTrigger[key]
+        : {}
+    );
+    fireEvent(this, "value-changed", { value: newTrigger });
+  }
+
+  private _computeLabelCallback(schema: HaFormSchema): string {
+    return this.hass.localize(
+      `ui.panel.config.automation.editor.triggers.type.mqtt.${schema.name}`
+    );
   }
 }
 
