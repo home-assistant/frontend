fcedf93ba25ee35e903eb86c5c42d8ce1442208e
Zack Barett <zackbarett@hey.com>
2022-02-10 15:29:36 -0600

    Update src/panels/config/automation/trigger/types/ha-automation-trigger-homeassistant.ts
    Co-authored-by: Paulus Schoutsen <balloob@gmail.com>

--- src/panels/config/automation/trigger/types/ha-automation-trigger-homeassistant.ts
+++ src/panels/config/automation/trigger/types/ha-automation-trigger-homeassistant.ts
@@ -6,7 +6,7 @@ import { fireEvent } from "../../../../../common/dom/fire_event";
 import type { HassTrigger } from "../../../../../data/automation";
 import type { HomeAssistant } from "../../../../../types";
 import { HaFormSchema } from "../../../../../components/ha-form/types";
-import { LocalizeFunc } from "../../../../../common/translations/localize";
+import type { LocalizeFunc } from "../../../../../common/translations/localize";
 
 @customElement("ha-automation-trigger-homeassistant")
 export class HaHassTrigger extends LitElement {
