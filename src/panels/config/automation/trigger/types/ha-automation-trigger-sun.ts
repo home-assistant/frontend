b1e96496ef4771c8299adf155ca96c41d1146f1b
Zack Barett <zackbarett@hey.com>
2022-02-10 16:11:57 -0600

    Convert Sun to Ha Form (#11647)
    Co-authored-by: Paulus Schoutsen <balloob@gmail.com>

--- src/panels/config/automation/trigger/types/ha-automation-trigger-sun.ts
+++ src/panels/config/automation/trigger/types/ha-automation-trigger-sun.ts
@@ -1,22 +1,41 @@
-import "@polymer/paper-input/paper-input";
-import { css, html, LitElement } from "lit";
+import { html, LitElement } from "lit";
 import { customElement, property } from "lit/decorators";
+import memoizeOne from "memoize-one";
 import { fireEvent } from "../../../../../common/dom/fire_event";
-import "../../../../../components/ha-radio";
-import "../../../../../components/ha-formfield";
-import type { HaRadio } from "../../../../../components/ha-radio";
 import type { SunTrigger } from "../../../../../data/automation";
 import type { HomeAssistant } from "../../../../../types";
-import {
-  handleChangeEvent,
-  TriggerElement,
-} from "../ha-automation-trigger-row";
+import type { TriggerElement } from "../ha-automation-trigger-row";
+import type { HaFormSchema } from "../../../../../components/ha-form/types";
+import type { LocalizeFunc } from "../../../../../common/translations/localize";
 
 @customElement("ha-automation-trigger-sun")
 export class HaSunTrigger extends LitElement implements TriggerElement {
   @property({ attribute: false }) public hass!: HomeAssistant;
 
   @property({ attribute: false }) public trigger!: SunTrigger;
+
+  private _schema = memoizeOne((localize: LocalizeFunc) => [
+    {
+      name: "event",
+      type: "select",
+      required: true,
+      options: [
+        [
+          "sunrise",
+          localize(
+            "ui.panel.config.automation.editor.triggers.type.sun.sunrise"
+          ),
+        ],
+        [
+          "sunset",
+          localize(
+            "ui.panel.config.automation.editor.triggers.type.sun.sunset"
+          ),
+        ],
+      ],
+    },
+    { name: "offset", selector: { text: {} } },
+  ]);
 
   public static get defaultConfig() {
     return {
@@ -26,69 +45,27 @@ export class HaSunTrigger extends LitElement implements TriggerElement {
   }
 
   protected render() {
-    const { offset, event } = this.trigger;
     return html`
-      <label>
-        ${this.hass.localize(
-          "ui.panel.config.automation.editor.triggers.type.sun.event"
-        )}
-        <ha-formfield
-          .label=${this.hass.localize(
-            "ui.panel.config.automation.editor.triggers.type.sun.sunrise"
-          )}
-        >
-          <ha-radio
-            name="event"
-            value="sunrise"
-            .checked=${event === "sunrise"}
-            @change=${this._radioGroupPicked}
-          ></ha-radio>
-        </ha-formfield>
-        <ha-formfield
-          .label=${this.hass.localize(
-            "ui.panel.config.automation.editor.triggers.type.sun.sunset"
-          )}
-        >
-          <ha-radio
-            name="event"
-            value="sunset"
-            .checked=${event === "sunset"}
-            @change=${this._radioGroupPicked}
-          ></ha-radio>
-        </ha-formfield>
-      </label>
-
-      <paper-input
-        .label=${this.hass.localize(
-          "ui.panel.config.automation.editor.triggers.type.sun.offset"
-        )}
-        name="offset"
-        .value=${offset}
+      <ha-form
+        .schema=${this._schema(this.hass.localize)}
+        .data=${this.trigger}
+        .hass=${this.hass}
+        .computeLabel=${this._computeLabelCallback}
         @value-changed=${this._valueChanged}
-      ></paper-input>
+      ></ha-form>
     `;
   }
 
   private _valueChanged(ev: CustomEvent): void {
-    handleChangeEvent(this, ev);
-  }
-
-  private _radioGroupPicked(ev) {
     ev.stopPropagation();
-    fireEvent(this, "value-changed", {
-      value: {
-        ...this.trigger,
-        event: (ev.target as HaRadio).value,
-      },
-    });
+    const newTrigger = ev.detail.value;
+    fireEvent(this, "value-changed", { value: newTrigger });
   }
 
-  static styles = css`
-    label {
-      display: flex;
-      align-items: center;
-    }
-  `;
+  private _computeLabelCallback = (schema: HaFormSchema): string =>
+    this.hass.localize(
+      `ui.panel.config.automation.editor.triggers.type.sun.${schema.name}`
+    );
 }
 
 declare global {
