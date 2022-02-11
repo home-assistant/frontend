1a82a9d6407569845b43d2267420d2e86df418c4
Zack <zackbarett@hey.com>
2022-02-10 19:54:03 -0600

    Update tests

--- src/common/entity/can_toggle_state.ts
+++ src/common/entity/can_toggle_state.ts
@@ -1,5 +1,5 @@
-import { HassEntity } from "home-assistant-js-websocket";
-import { HomeAssistant } from "../../types";
+import type { HassEntity } from "home-assistant-js-websocket";
+import type { HomeAssistant } from "../../types";
 import { canToggleDomain } from "./can_toggle_domain";
 import { computeStateDomain } from "./compute_state_domain";
 import { supportsFeature } from "./supports-feature";
@@ -8,12 +8,20 @@ export const canToggleState = (hass: HomeAssistant, stateObj: HassEntity) => {
   const domain = computeStateDomain(stateObj);
 
   if (domain === "group") {
-    for (const entity of stateObj.attributes.entity_id) {
-      const d = computeStateDomain(hass.states[entity]);
-      if (canToggleDomain(hass, d)) {
-        return stateObj.state === "on" || stateObj.state === "off";
-      }
+    if (
+      stateObj.attributes?.entity_id?.some((entity) => {
+        const entityStateObj = hass.states[entity];
+        if (!entityStateObj) {
+          return false;
+        }
+
+        const entityDomain = computeStateDomain(entityStateObj);
+        return canToggleDomain(hass, entityDomain);
+      })
+    ) {
+      return stateObj.state === "on" || stateObj.state === "off";
     }
+
     return false;
   }
 
