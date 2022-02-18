db7be6c15b97a90755a46c699e64accf2a33932f
Erik <erik@montnemery.com>
2022-02-18 13:50:20 +0100

    Remove custom MQTT delete device button

--- src/panels/config/devices/device-detail/integration-elements/mqtt/ha-device-actions-mqtt.ts
+++ src/panels/config/devices/device-detail/integration-elements/mqtt/ha-device-actions-mqtt.ts
@@ -1,7 +1,6 @@
 import { css, CSSResultGroup, html, LitElement, TemplateResult } from "lit";
 import { customElement, property } from "lit/decorators";
 import { DeviceRegistryEntry } from "../../../../../../data/device_registry";
-import { removeMQTTDeviceEntry } from "../../../../../../data/mqtt";
 import { showConfirmationDialog } from "../../../../../../dialogs/generic/show-dialog-box";
 import { haStyle } from "../../../../../../resources/styles";
 import { HomeAssistant } from "../../../../../../types";
@@ -16,22 +15,7 @@ export class HaDeviceActionsMqtt extends LitElement {
   protected render(): TemplateResult {
     return html`
       <mwc-button @click=${this._showDebugInfo}> MQTT Info </mwc-button>
-      <mwc-button class="warning" @click=${this._confirmDeleteEntry}>
-        ${this.hass.localize("ui.panel.config.devices.delete")}
-      </mwc-button>
     `;
-  }
-
-  private async _confirmDeleteEntry(): Promise<void> {
-    const confirmed = await showConfirmationDialog(this, {
-      text: this.hass.localize("ui.panel.config.devices.confirm_delete"),
-    });
-
-    if (!confirmed) {
-      return;
-    }
-
-    await removeMQTTDeviceEntry(this.hass!, this.device.id);
   }
 
   private async _showDebugInfo(): Promise<void> {
