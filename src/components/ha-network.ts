c2adeecc61bd206aa50b63e5fb0edbc5ad7e41a3
Teemu Rytilahti <tpr@iki.fi>
2022-02-22 01:56:17 +0100

    Show all network adapters per default

--- src/components/ha-network.ts
+++ src/components/ha-network.ts
@@ -31,16 +31,6 @@ const format_addresses = (
     i < addresses.length - 1 ? ", " : nothing,
   ])}`;
 
-const format_auto_detected_interfaces = (
-  adapters: Adapter[]
-): Array<TemplateResult | string> =>
-  adapters.map((adapter) =>
-    adapter.auto
-      ? html`${adapter.name}
-        (${format_addresses([...adapter.ipv4, ...adapter.ipv6])})`
-      : ""
-  );
-
 declare global {
   interface HASSDomEvents {
     "network-config-changed": { configured_adapters: string[] };
@@ -52,84 +42,37 @@ export class HaNetwork extends LitElement {
 
   @property({ attribute: false }) public networkConfig?: NetworkConfig;
 
-  @state() private _expanded?: boolean;
-
   protected render(): TemplateResult {
     if (this.networkConfig === undefined) {
       return html``;
     }
     const configured_adapters = this.networkConfig.configured_adapters || [];
     return html`
-      <ha-settings-row>
-        <span slot="prefix">
-          <ha-checkbox
-            id="auto_configure"
-            @change=${this._handleAutoConfigureCheckboxClick}
-            .checked=${!configured_adapters.length}
-            name="auto_configure"
-          >
-          </ha-checkbox>
-        </span>
-        <span slot="heading" data-for="auto_configure"> Auto Configure </span>
-        <span slot="description" data-for="auto_configure">
-          Detected:
-          ${format_auto_detected_interfaces(this.networkConfig.adapters)}
-        </span>
-      </ha-settings-row>
-      ${configured_adapters.length || this._expanded
-        ? this.networkConfig.adapters.map(
-            (adapter) =>
-              html`<ha-settings-row>
-                <span slot="prefix">
-                  <ha-checkbox
-                    id=${adapter.name}
-                    @change=${this._handleAdapterCheckboxClick}
-                    .checked=${configured_adapters.includes(adapter.name)}
-                    .adapter=${adapter.name}
-                    name=${adapter.name}
-                  >
-                  </ha-checkbox>
-                </span>
-                <span slot="heading">
-                  Adapter: ${adapter.name}
-                  ${adapter.default
-                    ? html`<ha-svg-icon .path=${mdiStar}></ha-svg-icon>
-                        (Default)`
-                    : ""}
-                </span>
-                <span slot="description">
-                  ${format_addresses([...adapter.ipv4, ...adapter.ipv6])}
-                </span>
-              </ha-settings-row>`
-          )
-        : ""}
+      ${this.networkConfig.adapters.map(
+        (adapter) =>
+          html`<ha-settings-row>
+            <span slot="prefix">
+              <ha-checkbox
+                id=${adapter.name}
+                @change=${this._handleAdapterCheckboxClick}
+                .checked=${configured_adapters.includes(adapter.name)}
+                .adapter=${adapter.name}
+                name=${adapter.name}
+              >
+              </ha-checkbox>
+            </span>
+            <span slot="heading">
+              Adapter: ${adapter.name}
+              ${adapter.default
+                ? html`<ha-svg-icon .path=${mdiStar}></ha-svg-icon> (Default)`
+                : ""}
+            </span>
+            <span slot="description">
+              ${format_addresses([...adapter.ipv4, ...adapter.ipv6])}
+            </span>
+          </ha-settings-row>`
+      )}
     `;
-  }
-
-  private _handleAutoConfigureCheckboxClick(ev: Event) {
-    const checkbox = ev.currentTarget as HaCheckbox;
-    if (this.networkConfig === undefined) {
-      return;
-    }
-
-    let configured_adapters = [...this.networkConfig.configured_adapters];
-
-    if (checkbox.checked) {
-      this._expanded = false;
-      configured_adapters = [];
-    } else {
-      this._expanded = true;
-      for (const adapter of this.networkConfig.adapters) {
-        if (adapter.default) {
-          configured_adapters = [adapter.name];
-          break;
-        }
-      }
-    }
-
-    fireEvent(this, "network-config-changed", {
-      configured_adapters: configured_adapters,
-    });
   }
 
   private _handleAdapterCheckboxClick(ev: Event) {
