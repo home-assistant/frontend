a9cb2a6802c0e1eab8f5b7290aea2be707224c0a
Zack <zackbarett@hey.com>
2022-02-22 19:02:06 -0600

    First commit (needs multi picker)

--- src/panels/lovelace/editor/config-elements/hui-calendar-card-editor.ts
+++ src/panels/lovelace/editor/config-elements/hui-calendar-card-editor.ts
@@ -1,7 +1,7 @@
-import "@material/mwc-list/mwc-list-item";
-import "@material/mwc-select/mwc-select";
-import { CSSResultGroup, html, LitElement, TemplateResult } from "lit";
+import "../../../../components/ha-form/ha-form";
+import { html, LitElement, TemplateResult } from "lit";
 import { customElement, property, state } from "lit/decorators";
+import memoizeOne from "memoize-one";
 import {
   array,
   assert,
@@ -13,16 +13,12 @@ import {
   union,
 } from "superstruct";
 import { fireEvent } from "../../../../common/dom/fire_event";
-import { stopPropagation } from "../../../../common/dom/stop_propagation";
-import "../../../../components/entity/ha-entities-picker";
+import { LocalizeFunc } from "../../../../common/translations/localize";
+import type { HaFormSchema } from "../../../../components/ha-form/types";
 import type { HomeAssistant } from "../../../../types";
 import type { CalendarCardConfig } from "../../cards/types";
-import "../../components/hui-entity-editor";
-import "../../components/hui-theme-select-editor";
 import type { LovelaceCardEditor } from "../../types";
 import { baseLovelaceCardConfig } from "../structs/base-card-struct";
-import type { EditorTarget, EntitiesEditorEvent } from "../types";
-import { configElementStyle } from "./config-elements-style";
 
 const cardConfigStruct = assign(
   baseLovelaceCardConfig,
@@ -43,141 +39,81 @@ export class HuiCalendarCardEditor
 {
   @property({ attribute: false }) public hass?: HomeAssistant;
 
-  @property({ attribute: false }) private _config?: CalendarCardConfig;
-
-  @state() private _configEntities?: string[];
+  @state() private _config?: CalendarCardConfig;
 
   public setConfig(config: CalendarCardConfig): void {
     assert(config, cardConfigStruct);
     this._config = config;
-    this._configEntities = config.entities;
   }
 
-  get _title(): string {
-    return this._config!.title || "";
-  }
-
-  get _initial_view(): string {
-    return this._config!.initial_view || "dayGridMonth";
-  }
-
-  get _theme(): string {
-    return this._config!.theme || "";
-  }
+  private _schema = memoizeOne((localize: LocalizeFunc) => [
+    {
+      name: "",
+      type: "grid",
+      schema: [
+        { name: "title", required: false, selector: { text: {} } },
+        {
+          name: "initial_view",
+          required: false,
+          selector: {
+            select: {
+              options: views.map((view) => [
+                view,
+                localize(
+                  `ui.panel.lovelace.editor.card.calendar.views.${view}`
+                ),
+              ]),
+            },
+          },
+        },
+      ],
+    },
+    { name: "theme", required: false, selector: { theme: {} } },
+    {
+      name: "entities",
+      selector: {
+        entity: { domain: "calendar" },
+      },
+    },
+  ]);
 
   protected render(): TemplateResult {
     if (!this.hass || !this._config) {
       return html``;
     }
 
+    const schema = this._schema(this.hass.localize);
+
     return html`
-      <div class="card-config">
-        <div class="side-by-side">
-          <paper-input
-            .label="${this.hass.localize(
-              "ui.panel.lovelace.editor.card.generic.title"
-            )} (${this.hass.localize(
-              "ui.panel.lovelace.editor.card.config.optional"
-            )})"
-            .value=${this._title}
-            .configValue=${"title"}
-            @value-changed=${this._valueChanged}
-          ></paper-input>
-          <mwc-select
-            .label=${this.hass.localize(
-              "ui.panel.lovelace.editor.card.calendar.inital_view"
-            )}
-            .value=${this._initial_view}
-            .configValue=${"initial_view"}
-            @selected=${this._viewChanged}
-            @closed=${stopPropagation}
-            naturalMenuWidth
-            fixedMenuPosition
-          >
-            ${views.map(
-              (view) => html`
-                <mwc-list-item .value=${view}
-                  >${this.hass!.localize(
-                    `ui.panel.lovelace.editor.card.calendar.views.${view}`
-                  )}
-                </mwc-list-item>
-              `
-            )}
-          </mwc-select>
-        </div>
-        <hui-theme-select-editor
-          .hass=${this.hass}
-          .value=${this._theme}
-          .configValue=${"theme"}
-          @value-changed=${this._valueChanged}
-        ></hui-theme-select-editor>
-      </div>
-      <h3>
-        ${this.hass.localize(
-          "ui.panel.lovelace.editor.card.calendar.calendar_entities"
-        ) +
-        " (" +
-        this.hass!.localize("ui.panel.lovelace.editor.card.config.required") +
-        ")"}
-      </h3>
-      <ha-entities-picker
-        .hass=${this.hass!}
-        .value=${this._configEntities}
-        .includeDomains=${["calendar"]}
+      <ha-form
+        .hass=${this.hass}
+        .data=${this._config}
+        .schema=${schema}
+        .computeLabel=${this._computeLabelCallback}
         @value-changed=${this._valueChanged}
-      >
-      </ha-entities-picker>
+      ></ha-form>
     `;
   }
 
-  private _valueChanged(ev: EntitiesEditorEvent | CustomEvent): void {
-    if (!this._config || !this.hass) {
-      return;
+  private _valueChanged(ev: CustomEvent): void {
+    const config = ev.detail.value;
+
+    if (!config.entities) {
+      config.entities = [];
     }
 
-    const target = ev.target! as EditorTarget;
-
-    if (this[`_${target.configValue}`] === target.value) {
-      return;
-    }
-
-    if (ev.detail && ev.detail.value && Array.isArray(ev.detail.value)) {
-      this._config = { ...this._config, entities: ev.detail.value };
-    } else if (target.configValue) {
-      if (target.value === "") {
-        this._config = { ...this._config };
-        delete this._config[target.configValue!];
-      } else {
-        this._config = {
-          ...this._config,
-          [target.configValue]: target.value,
-        };
-      }
-    }
-
-    fireEvent(this, "config-changed", { config: this._config });
+    fireEvent(this, "config-changed", { config });
   }
 
-  private _viewChanged(ev): void {
-    if (!this._config || !this.hass) {
-      return;
+  private _computeLabelCallback = (schema: HaFormSchema) => {
+    if (schema.name === "title") {
+      return this.hass!.localize("ui.panel.lovelace.editor.card.generic.title");
     }
 
-    if (ev.target.value === "") {
-      this._config = { ...this._config };
-      delete this._config.initial_view;
-    } else {
-      this._config = {
-        ...this._config,
-        initial_view: ev.target.value,
-      };
-    }
-    fireEvent(this, "config-changed", { config: this._config });
-  }
-
-  static get styles(): CSSResultGroup {
-    return configElementStyle;
-  }
+    return this.hass!.localize(
+      `ui.panel.lovelace.editor.card.calendar.${schema.name}`
+    );
+  };
 }
 
 declare global {
