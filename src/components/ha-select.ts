4c1b0fb256a0650c05f470e5938f5d130093d66e
Bram Kragten <mail@bramkragten.nl>
2022-02-23 19:38:31 +0100

    Update select options when translations change

--- src/components/ha-select.ts
+++ src/components/ha-select.ts
@@ -2,6 +2,7 @@ import { SelectBase } from "@material/mwc-select/mwc-select-base";
 import { styles } from "@material/mwc-select/mwc-select.css";
 import { html, nothing } from "lit";
 import { customElement } from "lit/decorators";
+import { nextRender } from "../common/util/render-status";
 
 @customElement("ha-select")
 export class HaSelect extends SelectBase {
@@ -16,8 +17,22 @@ export class HaSelect extends SelectBase {
   }
 
   static override styles = [styles];
-}
 
+  connectedCallback() {
+    super.connectedCallback();
+    window.addEventListener("translations-updated", this._resourcesUpdated);
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    window.removeEventListener("translations-updated", this._resourcesUpdated);
+  }
+
+  private _resourcesUpdated = async () => {
+    await nextRender();
+    this.layoutOptions();
+  };
+}
 declare global {
   interface HTMLElementTagNameMap {
     "ha-select": HaSelect;
