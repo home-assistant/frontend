a61288fd608034d69268e6c6fa38556487ea90a4
Steve Repsher <steverep@users.noreply.github.com>
2022-03-03 04:43:35 +0000

    Turn off Polymer setting to cancel synthetic click events for supervisor entrypoint

--- hassio/src/entrypoint.ts
+++ hassio/src/entrypoint.ts
@@ -1,8 +1,11 @@
 // Compat needs to be first import
 import "../../src/resources/compatibility";
+import { setCancelSyntheticClickEvents } from "@polymer/polymer/lib/utils/settings";
 import "../../src/resources/roboto";
 import "../../src/resources/safari-14-attachshadow-patch";
 import "./hassio-main";
+
+setCancelSyntheticClickEvents(false);
 
 const styleEl = document.createElement("style");
 styleEl.innerHTML = `
