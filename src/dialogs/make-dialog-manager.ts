47e964581bf254490f439490821ae1355b9b83bd
Steve Repsher <steverep@users.noreply.github.com>
2022-03-09 21:42:32 -0500

    Handle focus return after dialogs close

--- src/dialogs/make-dialog-manager.ts
+++ src/dialogs/make-dialog-manager.ts
@@ -1,6 +1,8 @@
 import { HASSDomEvent, ValidHassDomEvent } from "../common/dom/fire_event";
 import { mainWindow } from "../common/dom/get_main_window";
 import { ProvideHassElement } from "../mixins/provide-hass-lit-mixin";
+import { HaButtonMenu } from "../components/ha-button-menu";
+import { HaDialog } from "../components/ha-dialog";
 
 declare global {
   // for fire event
@@ -40,7 +42,17 @@ export interface DialogState {
   dialogParams?: unknown;
 }
 
-const LOADED = {};
+interface LoadedDialogInfo {
+  element: Promise<HassDialog>;
+  closedFocusTarget: Element | null;
+  closedFocusDialog: HaDialog | null;
+}
+
+interface LoadedDialogsDict {
+  [tag: string]: LoadedDialogInfo;
+}
+
+const LOADED: LoadedDialogsDict = {};
 
 export const showDialog = async (
   element: HTMLElement & ProvideHassElement,
@@ -60,11 +72,46 @@ export const showDialog = async (
       }
       return;
     }
-    LOADED[dialogTag] = dialogImport().then(() => {
-      const dialogEl = document.createElement(dialogTag) as HassDialog;
-      element.provideHass(dialogEl);
-      return dialogEl;
-    });
+    LOADED[dialogTag] = {
+      element: dialogImport().then(() => {
+        const dialogEl = document.createElement(dialogTag) as HassDialog;
+        element.provideHass(dialogEl);
+        return dialogEl;
+      }),
+      closedFocusTarget: null,
+      closedFocusDialog: null,
+    };
+  }
+
+  // Go down all components to find the active element,
+  // but keep the original focus target if dialog is being replaced
+  if (mainWindow.history.state?.replaced) {
+    LOADED[dialogTag].closedFocusTarget =
+      LOADED[mainWindow.history.state.dialog].closedFocusTarget;
+    LOADED[dialogTag].closedFocusDialog =
+      LOADED[mainWindow.history.state.dialog].closedFocusDialog;
+  } else {
+    let focusedDialog: LoadedDialogInfo["closedFocusDialog"] = null;
+    let focusedElement = document.activeElement;
+    while (focusedElement?.shadowRoot?.activeElement) {
+      focusedElement = focusedElement.shadowRoot.activeElement;
+
+      // Detect when focus is slotted into certain components
+      const slotRoot = focusedElement.assignedSlot?.getRootNode();
+      if (slotRoot instanceof ShadowRoot) {
+        const slotHost = slotRoot.host;
+        if (slotHost instanceof HaButtonMenu) {
+          // Use trigger button since popup menu will be hidden again
+          focusedElement = slotHost.querySelector('[slot="trigger"]');
+          break;
+        } else if (slotHost instanceof HaDialog) {
+          // Focus will return inside another dialog
+          focusedDialog = slotHost;
+        }
+      }
+    }
+    LOADED[dialogTag].closedFocusTarget = focusedElement;
+    LOADED[dialogTag].closedFocusDialog = focusedDialog;
   }
 
   if (addHistory) {
@@ -93,25 +140,29 @@ export const showDialog = async (
       );
     }
   }
-  const dialogElement = await LOADED[dialogTag];
+
+  const dialogElement = await LOADED[dialogTag].element;
+  dialogElement.addEventListener("dialog-closed", _handleClosedFocus);
+
   // Append it again so it's the last element in the root,
   // so it's guaranteed to be on top of the other elements
   root.appendChild(dialogElement);
   dialogElement.showDialog(dialogParams);
 };
 
-export const replaceDialog = () => {
+export const replaceDialog = (dialogElement: HassDialog) => {
   mainWindow.history.replaceState(
     { ...mainWindow.history.state, replaced: true },
     ""
   );
+  dialogElement.removeEventListener("dialog-closed", _handleClosedFocus);
 };
 
 export const closeDialog = async (dialogTag: string): Promise<boolean> => {
   if (!(dialogTag in LOADED)) {
     return true;
   }
-  const dialogElement: HassDialog = await LOADED[dialogTag];
+  const dialogElement = await LOADED[dialogTag].element;
   if (dialogElement.closeDialog) {
     return dialogElement.closeDialog() !== false;
   }
@@ -136,4 +187,35 @@ export const makeDialogManager = (
       );
     }
   );
+};
+
+const _handleClosedFocus = async (ev: HASSDomEvent<DialogClosedParams>) => {
+  const focusTarget = LOADED[ev.detail.dialog].closedFocusTarget;
+  const focusDialog = LOADED[ev.detail.dialog].closedFocusDialog;
+
+  // If target is in another dialog, make sure it is fully rendered before trying
+  await focusDialog?.updateComplete;
+  if (focusTarget instanceof HTMLElement) focusTarget.focus();
+
+  // Check if focusing was successful
+  let focusedElement = document.activeElement;
+  let focusSuccess = focusTarget === focusedElement;
+  while (!focusSuccess && focusedElement?.shadowRoot?.activeElement) {
+    focusedElement = focusedElement.shadowRoot.activeElement;
+    focusSuccess = focusTarget === focusedElement;
+  }
+
+  if (!focusSuccess) {
+    // Let dialog handle fallback if it exists
+    focusDialog?.focus();
+
+    if (__DEV__) {
+      // eslint-disable-next-line
+      console.warn(
+        "Tried to focus %o after closing dialog, but active element is %o",
+        focusTarget,
+        focusedElement
+      );
+    }
+  }
 };
