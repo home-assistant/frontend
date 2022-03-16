2c5a09c3dbf428d6bfa762112bea8be02f6a628c
Paulus Schoutsen <balloob@gmail.com>
2022-03-16 14:14:25 -0700

    Revamp URL form (#12060)

---
+++ src/common/string/is_ip_address.ts
@@ -0,0 +1,4 @@
+const regexp =
+  /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+
+export const isIPAddress = (input: string): boolean => regexp.test(input);
