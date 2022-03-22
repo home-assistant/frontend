021f391596db132b9fa2e198b27049a22f34d216
Zack <zackbarett@hey.com>
2022-03-22 16:25:17 -0500

    Fix merge

--- src/components/ha-selector/ha-selector-select.ts
+++ src/components/ha-selector/ha-selector-select.ts
@@ -35,6 +35,40 @@ export class HaSelectSelector extends LitElement {
       typeof option === "object" ? option : { value: option, label: option }
     );
 
+    if (this.selector.select.multiple) {
+      const value =
+        !this.value || this.value === "" ? [] : (this.value as string[]);
+
+      return html`
+        <ha-chip-set>
+          ${value?.map(
+            (item, idx) =>
+              html`
+                <ha-chip hasTrailingIcon>
+                  ${options.find((option) => option.value === item)?.label ||
+                  item}
+                  <ha-svg-icon
+                    slot="trailing-icon"
+                    .path=${mdiClose}
+                    .idx=${idx}
+                    @click=${this._removeItem}
+                  ></ha-svg-icon>
+                </ha-chip>
+              `
+          )}
+        </ha-chip-set>
+        <ha-combo-box
+          allowCustomValue=${ifDefined(this.selector.select.custom_value)}
+          item-value-path="value"
+          item-label-path="label"
+          .hass=${this.hass}
+          .label=${this.label}
+          .filteredItems=${options}
+          @value-changed=${this._comboBoxValueChanged}
+        ></ha-combo-box>
+      `;
+    }
+
     if (this.required && options!.length < 6) {
       return html`
         <div>
@@ -43,7 +77,7 @@ export class HaSelectSelector extends LitElement {
             (item: SelectOption) => html`
               <mwc-formfield .label=${item.label}>
                 <ha-radio
-                  .checked=${value === this.value}
+                  .checked=${item.value === this.value}
                   .value=${item.value}
                   .disabled=${this.disabled}
                   @change=${this._valueChanged}
@@ -55,57 +89,23 @@ export class HaSelectSelector extends LitElement {
       `;
     }
 
-    if (!this.selector.select.multiple) {
-      return html`
-        <ha-select
-          fixedMenuPosition
-          naturalMenuWidth
-          .label=${this.label}
-          .value=${this.value}
-          .helper=${this.helper}
-          .disabled=${this.disabled}
-          @closed=${stopPropagation}
-          @selected=${this._valueChanged}
-        >
-          ${options.map(
-            (item: SelectOption) => html`
-              <mwc-list-item .value=${item.value}>${item.label}</mwc-list-item>
-            `
-          )}
-        </ha-select>
-      `;
-    }
-
-    const value =
-      !this.value || this.value === "" ? [] : (this.value as string[]);
-
     return html`
-      <ha-chip-set>
-        ${value?.map(
-          (item, idx) =>
-            html`
-              <ha-chip hasTrailingIcon>
-                ${options.find((option) => option.value === item)?.label ||
-                item}
-                <ha-svg-icon
-                  slot="trailing-icon"
-                  .path=${mdiClose}
-                  .idx=${idx}
-                  @click=${this._removeItem}
-                ></ha-svg-icon>
-              </ha-chip>
-            `
-        )}
-      </ha-chip-set>
-      <ha-combo-box
-        allowCustomValue=${ifDefined(this.selector.select.custom_value)}
-        item-value-path="value"
-        item-label-path="label"
-        .hass=${this.hass}
+      <ha-select
+        fixedMenuPosition
+        naturalMenuWidth
         .label=${this.label}
-        .filteredItems=${options}
-        @value-changed=${this._comboBoxValueChanged}
-      ></ha-combo-box>
+        .value=${this.value}
+        .helper=${this.helper}
+        .disabled=${this.disabled}
+        @closed=${stopPropagation}
+        @selected=${this._valueChanged}
+      >
+        ${options.map(
+          (item: SelectOption) => html`
+            <mwc-list-item .value=${item.value}>${item.label}</mwc-list-item>
+          `
+        )}
+      </ha-select>
     `;
   }
 
