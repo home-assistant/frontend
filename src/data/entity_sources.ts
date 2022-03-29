a01797472fe7f15ff5de94ccd10902b9591a50ef
Paulus Schoutsen <balloob@gmail.com>
2022-03-29 16:33:41 -0700

    Add caching

--- src/data/entity_sources.ts
+++ src/data/entity_sources.ts
@@ -1,3 +1,4 @@
+import { timeCachePromiseFunc } from "../common/util/time-cache-function-promise";
 import { HomeAssistant } from "../types";
 
 interface EntitySourceConfigEntry {
@@ -13,13 +14,33 @@ interface EntitySourcePlatformConfig {
   custom_component: boolean;
 }
 
-type EntitySources = Record<
+export type EntitySources = Record<
   string,
   EntitySourceConfigEntry | EntitySourcePlatformConfig
 >;
 
-export const fetchEntitySources = (hass: HomeAssistant, entity_id?: string) =>
-  hass.callWS<EntitySources>({
+const fetchEntitySources = (
+  hass: HomeAssistant,
+  entity_id?: string
+): Promise<EntitySources> =>
+  hass.callWS({
     type: "entity/source",
     entity_id,
   });
+
+export const fetchEntitySourcesWithCache = (
+  hass: HomeAssistant,
+  entity_id?: string
+): Promise<EntitySources> =>
+  entity_id
+    ? fetchEntitySources(hass, entity_id)
+    : timeCachePromiseFunc(
+        "_entitySources",
+        // cache for 30 seconds
+        30000,
+        fetchEntitySources,
+        // We base the cache on number of states. If number of states
+        // changes we force a refresh
+        (hass2) => Object.keys(hass2.states).length,
+        hass
+      );
