6613914a8157803af9d42b7eef391171571bfab3
J. Nick Koston <nick@koston.org>
2022-04-25 14:45:20 -1000

    format durations

--- src/common/datetime/seconds_to_duration.ts
+++ src/common/datetime/seconds_to_duration.ts
@@ -1,10 +1,15 @@
 const leftPad = (num: number) => (num < 10 ? `0${num}` : num);
 
 export default function secondsToDuration(d: number) {
+  const days = Math.floor(d / 86400);
+  d -= days * 86400;
   const h = Math.floor(d / 3600);
   const m = Math.floor((d % 3600) / 60);
   const s = Math.floor((d % 3600) % 60);
 
+  if (days > 0) {
+    return `${days} ${h}:${leftPad(m)}:${leftPad(s)}`;
+  }
   if (h > 0) {
     return `${h}:${leftPad(m)}:${leftPad(s)}`;
   }
