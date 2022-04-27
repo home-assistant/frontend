e5824c4794934a9b62d3655e2229a676e6d19178
Paulus Schoutsen <balloob@gmail.com>
2022-04-26 21:58:18 -0700

    Fix my link for config dashboard and profile (#12461)
    * Fix my link for config dashboard and profile
    
    * add server control redirect
    
    Co-authored-by: Zack <zackbarett@hey.com>

--- src/panels/my/ha-panel-my.ts
+++ src/panels/my/ha-panel-my.ts
@@ -35,7 +35,7 @@ export const getMyRedirects = (hasSupervisor: boolean): Redirects => ({
     redirect: "/developer-tools/statistics",
   },
   config: {
-    redirect: "/config",
+    redirect: "/config/dashboard",
   },
   cloud: {
     component: "cloud",
@@ -132,7 +132,7 @@ export const getMyRedirects = (hasSupervisor: boolean): Redirects => ({
     redirect: "/config/core",
   },
   server_controls: {
-    redirect: "/config/server_control",
+    redirect: "/developer-tools/yaml",
   },
   logs: {
     redirect: "/config/logs",
@@ -145,7 +145,7 @@ export const getMyRedirects = (hasSupervisor: boolean): Redirects => ({
     redirect: "/config/dashboard",
   },
   profile: {
-    redirect: "/profile/dashboard",
+    redirect: "/profile",
   },
   logbook: {
     component: "logbook",
