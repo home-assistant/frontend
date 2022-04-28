b4f19de47916d20dd1cf01a2fe9754e897d9aeb9
Zack <zackbarett@hey.com>
2022-04-28 15:17:28 -0500

    some updates

--- src/data/hardware.ts
+++ src/data/hardware.ts
@@ -1,5 +1,5 @@
 export const BOARD_NAMES: Record<string, string> = {
-  "odroid-n2": "Home Assistant Blue / ODROID N2+",
+  "odroid-n2": "Home Assistant Blue / ODROID-N2",
   "odroid-xu3": "ODROID-XU4",
   "odroid-c2": "ODROID-C2",
   "odroid-c4": "ODROID-C4",
@@ -11,9 +11,10 @@ export const BOARD_NAMES: Record<string, string> = {
   rpi4: "Raspberry Pi 4 B",
   rpi4_64: "Raspberry Pi 4 B",
   rpi_4_32b: "Raspberry Pi 4 B",
-  "tinker-s-rk3288": "Tinker S RK3288",
-  "khadas-vim3": "",
-  generic_aarch64: "QEMU",
+  tinker: "ASUS Tinker Board",
+  "khadas-vim3": "Khadas VIM3",
+  "generic-aarch64": "Generic AArch64",
   ova: "Virtual Machine",
-  generic_x86_64: "Intel NUC",
+  "generic-x86-64": "Generic x86-64",
+  "intel-nuc": "Intel NUC",
 };
