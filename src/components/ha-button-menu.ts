60f13eb7ab48e738f65813e091db63dca8890bc2
Yosi Levy <yosilevy@gmail.com>
2022-05-05 11:40:02 +0300

    Switched to first-of-type

--- src/components/ha-button-menu.ts
+++ src/components/ha-button-menu.ts
@@ -61,37 +61,10 @@ export class HaButtonMenu extends LitElement {
     if (computeRTL(this.hass!)) {
       this.updateComplete.then(() => {
         this.querySelectorAll("mwc-list-item").forEach((item) => {
-          // @ts-ignore
-          item!._getUpdateComplete = item.getUpdateComplete;
-
           const style = document.createElement("style");
           style.innerHTML =
-            ".rtl-fix, .rtl-fix2 { margin-left: var(--mdc-list-item-graphic-margin, 32px) !important; margin-right: 0px !important;}";
+            "span.material-icons:first-of-type { margin-left: var(--mdc-list-item-graphic-margin, 32px) !important; margin-right: 0px !important;}";
           item!.shadowRoot!.appendChild(style);
-          const span = item.shadowRoot?.querySelector("span:first-child");
-          span!.classList.add("rtl-fix");
-
-          // @ts-ignore
-          item.getUpdateComplete = function () {
-            // @ts-ignore
-            const result = item._getUpdateComplete();
-
-            // re-apply class since something in ripple handler resets it even though no style changes
-            const rtlSpan = item.shadowRoot?.querySelector(".rtl-fix");
-            if (rtlSpan) {
-              rtlSpan!.classList.remove("rtl-fix");
-              rtlSpan!.classList.add("rtl-fix2");
-
-              new Promise((resolve) => {
-                setTimeout(() => resolve("done"), 0);
-              }).then(() => {
-                span!.classList.remove("rtl-fix2");
-                span!.classList.add("rtl-fix");
-              });
-            }
-
-            return result;
-          };
         });
       });
     }
