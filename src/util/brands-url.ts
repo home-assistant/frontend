c4624faa714d2b4b0d5fed83885e5a66dd66ef55
Zack Barett <zackbarett@hey.com>
2022-05-25 10:11:15 -0500

    Hardware MVP (#12773)

--- src/util/brands-url.ts
+++ src/util/brands-url.ts
@@ -5,9 +5,21 @@ export interface BrandsOptions {
   darkOptimized?: boolean;
 }
 
+export interface HardwareBrandsOptions {
+  category: string;
+  model?: string;
+  manufacturer: string;
+  darkOptimized?: boolean;
+}
+
 export const brandsUrl = (options: BrandsOptions): string =>
   `https://brands.home-assistant.io/${options.useFallback ? "_/" : ""}${
     options.domain
   }/${options.darkOptimized ? "dark_" : ""}${options.type}.png`;
+
+export const hardwareBrandsUrl = (options: HardwareBrandsOptions): string =>
+  `https://brands.home-assistant.io/hardware/${options.category}/${
+    options.darkOptimized ? "dark_" : ""
+  }${options.manufacturer}${options.model ? `_${options.model}` : ""}.png`;
 
 export const extractDomainFromBrandUrl = (url: string) => url.split("/")[4];
