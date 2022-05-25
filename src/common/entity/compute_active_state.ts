a3e323ec116d9d887f23a232411d4d18feb09f55
Philip Allgaier <mail@spacegaier.de>
2022-05-25 17:31:34 +0200

    Fix "unavailable" handling for climate state rendering

--- src/common/entity/compute_active_state.ts
+++ src/common/entity/compute_active_state.ts
@@ -1,6 +1,11 @@
 import { HassEntity } from "home-assistant-js-websocket";
+import { UNAVAILABLE_STATES } from "../../data/entity";
 
 export const computeActiveState = (stateObj: HassEntity): string => {
+  if (UNAVAILABLE_STATES.includes(stateObj.state)) {
+    return stateObj.state;
+  }
+
   const domain = stateObj.entity_id.split(".")[0];
   let state = stateObj.state;
 
