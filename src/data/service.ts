7cccd68a35d0838d03528e1042b48d6027292254
J. Nick Koston <nick@koston.org>
2022-06-03 17:00:46 -1000

    Localize logbook service descriptions

--- src/data/service.ts
+++ src/data/service.ts
@@ -12,3 +12,15 @@ export const callExecuteScript = (
 
 export const serviceCallWillDisconnect = (domain: string, service: string) =>
   domain === "homeassistant" && ["restart", "stop"].includes(service);
+
+export const serviceDescription = (
+  hass: HomeAssistant,
+  domain?: string,
+  service?: string
+) =>
+  domain &&
+  domain in hass.services &&
+  service &&
+  service in hass.services[domain]
+    ? hass.services[domain][service].description
+    : `${domain}.${service}`;
