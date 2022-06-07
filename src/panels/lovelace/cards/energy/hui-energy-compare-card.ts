856c40240f435e7b0a8309e0db1f3951ac3294e3
emanu <emanuele.ladaga@gmail.com>
2022-06-07 21:41:28 +0200

    update with suggestion
    update with suggestion @bramkragten

--- src/panels/lovelace/cards/energy/hui-energy-compare-card.ts
+++ src/panels/lovelace/cards/energy/hui-energy-compare-card.ts
@@ -56,21 +56,19 @@ export class HuiEnergyCompareCard
 
     return html`
       <ha-alert dismissable @alert-dismissed-clicked=${this._stopCompare}>
-        ${this.hass.localize("ui.panel.energy.compare.info_1")}
-        <b
-          >${formatDate(this._start!, this.hass.locale)}${dayDifference > 0
-            ? ` -
-        ${formatDate(this._end || endOfDay(new Date()), this.hass.locale)}`
-            : ""}</b
-        >
-        ${this.hass.localize("ui.panel.energy.compare.info_2")}
-        <b
-          >${formatDate(this._startCompare, this.hass.locale)}${dayDifference >
-          0
-            ? ` -
-        ${formatDate(this._endCompare, this.hass.locale)}`
-            : ""}</b
-        >
+        ${this.hass.localize("ui.panel.energy.compare.info", {start: html`<b
+        >${formatDate(this._start!, this.hass.locale)}${dayDifference > 0
+          ? ` -
+      ${formatDate(this._end || endOfDay(new Date()), this.hass.locale)}`
+          : ""}</b
+      >`, end: html`<b
+        >${formatDate(this._startCompare, this.hass.locale)}${dayDifference >
+        0
+          ? ` -
+      ${formatDate(this._endCompare, this.hass.locale)}`
+          : ""}</b
+      >`})}
+
       </ha-alert>
     `;
   }
