4b36770adf6f77520c1e4722fbbbd320491ba035
wizmo2 <wizmo.home@yahoo.com>
2022-06-07 10:42:37 -0400

    Fix overlapped tiles and hidden title for 2:3 aspect ratio media classes (#12853)

--- src/components/media-player/ha-media-player-browse.ts
+++ src/components/media-player/ha-media-player-browse.ts
@@ -485,7 +485,10 @@ export class HaMediaPlayerBrowse extends LitElement {
                       .layout=${grid({
                         itemSize: {
                           width: "175px",
-                          height: "225px",
+                          height:
+                            childrenMediaClass.thumbnail_ratio === "portrait"
+                              ? "312px"
+                              : "225px",
                         },
                         gap: "16px",
                         flex: { preserve: "aspect-ratio" },
