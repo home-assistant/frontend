d7a369de9c51895d6b32967721dfed637856fee3
Emanuele <55278049+elax46@users.noreply.github.com>
2022-06-08 10:54:34 +0200

    Update src/panels/lovelace/cards/energy/hui-energy-compare-card.ts
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>

--- src/panels/lovelace/cards/energy/hui-energy-compare-card.ts
+++ src/panels/lovelace/cards/energy/hui-energy-compare-card.ts
@@ -56,7 +56,6 @@ export class HuiEnergyCompareCard
 
     return html`
       <ha-alert dismissable @alert-dismissed-clicked=${this._stopCompare}>
-
         ${this.hass.localize("ui.panel.energy.compare.info", {start: 
         html`<b
           >${formatDate(this._start!, this.hass.locale)}${dayDifference > 0
