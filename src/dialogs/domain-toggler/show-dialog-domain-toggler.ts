3b3010115a6b6fe1561993f7ccf53b0ec568f83b
Paulus Schoutsen <balloob@gmail.com>
2022-06-13 15:27:36 -0700

    Update text around updating entity defaults

--- src/dialogs/domain-toggler/show-dialog-domain-toggler.ts
+++ src/dialogs/domain-toggler/show-dialog-domain-toggler.ts
@@ -1,6 +1,7 @@
 import { fireEvent } from "../../common/dom/fire_event";
 
 export interface HaDomainTogglerDialogParams {
+  title?: string;
   domains: string[];
   exposedDomains: string[] | null;
   toggleDomain: (domain: string, turnOn: boolean) => void;
