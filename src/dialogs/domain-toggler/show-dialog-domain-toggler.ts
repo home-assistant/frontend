d90c9697b047591a601f9188ac3ab8bca26b382a
Paulus Schoutsen <balloob@gmail.com>
2022-06-13 15:50:51 -0700

    Add missing key

--- src/dialogs/domain-toggler/show-dialog-domain-toggler.ts
+++ src/dialogs/domain-toggler/show-dialog-domain-toggler.ts
@@ -2,6 +2,7 @@ import { fireEvent } from "../../common/dom/fire_event";
 
 export interface HaDomainTogglerDialogParams {
   title?: string;
+  description?: string;
   domains: string[];
   exposedDomains: string[] | null;
   toggleDomain: (domain: string, turnOn: boolean) => void;
