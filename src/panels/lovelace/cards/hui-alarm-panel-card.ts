a4cb270f093dfdc879ed5aa266fb1b48e96c2a28
Josh McCarty <josh@joshmccarty.com>
2022-06-16 07:58:29 -0700

    Ensures inputmode is set properly for alarm code inputs (#12953)

--- src/panels/lovelace/cards/hui-alarm-panel-card.ts
+++ src/panels/lovelace/cards/hui-alarm-panel-card.ts
@@ -186,7 +186,7 @@ class HuiAlarmPanelCard extends LitElement implements LovelaceCard {
                 id="alarmCode"
                 .label=${this.hass.localize("ui.card.alarm_control_panel.code")}
                 type="password"
-                .inputmode=${stateObj.attributes.code_format === FORMAT_NUMBER
+                .inputMode=${stateObj.attributes.code_format === FORMAT_NUMBER
                   ? "numeric"
                   : "text"}
               ></ha-textfield>
