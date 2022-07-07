4754fabd3c9319878350b6732d5667b8ad5ea04e
Zack Barett <zackbarett@hey.com>
2022-07-07 07:27:28 -0500

    Refactor History Panel Code a bit (#13129)
    Co-authored-by: D3v01dZA <caltona1@gmail.com>

--- src/data/area_registry.ts
+++ src/data/area_registry.ts
@@ -3,11 +3,21 @@ import { Store } from "home-assistant-js-websocket/dist/store";
 import { stringCompare } from "../common/string/compare";
 import { debounce } from "../common/util/debounce";
 import { HomeAssistant } from "../types";
+import { DeviceRegistryEntry } from "./device_registry";
+import { EntityRegistryEntry } from "./entity_registry";
 
 export interface AreaRegistryEntry {
   area_id: string;
   name: string;
   picture: string | null;
+}
+
+export interface AreaEntityLookup {
+  [areaId: string]: EntityRegistryEntry[];
+}
+
+export interface AreaDeviceLookup {
+  [areaId: string]: DeviceRegistryEntry[];
 }
 
 export interface AreaRegistryEntryMutableParams {
@@ -79,3 +89,35 @@ export const subscribeAreaRegistry = (
     conn,
     onChange
   );
+
+export const getAreaEntityLookup = (
+  entities: EntityRegistryEntry[]
+): AreaEntityLookup => {
+  const areaEntityLookup: AreaEntityLookup = {};
+  for (const entity of entities) {
+    if (!entity.area_id) {
+      continue;
+    }
+    if (!(entity.area_id in areaEntityLookup)) {
+      areaEntityLookup[entity.area_id] = [];
+    }
+    areaEntityLookup[entity.area_id].push(entity);
+  }
+  return areaEntityLookup;
+};
+
+export const getAreaDeviceLookup = (
+  devices: DeviceRegistryEntry[]
+): AreaDeviceLookup => {
+  const areaDeviceLookup: AreaDeviceLookup = {};
+  for (const device of devices) {
+    if (!device.area_id) {
+      continue;
+    }
+    if (!(device.area_id in areaDeviceLookup)) {
+      areaDeviceLookup[device.area_id] = [];
+    }
+    areaDeviceLookup[device.area_id].push(device);
+  }
+  return areaDeviceLookup;
+};
