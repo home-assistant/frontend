c6ee41fabfe3bddac6ebb974fb2d5c9110fa0297
puddly <32534428+puddly@users.noreply.github.com>
2022-07-28 14:32:19 -0400

    Run prettier

--- src/panels/config/integrations/integration-panels/zha/dialog-zha-restore-backup.ts
+++ src/panels/config/integrations/integration-panels/zha/dialog-zha-restore-backup.ts
@@ -1,10 +1,4 @@
-import {
-  css,
-  CSSResultGroup,
-  html,
-  LitElement,
-  TemplateResult,
-} from "lit";
+import { css, CSSResultGroup, html, LitElement, TemplateResult } from "lit";
 import { customElement, property, state } from "lit/decorators";
 import { mdiFileUpload } from "@mdi/js";
 import { fireEvent } from "../../../../../common/dom/fire_event";
