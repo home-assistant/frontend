31e3e79e2b24088a53afaddd9d8a6af408ca8b83
Steve Repsher <steverep@users.noreply.github.com>
2022-08-16 16:48:46 -0400

    Edit pre-commit hook to fail on modifications to translation files

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   "*.{js,ts}": 'eslint --ignore-pattern "**/build-scripts/**/*.js" --fix',
   "!(/translations)*.{js,ts,json,css,md,html}": "prettier --write",
+  "translations/*/*.json": (files) =>
+    'printf "%s\n" "These files should not be modified.  Instead, make the necessary modifications in src/translations/en.json.  Please see translations/README.md for details." ' +
+    files.join(" ") +
+    " >&2 && exit 1",
 };
