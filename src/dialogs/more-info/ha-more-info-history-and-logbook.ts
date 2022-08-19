109d497901834e9ec367a9d4cc8b7e6945905c3b
Paulus Schoutsen <balloob@gmail.com>
2022-08-19 17:41:46 -0400

    Remove state from history

--- src/dialogs/more-info/ha-more-info-history-and-logbook.ts
+++ src/dialogs/more-info/ha-more-info-history-and-logbook.ts
@@ -1,11 +1,9 @@
 import { LitElement, html } from "lit";
 import { customElement, property } from "lit/decorators";
-import { computeDomain } from "../../common/entity/compute_domain";
 import { HomeAssistant } from "../../types";
 import {
   computeShowHistoryComponent,
   computeShowLogBookComponent,
-  DOMAINS_NO_INFO,
 } from "./const";
 import "./ha-more-info-history";
 import "./ha-more-info-logbook";
@@ -17,18 +15,7 @@ export class MoreInfoHistoryAndLogbook extends LitElement {
   @property() public entityId!: string;
 
   protected render() {
-    const stateObj = this.hass.states[this.entityId];
-
     return html`
-      ${stateObj && !DOMAINS_NO_INFO.includes(computeDomain(this.entityId))
-        ? html`
-            <state-card-content
-              in-dialog
-              .stateObj=${stateObj}
-              .hass=${this.hass}
-            ></state-card-content>
-          `
-        : ""}
       ${computeShowHistoryComponent(this.hass, this.entityId)
         ? html`
             <ha-more-info-history
