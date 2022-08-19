f447029f4c0510937b7239fe8c2f9642160f4c72
Franck Nijhof <git@frenck.dev>
2022-08-19 13:29:50 +0200

    No need to reset the state, as the state could be anything

--- src/components/ha-selector/ha-selector-state.ts
+++ src/components/ha-selector/ha-selector-state.ts
@@ -1,6 +1,5 @@
-import { html, LitElement, PropertyValues } from "lit";
+import { html, LitElement } from "lit";
 import { customElement, property } from "lit/decorators";
-import { fireEvent } from "../../common/dom/fire_event";
 import { StateSelector } from "../../data/selector";
 import { SubscribeMixin } from "../../mixins/subscribe-mixin";
 import { HomeAssistant } from "../../types";
@@ -40,47 +39,6 @@ export class HaSelectorState extends SubscribeMixin(LitElement) {
         allow-custom-value
       ></ha-entity-state-picker>
     `;
-  }
-
-  protected updated(changedProps: PropertyValues): void {
-    super.updated(changedProps);
-    if (
-      // No need to filter value if no value
-      !this.value ||
-      // Only adjust value if we used the context
-      this.selector.state.entity_id ||
-      // Only check if context has changed
-      !changedProps.has("context")
-    ) {
-      return;
-    }
-
-    const oldContext = changedProps.get("context") as this["context"];
-
-    if (
-      !this.context ||
-      oldContext?.filter_entity === this.context.filter_entity
-    ) {
-      return;
-    }
-
-    // Validate that that the attribute is still valid for this entity, else unselect.
-    let invalid = false;
-    if (this.context.filter_entity) {
-      const stateObj = this.hass.states[this.context.filter_entity];
-
-      if (!(stateObj && this.value in stateObj.attributes)) {
-        invalid = true;
-      }
-    } else {
-      invalid = this.value !== undefined;
-    }
-
-    if (invalid) {
-      fireEvent(this, "value-changed", {
-        value: undefined,
-      });
-    }
   }
 }
 
