a55bef0a93c786b0c5436d08ee5ff32d0a8a61d0
Franck Nijhof <git@frenck.dev>
2022-08-21 00:38:44 +0200

    Allow days in calendar trigger offset (#13427)

--- src/panels/config/automation/trigger/types/ha-automation-trigger-calendar.ts
+++ src/panels/config/automation/trigger/types/ha-automation-trigger-calendar.ts
@@ -44,7 +44,7 @@ export class HaCalendarTrigger extends LitElement implements TriggerElement {
             ],
           ],
         },
-        { name: "offset", selector: { duration: {} } },
+        { name: "offset", selector: { duration: { enable_day: true } } },
         {
           name: "offset_type",
           type: "select",
