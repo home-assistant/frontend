656071e6240736c6e52f3942b3650960aa09d6bf
Bram Kragten <mail@bramkragten.nl>
2022-08-31 17:13:28 +0200

    fix if

--- src/data/script_i18n.ts
+++ src/data/script_i18n.ts
@@ -210,11 +210,15 @@ export const describeAction = <T extends ActionType>(
   if (actionType === "if") {
     const config = action as IfAction;
     return `Perform an action if: ${
-      typeof config.if === "string"
+      !config.if
+        ? ""
+        : typeof config.if === "string"
         ? config.if
         : ensureArray(config.if).length > 1
         ? `${ensureArray(config.if).length} conditions`
-        : describeCondition(ensureArray(config.if)[0], hass)
+        : ensureArray(config.if).length
+        ? describeCondition(ensureArray(config.if)[0], hass)
+        : ""
     }${config.else ? " (or else!)" : ""}`;
   }
 
