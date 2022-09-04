649dced67e2e8f80ef95d84ef5f77567d78395a3
Allen Porter <allen@thebends.org>
2022-09-04 13:28:32 -0700

    Revert delete config entry change

--- src/data/config_entries.ts
+++ src/data/config_entries.ts
@@ -74,16 +74,10 @@ export const updateConfigEntry = (
     ...updatedValues,
   });
 
-export interface DeleteConfigEntryResult {
-  require_restart: boolean;
-  application_credential_id: string;
-}
-
 export const deleteConfigEntry = (hass: HomeAssistant, configEntryId: string) =>
-  hass.callApi<DeleteConfigEntryResult>(
-    "DELETE",
-    `config/config_entries/entry/${configEntryId}`
-  );
+  hass.callApi<{
+    require_restart: boolean;
+  }>("DELETE", `config/config_entries/entry/${configEntryId}`);
 
 export const reloadConfigEntry = (hass: HomeAssistant, configEntryId: string) =>
   hass.callApi<{
