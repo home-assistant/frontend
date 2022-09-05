4aff6e9870a7775aa61471aeccc927fa8cd5f0bc
Philip Allgaier <mail@spacegaier.de>
2022-09-05 12:23:38 +0200

    Add back blank before percent based on language

---
+++ src/common/translations/blank_before_percent.ts
@@ -0,0 +1,20 @@
+import { FrontendLocaleData } from "../../data/translation";
+
+// Logic based on https://en.wikipedia.org/wiki/Percent_sign#Form_and_spacing
+export const blankBeforePercent = (
+  localeOptions: FrontendLocaleData
+): string => {
+  switch (localeOptions.language) {
+    case "cz":
+    case "de":
+    case "en-gb":
+    case "fi":
+    case "fr":
+    case "nl":
+    case "sk":
+    case "sv":
+      return " ";
+    default:
+      return "";
+  }
+};
