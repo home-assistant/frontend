c3089de952faa7afbde8a38bb89c6e0a7d7dd180
Steve Repsher <steverep@users.noreply.github.com>
2022-09-14 10:48:07 -0400

    Fix punctuation in precommit message
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,7 +2,7 @@ module.exports = {
   "*.{js,ts}": ["prettier --write", "eslint --fix"],
   "!(/translations)*.{json,css,md,html}": "prettier --write",
   "translations/*/*.json": (files) =>
-    'printf "%s\n" "Translation files should not be added or modified here.Instead, make the necessary modifications in src/translations/en.json.  Other languages are managed externally. Please see https://developers.home-assistant.io/docs/translations/ for details." ' +
+    'printf "%s\n" "Translation files should not be added or modified here. Instead, make the necessary modifications in src/translations/en.json. Other languages are managed externally. Please see https://developers.home-assistant.io/docs/translations/ for details." ' +
     files.join(" ") +
     " >&2 && exit 1",
 };
