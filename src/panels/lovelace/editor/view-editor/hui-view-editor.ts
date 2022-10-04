f564420ac80b09ee55ba4dc3e41e75871301ca6d
Paul Bottein <paul.bottein@gmail.com>
2022-10-04 15:58:32 +0200

    Demo

--- src/panels/lovelace/editor/view-editor/hui-view-editor.ts
+++ src/panels/lovelace/editor/view-editor/hui-view-editor.ts
@@ -10,6 +10,7 @@ import type { HomeAssistant } from "../../../../types";
 import {
   DEFAULT_VIEW_LAYOUT,
   PANEL_VIEW_LAYOUT,
+  SECTIONS_VIEW_LAYOUT,
   SIDEBAR_VIEW_LAYOUT,
 } from "../../views/const";
 import { LovelaceViewConfig } from "../../../../data/lovelace/config/view";
@@ -53,6 +54,7 @@ export class HuiViewEditor extends LitElement {
                   DEFAULT_VIEW_LAYOUT,
                   SIDEBAR_VIEW_LAYOUT,
                   PANEL_VIEW_LAYOUT,
+                  SECTIONS_VIEW_LAYOUT,
                 ] as const
               ).map((type) => ({
                 value: type,
