8cfb182aa46bd857b012a876b638fa25e9d3549b
Steve Repsher <steverep@users.noreply.github.com>
2022-10-23 00:37:25 -0400

    Add helper to ha-form-integer

--- src/components/ha-form/ha-form-integer.ts
+++ src/components/ha-form/ha-form-integer.ts
@@ -21,6 +21,8 @@ export class HaFormInteger extends LitElement implements HaFormElement {
 
   @property() public label?: string;
 
+  @property() public helper?: string;
+
   @property({ type: Boolean }) public disabled = false;
 
   @query("ha-textfield ha-slider") private _input?:
@@ -74,10 +76,12 @@ export class HaFormInteger extends LitElement implements HaFormElement {
         type="number"
         inputMode="numeric"
         .label=${this.label}
+        .helper=${this.helper}
+        helperPersistent
         .value=${this.data !== undefined ? this.data : ""}
         .disabled=${this.disabled}
-        .required=${this.schema.required}
-        .autoValidate=${this.schema.required}
+        ?required=${this.schema.required}
+        ?autoValidate=${this.schema.required}
         .suffix=${this.schema.description?.suffix}
         .validationMessage=${this.schema.required ? "Required" : undefined}
         @input=${this._valueChanged}
