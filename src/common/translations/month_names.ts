1f2f5e763de3f63a2a87c4d340d8b31e861ef960
Philip Allgaier <philip.allgaier@gmx.de>
2022-12-05 17:18:24 +0100

    Update src/common/translations/month_names.ts
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>

--- src/common/translations/month_names.ts
+++ src/common/translations/month_names.ts
@@ -4,7 +4,7 @@ import { FrontendLocaleData } from "../../data/translation";
 import { formatDateMonth } from "../datetime/format_date";
 
 export const monthNames = memoizeOne((locale: FrontendLocaleData): string[] =>
-  Array.from({ length: 12 }, (_, m) => m =>
+  Array.from({ length: 12 }, (_, m) =>
     formatDateMonth(addMonths(startOfYear(new Date()), m), locale)
   )
 );
