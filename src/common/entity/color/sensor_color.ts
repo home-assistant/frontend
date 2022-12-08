0d0028fced3b23b02806daa82a4faa6b47f9c72b
Paul Bottein <paul.bottein@gmail.com>
2022-12-08 12:01:18 +0100

    Fixes state history colors edge cases (#14626)

--- src/common/entity/color/sensor_color.ts
+++ src/common/entity/color/sensor_color.ts
@@ -1,11 +1,14 @@
 import { HassEntity } from "home-assistant-js-websocket";
 import { batteryStateColor } from "./battery_color";
 
-export const sensorColor = (stateObj: HassEntity): string | undefined => {
+export const sensorColor = (
+  stateObj: HassEntity,
+  compareState: string
+): string | undefined => {
   const deviceClass = stateObj?.attributes.device_class;
 
   if (deviceClass === "battery") {
-    return batteryStateColor(stateObj);
+    return batteryStateColor(compareState);
   }
 
   return undefined;
