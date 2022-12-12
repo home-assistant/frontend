0fb35fd0d00adcaacf0f89aa54246a3bf6f55f47
Paul Bottein <paul.bottein@gmail.com>
2022-12-12 15:52:33 +0100

    Transparent unavailable state in history (#14710)
    * Use transparent color for unavailable state for history
    
    * Remove inactive color
    
    * Only color active state for badge icon
    
    * Simplify condition
    
    * Update src/components/chart/timeline-chart/textbar-element.ts
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>

--- src/panels/lovelace/cards/hui-entity-card.ts
+++ src/panels/lovelace/cards/hui-entity-card.ts
@@ -133,8 +133,8 @@ export class HuiEntityCard extends LitElement implements LovelaceCard {
 
     const name = this._config.name || computeStateName(stateObj);
 
-    const colored = stateObj && this.getStateColor(stateObj, this._config);
-    const active = stateObj && colored && stateActive(stateObj);
+    const active = stateObj && stateActive(stateObj);
+    const colored = active && this.getStateColor(stateObj, this._config);
 
     return html`
       <ha-card @click=${this._handleClick} tabindex="0">
@@ -193,12 +193,14 @@ export class HuiEntityCard extends LitElement implements LovelaceCard {
     `;
   }
 
-  private _computeColor(stateObj: HassEntity | LightEntity): string {
+  private _computeColor(
+    stateObj: HassEntity | LightEntity
+  ): string | undefined {
     const iconColor = stateColorCss(stateObj);
     if (iconColor) {
       return `rgb(${iconColor})`;
     }
-    return "";
+    return undefined;
   }
 
   protected shouldUpdate(changedProps: PropertyValues): boolean {
