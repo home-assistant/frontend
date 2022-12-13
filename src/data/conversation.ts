82db90f790b01ad5659e60f0c539ba298f001189
Paulus Schoutsen <balloob@gmail.com>
2022-12-13 15:44:17 -0500

    Update conversation API

--- src/data/conversation.ts
+++ src/data/conversation.ts
@@ -1,10 +1,48 @@
 import { HomeAssistant } from "../types";
 
-interface ProcessResults {
-  card: { [key: string]: Record<string, string> };
-  speech: {
-    [SpeechType in "plain" | "ssml"]: { extra_data: any; speech: string };
+interface IntentTarget {
+  type: "area" | "device" | "entity" | "domain" | "device_class" | "custom";
+  name: string;
+  id: string | null;
+}
+
+interface IntentResultBase {
+  language: string;
+  speech:
+    | {
+        [SpeechType in "plain" | "ssml"]: { extra_data: any; speech: string };
+      }
+    | null;
+}
+
+interface IntentResultActionDone extends IntentResultBase {
+  response_type: "action_done";
+  data: {
+    targets: IntentTarget[];
   };
+}
+
+interface IntentResultQueryAnswer extends IntentResultBase {
+  response_type: "query_answer";
+}
+
+interface IntentResultError extends IntentResultBase {
+  response_type: "error";
+  data: {
+    code:
+      | "no_intent_match"
+      | "no_valid_targets"
+      | "failed_to_handle"
+      | "unknown";
+  };
+}
+
+interface ConversationResult {
+  conversation_id: string;
+  response:
+    | IntentResultActionDone
+    | IntentResultQueryAnswer
+    | IntentResultError;
 }
 
 export interface AgentInfo {
@@ -12,12 +50,12 @@ export interface AgentInfo {
   onboarding?: { text: string; url: string };
 }
 
-export const processText = (
+export const processConversationInput = (
   hass: HomeAssistant,
   text: string,
   // eslint-disable-next-line: variable-name
   conversation_id: string
-): Promise<ProcessResults> =>
+): Promise<ConversationResult> =>
   hass.callWS({
     type: "conversation/process",
     text,
