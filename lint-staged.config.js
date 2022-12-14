d48853fcdd6ea8933b4fc1ba6199137649783072
Steve Repsher <steverep@users.noreply.github.com>
2022-12-14 06:11:58 -0500

    Add precommit hook to deduplicate dependencies (#14609)

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -5,4 +5,5 @@ module.exports = {
     'printf "%s\n" "Translation files should not be added or modified here. Instead, make the necessary modifications in src/translations/en.json. Other languages are managed externally. Please see https://developers.home-assistant.io/docs/translations/ for details." ' +
     files.join(" ") +
     " >&2 && exit 1",
+  "/yarn.lock": () => "yarn dedupe",
 };
