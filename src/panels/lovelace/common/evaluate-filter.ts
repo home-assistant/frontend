25a5bd568ad8893c484b016e05c3fe3214a34a25
karwosts <32912880+karwosts@users.noreply.github.com>
2022-12-14 09:51:33 -0800

    Fix entity-filter handling of numeric states for == and != operators. (#14726)
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>
    fixes undefined

--- src/panels/lovelace/common/evaluate-filter.ts
+++ src/panels/lovelace/common/evaluate-filter.ts
@@ -2,10 +2,23 @@ import { HassEntity } from "home-assistant-js-websocket";
 
 export const evaluateFilter = (stateObj: HassEntity, filter: any): boolean => {
   const operator = filter.operator || "==";
-  const value = filter.value ?? filter;
-  const state = filter.attribute
+  let value = filter.value ?? filter;
+  let state = filter.attribute
     ? stateObj.attributes[filter.attribute]
     : stateObj.state;
+
+  if (operator === "==" || operator === "!=") {
+    const valueIsNumeric =
+      typeof value === "number" ||
+      (typeof value === "string" && value.trim() && !isNaN(Number(value)));
+    const stateIsNumeric =
+      typeof state === "number" ||
+      (typeof state === "string" && state.trim() && !isNaN(Number(state)));
+    if (valueIsNumeric && stateIsNumeric) {
+      value = Number(value);
+      state = Number(state);
+    }
+  }
 
   switch (operator) {
     case "==":
