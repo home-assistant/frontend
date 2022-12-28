6a15216104f73bbcc0b3f020fe3880e7d80b4e71
Allen Porter <allen.porter@gmail.com>
2022-12-28 05:07:17 -0800

    Add monthly variations for recurrence rules (#14849)
    * Add variations on monthly recurrence rules
    
    * Recurrence rule code simplificiation
    
    * Invalidate when the interval changes
    
    * update
    
    * Update ha-recurrence-rule-editor.ts
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>

--- src/panels/calendar/dialog-calendar-event-editor.ts
+++ src/panels/calendar/dialog-calendar-event-editor.ts
@@ -248,6 +248,8 @@ class DialogCalendarEventEditor extends LitElement {
             </div>
           </div>
           <ha-recurrence-rule-editor
+            .hass=${this.hass}
+            .dtstart=${this._dtstart}
             .locale=${this.hass.locale}
             .timezone=${this.hass.config.time_zone}
             .value=${this._rrule || ""}
