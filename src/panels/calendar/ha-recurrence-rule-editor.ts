40ddf902539016821f7cbaf6a56c3fbca30a40ce
Allen Porter <allen.porter@gmail.com>
2022-12-29 20:46:37 -0800

    Update src/panels/calendar/ha-recurrence-rule-editor.ts
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>

--- src/panels/calendar/ha-recurrence-rule-editor.ts
+++ src/panels/calendar/ha-recurrence-rule-editor.ts
@@ -41,7 +41,7 @@ export class RecurrenceRuleEditor extends LitElement {
 
   @property() public dtstart?: Date;
 
-  @property() public allDay?: bool;
+  @property() public allDay?: Boolean;
 
   @property({ attribute: false }) public locale!: HomeAssistant["locale"];
 
