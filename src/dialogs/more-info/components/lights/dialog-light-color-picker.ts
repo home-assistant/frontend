1c5db00dccc03e46e1fe822dde432baca4f72623
Paul Bottein <paul.bottein@gmail.com>
2023-01-18 14:46:45 +0100

    Use tabs and back arrow for color mode picker

--- src/dialogs/more-info/components/lights/dialog-light-color-picker.ts
+++ src/dialogs/more-info/components/lights/dialog-light-color-picker.ts
@@ -1,23 +1,30 @@
 import "@material/mwc-button";
+import "@material/mwc-tab-bar/mwc-tab-bar";
+import "@material/mwc-tab/mwc-tab";
 import { css, CSSResultGroup, html, LitElement, TemplateResult } from "lit";
 import { customElement, property, state } from "lit/decorators";
-import memoizeOne from "memoize-one";
 import { fireEvent } from "../../../../common/dom/fire_event";
 import "../../../../components/ha-button-toggle-group";
-import { createCloseHeading } from "../../../../components/ha-dialog";
+import "../../../../components/ha-header-bar";
+import "../../../../components/ha-icon-button-prev";
 import {
   LightColorMode,
   LightEntity,
   lightSupportsColorMode,
 } from "../../../../data/light";
 import { haStyleDialog } from "../../../../resources/styles";
-import { HomeAssistant, ToggleButton } from "../../../../types";
-import { LightColorPickerDialogParams } from "./show-dialog-light-color-picker";
-import "./modes/light-color-picker-mode-color-temp";
+import { HomeAssistant } from "../../../../types";
 import "./modes/light-color-picker-mode-color";
+import "./modes/light-color-picker-mode-color-temp";
+import { LightColorPickerDialogParams } from "./show-dialog-light-color-picker";
 
-const COLOR_MODES = [LightColorMode.COLOR_TEMP, LightColorMode.HS] as const;
-type ColorMode = typeof COLOR_MODES[number];
+const MODES = ["color_temp", "color"] as const;
+type Mode = typeof MODES[number];
+
+const supportedModes: Record<Mode, LightColorMode[]> = {
+  color_temp: [LightColorMode.COLOR_TEMP],
+  color: [LightColorMode.HS],
+};
 
 @customElement("dialog-light-color-picker")
 class DialogLightColorPicker extends LitElement {
@@ -25,16 +32,18 @@ class DialogLightColorPicker extends LitElement {
 
   @state() private _params?: LightColorPickerDialogParams;
 
-  @state() private _mode?: ColorMode;
+  @state() private _mode?: Mode;
 
-  @state() private _modes: ColorMode[] = [];
+  @state() private _modes: Mode[] = [];
 
   public async showDialog(params: LightColorPickerDialogParams): Promise<void> {
     this._params = params;
     const stateObj = this.hass.states[this._params.entityId];
 
-    this._modes = COLOR_MODES.filter((mode) =>
-      lightSupportsColorMode(stateObj, mode)
+    this._modes = MODES.filter((mode) =>
+      supportedModes[mode].some((lightCodeMode) =>
+        lightSupportsColorMode(stateObj, lightCodeMode)
+      )
     );
     this._mode = this._modes[0];
 
@@ -52,27 +61,39 @@ class DialogLightColorPicker extends LitElement {
     }
     const stateObj = this.hass.states[this._params.entityId] as LightEntity;
 
-    const buttons = this._modesButtons(
-      stateObj.attributes.supported_color_modes
-    );
-
     return html`
       <ha-dialog
         open
         @closed=${this._close}
-        .heading=${createCloseHeading(this.hass, "Color picker")}
+        hideActions
+        .heading=${"Change Color"}
       >
+        <div slot="heading">
+          <ha-header-bar>
+            <ha-icon-button-prev
+              slot="navigationIcon"
+              dialogAction="cancel"
+              .label=${this.hass.localize(
+                "ui.dialogs.more_info_control.dismiss"
+              )}
+            ></ha-icon-button-prev>
+
+            <span slot="title">Change Color</span>
+          </ha-header-bar>
+        </div>
         <div>
-          ${buttons.length > 1
-            ? html`<ha-button-toggle-group
-                fullWidth
-                .buttons=${this._modesButtons(
-                  stateObj.attributes.supported_color_modes
-                )}
-                .active=${this._mode}
-                @value-changed=${this._modeChanged}
-              ></ha-button-toggle-group>`
-            : null}
+          ${this._modes.length > 1
+            ? html`
+                <mwc-tab-bar
+                  .activeIndex=${this._mode && this._modes.indexOf(this._mode)}
+                  @MDCTabBar:activated=${this._handleTabChanged}
+                >
+                  ${this._modes.map(
+                    (mode) => html` <mwc-tab .label=${mode}></mwc-tab> `
+                  )}
+                </mwc-tab-bar>
+              `
+            : ""}
           ${this._mode === "color_temp"
             ? html`
                 <light-color-picker-mode-color-temp
@@ -82,7 +103,7 @@ class DialogLightColorPicker extends LitElement {
                 </light-color-picker-mode-color-temp>
               `
             : null}
-          ${this._mode === "hs"
+          ${this._mode === "color"
             ? html`
                 <light-color-picker-mode-color
                   .stateObj=${stateObj}
@@ -92,27 +113,16 @@ class DialogLightColorPicker extends LitElement {
               `
             : null}
         </div>
-        <mwc-button @click=${this.closeDialog} slot="primaryAction">
-          Ok
-        </mwc-button>
       </ha-dialog>
     `;
   }
 
-  private _modesButtons = memoizeOne(
-    (supportedModes: LightColorMode[] | undefined): ToggleButton[] => {
-      const modes = COLOR_MODES.filter((mode) =>
-        supportedModes?.includes(mode)
-      );
-      return modes.map((mode) => ({
-        label: mode,
-        value: mode,
-      }));
+  private _handleTabChanged(ev: CustomEvent): void {
+    const newMode = this._modes[ev.detail.index];
+    if (newMode === this._mode) {
+      return;
     }
-  );
-
-  private _modeChanged(ev: CustomEvent) {
-    this._mode = ev.detail.value;
+    this._mode = newMode;
   }
 
   private _close(): void {
@@ -123,6 +133,13 @@ class DialogLightColorPicker extends LitElement {
     return [
       haStyleDialog,
       css`
+        ha-dialog {
+          --dialog-content-padding: 0px;
+        }
+        ha-header-bar {
+          --mdc-theme-on-primary: var(--primary-text-color);
+          --mdc-theme-primary: var(--mdc-theme-surface);
+        }
         ha-button-toggle-group {
           margin-bottom: 8px;
         }
