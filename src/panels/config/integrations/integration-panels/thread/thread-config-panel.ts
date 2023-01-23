b3d2bd300fcfcd24a49757775f3c1cb2de13038c
Paulus Schoutsen <balloob@gmail.com>
2023-01-23 13:55:31 -0500

    Show Thread panel on thread config entry (#15150)
    * Show Thread panel on thread config entry
    
    * Rename data file to use right domain
    
    * Make panel a bit more pleasant

--- src/panels/config/integrations/integration-panels/thread/thread-config-panel.ts
+++ src/panels/config/integrations/integration-panels/thread/thread-config-panel.ts
@@ -5,7 +5,9 @@ import "../../../../../components/ha-card";
 import "../../../../../layouts/hass-subpage";
 import { haStyle } from "../../../../../resources/styles";
 import { HomeAssistant } from "../../../../../types";
-import { threadGetInfo, ThreadInfo } from "../../../../../data/thread";
+import { getOTBRInfo, OTBRInfo } from "../../../../../data/otbr";
+import { isComponentLoaded } from "../../../../../common/config/is_component_loaded";
+import { showConfigFlowDialog } from "../../../../../dialogs/config-flow/show-dialog-config-flow";
 
 @customElement("thread-config-panel")
 export class ThreadConfigPanel extends LitElement {
@@ -13,29 +15,43 @@ export class ThreadConfigPanel extends LitElement {
 
   @property({ type: Boolean }) public narrow!: boolean;
 
-  @state() private _info?: ThreadInfo;
+  @state() private _info?: OTBRInfo;
 
   protected render(): TemplateResult {
     return html`
       <hass-subpage .narrow=${this.narrow} .hass=${this.hass} header="Thread">
         <div class="content">
-          <ha-card header="Thread Border Router">
-            <div class="card-content">
-              ${!this._info
-                ? html`<ha-circular-progress active></ha-circular-progress>`
-                : html`
-                    <table>
-                      <tr>
-                        <td>URL</td>
-                        <td>${this._info.url}</td>
-                      </tr>
-                      <tr>
-                        <td>Active Dataset TLVs</td>
-                        <td>${this._info.active_dataset_tlvs || "-"}</td>
-                      </tr>
-                    </table>
-                  `}
-            </div>
+          <ha-card header="Open Thread Border Router">
+            ${isComponentLoaded(this.hass, "otbr")
+              ? html`
+                  <div class="card-content">
+                    ${!this._info
+                      ? html`<ha-circular-progress
+                          active
+                        ></ha-circular-progress>`
+                      : html`
+                          <table>
+                            <tr>
+                              <td>URL</td>
+                              <td>${this._info.url}</td>
+                            </tr>
+                            <tr>
+                              <td>Active Dataset TLVs</td>
+                              <td>${this._info.active_dataset_tlvs || "-"}</td>
+                            </tr>
+                          </table>
+                        `}
+                  </div>
+                `
+              : html`
+                  <div class="card-content">No border routers found.</div>
+                  <div class="card-actions">
+                    <mwc-button
+                      @click=${this._addOTBR}
+                      label="Add border router"
+                    ></mwc-button>
+                  </div>
+                `}
           </ha-card>
         </div>
       </hass-subpage>
@@ -45,8 +61,24 @@ export class ThreadConfigPanel extends LitElement {
   protected override firstUpdated(changedProps: PropertyValues) {
     super.firstUpdated(changedProps);
 
-    threadGetInfo(this.hass).then((info) => {
-      this._info = info;
+    this._refresh();
+  }
+
+  private _refresh() {
+    if (isComponentLoaded(this.hass, "otbr")) {
+      getOTBRInfo(this.hass).then((info) => {
+        this._info = info;
+      });
+    }
+  }
+
+  private _addOTBR() {
+    showConfigFlowDialog(this, {
+      dialogClosedCallback: () => {
+        this._refresh();
+      },
+      startFlowHandler: "otbr",
+      showAdvanced: this.hass.userData?.showAdvanced,
     });
   }
 
