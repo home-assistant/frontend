413523407e4d877060d60324e5fa93f9d441cd84
Paul Bottein <paul.bottein@gmail.com>
2023-02-02 10:26:34 +0100

    Icon entity context icon selector (#15306)
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>

--- src/panels/lovelace/editor/config-elements/hui-light-card-editor.ts
+++ src/panels/lovelace/editor/config-elements/hui-light-card-editor.ts
@@ -1,11 +1,7 @@
-import type { HassEntity } from "home-assistant-js-websocket";
 import { CSSResultGroup, html, LitElement, TemplateResult } from "lit";
 import { customElement, property, state } from "lit/decorators";
-import memoizeOne from "memoize-one";
 import { assert, assign, object, optional, string } from "superstruct";
 import { fireEvent } from "../../../../common/dom/fire_event";
-import { computeDomain } from "../../../../common/entity/compute_domain";
-import { domainIcon } from "../../../../common/entity/domain_icon";
 import { entityId } from "../../../../common/structs/is-entity-id";
 import "../../../../components/ha-form/ha-form";
 import type { SchemaUnion } from "../../../../components/ha-form/types";
@@ -28,6 +24,39 @@ const cardConfigStruct = assign(
   })
 );
 
+const SCHEMA = [
+  {
+    name: "entity",
+    required: true,
+    selector: { entity: { domain: "light" } },
+  },
+  {
+    type: "grid",
+    name: "",
+    schema: [
+      { name: "name", selector: { text: {} } },
+      {
+        name: "icon",
+        selector: {
+          icon: {},
+        },
+        context: {
+          icon_entity: "entity",
+        },
+      },
+    ],
+  },
+  { name: "theme", selector: { theme: {} } },
+  {
+    name: "hold_action",
+    selector: { "ui-action": {} },
+  },
+  {
+    name: "double_tap_action",
+    selector: { "ui-action": {} },
+  },
+] as const;
+
 @customElement("hui-light-card-editor")
 export class HuiLightCardEditor
   extends LitElement
@@ -42,62 +71,16 @@ export class HuiLightCardEditor
     this._config = config;
   }
 
-  private _schema = memoizeOne(
-    (entity: string, icon: string | undefined, entityState: HassEntity) =>
-      [
-        {
-          name: "entity",
-          required: true,
-          selector: { entity: { domain: "light" } },
-        },
-        {
-          type: "grid",
-          name: "",
-          schema: [
-            { name: "name", selector: { text: {} } },
-            {
-              name: "icon",
-              selector: {
-                icon: {
-                  placeholder: icon || entityState?.attributes.icon,
-                  fallbackPath:
-                    !icon && !entityState?.attributes.icon && entityState
-                      ? domainIcon(computeDomain(entity), entityState)
-                      : undefined,
-                },
-              },
-            },
-          ],
-        },
-        { name: "theme", selector: { theme: {} } },
-        {
-          name: "hold_action",
-          selector: { "ui-action": {} },
-        },
-        {
-          name: "double_tap_action",
-          selector: { "ui-action": {} },
-        },
-      ] as const
-  );
-
   protected render(): TemplateResult {
     if (!this.hass || !this._config) {
       return html``;
     }
 
-    const entityState = this.hass.states[this._config.entity];
-    const schema = this._schema(
-      this._config.entity,
-      this._config.icon,
-      entityState
-    );
-
     return html`
       <ha-form
         .hass=${this.hass}
         .data=${this._config}
-        .schema=${schema}
+        .schema=${SCHEMA}
         .computeLabel=${this._computeLabelCallback}
         @value-changed=${this._valueChanged}
       ></ha-form>
@@ -108,9 +91,7 @@ export class HuiLightCardEditor
     fireEvent(this, "config-changed", { config: ev.detail.value });
   }
 
-  private _computeLabelCallback = (
-    schema: SchemaUnion<ReturnType<typeof this._schema>>
-  ) => {
+  private _computeLabelCallback = (schema: SchemaUnion<typeof SCHEMA>) => {
     switch (schema.name) {
       case "theme":
       case "hold_action":
