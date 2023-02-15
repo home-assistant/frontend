d6c1fbf8d2dea0b12b124640968aa2daa7f80594
Kevin Stillhammer <kevin.stillhammer@gmail.com>
2023-02-15 11:59:41 +0100

    Set emptied fields to null in OptionFlows

--- src/data/options_flow.ts
+++ src/data/options_flow.ts
@@ -25,8 +25,29 @@ export const handleOptionsFlowStep = (
   hass.callApi<DataEntryFlowStep>(
     "POST",
     `config/config_entries/options/flow/${flowId}`,
-    data
+    transformEmptiedFields(data)
   );
+
+/**
+ * If a field is undefined it was emptied by the user.
+ *
+ * Set those to null so they won't be overridden by voluptuous defaults in core
+ * @param data Data which is intended to be send as the OptionFlow payload
+ * @returns the same data where undefined fields are null instead
+ */
+function transformEmptiedFields<T>(
+  data: Record<string, T>
+): Record<string, T | null> {
+  // If a field is undefined it was emptied by the user
+  // Set it to null so it won't be overridden by voluptuous defaults in core
+  const toSendData: Record<string, T | null> = {};
+
+  for (const [key, value] of Object.entries(data)) {
+    toSendData[key] = value !== undefined ? value : null;
+  }
+
+  return toSendData;
+}
 
 export const deleteOptionsFlow = (hass: HomeAssistant, flowId: string) =>
   hass.callApi("DELETE", `config/config_entries/options/flow/${flowId}`);
