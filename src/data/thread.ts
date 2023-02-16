1638941a74bdc12379300118fbb0e6ec787110e6
Bram Kragten <mail@bramkragten.nl>
2023-02-16 16:29:56 +0100

    Add basic Thread network overview page (#15474)

---
+++ src/data/thread.ts
@@ -0,0 +1,66 @@
+import { HomeAssistant } from "../types";
+
+export interface ThreadRouter {
+  brand: "google" | "apple" | "homeassistant";
+  server: string;
+  extended_pan_id: string;
+  model_name: string | null;
+  network_name: string;
+  vendor_name: string;
+}
+
+export interface ThreadDataSet {
+  created;
+  dataset_id;
+  extended_pan_id;
+  network_name: string;
+  pan_id;
+  preferred: boolean;
+  source;
+}
+
+export interface ThreadRouterDiscoveryEvent {
+  key: string;
+  type: "router_discovered" | "router_removed";
+  data: ThreadRouter;
+}
+
+class DiscoveryStream {
+  hass: HomeAssistant;
+
+  routers: { [key: string]: ThreadRouter };
+
+  constructor(hass: HomeAssistant) {
+    this.hass = hass;
+    this.routers = {};
+  }
+
+  processEvent(streamMessage: ThreadRouterDiscoveryEvent): ThreadRouter[] {
+    if (streamMessage.type === "router_discovered") {
+      this.routers[streamMessage.key] = streamMessage.data;
+    } else if (streamMessage.type === "router_removed") {
+      delete this.routers[streamMessage.key];
+    }
+    return Object.values(this.routers);
+  }
+}
+
+export const subscribeDiscoverThreadRouters = (
+  hass: HomeAssistant,
+  callbackFunction: (routers: ThreadRouter[]) => void
+) => {
+  const stream = new DiscoveryStream(hass);
+  return hass.connection.subscribeMessage<ThreadRouterDiscoveryEvent>(
+    (message) => callbackFunction(stream.processEvent(message)),
+    {
+      type: "thread/discover_routers",
+    }
+  );
+};
+
+export const listThreadDataSets = (
+  hass: HomeAssistant
+): Promise<{ datasets: ThreadDataSet[] }> =>
+  hass.callWS({
+    type: "thread/list_datasets",
+  });
