d4f2f13e3046838b209d1526e7a4bbf34cb73b6b
karwosts <karwosts@gmail.com>
2023-02-21 06:44:23 -0800

    Add 'for' to UI for template trigger

--- src/panels/config/automation/trigger/types/ha-automation-trigger-template.ts
+++ src/panels/config/automation/trigger/types/ha-automation-trigger-template.ts
@@ -1,9 +1,18 @@
 import "../../../../../components/ha-textarea";
-import { css, html, LitElement } from "lit";
+import { html, LitElement, PropertyValues } from "lit";
 import { customElement, property } from "lit/decorators";
 import type { TemplateTrigger } from "../../../../../data/automation";
 import type { HomeAssistant } from "../../../../../types";
-import { handleChangeEvent } from "../ha-automation-trigger-row";
+import "../../../../../components/ha-form/ha-form";
+import { createDurationData } from "../../../../../common/datetime/create_duration_data";
+import { fireEvent } from "../../../../../common/dom/fire_event";
+import { hasTemplate } from "../../../../../common/string/has-template";
+import type { SchemaUnion } from "../../../../../components/ha-form/types";
+
+const SCHEMA = [
+  { name: "value_template", required: true, selector: { template: {} } },
+  { name: "for", selector: { duration: {} } },
+] as const;
 
 @customElement("ha-automation-trigger-template")
 export class HaTemplateTrigger extends LitElement {
@@ -17,39 +26,60 @@ export class HaTemplateTrigger extends LitElement {
     return { value_template: "" };
   }
 
+  public willUpdate(changedProperties: PropertyValues) {
+    if (!changedProperties.has("trigger")) {
+      return;
+    }
+    // Check for templates in trigger. If found, revert to YAML mode.
+    if (this.trigger && hasTemplate(this.trigger.for)) {
+      fireEvent(
+        this,
+        "ui-mode-not-available",
+        Error(this.hass.localize("ui.errors.config.no_template_editor_support"))
+      );
+    }
+  }
+
   protected render() {
-    const { value_template } = this.trigger;
+    const trgFor = createDurationData(this.trigger.for);
+
+    const data = {
+      ...this.trigger,
+      for: trgFor,
+    };
+
     return html`
-      <p>
-        ${this.hass.localize(
-          "ui.panel.config.automation.editor.triggers.type.template.value_template"
-        )}
-        *
-      </p>
-      <ha-code-editor
-        .name=${"value_template"}
-        mode="jinja2"
+      <ha-form
         .hass=${this.hass}
-        .value=${value_template}
-        .readOnly=${this.disabled}
-        autocomplete-entities
+        .data=${data}
+        .schema=${SCHEMA}
         @value-changed=${this._valueChanged}
-        dir="ltr"
-      ></ha-code-editor>
+        .computeLabel=${this._computeLabelCallback}
+        .disabled=${this.disabled}
+      ></ha-form>
     `;
   }
 
   private _valueChanged(ev: CustomEvent): void {
-    handleChangeEvent(this, ev);
+    ev.stopPropagation();
+    const newTrigger = ev.detail.value;
+
+    if (
+      newTrigger.for &&
+      Object.values(newTrigger.for).every((value) => value === 0)
+    ) {
+      delete newTrigger.for;
+    }
+
+    fireEvent(this, "value-changed", { value: newTrigger });
   }
 
-  static get styles() {
-    return css`
-      p {
-        margin-top: 0;
-      }
-    `;
-  }
+  private _computeLabelCallback = (
+    schema: SchemaUnion<typeof SCHEMA>
+  ): string =>
+    this.hass.localize(
+      `ui.panel.config.automation.editor.triggers.type.template.${schema.name}`
+    );
 }
 
 declare global {
