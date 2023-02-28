bba9f3adc3f470a37ae67f9e5473e8aadc85d6ef
Philip Allgaier <mail@spacegaier.de>
2023-02-28 19:54:54 +0100

    Cleaner solution

--- src/dialogs/more-info/components/lights/ha-more-info-view-light-color-picker.ts
+++ src/dialogs/more-info/components/lights/ha-more-info-view-light-color-picker.ts
@@ -264,18 +264,10 @@ class MoreInfoViewLightColorPicker extends LitElement {
       }
 
       this._modes = modes;
-      // If we have an active color mode, use it to determine what to render.
-      // Otherwise (e. g. light is off) fallback to the supported modes instead.
-      this._mode = this.stateObj!.attributes.color_mode
-        ? this.stateObj!.attributes.color_mode === LightColorMode.COLOR_TEMP
+      this._mode =
+        this.stateObj!.attributes.color_mode === LightColorMode.COLOR_TEMP
           ? LightColorMode.COLOR_TEMP
-          : "color"
-        : this.stateObj!.attributes.supported_color_modes &&
-          this.stateObj!.attributes.supported_color_modes.indexOf(
-            LightColorMode.COLOR_TEMP
-          ) >= 0
-        ? LightColorMode.COLOR_TEMP
-        : "color";
+          : this._modes[0];
     }
 
     this._updateSliderValues();
