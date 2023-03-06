9653e3bd94d84ec2e27a80d5de2481502d029132
karwosts <32912880+karwosts@users.noreply.github.com>
2023-03-06 00:52:16 -0800

    Fix statistics-picker filter when no entity selected (#15659)
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>

--- src/panels/lovelace/editor/config-elements/hui-statistics-graph-card-editor.ts
+++ src/panels/lovelace/editor/config-elements/hui-statistics-graph-card-editor.ts
@@ -121,7 +121,9 @@ export class HuiStatisticsGraphCardEditor
       !deepEqual(this._configEntities, changedProps.get("_configEntities"))
     ) {
       this._metaDatas = undefined;
-      this._getStatisticsMetaData(this._configEntities);
+      if (this._configEntities?.length) {
+        this._getStatisticsMetaData(this._configEntities);
+      }
     }
   }
 
