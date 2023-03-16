771c77eb4f2b6cf08d7936977a707828b93b0b5e
Paulus Schoutsen <balloob@gmail.com>
2023-03-15 23:33:36 -0400

    Add basic debug panel for voice assistant pipelines

---
+++ src/data/voice_assistant.ts
@@ -0,0 +1,168 @@
+import { HomeAssistant } from "../types";
+import { ConversationResult } from "./conversation";
+
+interface PipelineEventBase {
+  timestamp: number;
+}
+
+interface PipelineRunStartEvent extends PipelineEventBase {
+  type: "run-start";
+  data: {
+    session_id?: string;
+  };
+}
+interface PipelineRunFinishEvent extends PipelineEventBase {
+  type: "run-finish";
+  data: {
+    url: string;
+  };
+}
+interface PipelineErrorEvent extends PipelineEventBase {
+  type: "error";
+  data: {
+    code: string;
+    message: string;
+  };
+}
+interface PipelineSTTStartEvent extends PipelineEventBase {
+  type: "stt-start";
+  data: {
+    engine: string;
+  };
+}
+interface PipelineSTTFinishEvent extends PipelineEventBase {
+  type: "stt-finish";
+  data: {
+    text: string;
+  };
+}
+
+interface PipelineIntentStartEvent extends PipelineEventBase {
+  type: "intent-start";
+  data: {
+    agent_id: string;
+  };
+}
+interface PipelineIntentFinishEvent extends PipelineEventBase {
+  type: "intent-finish";
+  data: {
+    speech: string;
+    response: ConversationResult;
+  };
+}
+
+interface PipelineTTSStartEvent extends PipelineEventBase {
+  type: "tts-start";
+  data: {
+    engine: string;
+  };
+}
+interface PipelineTTSFinishEvent extends PipelineEventBase {
+  type: "tts-finish";
+  data: {
+    url: string;
+  };
+}
+
+type PipelineRunEvent =
+  | PipelineRunStartEvent
+  | PipelineRunFinishEvent
+  | PipelineErrorEvent
+  | PipelineSTTStartEvent
+  | PipelineSTTFinishEvent
+  | PipelineIntentStartEvent
+  | PipelineIntentFinishEvent
+  | PipelineTTSStartEvent
+  | PipelineTTSFinishEvent;
+
+interface PipelineRunOptions {
+  pipeline?: string;
+  stt_text?: string;
+  conversation_id?: string | null;
+}
+
+export interface PipelineRun {
+  options: PipelineRunOptions;
+  events: PipelineRunEvent[];
+  stage: "initialized" | "stt" | "intent" | "tts" | "finish" | "error";
+  session: PipelineRunStartEvent["data"];
+  error?: PipelineErrorEvent["data"];
+  stt?: PipelineSTTStartEvent["data"] & Partial<PipelineSTTFinishEvent["data"]>;
+  intent?: PipelineIntentStartEvent["data"] &
+    Partial<PipelineIntentFinishEvent["data"]>;
+  tts?: PipelineTTSStartEvent["data"] & Partial<PipelineTTSFinishEvent["data"]>;
+}
+
+export const runPipelineFromText = (
+  hass: HomeAssistant,
+  callback: (event: PipelineRun) => void,
+  options: PipelineRunOptions = {}
+) => {
+  if (!options.pipeline) {
+    options.pipeline = "default";
+  }
+
+  let run: PipelineRun | undefined;
+
+  const unsubProm = hass.connection.subscribeMessage<PipelineRunEvent>(
+    (updateEvent) => {
+      if (updateEvent.type === "run-start") {
+        run = {
+          options,
+          stage: "initialized",
+          session: updateEvent.data,
+          error: undefined,
+          stt: undefined,
+          intent: undefined,
+          tts: undefined,
+          events: [updateEvent],
+        };
+        callback(run);
+        return;
+      }
+
+      if (!run) {
+        // eslint-disable-next-line no-console
+        console.warn(
+          "Received unexpected event before receiving session",
+          updateEvent
+        );
+        return;
+      }
+
+      run.events.push(updateEvent);
+
+      if (updateEvent.type === "stt-start") {
+        run.stage = "stt";
+        run.stt = updateEvent.data;
+      } else if (updateEvent.type === "stt-finish") {
+        run.stt = { ...run.stt!, ...updateEvent.data };
+      } else if (updateEvent.type === "intent-start") {
+        run.stage = "intent";
+        run.intent = updateEvent.data;
+      } else if (updateEvent.type === "intent-finish") {
+        run.intent = { ...run.intent!, ...updateEvent.data };
+      } else if (updateEvent.type === "tts-start") {
+        run.stage = "tts";
+        run.tts = updateEvent.data;
+      } else if (updateEvent.type === "tts-finish") {
+        run.tts = { ...run.tts!, ...updateEvent.data };
+      } else if (updateEvent.type === "run-finish") {
+        run.stage = "finish";
+        unsubProm.then((unsub) => unsub());
+      } else if (updateEvent.type === "error") {
+        run.stage = "error";
+        run.error = updateEvent.data;
+        unsubProm.then((unsub) => unsub());
+      }
+
+      callback(run);
+    },
+    {
+      ...options,
+      type: "voice_assistant/run",
+    }
+  );
+
+  return unsubProm;
+};
---
+++ src/panels/config/integrations/integration-panels/voice_assistant/assist/assist-pipeline-debug.ts
@@ -0,0 +1,112 @@
+import "@material/mwc-button";
+import "@material/mwc-textfield";
+import { css, html, LitElement, TemplateResult } from "lit";
+import { customElement, property, query, state } from "lit/decorators";
+import "../../../../../../components/ha-card";
+import {
+  PipelineRun,
+  runPipelineFromText,
+} from "../../../../../../data/voice_assistant";
+import "../../../../../../layouts/hass-subpage";
+import { SubscribeMixin } from "../../../../../../mixins/subscribe-mixin";
+import { haStyle } from "../../../../../../resources/styles";
+import { HomeAssistant } from "../../../../../../types";
+
+@customElement("assist-pipeline-debug")
+export class AssistPipelineDebug extends SubscribeMixin(LitElement) {
+  @property({ attribute: false }) public hass!: HomeAssistant;
+
+  @property({ type: Boolean }) public narrow!: boolean;
+
+  @query(".run-pipeline-card mwc-textfield", true)
+  private _newRunInput!: HTMLInputElement;
+
+  @state()
+  private _pipelineRun?: PipelineRun;
+
+  protected render(): TemplateResult {
+    return html`
+      <hass-subpage
+        .narrow=${this.narrow}
+        .hass=${this.hass}
+        header="Assist Pipeline"
+      >
+        <div class="content">
+          <ha-card header="Run pipeline" class="run-pipeline-card">
+            <div class="card-content">
+              <mwc-textfield
+                label="Input"
+                value="Are the lights on?"
+              ></mwc-textfield>
+            </div>
+            <div class="card-actions">
+              <mwc-button
+                @click=${this._runPipeline}
+                .disabled=${this._pipelineRun &&
+                !["error", "finish"].includes(this._pipelineRun.stage)}
+              >
+                Run
+              </mwc-button>
+            </div>
+          </ha-card>
+          ${this._pipelineRun
+            ? html`
+                <ha-card heading="Pipeline Run">
+                  <div class="card-content">
+                    <pre>${JSON.stringify(this._pipelineRun, null, 2)}</pre>
+                  </div>
+                </ha-card>
+              `
+            : ""}
+        </div>
+      </hass-subpage>
+    `;
+  }
+
+  private _runPipeline(): void {
+    this._pipelineRun = undefined;
+    runPipelineFromText(
+      this.hass,
+      (run) => {
+        this._pipelineRun = run;
+      },
+      {
+        stt_text: this._newRunInput.value,
+      }
+    );
+  }
+
+  static styles = [
+    haStyle,
+    css`
+      .content {
+        padding: 24px 0 32px;
+        max-width: 600px;
+        margin: 0 auto;
+        direction: ltr;
+      }
+      ha-button-menu a {
+        text-decoration: none;
+      }
+      ha-card {
+        margin-bottom: 16px;
+      }
+      .run-pipeline-card mwc-textfield {
+        display: block;
+      }
+      h4 {
+        margin: 0;
+      }
+      .card-header {
+        display: flex;
+        justify-content: space-between;
+      }
+    `,
+  ];
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "assist-pipeline-debug": AssistPipelineDebug;
+  }
+}
