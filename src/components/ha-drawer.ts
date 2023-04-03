31e1967082716f00a18385218103468b99ccf28d
Bram Kragten <mail@bramkragten.nl>
2023-04-03 14:59:31 +0200

    Prevent document scroll when modal drawer is open (#16033)

--- src/components/ha-drawer.ts
+++ src/components/ha-drawer.ts
@@ -3,8 +3,26 @@ import { styles } from "@material/mwc-drawer/mwc-drawer.css";
 import { css } from "lit";
 import { customElement } from "lit/decorators";
 
+const blockingElements = (document as any).$blockingElements;
+
 @customElement("ha-drawer")
 export class HaDrawer extends DrawerBase {
+  protected createAdapter() {
+    return {
+      ...super.createAdapter(),
+      trapFocus: () => {
+        blockingElements.push(this);
+        this.appContent.inert = true;
+        document.body.style.overflow = "hidden";
+      },
+      releaseFocus: () => {
+        blockingElements.remove(this);
+        this.appContent.inert = false;
+        document.body.style.overflow = "";
+      },
+    };
+  }
+
   static override styles = [
     styles,
     css`
