cc4e815a58ecf46455037318944dd09e887176a0
Paul Bottein <paul.bottein@gmail.com>
2023-04-06 18:25:23 +0200

    Fix background and padding on dashboard views (#16098)Co-authored-by: Bram Kragten <mail@bramkragten.nl>
    * Fix background and padding on dashboard views
    
    * Clean code
    
    * update fab position clean
    
    ---------
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>

--- src/panels/lovelace/hui-root.ts
+++ src/panels/lovelace/hui-root.ts
@@ -1049,15 +1049,6 @@ class HUIRoot extends LitElement {
         #view {
           margin-top: calc(var(--header-height) + env(safe-area-inset-top));
           height: calc(100vh - var(--header-height) - env(safe-area-inset-top));
-          padding-left: env(safe-area-inset-left);
-          padding-right: env(safe-area-inset-right);
-          background: var(
-            --lovelace-background,
-            var(--primary-background-color)
-          );
-          overflow: auto;
-          transform: translateZ(0);
-          display: flex;
         }
         /**
          * In edit mode we have the tab bar on a new line *
@@ -1070,19 +1061,17 @@ class HUIRoot extends LitElement {
             var(--header-height) + 48px + env(safe-area-inset-top)
           );
         }
-        #view > * {
-          /**
-          * The view could get larger than the window in Firefox
-          * to prevent that we set the max-width to 100%
-          * flex-grow: 1 and flex-basis: 100% should make sure the view
-          * stays full width.
-          *
-          * https://github.com/home-assistant/home-assistant-polymer/pull/3806
-          */
-          flex: 1 1 100%;
+        hui-view {
+          padding-left: env(safe-area-inset-left);
+          padding-right: env(safe-area-inset-right);
+          background: var(
+            --lovelace-background,
+            var(--primary-background-color)
+          );
+          overflow: auto;
+          width: 100%;
           height: 100%;
-          max-width: 100%;
-          padding-bottom: env(safe-area-inset-bottom);
+          transform: translateZ(0);
           display: block;
         }
         .hide-tab {
