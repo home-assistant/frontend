b13138b7881d55ee68eecf74f1f9891a61556a84
karwosts <karwosts@gmail.com>
2023-04-12 06:17:53 -0700

    Automation UI support for non-string state attributes

--- src/panels/config/automation/trigger/types/ha-automation-trigger-state.ts
+++ src/panels/config/automation/trigger/types/ha-automation-trigger-state.ts
@@ -1,9 +1,10 @@
 import { html, LitElement, PropertyValues } from "lit";
-import { customElement, property } from "lit/decorators";
+import { customElement, property, state } from "lit/decorators";
 import {
   array,
   assert,
   assign,
+  boolean,
   literal,
   number,
   object,
@@ -22,6 +23,7 @@ import { TriggerElement } from "../ha-automation-trigger-row";
 import "../../../../../components/ha-form/ha-form";
 import { createDurationData } from "../../../../../common/datetime/create_duration_data";
 import type { SchemaUnion } from "../../../../../components/ha-form/types";
+import type { LocalizeFunc } from "../../../../../common/translations/localize";
 
 const stateTriggerStruct = assign(
   baseTriggerStruct,
@@ -36,6 +38,19 @@ const stateTriggerStruct = assign(
   })
 );
 
+const attributeTriggerStruct = assign(
+  baseTriggerStruct,
+  object({
+    alias: optional(string()),
+    platform: literal("state"),
+    entity_id: optional(union([string(), array(string())])),
+    attribute: optional(string()),
+    from: optional(union([string(), number(), boolean()])),
+    to: optional(union([string(), number(), boolean()])),
+    for: optional(union([number(), string(), forDictStruct])),
+  })
+);
+
 @customElement("ha-automation-trigger-state")
 export class HaStateTrigger extends LitElement implements TriggerElement {
   @property({ attribute: false }) public hass!: HomeAssistant;
@@ -44,12 +59,14 @@ export class HaStateTrigger extends LitElement implements TriggerElement {
 
   @property({ type: Boolean }) public disabled = false;
 
+  @state() numericMode = false;
+
   public static get defaultConfig() {
     return { entity_id: [] };
   }
 
   private _schema = memoizeOne(
-    (entityId, attribute) =>
+    (localize: LocalizeFunc, entityId, attribute, attributeTypeEnable) =>
       [
         {
           name: "entity_id",
@@ -113,6 +130,33 @@ export class HaStateTrigger extends LitElement implements TriggerElement {
             },
           },
         },
+        ...(attribute
+          ? [
+              {
+                name: "attribute_type",
+                selector: {
+                  select: {
+                    required: true,
+                    options: [
+                      {
+                        value: "string",
+                        label: localize(
+                          "ui.panel.config.automation.editor.triggers.type.state.attribute_type_string"
+                        ),
+                      },
+                      {
+                        value: "number_boolean",
+                        label: localize(
+                          "ui.panel.config.automation.editor.triggers.type.state.attribute_type_number_boolean"
+                        ),
+                        disabled: !attributeTypeEnable,
+                      },
+                    ],
+                  },
+                },
+              },
+            ]
+          : []),
         {
           name: "from",
           selector: {
@@ -146,6 +190,7 @@ export class HaStateTrigger extends LitElement implements TriggerElement {
     ) {
       delete this.trigger.for.milliseconds;
     }
+
     // Check for templates in trigger. If found, revert to YAML mode.
     if (this.trigger && hasTemplate(this.trigger)) {
       fireEvent(
@@ -155,8 +200,31 @@ export class HaStateTrigger extends LitElement implements TriggerElement {
       );
       return false;
     }
+
+    if (
+      this.trigger.attribute &&
+      this.trigger.to !== undefined &&
+      this.trigger.from !== undefined &&
+      typeof this.trigger.to !== typeof this.trigger.from
+    ) {
+      fireEvent(
+        this,
+        "ui-mode-not-available",
+        Error(
+          this.hass.localize(
+            "ui.panel.config.automation.editor.triggers.type.state.ui_error_attribute_type_mismatch"
+          )
+        )
+      );
+      return false;
+    }
+
     try {
-      assert(this.trigger, stateTriggerStruct);
+      if (this.trigger.attribute) {
+        assert(this.trigger, attributeTriggerStruct);
+      } else {
+        assert(this.trigger, stateTriggerStruct);
+      }
     } catch (e: any) {
       fireEvent(this, "ui-mode-not-available", e);
       return false;
@@ -164,15 +232,71 @@ export class HaStateTrigger extends LitElement implements TriggerElement {
     return true;
   }
 
+  private isBooleanOrNumeric(value): boolean {
+    return (
+      typeof value === "number" ||
+      typeof value === "boolean" ||
+      value === undefined
+    );
+  }
+
+  private maybeBooleanOrNumeric(value): boolean {
+    if (this.isBooleanOrNumeric(value)) {
+      return true;
+    }
+    if (typeof value === "string") {
+      value = value.toLowerCase();
+      return (
+        !isNaN(value) ||
+        value === "on" ||
+        value === "off" ||
+        value === "true" ||
+        value === "false" ||
+        value === "yes" ||
+        value === "no"
+      );
+    }
+    return false;
+  }
+
+  private stringToBoolean(value: string): boolean {
+    value = value.toLowerCase();
+    return value === "yes" || value === "on" || value === "true";
+  }
+
+  private booleanToString(value: boolean): string {
+    return value ? "true" : "false";
+  }
+
   protected render() {
     const trgFor = createDurationData(this.trigger.for);
+    const numeric =
+      this.trigger.to === undefined && this.trigger.from === undefined
+        ? this.numericMode
+        : this.isBooleanOrNumeric(this.trigger.from) &&
+          this.isBooleanOrNumeric(this.trigger.to);
+    this.numericMode = numeric;
+    const maybeNumeric =
+      this.maybeBooleanOrNumeric(this.trigger.from) &&
+      this.maybeBooleanOrNumeric(this.trigger.to);
 
     const data = {
       ...this.trigger,
       entity_id: ensureArray(this.trigger.entity_id),
       for: trgFor,
+      attribute_type: numeric ? "number_boolean" : "string",
     };
-    const schema = this._schema(this.trigger.entity_id, this.trigger.attribute);
+
+    data.from =
+      data.from === false ? "false" : data.from === 0 ? "0" : data.from;
+    data.to = data.to === false ? "false" : data.to === 0 ? "0" : data.to;
+
+    const schema = this._schema(
+      this.hass.localize,
+      this.trigger.entity_id,
+      this.trigger.attribute,
+      maybeNumeric
+    );
 
     return html`
       <ha-form
@@ -195,6 +319,45 @@ export class HaStateTrigger extends LitElement implements TriggerElement {
         ? delete newTrigger[key]
         : {}
     );
+
+    if (
+      newTrigger.attribute &&
+      newTrigger.attribute_type === "number_boolean" &&
+      this.maybeBooleanOrNumeric(newTrigger.from) &&
+      this.maybeBooleanOrNumeric(newTrigger.to)
+    ) {
+      this.numericMode = true;
+      if (typeof newTrigger.from === "string") {
+        if (!isNaN(newTrigger.from)) {
+          newTrigger.from = Number(newTrigger.from);
+        } else {
+          newTrigger.from = this.stringToBoolean(newTrigger.from);
+        }
+      }
+      if (typeof newTrigger.to === "string") {
+        if (!isNaN(newTrigger.to)) {
+          newTrigger.to = Number(newTrigger.to);
+        } else {
+          newTrigger.to = this.stringToBoolean(newTrigger.to);
+        }
+      }
+    } else {
+      this.numericMode = false;
+      if (typeof newTrigger.from === "boolean") {
+        newTrigger.from = this.booleanToString(newTrigger.from);
+      }
+      if (typeof newTrigger.to === "boolean") {
+        newTrigger.to = this.booleanToString(newTrigger.to);
+      }
+      if (typeof newTrigger.from === "number") {
+        newTrigger.from = newTrigger.from.toString();
+      }
+      if (typeof newTrigger.to === "number") {
+        newTrigger.to = newTrigger.to.toString();
+      }
+    }
+
+    delete newTrigger.attribute_type;
 
     fireEvent(this, "value-changed", { value: newTrigger });
   }
