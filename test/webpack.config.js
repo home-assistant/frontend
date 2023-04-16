0372b389279f601c5c389fd76718a2ca97dfb880
Steve Repsher <steverep@users.noreply.github.com>
2023-04-16 03:54:15 +0000

    Revert "Disable runtime chunk for mocha tests"
    This reverts commit f4fa0ceef520eab757b3e5f13df84f1b56f209f7.

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -1,14 +1,8 @@
 import webpack from "../build-scripts/webpack.cjs";
 
-const config = webpack.createAppConfig({
+export default webpack.createAppConfig({
   isProdBuild: false,
   latestBuild: true,
   isStatsBuild: false,
-  isTestBuild: false,
+  isTestBuild: true,
 });
-
-// instant-mocha overrides output filename to "main.js",
-// so we need to turn off runtime chunk to avoid a Webpack error
-config.optimization.runtimeChunk = undefined;
-
-export default config;
