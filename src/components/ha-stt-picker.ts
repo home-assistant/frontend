24b91abd700e77c3fde9d44267256fd3ec4c9792
Bram Kragten <mail@bramkragten.nl>
2023-04-17 17:57:29 +0200

    Update ha-stt-picker.ts

--- src/components/ha-stt-picker.ts
+++ src/components/ha-stt-picker.ts
@@ -84,7 +84,12 @@ export class HaSTTPicker extends LitElement {
 
   private _changed(ev): void {
     const target = ev.target as HaSelect;
-    if (!this.hass || target.value === "") {
+    if (
+      !this.hass ||
+      target.value === "" ||
+      target.value === this.value ||
+      (this.value === undefined && target.value === DEFAULT)
+    ) {
       return;
     }
     this.value = target.value === DEFAULT ? undefined : target.value;
