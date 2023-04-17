2383510994e95a674de02e7f23d513b741e8bd3d
Bram Kragten <mail@bramkragten.nl>
2023-04-17 22:11:26 +0200

    Not required

--- src/panels/config/voice-assistants/dialog-voice-assistant-pipeline-detail.ts
+++ src/panels/config/voice-assistants/dialog-voice-assistant-pipeline-detail.ts
@@ -123,7 +123,6 @@ export class DialogVoiceAssistantPipelineDetail extends LitElement {
         },
         {
           name: "conversation_engine",
-          required: true,
           selector: {
             conversation_agent: {},
           },
@@ -137,7 +136,6 @@ export class DialogVoiceAssistantPipelineDetail extends LitElement {
         },
         {
           name: "stt_engine",
-          required: true,
           selector: {
             stt: {},
           },
@@ -145,7 +143,6 @@ export class DialogVoiceAssistantPipelineDetail extends LitElement {
         },
         {
           name: "tts_engine",
-          required: true,
           selector: {
             tts: {},
           },
