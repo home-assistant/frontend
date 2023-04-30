72647c03e4218daa10684a0eb72a0316f7f2a77c
Raman Gupta <7243222+raman325@users.noreply.github.com>
2023-04-30 14:56:36 -0400

    Remove unnecessary data models and cleanup stateToIsoDateString

--- src/data/date.ts
+++ src/data/date.ts
@@ -1,26 +1,8 @@
-import {
-  HassEntityAttributeBase,
-  HassEntityBase,
-} from "home-assistant-js-websocket";
+import { HassEntityBase } from "home-assistant-js-websocket";
 import { HomeAssistant } from "../types";
 
-interface DateEntityAttributes extends HassEntityAttributeBase {
-  year?: number;
-  month?: number;
-  day?: number;
-}
-
-export interface DateEntity extends HassEntityBase {
-  attributes: DateEntityAttributes;
-}
-
-export const stateToIsoDateString = (entityState: DateEntity) =>
-  `${entityState.attributes.year || "1970"}-${String(
-    entityState.attributes.month || "01"
-  ).padStart(2, "0")}-${String(entityState.attributes.day || "01").padStart(
-    2,
-    "0"
-  )}T00:00:00`;
+export const stateToIsoDateString = (entityState: HassEntityBase) =>
+  `${entityState}T00:00:00`;
 
 export const setDateValue = (
   hass: HomeAssistant,
