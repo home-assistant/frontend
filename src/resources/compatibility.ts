748f85980139eefb71d188c519e1dc4a98b769d6
Bram Kragten <mail@bramkragten.nl>
2023-05-04 14:56:13 +0200

    remove globalthis-polyfill

--- src/resources/compatibility.ts
+++ src/resources/compatibility.ts
@@ -1,7 +1,6 @@
 // Caution before editing - For latest builds, this module is replaced with emptiness and thus not imported (see build-scripts/bundle.js)
 import "core-js";
 import "regenerator-runtime/runtime";
-import "globalthis-polyfill";
 import "lit/polyfill-support";
 
 // For localize & formatting
@@ -65,5 +64,29 @@ if (Element.prototype.getAttributeNames === undefined) {
       result[i] = attributes[i].name;
     }
     return result;
+  };
+}
+
+// Source: https://gist.github.com/rebelchris/365f26f95d7e9f432f64f21886d9b9ef
+if (!Element.prototype.toggleAttribute) {
+  Element.prototype.toggleAttribute = function (name, force) {
+    if (force !== undefined) {
+      force = !!force;
+    }
+
+    if (this.hasAttribute(name)) {
+      if (force) {
+        return true;
+      }
+
+      this.removeAttribute(name);
+      return false;
+    }
+    if (force === false) {
+      return false;
+    }
+
+    this.setAttribute(name, "");
+    return true;
   };
 }
