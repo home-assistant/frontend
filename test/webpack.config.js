012b5b44a54d1544cb99ef84a702f3764cb6184c
Bram Kragten <mail@bramkragten.nl>
2023-05-30 23:56:38 +0200

    Revert "Use ES modules and dynamic import for Webpack latest builds (… (#16679)
    Revert "Use ES modules and dynamic import for Webpack latest builds (#16620)"
    
    This reverts commit 25e3977ad4275ded7bdfca5a246bc07b6a9d3761.

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -1,13 +1,8 @@
 import webpack from "../build-scripts/webpack.cjs";
 
-const config = webpack.createAppConfig({
+export default webpack.createAppConfig({
   isProdBuild: false,
   latestBuild: true,
   isStatsBuild: false,
   isTestBuild: true,
 });
-
-// instant-mocha forces a CJS library, so cannot output ESM
-config.output.module = false;
-
-export default config;
