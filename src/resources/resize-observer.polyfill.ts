ca786189eda3a551769be5e2cc7d7f40e16a6dc4
Bram Kragten <mail@bramkragten.nl>
2023-06-06 21:33:58 +0200

    Update resize-observer.polyfill.ts

--- src/resources/resize-observer.polyfill.ts
+++ src/resources/resize-observer.polyfill.ts
@@ -2,7 +2,6 @@ export const loadPolyfillIfNeeded = async () => {
   try {
     // eslint-disable-next-line no-new
     new ResizeObserver(() => {});
-    return;
   } catch (e) {
     window.ResizeObserver = (
       await import(
