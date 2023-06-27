58065bafdc62a0e2ebb8a07a36f9312946e3142e
Bram Kragten <mail@bramkragten.nl>
2023-06-27 18:22:02 +0200

    Add service response support to dev tools (#17044)
    * Add service response support to dev tools
    
    * Change to `response_variable`

--- src/data/script.ts
+++ src/data/script.ts
@@ -116,6 +116,7 @@ export interface ServiceAction extends BaseAction {
   entity_id?: string;
   target?: HassServiceTarget;
   data?: Record<string, unknown>;
+  response_variable?: string;
 }
 
 export interface DeviceAction extends BaseAction {
@@ -221,6 +222,7 @@ export interface VariablesAction extends BaseAction {
 
 export interface StopAction extends BaseAction {
   stop: string;
+  response_variable?: string;
   error?: boolean;
 }
 
