8ce9738f545e15f1990894aea13b9737b87a86bf
Bram Kragten <mail@bramkragten.nl>
2023-06-28 14:09:02 +0200

    Refactor integration card (#17061)

--- src/panels/config/integrations/ha-integration-header.ts
+++ src/panels/config/integrations/ha-integration-header.ts
@@ -1,11 +1,7 @@
-import "@lrnwebcomponents/simple-tooltip/simple-tooltip";
-import { mdiCloud, mdiPackageVariant } from "@mdi/js";
-import { css, html, LitElement, TemplateResult } from "lit";
+import { LitElement, TemplateResult, css, html } from "lit";
 import { customElement, property } from "lit/decorators";
-import { classMap } from "lit/directives/class-map";
-import { computeRTL } from "../../../common/util/compute_rtl";
 import "../../../components/ha-svg-icon";
-import { domainToName, IntegrationManifest } from "../../../data/integration";
+import { IntegrationManifest, domainToName } from "../../../data/integration";
 import { HomeAssistant } from "../../../types";
 import { brandsUrl } from "../../../util/brands-url";
 
@@ -22,8 +18,6 @@ export class HaIntegrationHeader extends LitElement {
   @property() public domain!: string;
 
   @property({ attribute: false }) public manifest?: IntegrationManifest;
-
-  @property({ attribute: false }) public debugLoggingEnabled?: boolean;
 
   protected render(): TemplateResult {
     let primary: string;
@@ -43,31 +37,8 @@ export class HaIntegrationHeader extends LitElement {
       primary = domainName;
     }
 
-    const icons: [string, string][] = [];
-
-    if (this.manifest) {
-      if (!this.manifest.is_built_in) {
-        icons.push([
-          mdiPackageVariant,
-          this.hass.localize(
-            "ui.panel.config.integrations.config_entry.custom_integration"
-          ),
-        ]);
-      }
-
-      if (this.manifest.iot_class?.startsWith("cloud_")) {
-        icons.push([
-          mdiCloud,
-          this.hass.localize(
-            "ui.panel.config.integrations.config_entry.depends_on_cloud"
-          ),
-        ]);
-      }
-    }
-
     return html`
       ${!this.banner ? "" : html`<div class="banner">${this.banner}</div>`}
-      <slot name="above-header"></slot>
       <div class="header">
         <img
           alt=""
@@ -80,32 +51,6 @@ export class HaIntegrationHeader extends LitElement {
           @error=${this._onImageError}
           @load=${this._onImageLoad}
         />
-        ${icons.length === 0
-          ? ""
-          : html`
-              <div
-                class="icons ${classMap({
-                  double: icons.length > 1,
-                  cloud: Boolean(
-                    this.manifest?.iot_class?.startsWith("cloud_")
-                  ),
-                })}"
-              >
-                ${icons.map(
-                  ([icon, description]) => html`
-                    <span>
-                      <ha-svg-icon .path=${icon}></ha-svg-icon>
-                      <simple-tooltip
-                        animation-delay="0"
-                        .position=${computeRTL(this.hass) ? "left" : "right"}
-                        offset="4"
-                        >${description}</simple-tooltip
-                      >
-                    </span>
-                  `
-                )}
-              </div>
-            `}
         <div class="info">
           <div class="primary" role="heading">${primary}</div>
           <div class="secondary">${secondary}</div>
@@ -139,14 +84,13 @@ export class HaIntegrationHeader extends LitElement {
     .header {
       display: flex;
       position: relative;
-      padding-top: 0px;
-      padding-bottom: 8px;
+      padding-top: 16px;
+      padding-bottom: 16px;
       padding-inline-start: 16px;
       padding-inline-end: 8px;
       direction: var(--direction);
     }
     .header img {
-      margin-top: 16px;
       margin-inline-start: initial;
       margin-inline-end: 16px;
       width: 40px;
@@ -166,11 +110,13 @@ export class HaIntegrationHeader extends LitElement {
       text-overflow: ellipsis;
     }
     .header-button {
-      margin-top: 8px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 36px;
     }
     .primary {
       font-size: 16px;
-      margin-top: 16px;
       font-weight: 400;
       word-break: break-word;
       color: var(--primary-text-color);
@@ -178,41 +124,6 @@ export class HaIntegrationHeader extends LitElement {
     .secondary {
       font-size: 14px;
       color: var(--secondary-text-color);
-    }
-    .icons {
-      background: var(--warning-color);
-      border: 1px solid var(--card-background-color);
-      border-radius: 14px;
-      color: var(--text-primary-color);
-      position: absolute;
-      left: 40px;
-      top: 40px;
-      display: flex;
-      padding: 4px;
-      inset-inline-start: 40px;
-      inset-inline-end: initial;
-    }
-    .icons.cloud {
-      background: var(--info-color);
-    }
-    .icons.double {
-      background: var(--warning-color);
-      left: 28px;
-      inset-inline-start: 28px;
-      inset-inline-end: initial;
-    }
-    .icons ha-svg-icon {
-      width: 16px;
-      height: 16px;
-      display: block;
-    }
-    .icons span:not(:first-child) ha-svg-icon {
-      margin-left: 4px;
-      margin-inline-start: 4px;
-      margin-inline-end: inherit;
-    }
-    simple-tooltip {
-      white-space: nowrap;
     }
   `;
 }
