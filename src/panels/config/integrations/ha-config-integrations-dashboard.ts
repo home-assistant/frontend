67c941a369b22dbc241e24a6aacb0e2b9da48fa8
Bram Kragten <mail@bramkragten.nl>
2023-06-29 12:31:20 +0200

    auto-fill

--- src/panels/config/integrations/ha-config-integrations-dashboard.ts
+++ src/panels/config/integrations/ha-config-integrations-dashboard.ts
@@ -745,13 +745,12 @@ class HaConfigIntegrationsDashboard extends SubscribeMixin(LitElement) {
         }
         .container {
           display: grid;
-          grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+          grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
           grid-gap: 8px 8px;
           padding: 8px 16px 16px;
           margin-bottom: 64px;
         }
         .container > * {
-          max-width: 500px;
           grid-column-start: 1;
         }
         .ignored-item ~ .ignored-item,
