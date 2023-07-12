f84609152ae7f3f9c21a419573dbfa5a8ec40fb0
Erik <erik@montnemery.com>
2023-07-12 15:42:06 +0200

    Allow changing channel from the thread configuration panel

--- src/data/otbr.ts
+++ src/data/otbr.ts
@@ -3,6 +3,7 @@ import { HomeAssistant } from "../types";
 export interface OTBRInfo {
   url: string;
   active_dataset_tlvs: string;
+  channel: number;
 }
 
 export const getOTBRInfo = (hass: HomeAssistant): Promise<OTBRInfo> =>
@@ -29,4 +30,13 @@ export const OTBRGetExtendedAddress = (
 ): Promise<{ extended_address: string }> =>
   hass.callWS({
     type: "otbr/get_extended_address",
+  });
+
+export const OTBRSetChannel = (
+  hass: HomeAssistant,
+  channel: number
+): Promise<{ delay: number }> =>
+  hass.callWS({
+    type: "otbr/set_channel",
+    channel,
   });
