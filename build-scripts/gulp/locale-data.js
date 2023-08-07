58ecf4d6df4263b2f6cb57b3f619aa4b5a537a51
Steve Repsher <steverep@users.noreply.github.com>
2023-08-07 05:59:02 -0400

    Minify and compress Intl locale data (#17506)

--- build-scripts/gulp/locale-data.js
+++ build-scripts/gulp/locale-data.js
@@ -1,18 +1,12 @@
 import { deleteSync } from "del";
-import fs from "fs";
+import { mkdir, readFile, writeFile } from "fs/promises";
 import gulp from "gulp";
 import path from "path";
 import paths from "../paths.cjs";
 
-const outDir = "build/locale-data";
+const outDir = path.join(paths.build_dir, "locale-data");
 
-gulp.task("clean-locale-data", async () => deleteSync([outDir]));
-
-gulp.task("ensure-locale-data-build-dir", async () => {
-  fs.mkdirSync(outDir, { recursive: true });
-});
-
-const modules = {
+const INTL_PACKAGES = {
   "intl-relativetimeformat": "RelativeTimeFormat",
   "intl-datetimeformat": "DateTimeFormat",
   "intl-numberformat": "NumberFormat",
@@ -20,53 +14,60 @@ const modules = {
   "intl-listformat": "ListFormat",
 };
 
-gulp.task("create-locale-data", (done) => {
+const convertToJSON = async (pkg, lang) => {
+  let localeData;
+  try {
+    localeData = await readFile(
+      path.resolve(
+        paths.polymer_dir,
+        `node_modules/@formatjs/${pkg}/locale-data/${lang}.js`
+      ),
+      "utf-8"
+    );
+  } catch (e) {
+    // Ignore if language is missing (i.e. not supported by @formatjs)
+    if (e.code === "ENOENT") {
+      return;
+    } else {
+      throw e;
+    }
+  }
+  // Convert to JSON
+  const className = INTL_PACKAGES[pkg];
+  localeData = localeData
+    .replace(
+      new RegExp(
+        `\\/\\*\\s*@generated\\s*\\*\\/\\s*\\/\\/\\s*prettier-ignore\\s*if\\s*\\(Intl\\.${className}\\s*&&\\s*typeof\\s*Intl\\.${className}\\.__addLocaleData\\s*===\\s*'function'\\)\\s*{\\s*Intl\\.${className}\\.__addLocaleData\\(`,
+        "im"
+      ),
+      ""
+    )
+    .replace(/\)\s*}/im, "");
+  // Parse to validate JSON, then stringify to minify
+  localeData = JSON.stringify(JSON.parse(localeData));
+  await writeFile(path.join(outDir, `${pkg}/${lang}.json`), localeData);
+};
+
+gulp.task("clean-locale-data", async () => deleteSync([outDir]));
+
+gulp.task("create-locale-data", async () => {
   const translationMeta = JSON.parse(
-    fs.readFileSync(
-      path.join(paths.translations_src, "translationMetadata.json")
+    await readFile(
+      path.resolve(paths.translations_src, "translationMetadata.json"),
+      "utf-8"
     )
   );
-  Object.entries(modules).forEach(([module, className]) => {
-    Object.keys(translationMeta).forEach((lang) => {
-      try {
-        const localeData = fs
-          .readFileSync(
-            path.resolve(
-              paths.polymer_dir,
-              `node_modules/@formatjs/${module}/locale-data/${lang}.js`
-            ),
-            "utf-8"
-          )
-          .replace(
-            new RegExp(
-              `\\/\\*\\s*@generated\\s*\\*\\/\\s*\\/\\/\\s*prettier-ignore\\s*if\\s*\\(Intl\\.${className}\\s*&&\\s*typeof\\s*Intl\\.${className}\\.__addLocaleData\\s*===\\s*'function'\\)\\s*{\\s*Intl\\.${className}\\.__addLocaleData\\(`,
-              "im"
-            ),
-            ""
-          )
-          .replace(/\)\s*}/im, "");
-        // make sure we have valid JSON
-        JSON.parse(localeData);
-        fs.mkdirSync(path.join(outDir, module), { recursive: true });
-        fs.writeFileSync(
-          path.join(outDir, `${module}/${lang}.json`),
-          localeData
-        );
-      } catch (e) {
-        if (e.code !== "ENOENT") {
-          throw e;
-        }
-      }
-    });
-    done();
-  });
+  const conversions = [];
+  for (const pkg of Object.keys(INTL_PACKAGES)) {
+    await mkdir(path.join(outDir, pkg), { recursive: true });
+    for (const lang of Object.keys(translationMeta)) {
+      conversions.push(convertToJSON(pkg, lang));
+    }
+  }
+  await Promise.all(conversions);
 });
 
 gulp.task(
   "build-locale-data",
-  gulp.series(
-    "clean-locale-data",
-    "ensure-locale-data-build-dir",
-    "create-locale-data"
-  )
+  gulp.series("clean-locale-data", "create-locale-data")
 );
