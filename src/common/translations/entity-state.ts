da44c85efef21dd5522d419ec69ca5a29209f26d
Paul Bottein <paul.bottein@gmail.com>
2023-08-10 09:57:56 +0200

    Add format state/attribute to hass (#17249)

---
+++ src/common/translations/entity-state.ts
@@ -0,0 +1,47 @@
+import type { HassConfig, HassEntity } from "home-assistant-js-websocket";
+import type { FrontendLocaleData } from "../../data/translation";
+import type { HomeAssistant } from "../../types";
+import type { LocalizeFunc } from "./localize";
+
+export type FormatEntityStateFunc = {
+  formatEntityState: (stateObj: HassEntity, state?: string) => string;
+  formatEntityAttributeValue: (
+    stateObj: HassEntity,
+    attribute: string,
+    value?: any
+  ) => string;
+  formatEntityAttributeName: (
+    stateObj: HassEntity,
+    attribute: string
+  ) => string;
+};
+
+export const computeFormatFunctions = async (
+  localize: LocalizeFunc,
+  locale: FrontendLocaleData,
+  config: HassConfig,
+  entities: HomeAssistant["entities"]
+): Promise<FormatEntityStateFunc> => {
+  const { computeStateDisplay } = await import(
+    "../entity/compute_state_display"
+  );
+  const { computeAttributeValueDisplay, computeAttributeNameDisplay } =
+    await import("../entity/compute_attribute_display");
+
+  return {
+    formatEntityState: (stateObj, state) =>
+      computeStateDisplay(localize, stateObj, locale, config, entities, state),
+    formatEntityAttributeValue: (stateObj, attribute, value) =>
+      computeAttributeValueDisplay(
+        localize,
+        stateObj,
+        locale,
+        config,
+        entities,
+        attribute,
+        value
+      ),
+    formatEntityAttributeName: (stateObj, attribute) =>
+      computeAttributeNameDisplay(localize, stateObj, entities, attribute),
+  };
+};
