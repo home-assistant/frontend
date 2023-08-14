b2cce65eccbe8b9004249f2def099b8b786eb8b0
karwosts <32912880+karwosts@users.noreply.github.com>
2023-08-14 00:39:25 -0700

    Fix multiline select text in zwave info (#17564)

--- src/panels/config/devices/device-detail/integration-elements/zwave_js/ha-device-info-zwave_js.ts
+++ src/panels/config/devices/device-detail/integration-elements/zwave_js/ha-device-info-zwave_js.ts
@@ -93,70 +93,76 @@ export class HaDeviceInfoZWaveJS extends SubscribeMixin(LitElement) {
           "ui.panel.config.zwave_js.device_info.zwave_info"
         )}
       >
-        ${this._multipleConfigEntries
-          ? html`
-              <div>
-                ${this.hass.localize("ui.panel.config.zwave_js.common.source")}:
-                ${this._configEntry!.title}
-              </div>
-            `
-          : ""}
         <div>
-          ${this.hass.localize("ui.panel.config.zwave_js.device_info.node_id")}:
-          ${this._node.node_id}
+          ${this._multipleConfigEntries
+            ? html`
+                <div>
+                  ${this.hass.localize(
+                    "ui.panel.config.zwave_js.common.source"
+                  )}:
+                  ${this._configEntry!.title}
+                </div>
+              `
+            : nothing}
+          <div>
+            ${this.hass.localize(
+              "ui.panel.config.zwave_js.device_info.node_id"
+            )}:
+            ${this._node.node_id}
+          </div>
+          ${!this._node.is_controller_node
+            ? html`
+                <div>
+                  ${this.hass.localize(
+                    "ui.panel.config.zwave_js.device_info.node_status"
+                  )}:
+                  ${this.hass.localize(
+                    `ui.panel.config.zwave_js.node_status.${
+                      nodeStatus[this._node.status]
+                    }`
+                  )}
+                </div>
+                <div>
+                  ${this.hass.localize(
+                    "ui.panel.config.zwave_js.device_info.node_ready"
+                  )}:
+                  ${this._node.ready
+                    ? this.hass.localize("ui.common.yes")
+                    : this.hass.localize("ui.common.no")}
+                </div>
+                <div>
+                  ${this.hass.localize(
+                    "ui.panel.config.zwave_js.device_info.highest_security"
+                  )}:
+                  ${this._node.highest_security_class !== null
+                    ? this.hass.localize(
+                        `ui.panel.config.zwave_js.security_classes.${
+                          SecurityClass[this._node.highest_security_class]
+                        }.title`
+                      )
+                    : this._node.is_secure === false
+                    ? this.hass.localize(
+                        "ui.panel.config.zwave_js.security_classes.none.title"
+                      )
+                    : this.hass.localize(
+                        "ui.panel.config.zwave_js.device_info.unknown"
+                      )}
+                </div>
+                <div>
+                  ${this.hass.localize(
+                    "ui.panel.config.zwave_js.device_info.zwave_plus"
+                  )}:
+                  ${this._node.zwave_plus_version
+                    ? this.hass.localize(
+                        "ui.panel.config.zwave_js.device_info.zwave_plus_version",
+                        "version",
+                        this._node.zwave_plus_version
+                      )
+                    : this.hass.localize("ui.common.no")}
+                </div>
+              `
+            : nothing}
         </div>
-        ${!this._node.is_controller_node
-          ? html`
-              <div>
-                ${this.hass.localize(
-                  "ui.panel.config.zwave_js.device_info.node_status"
-                )}:
-                ${this.hass.localize(
-                  `ui.panel.config.zwave_js.node_status.${
-                    nodeStatus[this._node.status]
-                  }`
-                )}
-              </div>
-              <div>
-                ${this.hass.localize(
-                  "ui.panel.config.zwave_js.device_info.node_ready"
-                )}:
-                ${this._node.ready
-                  ? this.hass.localize("ui.common.yes")
-                  : this.hass.localize("ui.common.no")}
-              </div>
-              <div>
-                ${this.hass.localize(
-                  "ui.panel.config.zwave_js.device_info.highest_security"
-                )}:
-                ${this._node.highest_security_class !== null
-                  ? this.hass.localize(
-                      `ui.panel.config.zwave_js.security_classes.${
-                        SecurityClass[this._node.highest_security_class]
-                      }.title`
-                    )
-                  : this._node.is_secure === false
-                  ? this.hass.localize(
-                      "ui.panel.config.zwave_js.security_classes.none.title"
-                    )
-                  : this.hass.localize(
-                      "ui.panel.config.zwave_js.device_info.unknown"
-                    )}
-              </div>
-              <div>
-                ${this.hass.localize(
-                  "ui.panel.config.zwave_js.device_info.zwave_plus"
-                )}:
-                ${this._node.zwave_plus_version
-                  ? this.hass.localize(
-                      "ui.panel.config.zwave_js.device_info.zwave_plus_version",
-                      "version",
-                      this._node.zwave_plus_version
-                    )
-                  : this.hass.localize("ui.common.no")}
-              </div>
-            `
-          : ""}
       </ha-expansion-panel>
     `;
   }
