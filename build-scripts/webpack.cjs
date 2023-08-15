ce09a891378c97457a7ca5612f11413010c6119c
Steve Repsher <steverep@users.noreply.github.com>
2023-08-15 14:53:39 +0000

    Fix source URLs in source maps

--- build-scripts/webpack.cjs
+++ build-scripts/webpack.cjs
@@ -1,5 +1,6 @@
-const webpack = require("webpack");
+const { existsSync } = require("fs");
 const path = require("path");
+const webpack = require("webpack");
 const TerserPlugin = require("terser-webpack-plugin");
 const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
 const log = require("fancy-log");
@@ -191,19 +192,23 @@ const createWebpackConfig = ({
       // Since production source maps don't include sources, we need to point to them elsewhere
       // For dependencies, just provide the path (no source in browser)
       // Otherwise, point to the raw code on GitHub for browser to load
-      devtoolModuleFilenameTemplate:
-        !isTestBuild && isProdBuild
-          ? (info) => {
-              const sourcePath = info.resourcePath.replace(/^\.\//, "");
-              if (
-                sourcePath.startsWith("node_modules") ||
-                sourcePath.startsWith("webpack")
-              ) {
-                return `no-source/${sourcePath}`;
+      ...Object.fromEntries(
+        ["", "Fallback"].map((v) => [
+          `devtool${v}ModuleFilenameTemplate`,
+          !isTestBuild && isProdBuild
+            ? (info) => {
+                if (
+                  !path.isAbsolute(info.absoluteResourcePath) ||
+                  !existsSync(info.resourcePath) ||
+                  info.resourcePath.startsWith("./node_modules")
+                ) {
+                  return new URL(info.resourcePath, "webpack://frontend/").href;
+                }
+                return new URL(info.resourcePath, bundle.sourceMapURL()).href;
               }
-              return `${bundle.sourceMapURL()}/${sourcePath}`;
-            }
-          : undefined,
+            : undefined,
+        ])
+      ),
     },
     experiments: {
       outputModule: true,
