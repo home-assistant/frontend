1ddad3dd5a444630856d6738d164d6c7586b4de3
Erik <erik@montnemery.com>
2023-08-23 12:05:51 +0200

    Add preview support to binary sensor group

--- src/data/group.ts
+++ src/data/group.ts
@@ -43,3 +43,20 @@ export const subscribePreviewGroupSensor = (
     flow_type,
     user_input,
   });
+
+export const subscribePreviewGroupBinarySensor = (
+  hass: HomeAssistant,
+  flow_id: string,
+  flow_type: "config_flow" | "options_flow",
+  user_input: Record<string, any>,
+  callback: (preview: {
+    state: string;
+    attributes: Record<string, any>;
+  }) => void
+): Promise<UnsubscribeFunc> =>
+  hass.connection.subscribeMessage(callback, {
+    type: "group/binary_sensor/start_preview",
+    flow_id,
+    flow_type,
+    user_input,
+  });
