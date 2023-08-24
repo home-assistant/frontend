b4975344a13633e480a923892fe73d3a94f2cec1
Simon Lamon <32477463+silamon@users.noreply.github.com>
2023-08-24 11:56:03 +0200

    Don't show a battery if the entity domain is Number (#17631)

--- src/dialogs/more-info/controls/more-info-vacuum.ts
+++ src/dialogs/more-info/controls/more-info-vacuum.ts
@@ -262,12 +262,13 @@ class MoreInfoVacuum extends LitElement {
     const battery = batteryEntity
       ? this.hass.states[batteryEntity.entity_id]
       : undefined;
-
-    const batteryIsBinary =
-      battery && computeStateDomain(battery) === "binary_sensor";
+    const batteryDomain = battery ? computeStateDomain(battery) : undefined;
 
     // Use device battery entity
-    if (battery && (batteryIsBinary || !isNaN(battery.state as any))) {
+    if (
+      battery &&
+      (batteryDomain === "binary_sensor" || !isNaN(battery.state as any))
+    ) {
       const batteryChargingEntity = findBatteryChargingEntity(
         this.hass,
         entities
@@ -279,7 +280,9 @@ class MoreInfoVacuum extends LitElement {
       return html`
         <div>
           <span>
-            ${batteryIsBinary ? "" : this.hass.formatEntityState(battery)}
+            ${batteryDomain === "sensor"
+              ? this.hass.formatEntityState(battery)
+              : nothing}
             <ha-battery-icon
               .hass=${this.hass}
               .batteryStateObj=${battery}
