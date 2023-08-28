72ba6a7121532ade7da5d90702b0b382f5f8e410
karwosts <karwosts@gmail.com>
2023-08-28 15:17:54 -0700

    alternative fix

--- src/components/ha-language-picker.ts
+++ src/components/ha-language-picker.ts
@@ -32,8 +32,6 @@ export class HaLanguagePicker extends LitElement {
 
   @state() _defaultLanguages: string[] = [];
 
-  @state() redrawFlag = false;
-
   @query("ha-select") private _select!: HaSelect;
 
   protected firstUpdated(changedProps: PropertyValues) {
@@ -43,18 +41,18 @@ export class HaLanguagePicker extends LitElement {
 
   protected updated(changedProperties: PropertyValues) {
     super.updated(changedProperties);
-    this.redrawFlag = false;
-    if (changedProperties.has("hass")) {
-      const oldHass = changedProperties.get("hass");
-      if (
-        this.hass &&
-        oldHass &&
-        oldHass.locale.language !== this.hass.locale.language
-      ) {
-        this.redrawFlag = true;
-      }
-    }
-    if (changedProperties.has("languages") || changedProperties.has("value")) {
+
+    const localeChanged =
+      changedProperties.has("hass") &&
+      this.hass &&
+      changedProperties.get("hass") &&
+      changedProperties.get("hass").locale.language !==
+        this.hass.locale.language;
+    if (
+      changedProperties.has("languages") ||
+      changedProperties.has("value") ||
+      localeChanged
+    ) {
       this._select.layoutOptions();
       if (this._select.value !== this.value) {
         fireEvent(this, "value-changed", { value: this._select.value });
@@ -67,11 +65,14 @@ export class HaLanguagePicker extends LitElement {
         this.hass.locale,
         this.nativeName
       );
-      const selectedItem = languageOptions.find(
+      const selectedItemIndex = languageOptions.findIndex(
         (option) => option.value === this.value
       );
-      if (!selectedItem) {
+      if (selectedItemIndex === -1) {
         this.value = undefined;
+      }
+      if (localeChanged) {
+        this._select.select(selectedItemIndex);
       }
     }
   }
@@ -133,9 +134,8 @@ export class HaLanguagePicker extends LitElement {
       this.nativeName
     );
 
-    const value = this.redrawFlag
-      ? ""
-      : this.value ?? (this.required ? languageOptions[0]?.value : this.value);
+    const value =
+      this.value ?? (this.required ? languageOptions[0]?.value : this.value);
 
     return html`
       <ha-select
