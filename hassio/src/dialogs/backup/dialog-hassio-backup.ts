c485e8d03e5cdbe7e5bb2971b545c81a4f67d730
Bram Kragten <mail@bramkragten.nl>
2023-08-30 13:24:25 +0200

    Update onboarding (#17734)
    Co-authored-by: Paul Bottein <paul.bottein@gmail.com>

--- hassio/src/dialogs/backup/dialog-hassio-backup.ts
+++ hassio/src/dialogs/backup/dialog-hassio-backup.ts
@@ -173,6 +173,7 @@ class HassioBackupDialog
   private async _restoreClicked() {
     const backupDetails = this._backupContent.backupDetails();
     this._restoringBackup = true;
+    this._dialogParams?.onRestoring?.();
     if (this._backupContent.backupType === "full") {
       await this._fullRestoreClicked(backupDetails);
     } else {
@@ -219,7 +220,7 @@ class HassioBackupDialog
         this._error = error.body.message;
       }
     } else {
-      fireEvent(this, "restoring");
+      this._dialogParams?.onRestoring?.();
       await fetch(`/api/hassio/backups/${this._backup!.slug}/restore/partial`, {
         method: "POST",
         body: JSON.stringify(backupDetails),
@@ -268,7 +269,7 @@ class HassioBackupDialog
         }
       );
     } else {
-      fireEvent(this, "restoring");
+      this._dialogParams?.onRestoring?.();
       fetch(`/api/hassio/backups/${this._backup!.slug}/restore/full`, {
         method: "POST",
         body: JSON.stringify(backupDetails),
