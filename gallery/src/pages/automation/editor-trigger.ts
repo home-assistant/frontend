0585c7b80363ad62da47613ca8d7ab6df33e229d
tetele <tm.sandu@gmail.com>
2023-09-07 23:54:28 +0300

    Linting pass

--- gallery/src/pages/automation/editor-trigger.ts
+++ gallery/src/pages/automation/editor-trigger.ts
@@ -120,6 +120,8 @@ const SCHEMAS: { name: string; triggers: Trigger[] }[] = [
       {
         platform: "conversation",
         command: ["Turn on the lights", "Turn the lights on"],
+        response_success: "Lights are now on",
+        response_error: "Lights could not be turned on"
       },
     ],
   },
