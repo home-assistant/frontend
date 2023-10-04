242b018ece11f90090b3d3278f4188344b15be2e
Kendell R <KTibow@users.noreply.github.com>
2023-10-04 04:59:51 -0700

    Make the "icon next/prev" series load faster (#18087)

--- src/components/ha-icon-prev.ts
+++ src/components/ha-icon-prev.ts
@@ -4,17 +4,7 @@ import { HaSvgIcon } from "./ha-svg-icon";
 
 @customElement("ha-icon-prev")
 export class HaIconPrev extends HaSvgIcon {
-  public connectedCallback() {
-    super.connectedCallback();
-
-    // wait to check for direction since otherwise direction is wrong even though top level is RTL
-    setTimeout(() => {
-      this.path =
-        window.getComputedStyle(this).direction === "ltr"
-          ? mdiChevronLeft
-          : mdiChevronRight;
-    }, 100);
-  }
+  path = document.dir === "ltr" ? mdiChevronLeft : mdiChevronRight;
 }
 
 declare global {
