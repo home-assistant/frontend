1e375985983c836fbd5a03307f95ff19f73b9ca2
Paul Bottein <paul.bottein@gmail.com>
2023-10-05 11:26:50 +0200

    Add translations for restore backup dialogs (#18112)
    Co-authored-by: c0ffeeca7 <38767475+c0ffeeca7@users.noreply.github.com>

--- hassio/src/dialogs/backup/dialog-hassio-backup.ts
+++ hassio/src/dialogs/backup/dialog-hassio-backup.ts
@@ -31,6 +31,7 @@ import { fileDownload } from "../../../../src/util/file_download";
 import "../../components/supervisor-backup-content";
 import type { SupervisorBackupContent } from "../../components/supervisor-backup-content";
 import { HassioBackupDialogParams } from "./show-dialog-hassio-backup";
+import { BackupOrRestoreKey } from "../../util/translations";
 
 @customElement("dialog-hassio-backup")
 class HassioBackupDialog
@@ -64,6 +65,13 @@ class HassioBackupDialog
     fireEvent(this, "dialog-closed", { dialog: this.localName });
   }
 
+  private _localize(key: BackupOrRestoreKey) {
+    return (
+      this._dialogParams!.supervisor?.localize(`backup.${key}`) ||
+      this._dialogParams!.localize!(`ui.panel.page-onboarding.restore.${key}`)
+    );
+  }
+
   protected render() {
     if (!this._dialogParams || !this._backup) {
       return nothing;
@@ -79,7 +87,7 @@ class HassioBackupDialog
           <ha-header-bar>
             <span slot="title">${this._backup.name}</span>
             <ha-icon-button
-              .label=${this.hass?.localize("ui.common.close") || "Close"}
+              .label=${this._localize("close")}
               .path=${mdiClose}
               slot="actionItems"
               dialogAction="cancel"
@@ -87,29 +95,31 @@ class HassioBackupDialog
           </ha-header-bar>
         </div>
         ${this._restoringBackup
-          ? html` <ha-circular-progress active></ha-circular-progress>`
-          : html`<supervisor-backup-content
-              .hass=${this.hass}
-              .supervisor=${this._dialogParams.supervisor}
-              .backup=${this._backup}
-              .onboarding=${this._dialogParams.onboarding || false}
-              .localize=${this._dialogParams.localize}
-              dialogInitialFocus
-            >
-            </supervisor-backup-content>`}
+          ? html`<ha-circular-progress active></ha-circular-progress>`
+          : html`
+              <supervisor-backup-content
+                .hass=${this.hass}
+                .supervisor=${this._dialogParams.supervisor}
+                .backup=${this._backup}
+                .onboarding=${this._dialogParams.onboarding || false}
+                .localize=${this._dialogParams.localize}
+                dialogInitialFocus
+              >
+              </supervisor-backup-content>
+            `}
         ${this._error
           ? html`<ha-alert alert-type="error">${this._error}</ha-alert>`
-          : ""}
+          : nothing}
 
         <mwc-button
           .disabled=${this._restoringBackup}
           slot="secondaryAction"
           @click=${this._restoreClicked}
         >
-          Restore
+          ${this._localize("restore")}
         </mwc-button>
 
-        ${!this._dialogParams.onboarding
+        ${!this._dialogParams.onboarding && this._dialogParams.supervisor
           ? html`<ha-button-menu
               fixed
               slot="primaryAction"
@@ -117,22 +127,24 @@ class HassioBackupDialog
               @closed=${stopPropagation}
             >
               <ha-icon-button
-                .label=${this.hass!.localize("ui.common.menu") || "Menu"}
+                .label=${this._dialogParams.supervisor.localize(
+                  "backup.more_actions"
+                )}
                 .path=${mdiDotsVertical}
                 slot="trigger"
               ></ha-icon-button>
               <mwc-list-item
-                >${this._dialogParams.supervisor?.localize(
+                >${this._dialogParams.supervisor.localize(
                   "backup.download_backup"
                 )}</mwc-list-item
               >
               <mwc-list-item class="error"
-                >${this._dialogParams.supervisor?.localize(
+                >${this._dialogParams.supervisor.localize(
                   "backup.delete_backup_title"
                 )}</mwc-list-item
               >
             </ha-button-menu>`
-          : ""}
+          : nothing}
       </ha-dialog>
     `;
   }
@@ -183,21 +195,22 @@ class HassioBackupDialog
   }
 
   private async _partialRestoreClicked(backupDetails) {
-    if (
-      this._dialogParams?.supervisor !== undefined &&
-      this._dialogParams?.supervisor.info.state !== "running"
-    ) {
+    const supervisor = this._dialogParams?.supervisor;
+    if (supervisor !== undefined && supervisor.info.state !== "running") {
       await showAlertDialog(this, {
-        title: "Could not restore backup",
-        text: `Restoring a backup is not possible right now because the system is in ${this._dialogParams?.supervisor.info.state} state.`,
+        title: supervisor.localize("backup.could_not_restore"),
+        text: supervisor.localize("backup.restore_blocked_not_running", {
+          state: supervisor.info.state,
+        }),
       });
       return;
     }
     if (
       !(await showConfirmationDialog(this, {
-        title: "Are you sure you want to restore this partial backup?",
-        confirmText: "restore",
-        dismissText: "cancel",
+        title: this._localize("confirm_restore_partial_backup_title"),
+        text: this._localize("confirm_restore_partial_backup_text"),
+        confirmText: this._localize("restore"),
+        dismissText: this._localize("cancel"),
       }))
     ) {
       return;
@@ -230,22 +243,22 @@ class HassioBackupDialog
   }
 
   private async _fullRestoreClicked(backupDetails) {
-    if (
-      this._dialogParams?.supervisor !== undefined &&
-      this._dialogParams?.supervisor.info.state !== "running"
-    ) {
+    const supervisor = this._dialogParams?.supervisor;
+    if (supervisor !== undefined && supervisor.info.state !== "running") {
       await showAlertDialog(this, {
-        title: "Could not restore backup",
-        text: `Restoring a backup is not possible right now because the system is in ${this._dialogParams?.supervisor.info.state} state.`,
+        title: supervisor.localize("backup.could_not_restore"),
+        text: supervisor.localize("backup.restore_blocked_not_running", {
+          state: supervisor.info.state,
+        }),
       });
       return;
     }
     if (
       !(await showConfirmationDialog(this, {
-        title:
-          "Are you sure you want to wipe your system and restore this backup?",
-        confirmText: "restore",
-        dismissText: "cancel",
+        title: this._localize("confirm_restore_full_backup_title"),
+        text: this._localize("confirm_restore_full_backup_text"),
+        confirmText: this._localize("restore"),
+        dismissText: this._localize("cancel"),
       }))
     ) {
       return;
@@ -279,11 +292,15 @@ class HassioBackupDialog
   }
 
   private async _deleteClicked() {
+    const supervisor = this._dialogParams?.supervisor;
+    if (!supervisor) return;
+
     if (
       !(await showConfirmationDialog(this, {
-        title: "Are you sure you want to delete this backup?",
-        confirmText: "delete",
-        dismissText: "cancel",
+        title: supervisor!.localize("backup.confirm_delete_title"),
+        text: supervisor!.localize("backup.confirm_delete_text"),
+        confirmText: supervisor!.localize("backup.delete"),
+        dismissText: supervisor!.localize("backup.cancel"),
       }))
     ) {
       return;
@@ -301,6 +318,9 @@ class HassioBackupDialog
   }
 
   private async _downloadClicked() {
+    const supervisor = this._dialogParams?.supervisor;
+    if (!supervisor) return;
+
     let signedPath: { path: string };
     try {
       signedPath = await getSignedPath(
@@ -320,10 +340,10 @@ class HassioBackupDialog
 
     if (window.location.href.includes("ui.nabu.casa")) {
       const confirm = await showConfirmationDialog(this, {
-        title: "Potential slow download",
-        text: "Downloading backups over the Nabu Casa URL will take some time, it is recomended to use your local URL instead, do you want to continue?",
-        confirmText: "continue",
-        dismissText: "cancel",
+        title: supervisor.localize("backup.remote_download_title"),
+        text: supervisor.localize("backup.remote_download_text"),
+        confirmText: supervisor.localize("backup.download"),
+        dismissText: this._localize("cancel"),
       });
       if (!confirm) {
         return;
