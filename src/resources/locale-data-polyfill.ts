569c7cba45445406fb719cf060ddfc69864fdd75
Steve Repsher <steverep@users.noreply.github.com>
2023-10-09 08:28:16 -0400

    Move polyfill time zone data out of bundles (#18142)

--- src/resources/locale-data-polyfill.ts
+++ src/resources/locale-data-polyfill.ts
@@ -13,16 +13,17 @@ const loadedLocales: Set<string> = new Set();
 
 const addData = async (
   obj: (typeof INTL_POLYFILLS)[number],
-  language: string
+  language: string,
+  addFunc = "__addLocaleData"
 ) => {
   // Add function will only exist if constructor is polyfilled
-  if (typeof (Intl[obj] as any)?.__addLocaleData === "function") {
+  if (typeof (Intl[obj] as any)?.[addFunc] === "function") {
     const result = await fetch(
       `${__STATIC_PATH__}locale-data/intl-${obj.toLowerCase()}/${language}.json`
     );
     // Ignore if polyfill data does not exist for language
     if (result.ok) {
-      (Intl[obj] as any).__addLocaleData(await result.json());
+      (Intl[obj] as any)[addFunc](await result.json());
     }
   }
 };
@@ -34,3 +35,6 @@ export const polyfillLocaleData = async (language: string) => {
   loadedLocales.add(language);
   await Promise.all(INTL_POLYFILLS.map((obj) => addData(obj, language)));
 };
+
+export const polyfillTimeZoneData = () =>
+  addData("DateTimeFormat", "add-all-tz", "__addTZData");
