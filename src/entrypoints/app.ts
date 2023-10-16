3b2f6d71f5486a988073997222fbafe238e6cf3c
Steve Repsher <steverep@users.noreply.github.com>
2023-10-16 14:37:50 -0400

    Minify ha-style/Roboto and load asynchronously (#18226)

--- src/entrypoints/app.ts
+++ src/entrypoints/app.ts
@@ -4,8 +4,8 @@ import {
 } from "@polymer/polymer/lib/utils/settings";
 import "@webcomponents/scoped-custom-element-registry/scoped-custom-element-registry.min";
 import "../layouts/home-assistant";
-import "../resources/ha-style";
-import "../resources/roboto";
+
+import("../resources/ha-style");
 
 setPassiveTouchGestures(true);
 setCancelSyntheticClickEvents(false);
--- src/entrypoints/authorize.ts
+++ src/entrypoints/authorize.ts
@@ -2,9 +2,9 @@
 import "../resources/compatibility";
 import { setCancelSyntheticClickEvents } from "@polymer/polymer/lib/utils/settings";
 import "../auth/ha-authorize";
-import "../resources/ha-style";
-import "../resources/roboto";
 import "../resources/safari-14-attachshadow-patch";
 import "../resources/array.flat.polyfill";
+
+import("../resources/ha-style");
 
 setCancelSyntheticClickEvents(false);
