21e36261c6ba2f6ad0cadb2b4fec21d84f2fa68c
Steve Repsher <steverep@users.noreply.github.com>
2023-10-18 17:07:44 +0000

    Load and set Polymer settings asynchronously

--- src/entrypoints/app.ts
+++ src/entrypoints/app.ts
@@ -1,11 +1,10 @@
-import {
-  setPassiveTouchGestures,
-  setCancelSyntheticClickEvents,
-} from "@polymer/polymer/lib/utils/settings";
 import "@webcomponents/scoped-custom-element-registry/scoped-custom-element-registry.min";
 import "../layouts/home-assistant";
 
 import("../resources/ha-style");
-
-setPassiveTouchGestures(true);
-setCancelSyntheticClickEvents(false);
+import("@polymer/polymer/lib/utils/settings").then(
+  ({ setCancelSyntheticClickEvents, setPassiveTouchGestures }) => {
+    setCancelSyntheticClickEvents(false);
+    setPassiveTouchGestures(true);
+  }
+);
