ef203bce0d2137ae8ed8b227e89659f2ebe03216
Steve Repsher <steverep@users.noreply.github.com>
2023-10-19 11:09:35 -0400

    Load and set Polymer settings asynchronously (#18278)

--- src/entrypoints/authorize.ts
+++ src/entrypoints/authorize.ts
@@ -1,10 +1,10 @@
 // Compat needs to be first import
 import "../resources/compatibility";
-import { setCancelSyntheticClickEvents } from "@polymer/polymer/lib/utils/settings";
 import "../auth/ha-authorize";
 import "../resources/safari-14-attachshadow-patch";
 import "../resources/array.flat.polyfill";
 
 import("../resources/ha-style");
-
-setCancelSyntheticClickEvents(false);
+import("@polymer/polymer/lib/utils/settings").then(
+  ({ setCancelSyntheticClickEvents }) => setCancelSyntheticClickEvents(false)
+);
--- src/entrypoints/onboarding.ts
+++ src/entrypoints/onboarding.ts
@@ -1,13 +1,13 @@
 // Compat needs to be first import
 import "../resources/compatibility";
-import { setCancelSyntheticClickEvents } from "@polymer/polymer/lib/utils/settings";
 import "../onboarding/ha-onboarding";
 import "../resources/safari-14-attachshadow-patch";
 import "../resources/array.flat.polyfill";
 
 import("../resources/ha-style");
-
-setCancelSyntheticClickEvents(false);
+import("@polymer/polymer/lib/utils/settings").then(
+  ({ setCancelSyntheticClickEvents }) => setCancelSyntheticClickEvents(false)
+);
 
 declare global {
   interface Window {
