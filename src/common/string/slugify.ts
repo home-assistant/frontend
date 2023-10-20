cbbf8a6255dd7b74469c2272f08fa873f7c7319e
Caius-Bonus <123886836+Caius-Bonus@users.noreply.github.com>
2023-10-21 00:49:58 +0200

    just convert everything into the delimiter

--- src/common/string/slugify.ts
+++ src/common/string/slugify.ts
@@ -1,15 +1,15 @@
 // https://gist.github.com/hagemann/382adfc57adbd5af078dc93feef01fe1
 export const slugify = (value: string, delimiter = "_") => {
-  const a = "àáäâãåăæąçćčđďèéěėëêęğǵḧìíïîįłḿǹńňñòóöôœøṕŕřßşśšșťțùúüûǘůűūųẃẍÿýźžż·";
+  const a =
+    "àáäâãåăæąçćčđďèéěėëêęğǵḧìíïîįłḿǹńňñòóöôœøṕŕřßşśšșťțùúüûǘůűūųẃẍÿýźžż·";
   const b = `aaaaaaaaacccddeeeeeeegghiiiiilmnnnnooooooprrsssssttuuuuuuuuuwxyyzzz${delimiter}`;
   const p = new RegExp(a.split("").join("|"), "g");
 
   return value
     .toString()
     .toLowerCase()
-    .replace(/[\s\.|&+-/_,:;]+/g, delimiter) // Replace spaces with delimiter
     .replace(p, (c) => b.charAt(a.indexOf(c))) // Replace special characters
-    .replace(/[^\w]+/g, "") // Remove all non-word characters
+    .replace(/[^\w]+/g, delimiter) // Remove all non-word characters
     .replace(new RegExp(`(${delimiter})\\1+`, "g"), "$1") // Replace multiple delimiters with single delimiter
     .replace(new RegExp(`^${delimiter}+`), "") // Trim delimiter from start of text
     .replace(new RegExp(`${delimiter}+$`), ""); // Trim delimiter from end of text
