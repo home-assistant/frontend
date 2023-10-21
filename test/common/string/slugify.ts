37bba7d0bffddd011edee83e0088b6175fb4e540
Caius-Bonus <123886836+Caius-Bonus@users.noreply.github.com>
2023-10-21 02:33:24 +0200

    add test for empty string

--- test/common/string/slugify.ts
+++ test/common/string/slugify.ts
@@ -24,6 +24,7 @@ describe("slugify", () => {
   assert.strictEqual(slugify("abc---DEF---ghi"), "abc_def_ghi");
   assert.strictEqual(slugify("___abc___DEF___ghi___"), "abc_def_ghi");
   assert.strictEqual(slugify("___"), "unknown");
+  assert.strictEqual(slugify(""), "");
 
   // With custom delimiter
   assert.strictEqual(slugify("abc def", "-"), "abc-def");
