e52790b62580980c3ed8747c44560d5b1107e247
Caius-Bonus <123886836+Caius-Bonus@users.noreply.github.com>
2023-10-21 18:25:58 +0200

    refactor code, because I didn't like the linters suggestion

--- src/common/string/slugify.ts
+++ src/common/string/slugify.ts
@@ -5,20 +5,25 @@ export const slugify = (value: string, delimiter = "_") => {
   const b = `aaaaaaaaaacccddeeeeeeeegghiiiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz${delimiter}`;
   const p = new RegExp(a.split("").join("|"), "g");
 
-  const slugified = value
-    .toString()
-    .toLowerCase()
-    .replace(p, (c) => b.charAt(a.indexOf(c))) // Replace special characters
-    .replace(/(?<=\d),(?=\d)/g, "") // Remove Commas between numbers
-    .replace(/[^a-z0-9]+/g, delimiter) // Replace all non-word characters
-    .replace(new RegExp(`(${delimiter})\\1+`, "g"), "$1") // Replace multiple delimiters with single delimiter
-    .replace(new RegExp(`^${delimiter}+`), "") // Trim delimiter from start of text
-    .replace(new RegExp(`${delimiter}+$`), ""); // Trim delimiter from end of text
+  let slugified;
 
   if (value === "") {
-    return "";
-  } else if (slugified === "") {
-    return "unknown";
+    slugified = "";
+  } else {
+    slugified = value
+      .toString()
+      .toLowerCase()
+      .replace(p, (c) => b.charAt(a.indexOf(c))) // Replace special characters
+      .replace(/(?<=\d),(?=\d)/g, "") // Remove Commas between numbers
+      .replace(/[^a-z0-9]+/g, delimiter) // Replace all non-word characters
+      .replace(new RegExp(`(${delimiter})\\1+`, "g"), "$1") // Replace multiple delimiters with single delimiter
+      .replace(new RegExp(`^${delimiter}+`), "") // Trim delimiter from start of text
+      .replace(new RegExp(`${delimiter}+$`), ""); // Trim delimiter from end of text
+
+    if (slugified === "") {
+      slugified = "unknown";
+    }
   }
+
   return slugified;
 };
