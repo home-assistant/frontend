144cdd97baf6005529c188b0185fed7ce22bc10d
Paul Bottein <paul.bottein@gmail.com>
2023-10-23 16:17:23 +0200

    Add numeric state condition for conditional card (#18288)
    * Add numeric state condition for conditional card
    
    * Add validate ui
    
    * Clean entity data
    
    * Check for numeric state

--- src/panels/lovelace/common/icon-condition.ts
+++ src/panels/lovelace/common/icon-condition.ts
@@ -1,7 +1,13 @@
-import { mdiAccount, mdiResponsive, mdiStateMachine } from "@mdi/js";
+import {
+  mdiAccount,
+  mdiNumeric,
+  mdiResponsive,
+  mdiStateMachine,
+} from "@mdi/js";
 import { Condition } from "./validate-condition";
 
 export const ICON_CONDITION: Record<Condition["condition"], string> = {
+  numeric_state: mdiNumeric,
   state: mdiStateMachine,
   screen: mdiResponsive,
   user: mdiAccount,
