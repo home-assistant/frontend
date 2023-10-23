c6be4d6f4d51e6d90cb2478c4d8ad7ac4b4107be
Paul Bottein <paul.bottein@gmail.com>
2023-10-23 14:50:45 +0200

    Improve warning messages in conditional card (#18272)
    * Improve warning messages in conditional card
    
    * Update src/panels/lovelace/editor/conditions/ha-card-condition-editor.ts
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>
    
    ---------
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>

--- src/panels/lovelace/cards/types.ts
+++ src/panels/lovelace/cards/types.ts
@@ -1,7 +1,7 @@
 import { Statistic, StatisticType } from "../../../data/recorder";
 import { ActionConfig, LovelaceCardConfig } from "../../../data/lovelace";
 import { FullCalendarView, TranslationDict } from "../../../types";
-import { Condition } from "../common/validate-condition";
+import { Condition, LegacyCondition } from "../common/validate-condition";
 import { HuiImage } from "../components/hui-image";
 import { LovelaceElementConfig } from "../elements/types";
 import {
@@ -37,7 +37,7 @@ export interface CalendarCardConfig extends LovelaceCardConfig {
 
 export interface ConditionalCardConfig extends LovelaceCardConfig {
   card: LovelaceCardConfig;
-  conditions: Condition[];
+  conditions: (Condition | LegacyCondition)[];
 }
 
 export interface EmptyStateCardConfig extends LovelaceCardConfig {
