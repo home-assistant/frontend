71b6ab49f98e212613f696df1506a48e2d64a661
Erik <erik@montnemery.com>
2023-10-24 09:18:19 +0200

    Rename safe mode to recovery mode

--- src/fake_data/demo_config.ts
+++ src/fake_data/demo_config.ts
@@ -21,7 +21,7 @@ export const demoConfig: HassConfig = {
   allowlist_external_dirs: [],
   allowlist_external_urls: [],
   config_source: "storage",
-  safe_mode: false,
+  recovery_mode: false,
   state: STATE_RUNNING,
   internal_url: "http://homeassistant.local:8123",
   external_url: null,
