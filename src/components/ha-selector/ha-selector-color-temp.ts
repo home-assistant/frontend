a50aa8d76bad3ceb8e15cbc981d3cc2183a756b4
schelv <13403863+schelv@users.noreply.github.com>
2023-10-30 22:35:22 +0100

    wip

--- src/components/ha-selector/ha-selector-color-temp.ts
+++ src/components/ha-selector/ha-selector-color-temp.ts
@@ -4,6 +4,10 @@ import { fireEvent } from "../../common/dom/fire_event";
 import type { ColorTempSelector } from "../../data/selector";
 import type { HomeAssistant } from "../../types";
 import "../ha-labeled-slider";
+import { styleMap } from "lit/directives/style-map";
+import { generateColorTemperatureGradient } from "../../dialogs/more-info/components/lights/light-color-temp-picker";
+import memoizeOne from "memoize-one";
+import { mired2kelvin } from "../../common/color/convert-light-color";
 
 @customElement("ha-selector-color_temp")
 export class HaColorTempSelector extends LitElement {
@@ -20,22 +24,23 @@ export class HaColorTempSelector extends LitElement {
   @property({ type: Boolean, reflect: true }) public disabled = false;
 
   @property({ type: Boolean }) public required = true;
-  
+
   protected render() {
     const min = this.selector.color_temp?.min_mireds ?? 153;
     const max = this.selector.color_temp?.max_mireds ?? 500;
 
-    const firstPercentage = 100 * (153 - min) / 173.5;
-    const secondPercentage = 50 * (153 - min + 500 - max) / 173.5 + 50;
-    const thirdPercentage = 100 * (500 - max) / 173.5 + 100;
+
+    const gradient = this._generateTemperatureGradient(min, max);
+    // const firstPercentage = (100 * (153 - min)) / 173.5;
+    // const secondPercentage = (50 * (153 - min + 500 - max)) / 173.5 + 50;
+    // const thirdPercentage = (100 * (500 - max)) / 173.5 + 100;
 
     return html`
       <ha-labeled-slider
-        style="
-          --first-percentage: ${firstPercentage}%;
-          --second-percentage: ${secondPercentage}%;
-          --third-percentage: ${thirdPercentage}%;
-        "
+        style=${styleMap({
+          "--ha-slider-background": `linear-gradient( to var(--float-end), ${gradient})`,
+        })}
+  
         labeled
         icon="hass:thermometer"
         .caption=${this.label || ""}
@@ -50,22 +55,26 @@ export class HaColorTempSelector extends LitElement {
     `;
   }
 
+  private _generateTemperatureGradient = memoizeOne(
+    (min: number, max: number) => generateColorTemperatureGradient(mired2kelvin(max), mired2kelvin(min))
+  );
+
   private _valueChanged(ev: CustomEvent) {
     fireEvent(this, "value-changed", {
       value: Number((ev.detail as any).value),
     });
   }
 
-  static styles = css`
-    ha-labeled-slider {
-      --ha-slider-background: linear-gradient(
-        to var(--float-end),
-        rgb(255, 160, 0) var(--first-percentage),
-        white var(--second-percentage),
-        rgb(166, 209, 255) var(--third-percentage)
-      );
-    }
-  `;
+  // static styles = css`
+  //   ha-labeled-slider {
+  //     --ha-slider-background: linear-gradient(
+  //       to var(--float-end),
+  //       rgb(255, 160, 0) var(--first-percentage),
+  //       white var(--second-percentage),
+  //       rgb(166, 209, 255) var(--third-percentage)
+  //     );
+  //   }
+  // `;
 }
 
 declare global {
