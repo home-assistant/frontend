bff09a7a36efa51ad64b026b15acbd30654e66a7
karwosts <32912880+karwosts@users.noreply.github.com>
2023-11-13 10:45:34 -0800

    Fix login password form (#18619)

--- src/components/ha-form/ha-form-string.ts
+++ src/components/ha-form/ha-form-string.ts
@@ -25,7 +25,7 @@ const MASKED_FIELDS = ["password", "secret", "token"];
 
 @customElement("ha-form-string")
 export class HaFormString extends LitElement implements HaFormElement {
-  @property() public hass!: HomeAssistant;
+  @property() public hass?: HomeAssistant;
 
   @property() public schema!: HaFormStringSchema;
 
@@ -81,11 +81,11 @@ export class HaFormString extends LitElement implements HaFormElement {
     return html`
       <ha-icon-button
         toggles
-        .label=${this.hass.localize(
+        .label=${this.hass?.localize(
           this.unmaskedPassword
             ? "ui.components.selectors.text.hide_password"
             : "ui.components.selectors.text.show_password"
-        )}
+        ) || (this.unmaskedPassword ? "Hide password" : "Show password")}
         @click=${this.toggleUnmaskedPassword}
         .path=${this.unmaskedPassword ? mdiEyeOff : mdiEye}
       ></ha-icon-button>
