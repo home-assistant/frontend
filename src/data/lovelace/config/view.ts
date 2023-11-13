42eb25acdb8564734f1ea955637b4095466165fb
Paul Bottein <paul.bottein@gmail.com>
2023-11-13 14:44:32 +0100

    Split lovelace types into multiple files

---
+++ src/data/lovelace/config/view.ts
@@ -0,0 +1,40 @@
+import type { LovelaceBadgeConfig } from "./badge";
+import type { LovelaceCardConfig } from "./card";
+import type { LovelaceStrategyConfig } from "./strategy";
+
+export interface ShowViewConfig {
+  user?: string;
+}
+
+export interface LovelaceBaseViewConfig {
+  index?: number;
+  title?: string;
+  path?: string;
+  icon?: string;
+  theme?: string;
+  panel?: boolean;
+  background?: string;
+  visible?: boolean | ShowViewConfig[];
+  subview?: boolean;
+  back_path?: string;
+}
+
+export interface LovelaceViewConfig extends LovelaceBaseViewConfig {
+  type?: string;
+  badges?: Array<string | LovelaceBadgeConfig>;
+  cards?: LovelaceCardConfig[];
+}
+
+export interface LovelaceStrategyViewConfig extends LovelaceBaseViewConfig {
+  strategy: LovelaceStrategyConfig;
+}
+
+export type LovelaceViewRawConfig =
+  | LovelaceViewConfig
+  | LovelaceStrategyViewConfig;
+
+export function isStrategyView(
+  view: LovelaceViewRawConfig
+): view is LovelaceStrategyViewConfig {
+  return "strategy" in view;
+}
