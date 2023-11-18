a45eefa742c3bc18b1e8efd27017fa53e089660f
karwosts <32912880+karwosts@users.noreply.github.com>
2023-11-18 08:26:21 -0800

    Add live camera & custom AR to Area card (#18643)

--- src/panels/lovelace/editor/config-elements/hui-area-card-editor.ts
+++ src/panels/lovelace/editor/config-elements/hui-area-card-editor.ts
@@ -1,8 +1,10 @@
 import { html, LitElement, nothing } from "lit";
 import { customElement, property, state } from "lit/decorators";
+import memoizeOne from "memoize-one";
 import { assert, assign, boolean, object, optional, string } from "superstruct";
 import { fireEvent } from "../../../../common/dom/fire_event";
 import "../../../../components/ha-form/ha-form";
+import { DEFAULT_ASPECT_RATIO } from "../../cards/hui-area-card";
 import type { SchemaUnion } from "../../../../components/ha-form/types";
 import type { HomeAssistant } from "../../../../types";
 import type { AreaCardConfig } from "../../cards/types";
@@ -16,26 +18,10 @@ const cardConfigStruct = assign(
     navigation_path: optional(string()),
     theme: optional(string()),
     show_camera: optional(boolean()),
+    camera_view: optional(string()),
+    aspect_ratio: optional(string()),
   })
 );
-
-const SCHEMA = [
-  { name: "area", selector: { area: {} } },
-  { name: "show_camera", required: false, selector: { boolean: {} } },
-  {
-    name: "",
-    type: "grid",
-    schema: [
-      {
-        name: "navigation_path",
-        required: false,
-        selector: { navigation: {} },
-      },
-      { name: "theme", required: false, selector: { theme: {} } },
-    ],
-  },
-] as const;
-
 @customElement("hui-area-card-editor")
 export class HuiAreaCardEditor
   extends LitElement
@@ -44,6 +30,39 @@ export class HuiAreaCardEditor
   @property({ attribute: false }) public hass?: HomeAssistant;
 
   @state() private _config?: AreaCardConfig;
+
+  private _schema = memoizeOne(
+    (showCamera: boolean) =>
+      [
+        { name: "area", selector: { area: {} } },
+        { name: "show_camera", required: false, selector: { boolean: {} } },
+        ...(showCamera
+          ? ([
+              {
+                name: "camera_view",
+                selector: { select: { options: ["auto", "live"] } },
+              },
+            ] as const)
+          : []),
+        {
+          name: "",
+          type: "grid",
+          schema: [
+            {
+              name: "navigation_path",
+              required: false,
+              selector: { navigation: {} },
+            },
+            { name: "theme", required: false, selector: { theme: {} } },
+            {
+              name: "aspect_ratio",
+              default: DEFAULT_ASPECT_RATIO,
+              selector: { text: {} },
+            },
+          ],
+        },
+      ] as const
+  );
 
   public setConfig(config: AreaCardConfig): void {
     assert(config, cardConfigStruct);
@@ -55,11 +74,18 @@ export class HuiAreaCardEditor
       return nothing;
     }
 
+    const schema = this._schema(this._config.show_camera || false);
+
+    const data = {
+      camera_view: "auto",
+      ...this._config,
+    };
+
     return html`
       <ha-form
         .hass=${this.hass}
-        .data=${this._config}
-        .schema=${SCHEMA}
+        .data=${data}
+        .schema=${schema}
         .computeLabel=${this._computeLabelCallback}
         @value-changed=${this._valueChanged}
       ></ha-form>
@@ -68,10 +94,15 @@ export class HuiAreaCardEditor
 
   private _valueChanged(ev: CustomEvent): void {
     const config = ev.detail.value;
+    if (!config.show_camera) {
+      delete config.camera_view;
+    }
     fireEvent(this, "config-changed", { config });
   }
 
-  private _computeLabelCallback = (schema: SchemaUnion<typeof SCHEMA>) => {
+  private _computeLabelCallback = (
+    schema: SchemaUnion<ReturnType<typeof this._schema>>
+  ) => {
     switch (schema.name) {
       case "theme":
         return `${this.hass!.localize(
@@ -84,6 +115,14 @@ export class HuiAreaCardEditor
       case "navigation_path":
         return this.hass!.localize(
           "ui.panel.lovelace.editor.action-editor.navigation_path"
+        );
+      case "aspect_ratio":
+        return this.hass!.localize(
+          "ui.panel.lovelace.editor.card.generic.aspect_ratio"
+        );
+      case "camera_view":
+        return this.hass!.localize(
+          "ui.panel.lovelace.editor.card.generic.camera_view"
         );
     }
     return this.hass!.localize(
