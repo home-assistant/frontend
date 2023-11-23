ad086fd152b88948c4e2b9f1ca2fb9bf2bfb3867
Bram Kragten <mail@bramkragten.nl>
2023-11-23 16:06:46 +0100

    Make thermostat centered in panel or grid view

--- src/panels/lovelace/cards/hui-thermostat-card.ts
+++ src/panels/lovelace/cards/hui-thermostat-card.ts
@@ -149,6 +149,7 @@ export class HuiThermostatCard extends LitElement implements LovelaceCard {
     return css`
       :host {
         display: block;
+        height: 100%;
       }
 
       ha-card {
@@ -159,6 +160,7 @@ export class HuiThermostatCard extends LitElement implements LovelaceCard {
         display: flex;
         flex-direction: column;
         align-items: center;
+        justify-content: center;
       }
 
       .title {
