0cd531c43d6af68ac3cb5b042c56a0410d58a4a7
G Johansson <goran.johansson@shiftit.se>
2023-11-27 18:31:20 +0000

    Handle submit correctly

--- src/dialogs/enter-code/dialog-enter-code.ts
+++ src/dialogs/enter-code/dialog-enter-code.ts
@@ -52,14 +52,18 @@ export class DialogEnterCode
   }
 
   private _submit(): void {
-    if (
-      this._dialogParams &&
-      this._dialogParams.codePattern &&
-      RegExp(this._dialogParams.codePattern).test(this._input?.value ?? "")
-    ) {
-      this._dialogParams?.submit?.(this._input?.value ?? "");
-      this.closeDialog();
+    if (this._dialogParams && this._dialogParams.codePattern) {
+      if (
+        RegExp(this._dialogParams.codePattern).test(this._input?.value ?? "")
+      ) {
+        this._dialogParams?.submit?.(this._input?.value ?? "");
+        this.closeDialog();
+      } else {
+        this.closeDialog();
+      }
     }
+    this._dialogParams?.submit?.(this._input?.value ?? "");
+    this.closeDialog();
   }
 
   private _cancel(): void {
