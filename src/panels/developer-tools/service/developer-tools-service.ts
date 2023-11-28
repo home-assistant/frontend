fabe6345c60fc7de19524ab6ced0919609436a4a
jbouwh <jan@jbsoft.nl>
2023-11-28 12:57:44 +0000

    Add translation support for script errors

--- src/panels/developer-tools/service/developer-tools-service.ts
+++ src/panels/developer-tools/service/developer-tools-service.ts
@@ -424,7 +424,17 @@ class HaPanelDevService extends LitElement {
       }
       forwardHaptic("failure");
       button.actionError();
+
+      const lokalize = await this.hass.loadBackendTranslation(
+        "exceptions",
+        err.translation_domain
+      );
+      const localizedErrorMessage = lokalize(
+        `component.${err.translation_domain}.exceptions.${err.translation_key}.message`,
+        err.translation_placeholders
+      );
       this._error =
+        localizedErrorMessage ||
         this.hass.localize("ui.notification_toast.service_call_failed", {
           service: this._serviceData!.service!,
         }) + ` ${err.message}`;
