cf2e0858353bb90b86e50e1d183fb1adafe7af31
Paul Bottein <paul.bottein@gmail.com>
2023-11-30 11:03:52 +0100

    Fix map dark mode

--- src/components/map/ha-map.ts
+++ src/components/map/ha-map.ts
@@ -155,10 +155,11 @@ export class HaMap extends ReactiveElement {
   }
 
   private _updateMapStyle(): void {
-    const darkMode = this.darkMode ?? this.hass.themes.darkMode;
+    const darkMode = this.darkMode ?? this.hass.themes.darkMode ?? false;
+    const forcedDark = this.darkMode ?? false;
     const map = this.shadowRoot!.getElementById("map");
     map!.classList.toggle("dark", darkMode);
-    map!.classList.toggle("forced-dark", this.darkMode);
+    map!.classList.toggle("forced-dark", forcedDark);
   }
 
   private async _loadMap(): Promise<void> {
