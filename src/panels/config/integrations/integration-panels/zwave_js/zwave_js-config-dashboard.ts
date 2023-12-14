c57e85896d804e6d3da06db4fe8986a931ba53b5
Charles Garwood <cgarwood@gmail.com>
2023-12-14 08:24:00 -0500

    remove leftover import

--- src/panels/config/integrations/integration-panels/zwave_js/zwave_js-config-dashboard.ts
+++ src/panels/config/integrations/integration-panels/zwave_js/zwave_js-config-dashboard.ts
@@ -50,7 +50,6 @@ import "../../../../../layouts/hass-tabs-subpage";
 import { SubscribeMixin } from "../../../../../mixins/subscribe-mixin";
 import { haStyle } from "../../../../../resources/styles";
 import type { HomeAssistant, Route } from "../../../../../types";
-import "../../../ha-config-section";
 import { showZWaveJSAddNodeDialog } from "./show-dialog-zwave_js-add-node";
 import { showZWaveJSRebuildNetworkRoutesDialog } from "./show-dialog-zwave_js-rebuild-network-routes";
 import { showZWaveJSRemoveNodeDialog } from "./show-dialog-zwave_js-remove-node";
