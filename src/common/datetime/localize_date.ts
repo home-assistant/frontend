278713637e9cb92676f9bf6c9d2689cb67ad8768
karwosts <karwosts@gmail.com>
2023-12-18 10:06:18 -0800

    reuse dtformat

--- src/common/datetime/localize_date.ts
+++ src/common/datetime/localize_date.ts
@@ -1,46 +1,30 @@
 import memoizeOne from "memoize-one";
 
-// Sunday = 0
-const localizeWeekday = (
-  language: string,
-  day_of_week: number,
-  short: boolean
-): string => {
-  const date = new Date(Date.UTC(1970, 0, 1 + 3 + day_of_week));
-  return new Intl.DateTimeFormat(language, {
-    weekday: short ? "short" : "long",
-    timeZone: "UTC",
-  }).format(date);
-};
-
 export const localizeWeekdays = memoizeOne(
   (language: string, short: boolean): string[] => {
     const days: string[] = [];
+    const format = new Intl.DateTimeFormat(language, {
+      weekday: short ? "short" : "long",
+      timeZone: "UTC",
+    });
     for (let i = 0; i < 7; i++) {
-      days.push(localizeWeekday(language, i, short));
+      const date = new Date(Date.UTC(1970, 0, 1 + 3 + i));
+      days.push(format.format(date));
     }
     return days;
   }
 );
 
-// January = 0
-const localizeMonth = (
-  language: string,
-  month: number,
-  short: boolean
-): string => {
-  const date = new Date(Date.UTC(1970, 0 + month, 1));
-  return new Intl.DateTimeFormat(language, {
-    month: short ? "short" : "long",
-    timeZone: "UTC",
-  }).format(date);
-};
-
 export const localizeMonths = memoizeOne(
   (language: string, short: boolean): string[] => {
     const months: string[] = [];
+    const format = new Intl.DateTimeFormat(language, {
+      month: short ? "short" : "long",
+      timeZone: "UTC",
+    });
     for (let i = 0; i < 12; i++) {
-      months.push(localizeMonth(language, i, short));
+      const date = new Date(Date.UTC(1970, 0 + i, 1));
+      months.push(format.format(date));
     }
     return months;
   }
