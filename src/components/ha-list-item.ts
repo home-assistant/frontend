f4c21589a357e4c1ce95573ad39e05a78c4bc034
Yosi Levy <37745463+yosilevy@users.noreply.github.com>
2023-12-27 11:59:28 +0200

    Various RTL fixes (#19134)
    * Various RTL fixes
    
    * Various RTL fixes

--- src/components/ha-list-item.ts
+++ src/components/ha-list-item.ts
@@ -36,12 +36,12 @@ export class HaListItem extends ListItemBase {
             --mdc-list-item-graphic-margin,
             16px
           ) !important;
-          direction: var(--direction);
+          direction: var(--direction) !important;
         }
         span.material-icons:last-of-type {
           margin-inline-start: auto !important;
           margin-inline-end: 0px !important;
-          direction: var(--direction);
+          direction: var(--direction) !important;
         }
         .mdc-deprecated-list-item__meta {
           display: var(--mdc-list-item-meta-display);
@@ -85,6 +85,15 @@ export class HaListItem extends ListItemBase {
           pointer-events: unset;
         }
       `,
+      // safari workaround - must be explicit
+      document.dir === "rtl"
+        ? css`
+            span.material-icons:first-of-type,
+            span.material-icons:last-of-type {
+              direction: rtl !important;
+            }
+          `
+        : css``,
     ];
   }
 }
