3a89961934688bf9ee911b6ea14a4bcee277bcc0
Joakim Plate <elupus@ecce.se>
2024-01-09 19:30:16 +0100

    Add volume flow rate

--- src/common/entity/get_states.ts
+++ src/common/entity/get_states.ts
@@ -211,6 +211,7 @@ const FIXED_DOMAIN_ATTRIBUTE_STATES = {
       "volatile_organic_compounds",
       "volatile_organic_compounds_parts",
       "voltage",
+      "volume_flow_rate",
     ],
     state_class: ["measurement", "total", "total_increasing"],
   },
