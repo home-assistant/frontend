b159f4c0741c62f57534d2e7dc2936767a0564aa
Marcel van der Veldt <m.vanderveldt@outlook.com>
2024-01-31 14:16:21 +0100

    Add matter device info and actions (#19578)
    * add matter device info panel (WIP)
    
    * actually enable card on device page
    
    * fix remove fabric
    
    * add some translation labels
    
    * add dialog to interview node
    
    * do not show info for bridged devices
    
    * first device action
    
    * add ping node action and dialog
    
    * ping should be always available
    
    * update model for MatterCommissioningParameters
    
    * add basic support for open commissioning window
    
    * move fabric management to dialog
    
    * review
    
    * Add link to thread panel
    
    ---------
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>

--- src/data/matter.ts
+++ src/data/matter.ts
@@ -3,6 +3,50 @@ import { navigate } from "../common/navigate";
 import { HomeAssistant } from "../types";
 import { subscribeDeviceRegistry } from "./device_registry";
 
+export enum NetworkType {
+  THREAD = "thread",
+  WIFI = "wifi",
+  ETHERNET = "ethernet",
+  UNKNOWN = "unknown",
+}
+
+export enum NodeType {
+  END_DEVICE = "end_device",
+  SLEEPY_END_DEVICE = "sleepy_end_device",
+  ROUTING_END_DEVICE = "routing_end_device",
+  BRIDGE = "bridge",
+  UNKNOWN = "unknown",
+}
+
+export interface MatterFabricData {
+  fabric_id: number;
+  vendor_id: number;
+  fabric_index: number;
+  fabric_label?: string;
+  vendor_name?: string;
+}
+
+export interface MatterNodeDiagnostics {
+  node_id: number;
+  network_type: NetworkType;
+  node_type: NodeType;
+  network_name?: string;
+  ip_adresses: string[];
+  mac_address?: string;
+  available: boolean;
+  active_fabrics: MatterFabricData[];
+}
+
+export interface MatterPingResult {
+  [ip_address: string]: boolean;
+}
+
+export interface MatterCommissioningParameters {
+  setup_pin_code: number;
+  setup_manual_code: string;
+  setup_qr_code: string;
+}
+
 export const canCommissionMatterExternal = (hass: HomeAssistant) =>
   hass.auth.external?.config.canCommissionMatter;
 
@@ -85,4 +129,51 @@ export const matterSetThread = (
   hass.callWS({
     type: "matter/set_thread",
     thread_operation_dataset,
+  });
+
+export const getMatterNodeDiagnostics = (
+  hass: HomeAssistant,
+  device_id: string
+): Promise<MatterNodeDiagnostics> =>
+  hass.callWS({
+    type: "matter/node_diagnostics",
+    device_id,
+  });
+
+export const pingMatterNode = (
+  hass: HomeAssistant,
+  device_id: string
+): Promise<MatterPingResult> =>
+  hass.callWS({
+    type: "matter/ping_node",
+    device_id,
+  });
+
+export const openMatterCommissioningWindow = (
+  hass: HomeAssistant,
+  device_id: string
+): Promise<MatterCommissioningParameters> =>
+  hass.callWS({
+    type: "matter/open_commissioning_window",
+    device_id,
+  });
+
+export const removeMatterFabric = (
+  hass: HomeAssistant,
+  device_id: string,
+  fabric_index: number
+): Promise<void> =>
+  hass.callWS({
+    type: "matter/remove_matter_fabric",
+    device_id,
+    fabric_index,
+  });
+
+export const interviewMatterNode = (
+  hass: HomeAssistant,
+  device_id: string
+): Promise<void> =>
+  hass.callWS({
+    type: "matter/interview_node",
+    device_id,
   });
