0b22c741f8c26e3d62740b1e068741ecc276a4ad
Bram Kragten <mail@bramkragten.nl>
2024-02-05 17:39:56 +0100

    Improve matter share device dialog

--- src/panels/config/integrations/integration-panels/matter/dialog-matter-open-commissioning-window.ts
+++ src/panels/config/integrations/integration-panels/matter/dialog-matter-open-commissioning-window.ts
@@ -1,18 +1,21 @@
 import "@material/mwc-button/mwc-button";
-import { mdiCheckCircle, mdiCloseCircle } from "@mdi/js";
-import { css, CSSResultGroup, html, LitElement, nothing } from "lit";
+import { mdiCloseCircle } from "@mdi/js";
+import { CSSResultGroup, LitElement, css, html, nothing } from "lit";
 import { customElement, property, state } from "lit/decorators";
 import { fireEvent } from "../../../../../common/dom/fire_event";
 import "../../../../../components/ha-circular-progress";
-import "../../../../../components/ha-qr-code";
 import { createCloseHeading } from "../../../../../components/ha-dialog";
+import "../../../../../components/ha-qr-code";
+import { domainToName } from "../../../../../data/integration";
 import {
-  openMatterCommissioningWindow,
   MatterCommissioningParameters,
+  openMatterCommissioningWindow,
 } from "../../../../../data/matter";
 import { haStyleDialog } from "../../../../../resources/styles";
 import { HomeAssistant } from "../../../../../types";
+import { brandsUrl } from "../../../../../util/brands-url";
 import { MatterOpenCommissioningWindowDialogParams } from "./show-dialog-matter-open-commissioning-window";
+import { copyToClipboard } from "../../../../../common/util/copy-clipboard";
 
 @customElement("dialog-matter-open-commissioning-window")
 class DialogMatterOpenCommissioningWindow extends LitElement {
@@ -52,28 +55,44 @@ class DialogMatterOpenCommissioningWindow extends LitElement {
                 ${this.hass.localize(
                   "ui.panel.config.matter.open_commissioning_window.success"
                 )}
+                <br />
+                ${this.hass.localize(
+                  "ui.panel.config.matter.open_commissioning_window.scan_code"
+                )}
               </p>
-              <div class="flex-container">
-                <ha-svg-icon
-                  .path=${mdiCheckCircle}
-                  class="success"
-                ></ha-svg-icon>
-                <div class="status">
-                  <p>
-                    ${this.hass.localize(
-                      "ui.panel.config.matter.open_commissioning_window.sharing_code"
-                    )}: <b>${this._commissionParams.setup_manual_code}</b>
-                  </p>
-                </div>
+              <div class="status sharing-code">
+                <img
+                  crossorigin="anonymous"
+                  referrerpolicy="no-referrer"
+                  alt=${domainToName(this.hass.localize, "matter")}
+                  src=${brandsUrl({
+                    domain: "matter",
+                    type: "logo",
+                    darkOptimized: this.hass.themes?.darkMode,
+                  })}
+                />
+                <ha-qr-code
+                  .data=${this._commissionParams.setup_qr_code}
+                  errorCorrectionLevel="quartile"
+                  scale="6"
+                  margin="1"
+                ></ha-qr-code>
+                <span class="code"
+                  >${this._commissionParams.setup_manual_code.substring(
+                    0,
+                    4
+                  )}-${this._commissionParams.setup_manual_code.substring(
+                    4,
+                    7
+                  )}-${this._commissionParams.setup_manual_code.substring(
+                    7
+                  )}</span
+                >
               </div>
-              <ha-qr-code
-                .data=${this._commissionParams.setup_qr_code}
-                errorCorrectionLevel="quartile"
-                scale="6"
-              ></ha-qr-code>
-              <div></div>
-              <mwc-button slot="primaryAction" @click=${this.closeDialog}>
-                ${this.hass.localize("ui.common.close")}
+              <mwc-button slot="primaryAction" @click=${this._copyCode}>
+                ${this.hass.localize(
+                  "ui.panel.config.matter.open_commissioning_window.copy_code"
+                )}
               </mwc-button>
             `
           : this._status === "started"
@@ -145,6 +164,14 @@ class DialogMatterOpenCommissioningWindow extends LitElement {
     }
   }
 
+  private async _copyCode() {
+    if (!this._commissionParams) {
+      return;
+    }
+    await copyToClipboard(this._commissionParams.setup_manual_code);
+    this.closeDialog();
+  }
+
   public closeDialog(): void {
     this.device_id = undefined;
     this._status = undefined;
@@ -192,6 +219,21 @@ class DialogMatterOpenCommissioningWindow extends LitElement {
         .flex-container ha-circular-progress,
         .flex-container ha-svg-icon {
           margin-right: 20px;
+        }
+
+        .sharing-code {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+        }
+
+        .sharing-code img {
+          width: 160px;
+          margin-bottom: 8px;
+        }
+
+        .code {
+          font-family: monospace;
         }
       `,
     ];
