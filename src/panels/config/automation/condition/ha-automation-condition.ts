84938ccc94e50b6528a222895da375daba3251d9
karwosts <32912880+karwosts@users.noreply.github.com>
2024-02-15 04:04:40 -0500

    stopPropagation on automation moveUp/moveDown (#19804)
    * stopPropagation on automation moveUp/moveDown
    
    * also conditions and triggers

--- src/panels/config/automation/condition/ha-automation-condition.ts
+++ src/panels/config/automation/condition/ha-automation-condition.ts
@@ -227,12 +227,14 @@ export default class HaAutomationCondition extends LitElement {
   }
 
   private _moveUp(ev) {
+    ev.stopPropagation();
     const index = (ev.target as any).index;
     const newIndex = index - 1;
     this._move(index, newIndex);
   }
 
   private _moveDown(ev) {
+    ev.stopPropagation();
     const index = (ev.target as any).index;
     const newIndex = index + 1;
     this._move(index, newIndex);
