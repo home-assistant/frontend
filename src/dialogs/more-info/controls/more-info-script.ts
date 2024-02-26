039015273c250099c7d63b0eda45ebd5e35e8d63
Paulus Schoutsen <balloob@gmail.com>
2024-02-25 22:15:23 -0500

    Show script fields in more info dialog

--- src/dialogs/more-info/controls/more-info-script.ts
+++ src/dialogs/more-info/controls/more-info-script.ts
@@ -1,8 +1,21 @@
+import "@material/mwc-button";
 import { HassEntity } from "home-assistant-js-websocket";
-import { css, CSSResultGroup, html, LitElement, nothing } from "lit";
+import {
+  css,
+  CSSResultGroup,
+  html,
+  LitElement,
+  nothing,
+  PropertyValueMap,
+} from "lit";
 import { customElement, property } from "lit/decorators";
 import "../../../components/ha-relative-time";
+import "../../../components/ha-service-control";
+import "../../../components/entity/state-info";
 import { HomeAssistant } from "../../../types";
+import { canRun, ScriptEntity } from "../../../data/script";
+import { isUnavailableState } from "../../../data/entity";
+import { computeObjectId } from "../../../common/entity/compute_object_id";
 
 @customElement("more-info-script")
 class MoreInfoScript extends LitElement {
@@ -10,12 +23,61 @@ class MoreInfoScript extends LitElement {
 
   @property({ attribute: false }) public stateObj?: HassEntity;
 
+  private _scriptData: Record<string, any> = {};
+
   protected render() {
     if (!this.hass || !this.stateObj) {
       return nothing;
     }
+    const stateObj = this.stateObj as ScriptEntity;
+
+    const fields =
+      this.hass.services.script[computeObjectId(this.stateObj.entity_id)]
+        ?.fields;
+
+    const hasFields = fields && Object.keys(fields).length > 0;
 
     return html`
+      <div class="flex">
+        <state-info
+          .hass=${this.hass}
+          .stateObj=${stateObj}
+          inDialog
+        ></state-info>
+        ${stateObj.state === "on"
+          ? html`<mwc-button @click=${this._cancelScript}>
+              ${stateObj.attributes.mode !== "single" &&
+              (stateObj.attributes.current || 0) > 0
+                ? this.hass.localize("ui.card.script.cancel_multiple", {
+                    number: stateObj.attributes.current,
+                  })
+                : this.hass.localize("ui.card.script.cancel")}
+            </mwc-button>`
+          : ""}
+        ${stateObj.state === "off" || stateObj.attributes.max
+          ? html`<mwc-button
+              @click=${this._runScript}
+              .disabled=${isUnavailableState(stateObj.state) ||
+              !canRun(stateObj)}
+            >
+              ${this.hass!.localize("ui.card.script.run")}
+            </mwc-button>`
+          : ""}
+      </div>
+
+      ${hasFields
+        ? html`
+            <ha-service-control
+              hidePicker
+              hideDescription
+              .hass=${this.hass}
+              .value=${this._scriptData}
+              .showAdvanced=${this.hass.userData?.showAdvanced}
+              @value-changed=${this._scriptDataChanged}
+            ></ha-service-control>
+          `
+        : ""}
+
       <hr />
       <div class="flex">
         <div>
@@ -36,16 +98,59 @@ class MoreInfoScript extends LitElement {
     `;
   }
 
+  protected override willUpdate(
+    changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>
+  ): void {
+    super.willUpdate(changedProperties);
+    const oldState = changedProperties.get("stateObj") as
+      | HassEntity
+      | undefined;
+    const newState = this.stateObj;
+
+    if (newState && (!oldState || oldState.entity_id !== newState.entity_id)) {
+      this._scriptData = { service: newState.entity_id, data: {} };
+    }
+  }
+
+  private _cancelScript(ev: Event) {
+    ev.stopPropagation();
+    this._callService("turn_off");
+  }
+
+  private async _runScript(ev: Event) {
+    ev.stopPropagation();
+    this.hass.callService(
+      "script",
+      computeObjectId(this.stateObj!.entity_id),
+      this._scriptData
+    );
+  }
+
+  private _callService(service: string): void {
+    this.hass.callService("script", service, {
+      entity_id: this.stateObj!.entity_id,
+    });
+  }
+
+  private _scriptDataChanged(ev: CustomEvent): void {
+    this._scriptData = { ...this._scriptData, ...ev.detail.value };
+  }
+
   static get styles(): CSSResultGroup {
     return css`
       .flex {
         display: flex;
         justify-content: space-between;
+        margin-bottom: 16px;
       }
       hr {
         border-color: var(--divider-color);
         border-bottom: none;
         margin: 16px 0;
+      }
+      ha-service-control {
+        --service-control-padding: 0;
+        --service-control-items-border-top: none;
       }
     `;
   }
