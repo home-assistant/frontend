c2f639e24831ef63034630768ee7630b98a26aab
James Woglom <j@wogloms.net>
2024-02-26 21:11:05 -0500

    Resolve iframe CORS errors - Fix #19724

--- src/common/dom/get_main_window.ts
+++ src/common/dom/get_main_window.ts
@@ -1,8 +1,13 @@
 import { MAIN_WINDOW_NAME } from "../../data/main_window";
 
-export const mainWindow =
-  window.name === MAIN_WINDOW_NAME
-    ? window
-    : parent.name === MAIN_WINDOW_NAME
-      ? parent
-      : top!;
+export const mainWindow = (() => {
+  try {
+    return window.name === MAIN_WINDOW_NAME
+      ? window
+      : parent.name === MAIN_WINDOW_NAME
+        ? parent
+        : top!;
+  } catch {
+    return window;
+  }
+})();
