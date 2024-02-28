1868b358d91f0545cc26cecf37889e1dcf971d52
Paulus Schoutsen <balloob@gmail.com>
2024-02-27 22:47:29 -0500

    Update script more info

--- src/dialogs/more-info/controls/more-info-script.ts
+++ src/dialogs/more-info/controls/more-info-script.ts
@@ -1,3 +1,4 @@
+import { mdiPlay } from "@mdi/js";
 import "@material/mwc-button";
 import { HassEntity } from "home-assistant-js-websocket";
 import {
@@ -8,28 +9,52 @@ import {
   nothing,
   PropertyValues,
 } from "lit";
-import { customElement, property } from "lit/decorators";
+import { customElement, property, state } from "lit/decorators";
 import "../../../components/ha-relative-time";
 import "../../../components/ha-service-control";
+import "../../../components/ha-button";
 import "../../../components/entity/state-info";
 import { HomeAssistant } from "../../../types";
 import { canRun, ScriptEntity } from "../../../data/script";
 import { isUnavailableState } from "../../../data/entity";
 import { computeObjectId } from "../../../common/entity/compute_object_id";
+import { listenMediaQuery } from "../../../common/dom/media_query";
 
 @customElement("more-info-script")
 class MoreInfoScript extends LitElement {
   @property({ attribute: false }) public hass!: HomeAssistant;
 
-  @property({ attribute: false }) public stateObj?: HassEntity;
+  @property({ attribute: false }) public stateObj?: ScriptEntity;
 
-  private _scriptData: Record<string, any> = {};
+  @state() private _scriptData: Record<string, any> = {};
+
+  @state() private narrow = false;
+
+  private _unsubMediaQuery?: () => void;
+
+  public connectedCallback(): void {
+    super.connectedCallback();
+    this._unsubMediaQuery = listenMediaQuery(
+      "(max-width: 870px)",
+      (matches) => {
+        this.narrow = matches;
+      }
+    );
+  }
+
+  public disconnectedCallback(): void {
+    super.disconnectedCallback();
+    if (this._unsubMediaQuery) {
+      this._unsubMediaQuery();
+      this._unsubMediaQuery = undefined;
+    }
+  }
 
   protected render() {
     if (!this.hass || !this.stateObj) {
       return nothing;
     }
-    const stateObj = this.stateObj as ScriptEntity;
+    const stateObj = this.stateObj;
 
     const fields =
       this.hass.services.script[computeObjectId(this.stateObj.entity_id)]
@@ -37,54 +62,77 @@ class MoreInfoScript extends LitElement {
 
     const hasFields = fields && Object.keys(fields).length > 0;
 
+    const current = stateObj.attributes.current || 0;
+    const isQueued = stateObj.attributes.mode === "queued";
+    const isParallel = stateObj.attributes.mode === "parallel";
+    const hasQueue = isQueued && current > 1;
+
     return html`
-      <div class="flex">
-        <state-info
-          .hass=${this.hass}
-          .stateObj=${stateObj}
-          inDialog
-        ></state-info>
-        ${stateObj.state === "on"
-          ? html`<mwc-button @click=${this._cancelScript}>
-              ${stateObj.attributes.mode !== "single" &&
-              (stateObj.attributes.current || 0) > 0
-                ? this.hass.localize("ui.card.script.cancel_multiple", {
-                    number: stateObj.attributes.current,
-                  })
-                : this.hass.localize("ui.card.script.cancel")}
-            </mwc-button>`
-          : nothing}
-        ${stateObj.state === "off" || stateObj.attributes.max
-          ? html`<mwc-button
-              @click=${this._runScript}
-              .disabled=${isUnavailableState(stateObj.state) ||
-              !canRun(stateObj)}
-            >
-              ${this.hass!.localize("ui.card.script.run")}
-            </mwc-button>`
-          : nothing}
+      <div class="state">
+        <h2>
+          ${current > 0
+            ? html`
+                ${isParallel && current > 1
+                  ? this.hass.localize("ui.card.script.running_parallel", {
+                      active: current,
+                    })
+                  : this.hass.localize("ui.card.script.running_single")}
+              `
+            : this.hass.localize("ui.card.script.idle")}
+        </h2>
+        <div class=${`queue ${hasQueue ? "has-queue" : ""}`}>
+          ${hasQueue
+            ? html`
+                ${this.hass.localize("ui.card.script.running_queued", {
+                  queued: current - 1,
+                })}
+              `
+            : "&nbsp;"}
+        </div>
+
+        <ha-button
+          class=${current > 0 ? "can-cancel" : ""}
+          raised
+          @click=${this._cancelScript}
+        >
+          ${(isQueued || isParallel) && current > 1
+            ? this.hass.localize("ui.card.script.cancel_all")
+            : this.hass.localize("ui.card.script.cancel")}
+        </ha-button>
       </div>
 
-      ${hasFields
-        ? html`
-            <ha-service-control
-              hidePicker
-              hideDescription
-              .hass=${this.hass}
-              .value=${this._scriptData}
-              .showAdvanced=${this.hass.userData?.showAdvanced}
-              @value-changed=${this._scriptDataChanged}
-            ></ha-service-control>
-          `
-        : nothing}
-
-      <hr />
-      <div class="flex">
-        <div>
-          ${this.hass.localize(
-            "ui.dialogs.more_info_control.script.last_triggered"
-          )}:
+      <div class="run">
+        <div class="title">
+          ${this.hass.localize("ui.card.script.run_script")}
         </div>
+
+        ${hasFields
+          ? html`
+              <ha-service-control
+                hidePicker
+                hideDescription
+                .hass=${this.hass}
+                .value=${this._scriptData}
+                .showAdvanced=${this.hass.userData?.showAdvanced}
+                .narrow=${this.narrow}
+                @value-changed=${this._scriptDataChanged}
+              ></ha-service-control>
+            `
+          : nothing}
+
+        <div class="run-button">
+          <ha-button
+            raised
+            @click=${this._runScript}
+            .disabled=${isUnavailableState(stateObj.state) || !this._canRun()}
+          >
+            <ha-svg-icon .path=${mdiPlay} slot="icon"></ha-svg-icon>
+            ${this.hass!.localize("ui.card.script.run")}
+          </ha-button>
+        </div>
+      </div>
+
+      <div class="footer">
         ${this.stateObj.attributes.last_triggered
           ? html`
               <ha-relative-time
@@ -139,8 +187,55 @@ class MoreInfoScript extends LitElement {
     this._scriptData = { ...this._scriptData, ...ev.detail.value };
   }
 
+  private _canRun() {
+    if (
+      canRun(this.stateObj!) ||
+      // Restart can also always runs. Just cancels other run.
+      this.stateObj!.attributes.mode === "restart"
+    ) {
+      return true;
+    }
+    return false;
+  }
+
   static get styles(): CSSResultGroup {
     return css`
+      .state {
+        text-align: center;
+        margin: 2em 16px 1em;
+      }
+      .state .queue {
+        visibility: hidden;
+        color: var(--secondary-text-color);
+      }
+      .state .queue.has-queue {
+        visibility: visible;
+      }
+      .state ha-button {
+        margin-top: 16px;
+        --mdc-theme-primary: var(--error-color);
+        visibility: hidden;
+      }
+      .state ha-button.can-cancel {
+        visibility: visible;
+      }
+      .run {
+        margin: 8px;
+        padding: 16px;
+        border: 1px solid var(--divider-color);
+        border-radius: 8px;
+      }
+      .run .title {
+        font-weight: bold;
+      }
+      .run-button {
+        margin-top: 16px;
+        text-align: center;
+      }
+      .footer {
+        margin-top: 24px;
+        text-align: center;
+      }
       .flex {
         display: flex;
         justify-content: space-between;
