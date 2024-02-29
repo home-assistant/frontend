c8e81ed459407082f6d7027123c05b131f127b8d
karwosts <karwosts@gmail.com>
2024-02-29 07:27:25 -0800

    drop unneeded imports

--- src/panels/lovelace/editor/picture-element-editor/hui-picture-element-element-editor.ts
+++ src/panels/lovelace/editor/picture-element-editor/hui-picture-element-element-editor.ts
@@ -2,13 +2,6 @@ import { customElement } from "lit/decorators";
 import { LovelaceElementConfig } from "../../elements/types";
 import type { LovelacePictureElementEditor } from "../../types";
 import { HuiElementEditor } from "../hui-element-editor";
-import "../config-elements/elements/hui-state-badge-element-editor";
-import "../config-elements/elements/hui-state-icon-element-editor";
-import "../config-elements/elements/hui-state-label-element-editor";
-import "../config-elements/elements/hui-service-button-element-editor";
-import "../config-elements/elements/hui-icon-element-editor";
-import "../config-elements/elements/hui-image-element-editor";
-import "../config-elements/elements/hui-conditional-element-editor";
 import { getPictureElementClass } from "../../create-element/create-picture-element";
 
 @customElement("hui-picture-element-element-editor")
