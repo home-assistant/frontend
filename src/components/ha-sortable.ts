a34332b48dec3fef7d58c58d2295f23a5885f21e
Paul Bottein <paul.bottein@gmail.com>
2024-02-29 15:12:19 +0100

    Fix section editing after disconnect/reconnect (#19917)
    * Fix section editing after disconnect/reconnect
    
    * Update src/components/ha-sortable.ts
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>
    
    ---------
    
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>

--- src/components/ha-sortable.ts
+++ src/components/ha-sortable.ts
@@ -82,6 +82,9 @@ export class HaSortable extends LitElement {
   public connectedCallback() {
     super.connectedCallback();
     this._shouldBeDestroy = false;
+    if (this.hasUpdated) {
+      this.requestUpdate();
+    }
   }
 
   protected createRenderRoot() {
