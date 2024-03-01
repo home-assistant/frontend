6721c1cc774daa3eed5f8ef469fbcc52bcc49988
Paul Bottein <paul.bottein@gmail.com>
2024-03-01 15:31:25 +0100

    Transform helper to warning for edit view type (#19934)

--- src/panels/lovelace/editor/view-editor/hui-dialog-edit-view.ts
+++ src/panels/lovelace/editor/view-editor/hui-dialog-edit-view.ts
@@ -57,6 +57,8 @@ import { EditViewDialogParams } from "./show-edit-view-dialog";
 export class HuiDialogEditView extends LitElement {
   @property({ attribute: false }) public hass?: HomeAssistant;
 
+  @state() private _currentType?: string;
+
   @state() private _params?: EditViewDialogParams;
 
   @state() private _config?: LovelaceViewConfig;
@@ -111,6 +113,7 @@ export class HuiDialogEditView extends LitElement {
       this._badges = [];
       return;
     }
+    this._currentType = view.type;
     const { badges, ...viewConfig } = view;
     this._config = viewConfig;
     this._badges = badges ? processEditorEntities(badges) : [];
@@ -211,6 +214,15 @@ export class HuiDialogEditView extends LitElement {
       }
     }
 
+    const isEmpty =
+      !this._config?.cards?.length && !this._config?.sections?.length;
+
+    const isCompatibleViewType =
+      isEmpty ||
+      (this._currentType === SECTION_VIEW_LAYOUT
+        ? this._config?.type === SECTION_VIEW_LAYOUT
+        : this._config?.type !== SECTION_VIEW_LAYOUT);
+
     return html`
       <ha-dialog
         open
@@ -308,9 +320,25 @@ export class HuiDialogEditView extends LitElement {
               </mwc-button>
             `
           : nothing}
+        ${!isCompatibleViewType
+          ? html`
+              <ha-alert class="incompatible" alert-type="warning">
+                ${this._config?.type === SECTION_VIEW_LAYOUT
+                  ? this.hass!.localize(
+                      "ui.panel.lovelace.editor.edit_view.type_warning_sections"
+                    )
+                  : this.hass!.localize(
+                      "ui.panel.lovelace.editor.edit_view.type_warning_others"
+                    )}
+              </ha-alert>
+            `
+          : nothing}
         <mwc-button
           slot="primaryAction"
-          ?disabled=${!this._config || this._saving || !this._dirty}
+          ?disabled=${!this._config ||
+          this._saving ||
+          !this._dirty ||
+          !isCompatibleViewType}
           @click=${this._save}
         >
           ${this._saving
@@ -553,6 +581,10 @@ export class HuiDialogEditView extends LitElement {
           justify-content: center;
           margin: 12px 16px;
           flex-wrap: wrap;
+        }
+        .incompatible {
+          display: block;
+          margin-top: 16px;
         }
 
         @media all and (min-width: 600px) {
