a9f453ea367f45591b544decbe612a54a9521db6
Paul Bottein <paul.bottein@gmail.com>
2024-03-04 15:22:04 +0100

    Add sections dashboard to demo dashboard (#19976)

---
+++ demo/src/configs/sections/index.ts
@@ -0,0 +1,12 @@
+import { DemoConfig } from "../types";
+import { demoEntitiesSections } from "./entities";
+import { demoLovelaceSections } from "./lovelace";
+
+export const demoSections: DemoConfig = {
+  authorName: "Home Assistant",
+  authorUrl: "https://github.com/home-assistant/frontend/",
+  name: "Home Demo",
+  lovelace: demoLovelaceSections,
+  entities: demoEntitiesSections,
+  theme: () => ({}),
+};
