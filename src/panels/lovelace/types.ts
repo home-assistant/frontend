effefdbff1e591b1e44c136d65a2b8400ef3cce8
Paul Bottein <paul.bottein@gmail.com>
2024-03-07 11:22:26 +0100

    Add layout options to cards and improve grid and sections defaults (#20001)
    * Add grid options to cards
    
    * Fix the height of the card it's rows option is provided
    
    * Add variable
    
    * Adjust grid margin
    
    * Use layout options
    
    * Fix max width when only one column
    
    * Update card API

--- src/panels/lovelace/types.ts
+++ src/panels/lovelace/types.ts
@@ -39,12 +39,17 @@ export interface LovelaceBadge extends HTMLElement {
   setConfig(config: LovelaceBadgeConfig): void;
 }
 
+export type LovelaceLayoutOptions = {
+  grid_columns?: number;
+  grid_rows?: number;
+};
+
 export interface LovelaceCard extends HTMLElement {
   hass?: HomeAssistant;
   isPanel?: boolean;
   editMode?: boolean;
   getCardSize(): number | Promise<number>;
-  getGridSize?(): [number, number];
+  getLayoutOptions?(): LovelaceLayoutOptions;
   setConfig(config: LovelaceCardConfig): void;
 }
 
