8d8807e65986fd06859df8b207ae974e5a575c7b
Paul Bottein <paul.bottein@gmail.com>
2024-03-27 11:17:31 +0100

    Fix profile page (#20199)
    * Add icon to profile page
    
    * Fix notification dot color

--- src/panels/profile/ha-panel-profile.ts
+++ src/panels/profile/ha-panel-profile.ts
@@ -1,18 +1,22 @@
 import { customElement, property } from "lit/decorators";
 
-import { HomeAssistant } from "../../types";
+import { mdiAccount, mdiLock } from "@mdi/js";
+import { PropertyValues } from "lit";
 import { HassRouterPage, RouterOptions } from "../../layouts/hass-router-page";
 import { PageNavigation } from "../../layouts/hass-tabs-subpage";
 import { SubscribeMixin } from "../../mixins/subscribe-mixin";
+import { HomeAssistant } from "../../types";
 
 export const profileSections: PageNavigation[] = [
   {
     path: "/profile/general",
     translationKey: "ui.panel.profile.tabs.general",
+    iconPath: mdiAccount,
   },
   {
     path: "/profile/security",
     translationKey: "ui.panel.profile.tabs.security",
+    iconPath: mdiLock,
   },
 ];
 
@@ -40,6 +44,22 @@ class HaPanelProfile extends SubscribeMixin(HassRouterPage) {
     el.route = this.routeTail;
     el.hass = this.hass;
     el.narrow = this.narrow;
+  }
+
+  protected firstUpdated(changedProps: PropertyValues) {
+    super.firstUpdated(changedProps);
+    this.style.setProperty(
+      "--app-header-background-color",
+      "var(--sidebar-background-color)"
+    );
+    this.style.setProperty(
+      "--app-header-text-color",
+      "var(--sidebar-text-color)"
+    );
+    this.style.setProperty(
+      "--app-header-border-bottom",
+      "1px solid var(--divider-color)"
+    );
   }
 }
 declare global {
