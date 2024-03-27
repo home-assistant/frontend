3240b7ed717e5b5e2cda0ca6a2cfe3ca9322dca1
karwosts <32912880+karwosts@users.noreply.github.com>
2024-03-27 08:44:09 -0700

    Update ha-automation-picker.ts

--- src/panels/config/automation/ha-automation-picker.ts
+++ src/panels/config/automation/ha-automation-picker.ts
@@ -73,7 +73,6 @@ import "../../../layouts/hass-tabs-subpage-data-table";
 import { SubscribeMixin } from "../../../mixins/subscribe-mixin";
 import { haStyle } from "../../../resources/styles";
 import { HomeAssistant, Route } from "../../../types";
-import { LocalizeFunc } from "../../../common/translations/localize";
 import { documentationUrl } from "../../../util/documentation-url";
 import { showAssignCategoryDialog } from "../category/show-dialog-assign-category";
 import { configSections } from "../ha-panel-config";
