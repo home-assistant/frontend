cb1c2b59df14909a5054a97a37c060bc22708012
Paul Bottein <paul.bottein@gmail.com>
2024-03-29 16:12:18 +0100

    Use primary color for filter badge color (#20263)

--- src/components/ha-filter-entities.ts
+++ src/components/ha-filter-entities.ts
@@ -199,11 +199,11 @@ export class HaFilterEntities extends LitElement {
           border-radius: 50%;
           font-weight: 400;
           font-size: 11px;
-          background-color: var(--accent-color);
+          background-color: var(--primary-color);
           line-height: 16px;
           text-align: center;
           padding: 0px 2px;
-          color: var(--text-accent-color, var(--text-primary-color));
+          color: var(--text-primary-color);
         }
         ha-check-list-item {
           --mdc-list-item-graphic-margin: 16px;
