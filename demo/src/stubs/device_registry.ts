1795ec4e3e68c23c1c11c33e4ea3ac71a82ac6ed
Bram Kragten <mail@bramkragten.nl>
2024-04-02 10:29:27 +0200

    fix mocks

--- demo/src/stubs/device_registry.ts
+++ demo/src/stubs/device_registry.ts
@@ -4,4 +4,11 @@ import type { MockHomeAssistant } from "../../../src/fake_data/provide_hass";
 export const mockDeviceRegistry = (
   hass: MockHomeAssistant,
   data: DeviceRegistryEntry[] = []
-) => hass.mockWS("config/device_registry/list", () => data);
+) => {
+  hass.mockWS("config/device_registry/list", () => data);
+  const devices = {};
+  data.forEach((device) => {
+    devices[device.id] = device;
+  });
+  hass.updateHass({ devices });
+};
