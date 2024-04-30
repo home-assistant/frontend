7120ad99b9bdd3d6dc648deb6ae5285d986f8744
Paul Bottein <paul.bottein@gmail.com>
2024-04-30 18:38:51 +0200

    Add customize mode option to card features with modes (#20670)
    * Add customize mode options to card features with modes
    
    * Better type
    
    * Fix water heater and humidifier
    
    * Clean schema

--- src/panels/lovelace/card-features/common/filter-modes.ts
+++ src/panels/lovelace/card-features/common/filter-modes.ts
@@ -2,6 +2,6 @@ export const filterModes = (
   supportedModes: string[] | undefined,
   selectedModes: string[] | undefined
 ): string[] =>
-  (selectedModes || []).length
-    ? selectedModes!.filter((mode) => (supportedModes || []).includes(mode))
+  selectedModes
+    ? selectedModes.filter((mode) => (supportedModes || []).includes(mode))
     : supportedModes || [];
