9cae8f4c4dd770c3a6bb0c4a96062b8fd5f1282c
Bram Kragten <mail@bramkragten.nl>
2024-05-29 14:48:28 +0200

    use image selector for view background

--- src/panels/lovelace/editor/view-editor/hui-view-background-editor.ts
+++ src/panels/lovelace/editor/view-editor/hui-view-background-editor.ts
@@ -2,19 +2,11 @@ import "@material/mwc-list/mwc-list-item";
 import { CSSResultGroup, LitElement, css, html, nothing } from "lit";
 import { customElement, property, state } from "lit/decorators";
 import { fireEvent } from "../../../../common/dom/fire_event";
-import "../../../../components/user/ha-user-badge";
+import "../../../../components/ha-selector/ha-selector-image";
 import { LovelaceViewConfig } from "../../../../data/lovelace/config/view";
 import { HomeAssistant, ValueChangedEvent } from "../../../../types";
-import "../../../../components/ha-picture-upload";
-import type { HaPictureUpload } from "../../../../components/ha-picture-upload";
-import { CropOptions } from "../../../../dialogs/image-cropper-dialog/show-image-cropper-dialog";
 
-const cropOptions: CropOptions = {
-  round: false,
-  type: "image/jpeg",
-  quality: 0.75,
-  aspectRatio: 1.78,
-};
+const SELECTOR = { image: { original: true } };
 
 @customElement("hui-view-background-editor")
 export class HuiViewBackgroundEditor extends LitElement {
@@ -43,19 +35,17 @@ export class HuiViewBackgroundEditor extends LitElement {
           "ui.panel.lovelace.editor.edit_view.background.title"
         )}
       </p>
-      <ha-picture-upload
+      <ha-selector-image
         .hass=${this.hass}
         .value=${backgroundUrl}
-        crop
-        .cropOptions=${cropOptions}
-        original
-        @change=${this._backgroundChanged}
-      ></ha-picture-upload>
+        .selector=${SELECTOR}
+        @value-changed=${this._backgroundChanged}
+      ></ha-selector-image>
     `;
   }
 
   private _backgroundChanged(ev: ValueChangedEvent<string | null>) {
-    const backgroundUrl = (ev.target as HaPictureUpload).value;
+    const backgroundUrl = ev.detail.value;
     const config = {
       ...this._config,
       background: backgroundUrl
