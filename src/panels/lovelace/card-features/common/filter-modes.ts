7f9251ad53267e046893fafb4684c04d10ccef40
Paul Bottein <paul.bottein@gmail.com>
2024-06-11 10:30:45 +0200

    Fix current mode not selected in card feature

--- src/panels/lovelace/card-features/common/filter-modes.ts
+++ src/panels/lovelace/card-features/common/filter-modes.ts
@@ -1,7 +1,7 @@
-export const filterModes = (
-  supportedModes: string[] | undefined,
-  selectedModes: string[] | undefined
-): string[] =>
+export const filterModes = <T extends string = string>(
+  supportedModes: T[] | undefined,
+  selectedModes: T[] | undefined
+): T[] =>
   selectedModes
     ? selectedModes.filter((mode) => (supportedModes || []).includes(mode))
     : supportedModes || [];
