baa4e609a2baab10aae7a5cfc4950e98a4b478b5
Paul Bottein <paul.bottein@gmail.com>
2024-06-26 23:18:18 +0200

    Revert "20240626.1 (#21179)"
    This reverts commit a16cae0671c744b4ea87ec90a04ecbff918a7ca6.

--- hassio/src/components/hassio-filter-addons.ts
+++ hassio/src/components/hassio-filter-addons.ts
@@ -2,7 +2,6 @@ import type { IFuseOptions } from "fuse.js";
 import Fuse from "fuse.js";
 import { stripDiacritics } from "../../../src/common/string/strip-diacritics";
 import { StoreAddon } from "../../../src/data/supervisor/store";
-import { getStripDiacriticsFn } from "../../../src/util/fuse";
 
 export function filterAndSort(addons: StoreAddon[], filter: string) {
   const options: IFuseOptions<StoreAddon> = {
@@ -10,7 +9,7 @@ export function filterAndSort(addons: StoreAddon[], filter: string) {
     isCaseSensitive: false,
     minMatchCharLength: Math.min(filter.length, 2),
     threshold: 0.2,
-    getFn: getStripDiacriticsFn,
+    getFn: (obj, path) => stripDiacritics(Fuse.config.getFn(obj, path)),
   };
   const fuse = new Fuse(addons, options);
   return fuse.search(stripDiacritics(filter)).map((result) => result.item);
