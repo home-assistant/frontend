27c7799d9327a7dc6cf33a5b4506ce9dc17f08d1
Steve Repsher <steverep@users.noreply.github.com>
2024-07-02 03:52:45 +0000

    Remove Safari 14.0 patch for delegatesFocus

--- cast/src/launcher/entrypoint.ts
+++ cast/src/launcher/entrypoint.ts
@@ -1,4 +1,3 @@
-import "../../../src/resources/safari-14-attachshadow-patch";
 import "./layout/hc-connect";
 
 import("../../../src/resources/ha-style");
--- src/entrypoints/authorize.ts
+++ src/entrypoints/authorize.ts
@@ -1,7 +1,6 @@
 // Compat needs to be first import
 import "../resources/compatibility";
 import "../auth/ha-authorize";
-import "../resources/safari-14-attachshadow-patch";
 
 import("../resources/ha-style");
 import("@polymer/polymer/lib/utils/settings").then(
