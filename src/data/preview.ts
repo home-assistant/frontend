f51f7467e6cc188b006810a05a64a5c91ec9f8b4
karwosts <karwosts@gmail.com>
2024-07-12 16:38:46 -0700

    Make config flow previews more generic

--- src/data/preview.ts
+++ src/data/preview.ts
@@ -1,21 +1,25 @@
 import { UnsubscribeFunc } from "home-assistant-js-websocket";
 import { HomeAssistant } from "../types";
 
-export interface TimeDatePreview {
+export interface GenericPreview {
   state: string;
   attributes: Record<string, any>;
 }
 
-export const subscribePreviewTimeDate = (
+export const subscribePreviewGeneric = (
   hass: HomeAssistant,
+  domain: string,
   flow_id: string,
   flow_type: "config_flow" | "options_flow",
   user_input: Record<string, any>,
-  callback: (preview: TimeDatePreview) => void
+  callback: (preview: GenericPreview) => void
 ): Promise<UnsubscribeFunc> =>
   hass.connection.subscribeMessage(callback, {
-    type: "time_date/start_preview",
+    type: `${domain}/start_preview`,
     flow_id,
     flow_type,
     user_input,
   });
+
+export const previewModule = (domain: string): string =>
+  ["template"].includes(domain) ? domain : "generic";
