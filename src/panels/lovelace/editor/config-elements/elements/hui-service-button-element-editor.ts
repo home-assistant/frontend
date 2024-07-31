4d7194fa0db29a547f32ea7c7850b435ff1cdff8
Bram Kragten <mail@bramkragten.nl>
2024-07-31 14:54:01 +0200

    Migrate service call element to use action key (#21506)
    Migrate service call element

--- src/panels/lovelace/editor/config-elements/elements/hui-service-button-element-editor.ts
+++ src/panels/lovelace/editor/config-elements/elements/hui-service-button-element-editor.ts
@@ -1,32 +1,29 @@
-import { html, LitElement, nothing } from "lit";
+import { css, html, LitElement, nothing } from "lit";
 import { customElement, property, state } from "lit/decorators";
+import memoizeOne from "memoize-one";
 import { any, assert, literal, object, optional, string } from "superstruct";
 import { fireEvent } from "../../../../../common/dom/fire_event";
-import type { SchemaUnion } from "../../../../../components/ha-form/types";
-import type { HomeAssistant } from "../../../../../types";
 import "../../../../../components/ha-form/ha-form";
-import { LovelacePictureElementEditor } from "../../../types";
+import type { SchemaUnion } from "../../../../../components/ha-form/types";
+import "../../../../../components/ha-service-control";
+import { ServiceAction } from "../../../../../data/script";
+import type { HomeAssistant } from "../../../../../types";
 import { ServiceButtonElementConfig } from "../../../elements/types";
-// import { UiAction } from "../../components/hui-action-editor";
+import { LovelacePictureElementEditor } from "../../../types";
 
 const serviceButtonElementConfigStruct = object({
   type: literal("service-button"),
   style: optional(any()),
   title: optional(string()),
+  action: optional(string()),
   service: optional(string()),
   service_data: optional(any()),
+  data: optional(any()),
+  target: optional(any()),
 });
 
 const SCHEMA = [
   { name: "title", required: true, selector: { text: {} } },
-  /* {
-    name: "service",
-    selector: {
-      ui_action: { actions: ["call-service"] as UiAction[] },
-    },
-  }, */
-  { name: "service", required: true, selector: { text: {} } },
-  { name: "service_data", selector: { object: {} } },
   { name: "style", selector: { object: {} } },
 ] as const;
 
@@ -44,6 +41,14 @@ export class HuiServiceButtonElementEditor
     this._config = config;
   }
 
+  private _serviceData = memoizeOne(
+    (config: ServiceButtonElementConfig): ServiceAction => ({
+      action: config?.action ?? config?.service,
+      data: config?.data ?? config?.service_data,
+      target: config?.target,
+    })
+  );
+
   protected render() {
     if (!this.hass || !this._config) {
       return nothing;
@@ -57,11 +62,41 @@ export class HuiServiceButtonElementEditor
         .computeLabel=${this._computeLabelCallback}
         @value-changed=${this._valueChanged}
       ></ha-form>
+      <ha-service-control
+        .hass=${this.hass}
+        .value=${this._serviceData(this._config)}
+        .showAdvanced=${this.hass.userData?.showAdvanced}
+        narrow
+        @value-changed=${this._serviceDataChanged}
+      ></ha-service-control>
     `;
   }
 
   private _valueChanged(ev: CustomEvent): void {
-    fireEvent(this, "config-changed", { config: ev.detail.value });
+    fireEvent(this, "config-changed", {
+      config: { ...this._config, ...ev.detail.value },
+    });
+  }
+
+  private _serviceDataChanged(ev: CustomEvent<{ value: ServiceAction }>): void {
+    const config: ServiceButtonElementConfig = {
+      ...this._config!,
+      action: ev.detail.value.action,
+      data: ev.detail.value.data,
+      target: ev.detail.value.target,
+    };
+
+    if ("service" in config) {
+      delete config.service;
+    }
+
+    if ("service_data" in config) {
+      delete config.service_data;
+    }
+
+    fireEvent(this, "config-changed", {
+      config,
+    });
   }
 
   private _computeLabelCallback = (schema: SchemaUnion<typeof SCHEMA>) =>
@@ -70,6 +105,16 @@ export class HuiServiceButtonElementEditor
     ) ||
     this.hass!.localize(`ui.panel.lovelace.editor.elements.${schema.name}`) ||
     schema.name;
+
+  static get styles() {
+    return css`
+      ha-service-control {
+        display: block;
+        margin-top: 16px;
+        --service-control-padding: 0;
+      }
+    `;
+  }
 }
 
 declare global {
