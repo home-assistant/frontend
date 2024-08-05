3cf739dbdace6f31878621d75bb8295c757eb981
karwosts <karwosts@gmail.com>
2024-08-05 07:21:48 -0700

    Fix pointer cursor issues

--- src/panels/lovelace/common/has-action.ts
+++ src/panels/lovelace/common/has-action.ts
@@ -1,5 +1,15 @@
 import { ActionConfig } from "../../../data/lovelace/config/action";
+import { ConfigEntity } from "../cards/types";
 
 export function hasAction(config?: ActionConfig): boolean {
   return config !== undefined && config.action !== "none";
+}
+
+export function hasAnyAction(config: ConfigEntity): boolean {
+  return (
+    !config.tap_action ||
+    hasAction(config.tap_action) ||
+    hasAction(config.hold_action) ||
+    hasAction(config.double_tap_action)
+  );
 }
