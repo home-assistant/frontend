a1b1520030c4b591c29916d70a9e3f471a9ebc6a
Douwe <61123717+dhoeben@users.noreply.github.com>
2024-08-09 09:03:05 +0200

    Update hui-entity-badge.ts
    Removed variables for ripple, as these are already defined in the host. It is bad practise.
    
    Added three variables to change the size of the badge.

--- src/panels/lovelace/badges/hui-entity-badge.ts
+++ src/panels/lovelace/badges/hui-entity-badge.ts
@@ -209,8 +209,6 @@ export class HuiEntityBadge extends LitElement implements LovelaceBadge {
       .badge {
         position: relative;
         --ha-ripple-color: var(--badge-color);
-        --ha-ripple-hover-opacity: 0.04;
-        --ha-ripple-pressed-opacity: 0.12;
         transition:
           box-shadow 180ms ease-in-out,
           border-color 180ms ease-in-out;
@@ -232,7 +230,7 @@ export class HuiEntityBadge extends LitElement implements LovelaceBadge {
           --ha-card-border-color,
           var(--divider-color, #e0e0e0)
         );
-        --mdc-icon-size: 18px;
+        --mdc-icon-size: var(--ha-badge-icon-size, 18px);
         text-align: center;
         font-family: Roboto;
       }
