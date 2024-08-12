36684f1d90e77a29c829502946e0ce09af99832c
Douwe <61123717+dhoeben@users.noreply.github.com>
2024-08-12 13:56:17 +0200

    Update hui-entity-badge.ts
    add option to chsnge badge size

--- src/panels/lovelace/badges/hui-entity-badge.ts
+++ src/panels/lovelace/badges/hui-entity-badge.ts
@@ -219,12 +219,12 @@ export class HuiEntityBadge extends LitElement implements LovelaceBadge {
         align-items: center;
         justify-content: center;
         gap: 8px;
-        height: 36px;
-        min-width: 36px;
+        height: var(--ha-badge-size, 36px);
+        min-width: var(--ha-badge-size, 36px);
         padding: 0px 8px;
         box-sizing: border-box;
         width: auto;
-        border-radius: 18px;
+        border-radius: calc(var(--ha-badge-size, 36px) / 2);
         background-color: var(--card-background-color, white);
         border-width: var(--ha-card-border-width, 1px);
         border-style: solid;
