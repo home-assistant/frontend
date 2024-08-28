f522a82df7c52931d6f21906eeea1ffed7cdbfd1
karwosts <32912880+karwosts@users.noreply.github.com>
2024-08-27 23:35:11 -0700

    Revert display_precision override for duration format (#21755)
    Remove precision override for duration format

--- src/common/entity/compute_state_display.ts
+++ src/common/entity/compute_state_display.ts
@@ -71,8 +71,7 @@ export const computeStateDisplayFromEntityAttributes = (
     if (
       attributes.device_class === "duration" &&
       attributes.unit_of_measurement &&
-      UNIT_TO_MILLISECOND_CONVERT[attributes.unit_of_measurement] &&
-      entity?.display_precision === undefined
+      UNIT_TO_MILLISECOND_CONVERT[attributes.unit_of_measurement]
     ) {
       try {
         return formatDuration(state, attributes.unit_of_measurement);
