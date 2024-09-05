b932a70c1abfd3cf0d7bb8101f4d8b20aee6c975
Wendelin <w@pe8.at>
2024-09-05 11:29:12 +0200

    Remove dashboard resources options from advanced mode

--- src/panels/config/lovelace/dashboards/ha-config-lovelace-dashboards.ts
+++ src/panels/config/lovelace/dashboards/ha-config-lovelace-dashboards.ts
@@ -322,22 +322,16 @@ export class HaConfigLovelaceDashboards extends LitElement {
         hasFab
         clickable
       >
-        ${this.hass.userData?.showAdvanced
-          ? html`
-              <ha-button-menu slot="toolbar-icon" activatable>
-                <ha-icon-button
-                  slot="trigger"
-                  .label=${this.hass.localize("ui.common.menu")}
-                  .path=${mdiDotsVertical}
-                ></ha-icon-button>
-                <ha-clickable-list-item href="/config/lovelace/resources">
-                  ${this.hass.localize(
-                    "ui.panel.config.lovelace.resources.caption"
-                  )}
-                </ha-clickable-list-item>
-              </ha-button-menu>
-            `
-          : ""}
+        <ha-button-menu slot="toolbar-icon" activatable>
+          <ha-icon-button
+            slot="trigger"
+            .label=${this.hass.localize("ui.common.menu")}
+            .path=${mdiDotsVertical}
+          ></ha-icon-button>
+          <ha-clickable-list-item href="/config/lovelace/resources">
+            ${this.hass.localize("ui.panel.config.lovelace.resources.caption")}
+          </ha-clickable-list-item>
+        </ha-button-menu>
         <ha-fab
           slot="fab"
           .label=${this.hass.localize(
