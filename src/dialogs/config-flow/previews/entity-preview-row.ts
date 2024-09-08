ac3e041f3331a13f97bf2d70864cd08a40ad6cec
G Johansson <goran.johansson@shiftit.se>
2024-09-08 18:28:27 +0000

    Render preview based of entity domain

--- src/dialogs/config-flow/previews/entity-preview-row.ts
+++ src/dialogs/config-flow/previews/entity-preview-row.ts
@@ -1,9 +1,11 @@
 import type { HassEntity } from "home-assistant-js-websocket";
-import type { CSSResultGroup } from "lit";
+import type { CSSResultGroup, TemplateResult } from "lit";
 import { LitElement, css, html, nothing } from "lit";
 import { customElement, property, state } from "lit/decorators";
 import { computeStateName } from "../../../common/entity/compute_state_name";
 import "../../../components/entity/state-badge";
+import "../../../components/entity/ha-entity-toggle";
+import "../../../components/ha-select";
 import { isUnavailableState } from "../../../data/entity";
 import { SENSOR_DEVICE_CLASS_TIMESTAMP } from "../../../data/sensor";
 import "../../../panels/lovelace/components/hui-timestamp-display";
@@ -28,18 +30,7 @@ class EntityPreviewRow extends LitElement {
       <div class="name" .title=${computeStateName(stateObj)}>
         ${computeStateName(stateObj)}
       </div>
-      <div class="value">
-        ${stateObj.attributes.device_class === SENSOR_DEVICE_CLASS_TIMESTAMP &&
-        !isUnavailableState(stateObj.state)
-          ? html`
-              <hui-timestamp-display
-                .hass=${this.hass}
-                .ts=${new Date(stateObj.state)}
-                capitalize
-              ></hui-timestamp-display>
-            `
-          : this.hass.formatEntityState(stateObj)}
-      </div>`;
+      <div class="value">${this.renderEntityState(stateObj)}</div>`;
   }
 
   static get styles(): CSSResultGroup {
@@ -59,7 +50,121 @@ class EntityPreviewRow extends LitElement {
       .value {
         direction: ltr;
       }
+      .numberflex {
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        flex-grow: 2;
+      }
+      .numberstate {
+        min-width: 45px;
+        text-align: end;
+      }
+      ha-textfield {
+        text-align: end;
+        direction: ltr !important;
+      }
+      ha-slider {
+        width: 100%;
+        max-width: 200px;
+      }
     `;
+  }
+
+  private renderEntityState(stateObj: HassEntity): TemplateResult {
+    const domain = stateObj.entity_id.split(".", 1)[0];
+    if (domain === "sensor") {
+      const showSensor =
+        stateObj.attributes.device_class === SENSOR_DEVICE_CLASS_TIMESTAMP &&
+        !isUnavailableState(stateObj.state);
+      return html`
+        ${showSensor
+          ? html`
+              <hui-timestamp-display
+                .hass=${this.hass}
+                .ts=${new Date(stateObj.state)}
+                capitalize
+              ></hui-timestamp-display>
+            `
+          : html` ${this.hass.formatEntityState(stateObj)} `}
+      `;
+    }
+    if (domain === "switch") {
+      const showToggle =
+        stateObj.state === "on" ||
+        stateObj.state === "off" ||
+        isUnavailableState(stateObj.state);
+      return html`
+        ${showToggle
+          ? html`
+              <ha-entity-toggle
+                .hass=${this.hass}
+                .stateObj=${stateObj}
+              ></ha-entity-toggle>
+            `
+          : html` ${this.hass.formatEntityState(stateObj)} `}
+      `;
+    }
+    if (domain === "number") {
+      const showNumberSlider =
+        stateObj.attributes.mode === "slider" ||
+        (stateObj.attributes.mode === "auto" &&
+          (Number(stateObj.attributes.max) - Number(stateObj.attributes.min)) /
+            Number(stateObj.attributes.step) <=
+            256);
+      return html`
+        ${showNumberSlider
+          ? html`
+              <div class="numberflex">
+                <ha-slider
+                  labeled
+                  .disabled=${isUnavailableState(stateObj.state)}
+                  .step=${Number(stateObj.attributes.step)}
+                  .min=${Number(stateObj.attributes.min)}
+                  .max=${Number(stateObj.attributes.max)}
+                  .value=${Number(stateObj.state)}
+                ></ha-slider>
+                <span class="state">
+                  ${this.hass.formatEntityState(stateObj)}
+                </span>
+              </div>
+            `
+          : html` <div class="numberflex numberstate">
+              <ha-textfield
+                autoValidate
+                .disabled=${isUnavailableState(stateObj.state)}
+                pattern="[0-9]+([\\.][0-9]+)?"
+                .step=${Number(stateObj.attributes.step)}
+                .min=${Number(stateObj.attributes.min)}
+                .max=${Number(stateObj.attributes.max)}
+                .value=${stateObj.state}
+                .suffix=${stateObj.attributes.unit_of_measurement}
+                type="number"
+              ></ha-textfield>
+            </div>`}
+      `;
+    }
+    if (domain === "select") {
+      return html`
+        <ha-select
+          .label=${computeStateName(stateObj)}
+          .value=${stateObj.state}
+          .disabled=${isUnavailableState(stateObj.state)}
+          naturalMenuWidth
+        >
+          ${stateObj.attributes.options
+            ? stateObj.attributes.options.map(
+                (option) => html`
+                  <mwc-list-item .value=${option}>
+                    ${this.hass!.formatEntityState(stateObj, option)}
+                  </mwc-list-item>
+                `
+              )
+            : ""}
+        </ha-select>
+      `;
+    }
+    return html` ${this.hass.formatEntityState(stateObj)} `;
   }
 }
 
