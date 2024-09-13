32feab6a7043ca8d050802abc802000fc794c7b2
Paul Bottein <paul.bottein@gmail.com>
2024-09-13 11:07:04 +0200

    Add floors to hass (#21960)

--- src/panels/config/ha-panel-config.ts
+++ src/panels/config/ha-panel-config.ts
@@ -35,22 +35,17 @@ import { customElement, property, state } from "lit/decorators";
 import { isComponentLoaded } from "../../common/config/is_component_loaded";
 import { listenMediaQuery } from "../../common/dom/media_query";
 import { CloudStatus, fetchCloudStatus } from "../../data/cloud";
-import {
-  floorsContext,
-  fullEntitiesContext,
-  labelsContext,
-} from "../../data/context";
+import { fullEntitiesContext, labelsContext } from "../../data/context";
 import {
   entityRegistryByEntityId,
   entityRegistryById,
   subscribeEntityRegistry,
 } from "../../data/entity_registry";
+import { subscribeLabelRegistry } from "../../data/label_registry";
 import { HassRouterPage, RouterOptions } from "../../layouts/hass-router-page";
 import { PageNavigation } from "../../layouts/hass-tabs-subpage";
 import { SubscribeMixin } from "../../mixins/subscribe-mixin";
 import { HomeAssistant, Route } from "../../types";
-import { subscribeLabelRegistry } from "../../data/label_registry";
-import { subscribeFloorRegistry } from "../../data/floor_registry";
 
 declare global {
   // for fire event
@@ -390,11 +385,6 @@ class HaPanelConfig extends SubscribeMixin(HassRouterPage) {
     initialValue: [],
   });
 
-  private _floorsContext = new ContextProvider(this, {
-    context: floorsContext,
-    initialValue: [],
-  });
-
   public hassSubscribe(): UnsubscribeFunc[] {
     return [
       subscribeEntityRegistry(this.hass.connection!, (entities) => {
@@ -402,9 +392,6 @@ class HaPanelConfig extends SubscribeMixin(HassRouterPage) {
       }),
       subscribeLabelRegistry(this.hass.connection!, (labels) => {
         this._labelsContext.setValue(labels);
-      }),
-      subscribeFloorRegistry(this.hass.connection!, (floors) => {
-        this._floorsContext.setValue(floors);
       }),
     ];
   }
