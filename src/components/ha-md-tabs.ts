0027c37cebab1b58f19b7dafded908abfc92dc23
Simon Lamon <32477463+silamon@users.noreply.github.com>
2024-09-21 11:00:03 +0000

    Remove buttons tryout

--- src/components/ha-md-tabs.ts
+++ src/components/ha-md-tabs.ts
@@ -9,40 +9,6 @@ import { HomeAssistant } from "../types";
 export class HaMdTabs extends MdTabs {
   @property({ attribute: false }) public hass!: HomeAssistant;
 
-  // @property({ attribute: false }) public buttons: boolean = false;
-
-  /* protected render() {
-    return html`
-      ${this.buttons
-        ? html`<ha-icon-button-prev
-            .label=${this.hass?.localize(
-              "ui.panel.lovelace.components.energy_period_selector.previous"
-            ) || "Prev"}
-            @click=${this._pickPrevious}
-          ></ha-icon-button-prev>`
-        : nothing}
-      ${super.render()}
-      ${this.buttons
-        ? html`<ha-icon-button-next
-            .label=${this.hass?.localize(
-              "ui.panel.lovelace.components.energy_period_selector.next"
-            ) || "Next"}
-            @click=${this._pickNext}
-          ></ha-icon-button-next>`
-        : nothing}
-    `;
-  } */
-
-  private _pickPrevious() {
-    this.activeTabIndex -= 1;
-    this.scrollToTab();
-  }
-
-  private _pickNext() {
-    this.activeTabIndex += 1;
-    this.scrollToTab();
-  }
-
   protected updated(c) {
     super.updated(c);
     const slider = this.shadowRoot?.querySelector(".tabs");
