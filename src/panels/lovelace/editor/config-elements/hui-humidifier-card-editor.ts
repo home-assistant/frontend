7d0e30e2cb3087b75f099a55dbba7aac7a03a993
Paul Bottein <paul.bottein@gmail.com>
2024-09-25 16:47:38 +0200

    Use callback instead of changing nested config with sub editor (#22081)
    Use callback instead of changing nested config

--- src/panels/lovelace/editor/config-elements/hui-humidifier-card-editor.ts
+++ src/panels/lovelace/editor/config-elements/hui-humidifier-card-editor.ts
@@ -27,7 +27,7 @@ import {
 import type { HumidifierCardConfig } from "../../cards/types";
 import type { LovelaceCardEditor } from "../../types";
 import { baseLovelaceCardConfig } from "../structs/base-card-struct";
-import { EditDetailElementEvent } from "../types";
+import { EditDetailElementEvent, EditSubElementEvent } from "../types";
 import { configElementStyle } from "./config-elements-style";
 import "./hui-card-features-editor";
 import type { FeatureType } from "./hui-card-features-editor";
@@ -145,12 +145,28 @@ export class HuiHumidifierCardEditor
   }
 
   private _editDetailElement(ev: HASSDomEvent<EditDetailElementEvent>): void {
+    const index = ev.detail.subElementConfig.index;
+    const config = this._config!.features![index!];
+
     fireEvent(this, "edit-sub-element", {
-      path: ["features", ev.detail.subElementConfig.index!],
+      config: config,
+      saveConfig: (newConfig) => this._updateFeature(index!, newConfig),
       context: {
         entity_id: this._config!.entity,
-      } as LovelaceCardFeatureContext,
+      },
       type: "feature",
+    } as EditSubElementEvent<
+      LovelaceCardFeatureConfig,
+      LovelaceCardFeatureContext
+    >);
+  }
+
+  private _updateFeature(index: number, feature: LovelaceCardFeatureConfig) {
+    const features = this._config!.features!.concat();
+    features[index] = feature;
+    const config = { ...this._config!, features };
+    fireEvent(this, "config-changed", {
+      config: config,
     });
   }
 
