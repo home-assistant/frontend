c61b24c87aa85c94914e9c561448fe0d25a3486f
Simon Lamon <32477463+silamon@users.noreply.github.com>
2024-09-28 05:34:15 +0000

    Fixup gallery

--- gallery/src/pages/automation/describe-action.ts
+++ gallery/src/pages/automation/describe-action.ts
@@ -142,7 +142,7 @@ export class DemoAutomationDescribeAction extends LitElement {
         <div class="action">
           <span>
             ${this._action
-              ? describeAction(this.hass, [], [], [], this._action)
+              ? describeAction(this.hass, [], [], this._action)
               : "<invalid YAML>"}
           </span>
           <ha-yaml-editor
@@ -155,7 +155,7 @@ export class DemoAutomationDescribeAction extends LitElement {
         ${ACTIONS.map(
           (conf) => html`
             <div class="action">
-              <span>${describeAction(this.hass, [], [], [], conf as any)}</span>
+              <span>${describeAction(this.hass, [], [], conf as any)}</span>
               <pre>${dump(conf)}</pre>
             </div>
           `
