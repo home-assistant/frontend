30dc55c0d73d5d4da60b0050a484529126d5faf9
Wendelin <w@pe8.at>
2024-10-10 20:24:22 +0200

    Add hide completed option to hui-todo-list-card

--- src/panels/lovelace/cards/hui-todo-list-card.ts
+++ src/panels/lovelace/cards/hui-todo-list-card.ts
@@ -275,7 +275,7 @@ export class HuiTodoListCard extends LitElement implements LovelaceCard {
                     "ui.panel.lovelace.cards.todo-list.no_unchecked_items"
                   )}
                 </p>`}
-            ${checkedItems.length
+            ${this._config.hide_completed !== true && checkedItems.length
               ? html`
                   <div role="separator">
                     <div class="divider"></div>
