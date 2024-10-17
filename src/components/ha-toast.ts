f131817ef90009bc146e7d5139ab8c2a29b436e9
Paul Bottein <paul.bottein@gmail.com>
2024-10-17 14:53:47 +0200

    Center ha-toast

--- src/components/ha-toast.ts
+++ src/components/ha-toast.ts
@@ -1,8 +1,19 @@
-import { customElement } from "lit/decorators";
 import { Snackbar } from "@material/mwc-snackbar/mwc-snackbar";
+import { styles } from "@material/mwc-snackbar/mwc-snackbar.css";
+import { css } from "lit";
+import { customElement } from "lit/decorators";
 
 @customElement("ha-toast")
-export class HaToast extends Snackbar {}
+export class HaToast extends Snackbar {
+  static override styles = [
+    styles,
+    css`
+      .mdc-snackbar--leading {
+        justify-content: center;
+      }
+    `,
+  ];
+}
 
 declare global {
   interface HTMLElementTagNameMap {
