410e01dece45fc28014a2592bba01a95e62a8d7f
Paul Bottein <paul.bottein@gmail.com>
2024-10-17 15:28:08 +0200

    Improve margin

--- src/components/ha-toast.ts
+++ src/components/ha-toast.ts
@@ -11,6 +11,36 @@ export class HaToast extends Snackbar {
       .mdc-snackbar--leading {
         justify-content: center;
       }
+
+      .mdc-snackbar {
+        margin: 8px;
+        right: calc(8px + env(safe-area-inset-right));
+        bottom: calc(8px + env(safe-area-inset-bottom));
+        left: calc(8px + env(safe-area-inset-left));
+      }
+
+      .mdc-snackbar__surface {
+        min-width: 350px;
+        max-width: 650px;
+      }
+
+      // Revert the default styles set by mwc-snackbar
+      @media (max-width: 480px), (max-width: 344px) {
+        .mdc-snackbar__surface {
+          min-width: inherit;
+        }
+      }
+
+      @media all and (max-width: 450px), all and (max-height: 500px) {
+        .mdc-snackbar {
+          right: env(safe-area-inset-right);
+          bottom: env(safe-area-inset-bottom);
+          left: env(safe-area-inset-left);
+        }
+        .mdc-snackbar__surface {
+          min-width: 100%;
+        }
+      }
     `,
   ];
 }
