6f6d18885e7ffc44b3e287657a8c77514c6ad8e2
Wendelin <w@pe8.at>
2024-10-23 15:52:47 +0200

    Improve landing-page build

--- build-scripts/gulp/gather-static.js
+++ build-scripts/gulp/gather-static.js
@@ -27,6 +27,16 @@ function copyTranslations(staticDir) {
   );
 }
 
+function copyFragmentTranslations(staticDir, pageName) {
+  const staticPath = genStaticPath(staticDir);
+
+  // Translation output
+  fs.copySync(
+    polyPath(`build/translations/output/${pageName}`),
+    staticPath(`translations/${pageName}`)
+  );
+}
+
 function copyLocaleData(staticDir) {
   const staticPath = genStaticPath(staticDir);
 
