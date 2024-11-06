ce372a481e87a7090725c2db5d8c6e13d2d13337
Paul Bottein <paul.bottein@gmail.com>
2024-11-06 17:21:23 +0100

    Improve size

--- src/panels/lovelace/editor/card-editor/hui-card-layout-editor.ts
+++ src/panels/lovelace/editor/card-editor/hui-card-layout-editor.ts
@@ -150,7 +150,7 @@ export class HuiCardLayoutEditor extends LitElement {
         : html`
             <ha-grid-size-picker
               style=${styleMap({
-                "max-width": `${(this.sectionConfig.column_span ?? 1) * 250 + 50}px`,
+                "max-width": `${(this.sectionConfig.column_span ?? 1) * 250 + 40}px`,
               })}
               .columns=${gridTotalColumns}
               .hass=${this.hass}
