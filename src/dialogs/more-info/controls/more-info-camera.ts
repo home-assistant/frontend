d7ee0a34c2bc5db09385da6b14042ce751ad5bd5
Jan-Philipp Benecke <jan-philipp@bnck.me>
2024-11-07 08:01:01 +0100

    Add take snapshot button to camera more info dialog

--- src/dialogs/more-info/controls/more-info-camera.ts
+++ src/dialogs/more-info/controls/more-info-camera.ts
@@ -4,6 +4,8 @@ import { property, state } from "lit/decorators";
 import "../../../components/ha-camera-stream";
 import type { CameraEntity } from "../../../data/camera";
 import type { HomeAssistant } from "../../../types";
+import "../../../components/buttons/ha-progress-button";
+import { UNAVAILABLE } from "../../../data/entity";
 
 class MoreInfoCamera extends LitElement {
   @property({ attribute: false }) public hass?: HomeAssistant;
@@ -34,13 +36,50 @@ class MoreInfoCamera extends LitElement {
         allow-exoplayer
         controls
       ></ha-camera-stream>
+
+      <div class="actions">
+        <ha-progress-button
+          @click=${this._takeSnapshot}
+          .disabled=${this.stateObj.state === UNAVAILABLE}
+        >
+          ${this.hass.localize(
+            "ui.dialogs.more_info_control.camera.take_snapshot"
+          )}
+        </ha-progress-button>
+      </div>
     `;
+  }
+
+  private async _takeSnapshot() {
+    const progressElement =
+      this.shadowRoot!.querySelector("ha-progress-button")!;
+
+    try {
+      await this.hass!.callService("camera", "snapshot", {
+        entity_id: this.stateObj!.entity_id,
+      });
+      progressElement.actionSuccess();
+    } catch (e) {
+      progressElement.actionError();
+    }
   }
 
   static get styles(): CSSResultGroup {
     return css`
       :host {
         display: block;
+      }
+
+      .actions {
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: flex-end;
+        box-sizing: border-box;
+        padding: 12px;
+        z-index: 1;
+        gap: 8px;
       }
     `;
   }
