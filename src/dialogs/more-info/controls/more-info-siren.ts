ed12bb61e898cc8af8af7af4bfed35717964db49
Petar Petrov <MindFreeze@users.noreply.github.com>
2024-11-11 15:57:22 +0200

    Add tone selector to more-info for sirens

--- src/dialogs/more-info/controls/more-info-siren.ts
+++ src/dialogs/more-info/controls/more-info-siren.ts
@@ -5,9 +5,15 @@ import { LitElement, html, nothing } from "lit";
 import { customElement, property } from "lit/decorators";
 import "../../../components/ha-attributes";
 import "../../../state-control/ha-state-control-toggle";
+import "../../../components/ha-control-select-menu";
+import "../../../components/ha-list-item";
+import "../components/ha-more-info-control-select-container";
 import type { HomeAssistant } from "../../../types";
 import "../components/ha-more-info-state-header";
 import { moreInfoControlStyle } from "../components/more-info-control-style";
+import { supportsFeature } from "../../../common/entity/supports-feature";
+import { SirenEntityFeature } from "../../../data/siren";
+import { stopPropagation } from "../../../common/dom/stop_propagation";
 
 @customElement("more-info-siren")
 class MoreInfoSiren extends LitElement {
@@ -19,6 +25,10 @@ class MoreInfoSiren extends LitElement {
     if (!this.hass || !this.stateObj) {
       return nothing;
     }
+
+    const supportsTones =
+      supportsFeature(this.stateObj, SirenEntityFeature.TONES) &&
+      this.stateObj.attributes.available_tones;
 
     return html`
       <ha-more-info-state-header
@@ -33,6 +43,23 @@ class MoreInfoSiren extends LitElement {
           .iconPathOff=${mdiVolumeOff}
         ></ha-state-control-toggle>
       </div>
+      ${supportsTones
+        ? html`
+            <ha-more-info-control-select-container>
+              <ha-control-select-menu
+                .hass=${this.hass}
+                .label=${this.hass.localize("ui.components.siren.tone")}
+                @closed=${stopPropagation}
+              >
+                ${Object.entries(this.stateObj.attributes.available_tones).map(
+                  ([toneId, toneName]) => html`
+                    <ha-list-item .value=${toneId}>${toneName}</ha-list-item>
+                  `
+                )}
+              </ha-control-select-menu>
+            </ha-more-info-control-select-container>
+          `
+        : nothing}
       <ha-attributes
         .hass=${this.hass}
         .stateObj=${this.stateObj}
