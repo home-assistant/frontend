a337d70068a7d969f0454469e5f26ba194aaf26b
Wendelin <w@pe8.at>
2024-11-14 14:31:54 +0100

    Reduces test pipeline

--- test/setup.ts
+++ test/setup.ts
@@ -1,5 +1,3 @@
-import fs from "node:fs";
-import path from "node:path";
 import { beforeAll } from "vitest";
 
 beforeAll(() => {
@@ -7,11 +5,4 @@ beforeAll(() => {
   global.navigator = {} as any;
 
   global.__DEMO__ = false;
-
-  const MDI_OUTPUT_DIR = path.resolve(__dirname, "../build/mdi");
-
-  if (!fs.existsSync(MDI_OUTPUT_DIR)) {
-    fs.mkdirSync(MDI_OUTPUT_DIR, { recursive: true });
-    fs.writeFileSync(path.resolve(MDI_OUTPUT_DIR, "iconMetadata.json"), "{}");
-  }
 });
