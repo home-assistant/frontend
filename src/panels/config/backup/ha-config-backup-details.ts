3297568a5afd757f3ee620900213166a4099dec8
Paul Bottein <paul.bottein@gmail.com>
2024-11-19 18:46:38 +0100

    Add delete and download action

--- src/panels/config/backup/ha-config-backup-details.ts
+++ src/panels/config/backup/ha-config-backup-details.ts
@@ -1,17 +1,30 @@
+import { mdiDelete, mdiDotsVertical, mdiDownload } from "@mdi/js";
 import { css, html, LitElement, nothing } from "lit";
 import { customElement, property, state } from "lit/decorators";
+import type { ActionDetail } from "@material/mwc-list";
 import { formatDateTime } from "../../../common/datetime/format_date_time";
+import { navigate } from "../../../common/navigate";
 import "../../../components/ha-alert";
+import "../../../components/ha-button-menu";
 import "../../../components/ha-card";
 import "../../../components/ha-circular-progress";
+import "../../../components/ha-icon-button";
+import "../../../components/ha-list-item";
 import "../../../components/ha-md-list";
 import "../../../components/ha-md-list-item";
+import { getSignedPath } from "../../../data/auth";
 import type { BackupContent } from "../../../data/backup";
-import { fetchBackupDetails } from "../../../data/backup";
+import {
+  fetchBackupDetails,
+  getBackupDownloadUrl,
+  removeBackup,
+} from "../../../data/backup";
+import { domainToName } from "../../../data/integration";
 import "../../../layouts/hass-subpage";
 import type { HomeAssistant } from "../../../types";
 import { brandsUrl } from "../../../util/brands-url";
-import { domainToName } from "../../../data/integration";
+import { fileDownload } from "../../../util/file_download";
+import { showConfirmationDialog } from "../../lovelace/custom-card-helpers";
 
 @customElement("ha-config-backup-details")
 class HaConfigBackupDetails extends LitElement {
@@ -47,6 +60,21 @@ class HaConfigBackupDetails extends LitElement {
         .narrow=${this.narrow}
         .header=${this._backup?.name || "Backup"}
       >
+        <ha-button-menu slot="toolbar-icon" @action=${this._handleAction}>
+          <ha-icon-button
+            slot="trigger"
+            .label=${this.hass.localize("ui.common.menu")}
+            .path=${mdiDotsVertical}
+          ></ha-icon-button>
+          <ha-list-item graphic="icon">
+            <ha-svg-icon slot="graphic" .path=${mdiDownload}></ha-svg-icon>
+            ${this.hass.localize("ui.common.download")}
+          </ha-list-item>
+          <ha-list-item graphic="icon" class="warning">
+            <ha-svg-icon slot="graphic" .path=${mdiDelete}></ha-svg-icon>
+            ${this.hass.localize("ui.common.delete")}
+          </ha-list-item>
+        </ha-button-menu>
         <div class="content">
           ${this._error &&
           html`<ha-alert alert-type="error">${this._error}</ha-alert>`}
@@ -123,6 +151,41 @@ class HaConfigBackupDetails extends LitElement {
     }
   }
 
+  private _handleAction(ev: CustomEvent<ActionDetail>) {
+    switch (ev.detail.index) {
+      case 0:
+        this._downloadBackup();
+        break;
+      case 1:
+        this._deleteBackup();
+        break;
+    }
+  }
+
+  private async _downloadBackup(): Promise<void> {
+    const signedUrl = await getSignedPath(
+      this.hass,
+      getBackupDownloadUrl(this._backup!.backup_id)
+    );
+    fileDownload(signedUrl.path);
+  }
+
+  private async _deleteBackup(): Promise<void> {
+    const confirm = await showConfirmationDialog(this, {
+      title: "Delete backup",
+      text: "This backup will be permanently deleted.",
+      confirmText: this.hass.localize("ui.common.delete"),
+      destructive: true,
+    });
+
+    if (!confirm) {
+      return;
+    }
+
+    await removeBackup(this.hass, this._backup!.backup_id);
+    navigate("/config/backup");
+  }
+
   static styles = css`
     .content {
       padding: 28px 20px 0;
@@ -144,6 +207,12 @@ class HaConfigBackupDetails extends LitElement {
     }
     ha-md-list-item img {
       width: 48px;
+    }
+    .warning {
+      color: var(--error-color);
+    }
+    .warning ha-svg-icon {
+      color: var(--error-color);
     }
   `;
 }
