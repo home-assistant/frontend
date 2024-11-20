6a337cc486c637ddff55f746ac3769819bab2946
Simon Lamon <32477463+silamon@users.noreply.github.com>
2024-11-20 17:02:15 +0100

    Remove activate scene in automation editor (#22258)

--- src/data/script.ts
+++ src/data/script.ts
@@ -72,16 +72,6 @@ const playMediaActionStruct: Describe<PlayMediaAction> = assign(
   })
 );
 
-const activateSceneActionStruct: Describe<ServiceSceneAction> = assign(
-  baseActionStruct,
-  object({
-    action: literal("scene.turn_on"),
-    target: optional(object({ entity_id: optional(string()) })),
-    entity_id: optional(string()),
-    metadata: object(),
-  })
-);
-
 export interface ScriptEntity extends HassEntityBase {
   attributes: HassEntityAttributeBase & {
     last_triggered: string;
@@ -160,17 +150,6 @@ export interface DelayActionParts extends BaseAction {
 export interface DelayAction extends BaseAction {
   delay: number | Partial<DelayActionParts> | string;
 }
-
-export interface ServiceSceneAction extends BaseAction {
-  action: "scene.turn_on";
-  target?: { entity_id?: string };
-  entity_id?: string;
-  metadata: Record<string, unknown>;
-}
-export interface LegacySceneAction extends BaseAction {
-  scene: string;
-}
-export type SceneAction = ServiceSceneAction | LegacySceneAction;
 
 export interface WaitAction extends BaseAction {
   wait_template: string;
@@ -272,7 +251,6 @@ export type NonConditionAction =
   | DeviceAction
   | ServiceAction
   | DelayAction
-  | SceneAction
   | WaitAction
   | WaitForTriggerAction
   | RepeatAction
@@ -298,7 +276,6 @@ export interface ActionTypes {
   check_condition: Condition;
   fire_event: EventAction;
   device_action: DeviceAction;
-  activate_scene: SceneAction;
   repeat: RepeatAction;
   choose: ChooseAction;
   if: IfAction;
@@ -381,9 +358,6 @@ export const getActionType = (action: Action): ActionType => {
   if ("device_id" in action) {
     return "device_action";
   }
-  if ("scene" in action) {
-    return "activate_scene";
-  }
   if ("repeat" in action) {
     return "repeat";
   }
@@ -413,9 +387,6 @@ export const getActionType = (action: Action): ActionType => {
   }
   if ("action" in action || "service" in action) {
     if ("metadata" in action) {
-      if (is(action, activateSceneActionStruct)) {
-        return "activate_scene";
-      }
       if (is(action, playMediaActionStruct)) {
         return "play_media";
       }
@@ -445,6 +416,15 @@ export const migrateAutomationAction = (
       action.action = action.service;
     }
     delete action.service;
+  }
+
+  // legacy scene (scene: scene_name)
+  if ("scene" in action) {
+    action.action = "scene.turn_on";
+    action.target = {
+      entity_id: action.scene,
+    };
+    delete action.scene;
   }
 
   if ("sequence" in action) {
