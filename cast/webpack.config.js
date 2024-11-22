3d7ce38b0d176fd1942069fa245d80c7d58b42f4
Wendelin <w@pe8.at>
2024-11-22 11:21:31 +0100

    Remove unused webpack configs

--- cast/webpack.config.js
+++
@@ -1,8 +0,0 @@
-import rspack from "../build-scripts/rspack.cjs";
-import env from "../build-scripts/env.cjs";
-
-export default rspack.createCastConfig({
-  isProdBuild: env.isProdBuild(),
-  isStatsBuild: env.isStatsBuild(),
-  latestBuild: true,
-});
--- gallery/webpack.config.js
+++
@@ -1,8 +0,0 @@
-import rspack from "../build-scripts/rspack.cjs";
-import env from "../build-scripts/env.cjs";
-
-export default rspack.createGalleryConfig({
-  isProdBuild: env.isProdBuild(),
-  isStatsBuild: env.isStatsBuild(),
-  latestBuild: true,
-});
