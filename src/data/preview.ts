03c05f84b7ede91a9c2ac22156cbb02bf3c311e8
Dave T <17680170+davet2001@users.noreply.github.com>
2024-11-25 21:48:03 +0000

    Revert unintentional change

--- src/data/preview.ts
+++ src/data/preview.ts
@@ -12,11 +12,15 @@ export const subscribePreviewGeneric = (
   hass: HomeAssistant,
   domain: string,
   flow_id: string,
+  flow_type: "config_flow" | "options_flow",
+  user_input: Record<string, any>,
   callback: (preview: GenericPreview) => void
 ): Promise<UnsubscribeFunc> =>
   hass.connection.subscribeMessage(callback, {
     type: `${domain}/start_preview`,
     flow_id,
+    flow_type,
+    user_input,
   });
 
 export const previewModule = (domain: string): string =>
