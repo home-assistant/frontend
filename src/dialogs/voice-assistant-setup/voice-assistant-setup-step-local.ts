0993b77086cd27ad061290e94ffde750559cf458
Bram Kragten <mail@bramkragten.nl>
2024-12-04 13:51:00 +0100

    Update voice-assistant-setup-step-local.ts

--- src/dialogs/voice-assistant-setup/voice-assistant-setup-step-local.ts
+++ src/dialogs/voice-assistant-setup/voice-assistant-setup-step-local.ts
@@ -24,7 +24,6 @@ import type { HomeAssistant } from "../../types";
 import { documentationUrl } from "../../util/documentation-url";
 import { AssistantSetupStyles } from "./styles";
 import { STEP } from "./voice-assistant-setup-dialog";
-import { nextRender } from "../../common/util/render-status";
 
 @customElement("ha-voice-assistant-setup-step-local")
 export class HaVoiceAssistantSetupStepLocal extends LitElement {
@@ -333,9 +332,6 @@ export class HaVoiceAssistantSetupStepLocal extends LitElement {
       this._localTts[0].entity_id,
       this._localStt[0].entity_id
     );
-
-    // wait a render so the `hui-select-entity-row` is also updated and doesn't undo the select action
-    await nextRender();
 
     await this.hass.callService(
       "select",
