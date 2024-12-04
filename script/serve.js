a5da4a062b58af654fc5edccec0ed537180a3c19
Marte Tassyns <marte.tassyns@outlook.be>
2024-12-04 15:42:36 +0000

    forward locally made api calls to the backend

--- script/serve.js
+++ script/serve.js
@@ -2,16 +2,33 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 
-const port = parseInt(process.argv[2] ?? "8123");
+const coreUrl = process.argv[2];
+const port = parseInt(process.argv[3] ?? "8123");
+
 const repoDir = path.join(fileURLToPath(import.meta.url), "../..");
+
+import { createProxyMiddleware } from "http-proxy-middleware";
+
+const coreProxy = createProxyMiddleware({
+  target: coreUrl,
+  changeOrigin: true,
+});
 
 const app = express();
 app.use("/", express.static(path.join(repoDir, "hass_frontend")));
 app.use("/api/hassio/app", express.static(path.join(repoDir, "hassio/build")));
+app.use("/api", coreProxy);
 app.get("*", (req, res) => {
   res.sendFile(path.join(repoDir, "hass_frontend/index.html"));
 });
 
-app.listen(port, () => {
-  console.log(`Running at http://localhost:${port}`);
+var server = app.listen(port, () => {
+  console.log(
+    `Running at http://localhost:${port}, connected to core on ${coreUrl}`
+  );
+});
+
+process.on("SIGINT", function () {
+  console.log("Shutting down file server");
+  server.close();
 });
