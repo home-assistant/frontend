e132aafe6057fda584f364110534f577f67fdaee
Petar Petrov <MindFreeze@users.noreply.github.com>
2024-12-05 12:01:21 +0200

    Remove Zopfli compression

--- build-scripts/gulp/compress.js
+++ build-scripts/gulp/compress.js
@@ -3,7 +3,6 @@
 import { constants } from "node:zlib";
 import gulp from "gulp";
 import brotli from "gulp-brotli";
-import zopfli from "gulp-zopfli-green";
 import paths from "../paths.cjs";
 
 const filesGlob = "*.{js,json,css,svg,xml}";
@@ -13,7 +12,6 @@ const brotliOptions = {
     [constants.BROTLI_PARAM_QUALITY]: constants.BROTLI_MAX_QUALITY,
   },
 };
-const zopfliOptions = { threshold: 150 };
 
 const compressDistBrotli = (rootDir, modernDir, compressServiceWorker = true) =>
   gulp
@@ -29,19 +27,6 @@ const compressDistBrotli = (rootDir, modernDir, compressServiceWorker = true) =>
     .pipe(brotli(brotliOptions))
     .pipe(gulp.dest(rootDir));
 
-const compressDistZopfli = (rootDir, modernDir, compressModern = false) =>
-  gulp
-    .src(
-      [
-        `${rootDir}/**/${filesGlob}`,
-        compressModern ? undefined : `!${modernDir}/**/${filesGlob}`,
-        `!${rootDir}/{sw-modern,service_worker}.js`,
-        `${rootDir}/{authorize,onboarding}.html`,
-      ].filter(Boolean),
-      { base: rootDir }
-    )
-    .pipe(zopfli(zopfliOptions))
-    .pipe(gulp.dest(rootDir));
 
 const compressAppBrotli = () =>
   compressDistBrotli(paths.app_output_root, paths.app_output_latest);
@@ -52,17 +37,8 @@ const compressHassioBrotli = () =>
     false
   );
 
-const compressAppZopfli = () =>
-  compressDistZopfli(paths.app_output_root, paths.app_output_latest);
-const compressHassioZopfli = () =>
-  compressDistZopfli(
-    paths.hassio_output_root,
-    paths.hassio_output_latest,
-    true
-  );
-
-gulp.task("compress-app", gulp.parallel(compressAppBrotli, compressAppZopfli));
+gulp.task("compress-app", gulp.parallel(compressAppBrotli));
 gulp.task(
   "compress-hassio",
-  gulp.parallel(compressHassioBrotli, compressHassioZopfli)
+  gulp.parallel(compressHassioBrotli)
 );
