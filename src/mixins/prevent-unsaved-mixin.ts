96b9d25bc57a237eb55a341ca956d6a3ea842e09
Jan-Philipp Benecke <jan-philipp@bnck.me>
2024-12-06 12:02:26 +0100

    Process code review

--- src/mixins/prevent-unsaved-mixin.ts
+++ src/mixins/prevent-unsaved-mixin.ts
@@ -1,48 +1,57 @@
-import type { LitElement } from "lit";
+import type { LitElement, PropertyValues } from "lit";
 import type { Constructor } from "../types";
 import { isNavigationClick } from "../common/dom/is-navigation-click";
-import { navigate } from "../common/navigate";
+import { fireEvent } from "../common/dom/fire_event";
 
 export const PreventUnsavedMixin = <T extends Constructor<LitElement>>(
   superClass: T
 ) =>
   class extends superClass {
     private _handleClick = async (e: MouseEvent) => {
-      const href = isNavigationClick(e);
-
-      if (!href) {
+      // get the right target, otherwise the composedPath would return <home-assistant> in the new event
+      const target = e
+        .composedPath()
+        .find(
+          (n) => (n as HTMLElement).tagName === "HA-SVG-ICON"
+        ) as HTMLAnchorElement;
+      if (!isNavigationClick(e)) {
         return;
       }
 
-      e.preventDefault();
       const result = await this.promptDiscardChanges();
       if (result) {
-        navigate(href);
+        this._removeListeners();
+        if (target) {
+          const newEvent = new MouseEvent(e.type, e);
+          target.dispatchEvent(newEvent);
+        }
+      } else {
+        fireEvent(this, "hass-reset-sidebar", this.getPanel());
       }
     };
 
-    private _handleUnload = (e: BeforeUnloadEvent) => {
+    private _handleUnload = (e: BeforeUnloadEvent) => e.preventDefault();
+
+    private _removeListeners() {
+      window.removeEventListener("click", this._handleClick, true);
+      window.removeEventListener("beforeunload", this._handleUnload);
+    }
+
+    public willUpdate(changedProperties: PropertyValues): void {
+      super.willUpdate(changedProperties);
+
       if (this.isDirty()) {
-        e.preventDefault();
+        window.addEventListener("click", this._handleClick, true);
+        window.addEventListener("beforeunload", this._handleUnload);
+      } else {
+        this._removeListeners();
       }
-    };
-
-    public connectedCallback(): void {
-      super.connectedCallback();
-
-      document.body.addEventListener("mousedown", this._handleClick, {
-        capture: true,
-      });
-      window.addEventListener("beforeunload", this._handleUnload);
     }
 
     public disconnectedCallback(): void {
       super.disconnectedCallback();
 
-      document.body.removeEventListener("click", this._handleClick, {
-        capture: true,
-      });
-      window.removeEventListener("beforeunload", this._handleUnload);
+      this._removeListeners();
     }
 
     protected isDirty(): boolean {
@@ -51,5 +60,9 @@ export const PreventUnsavedMixin = <T extends Constructor<LitElement>>(
 
     protected async promptDiscardChanges(): Promise<boolean> {
       return true;
+    }
+
+    protected getPanel(): string {
+      return "config";
     }
   };
