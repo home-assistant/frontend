62c66e241070303270170d93e107b846f94f83c7
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2024-12-10 07:36:58 +0300

    prettier

--- src/panels/developer-tools/template/developer-tools-template.ts
+++ src/panels/developer-tools/template/developer-tools-template.ts
@@ -285,11 +285,7 @@ class HaPanelDevTemplate extends LitElement {
         }
 
         .content.horizontal {
-          --code-mirror-max-height: calc(
-            100vh - var(--header-height) - var(--paper-font-body1_-_line-height) * 3 - 1em * 2
-            - max(16px,env(safe-area-inset-top)) * 2 - max(16px,env(safe-area-inset-bottom)) * 2
-            - var(--ha-card-border-width,1px) * 2 - 179px
-          );
+          --code-mirror-max-height: calc(100vh - 389px);
         }
 
         ha-card {
