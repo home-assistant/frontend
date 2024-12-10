eb888e57cbe3d82c0a701422bf70d6e0a294eefb
Jan-Philipp Benecke <jan-philipp@bnck.me>
2024-12-10 09:02:26 +0100

    Remove id on sidebar

--- src/components/ha-sidebar.ts
+++ src/components/ha-sidebar.ts
@@ -408,7 +408,6 @@ class HaSidebar extends SubscribeMixin(LitElement) {
     // prettier-ignore
     return html`
       <paper-listbox
-          id="sidebar"
         attr-for-selected="data-panel"
         class="ha-scrollbar"
         .selected=${selectedPanel}
