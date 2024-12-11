9c14c73509a97a5fbf5c5eca3078bc721c454132
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2024-12-11 13:03:18 +0300

    prettier
    Co-authored-by: Wendelin <12148533+wendevlin@users.noreply.github.com>

--- src/panels/developer-tools/template/developer-tools-template.ts
+++ src/panels/developer-tools/template/developer-tools-template.ts
@@ -185,7 +185,8 @@ class HaPanelDevTemplate extends LitElement {
                     class="rendered ${classMap({
                       [resultType]: resultType,
                     })}"
-                  >${type === "object"
+                  >
+${type === "object"
                       ? JSON.stringify(this._templateResult.result, null, 2)
                       : this._templateResult.result}</pre
                   >
