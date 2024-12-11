b585be128d3e074209bcff73b5c7ce595cf163c1
Petar Petrov <MindFreeze@users.noreply.github.com>
2024-12-11 13:59:47 +0200

    fix

--- src/common/navigate.ts
+++ src/common/navigate.ts
@@ -14,42 +14,46 @@ export interface NavigateOptions {
   data?: any;
 }
 
-export const navigate = async (path: string, options?: NavigateOptions) => {
-  const { history } = mainWindow;
-  let replace = options?.replace || false;
+export const navigate = async (path: string, options?: NavigateOptions) =>
+  new Promise<boolean>((resolve) => {
+    // need to wait for history state to be updated in case a dialog was closed before calling navigate
+    setTimeout(async () => {
+      const { history } = mainWindow;
+      let replace = options?.replace || false;
+      if (history.state?.dialog) {
+        const closed = await closeAllDialogs();
+        if (!closed) {
+          // eslint-disable-next-line no-console
+          console.warn("Navigation blocked, because dialog refused to close");
+          resolve(false);
+          return;
+        }
+        // if there were open dialogs, we discard the current state
+        replace = true;
+      }
 
-  if (history.state?.dialog) {
-    const closed = await closeAllDialogs();
-    if (!closed) {
-      // eslint-disable-next-line no-console
-      console.warn("Navigation blocked, because dialog refused to close");
-      return false;
-    }
-    // if there were open dialogs, we discard the current state
-    replace = true;
-  }
-
-  if (__DEMO__) {
-    if (replace) {
-      history.replaceState(
-        history.state?.root ? { root: true } : (options?.data ?? null),
-        "",
-        `${mainWindow.location.pathname}#${path}`
-      );
-    } else {
-      mainWindow.location.hash = path;
-    }
-  } else if (replace) {
-    history.replaceState(
-      history.state?.root ? { root: true } : (options?.data ?? null),
-      "",
-      path
-    );
-  } else {
-    history.pushState(options?.data ?? null, "", path);
-  }
-  fireEvent(mainWindow, "location-changed", {
-    replace,
+      if (__DEMO__) {
+        if (replace) {
+          history.replaceState(
+            history.state?.root ? { root: true } : (options?.data ?? null),
+            "",
+            `${mainWindow.location.pathname}#${path}`
+          );
+        } else {
+          mainWindow.location.hash = path;
+        }
+      } else if (replace) {
+        history.replaceState(
+          history.state?.root ? { root: true } : (options?.data ?? null),
+          "",
+          path
+        );
+      } else {
+        history.pushState(options?.data ?? null, "", path);
+      }
+      fireEvent(mainWindow, "location-changed", {
+        replace,
+      });
+      resolve(true);
+    });
   });
-  return true;
-};
