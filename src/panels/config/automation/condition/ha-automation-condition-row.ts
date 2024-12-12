165aa12708f88988d5b35f7c1774966ed68c9e57
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2024-12-12 07:31:25 +0300

    Update ha-automation-condition-row.ts

--- src/panels/config/automation/condition/ha-automation-condition-row.ts
+++ src/panels/config/automation/condition/ha-automation-condition-row.ts
@@ -536,8 +536,8 @@ export default class HaAutomationConditionRow extends LitElement {
         .disabled-bar {
           background: var(--divider-color, #e0e0e0);
           text-align: center;
-          border-top-right-radius: var(--ha-card-border-radius);
-          border-top-left-radius: var(--ha-card-border-radius);
+          border-top-right-radius: var(--ha-card-border-radius, 12px);
+          border-top-left-radius: var(--ha-card-border-radius, 12px);
         }
         ha-list-item[disabled] {
           --mdc-theme-text-primary-on-background: var(--disabled-text-color);
