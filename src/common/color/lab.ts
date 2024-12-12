27ce395d683869f3e97cf1eb7701165e18d91962
Wendelin <12148533+wendevlin@users.noreply.github.com>
2024-12-12 14:44:17 +0100

    Common-color-tests (#23258)

--- src/common/color/lab.ts
+++ src/common/color/lab.ts
@@ -1,12 +1,13 @@
 // From https://github.com/gka/chroma.js
 // Copyright (c) 2011-2019, Gregor Aisch
 
-export const labDarken = (
-  lab: [number, number, number],
-  amount = 1
-): [number, number, number] => [lab[0] - 18 * amount, lab[1], lab[2]];
+export type LabColor = [number, number, number];
 
-export const labBrighten = (
-  lab: [number, number, number],
-  amount = 1
-): [number, number, number] => labDarken(lab, -amount);
+export const labDarken = (lab: LabColor, amount = 1): LabColor => [
+  lab[0] - 18 * amount,
+  lab[1],
+  lab[2],
+];
+
+export const labBrighten = (lab: LabColor, amount = 1): LabColor =>
+  labDarken(lab, -amount);
