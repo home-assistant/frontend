b9ef3350c27cdff8765357a5d9fa364b904a9be4
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2024-12-12 08:36:25 +0300

    Update ha-automation-condition-row.ts

--- src/panels/config/automation/condition/ha-automation-condition-row.ts
+++ src/panels/config/automation/condition/ha-automation-condition-row.ts
@@ -514,7 +514,6 @@ export default class HaAutomationConditionRow extends LitElement {
         ha-expansion-panel {
           --expansion-panel-summary-padding: 0 0 0 8px;
           --expansion-panel-content-padding: 0;
-          --ha-card-border-radius: 0;
         }
         h3 {
           margin: 0;
