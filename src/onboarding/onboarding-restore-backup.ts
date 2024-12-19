580ba1ac5df3e09a8c9f534d69b5aa2311ec3fd2
Wendelin <w@pe8.at>
2024-12-19 09:23:27 +0100

    Improve onboarding backup restore

--- src/onboarding/onboarding-restore-backup.ts
+++ src/onboarding/onboarding-restore-backup.ts
@@ -1,12 +1,13 @@
-import "@material/mwc-button/mwc-button";
 import type { CSSResultGroup, TemplateResult } from "lit";
-import { css, html, LitElement } from "lit";
+import { css, html, LitElement, nothing } from "lit";
 import { customElement, property, state } from "lit/decorators";
 import { showHassioBackupDialog } from "../../hassio/src/dialogs/backup/show-dialog-hassio-backup";
 import "../../hassio/src/components/hassio-upload-backup";
 import type { LocalizeFunc } from "../common/translations/localize";
 import "../components/ha-ansi-to-html";
 import "../components/ha-card";
+import "../components/ha-alert";
+import "../components/ha-button";
 import { fetchInstallationType } from "../data/onboarding";
 import type { HomeAssistant } from "../types";
 import "./onboarding-loading";
@@ -16,46 +17,82 @@ import { navigate } from "../common/navigate";
 
 @customElement("onboarding-restore-backup")
 class OnboardingRestoreBackup extends LitElement {
-  @property({ attribute: false }) public hass!: HomeAssistant;
+  @property({ attribute: false }) public hass?: HomeAssistant;
 
   @property({ attribute: false }) public localize!: LocalizeFunc;
 
   @property() public language!: string;
 
-  @state() public _restoring = false;
+  @state() private _restoring = false;
+
+  @state() private _error?: string;
+
+  @state() private _backupSlug?: string;
 
   protected render(): TemplateResult {
-    return html`${this._restoring
+    return html`
+      ${this._error
         ? html`<h1>
-              ${this.localize("ui.panel.page-onboarding.restore.in_progress")}
+              ${this.localize("ui.panel.page-onboarding.restore.failed")}
             </h1>
-            <onboarding-loading></onboarding-loading>`
-        : html` <h1>
-              ${this.localize("ui.panel.page-onboarding.restore.header")}
-            </h1>
-            <hassio-upload-backup
-              @backup-uploaded=${this._backupUploaded}
-              .hass=${this.hass}
-            ></hassio-upload-backup>`}
+            <p>${this._error}</p>`
+        : this._restoring
+          ? html`<h1>
+                ${this.localize("ui.panel.page-onboarding.restore.in_progress")}
+              </h1>
+              <ha-alert alert-type="info">
+                ${this.localize("ui.panel.page-onboarding.restore.in_progress")}
+              </ha-alert>
+              <onboarding-loading></onboarding-loading>`
+          : html` <h1>
+                ${this.localize("ui.panel.page-onboarding.restore.header")}
+              </h1>
+              <hassio-upload-backup
+                @backup-uploaded=${this._backupUploaded}
+                @backup-cleared=${this._backupCleared}
+                .hass=${this.hass}
+                .localize=${this.localize}
+              ></hassio-upload-backup>`}
       <div class="footer">
-        <mwc-button @click=${this._back} .disabled=${this._restoring}>
+        <ha-button @click=${this._back} .disabled=${!!this._restoring}>
           ${this.localize("ui.panel.page-onboarding.back")}
-        </mwc-button>
-      </div> `;
+        </ha-button>
+        ${!this._error && this._backupSlug
+          ? html`<ha-button @click=${this._showBackupDialog}>
+              ${this.localize("ui.panel.page-onboarding.restore.restore")}
+            </ha-button>`
+          : nothing}
+        ${this._error
+          ? html`<ha-button @click=${this._retry}>
+              ${this.localize("ui.panel.page-onboarding.restore.retry")}
+            </ha-button>`
+          : nothing}
+      </div>
+    `;
   }
 
   private _back(): void {
     navigate(`${location.pathname}?${removeSearchParam("page")}`);
   }
 
+  private _retry(): void {
+    this._error = undefined;
+    this._restoring = false;
+    this._backupSlug = undefined;
+  }
+
   private _backupUploaded(ev) {
     const backup = ev.detail.backup;
-    this._showBackupDialog(backup.slug);
+    this._backupSlug = backup.slug;
+    this._showBackupDialog();
+  }
+
+  private _backupCleared() {
+    this._backupSlug = undefined;
   }
 
   protected firstUpdated(changedProps) {
     super.firstUpdated(changedProps);
-    setInterval(() => this._checkRestoreStatus(), 1000);
   }
 
   private async _checkRestoreStatus(): Promise<void> {
@@ -73,13 +110,17 @@ class OnboardingRestoreBackup extends LitElement {
     }
   }
 
-  private _showBackupDialog(slug: string): void {
+  private _scheduleCheckRestoreStatus(): void {
+    setTimeout(() => this._checkRestoreStatus(), 1000);
+  }
+
+  private _showBackupDialog(): void {
     showHassioBackupDialog(this, {
-      slug,
+      slug: this._backupSlug!,
       onboarding: true,
       localize: this.localize,
       onRestoring: () => {
-        this._restoring = true;
+        this._scheduleCheckRestoreStatus();
       },
     });
   }
@@ -97,8 +138,9 @@ class OnboardingRestoreBackup extends LitElement {
           width: 100%;
         }
         .footer {
+          display: flex;
+          justify-content: space-between;
           width: 100%;
-          text-align: left;
         }
       `,
     ];
