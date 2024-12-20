d797168f764f10cbfe141c0816df396e09e16e9d
Paul Bottein <paul.bottein@gmail.com>
2024-12-20 13:53:22 +0100

    Revert remove hass debug connection in dev

--- src/state/connection-mixin.ts
+++ src/state/connection-mixin.ts
@@ -89,7 +89,7 @@ export const connectionMixin = <T extends Constructor<HassBaseEl>>(
           notifyOnError = true,
           returnResponse = false
         ) => {
-          if (this.hass?.debugConnection) {
+          if (__DEV__ || this.hass?.debugConnection) {
             // eslint-disable-next-line no-console
             console.log(
               "Calling service",
@@ -115,7 +115,7 @@ export const connectionMixin = <T extends Constructor<HassBaseEl>>(
             ) {
               return { context: { id: "" } };
             }
-            if (this.hass?.debugConnection) {
+            if (__DEV__ || this.hass?.debugConnection) {
               // eslint-disable-next-line no-console
               console.error(
                 "Error calling service",
@@ -167,7 +167,7 @@ export const connectionMixin = <T extends Constructor<HassBaseEl>>(
         ) => fetchWithAuth(auth, `${auth.data.hassUrl}${path}`, init),
         // For messages that do not get a response
         sendWS: (msg) => {
-          if (this.hass?.debugConnection) {
+          if (__DEV__ || this.hass?.debugConnection) {
             // eslint-disable-next-line no-console
             console.log("Sending", msg);
           }
@@ -175,14 +175,14 @@ export const connectionMixin = <T extends Constructor<HassBaseEl>>(
         },
         // For messages that expect a response
         callWS: <R>(msg) => {
-          if (this.hass?.debugConnection) {
+          if (__DEV__ || this.hass?.debugConnection) {
             // eslint-disable-next-line no-console
             console.log("Sending", msg);
           }
 
           const resp = conn.sendMessagePromise<R>(msg);
 
-          if (this.hass?.debugConnection) {
+          if (__DEV__ || this.hass?.debugConnection) {
             resp.then(
               // eslint-disable-next-line no-console
               (result) => console.log("Received", result),
