3b6cafdcc5dece462d38a7943e9f943bcbd54ebe
Petar Petrov <MindFreeze@users.noreply.github.com>
2024-12-27 13:44:05 +0100

    Revert "Automation/Script editor border-radius fix (#23267)"
    This reverts commit 45c06815faf3b4de47b475798d7875f269b3c265.

--- src/panels/config/automation/action/ha-automation-action-row.ts
+++ src/panels/config/automation/action/ha-automation-action-row.ts
@@ -614,9 +614,6 @@ export default class HaAutomationActionRow extends LitElement {
         ha-icon-button {
           --mdc-theme-text-primary-on-background: var(--primary-text-color);
         }
-        ha-card {
-          overflow: hidden;
-        }
         .disabled {
           opacity: 0.5;
           pointer-events: none;
@@ -649,6 +646,8 @@ export default class HaAutomationActionRow extends LitElement {
         .disabled-bar {
           background: var(--divider-color, #e0e0e0);
           text-align: center;
+          border-top-right-radius: var(--ha-card-border-radius);
+          border-top-left-radius: var(--ha-card-border-radius);
         }
 
         mwc-list-item[disabled] {
