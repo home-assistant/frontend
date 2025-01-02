ab447f06197ab3426efabbb1fc05e66e98bc16c1
Bram Kragten <mail@bramkragten.nl>
2025-01-02 16:11:50 +0100

    always zoom timeline charts on x axis (#23554)

--- src/components/chart/ha-chart-base.ts
+++ src/components/chart/ha-chart-base.ts
@@ -433,7 +433,10 @@ export class HaChartBase extends LitElement {
               speed: 0.05,
             },
             mode:
-              (this.options?.scales?.y as any)?.type === "category" ? "y" : "x",
+              this.chartType !== "timeline" &&
+              (this.options?.scales?.y as any)?.type === "category"
+                ? "y"
+                : "x",
             onZoomComplete: () => {
               const isZoomed = this.chart?.isZoomedOrPanned() ?? false;
               if (this._isZoomed && !isZoomed) {
