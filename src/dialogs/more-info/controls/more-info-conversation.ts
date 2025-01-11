799ea461bf7a472532afbc8f1f6a1179f2213fc5
Paulus Schoutsen <balloob@gmail.com>
2025-01-11 03:45:15 +0000

    Add more info dialog for conversation agent

---
+++ src/dialogs/more-info/controls/more-info-conversation.ts
@@ -0,0 +1,82 @@
+import type { HassEntity } from "home-assistant-js-websocket";
+import type { PropertyValues } from "lit";
+import { html, LitElement, nothing } from "lit";
+import { customElement, property, state } from "lit/decorators";
+import "../../../components/ha-attributes";
+import type { HomeAssistant } from "../../../types";
+import "../../../components/ha-assist-chat";
+import "../../../components/ha-circular-progress";
+import "../../../components/ha-alert";
+import type { AssistPipeline } from "../../../data/assist_pipeline";
+import { getAssistPipeline } from "../../../data/assist_pipeline";
+
+@customElement("more-info-conversation")
+class MoreInfoConversation extends LitElement {
+  @property({ attribute: false }) public hass!: HomeAssistant;
+
+  @property({ attribute: false }) public stateObj?: HassEntity;
+
+  @state() public _pipeline?: AssistPipeline;
+
+  @state() private _errorLoadAssist?: "not_found" | "unknown";
+
+  protected willUpdate(changedProperties: PropertyValues): void {
+    if (changedProperties.has("stateObj") && this.stateObj) {
+      this._getPipeline();
+    }
+  }
+
+  private async _getPipeline() {
+    const pipelineId = this.stateObj!.entity_id;
+    try {
+      const pipeline = await getAssistPipeline(this.hass, pipelineId);
+      // Verify the pipeline is still the same.
+      if (this.stateObj && pipelineId === this.stateObj.entity_id) {
+        this._pipeline = pipeline;
+      }
+    } catch (e: any) {
+      if (e.code === "not_found") {
+        this._errorLoadAssist = "not_found";
+      } else {
+        this._errorLoadAssist = "unknown";
+        // eslint-disable-next-line no-console
+        console.error(e);
+      }
+    }
+  }
+
+  protected render() {
+    if (!this.hass || !this.stateObj) {
+      return nothing;
+    }
+
+    return html`
+      ${this._pipeline
+        ? html`
+            <ha-assist-chat
+              .hass=${this.hass}
+              .pipeline=${this._pipeline}
+            ></ha-assist-chat>
+          `
+        : html`<div class="pipelines-loading">
+            <ha-circular-progress
+              indeterminate
+              size="large"
+            ></ha-circular-progress>
+          </div>`}
+      ${this._errorLoadAssist
+        ? html`<ha-alert alert-type="error">
+            ${this.hass.localize(
+              `ui.dialogs.voice_command.${this._errorLoadAssist}_error_load_assist`
+            )}
+          </ha-alert>`
+        : nothing}
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "more-info-conversation": MoreInfoConversation;
+  }
+}
