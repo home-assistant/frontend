7ba7a74f5863203182f9bf7c742ed0fe208ff6f0
Paul Bottein <paul.bottein@gmail.com>
2025-01-17 17:37:51 +0100

    Use state name provided by backend

--- src/common/entity/compute_entity_name.ts
+++ src/common/entity/compute_entity_name.ts
@@ -5,30 +5,6 @@ import { computeDeviceName } from "./compute_device_name";
 import { computeStateName } from "./compute_state_name";
 import { stripPrefixFromEntityName } from "./strip_prefix_from_entity_name";
 
-export const computeEntityFullName = (
-  stateObj: HassEntity,
-  hass: HomeAssistant
-): string | undefined => {
-  const entry = hass.entities[stateObj.entity_id] as
-    | EntityRegistryDisplayEntry
-    | undefined;
-
-  const entityName = computeEntityName(stateObj, hass);
-
-  if (!entry?.has_entity_name) {
-    return entityName;
-  }
-
-  const device = entry?.device_id ? hass.devices[entry.device_id] : undefined;
-  const deviceName = device ? computeDeviceName(device) : undefined;
-
-  if (!entityName || !deviceName || entityName === deviceName) {
-    return entityName || deviceName;
-  }
-
-  return `${deviceName} ${entityName}`;
-};
-
 export const computeEntityName = (
   stateObj: HassEntity,
   hass: HomeAssistant
