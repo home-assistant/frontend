a5bde6858f1289302e7a6184341a2754b52eed66
Wendelin <w@pe8.at>
2025-01-17 09:26:43 +0100

    Add handler for unknown error and wrong encryption key

--- src/panels/config/backup/helper/download_backup.ts
+++ src/panels/config/backup/helper/download_backup.ts
@@ -8,36 +8,68 @@ import {
 import type { HomeAssistant } from "../../../../types";
 import {
   showAlertDialog,
+  showConfirmationDialog,
   showPromptDialog,
 } from "../../../lovelace/custom-card-helpers";
 import { getSignedPath } from "../../../../data/auth";
 import { fileDownload } from "../../../../util/file_download";
 
+const triggerDownload = async (
+  hass: HomeAssistant,
+  backupId: string,
+  preferedAgent: string,
+  encryptionKey?: string | null
+) => {
+  const signedUrl = await getSignedPath(
+    hass,
+    getBackupDownloadUrl(backupId, preferedAgent, encryptionKey)
+  );
+  fileDownload(signedUrl.path);
+};
+
+const downloadEncryptedBackup = async (
+  hass: HomeAssistant,
+  element: LitElement,
+  backup: BackupContent,
+  agentId?: string
+) => {
+  if (
+    await showConfirmationDialog(element, {
+      title: "Encryption key incorrect",
+      text: hass.localize(
+        "ui.panel.config.backup.dialogs.download.incorrect_entered_encryption_key"
+      ),
+      confirmText: "Download encrypted",
+    })
+  ) {
+    triggerDownload(
+      hass,
+      backup.backup_id,
+      agentId ?? getPreferredAgentForDownload(backup.agent_ids!)
+    );
+  }
+};
+
 const requestEncryptionKey = async (
   hass: HomeAssistant,
   element: LitElement,
   backup: BackupContent,
-  userProvided: boolean,
   agentId?: string
 ): Promise<void> => {
   const encryptionKey = await showPromptDialog(element, {
     title: hass.localize(
       "ui.panel.config.backup.dialogs.show_encryption_key.title"
     ),
-    text: userProvided
-      ? hass.localize(
-          "ui.panel.config.backup.dialogs.download.incorrect_entered_encryption_key"
-        )
-      : hass.localize(
-          "ui.panel.config.backup.dialogs.download.incorrect_current_encryption_key"
-        ),
+    text: hass.localize(
+      "ui.panel.config.backup.dialogs.download.incorrect_current_encryption_key"
+    ),
     inputLabel: hass.localize(
       "ui.panel.config.backup.dialogs.show_encryption_key.title"
     ),
     inputType: "password",
     confirmText: hass.localize("ui.common.download"),
   });
-  if (!encryptionKey) {
+  if (encryptionKey === null) {
     return;
   }
   downloadBackup(hass, element, backup, encryptionKey, agentId, true);
@@ -65,27 +97,50 @@ export const downloadBackup = async (
         );
       } catch (err: any) {
         if (err?.code === "password_incorrect") {
-          requestEncryptionKey(hass, element, backup, userProvided, agentId);
+          if (userProvided) {
+            downloadEncryptedBackup(hass, element, backup, agentId);
+          } else {
+            requestEncryptionKey(hass, element, backup, agentId);
+          }
           return;
         }
         if (err?.code === "decrypt_not_supported") {
           showAlertDialog(element, {
+            title: hass.localize(
+              "ui.panel.config.backup.dialogs.download.decryption_unsupported_title"
+            ),
             text: hass.localize(
               "ui.panel.config.backup.dialogs.download.decryption_unsupported"
             ),
+            confirm() {
+              triggerDownload(hass, backup.backup_id, preferedAgent);
+            },
           });
           encryptionKey = undefined;
+          return;
         }
+
+        showAlertDialog(element, {
+          title: hass.localize(
+            "ui.panel.config.backup.dialogs.download.error_check_title",
+            {
+              error: err.message,
+            }
+          ),
+          text: hass.localize(
+            "ui.panel.config.backup.dialogs.download.error_check_description",
+            {
+              error: err.message,
+            }
+          ),
+        });
+        return;
       }
     } else {
-      requestEncryptionKey(hass, element, backup, userProvided, agentId);
+      requestEncryptionKey(hass, element, backup, agentId);
       return;
     }
   }
 
-  const signedUrl = await getSignedPath(
-    hass,
-    getBackupDownloadUrl(backup.backup_id, preferedAgent, encryptionKey)
-  );
-  fileDownload(signedUrl.path);
+  await triggerDownload(hass, backup.backup_id, preferedAgent, encryptionKey);
 };
