4bbce1b8aec6f84fd5bfb5e08028deb620f1c4c7
Grzegorz Libiszewski <grzegorz.libiszewski7@gmail.com>
2025-01-20 17:16:16 +0100

    feat: Introduce new css variables for safe area

--- src/resources/ha-style.ts
+++ src/resources/ha-style.ts
@@ -436,6 +436,24 @@ const mainStyles = css`
     --margin-title-ltr: 0 0 0 24px;
     --margin-title-rtl: 0 24px 0 0;
 
+    /* safe-area-insets */
+    --safe-area-inset-top: var(
+      --android-safe-area-inset-top,
+      env(safe-area-inset-top, 0)
+    );
+    --safe-area-inset-bottom: var(
+      --android-safe-area-inset-bottom,
+      env(safe-area-inset-bottom, 0)
+    );
+    --safe-area-inset-left: var(
+      --android-safe-area-inset-left,
+      env(safe-area-inset-left, 0)
+    );
+    --safe-area-inset-right: var(
+      --android-safe-area-inset-right,
+      env(safe-area-inset-right, 0)
+    );
+
     ${unsafeCSS(
       Object.entries(derivedStyles)
         .map(([key, value]) => `--${key}: ${value};`)
