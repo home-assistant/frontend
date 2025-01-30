d8f6b1812c48baa5a1c8e16e6975fc47d522ea4b
Norbert Rittel <norbert@rittel.de>
2025-01-30 11:50:20 +0100

    Add localizable "Actions" label to OAuth credentials picker

--- src/panels/config/application_credentials/ha-config-application-credentials.ts
+++ src/panels/config/application_credentials/ha-config-application-credentials.ts
@@ -106,6 +106,9 @@ export class HaConfigApplicationCredentials extends LitElement {
         },
         actions: {
           title: "",
+          label: localize(
+            "ui.panel.config.generic.headers.actions"
+          ),
           type: "overflow-menu",
           showNarrow: true,
           hideable: false,
