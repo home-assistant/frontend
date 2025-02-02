966bb52f0cd57cd857048b496517b05b7c578b6d
karwosts <karwosts@gmail.com>
2025-02-02 22:07:14 +0000

    Remove the default stub image if we switch to an entity with a picture

--- src/panels/lovelace/cards/hui-picture-entity-card.ts
+++ src/panels/lovelace/cards/hui-picture-entity-card.ts
@@ -22,6 +22,9 @@ import type { PictureEntityCardConfig } from "./types";
 import type { CameraEntity } from "../../../data/camera";
 import type { PersonEntity } from "../../../data/person";
 
+export const STUB_IMAGE =
+  "https://demo.home-assistant.io/stub_config/bedroom.png";
+
 @customElement("hui-picture-entity-card")
 class HuiPictureEntityCard extends LitElement implements LovelaceCard {
   public static async getConfigElement(): Promise<LovelaceCardEditor> {
@@ -46,7 +49,7 @@ class HuiPictureEntityCard extends LitElement implements LovelaceCard {
     return {
       type: "picture-entity",
       entity: foundEntities[0] || "",
-      image: "https://demo.home-assistant.io/stub_config/bedroom.png",
+      image: STUB_IMAGE,
     };
   }
 
