f87de4a6584debcbb07a6ade49a078b57da9d3fd
Petar Petrov <MindFreeze@users.noreply.github.com>
2025-02-06 10:36:58 +0200

    Reduce padding in energy charts and align unit (#24095)

--- src/panels/lovelace/cards/energy/hui-energy-devices-detail-graph-card.ts
+++ src/panels/lovelace/cards/energy/hui-energy-devices-detail-graph-card.ts
@@ -193,9 +193,10 @@ export class HuiEnergyDevicesDetailGraphCard
           icon: "circle",
         },
         grid: {
+          top: 45,
           bottom: 0,
-          left: 5,
-          right: 5,
+          left: 1,
+          right: 1,
           containLabel: true,
         },
       };
