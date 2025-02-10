a26bf80b13b6d5eae27c98b03b6c0cfb56e7dd83
Paul Bottein <paul.bottein@gmail.com>
2025-02-10 15:53:05 +0100

    Fix section border radius (#24159)

--- src/panels/lovelace/sections/hui-grid-section.ts
+++ src/panels/lovelace/sections/hui-grid-section.ts
@@ -240,6 +240,7 @@ export class GridSection extends LitElement implements LovelaceSectionElement {
         .container.edit-mode {
           padding: 8px;
           border-radius: var(--ha-card-border-radius, 12px);
+          border-start-end-radius: 0;
           border: 2px dashed var(--divider-color);
           min-height: var(--row-height);
         }
