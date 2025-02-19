b7ea4f3c586e3c45a0424610169cab31022c57d2
Petar Petrov <MindFreeze@users.noreply.github.com>
2025-02-19 12:07:09 +0200

    remove unneeded symbol set

--- src/components/chart/statistics-chart.ts
+++ src/components/chart/statistics-chart.ts
@@ -511,7 +511,6 @@ export class StatisticsChart extends LitElement {
           if (band && this.chartType === "line") {
             series.stack = `band-${statistic_id}`;
             series.stackStrategy = "all";
-            (series as LineSeriesOption).symbol = "none";
             if (drawBands && type === "max") {
               (series as LineSeriesOption).areaStyle = {
                 color: color + "3F",
