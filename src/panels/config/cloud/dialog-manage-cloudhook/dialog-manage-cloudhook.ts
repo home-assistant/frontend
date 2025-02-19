c5c067ef1959432c19a0278b91767928a8605296
Jan-Philipp Benecke <jan-philipp@bnck.me>
2025-02-19 15:56:29 +0100

    Create copyable textfield component (#24247)

--- src/panels/config/cloud/dialog-manage-cloudhook/dialog-manage-cloudhook.ts
+++ src/panels/config/cloud/dialog-manage-cloudhook/dialog-manage-cloudhook.ts
@@ -1,26 +1,22 @@
 import "@material/mwc-button";
-import { mdiContentCopy, mdiOpenInNew } from "@mdi/js";
+import { mdiOpenInNew } from "@mdi/js";
 import type { CSSResultGroup } from "lit";
 import { css, html, LitElement, nothing } from "lit";
-import { query, state } from "lit/decorators";
+import { state } from "lit/decorators";
 import { fireEvent } from "../../../../common/dom/fire_event";
-import { copyToClipboard } from "../../../../common/util/copy-clipboard";
 import { createCloseHeading } from "../../../../components/ha-dialog";
-import "../../../../components/ha-textfield";
-import type { HaTextField } from "../../../../components/ha-textfield";
 import { showConfirmationDialog } from "../../../../dialogs/generic/show-dialog-box";
 import { haStyle, haStyleDialog } from "../../../../resources/styles";
 import type { HomeAssistant } from "../../../../types";
 import { documentationUrl } from "../../../../util/documentation-url";
-import { showToast } from "../../../../util/toast";
 import type { WebhookDialogParams } from "./show-dialog-manage-cloudhook";
+
+import "../../../../components/ha-copy-textfield";
 
 export class DialogManageCloudhook extends LitElement {
   protected hass?: HomeAssistant;
 
   @state() private _params?: WebhookDialogParams;
-
-  @query("ha-textfield") _input!: HaTextField;
 
   public showDialog(params: WebhookDialogParams) {
     this._params = params;
@@ -82,21 +78,12 @@ export class DialogManageCloudhook extends LitElement {
               <ha-svg-icon .path=${mdiOpenInNew}></ha-svg-icon>
             </a>
           </p>
-          <ha-textfield
-            .label=${this.hass!.localize(
-              "ui.panel.config.cloud.dialog_cloudhook.public_url"
-            )}
+
+          <ha-copy-textfield
+            .hass=${this.hass}
             .value=${cloudhook.cloudhook_url}
-            iconTrailing
-            readOnly
-            @click=${this._focusInput}
-          >
-            <ha-icon-button
-              @click=${this._copyUrl}
-              slot="trailingIcon"
-              .path=${mdiContentCopy}
-            ></ha-icon-button>
-          </ha-textfield>
+            .label=${this.hass!.localize("ui.panel.config.common.copy_link")}
+          ></ha-copy-textfield>
         </div>
 
         <a
@@ -137,24 +124,6 @@ export class DialogManageCloudhook extends LitElement {
     }
   }
 
-  private _focusInput(ev) {
-    const inputElement = ev.currentTarget as HaTextField;
-    inputElement.select();
-  }
-
-  private async _copyUrl(ev): Promise<void> {
-    if (!this.hass) return;
-    ev.stopPropagation();
-    const inputElement = ev.target.parentElement as HaTextField;
-    inputElement.select();
-    const url = this.hass.hassUrl(inputElement.value);
-
-    await copyToClipboard(url);
-    showToast(this, {
-      message: this.hass.localize("ui.common.copied_clipboard"),
-    });
-  }
-
   static get styles(): CSSResultGroup {
     return [
       haStyle,
@@ -162,13 +131,6 @@ export class DialogManageCloudhook extends LitElement {
       css`
         ha-dialog {
           width: 650px;
-        }
-        ha-textfield {
-          display: block;
-        }
-        ha-textfield > ha-icon-button {
-          --mdc-icon-button-size: 24px;
-          --mdc-icon-size: 18px;
         }
         button.link {
           color: var(--primary-color);
