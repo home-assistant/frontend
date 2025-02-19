341314295605dc16f424e8bc6ef5aa572bcd4251
Wendelin <w@pe8.at>
2025-02-19 10:11:41 +0100

    Fix hassio backup restore url

--- src/data/hassio/backup.ts
+++ src/data/hassio/backup.ts
@@ -233,11 +233,11 @@ export const restoreBackup = async (
   type: HassioBackupDetail["type"],
   backupSlug: string,
   backupDetails: HassioPartialBackupCreateParams | HassioFullBackupCreateParams,
-  useSnapshotUrl: boolean
+  useBackupUrl: boolean
 ): Promise<void> => {
   await hass.callApi<HassioResponse<{ job_id: string }>>(
     "POST",
-    `hassio/${useSnapshotUrl ? "snapshots" : "backups"}/${backupSlug}/restore/${type}`,
+    `hassio/${useBackupUrl ? "backups" : "snapshots"}/${backupSlug}/restore/${type}`,
     backupDetails
   );
 };
