38e00be6fdc69122b56b4796998607446f0620e2
Wendelin <w@pe8.at>
2025-02-24 13:18:32 +0100

    Improve styles

--- src/components/ha-tooltip.ts
+++ src/components/ha-tooltip.ts
@@ -2,6 +2,17 @@ import SlTooltip from "@shoelace-style/shoelace/dist/components/tooltip/tooltip.
 import styles from "@shoelace-style/shoelace/dist/components/tooltip/tooltip.styles";
 import { css } from "lit";
 import { customElement } from "lit/decorators";
+import { setDefaultAnimation } from "@shoelace-style/shoelace/dist/utilities/animation-registry";
+
+setDefaultAnimation("tooltip.show", {
+  keyframes: [{ opacity: 0 }, { opacity: 1 }],
+  options: { duration: 150, easing: "ease" },
+});
+
+setDefaultAnimation("tooltip.hide", {
+  keyframes: [{ opacity: 1 }, { opacity: 0 }],
+  options: { duration: 400, easing: "ease" },
+});
 
 @customElement("ha-tooltip")
 export class HaTooltip extends SlTooltip {
@@ -10,9 +21,17 @@ export class HaTooltip extends SlTooltip {
     css`
       :host {
         --sl-tooltip-background-color: var(--secondary-background-color);
+        --sl-tooltip-color: var(--primary-text-color);
+        --sl-tooltip-font-family: var(--ha-font-family);
+        --sl-tooltip-font-size: var(--ha-font-size-s);
+        --sl-tooltip-font-weight: normal;
+        --sl-tooltip-line-height: 1;
         --sl-tooltip-padding: 8px;
-        --sl-tooltip-border-radius: 4px;
-        --sl-tooltip-arrow-size: 8px;
+        --sl-tooltip-border-radius: var(
+          --ha-tooltip-border-radius,
+          --ha-border-radius-s
+        );
+        --sl-tooltip-arrow-size: var(--ha-tooltip-arrow-size, 8px);
       }
     `,
   ];
