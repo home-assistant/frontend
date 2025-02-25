bbaf23e049c7e5ca03d675273d5f80ee0637cbe6
Wendelin <12148533+wendevlin@users.noreply.github.com>
2025-02-25 16:21:32 +0100

    Add lovelace bg opacity preview (#24387)
    * Add lovelace bg opacity preview
    
    * Add yaml preview
    
    * Simplify logic with css var
    
    * Clean up
    
    * Remove important
    
    * Add opacity default to 100

--- src/panels/lovelace/editor/view-editor/hui-view-background-editor.ts
+++ src/panels/lovelace/editor/view-editor/hui-view-background-editor.ts
@@ -38,7 +38,7 @@ export class HuiViewBackgroundEditor extends LitElement {
               {
                 name: "opacity",
                 selector: {
-                  number: { min: 1, max: 100, mode: "slider" },
+                  number: { min: 0, max: 100, mode: "slider" },
                 },
               },
               {
@@ -144,6 +144,7 @@ export class HuiViewBackgroundEditor extends LitElement {
         .computeLabel=${this._computeLabelCallback}
         @value-changed=${this._valueChanged}
         .localizeValue=${this._localizeValueCallback}
+        style=${`--picture-opacity: ${(background.opacity ?? 100) / 100};`}
       ></ha-form>
     `;
   }
@@ -194,6 +195,7 @@ export class HuiViewBackgroundEditor extends LitElement {
   static styles = css`
     :host {
       display: block;
+      --file-upload-image-border-radius: 4px;
     }
   `;
 }
