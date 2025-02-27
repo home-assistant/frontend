4e9022ac533dc0f8a29ec7e62a3dec1d7a94a283
Wendelin <w@pe8.at>
2025-02-27 08:53:43 +0100

    Add loading spinner component

---
+++ src/components/ha-loading-spinner.ts
@@ -0,0 +1,50 @@
+import Spinner from "@shoelace-style/shoelace/dist/components/spinner/spinner.component";
+import spinnerStyles from "@shoelace-style/shoelace/dist/components/spinner/spinner.styles";
+import type { PropertyValues } from "lit";
+import { css } from "lit";
+import { customElement, property } from "lit/decorators";
+
+@customElement("ha-loading-spinner")
+export class HaLoadingSpinner extends Spinner {
+  @property() public size?: "tiny" | "small" | "medium" | "large";
+
+  protected updated(changedProps: PropertyValues) {
+    super.updated(changedProps);
+
+    if (changedProps.has("size")) {
+      switch (this.size) {
+        case "tiny":
+          this.style.setProperty("font-size", "16px");
+          break;
+        case "small":
+          this.style.setProperty("font-size", "28px");
+          break;
+        case "medium":
+          this.style.setProperty("font-size", "48px");
+          break;
+        case "large":
+          this.style.setProperty("font-size", "68px");
+          break;
+      }
+    }
+  }
+
+  static override styles = [
+    spinnerStyles,
+    css`
+      :host {
+        --indicator-color: var(--primary-color);
+        --track-color: var(--divider-color);
+        --track-width: 4px;
+        --speed: 3.5s;
+        font-size: 48px;
+      }
+    `,
+  ];
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "ha-loading-spinner": HaLoadingSpinner;
+  }
+}
