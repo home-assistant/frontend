cde3896470a4f7aa64a43ed1b0787c366236377d
Bram Kragten <mail@bramkragten.nl>
2025-02-28 15:51:06 +0100

    Align common dialog translations (#24450)

--- src/panels/config/backup/dialogs/dialog-restore-backup.ts
+++ src/panels/config/backup/dialogs/dialog-restore-backup.ts
@@ -139,7 +139,7 @@ class DialogRestoreBackup extends LitElement implements HassDialog {
         <ha-dialog-header slot="headline">
           <ha-icon-button
             slot="navigationIcon"
-            .label=${this.hass.localize("ui.dialogs.generic.close")}
+            .label=${this.hass.localize("ui.common.close")}
             .path=${mdiClose}
             @click=${this.closeDialog}
           ></ha-icon-button>
