1f36688782081a73a801b50d64c97311756bdbd5
ludeeus <joasoe@gmail.com>
2025-03-04 08:24:57 +0000

    Include name and version in already connected dialog if present

--- src/panels/config/cloud/dialog-cloud-already-connected/dialog-cloud-already-connected.ts
+++ src/panels/config/cloud/dialog-cloud-already-connected/dialog-cloud-already-connected.ts
@@ -61,7 +61,28 @@ class DialogCloudAlreadyConnected extends LitElement {
             )}
           </b>
         </div>
-        <div class="instance-details">
+
+        ${details.name
+          ? html` <div class="instance-details">
+              <span>
+                ${this.hass.localize(
+                  "ui.panel.config.cloud.dialog_already_connected.instance_name"
+                )}:
+              </span>
+              <span>${details.name})}</span>
+            </div>`
+          : nothing}
+        ${details.version
+          ? html` <div class="instance-details">
+              <span>
+                ${this.hass.localize(
+                  "ui.panel.config.cloud.dialog_already_connected.instance_version"
+                )}:
+              </span>
+              <span>${details.version})}</span>
+            </div>`
+          : nothing}
+        <div class="instance-detail">
           <div class="instance-detail">
             <span>
               ${this.hass.localize(
--- src/panels/config/cloud/dialog-cloud-already-connected/show-dialog-cloud-already-connected.ts
+++ src/panels/config/cloud/dialog-cloud-already-connected/show-dialog-cloud-already-connected.ts
@@ -4,6 +4,8 @@ export interface CloudAlreadyConnectedParams {
   details: {
     remote_ip_address: string;
     connected_at: string;
+    name?: string;
+    version?: string;
   };
   logInHereAction: () => void;
   closeDialog: () => void;
