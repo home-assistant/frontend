efb28dde1cf7f498f60b1266522981e48323d20f
Josef Zweck <josef@zweck.dev>
2025-03-07 12:54:33 +0100

    Update config_flow.ts

--- src/data/config_flow.ts
+++ src/data/config_flow.ts
@@ -34,7 +34,8 @@ const HEADERS = {
 export const createConfigFlow = (
   hass: HomeAssistant,
   handler: string,
-  entry_id?: string
+  entry_id?: string,
+  virtual_domain?: string, 
 ) =>
   hass.callApi<DataEntryFlowStep>(
     "POST",
@@ -43,6 +44,7 @@ export const createConfigFlow = (
       handler,
       show_advanced_options: Boolean(hass.userData?.showAdvanced),
       entry_id,
+      virtual_domain,
     },
     HEADERS
   );
