f2716b27c73dc71c66be4a11c1cf4a0a51118950
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2025-03-22 04:49:29 +0300

    prettier

--- src/panels/developer-tools/yaml_configuration/developer-yaml-config.ts
+++ src/panels/developer-tools/yaml_configuration/developer-yaml-config.ts
@@ -120,7 +120,9 @@ export class DeveloperYamlConfig extends LitElement {
                               "ui.panel.developer-tools.tabs.yaml.section.validation.errors"
                             )}
                           >
-                            <pre class="validate-log">${this._validateResult.errors}</pre>
+                            <pre class="validate-log">
+                              ${this._validateResult.errors}</pre
+                            >
                           </ha-alert>`
                         : ""
                     }
@@ -132,7 +134,9 @@ export class DeveloperYamlConfig extends LitElement {
                               "ui.panel.developer-tools.tabs.yaml.section.validation.warnings"
                             )}
                           >
-                            <pre class="validate-log">${this._validateResult.warnings}</pre>
+                            <pre class="validate-log">
+                              ${this._validateResult.warnings}</pre
+                            >
                           </ha-alert>`
                         : ""
                     }
