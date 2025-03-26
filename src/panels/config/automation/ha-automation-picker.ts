08edbed9d05771010b686e01d8bb6ac28507e3bc
Bram Kragten <mail@bramkragten.nl>
2025-03-26 11:20:39 +0100

    Apply suggestions from code review

--- src/panels/config/automation/ha-automation-picker.ts
+++ src/panels/config/automation/ha-automation-picker.ts
@@ -114,7 +114,6 @@ import { showLabelDetailDialog } from "../labels/show-dialog-label-detail";
 import { showNewAutomationDialog } from "./show-dialog-new-automation";
 
 type AutomationItem = AutomationEntity & {
-  entity_id: string;
   name: string;
   area: string | undefined;
   last_triggered?: string | undefined;
@@ -232,7 +231,6 @@ class HaAutomationPicker extends SubscribeMixin(LitElement) {
         const labels = labelReg && entityRegEntry?.labels;
         return {
           ...automation,
-          entity_id: automation.entity_id,
           name: computeStateName(automation),
           area: entityRegEntry?.area_id
             ? areas[entityRegEntry?.area_id]?.name
