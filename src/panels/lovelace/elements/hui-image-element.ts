c90a5d73483cba60f854865c124f81d5639f0c7d
Paul Bottein <paul.bottein@gmail.com>
2025-03-28 13:12:07 +0100

    Add hold and double tap action in the UI for every card that supports it. (#24824)
    * Add double tap action to button card UI editor
    
    * Add double tap action to light card UI editor
    
    * Add hold action and double tap action to gauge card UI editor
    
    * Add hold action and double tap action to picture glance card UI editor
    
    * Add hold action and double tap action to picture card UI editor
    
    * Add hold action and double tap action to entity card UI editor
    
    * Add hold action and double tap action to elements

--- src/panels/lovelace/elements/hui-image-element.ts
+++ src/panels/lovelace/elements/hui-image-element.ts
@@ -29,7 +29,7 @@ export class HuiImageElement extends LitElement implements LovelaceElement {
       throw Error("Invalid configuration");
     }
 
-    this._config = { hold_action: { action: "more-info" }, ...config };
+    this._config = { tap_action: { action: "more-info" }, ...config };
 
     this.classList.toggle(
       "clickable",
