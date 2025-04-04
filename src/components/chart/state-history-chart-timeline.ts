498ed81868fe6557e9e528e7c0db7d6540ee8594
Yosi Levy <yosilevy@gmail.com>
2025-04-04 05:50:16 +0000

    History tooltip RTL fix

--- src/components/chart/state-history-chart-timeline.ts
+++ src/components/chart/state-history-chart-timeline.ts
@@ -138,8 +138,12 @@ export class StateHistoryChartTimeline extends LitElement {
         "ui.components.history_charts.duration"
       )}: ${millisecondsToDuration(durationInMs)}`;
 
+      const markerLocalized = !computeRTL(this.hass)
+        ? marker
+        : '<span style="direction: rtl;display:inline-block;margin-right:4px;margin-inline-end:4px;border-radius:10px;width:10px;height:10px;background-color:#9e9e9e;"></span>';
+
       const lines = [
-        marker + name,
+        markerLocalized + name,
         formatDateTimeWithSeconds(
           new Date(value![1]),
           this.hass.locale,
