939e6bd7037c835008aff8d238c44f4eb6e8b5ea
Wendelin <w@pe8.at>
2025-04-08 10:50:20 +0200

    Add controller LR check for add node with my button

--- src/panels/config/integrations/integration-panels/zwave_js/zwave_js-add-node.ts
+++ src/panels/config/integrations/integration-panels/zwave_js/zwave_js-add-node.ts
@@ -3,6 +3,7 @@ import { customElement } from "lit/decorators";
 import { navigate } from "../../../../../common/navigate";
 import type { HomeAssistant } from "../../../../../types";
 import { showZWaveJSAddNodeDialog } from "./show-dialog-zwave_js-add-node";
+import { fetchZwaveNetworkStatus } from "../../../../../data/zwave_js";
 
 @customElement("zwave_js-add-node")
 export class DialogZWaveJSAddNode extends HTMLElement {
@@ -21,9 +22,22 @@ export class DialogZWaveJSAddNode extends HTMLElement {
         replace: true,
       }
     );
+
+    let longRangeSupported = false;
+
+    try {
+      const zwaveNetwork = await fetchZwaveNetworkStatus(this.hass, {
+        entry_id: this.configEntryId,
+      });
+      longRangeSupported = zwaveNetwork?.controller?.supports_long_range;
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.error(err);
+    }
+
     showZWaveJSAddNodeDialog(this, {
       entry_id: this.configEntryId,
-      longRangeSupported: false,
+      longRangeSupported,
     });
   }
 }
