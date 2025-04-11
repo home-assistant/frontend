31785b5c47b458bbf91e9c30371665efe7d051dc
Paul Bottein <paul.bottein@gmail.com>
2025-04-11 14:41:12 +0200

    Only ask to refresh dashboard if necessary (#24993)

--- src/panels/lovelace/ha-panel-lovelace.ts
+++ src/panels/lovelace/ha-panel-lovelace.ts
@@ -9,6 +9,8 @@ import {
   addSearchParam,
   removeSearchParam,
 } from "../../common/url/search-params";
+import { debounce } from "../../common/util/debounce";
+import { deepEqual } from "../../common/util/deep-equal";
 import { domainToName } from "../../data/integration";
 import { subscribeLovelaceUpdates } from "../../data/lovelace";
 import type {
@@ -50,6 +52,12 @@ interface LovelacePanelConfig {
 
 let editorLoaded = false;
 let resourcesLoaded = false;
+
+declare global {
+  interface HASSDomEvents {
+    "strategy-config-changed": undefined;
+  }
+}
 
 @customElement("ha-panel-lovelace")
 export class LovelacePanel extends LitElement {
@@ -127,6 +135,7 @@ export class LovelacePanel extends LitElement {
           .route=${this.route}
           .narrow=${this.narrow}
           @config-refresh=${this._forceFetchConfig}
+          @strategy-config-changed=${this._strategyConfigChanged}
         ></hui-root>
       `;
     }
@@ -199,7 +208,7 @@ export class LovelacePanel extends LitElement {
         oldHass.floors !== this.hass.floors
       ) {
         if (this.hass.config.state === "RUNNING") {
-          this._askRefreshConfig();
+          this._debounceRegistriesChanged();
         }
       }
       // If ha started, refresh the config
@@ -207,25 +216,59 @@ export class LovelacePanel extends LitElement {
         this.hass.config.state === "RUNNING" &&
         oldHass.config.state !== "RUNNING"
       ) {
-        this._refreshConfig();
+        this._regenerateStrategyConfig();
       }
     }
   }
 
-  private _askRefreshConfig = () => {
+  private _debounceRegistriesChanged = debounce(
+    () => this._registriesChanged(),
+    200
+  );
+
+  private _registriesChanged = async () => {
+    if (!this.hass || !this.lovelace) {
+      return;
+    }
+    const rawConfig = this.lovelace.rawConfig;
+
+    if (!isStrategyDashboard(rawConfig)) {
+      return;
+    }
+
+    const oldConfig = this.lovelace.config;
+    const generatedConfig = await generateLovelaceDashboardStrategy(
+      rawConfig,
+      this.hass!
+    );
+
+    const newConfig = checkLovelaceConfig(generatedConfig) as LovelaceConfig;
+
+    // Ask to regenerate if the config changed
+    if (!deepEqual(newConfig, oldConfig)) {
+      this._askRegenerateStrategyConfig();
+    }
+  };
+
+  private _strategyConfigChanged = (ev: CustomEvent) => {
+    ev.stopPropagation();
+    this._askRegenerateStrategyConfig();
+  };
+
+  private _askRegenerateStrategyConfig = () => {
     showToast(this, {
       message: this.hass!.localize("ui.panel.lovelace.changed_toast.message"),
       action: {
-        action: () => this._refreshConfig(),
+        action: () => this._regenerateStrategyConfig(),
         text: this.hass!.localize("ui.common.refresh"),
       },
       duration: -1,
-      id: "entity-registry-changed",
+      id: "regenerate-strategy-config",
       dismissable: false,
     });
   };
 
-  private async _refreshConfig() {
+  private async _regenerateStrategyConfig() {
     if (!this.hass || !this.lovelace) {
       return;
     }
