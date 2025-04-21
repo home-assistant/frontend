e764a15a631683a337bb93a79a3b24f89d1b577c
Simon Lamon <32477463+silamon@users.noreply.github.com>
2025-04-21 07:31:31 +0000

    Clean up

--- src/panels/lovelace/editor/config-elements/elements/hui-image-element-editor.ts
+++ src/panels/lovelace/editor/config-elements/elements/hui-image-element-editor.ts
@@ -93,20 +93,13 @@ export class HuiImageElementEditor
           name: "camera_view",
           selector: {
             select: {
-              options: [
-                {
-                  value: "auto",
-                  label: localize(
-                    `ui.panel.lovelace.editor.card.generic.camera_view_options.auto`
-                  ),
-                },
-                {
-                  value: "live",
-                  label: localize(
-                    `ui.panel.lovelace.editor.card.generic.camera_view_options.live`
-                  ),
-                },
-              ],
+              options: ["auto", "live"].map((value) => ({
+                value,
+                label: localize(
+                  `ui.panel.lovelace.editor.card.generic.camera_view_options.${value}`
+                ),
+              })),
+              mode: "dropdown",
             },
           },
         },
