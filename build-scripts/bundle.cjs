f83693cc403920b612d0309071bc029b04ba1744
Bram Kragten <mail@bramkragten.nl>
2025-04-23 22:32:08 +0200

    Update bundle.cjs

--- build-scripts/bundle.cjs
+++ build-scripts/bundle.cjs
@@ -24,13 +24,13 @@ module.exports.emptyPackages = ({ isHassioBuild }) =>
     require.resolve("@vaadin/vaadin-material-styles/font-icons.js"),
     // Icons in supervisor conflict with icons in HA so we don't load.
     isHassioBuild &&
-    require.resolve(
-      path.resolve(paths.polymer_dir, "src/components/ha-icon.ts")
-    ),
+      require.resolve(
+        path.resolve(paths.root_dir, "src/components/ha-icon.ts")
+      ),
     isHassioBuild &&
-    require.resolve(
-      path.resolve(paths.polymer_dir, "src/components/ha-icon-picker.ts")
-    ),
+      require.resolve(
+        path.resolve(paths.root_dir, "src/components/ha-icon-picker.ts")
+      ),
   ].filter(Boolean);
 
 module.exports.definedVars = ({ isProdBuild, latestBuild, defineOverlay }) => ({
@@ -41,11 +41,12 @@ module.exports.definedVars = ({ isProdBuild, latestBuild, defineOverlay }) => ({
   __SUPERVISOR__: false,
   __BACKWARDS_COMPAT__: false,
   __STATIC_PATH__: "/static/",
-  __HASS_URL__: `\`${"HASS_URL" in process.env
-    ? process.env.HASS_URL
-    : // eslint-disable-next-line no-template-curly-in-string
-    "${location.protocol}//${location.host}"
-    }\``,
+  __HASS_URL__: `\`${
+    "HASS_URL" in process.env
+      ? process.env.HASS_URL
+      : // eslint-disable-next-line no-template-curly-in-string
+        "${location.protocol}//${location.host}"
+  }\``,
   "process.env.NODE_ENV": JSON.stringify(
     isProdBuild ? "production" : "development"
   ),
@@ -73,9 +74,7 @@ module.exports.terserOptions = ({ latestBuild, isTestBuild }) => ({
 });
 
 /** @type {import('@rspack/core').SwcLoaderOptions} */
-module.exports.swcOptions = ({
-  latestBuild,
-}) => ({
+module.exports.swcOptions = ({ latestBuild }) => ({
   jsc: {
     loose: true,
     externalHelpers: true,
@@ -229,9 +228,9 @@ module.exports.config = {
       entry: {
         "service-worker": !latestBuild
           ? {
-            import: "./src/entrypoints/service-worker.ts",
-            layer: "sw",
-          }
+              import: "./src/entrypoints/service-worker.ts",
+              layer: "sw",
+            }
           : "./src/entrypoints/service-worker.ts",
         app: "./src/entrypoints/app.ts",
         authorize: "./src/entrypoints/authorize.ts",
