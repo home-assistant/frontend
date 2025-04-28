456e226422e19a741d307175ae5384daaa1d807c
Wendelin <12148533+wendevlin@users.noreply.github.com>
2025-04-28 16:10:32 +0200

    Update src/components/ha-badge.ts

--- src/components/ha-badge.ts
+++ src/components/ha-badge.ts
@@ -103,7 +103,7 @@ export class HaBadge extends LitElement {
       color: var(--secondary-text-color);
     }
     .content {
-      font-size: var(--badge-font-size, 12px);
+      font-size: var(--ha-font-size-badge, var(--ha-font-size-s));
       font-style: normal;
       font-weight: 500;
       line-height: 16px;
