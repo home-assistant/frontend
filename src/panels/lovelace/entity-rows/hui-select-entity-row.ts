8f422357f108d5821c460f2268c3f042a20ba4f7
Wendelin <12148533+wendevlin@users.noreply.github.com>
2025-05-05 11:13:50 +0200

    Fix select entity change (#25310)

--- src/panels/lovelace/entity-rows/hui-select-entity-row.ts
+++ src/panels/lovelace/entity-rows/hui-select-entity-row.ts
@@ -93,7 +93,6 @@ class HuiSelectEntityRow extends LitElement implements LovelaceRow {
   `;
 
   private _handleAction(ev): void {
-    ev.stopPropagation();
     const stateObj = this.hass!.states[this._config!.entity] as SelectEntity;
 
     const option = ev.target.value;
