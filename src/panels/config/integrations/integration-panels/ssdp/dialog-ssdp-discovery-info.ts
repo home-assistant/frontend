fc90f838a25d6d8c4e27a454d84973e857650eaf
J. Nick Koston <nick@koston.org>
2025-05-07 12:50:50 -0500

    Update src/panels/config/integrations/integration-panels/ssdp/dialog-ssdp-discovery-info.ts
    Co-authored-by: Petar Petrov <MindFreeze@users.noreply.github.com>

--- src/panels/config/integrations/integration-panels/ssdp/dialog-ssdp-discovery-info.ts
+++ src/panels/config/integrations/integration-panels/ssdp/dialog-ssdp-discovery-info.ts
@@ -96,7 +96,7 @@ class DialogSSDPDiscoveryInfo extends LitElement implements HassDialog {
                   <td><b>${key}</b></td>
                   <td>
                     ${typeof value === "object" && value !== null
-                      ? html`<a href="#" @click=${this._showRawData(key, value)}
+                      ? html`<a href="#" @click=${this._showRawData(key, value as Record<string, unknown>)}
                           >${this.hass.localize(
                             "ui.panel.config.ssdp.show_raw_data"
                           )}</a
