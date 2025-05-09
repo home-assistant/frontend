8fc55cb6e290f420bc27318d129c9c426a4f202a
Paul Bottein <paul.bottein@gmail.com>
2025-05-09 18:35:38 +0200

    Fix selected entity in the entity picker dropdown (#25405)

--- src/components/entity/ha-entity-combo-box.ts
+++ src/components/entity/ha-entity-combo-box.ts
@@ -408,7 +408,9 @@ export class HaEntityComboBox extends LitElement {
   protected render(): TemplateResult {
     return html`
       <ha-combo-box
+        item-id-path="id"
         item-value-path="id"
+        item-label-path="label"
         .hass=${this.hass}
         .value=${this._value}
         .label=${this.label === undefined
