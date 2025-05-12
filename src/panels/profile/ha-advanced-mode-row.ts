3e598ddda78c2891987ec49e888bf689db57e5ea
Wendelin <w@pe8.at>
2025-05-12 15:46:05 +0200

    Subscribe to frontend user data

--- src/panels/profile/ha-advanced-mode-row.ts
+++ src/panels/profile/ha-advanced-mode-row.ts
@@ -1,11 +1,12 @@
 import type { TemplateResult } from "lit";
-import { css, html, LitElement } from "lit";
-import { customElement, property } from "lit/decorators";
+import { css, html, LitElement, nothing } from "lit";
+import { customElement, property, state } from "lit/decorators";
+import "../../components/ha-alert";
 import "../../components/ha-card";
 import "../../components/ha-settings-row";
 import "../../components/ha-switch";
 import type { CoreFrontendUserData } from "../../data/frontend";
-import { getOptimisticFrontendUserDataCollection } from "../../data/frontend";
+import { saveFrontendUserData } from "../../data/frontend";
 import type { HomeAssistant } from "../../types";
 
 @customElement("ha-advanced-mode-row")
@@ -16,8 +17,13 @@ class AdvancedModeRow extends LitElement {
 
   @property({ attribute: false }) public coreUserData?: CoreFrontendUserData;
 
+  @state() private _error?: string;
+
   protected render(): TemplateResult {
     return html`
+      ${this._error
+        ? html`<ha-alert alert-type="error">${this._error}</ha-alert>`
+        : nothing}
       <ha-settings-row .narrow=${this.narrow}>
         <span slot="heading">
           ${this.hass.localize("ui.panel.profile.advanced_mode.title")}
@@ -41,15 +47,23 @@ class AdvancedModeRow extends LitElement {
   }
 
   private async _advancedToggled(ev) {
-    getOptimisticFrontendUserDataCollection(this.hass.connection, "core").save({
-      ...this.coreUserData,
-      showAdvanced: ev.currentTarget.checked,
-    });
+    try {
+      saveFrontendUserData(this.hass.connection, "core", {
+        ...this.coreUserData,
+        showAdvanced: ev.currentTarget.checked,
+      });
+    } catch (err: any) {
+      this._error = err.message || err;
+    }
   }
 
   static styles = css`
     a {
       color: var(--primary-color);
+    }
+    ha-alert {
+      margin: 0 16px;
+      display: block;
     }
   `;
 }
--- src/panels/profile/ha-entity-id-picker-row.ts
+++ src/panels/profile/ha-entity-id-picker-row.ts
@@ -1,11 +1,12 @@
 import type { TemplateResult } from "lit";
-import { css, html, LitElement } from "lit";
-import { customElement, property } from "lit/decorators";
+import { css, html, LitElement, nothing } from "lit";
+import { customElement, property, state } from "lit/decorators";
+import "../../components/ha-alert";
 import "../../components/ha-card";
 import "../../components/ha-settings-row";
 import "../../components/ha-switch";
 import type { CoreFrontendUserData } from "../../data/frontend";
-import { getOptimisticFrontendUserDataCollection } from "../../data/frontend";
+import { saveFrontendUserData } from "../../data/frontend";
 import type { HomeAssistant } from "../../types";
 
 @customElement("ha-entity-id-picker-row")
@@ -16,8 +17,13 @@ class EntityIdPickerRow extends LitElement {
 
   @property({ attribute: false }) public coreUserData?: CoreFrontendUserData;
 
+  @state() private _error?: string;
+
   protected render(): TemplateResult {
     return html`
+      ${this._error
+        ? html`<ha-alert alert-type="error">${this._error}</ha-alert>`
+        : nothing}
       <ha-settings-row .narrow=${this.narrow}>
         <span slot="heading">
           ${this.hass.localize("ui.panel.profile.entity_id_picker.title")}</span
@@ -35,15 +41,23 @@ class EntityIdPickerRow extends LitElement {
   }
 
   private async _toggled(ev) {
-    getOptimisticFrontendUserDataCollection(this.hass.connection, "core").save({
-      ...this.coreUserData,
-      showEntityIdPicker: ev.currentTarget.checked,
-    });
+    try {
+      saveFrontendUserData(this.hass.connection, "core", {
+        ...this.coreUserData,
+        showEntityIdPicker: ev.currentTarget.checked,
+      });
+    } catch (err: any) {
+      this._error = err.message || err;
+    }
   }
 
   static styles = css`
     a {
       color: var(--primary-color);
+    }
+    ha-alert {
+      margin: 0 16px;
+      display: block;
     }
   `;
 }
