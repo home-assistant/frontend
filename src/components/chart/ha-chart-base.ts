6889d13b85c5b072b6747a5e582916599618ff66
Petar Petrov <MindFreeze@users.noreply.github.com>
2025-05-15 17:25:25 +0300

    make extraComponents non reactive

--- src/components/chart/ha-chart-base.ts
+++ src/components/chart/ha-chart-base.ts
@@ -48,7 +48,9 @@ export class HaChartBase extends LitElement {
   @property({ attribute: "expand-legend", type: Boolean })
   public expandLegend?: boolean;
 
-  @property({ attribute: false }) public extraComponents?: any[];
+  // extraComponents is not reactive and should not trigger updates
+  @property({ attribute: false, hasChanged: () => false })
+  public extraComponents?: any[];
 
   @state()
   @consume({ context: themesContext, subscribe: true })
