28fe60f02b6f1bb0192df2b33d0a9cea8afad02d
Jan-Philipp Benecke <jan-philipp@bnck.me>
2025-05-26 10:45:22 +0200

    Improve broken cards on dashboards (#25557)

--- src/panels/lovelace/entity-rows/hui-lock-entity-row.ts
+++ src/panels/lovelace/entity-rows/hui-lock-entity-row.ts
@@ -37,7 +37,7 @@ class HuiLockEntityRow extends LitElement implements LovelaceRow {
 
     if (!stateObj) {
       return html`
-        <hui-warning>
+        <hui-warning .hass=${this.hass}>
           ${createEntityNotFoundWarning(this.hass, this._config.entity)}
         </hui-warning>
       `;
