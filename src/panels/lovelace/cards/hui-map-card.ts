e87cc88e3a3fc54cde23bc576e6209e2129c16af
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2025-05-29 18:03:27 +0300

    remove prefix, rename suffix -> unit

--- src/panels/lovelace/cards/hui-map-card.ts
+++ src/panels/lovelace/cards/hui-map-card.ts
@@ -45,8 +45,7 @@ export const DEFAULT_ZOOM = 14;
 interface MapEntityConfig extends EntityConfig {
   label_mode?: "state" | "attribute" | "name";
   attribute?: string;
-  prefix?: string;
-  suffix?: string;
+  unit?: string;
   focus?: boolean;
 }
 
@@ -54,8 +53,7 @@ interface GeoEntity {
   entity_id: string;
   label_mode?: "state" | "attribute" | "name" | "icon";
   attribute?: string;
-  prefix?: string;
-  suffix?: string;
+  unit?: string;
   focus: boolean;
 }
 
@@ -429,8 +427,7 @@ class HuiMapCard extends LitElement implements LovelaceCard {
           entity_id: stateObj.entity_id,
           label_mode: sourceObj?.label_mode ?? allSource?.label_mode,
           attribute: sourceObj?.attribute ?? allSource?.attribute,
-          prefix: sourceObj?.prefix ?? allSource?.prefix,
-          suffix: sourceObj?.suffix ?? allSource?.suffix,
+          unit: sourceObj?.unit ?? allSource?.unit,
           focus: sourceObj
             ? (sourceObj.focus ?? true)
             : (allSource?.focus ?? true),
@@ -447,8 +444,7 @@ class HuiMapCard extends LitElement implements LovelaceCard {
         color: this._getColor(entityConf.entity),
         label_mode: entityConf.label_mode,
         attribute: entityConf.attribute,
-        prefix: entityConf.prefix,
-        suffix: entityConf.suffix,
+        unit: entityConf.unit,
         focus: entityConf.focus,
         name: entityConf.name,
       })),
