76146172a71278804615befd6c1e6ce2a68aea2c
Paul Bottein <paul.bottein@gmail.com>
2025-06-11 12:04:54 +0200

    Display full error for card preview mode (#25747)

--- src/panels/lovelace/types.ts
+++ src/panels/lovelace/types.ts
@@ -62,6 +62,8 @@ export interface LovelaceGridOptions {
   min_columns?: number;
   min_rows?: number;
   max_rows?: number;
+  fixed_rows?: boolean;
+  fixed_columns?: boolean;
 }
 
 export interface LovelaceCard extends HTMLElement {
