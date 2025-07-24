b8f95ee6af72838b6b83a2d615bfd8c7585cd25f
Paulus Schoutsen <balloob@gmail.com>
2025-07-24 13:55:45 +0000

    Add download device info button

--- src/panels/config/core/ha-config-section-analytics.ts
+++ src/panels/config/core/ha-config-section-analytics.ts
@@ -1,6 +1,12 @@
+import { mdiDotsVertical, mdiDownload } from "@mdi/js";
 import type { TemplateResult } from "lit";
 import { css, html, LitElement } from "lit";
-import { customElement, property } from "lit/decorators";
+import { customElement, property, state } from "lit/decorators";
+import "../../../components/ha-button-menu";
+import "../../../components/ha-icon-button";
+import "../../../components/ha-list-item";
+import "../../../components/ha-svg-icon";
+import { getSignedPath } from "../../../data/auth";
 import "../../../layouts/hass-subpage";
 import type { HomeAssistant, Route } from "../../../types";
 import "./ha-config-analytics";
@@ -13,6 +19,8 @@ class HaConfigSectionAnalytics extends LitElement {
 
   @property({ type: Boolean }) public narrow = false;
 
+  @state() private _error?: string;
+
   protected render(): TemplateResult {
     return html`
       <hass-subpage
@@ -21,11 +29,36 @@ class HaConfigSectionAnalytics extends LitElement {
         .narrow=${this.narrow}
         .header=${this.hass.localize("ui.panel.config.analytics.caption")}
       >
+        <ha-button-menu
+          @action=${this._handleOverflowAction}
+          slot="toolbar-icon"
+        >
+          <ha-icon-button slot="trigger" .path=${mdiDotsVertical}>
+          </ha-icon-button>
+          <ha-list-item graphic="icon">
+            <ha-svg-icon slot="graphic" .path=${mdiDownload}></ha-svg-icon>
+            ${this.hass.localize(
+              "ui.panel.config.analytics.download_device_info"
+            )}
+          </ha-list-item>
+        </ha-button-menu>
         <div class="content">
           <ha-config-analytics .hass=${this.hass}></ha-config-analytics>
         </div>
       </hass-subpage>
     `;
+  }
+
+  private async _handleOverflowAction(): Promise<void> {
+    try {
+      const signedPath = await getSignedPath(
+        this.hass,
+        "/api/analytics/devices"
+      );
+      window.open(signedPath.path, "_blank");
+    } catch (err: any) {
+      this._error = err.message || err;
+    }
   }
 
   static styles = css`
