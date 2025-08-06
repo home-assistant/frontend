88c4f37f43f1f734be39ab2841375eddda5161f9
Wendelin <w@pe8.at>
2025-08-06 14:50:26 +0200

    Add border radius css var ha prefix

--- src/resources/theme/core.globals.ts
+++ src/resources/theme/core.globals.ts
@@ -3,21 +3,21 @@ import { extractDerivedVars } from "../../common/style/derived-css-vars";
 
 export const coreStyles = css`
   html {
-    --border-width-sm: 1px;
-    --border-width-md: 2px;
-    --border-width-lg: 3px;
+    --ha-border-width-sm: 1px;
+    --ha-border-width-md: 2px;
+    --ha-border-width-lg: 3px;
 
-    --border-radius-sm: 4px;
-    --border-radius-md: 8px;
-    --border-radius-lg: 12px;
-    --border-radius-xl: 16px;
-    --border-radius-2xl: 24px;
-    --border-radius-3xl: 28px;
-    --border-radius-4xl: 32px;
-    --border-radius-5xl: 36px;
-    --border-radius-pill: 9999px;
-    --border-radius-circle: 50%;
-    --border-radius-square: 0;
+    --ha-border-radius-sm: 4px;
+    --ha-border-radius-md: 8px;
+    --ha-border-radius-lg: 12px;
+    --ha-border-radius-xl: 16px;
+    --ha-border-radius-2xl: 24px;
+    --ha-border-radius-3xl: 28px;
+    --ha-border-radius-4xl: 32px;
+    --ha-border-radius-5xl: 36px;
+    --ha-border-radius-pill: 9999px;
+    --ha-border-radius-circle: 50%;
+    --ha-border-radius-square: 0;
   }
 `;
 
