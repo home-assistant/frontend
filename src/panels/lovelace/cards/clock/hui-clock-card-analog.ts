c7babe884cd8d9be414f24b6a910321604d9b7a2
Aidan Timson <aidan@timmo.dev>
2025-08-27 13:36:33 +0100

    Add variables for analog clock background (#26726)

--- src/panels/lovelace/cards/clock/hui-clock-card-analog.ts
+++ src/panels/lovelace/cards/clock/hui-clock-card-analog.ts
@@ -191,6 +191,9 @@ export class HuiClockCardAnalog extends LitElement {
       justify-content: center;
       width: var(--clock-size);
       height: var(--clock-size);
+      background: var(--ha-clock-card-analog-face-background, none);
+      border-radius: var(--ha-clock-card-analog-face-border-radius, none);
+      padding: var(--ha-clock-card-analog-face-padding, none);
     }
 
     .analog-clock.size-medium {
