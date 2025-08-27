a76539c73251457a42fa72450c0d193c005496ec
Paul Bottein <paul.bottein@gmail.com>
2025-08-27 14:55:15 +0200

    Revert background section (#26730)

--- src/data/lovelace/config/section.ts
+++ src/data/lovelace/config/section.ts
@@ -2,16 +2,10 @@ import type { Condition } from "../../../panels/lovelace/common/validate-conditi
 import type { LovelaceCardConfig } from "./card";
 import type { LovelaceStrategyConfig } from "./strategy";
 
-export interface LovelaceSectionStyleConfig {
-  background_color?: string;
-  background_opacity?: number;
-}
-
 export interface LovelaceBaseSectionConfig {
   visibility?: Condition[];
   column_span?: number;
   row_span?: number;
-  style?: LovelaceSectionStyleConfig;
   /**
    * @deprecated Use heading card instead.
    */
