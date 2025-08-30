b58ad1e8fce66322eaea7cf7f6a244c06b57392d
Douwe <61123717+dhoeben@users.noreply.github.com>
2025-08-30 12:25:34 +0200

    Update hui-button-card-feature.ts

--- src/panels/lovelace/card-features/hui-button-card-feature.ts
+++ src/panels/lovelace/card-features/hui-button-card-feature.ts
@@ -7,7 +7,10 @@ import "../../../components/ha-control-button-group";
 import type { HomeAssistant } from "../../../types";
 import type { LovelaceCardFeature, LovelaceCardFeatureEditor } from "../types";
 import { cardFeatureStyles } from "./common/card-feature-styles";
-import type { ActionHandlerEvent } from "../../../data/lovelace/action_handler";
+import type { HomeAssistant } from "../../../types";
+import { actionHandler } from "../common/directives/action-handler-directive";
+import { handleAction } from "../common/handle-action";
+import { hasAction } from "../common/has-action";
 import type {
   ButtonCardFeatureConfig,
   LovelaceCardFeatureContext,
