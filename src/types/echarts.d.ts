209679f3ed1b2a5574005835398f49caa4637fa2
Petar Petrov <MindFreeze@users.noreply.github.com>
2025-09-01 08:51:54 +0300

    ts fix

--- src/types/echarts.d.ts
+++ src/types/echarts.d.ts
@@ -9,3 +9,8 @@ declare module "echarts/lib/util/graphic" {
 declare module "echarts/lib/util/states" {
   export * from "echarts/types/src/util/states";
 }
+
+declare module "echarts/lib/chart/sankey/sankeyView" {
+  // eslint-disable-next-line no-restricted-exports
+  export { default } from "echarts/types/src/chart/sankey/SankeyView";
+}
