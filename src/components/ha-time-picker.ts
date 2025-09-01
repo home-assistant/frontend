44cd425ce87a666293fa80c19b9362f864e8f0b9
Aidan Timson <aidan@timmo.dev>
2025-09-01 11:38:31 +0100

    Remove duplicate event

--- src/components/ha-time-picker.ts
+++ src/components/ha-time-picker.ts
@@ -134,9 +134,6 @@ export class HaTimePicker extends LitElement {
 
   protected updated(changedProperties: PropertyValues) {
     super.updated(changedProperties);
-    if (changedProperties.has("value")) {
-      this._timeUpdated();
-    }
 
     if (changedProperties.has("_hours")) {
       this._timeUpdated();
