9c49e38baffb045e239b799831528dbcf70f1edf
Simon Lamon <32477463+silamon@users.noreply.github.com>
2025-09-11 09:07:38 +0200

    Apply suggestions from code review

--- src/components/ha-tooltip.ts
+++ src/components/ha-tooltip.ts
@@ -3,17 +3,9 @@ import { css } from "lit";
 import type { CSSResultGroup } from "lit";
 import { customElement, property } from "lit/decorators";
 
-import { StateSet } from "../resources/polyfills/stateset";
 
 @customElement("ha-tooltip")
 export class HaTooltip extends Tooltip {
-  attachInternals() {
-    const internals = super.attachInternals();
-    Object.defineProperty(internals, "states", {
-      value: new StateSet(this, internals.states),
-    });
-    return internals;
-  }
 
   /** The amount of time to wait before showing the tooltip when the user mouses in. */
   @property({ attribute: "show-delay", type: Number }) showDelay = 150;
@@ -47,7 +39,7 @@ export class HaTooltip extends Tooltip {
           --wa-tooltip-padding: 8px;
           --wa-tooltip-border-radius: var(--ha-tooltip-border-radius, 4px);
           --wa-tooltip-arrow-size: var(--ha-tooltip-arrow-size, 8px);
-          --sl-z-index-tooltip: var(--ha-tooltip-z-index, 1000);
+          --wa-z-index-tooltip: var(--ha-tooltip-z-index, 1000);
         }
       `,
     ];
