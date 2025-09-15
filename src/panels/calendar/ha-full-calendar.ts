9d02a1d3910ebe84fcca2f78698ac4656024cb85
Aidan Timson <aidan@timmo.dev>
2025-09-15 15:12:37 +0100

    Fix calendar toggle group wrapping (#27049)

--- src/panels/calendar/ha-full-calendar.ts
+++ src/panels/calendar/ha-full-calendar.ts
@@ -162,6 +162,7 @@ export class HAFullCalendar extends LitElement {
                       .buttons=${viewToggleButtons}
                       .active=${this._activeView}
                       size="small"
+                      no-wrap
                       @value-changed=${this._handleView}
                     ></ha-button-toggle-group>
                   `
@@ -197,6 +198,7 @@ export class HAFullCalendar extends LitElement {
                         .buttons=${viewToggleButtons}
                         .active=${this._activeView}
                         size="small"
+                        no-wrap
                         @value-changed=${this._handleView}
                       ></ha-button-toggle-group>
                     </div>
