cef926b54f7c255fbe62228ad9ad4908e59532d7
Aidan Timson <aidan@timmo.dev>
2025-09-15 14:23:21 +0100

    Update src/panels/calendar/ha-full-calendar.ts
    Co-authored-by: Bram Kragten <mail@bramkragten.nl>

--- src/panels/calendar/ha-full-calendar.ts
+++ src/panels/calendar/ha-full-calendar.ts
@@ -196,7 +196,7 @@ export class HAFullCalendar extends LitElement {
                       <ha-button-toggle-group
                         .buttons=${viewToggleButtons}
                         .active=${this._activeView}
-                        .size=${"small"}
+                        size="small"
                         @value-changed=${this._handleView}
                       ></ha-button-toggle-group>
                     </div>
