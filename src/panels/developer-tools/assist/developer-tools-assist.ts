8cf0d8d2c36fd7de953c216c8d6319218080af11
Aidan Timson <aidan@timmo.dev>
2025-09-16 15:35:48 +0100

    Safe area: devtools (#26969)
    * Remove extra padding
    
    * Remove extra padding
    
    * Remove extra padding
    
    * Fixes
    
    * use change from #26971
    
    * Remove
    
    * Remove
    
    * Remove
    
    * Remove
    
    * Restore for future change
    
    * Fix

--- src/panels/developer-tools/assist/developer-tools-assist.ts
+++ src/panels/developer-tools/assist/developer-tools-assist.ts
@@ -241,10 +241,6 @@ class HaPanelDevAssist extends SubscribeMixin(LitElement) {
       css`
         .content {
           padding: 28px 20px 16px;
-          padding: max(28px, calc(12px + var(--safe-area-inset-top)))
-            max(20px, calc(4px + var(--safe-area-inset-right)))
-            max(16px, var(--safe-area-inset-bottom))
-            max(20px, calc(4px + var(--safe-area-inset-left)));
           max-width: 1040px;
           margin: 0 auto;
         }
