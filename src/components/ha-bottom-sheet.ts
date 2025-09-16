efca004717b65eb85c5b2c2ea464cba92d41831f
Aidan Timson <aidan@timmo.dev>
2025-09-16 09:52:34 +0100

    Add safe areas

--- src/components/ha-bottom-sheet.ts
+++ src/components/ha-bottom-sheet.ts
@@ -57,6 +57,8 @@ export class HaBottomSheet extends LitElement {
     }
     wa-drawer::part(body) {
       padding-bottom: var(--safe-area-inset-bottom);
+      padding-left: var(--safe-area-inset-left);
+      padding-right: var(--safe-area-inset-right);
     }
   `;
 }
