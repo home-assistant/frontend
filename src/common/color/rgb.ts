ebbe1a3c4b26eb8f15404b0809972eec747047e3
Aidan Timson <aidan@timmo.dev>
2025-09-25 15:00:18 +0100

    Migrate rgb color helper functions to culori

--- src/common/color/rgb.ts
+++ src/common/color/rgb.ts
@@ -1,27 +1,31 @@
-export const luminosity = (rgb: [number, number, number]): number => {
-  // http://www.w3.org/TR/WCAG20/#relativeluminancedef
-  const lum: [number, number, number] = [0, 0, 0];
-  for (let i = 0; i < rgb.length; i++) {
-    const chan = rgb[i] / 255;
-    lum[i] = chan <= 0.03928 ? chan / 12.92 : ((chan + 0.055) / 1.055) ** 2.4;
-  }
+import { wcagLuminance, wcagContrast } from "culori";
 
-  return 0.2126 * lum[0] + 0.7152 * lum[1] + 0.0722 * lum[2];
-};
+export const luminosity = (rgb: [number, number, number]): number =>
+  wcagLuminance({
+    mode: "rgb",
+    r: rgb[0] / 255,
+    g: rgb[1] / 255,
+    b: rgb[2] / 255,
+  });
 
 export const rgbContrast = (
   color1: [number, number, number],
   color2: [number, number, number]
-) => {
-  const lum1 = luminosity(color1);
-  const lum2 = luminosity(color2);
-
-  if (lum1 > lum2) {
-    return (lum1 + 0.05) / (lum2 + 0.05);
-  }
-
-  return (lum2 + 0.05) / (lum1 + 0.05);
-};
+) =>
+  wcagContrast(
+    {
+      mode: "rgb",
+      r: color1[0] / 255,
+      g: color1[1] / 255,
+      b: color1[2] / 255,
+    },
+    {
+      mode: "rgb",
+      r: color2[0] / 255,
+      g: color2[1] / 255,
+      b: color2[2] / 255,
+    }
+  );
 
 export const getRGBContrastRatio = (
   rgb1: [number, number, number],
