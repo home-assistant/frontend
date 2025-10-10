1b6c33efd43fea91e13037d9282c6d3763e0fdc1
Petar Petrov <MindFreeze@users.noreply.github.com>
2025-10-10 11:25:21 +0300

    Escape device names in energy dashboard (#27425)

--- src/panels/lovelace/cards/energy/common/energy-chart-options.ts
+++ src/panels/lovelace/cards/energy/common/energy-chart-options.ts
@@ -27,6 +27,7 @@ import {
 } from "../../../../../common/datetime/format_date";
 import { formatTime } from "../../../../../common/datetime/format_time";
 import type { ECOption } from "../../../../../resources/echarts";
+import { filterXSS } from "../../../../../common/util/xss";
 
 export function getSuggestedMax(dayDifference: number, end: Date): number {
   let suggestedMax = new Date(end);
@@ -201,7 +202,7 @@ function formatTooltip(
           countNegative++;
         }
       }
-      return `${param.marker} ${param.seriesName}: ${value} ${unit}`;
+      return `${param.marker} ${filterXSS(param.seriesName!)}: ${value} ${unit}`;
     })
     .filter(Boolean);
   let footer = "";
