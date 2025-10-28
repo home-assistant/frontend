f17bbc3f799f8a10fbf02c4ae8bd470529bc26be
Aidan Timson <aidan@timmo.dev>
2025-10-28 15:02:50 +0000

    Fix activity card height and add constraints for grid layout (#27683)
    * Fix logbook height
    
    * Add grid option constraints
    
    * Reverse

--- src/panels/lovelace/cards/hui-logbook-card.ts
+++ src/panels/lovelace/cards/hui-logbook-card.ts
@@ -14,7 +14,11 @@ import { findEntities } from "../common/find-entities";
 import { processConfigEntities } from "../common/process-config-entities";
 import "../components/hui-warning";
 import type { EntityConfig } from "../entity-rows/types";
-import type { LovelaceCard, LovelaceCardEditor } from "../types";
+import type {
+  LovelaceCard,
+  LovelaceCardEditor,
+  LovelaceGridOptions,
+} from "../types";
 import type { LogbookCardConfig } from "./types";
 import { resolveEntityIDs } from "../../../data/selector";
 import { ensureArray } from "../../../common/array/ensure-array";
@@ -62,6 +66,15 @@ export class HuiLogbookCard extends LitElement implements LovelaceCard {
 
   public getCardSize(): number {
     return 9 + (this._config?.title ? 1 : 0);
+  }
+
+  public getGridOptions(): LovelaceGridOptions {
+    return {
+      rows: 6,
+      columns: 12,
+      min_columns: 6,
+      min_rows: this._config?.title ? 4 : 3,
+    };
   }
 
   public validateTarget(
@@ -189,6 +202,10 @@ export class HuiLogbookCard extends LitElement implements LovelaceCard {
       >
         <div class="content">
           <ha-logbook
+            class=${classMap({
+              "is-grid": this.layout === "grid",
+              "is-panel": this.layout === "panel",
+            })}
             .hass=${this.hass}
             .time=${this._time}
             .entityIds=${this._getEntityIds()}
@@ -212,6 +229,7 @@ export class HuiLogbookCard extends LitElement implements LovelaceCard {
         }
 
         .content {
+          height: 100%;
           padding: 0 16px 16px;
         }
 
@@ -222,6 +240,11 @@ export class HuiLogbookCard extends LitElement implements LovelaceCard {
         ha-logbook {
           height: 385px;
           display: block;
+        }
+
+        ha-logbook.is-grid,
+        ha-logbook.is-panel {
+          height: 100%;
         }
 
         :host([ispanel]) .content,
