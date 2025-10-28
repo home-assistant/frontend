ec76bf3129857e4f22b1cf7daad48f043f8bd2b0
Aidan Timson <aidan@timmo.dev>
2025-10-28 15:48:16 +0000

    Remove entities length check

--- src/panels/lovelace/cards/hui-entities-card.ts
+++ src/panels/lovelace/cards/hui-entities-card.ts
@@ -147,9 +147,7 @@ class HuiEntitiesCard extends LitElement implements LovelaceCard {
     return {
       columns: 12,
       min_columns: 6,
-      min_rows:
-        (this._config?.title || this._showHeaderToggle ? 2 : 1) +
-        this._config.entities.length,
+      min_rows: this._config?.title || this._showHeaderToggle ? 3 : 2,
     };
   }
 
