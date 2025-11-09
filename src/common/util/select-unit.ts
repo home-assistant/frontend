89853e5f10d681a82dece5bc12dec7680e541bde
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2025-11-09 08:02:57 +0300

    restored for days & hours

--- src/common/util/select-unit.ts
+++ src/common/util/select-unit.ts
@@ -121,8 +121,8 @@ type Thresholds = Record<
 export const DEFAULT_THRESHOLDS: Thresholds = {
   second: 59, // seconds to minute
   minute: 59, // minutes to hour
-  hour: 23, // hour to day
-  day: 6, // day to week
+  hour: 22, // hour to day
+  day: 5, // day to week
   week: 4, // week to months
   month: 11, // month to years
 };
