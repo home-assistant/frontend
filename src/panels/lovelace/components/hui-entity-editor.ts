dc8f1211e6f45939f831c15a2dd030ee4d57f4ac
karwosts <32912880+karwosts@users.noreply.github.com>
2025-11-10 04:09:44 -0800

    Fix entity editor with non-existant entity (#27875)

--- src/panels/lovelace/components/hui-entity-editor.ts
+++ src/panels/lovelace/components/hui-entity-editor.ts
@@ -39,29 +39,31 @@ export class HuiEntityEditor extends LitElement {
   private _renderItem(item: EntityConfig, index: number) {
     const stateObj = this.hass.states[item.entity];
 
-    const useDeviceName = entityUseDeviceName(
-      stateObj,
-      this.hass.entities,
-      this.hass.devices
-    );
+    const useDeviceName =
+      stateObj &&
+      entityUseDeviceName(stateObj, this.hass.entities, this.hass.devices);
 
     const isRTL = computeRTL(this.hass);
 
     const primary =
+      (stateObj &&
+        this.hass.formatEntityName(
+          stateObj,
+          useDeviceName ? { type: "device" } : { type: "entity" }
+        )) ||
+      item.entity;
+
+    const secondary =
+      stateObj &&
       this.hass.formatEntityName(
         stateObj,
-        useDeviceName ? { type: "device" } : { type: "entity" }
-      ) || item.entity;
-
-    const secondary = this.hass.formatEntityName(
-      stateObj,
-      useDeviceName
-        ? [{ type: "area" }]
-        : [{ type: "area" }, { type: "device" }],
-      {
-        separator: isRTL ? " ◂ " : " ▸ ",
-      }
-    );
+        useDeviceName
+          ? [{ type: "area" }]
+          : [{ type: "area" }, { type: "device" }],
+        {
+          separator: isRTL ? " ◂ " : " ▸ ",
+        }
+      );
 
     return html`
       <ha-md-list-item class="item">
