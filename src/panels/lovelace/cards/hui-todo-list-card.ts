ae87338bf486fda2d0724b0e04d1a16c057c125d
github-pallavi <palgup31@gmail.com>
2025-12-02 18:07:03 +0100

    added priority display in span class

--- src/panels/lovelace/cards/hui-todo-list-card.ts
+++ src/panels/lovelace/cards/hui-todo-list-card.ts
@@ -492,7 +492,6 @@ export class HuiTodoListCard extends LitElement implements LovelaceCard {
               <div class="column">
                 <span class="summary">${item.summary}</span>
                 <span class="summary">Priority:${item.priority}</span> #priority
-                display only
                 ${item.description
                   ? html`<ha-markdown-element
                       class="description"
