5e3d38c073974504b0fc1b3dcba6d34b85f18091
Wendelin <w@pe8.at>
2025-12-03 08:53:58 +0100

    Add unchecked icon support to ha-dropdown-item component

--- src/components/ha-dropdown-item.ts
+++ src/components/ha-dropdown-item.ts
@@ -1,6 +1,9 @@
 import DropdownItem from "@home-assistant/webawesome/dist/components/dropdown-item/dropdown-item";
-import { css, type CSSResultGroup } from "lit";
+import "@home-assistant/webawesome/dist/components/icon/icon";
+import { mdiCheckboxBlankOutline, mdiCheckboxMarked } from "@mdi/js";
+import { css, type CSSResultGroup, html, nothing } from "lit";
 import { customElement } from "lit/decorators";
+import "./ha-svg-icon";
 
 /**
  * Home Assistant dropdown item component
@@ -14,12 +17,76 @@ import { customElement } from "lit/decorators";
  */
 @customElement("ha-dropdown-item")
 export class HaDropdownItem extends DropdownItem {
+  // overwritten wa-dropdown-item render function, to add unchecked icon support
+  render() {
+    return html`
+      ${this.type === "checkbox"
+        ? html`
+            <ha-svg-icon
+              id="check"
+              part="checkmark"
+              exportparts="svg:checkmark__svg"
+              library="system"
+              name="check"
+              .path=${this.checked
+                ? mdiCheckboxMarked
+                : mdiCheckboxBlankOutline}
+              slot="icon"
+            ></ha-svg-icon>
+          `
+        : nothing}
+
+      <span id="icon" part="icon">
+        <slot name="icon"></slot>
+      </span>
+
+      <span id="label" part="label">
+        <slot></slot>
+      </span>
+
+      <span id="details" part="details">
+        <slot name="details"></slot>
+      </span>
+
+      ${this.hasSubmenu
+        ? html`
+            <wa-icon
+              id="submenu-indicator"
+              part="submenu-icon"
+              exportparts="svg:submenu-icon__svg"
+              library="system"
+              name="chevron-right"
+            ></wa-icon>
+          `
+        : nothing}
+      ${this.hasSubmenu
+        ? html`
+            <div
+              id="submenu"
+              part="submenu"
+              popover="manual"
+              role="menu"
+              tabindex="-1"
+              aria-orientation="vertical"
+              hidden
+            >
+              <slot name="submenu"></slot>
+            </div>
+          `
+        : nothing}
+    `;
+  }
+
   static get styles(): CSSResultGroup {
     return [
       DropdownItem.styles,
       css`
         :host {
           min-height: var(--ha-space-10);
+        }
+
+        #check {
+          visibility: visible;
         }
 
         #icon ::slotted(*) {
