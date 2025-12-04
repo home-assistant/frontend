438a1b18c16fba99d8f8b7f7ab69974934e58f30
Jan-Philipp Benecke <jan-philipp@bnck.me>
2025-12-04 10:32:23 +0100

    Fix layout of sidebar settings row in user profile

--- src/panels/profile/ha-profile-section-general.ts
+++ src/panels/profile/ha-profile-section-general.ts
@@ -158,7 +158,7 @@ class HaProfileSectionGeneral extends LitElement {
               .narrow=${this.narrow}
               .hass=${this.hass}
             ></ha-pick-dashboard-row>
-            <ha-settings-row .narrow=${this.narrow}>
+            <ha-settings-row .narrow=${this.narrow} class="customize-sidebar">
               <span slot="heading">
                 ${this.hass.localize(
                   "ui.panel.profile.customize_sidebar.header"
@@ -294,6 +294,10 @@ class HaProfileSectionGeneral extends LitElement {
         .promo-advanced {
           text-align: center;
           color: var(--secondary-text-color);
+        }
+
+        .customize-sidebar {
+          --settings-row-content-display: contents;
         }
       `,
     ];
