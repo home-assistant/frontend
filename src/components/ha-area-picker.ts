c963d6641ce7d7bcd0174b2d1f71fdbe835aae87
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2025-12-12 02:22:04 +0300

    Apply suggestion from @timmo001
    Co-authored-by: Aidan Timson <aidan@timmo.dev>

--- src/components/ha-area-picker.ts
+++ src/components/ha-area-picker.ts
@@ -367,12 +367,11 @@ export class HaAreaPicker extends LitElement {
 
     let showLabel = this.showLabel;
     if (this.value) {
-      const area_id = this.value;
-      const area = this.hass.areas[area_id];
-      const { floor } = getAreaContext(area, this.hass.floors);
-      showLabel = floor ? false : this.showLabel;
-    } else {
-      showLabel = this.showLabel;
+      const area = this.hass.areas[this.value];
+      if (area) {
+        const { floor } = getAreaContext(area, this.hass.floors);
+        showLabel = !floor && this.showLabel;
+      }
     }
 
     return html`
