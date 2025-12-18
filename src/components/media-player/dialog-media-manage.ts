f2fe2c240641a0f4ff2744a31625ea9f532d6fea
karwosts <karwosts@gmail.com>
2025-12-18 13:38:47 +0000

    remove old styles

--- src/components/media-player/dialog-media-manage.ts
+++ src/components/media-player/dialog-media-manage.ts
@@ -22,7 +22,6 @@ import {
   removeLocalMedia,
 } from "../../data/media_source";
 import { showConfirmationDialog } from "../../dialogs/generic/show-dialog-box";
-import { haStyleDialog, haStyleDialogFixedTop } from "../../resources/styles";
 import type { HomeAssistant } from "../../types";
 import "../ha-button";
 import "../ha-check-list-item";
@@ -333,23 +332,7 @@ class DialogMediaManage extends LitElement {
 
   static get styles(): CSSResultGroup {
     return [
-      haStyleDialog,
-      haStyleDialogFixedTop,
       css`
-        ha-dialog {
-          --dialog-z-index: 9;
-          --dialog-content-padding: 0;
-        }
-
-        @media (min-width: 800px) {
-          ha-dialog {
-            --mdc-dialog-max-width: 800px;
-            --mdc-dialog-max-height: calc(
-              100vh - var(--ha-space-18) - var(--safe-area-inset-y)
-            );
-          }
-        }
-
         ha-dialog-header ha-media-upload-button,
         ha-dialog-header ha-button {
           --mdc-theme-primary: var(--primary-text-color);
