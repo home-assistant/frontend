af38dc0a079f6f0f79ea1565e28b9dc3b22f155c
sebcaps <sebastien.carecolin@gmail.com>
2025-12-28 07:27:31 +0000

    showPercent

--- src/panels/lovelace/cards/energy/common/energy-chart-options.ts
+++ src/panels/lovelace/cards/energy/common/energy-chart-options.ts
@@ -216,7 +216,7 @@ function formatTooltip(
           countNegative++;
         }
       }
-      return `${param.marker} ${filterXSS(param.seriesName!)}: <div style="direction:ltr; display: inline;">${value} ${unit}</div>`;
+      return `${param.marker} ${filterXSS(param.seriesName!)}: <div style="direction:ltr; display: inline;">${value} ${unit} (${(parseFloat(value) / sumPositive) * 100}%)</div>`;
     })
     .filter(Boolean);
   let footer = "";
