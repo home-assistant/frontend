6cdd427402865540b5a5c9dc9787a3f9db1b8656
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2026-01-09 09:12:58 +0300

    Update available-assistants.ts

--- src/panels/config/voice-assistants/expose/available-assistants.ts
+++ src/panels/config/voice-assistants/expose/available-assistants.ts
@@ -1,1 +1,29 @@
+import memoizeOne from "memoize-one";
+import type { CloudStatus } from "../../../../data/cloud";
+import { voiceAssistants } from "../../../../data/expose";
 
+export const getAvailableAssistants = memoizeOne(
+  (cloudStatus: CloudStatus | undefined) => {
+    const googleEnabled =
+      cloudStatus?.logged_in === true &&
+      cloudStatus.prefs.google_enabled === true;
+    const alexaEnabled =
+      cloudStatus?.logged_in === true &&
+      cloudStatus.prefs.alexa_enabled === true;
+
+    const showAssistants = [...Object.keys(voiceAssistants)];
+
+    if (!googleEnabled) {
+      showAssistants.splice(
+        showAssistants.indexOf("cloud.google_assistant"),
+        1
+      );
+    }
+
+    if (!alexaEnabled) {
+      showAssistants.splice(showAssistants.indexOf("cloud.alexa"), 1);
+    }
+
+    return showAssistants;
+  }
+);
