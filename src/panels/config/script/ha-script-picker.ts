04cb46bc9ee40e58573f7847996e8c8e375c094a
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2026-01-12 13:32:59 +0300

    Update src/panels/config/script/ha-script-picker.ts
    Co-authored-by: Wendelin <12148533+wendevlin@users.noreply.github.com>

--- src/panels/config/script/ha-script-picker.ts
+++ src/panels/config/script/ha-script-picker.ts
@@ -262,7 +262,7 @@ class HaScriptPicker extends SubscribeMixin(LitElement) {
           labels: (labels || []).map(
             (lbl) => labelReg!.find((label) => label.label_id === lbl)!
           ),
-          assistants: assistants,
+          assistants,
           assistants_sortable_key: getAssistantsSortableKey(assistants),
           selectable: entityRegEntry !== undefined,
         };
