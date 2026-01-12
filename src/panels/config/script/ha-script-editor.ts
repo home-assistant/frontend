a27f193bfac1701d0111739374a7463b2372519d
leonace924 <meobius123@gmail.com>
2026-01-12 12:11:38 +0100

    fix: revert the unrelated code

--- src/panels/config/script/ha-script-editor.ts
+++ src/panels/config/script/ha-script-editor.ts
@@ -508,7 +508,6 @@ export class HaScriptEditor extends SubscribeMixin(
                     @editor-save=${this._handleSaveScript}
                     .showErrors=${false}
                   ></ha-yaml-editor>
-                  <div class="fab-bottom-space"></div>
                   <ha-fab
                     slot="fab"
                     class=${!this._readOnly && this._dirty ? "dirty" : ""}
@@ -1253,10 +1252,6 @@ export class HaScriptEditor extends SubscribeMixin(
           min-height: 0;
           display: flex;
           flex-direction: column;
-        }
-        .yaml-mode .fab-bottom-space {
-          flex-shrink: 0;
-          height: calc(80px + var(--safe-area-inset-bottom, 0px));
         }
         p {
           margin-bottom: 0;
