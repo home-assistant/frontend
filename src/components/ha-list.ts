246aba24b975de9fa3515aff336147f60fa328f4
Pavilion <pavilionsahota@gmail.com>
2026-01-13 09:58:18 +0000

    removing default setting

--- src/components/ha-list.ts
+++ src/components/ha-list.ts
@@ -5,12 +5,6 @@ import { customElement } from "lit/decorators";
 @customElement("ha-list")
 export class HaList extends ListBase {
   static styles = styles;
-
-  constructor() {
-    super();
-    if (this.innerRole == null) this.innerRole = "list";
-    if (this.itemRoles == null) this.itemRoles = "listitem";
-  }
 }
 
 declare global {
