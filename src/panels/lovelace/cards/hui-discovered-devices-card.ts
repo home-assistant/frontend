04688cd1f877fe0ff4d35400926b008216d80ef8
Paul Bottein <paul.bottein@gmail.com>
2026-01-16 17:40:39 +0100

    Migrate discovered devices card

--- src/panels/lovelace/cards/hui-discovered-devices-card.ts
+++ src/panels/lovelace/cards/hui-discovered-devices-card.ts
@@ -1,14 +1,11 @@
 import { mdiDevices } from "@mdi/js";
-import type { PropertyValues, TemplateResult } from "lit";
 import type { UnsubscribeFunc } from "home-assistant-js-websocket";
+import type { PropertyValues, TemplateResult } from "lit";
 import { css, html, LitElement, nothing } from "lit";
 import { customElement, property, state } from "lit/decorators";
-import { classMap } from "lit/directives/class-map";
-import { ifDefined } from "lit/directives/if-defined";
 import { fireEvent } from "../../../common/dom/fire_event";
 import "../../../components/ha-card";
-import "../../../components/ha-ripple";
-import "../../../components/ha-svg-icon";
+import "../../../components/tile/ha-tile-container";
 import "../../../components/tile/ha-tile-icon";
 import "../../../components/tile/ha-tile-info";
 import {
@@ -20,13 +17,10 @@ import type { DataEntryFlowProgress } from "../../../data/data_entry_flow";
 import type { ActionHandlerEvent } from "../../../data/lovelace/action_handler";
 import { SubscribeMixin } from "../../../mixins/subscribe-mixin";
 import type { HomeAssistant } from "../../../types";
-import { actionHandler } from "../common/directives/action-handler-directive";
 import { handleAction } from "../common/handle-action";
 import { hasAction } from "../common/has-action";
 import type { LovelaceCard, LovelaceGridOptions } from "../types";
 import type { DiscoveredDevicesCardConfig } from "./types";
-
-const ICON = mdiDevices;
 
 @customElement("hui-discovered-devices-card")
 export class HuiDiscoveredDevicesCard
@@ -159,35 +153,24 @@ export class HuiDiscoveredDevicesCard
           })
         : this.hass.localize("ui.card.discovered-devices.no_devices");
 
-    const contentClasses = { vertical: Boolean(this._config.vertical) };
-
     return html`
       <ha-card>
-        <div
-          class="background"
-          @action=${this._handleAction}
-          .actionHandler=${actionHandler({
+        <ha-tile-container
+          .vertical=${Boolean(this._config.vertical)}
+          .interactive=${this._hasCardAction}
+          .actionHandlerOptions=${{
             hasHold: hasAction(this._config!.hold_action),
             hasDoubleClick: hasAction(this._config!.double_tap_action),
-          })}
-          role=${ifDefined(this._hasCardAction ? "button" : undefined)}
-          tabindex=${ifDefined(this._hasCardAction ? "0" : undefined)}
-          aria-labelledby="info"
+          }}
+          @action=${this._handleAction}
         >
-          <ha-ripple .disabled=${!this._hasCardAction}></ha-ripple>
-        </div>
-        <div class="container">
-          <div class="content ${classMap(contentClasses)}">
-            <ha-tile-icon>
-              <ha-svg-icon slot="icon" .path=${ICON}></ha-svg-icon>
-            </ha-tile-icon>
-            <ha-tile-info
-              id="info"
-              .primary=${label}
-              .secondary=${secondary}
-            ></ha-tile-info>
-          </div>
-        </div>
+          <ha-tile-icon slot="icon" .iconPath=${mdiDevices}></ha-tile-icon>
+          <ha-tile-info
+            slot="info"
+            .primary=${label}
+            .secondary=${secondary}
+          ></ha-tile-info>
+        </ha-tile-container>
       </ha-card>
     `;
   }
@@ -197,7 +180,7 @@ export class HuiDiscoveredDevicesCard
       --tile-color: var(--primary-color);
       -webkit-tap-highlight-color: transparent;
     }
-    ha-card:has(.background:focus-visible) {
+    ha-card:has(ha-tile-container[focused]) {
       --shadow-default: var(--ha-card-box-shadow, 0 0 0 0 transparent);
       --shadow-focus: 0 0 0 1px var(--tile-color);
       border-color: var(--tile-color);
@@ -211,67 +194,9 @@ export class HuiDiscoveredDevicesCard
       transition:
         box-shadow 180ms ease-in-out,
         border-color 180ms ease-in-out;
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
     }
-    [role="button"] {
-      cursor: pointer;
-      pointer-events: auto;
-    }
-    [role="button"]:focus {
-      outline: none;
-    }
-    .background {
-      position: absolute;
-      top: 0;
-      left: 0;
-      bottom: 0;
-      right: 0;
-      border-radius: var(--ha-card-border-radius, var(--ha-border-radius-lg));
-      margin: calc(-1 * var(--ha-card-border-width, 1px));
-      overflow: hidden;
-    }
-    .container {
-      margin: calc(-1 * var(--ha-card-border-width, 1px));
-      display: flex;
-      flex-direction: column;
-      flex: 1;
-    }
-    .content {
-      position: relative;
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-      padding: 10px;
-      flex: 1;
-      min-width: 0;
-      box-sizing: border-box;
-      pointer-events: none;
-      gap: 10px;
-    }
-
-    .vertical {
-      flex-direction: column;
-      text-align: center;
-      justify-content: center;
-    }
-    .vertical ha-tile-info {
-      width: 100%;
-      flex: none;
-    }
-
     ha-tile-icon {
       --tile-icon-color: var(--tile-color);
-      position: relative;
-      padding: 6px;
-      margin: -6px;
-    }
-    ha-tile-info {
-      position: relative;
-      min-width: 0;
-      transition: background-color 180ms ease-in-out;
-      box-sizing: border-box;
     }
   `;
 }
