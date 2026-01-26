aa1542b918fd748e268074f3191c037d0eb8a52d
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2026-01-26 12:51:35 +0300

    Create dialog-enlarge-mixin.ts

---
+++ src/mixins/dialog-enlarge-mixin.ts
@@ -0,0 +1,59 @@
+import { property } from "lit/decorators";
+import { type LitElement, css, unsafeCSS } from "lit";
+import type { Constructor } from "../types";
+import { getInheritedStyles } from "./common";
+
+export interface DialogEnlargeConfig {
+  enlargeableElement: string;
+  minWidth: number;
+  style: string;
+}
+
+/**
+ * Mixin that adds an ability to enlarge a dialog by tapping on a title.
+ * @param superClass - The LitElement class to extend.
+ * @returns Extended class with "enlarge" functionality.
+ */
+export const DialogEnlargeMixin = <T extends Constructor<LitElement>>(
+  superClass: T
+) => {
+  class DialogEnlargeClass extends superClass {
+    @property({ type: Boolean, reflect: true }) public large = false;
+
+    protected static getEnlargeConfig(): DialogEnlargeConfig {
+      return {
+        enlargeableElement: ".content",
+        minWidth: 451,
+        style: "max-width: none;",
+      };
+    }
+
+    protected enlarge() {
+      this.large = !this.large;
+    }
+
+    static get styles() {
+      // prettier-ignore
+      return [
+        ...getInheritedStyles(this),
+        css`
+          .title-enlargeable {
+            display: block;
+          }
+
+          @media all and
+          (min-width: ${this.getEnlargeConfig().minWidth}px) {
+            :host([large])
+              ${unsafeCSS(
+                this.getEnlargeConfig().enlargeableElement
+              )} {
+                ${unsafeCSS(this.getEnlargeConfig().style)}
+              }
+          }
+        `,
+      ];
+    }
+  }
+
+  return DialogEnlargeClass;
+};
