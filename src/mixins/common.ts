d05362635acc77043582bec7ca23ec1ce8107fe1
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2026-01-26 12:49:56 +0300

    Update common.ts

--- src/mixins/common.ts
+++ src/mixins/common.ts
@@ -1,1 +1,17 @@
+import type { CSSResultGroup, LitElement } from "lit";
 
+// prettier-ignore
+const stylesArray = (styles?: CSSResultGroup | CSSResultGroup[]) =>
+  styles === undefined
+    ? []
+    : Array.isArray(styles)
+      ? styles : [styles];
+
+export const getInheritedStyles = (thisElement) => {
+  const superCtor = Object.getPrototypeOf(thisElement) as
+    | typeof LitElement
+    | undefined;
+  return stylesArray(
+    (superCtor?.styles ?? []) as CSSResultGroup | CSSResultGroup[]
+  );
+};
