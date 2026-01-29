2b6c404f3fe4e65bae29590237452df8185c0873
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2026-01-29 11:26:47 +0300

    fix condition

--- src/common/entity/group_entities.ts
+++ src/common/entity/group_entities.ts
@@ -8,7 +8,7 @@ export const computeGroupEntitiesState = (states: HassEntity[]): string => {
     return UNAVAILABLE;
   }
 
-  const validState = states.filter((stateObj) => !isUnavailableState(stateObj.state));
+  const validState = states.some((stateObj) => !isUnavailableState(stateObj.state));
 
   if (!validState) {
     return UNAVAILABLE;
