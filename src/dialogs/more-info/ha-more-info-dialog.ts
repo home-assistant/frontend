525c61131c13cb26f45df0cad71e896258b40925
Pavilion <pavilionsahota@gmail.com>
2026-02-02 13:37:29 +0000

    added transition for more info dialog

--- src/dialogs/more-info/ha-more-info-dialog.ts
+++ src/dialogs/more-info/ha-more-info-dialog.ts
@@ -808,6 +808,9 @@ export class MoreInfoDialog extends ScrollableFadeMixin(LitElement) {
       css`
         ha-dialog {
           --dialog-content-padding: 0;
+          --ha-dialog-surface-transition:
+            min-width var(--ha-animation-base-duration) ease,
+            max-width var(--ha-animation-base-duration) ease;
         }
 
         .content-wrapper {
