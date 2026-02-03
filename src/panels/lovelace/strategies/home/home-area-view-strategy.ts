cfa46719abda2f812c0b9a1264efd7e7fa4856bd
Paul Bottein <paul.bottein@gmail.com>
2026-02-03 16:37:57 +0100

    Improve other devices page in home dashboard

--- src/panels/lovelace/strategies/home/home-area-view-strategy.ts
+++ src/panels/lovelace/strategies/home/home-area-view-strategy.ts
@@ -253,11 +253,6 @@ export class HomeAreaViewStrategy extends ReactiveElement {
       device_class: "battery",
     });
 
-    const energyFilter = generateEntityFilter(hass, {
-      domain: "sensor",
-      device_class: ["energy", "power"],
-    });
-
     const primaryFilter = generateEntityFilter(hass, {
       entity_category: "none",
     });
@@ -269,7 +264,7 @@ export class HomeAreaViewStrategy extends ReactiveElement {
         batteryFilter(e)
       );
       const entities = deviceEntities.entities.filter(
-        (e) => !batteryFilter(e) && !energyFilter(e) && primaryFilter(e)
+        (e) => !batteryFilter(e) && primaryFilter(e)
       );
 
       if (entities.length === 0) {
