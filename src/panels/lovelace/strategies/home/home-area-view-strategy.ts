bc5b04a49d872158fe9dea893204587dd1469cff
Paul Bottein <paul.bottein@gmail.com>
2026-02-03 16:53:30 +0100

    Use area icon for area empty state

--- src/panels/lovelace/strategies/home/home-area-view-strategy.ts
+++ src/panels/lovelace/strategies/home/home-area-view-strategy.ts
@@ -365,7 +365,7 @@ export class HomeAreaViewStrategy extends ReactiveElement {
         cards: [
           {
             type: "empty-state",
-            icon: "mdi:sofa-outline",
+            icon: area.icon || "mdi:sofa-outline",
             icon_color: "primary",
             content_only: true,
             title: hass.localize(
