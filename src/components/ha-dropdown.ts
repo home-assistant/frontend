905c44b74bc45ccda490dd2cb18628a5c99c984f
Aidan Timson <aidan@timmo.dev>
2026-02-04 13:56:59 +0000

    Fix potential type conflict

--- src/components/ha-dropdown.ts
+++ src/components/ha-dropdown.ts
@@ -4,7 +4,7 @@ import { customElement, property } from "lit/decorators";
 import type { HaDropdownItem } from "./ha-dropdown-item";
 
 export type HaDropdownSelectEvent<T = string> = CustomEvent<{
-  item: HaDropdownItem & { value: T };
+  item: Omit<HaDropdownItem, "value"> & { value: T };
 }>;
 
 /**
