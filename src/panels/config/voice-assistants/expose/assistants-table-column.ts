68be860fa9b1dc819daad60c3b0e296c9d58caef
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2026-02-09 00:06:00 +0300

    show icons in flex

--- src/panels/config/voice-assistants/expose/assistants-table-column.ts
+++ src/panels/config/voice-assistants/expose/assistants-table-column.ts
@@ -26,21 +26,23 @@ export function getAssistantsTableColumn<T>(
     valueColumn: "assistants_sortable_key",
     template: (entry: any) =>
       html`${entry.assistants.length !== 0
-        ? availableAssistants.map((vaId) => {
-            const supported =
-              !supportedEntities?.[vaId] ||
-              supportedEntities[vaId].includes(entry.entity_id);
-            const manual = entry.manAssistants?.includes(vaId);
-            return getAssistantsTableColumnIcon(
-              entry.entity_id,
-              entry.assistants.includes(vaId),
-              vaId,
-              hass,
-              entitiesToCheck,
-              manual,
-              !supported
-            );
-          })
+        ? html`<div style="display: flex; gap: var(--ha-space-4);">
+            ${availableAssistants.map((vaId) => {
+              const supported =
+                !supportedEntities?.[vaId] ||
+                supportedEntities[vaId].includes(entry.entity_id);
+              const manual = entry.manAssistants?.includes(vaId);
+              return getAssistantsTableColumnIcon(
+                entry.entity_id,
+                entry.assistants.includes(vaId),
+                vaId,
+                hass,
+                entitiesToCheck,
+                manual,
+                !supported
+              );
+            })}
+          </div>`
         : nothing}`,
   };
 }
