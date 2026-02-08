b2fdb32f1b9aefd91fbcbced8386e1d73f0a199b
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2026-02-08 19:41:53 +0300

    use ha-space-4

--- src/components/ha-filter-domains.ts
+++ src/components/ha-filter-domains.ts
@@ -180,7 +180,7 @@ export class HaFilterDomains extends LitElement {
           margin-inline-end: 8px;
         }
         ha-check-list-item {
-          --mdc-list-item-graphic-margin: 16px;
+          --mdc-list-item-graphic-margin: var(--ha-space-4);
         }
         .badge {
           display: inline-block;
