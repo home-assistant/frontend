c190cd0b28f2abac12aa0dea0e4a622e931bc456
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2026-02-09 13:18:00 +0300

    remove prettier-ignore

--- src/common/color/compute-color.ts
+++ src/common/color/compute-color.ts
@@ -7,10 +7,7 @@ const YAML_ONLY_THEMES_COLORS = new Set([
 ]);
 
 export function computeCssColor(color: string): string {
-  // prettier-ignore
-  if (
-    color in DEFAULT_THEME_COLORS ||
-    YAML_ONLY_THEMES_COLORS.has(color)
+  if (color in DEFAULT_THEME_COLORS || YAML_ONLY_THEMES_COLORS.has(color)
   ) {
     return `var(--${color}-color)`;
   }
