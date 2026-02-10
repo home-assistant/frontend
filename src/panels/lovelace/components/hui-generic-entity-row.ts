99bb3f0bcc603bed74dd5136880d2873a7a23b4c
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2026-02-10 16:32:25 +0300

    Apply suggestion from @MindFreeze
    Co-authored-by: Petar Petrov <MindFreeze@users.noreply.github.com>

--- src/panels/lovelace/components/hui-generic-entity-row.ts
+++ src/panels/lovelace/components/hui-generic-entity-row.ts
@@ -212,7 +212,7 @@ export class HuiGenericEntityRow extends LitElement {
                                         )}`
                                       : this.config.secondary_info === "area"
                                         ? area
-                                          ? html`${area}`
+                                          ? area
                                           : nothing
                                         : nothing)}
                     </div>
