d225e7ef5f62a83c8b26615e0e7d1c7128478e8c
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2026-02-10 17:48:23 +0300

    remove styles

--- src/panels/config/entities/ha-config-entities.ts
+++ src/panels/config/entities/ha-config-entities.ts
@@ -1688,10 +1688,6 @@ ${rejected
         ha-dropdown ha-assist-chip {
           --md-assist-chip-trailing-space: 8px;
         }
-        ha-label {
-          --ha-label-background-color: var(--color, var(--grey-color));
-          --primary-text-color: var(--color-text);
-        }
       `,
     ];
   }
