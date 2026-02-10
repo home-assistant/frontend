2ce317b666e64c7b7ab8417c50d55b537a0791b7
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2026-02-10 17:46:10 +0300

    remove styles

--- src/panels/config/automation/ha-automation-picker.ts
+++ src/panels/config/automation/ha-automation-picker.ts
@@ -1495,10 +1495,6 @@ ${rejected
         ha-dropdown ha-assist-chip {
           --md-assist-chip-trailing-space: 8px;
         }
-        ha-label {
-          --ha-label-background-color: var(--color, var(--grey-color));
-          --primary-text-color: var(--color-text);
-        }
       `,
     ];
   }
