eca535dd81252a2d75d4427e53a2c2d75d20bf9c
Aidan Timson <aidan@timmo.dev>
2026-02-13 12:43:46 +0000

    Migrate more info dialog to ha-wa-dialog (#28583)

--- src/resources/styles.ts
+++ src/resources/styles.ts
@@ -193,7 +193,8 @@ export const haStyleDialog = css`
 `;
 
 export const haStyleDialogFixedTop = css`
-  ha-dialog {
+  ha-dialog,
+  ha-wa-dialog {
     /* Pin dialog to top so it doesn't jump when content changes size */
     --vertical-align-dialog: flex-start;
     --dialog-surface-margin-top: var(--ha-space-10);
@@ -209,16 +210,31 @@ export const haStyleDialogFixedTop = css`
           0px
         )
     );
+    --ha-dialog-max-height: calc(
+      100vh - var(--dialog-surface-margin-top) - var(--ha-space-2) - var(
+          --safe-area-inset-y,
+          0px
+        )
+    );
+    --ha-dialog-max-height: calc(
+      100svh - var(--dialog-surface-margin-top) - var(--ha-space-2) - var(
+          --safe-area-inset-y,
+          0px
+        )
+    );
   }
 
   @media all and (max-width: 450px), all and (max-height: 500px) {
-    ha-dialog {
+    ha-dialog,
+    ha-wa-dialog {
       /* When in fullscreen, dialog should be attached to top */
       --dialog-surface-margin-top: 0px;
       --mdc-dialog-min-height: 100vh;
       --mdc-dialog-min-height: 100svh;
       --mdc-dialog-max-height: 100vh;
       --mdc-dialog-max-height: 100svh;
+      --ha-dialog-max-height: 100vh;
+      --ha-dialog-max-height: 100svh;
     }
   }
 `;
