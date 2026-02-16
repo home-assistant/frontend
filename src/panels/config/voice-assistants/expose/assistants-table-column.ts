0525a84c51623d12594c6dca74e1d2b20b8304e1
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2026-02-16 11:02:10 +0300

    narrow dow Assistants a bit more

--- src/panels/config/voice-assistants/expose/assistants-table-column.ts
+++ src/panels/config/voice-assistants/expose/assistants-table-column.ts
@@ -21,12 +21,12 @@ export function getAssistantsTableColumn<T>(
     defaultHidden: !visible,
     sortable: true,
     showNarrow: true,
-    minWidth: "120px",
-    maxWidth: "120px",
+    minWidth: "112px",
+    maxWidth: "112px",
     valueColumn: "assistants_sortable_key",
     template: (entry: any) =>
       html`${entry.assistants.length !== 0
-        ? html`<div style="display: flex; gap: var(--ha-space-2);">
+        ? html`<div style="display: flex; gap: var(--ha-space-1);">
             ${availableAssistants.map((vaId) => {
               const supported =
                 !supportedEntities?.[vaId] ||
