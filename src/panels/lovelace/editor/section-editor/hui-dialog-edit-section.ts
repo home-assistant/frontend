0c34165bcf2c86b23c3f7ff650e730ff96e02f52
karwosts <32912880+karwosts@users.noreply.github.com>
2026-02-23 02:54:11 -0800

    Disallow moving a section to non-sections view (#29756)

--- src/panels/lovelace/editor/section-editor/hui-dialog-edit-section.ts
+++ src/panels/lovelace/editor/section-editor/hui-dialog-edit-section.ts
@@ -46,6 +46,8 @@ import "./hui-section-settings-editor";
 import "./hui-section-visibility-editor";
 import type { EditSectionDialogParams } from "./show-edit-section-dialog";
 import type { HaDropdownSelectEvent } from "../../../../components/ha-dropdown";
+import { getViewType } from "../../views/get-view-type";
+import { SECTIONS_VIEW_LAYOUT } from "../../views/const";
 
 const TABS = ["tab-settings", "tab-visibility"] as const;
 
@@ -290,13 +292,16 @@ export class HuiDialogEditSection
 
     const toView = selectedDashConfig.views[viewIndex];
 
-    if (isStrategyView(toView)) {
+    if (
+      isStrategyView(toView) ||
+      getViewType(toView) !== SECTIONS_VIEW_LAYOUT
+    ) {
       showAlertDialog(this, {
         title: this.hass!.localize(
           "ui.panel.lovelace.editor.move_section.error_title"
         ),
         text: this.hass!.localize(
-          "ui.panel.lovelace.editor.move_section.error_text_strategy"
+          "ui.panel.lovelace.editor.move_section.error_text"
         ),
         warning: true,
       });
