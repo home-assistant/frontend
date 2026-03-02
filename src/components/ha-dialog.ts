640f2b9245e8e6b4c3bb68dd91ec279d213d8221
Wendelin <12148533+wendevlin@users.noreply.github.com>
2026-03-02 12:41:52 +0100

    Dialog: Add show event target check (#29927)
    Add event phase check in _handleShow and _handleAfterShow methods

--- src/components/ha-dialog.ts
+++ src/components/ha-dialog.ts
@@ -204,7 +204,10 @@ export class HaDialog extends ScrollableFadeMixin(LitElement) {
     `;
   }
 
-  private _handleShow = async () => {
+  private _handleShow = async (ev: Event) => {
+    if (ev.eventPhase !== Event.AT_TARGET) {
+      return;
+    }
     this._open = true;
     fireEvent(this, "opened");
 
@@ -230,7 +233,10 @@ export class HaDialog extends ScrollableFadeMixin(LitElement) {
     });
   };
 
-  private _handleAfterShow = () => {
+  private _handleAfterShow = (ev: Event) => {
+    if (ev.eventPhase !== Event.AT_TARGET) {
+      return;
+    }
     fireEvent(this, "after-show");
   };
 
