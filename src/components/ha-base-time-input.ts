9a7a8fd3773b0dbac3d78991226d656441579b09
Wendelin <12148533+wendevlin@users.noreply.github.com>
2026-03-03 08:05:49 +0100

    Add reportValidity in ha-form (#29884)
    * Add validation for required fields in ha-auth-flow before submission
    
    * Add reportValidity methods to form components for improved validation handling
    
    * Remove async reportValidity funcs
    
    * Review

--- src/components/ha-base-time-input.ts
+++ src/components/ha-base-time-input.ts
@@ -1,7 +1,7 @@
 import { mdiClose } from "@mdi/js";
 import type { TemplateResult } from "lit";
 import { css, html, LitElement, nothing } from "lit";
-import { customElement, property } from "lit/decorators";
+import { customElement, property, queryAll } from "lit/decorators";
 import { ifDefined } from "lit/directives/if-defined";
 import { fireEvent } from "../common/dom/fire_event";
 import "./ha-icon-button";
@@ -132,6 +132,17 @@ export class HaBaseTimeInput extends LitElement {
   @property({ attribute: false }) amPm: "AM" | "PM" = "AM";
 
   @property({ type: Boolean, reflect: true }) public clearable?: boolean;
+
+  @queryAll("ha-textfield") private _inputs?: HaTextField[];
+
+  static shadowRootOptions = {
+    ...LitElement.shadowRootOptions,
+    delegatesFocus: true,
+  };
+
+  public reportValidity(): boolean {
+    return this._inputs?.every((input) => input.reportValidity()) ?? true;
+  }
 
   protected render(): TemplateResult {
     return html`
