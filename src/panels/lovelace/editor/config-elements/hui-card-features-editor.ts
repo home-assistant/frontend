9d3aafe219aa50d0a2c2b0bbc8308c6a79cc95fa
karwosts <32912880+karwosts@users.noreply.github.com>
2026-03-11 05:59:01 -0700

    Light favorite color card feature (#29995)
    * Light favorite color card feature
    
    * rewrite resizeObserver
    
    * code review
    
    * Apply suggestions from code review
    
    Co-authored-by: Petar Petrov <MindFreeze@users.noreply.github.com>
    
    * Apply suggestion
    
    Co-authored-by: Petar Petrov <MindFreeze@users.noreply.github.com>
    
    * Apply suggestion from @MindFreeze
    
    ---------
    
    Co-authored-by: Petar Petrov <MindFreeze@users.noreply.github.com>

--- src/panels/lovelace/editor/config-elements/hui-card-features-editor.ts
+++ src/panels/lovelace/editor/config-elements/hui-card-features-editor.ts
@@ -71,6 +71,7 @@ import type {
   LovelaceCardFeatureContext,
 } from "../../card-features/types";
 import { getCardFeatureElementClass } from "../../create-element/create-card-feature-element";
+import { supportsLightColorFavoritesCardFeature } from "../../card-features/hui-light-color-favorites-card-feature";
 
 export type FeatureType = LovelaceCardFeatureConfig["type"];
 
@@ -106,6 +107,7 @@ const UI_FEATURE_TYPES = [
   "lawn-mower-commands",
   "light-brightness",
   "light-color-temp",
+  "light-color-favorites",
   "lock-commands",
   "lock-open-door",
   "media-player-playback",
@@ -182,6 +184,7 @@ const SUPPORTS_FEATURE_TYPES: Record<
   "lawn-mower-commands": supportsLawnMowerCommandCardFeature,
   "light-brightness": supportsLightBrightnessCardFeature,
   "light-color-temp": supportsLightColorTempCardFeature,
+  "light-color-favorites": supportsLightColorFavoritesCardFeature,
   "lock-commands": supportsLockCommandsCardFeature,
   "lock-open-door": supportsLockOpenDoorCardFeature,
   "media-player-playback": supportsMediaPlayerPlaybackCardFeature,
