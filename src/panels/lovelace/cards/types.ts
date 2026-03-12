7591e0da49603cbb7251c4bc28dba49b97ee42d6
Tom Carpenter <t.carpenter@leeds.ac.uk>
2026-03-12 11:52:49 +0000

    Fix value lost in dev merge

--- src/panels/lovelace/cards/types.ts
+++ src/panels/lovelace/cards/types.ts
@@ -178,7 +178,7 @@ export interface EnergyCardSankeyConfig extends EnergyCardBaseConfig {
 
 export interface EnergyDateSelectorCardConfig extends EnergyCardBaseConfig {
   vertical_opening_direction?: "auto" | "up" | "down";
-  opening_direction?: "auto" | "right" | "left" | "center";
+  opening_direction?: "auto" | "right" | "left" | "center" | "inline";
   disable_compare?: boolean;
 }
 
