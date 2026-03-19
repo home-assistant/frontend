70cc4878c659741cccec1f624576ce83d41f667f
Petar Petrov <MindFreeze@users.noreply.github.com>
2026-03-19 12:13:18 +0200

    Use --ha-box-shadow-l for footer

--- src/panels/lovelace/views/hui-view-footer.ts
+++ src/panels/lovelace/views/hui-view-footer.ts
@@ -236,11 +236,7 @@ export class HuiViewFooter extends LitElement {
     }
 
     .wrapper:not(.edit-mode) {
-      --ha-card-box-shadow:
-        0px 3px 5px -1px rgba(0, 0, 0, 0.2),
-        0px 6px 10px 0px rgba(0, 0, 0, 0.14),
-        0px 1px 18px 0px rgba(0, 0, 0, 0.12);
-      --ha-card-border-color: var(--divider-color);
+      --ha-card-box-shadow: var(--ha-box-shadow-l);
     }
 
     .container {
