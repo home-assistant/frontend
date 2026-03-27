8c69f8526056f3e7e665cc732bd9da7f5ddaa7d4
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2026-03-27 21:24:58 +0300

    revert

--- test/common/color/compute-color.test.ts
+++ test/common/color/compute-color.test.ts
@@ -1,10 +1,12 @@
 import { describe, it, expect } from "vitest";
-import { computeCssColor } from "../../../src/common/color/compute-color";
-import { DEFAULT_THEME_COLORS } from "../../../src/resources/theme/color/color.globals";
+import {
+  computeCssColor,
+  THEME_COLORS,
+} from "../../../src/common/color/compute-color";
 
 describe("computeCssColor", () => {
   it("should return CSS variable for theme colors", () => {
-    Object.keys(DEFAULT_THEME_COLORS).forEach((color) => {
+    THEME_COLORS.forEach((color) => {
       expect(computeCssColor(color)).toBe(`var(--${color}-color)`);
     });
   });
