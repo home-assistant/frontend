b9cab1f3b6fa37a6c99712b5d7e7aeb953d88bf4
Aidan Timson <aidan@timmo.dev>
2026-03-27 12:21:15 +0000

    Add docstring

--- src/common/feature-detect/support-popover.ts
+++ src/common/feature-detect/support-popover.ts
@@ -1,3 +1,8 @@
+/**
+ * Indicates whether the current browser supports the Popover API.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/Popover_API
+ */
 export const popoverSupported = globalThis?.HTMLElement?.prototype
   ? Object.prototype.hasOwnProperty.call(
       globalThis.HTMLElement.prototype,
