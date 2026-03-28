2f911c380624f2ef0a1aed3bf5091af285a4b3fa
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2026-03-28 16:34:40 +0300

    prettier

--- src/common/entity/get_states.ts
+++ src/common/entity/get_states.ts
@@ -270,16 +270,17 @@ export const getStatesDomain = (
       if (!attribute) {
         result.push(
           ...Object.entries(hass.states)
-          .filter(
-            ([entityId, stateObj]) =>
-              computeDomain(entityId) === "zone" &&
-              entityId !== "zone.home" &&
-              stateObj.attributes.friendly_name
-          )
-          .map(([_entityId, stateObj]) => stateObj.attributes.friendly_name!)
-          .sort((zone1, zone2) =>
-            stringCompare(zone1, zone2, hass.locale.language)
-          ));
+            .filter(
+              ([entityId, stateObj]) =>
+                computeDomain(entityId) === "zone" &&
+                entityId !== "zone.home" &&
+                stateObj.attributes.friendly_name
+            )
+            .map(([_entityId, stateObj]) => stateObj.attributes.friendly_name!)
+            .sort((zone1, zone2) =>
+              stringCompare(zone1, zone2, hass.locale.language)
+            )
+        );
       }
       break;
   }
