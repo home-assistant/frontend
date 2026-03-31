9a62a9217ce9109ca058c6ff2dabfb984ec651ce
Wendelin <12148533+wendevlin@users.noreply.github.com>
2026-03-31 13:36:56 +0200

    Fix automation add TCA dialog sometimes not opening (#51306)

--- src/panels/config/automation/add-automation-element-dialog.ts
+++ src/panels/config/automation/add-automation-element-dialog.ts
@@ -287,6 +287,7 @@ class DialogAddAutomationElement
 
   public showDialog(params): void {
     this._params = params;
+    this._resetVariables();
 
     this.addKeyboardShortcuts();
 
@@ -376,9 +377,15 @@ class DialogAddAutomationElement
     if (this._params) {
       fireEvent(this, "dialog-closed", { dialog: this.localName });
     }
+    this._params = undefined;
+    this._resetVariables();
+
+    return true;
+  }
+
+  private _resetVariables() {
     this._open = true;
     this._closing = false;
-    this._params = undefined;
     this._selectedCollectionIndex = undefined;
     this._selectedGroup = undefined;
     this._selectedTarget = undefined;
@@ -390,7 +397,6 @@ class DialogAddAutomationElement
     this._narrow = false;
     this._targetItems = undefined;
     this._loadItemsError = false;
-    return true;
   }
 
   private _updateNarrow = () => {
