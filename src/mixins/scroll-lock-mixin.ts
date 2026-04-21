d4eec8b420b8dfa76b5c8d5a0cb641f20ffe2c9b
Aidan Timson <aidan@timmo.dev>
2026-04-21 11:13:33 +0100

    Preserve scrollbar gutter if overflow is present

--- src/mixins/scroll-lock-mixin.ts
+++ src/mixins/scroll-lock-mixin.ts
@@ -1,15 +1,24 @@
 import type { ReactiveElement } from "lit";
 import type { Constructor } from "../types";
 
-let bodyScrollLockCount = 0;
-
-let previousBodyOverflow = "";
-
 export const ScrollLockMixin = <T extends Constructor<ReactiveElement>>(
   superClass: T
 ) => {
   class ScrollLockClass extends superClass {
+    private static _bodyScrollLockCount = 0;
+
+    private static _previousBodyOverflow = "";
+
+    private static _previousScrollbarGutter = "";
+
+    private static _reserveScrollbarGutter = false;
+
     private _bodyScrollLocked = false;
+
+    public disconnectedCallback(): void {
+      this.unlockBodyScroll();
+      super.disconnectedCallback();
+    }
 
     protected lockBodyScroll() {
       if (this._bodyScrollLocked) {
@@ -18,12 +27,23 @@ export const ScrollLockMixin = <T extends Constructor<ReactiveElement>>(
 
       this._bodyScrollLocked = true;
 
-      if (bodyScrollLockCount === 0) {
-        previousBodyOverflow = document.body.style.overflow;
+      if (ScrollLockClass._bodyScrollLockCount === 0) {
+        ScrollLockClass._previousBodyOverflow = document.body.style.overflow;
+        ScrollLockClass._previousScrollbarGutter =
+          document.documentElement.style.getPropertyValue("scrollbar-gutter");
+        ScrollLockClass._reserveScrollbarGutter =
+          window.innerWidth > document.documentElement.clientWidth;
       }
 
-      bodyScrollLockCount += 1;
+      ScrollLockClass._bodyScrollLockCount += 1;
       document.body.style.overflow = "hidden";
+
+      if (ScrollLockClass._reserveScrollbarGutter) {
+        document.documentElement.style.setProperty(
+          "scrollbar-gutter",
+          "stable"
+        );
+      }
     }
 
     protected unlockBodyScroll() {
@@ -32,16 +52,22 @@ export const ScrollLockMixin = <T extends Constructor<ReactiveElement>>(
       }
 
       this._bodyScrollLocked = false;
-      bodyScrollLockCount -= 1;
+      ScrollLockClass._bodyScrollLockCount -= 1;
 
-      if (bodyScrollLockCount === 0) {
-        document.body.style.overflow = previousBodyOverflow;
+      if (ScrollLockClass._bodyScrollLockCount === 0) {
+        document.body.style.overflow = ScrollLockClass._previousBodyOverflow;
+
+        if (ScrollLockClass._previousScrollbarGutter) {
+          document.documentElement.style.setProperty(
+            "scrollbar-gutter",
+            ScrollLockClass._previousScrollbarGutter
+          );
+        } else {
+          document.documentElement.style.removeProperty("scrollbar-gutter");
+        }
+
+        ScrollLockClass._reserveScrollbarGutter = false;
       }
-    }
-
-    public disconnectedCallback(): void {
-      this.unlockBodyScroll();
-      super.disconnectedCallback();
     }
   }
 
