cce3254fb56a00066669465289e50d6afe3ae925
Aidan Timson <aidan@timmo.dev>
2026-05-05 16:40:27 +0100

    Condition action and navigation actions

--- src/dialogs/more-info/ha-more-info-add-to.ts
+++ src/dialogs/more-info/ha-more-info-add-to.ts
@@ -118,7 +118,7 @@ export class HaMoreInfoAddTo extends LitElement {
       return;
     }
 
-    defaultActionHandler(key);
+    defaultActionHandler(key, this.entityId);
   }
 
   private _renderActionItems(actions: EntityAddToActions) {
