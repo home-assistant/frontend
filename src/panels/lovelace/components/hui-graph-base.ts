3f39998c4e485e81d4a2ee440f64afba0ab8f9de
Aidan Timson <aidan@timmo.dev>
2026-05-06 14:53:47 +0100

    Merge rendering with animation, remove opacity switch

--- src/panels/lovelace/components/hui-graph-base.ts
+++ src/panels/lovelace/components/hui-graph-base.ts
@@ -1,5 +1,5 @@
 import type { PropertyValues, TemplateResult } from "lit";
-import { css, html, LitElement, svg } from "lit";
+import { css, html, LitElement, nothing, svg } from "lit";
 import { customElement, property, state } from "lit/decorators";
 import { strokeWidth } from "../../../data/graph";
 import { getPath } from "../common/graph/get-path";
@@ -21,73 +21,63 @@ export class HuiGraphBase extends LitElement {
     const width = this.clientWidth || 500;
     const height = this.clientHeight || width / 5;
     const yAxisOrigin = this.yAxisOrigin ?? height;
+    const path =
+      this._path ??
+      (this.loading ? `M 0,${height / 2} L ${width},${height / 2}` : undefined);
     const lastX = this.coordinates?.length
       ? this.coordinates[this.coordinates.length - 1][0]
       : width;
 
-    if (this.loading) {
-      const loadingPath =
-        this._path ?? `M 0,${height / 2} L ${width},${height / 2}`;
-      const loadingScanId = `${this._uniqueId}-loading-scan`;
-
+    if (!path) {
       return html`
-        ${svg`<svg width="100%" height="100%" viewBox="0 0 ${width} ${height}" preserveAspectRatio="none" aria-hidden="true">
-          <defs>
-            <linearGradient id=${loadingScanId} x1="-100%" x2="0%" y1="0" y2="0">
-              <stop offset="0%" stop-color="var(--accent-color)" stop-opacity="0.3" />
-              <stop offset="50%" stop-color="var(--accent-color)" stop-opacity="0.75" />
-              <stop offset="100%" stop-color="var(--accent-color)" stop-opacity="0.3" />
-              <animate attributeName="x1" values="-100%;100%" dur="1.8s" repeatCount="indefinite" />
-              <animate attributeName="x2" values="0%;200%" dur="1.8s" repeatCount="indefinite" />
-            </linearGradient>
-          </defs>
-          <path
-            class="fill"
-            fill="url(#${loadingScanId})"
-            d="${loadingPath} L ${lastX}, ${yAxisOrigin} L 0, ${yAxisOrigin} z"
-          />
-          <path
-            vector-effect="non-scaling-stroke"
-            class="line"
-            fill="none"
-            stroke="url(#${loadingScanId})"
-            stroke-width="${strokeWidth}"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d=${loadingPath}
-          ></path>
-        </svg>`}
+        ${svg`<svg width="100%" height="100%" viewBox="0 0 ${width} ${height}"></svg>`}
       `;
     }
 
+    const shimmerId = `${this._uniqueId}-shimmer`;
+    const fillRect = this.loading
+      ? `url(#${shimmerId})`
+      : "var(--accent-color)";
+
     return html`
-      ${this._path
-        ? svg`<svg width="100%" height="100%" viewBox="0 0 ${width} ${height}" preserveAspectRatio="none">
-          <g>
-            <mask id="${this._uniqueId}-fill">
-              <path
-                class='fill'
-                fill='white'
-                d="${this._path} L ${lastX}, ${yAxisOrigin} L 0, ${yAxisOrigin} z"
-              />
-            </mask>
-            <rect height="100%" width="100%" fill="var(--accent-color)" mask="url(#${this._uniqueId}-fill)"></rect>
-            <mask id="${this._uniqueId}-line">
-              <path
-                vector-effect="non-scaling-stroke"
-                class='line'
-                fill="none"
-                stroke="white"
-                stroke-width="${strokeWidth}"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d=${this._path}
-              ></path>
-            </mask>
-            <rect height="100%" width="100%" fill="var(--accent-color)" mask="url(#${this._uniqueId}-line)"></rect>
-          </g>
-        </svg>`
-        : svg`<svg width="100%" height="100%" viewBox="0 0 ${width} ${height}"></svg>`}
+      ${svg`<svg width="100%" height="100%" viewBox="0 0 ${width} ${height}" preserveAspectRatio="none">
+        ${
+          this.loading
+            ? svg`<defs>
+                <linearGradient id=${shimmerId} x1="-50%" x2="-30%" y1="0" y2="0">
+                  <stop offset="0%" stop-color="var(--accent-color)" />
+                  <stop offset="50%" stop-color="white" />
+                  <stop offset="100%" stop-color="var(--accent-color)" />
+                  <animate attributeName="x1" values="-50%;120%" dur="1.8s" repeatCount="indefinite" />
+                  <animate attributeName="x2" values="-30%;140%" dur="1.8s" repeatCount="indefinite" />
+                </linearGradient>
+              </defs>`
+            : nothing
+        }
+        <g>
+          <mask id="${this._uniqueId}-fill">
+            <path
+              class='fill'
+              fill='white'
+              d="${path} L ${lastX}, ${yAxisOrigin} L 0, ${yAxisOrigin} z"
+            />
+          </mask>
+          <rect height="100%" width="100%" fill=${fillRect} mask="url(#${this._uniqueId}-fill)"></rect>
+          <mask id="${this._uniqueId}-line">
+            <path
+              vector-effect="non-scaling-stroke"
+              class='line'
+              fill="none"
+              stroke="white"
+              stroke-width="${strokeWidth}"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d=${path}
+            ></path>
+          </mask>
+          <rect height="100%" width="100%" fill=${fillRect} mask="url(#${this._uniqueId}-line)"></rect>
+        </g>
+      </svg>`}
     `;
   }
 
@@ -112,12 +102,6 @@ export class HuiGraphBase extends LitElement {
     }
     .fill {
       opacity: 0.1;
-    }
-    :host([loading]) .fill {
-      opacity: 0.16;
-    }
-    :host([loading]) .line {
-      opacity: 0.48;
     }
   `;
 }
