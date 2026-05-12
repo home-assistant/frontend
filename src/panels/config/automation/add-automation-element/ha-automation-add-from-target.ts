93afaa80d01f332117b346316ef709a665ee9405
Wendelin <w@pe8.at>
2026-05-12 13:33:28 +0200

    Auto open single floor

--- src/panels/config/automation/add-automation-element/ha-automation-add-from-target.ts
+++ src/panels/config/automation/add-automation-element/ha-automation-add-from-target.ts
@@ -867,9 +867,12 @@ export default class HaAutomationAddFromTarget extends LitElement {
     );
 
     this._floorAreas.forEach((floor) => {
+      const filteredFloors = this._floorAreas.filter(
+        ({ id, areas }) => id !== undefined && areas.length
+      );
       this._entries[floor.id || `floor${TARGET_SEPARATOR}`] = {
         // auto expand if only one floor is present
-        open: this._floorAreas.length === 1,
+        open: filteredFloors.length === 1 && filteredFloors[0].id === floor.id,
         areas: {},
       };
 
