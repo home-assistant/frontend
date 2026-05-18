854e56947fe2c244e7280b28fadb6707fa82f5c1
Paulus Schoutsen <balloob@gmail.com>
2026-05-18 12:12:21 -0400

    Update IFRAME_SANDBOX to remove 'allow-same-origin'
    Removed 'allow-same-origin' from the IFRAME_SANDBOX settings.

--- src/util/iframe.ts
+++ src/util/iframe.ts
@@ -1,2 +1,2 @@
 export const IFRAME_SANDBOX =
-  "allow-forms allow-popups allow-pointer-lock allow-same-origin allow-scripts allow-modals allow-downloads";
+  "allow-forms allow-popups allow-pointer-lock allow-scripts allow-modals allow-downloads";
