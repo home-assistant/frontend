5fa8508383b3a9d58ba935e7e2e87393c8717b68
Petar Petrov <MindFreeze@users.noreply.github.com>
2026-05-23 16:15:13 +0300

    Update src/dialogs/config-flow/dialog-data-entry-flow.ts

--- src/dialogs/config-flow/dialog-data-entry-flow.ts
+++ src/dialogs/config-flow/dialog-data-entry-flow.ts
@@ -649,7 +649,7 @@ class DataEntryFlowDialog extends LitElement {
       } else if (_step.next_flow[0] === "repair_flow") {
         showRepairsFlowDialog(
           this,
-          (_step.result as unknown as RepairsIssue)!,
+          (_step.result as unknown as RepairsIssue),
           {
             continueFlowId: _step.next_flow[1],
             navigateToResult: this._params!.navigateToResult,
