882e5921848579dc29fcdc9edcfc682380172fd5
Paul Bottein <paul.bottein@gmail.com>
2026-06-04 15:00:33 +0200

    Add customize toggle to media player source and sound mode feature editors

--- src/panels/lovelace/editor/config-elements/hui-media-player-sound-mode-card-feature-editor.ts
+++ src/panels/lovelace/editor/config-elements/hui-media-player-sound-mode-card-feature-editor.ts
@@ -5,12 +5,17 @@ import { fireEvent } from "../../../../common/dom/fire_event";
 import "../../../../components/ha-form/ha-form";
 import type { SchemaUnion } from "../../../../components/ha-form/types";
 import type { MediaPlayerEntity } from "../../../../data/media-player";
-import type { HomeAssistant, ValueChangedEvent } from "../../../../types";
+import type { HomeAssistant } from "../../../../types";
 import type {
   LovelaceCardFeatureContext,
   MediaPlayerSoundModeCardFeatureConfig,
 } from "../../card-features/types";
 import type { LovelaceCardFeatureEditor } from "../../types";
+import {
+  customizableListData,
+  customizableListSchema,
+  processCustomizableListValue,
+} from "./customizable-list-feature";
 
 @customElement("hui-media-player-sound-mode-card-feature-editor")
 export class HuiMediaPlayerSoundModeCardFeatureEditor
@@ -28,28 +33,20 @@ export class HuiMediaPlayerSoundModeCardFeatureEditor
   }
 
   private _schema = memoizeOne(
-    (hass: HomeAssistant, stateObj?: MediaPlayerEntity) =>
-      [
-        {
-          name: "sound_modes",
-          selector: {
-            select: {
-              multiple: true,
-              mode: "list" as const,
-              reorder: true,
-              options:
-                stateObj?.attributes.sound_mode_list?.map((mode) => ({
-                  value: mode,
-                  label: hass.formatEntityAttributeValue(
-                    stateObj,
-                    "sound_mode",
-                    mode
-                  ),
-                })) ?? [],
-            },
-          },
-        },
-      ] as const
+    (stateObj: MediaPlayerEntity | undefined, customize: boolean) =>
+      customizableListSchema({
+        field: "sound_modes",
+        customize,
+        options:
+          stateObj?.attributes.sound_mode_list?.map((mode) => ({
+            value: mode,
+            label: this.hass!.formatEntityAttributeValue(
+              stateObj,
+              "sound_mode",
+              mode
+            ),
+          })) ?? [],
+      })
   );
 
   protected render() {
@@ -63,12 +60,13 @@ export class HuiMediaPlayerSoundModeCardFeatureEditor
           | undefined)
       : undefined;
 
-    const schema = this._schema(this.hass!, stateObj);
+    const data = customizableListData(this._config, "sound_modes");
+    const schema = this._schema(stateObj, data.customize);
 
     return html`
       <ha-form
         .hass=${this.hass}
-        .data=${this._config}
+        .data=${data}
         .schema=${schema}
         .computeLabel=${this._computeLabelCallback}
         @value-changed=${this._valueChanged}
@@ -76,24 +74,28 @@ export class HuiMediaPlayerSoundModeCardFeatureEditor
     `;
   }
 
-  private _valueChanged(
-    ev: ValueChangedEvent<MediaPlayerSoundModeCardFeatureConfig>
-  ): void {
-    fireEvent(this, "config-changed", { config: ev.detail.value });
+  private _valueChanged(ev: CustomEvent): void {
+    const stateObj = this.context?.entity_id
+      ? (this.hass!.states[this.context.entity_id] as
+          | MediaPlayerEntity
+          | undefined)
+      : undefined;
+    const defaults = stateObj?.attributes.sound_mode_list ?? [];
+    const config =
+      processCustomizableListValue<MediaPlayerSoundModeCardFeatureConfig>(
+        ev.detail.value,
+        "sound_modes",
+        defaults
+      );
+    fireEvent(this, "config-changed", { config });
   }
 
   private _computeLabelCallback = (
     schema: SchemaUnion<ReturnType<typeof this._schema>>
-  ) => {
-    switch (schema.name) {
-      case "sound_modes":
-        return this.hass?.localize(
-          `ui.panel.lovelace.editor.features.types.media-player-sound-mode.${schema.name}`
-        );
-      default:
-        return "";
-    }
-  };
+  ) =>
+    this.hass!.localize(
+      `ui.panel.lovelace.editor.features.types.media-player-sound-mode.${schema.name}`
+    );
 }
 
 declare global {
--- src/panels/lovelace/editor/config-elements/hui-media-player-source-card-feature-editor.ts
+++ src/panels/lovelace/editor/config-elements/hui-media-player-source-card-feature-editor.ts
@@ -5,12 +5,17 @@ import { fireEvent } from "../../../../common/dom/fire_event";
 import "../../../../components/ha-form/ha-form";
 import type { SchemaUnion } from "../../../../components/ha-form/types";
 import type { MediaPlayerEntity } from "../../../../data/media-player";
-import type { HomeAssistant, ValueChangedEvent } from "../../../../types";
+import type { HomeAssistant } from "../../../../types";
 import type {
   LovelaceCardFeatureContext,
   MediaPlayerSourceCardFeatureConfig,
 } from "../../card-features/types";
 import type { LovelaceCardFeatureEditor } from "../../types";
+import {
+  customizableListData,
+  customizableListSchema,
+  processCustomizableListValue,
+} from "./customizable-list-feature";
 
 @customElement("hui-media-player-source-card-feature-editor")
 export class HuiMediaPlayerSourceCardFeatureEditor
@@ -28,24 +33,20 @@ export class HuiMediaPlayerSourceCardFeatureEditor
   }
 
   private _schema = memoizeOne(
-    (stateObj?: MediaPlayerEntity) =>
-      [
-        {
-          name: "sources",
-          selector: {
-            select: {
-              multiple: true,
-              mode: "list" as const,
-              reorder: true,
-              options:
-                stateObj?.attributes.source_list?.map((source) => ({
-                  value: source,
-                  label: source,
-                })) ?? [],
-            },
-          },
-        },
-      ] as const
+    (stateObj: MediaPlayerEntity | undefined, customize: boolean) =>
+      customizableListSchema({
+        field: "sources",
+        customize,
+        options:
+          stateObj?.attributes.source_list?.map((source) => ({
+            value: source,
+            label: this.hass!.formatEntityAttributeValue(
+              stateObj,
+              "source",
+              source
+            ),
+          })) ?? [],
+      })
   );
 
   protected render() {
@@ -59,12 +60,13 @@ export class HuiMediaPlayerSourceCardFeatureEditor
           | undefined)
       : undefined;
 
-    const schema = this._schema(stateObj);
+    const data = customizableListData(this._config, "sources");
+    const schema = this._schema(stateObj, data.customize);
 
     return html`
       <ha-form
         .hass=${this.hass}
-        .data=${this._config}
+        .data=${data}
         .schema=${schema}
         .computeLabel=${this._computeLabelCallback}
         @value-changed=${this._valueChanged}
@@ -72,24 +74,28 @@ export class HuiMediaPlayerSourceCardFeatureEditor
     `;
   }
 
-  private _valueChanged(
-    ev: ValueChangedEvent<MediaPlayerSourceCardFeatureConfig>
-  ): void {
-    fireEvent(this, "config-changed", { config: ev.detail.value });
+  private _valueChanged(ev: CustomEvent): void {
+    const stateObj = this.context?.entity_id
+      ? (this.hass!.states[this.context.entity_id] as
+          | MediaPlayerEntity
+          | undefined)
+      : undefined;
+    const defaults = stateObj?.attributes.source_list ?? [];
+    const config =
+      processCustomizableListValue<MediaPlayerSourceCardFeatureConfig>(
+        ev.detail.value,
+        "sources",
+        defaults
+      );
+    fireEvent(this, "config-changed", { config });
   }
 
   private _computeLabelCallback = (
     schema: SchemaUnion<ReturnType<typeof this._schema>>
-  ) => {
-    switch (schema.name) {
-      case "sources":
-        return this.hass?.localize(
-          `ui.panel.lovelace.editor.features.types.media-player-source.${schema.name}`
-        );
-      default:
-        return "";
-    }
-  };
+  ) =>
+    this.hass!.localize(
+      `ui.panel.lovelace.editor.features.types.media-player-source.${schema.name}`
+    );
 }
 
 declare global {
