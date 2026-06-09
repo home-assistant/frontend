f0f28789de626ab4307d766448fda0c113aa04da
Jan-Philipp Benecke <jan-philipp@bnck.me>
2026-06-09 13:21:40 +0200

    Fix scrolling behavior for auto-height data table (#52508)

--- src/components/data-table/ha-data-table.ts
+++ src/components/data-table/ha-data-table.ts
@@ -1477,6 +1477,11 @@ export class HaDataTable extends LitElement {
         .mdc-data-table__table.auto-height .scroller {
           overflow-y: hidden !important;
         }
+
+        .mdc-data-table__table.auto-height lit-virtualizer {
+          overscroll-behavior-y: auto;
+        }
+
         .grows {
           flex-grow: 1;
           flex-shrink: 1;
