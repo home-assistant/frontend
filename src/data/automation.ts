d64845f206ceacf6543f19a7b2b6649ea05e1607
Franck Nijhof <git@frenck.dev>
2026-06-19 08:36:05 +0200

    Support a list of entities in the zone trigger editor (#52738)

--- src/data/automation.ts
+++ src/data/automation.ts
@@ -180,7 +180,7 @@ export interface PersistentNotificationTrigger extends BaseTrigger {
 
 export interface ZoneTrigger extends BaseTrigger {
   trigger: "zone";
-  entity_id: string;
+  entity_id: string | string[];
   zone: string;
   event: "enter" | "leave";
 }
