93f9449a0f9b508393c8d19482dc9a51f6ad0ca6
karwosts <karwosts@gmail.com>
2026-06-27 15:32:01 +0000

    unused imports

--- src/components/ha-entity-id-icon.ts
+++ src/components/ha-entity-id-icon.ts
@@ -1,8 +1,6 @@
 import type { HassEntity } from "home-assistant-js-websocket";
 import { html, LitElement } from "lit";
 import { customElement, property, state } from "lit/decorators";
-import "./ha-icon";
-import "./ha-svg-icon";
 import { consumeEntityState } from "../common/decorators/consume-context-entry";
 import { computeDomain } from "../common/entity/compute_domain";
 import "./ha-domain-icon";
