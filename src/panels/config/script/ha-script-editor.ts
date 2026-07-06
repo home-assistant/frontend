b0e8d45003a08788e77948ded607931f312057e6
Simon Lamon <32477463+silamon@users.noreply.github.com>
2026-07-06 09:33:56 +0200

    Add entity update tracking again in Automation and Script editor (#53002)

--- src/panels/config/script/ha-script-editor.ts
+++ src/panels/config/script/ha-script-editor.ts
@@ -594,15 +594,30 @@ export class HaScriptEditor extends SubscribeMixin(
         ...baseConfig,
         ...initData,
       } as ScriptConfig;
-      this._initDirtyTracking({ type: "deep" }, baseConfig as ScriptConfig);
-      this._updateDirtyState(this.config);
+      this._initDirtyTracking(
+        { type: "deep" },
+        {
+          config: baseConfig as ScriptConfig,
+          entityRegistryUpdate: this.entityRegistryUpdate,
+        }
+      );
+      this._updateDirtyState({
+        config: this.config,
+        entityRegistryUpdate: this.entityRegistryUpdate,
+      });
       this.readOnly = false;
     }
 
     if (changedProps.has("entityId") && this.entityId) {
       getScriptStateConfig(this.hass, this.entityId).then((c) => {
         this.config = normalizeScriptConfig(c.config);
-        this._initDirtyTracking({ type: "deep" }, this.config);
+        this._initDirtyTracking(
+          { type: "deep" },
+          {
+            config: this.config,
+            entityRegistryUpdate: this.entityRegistryUpdate,
+          }
+        );
         this._checkValidation();
       });
       const regEntry = this.entityRegistry?.find(
@@ -647,7 +662,10 @@ export class HaScriptEditor extends SubscribeMixin(
 
     this.config = ev.detail.value;
     this.errors = undefined;
-    this._updateDirtyState(this.config!);
+    this._updateDirtyState({
+      config: this.config!,
+      entityRegistryUpdate: this.entityRegistryUpdate,
+    });
   }
 
   private async _runScript() {
@@ -734,7 +752,10 @@ export class HaScriptEditor extends SubscribeMixin(
     }
 
     this._manualEditor?.addFields();
-    this._updateDirtyState(this.config!);
+    this._updateDirtyState({
+      config: this.config!,
+      entityRegistryUpdate: this.entityRegistryUpdate,
+    });
   }
 
   private _preprocessYaml() {
@@ -749,7 +770,10 @@ export class HaScriptEditor extends SubscribeMixin(
     }
     this.yamlErrors = undefined;
     this.config = ev.detail.value;
-    this._updateDirtyState(this.config!);
+    this._updateDirtyState({
+      config: this.config!,
+      entityRegistryUpdate: this.entityRegistryUpdate,
+    });
     this.errors = undefined;
   }
 
@@ -765,7 +789,10 @@ export class HaScriptEditor extends SubscribeMixin(
         updateConfig: async (config, entityRegistryUpdate) => {
           this.config = config;
           this.entityRegistryUpdate = entityRegistryUpdate;
-          this._updateDirtyState(this.config);
+          this._updateDirtyState({
+            config: this.config,
+            entityRegistryUpdate: this.entityRegistryUpdate,
+          });
           this.requestUpdate();
 
           const id = this.scriptId || String(Date.now());
@@ -880,7 +907,10 @@ export class HaScriptEditor extends SubscribeMixin(
         updateConfig: async (config, entityRegistryUpdate) => {
           this.config = config;
           this.entityRegistryUpdate = entityRegistryUpdate;
-          this._updateDirtyState(this.config);
+          this._updateDirtyState({
+            config: this.config,
+            entityRegistryUpdate: this.entityRegistryUpdate,
+          });
           this.requestUpdate();
           resolve(true);
         },
@@ -899,7 +929,10 @@ export class HaScriptEditor extends SubscribeMixin(
         config: this.config!,
         updateConfig: (config) => {
           this.config = config;
-          this._updateDirtyState(config);
+          this._updateDirtyState({
+            config,
+            entityRegistryUpdate: this.entityRegistryUpdate,
+          });
           this.requestUpdate();
           resolve();
         },
@@ -1048,7 +1081,10 @@ export class HaScriptEditor extends SubscribeMixin(
   private _applyUndoRedo(config: ScriptConfig) {
     this._manualEditor?.triggerCloseSidebar();
     this.config = config;
-    this._updateDirtyState(this.config);
+    this._updateDirtyState({
+      config: this.config,
+      entityRegistryUpdate: this.entityRegistryUpdate,
+    });
   }
 
   private _undo() {
