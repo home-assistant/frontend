57f01133724c7ee40ee9d293da7ffcdc7816a4ef
karwosts <karwosts@gmail.com>
2026-07-21 15:46:45 +0000

    Add button/valve to canToggleDomain

--- src/common/entity/can_toggle_domain.ts
+++ src/common/entity/can_toggle_domain.ts
@@ -6,11 +6,17 @@ export const canToggleDomain = (hass: HomeAssistant, domain: string) => {
     return false;
   }
 
+  if (domain === "button" || domain === "input_button") {
+    return "press" in services;
+  }
   if (domain === "lock") {
     return "lock" in services;
   }
   if (domain === "cover") {
     return "open_cover" in services;
+  }
+  if (domain === "valve") {
+    return "open_valve" in services;
   }
   return "turn_on" in services;
 };
