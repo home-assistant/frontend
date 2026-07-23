1f14f8689c62729c6d67d08a28d2ed5990f41083
karwosts <32912880+karwosts@users.noreply.github.com>
2026-07-23 14:09:03 +0800

    Cleanup some toggle behavior (#53242)
    * Cleanup some toggle behavior
    
    * fix tests
    
    * prior behavior did not consider off
    
    * Revert "prior behavior did not consider off"
    
    This reverts commit 59cc93d308abc042e13cc6208975a174936caf46.
    
    * Attempt rearranging features to address bundle growth failure
    
    * Revert a no-longer-needed change to setup.ts
    
    * Add test
    
    * test
    
    * Apply suggestion from @MindFreeze
    
    ---------
    
    Co-authored-by: Petar Petrov <MindFreeze@users.noreply.github.com>

--- src/panels/lovelace/common/entity/turn-on-off-entity.ts
+++ src/panels/lovelace/common/entity/turn-on-off-entity.ts
@@ -1,4 +1,5 @@
 import { computeDomain } from "../../../../common/entity/compute_domain";
+import { getToggleAction } from "../../../../common/entity/get_toggle_action";
 import type { HomeAssistant, ServiceCallResponse } from "../../../../types";
 
 export const turnOnOffEntity = (
@@ -9,27 +10,7 @@ export const turnOnOffEntity = (
   const stateDomain = computeDomain(entityId);
   const serviceDomain = stateDomain === "group" ? "homeassistant" : stateDomain;
 
-  let service;
-  switch (stateDomain) {
-    case "lock":
-      service = turnOn ? "unlock" : "lock";
-      break;
-    case "cover":
-      service = turnOn ? "open_cover" : "close_cover";
-      break;
-    case "button":
-    case "input_button":
-      service = "press";
-      break;
-    case "scene":
-      service = "turn_on";
-      break;
-    case "valve":
-      service = turnOn ? "open_valve" : "close_valve";
-      break;
-    default:
-      service = turnOn ? "turn_on" : "turn_off";
-  }
+  const service = getToggleAction(stateDomain, turnOn);
 
   return hass.callService(serviceDomain, service, { entity_id: entityId });
 };
