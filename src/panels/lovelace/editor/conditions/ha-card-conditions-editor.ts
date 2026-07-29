290736f641899e8ab78fcfe0c8be9bdad911782d
Aidan Timson <aidan@timmo.dev>
2026-07-29 15:40:22 +0100

    Format

--- src/panels/lovelace/editor/conditions/ha-card-conditions-editor.ts
+++ src/panels/lovelace/editor/conditions/ha-card-conditions-editor.ts
@@ -172,9 +172,9 @@ export class HaCardConditionsEditor extends LitElement {
               >
                 <div
                   slot="drag-handle"
-                  class="handle ${
-                    this._rowSortSelected === idx ? "active" : ""
-                  }"
+                  class="handle ${this._rowSortSelected === idx
+                    ? "active"
+                    : ""}"
                   role="button"
                   tabindex="0"
                   aria-label=${this.hass.localize("ui.common.move")}
@@ -195,29 +195,25 @@ export class HaCardConditionsEditor extends LitElement {
                   "ui.panel.lovelace.editor.condition-editor.add"
                 )}
               </ha-button>
-              ${
-                this._canPaste
-                  ? html`
-                      <ha-dropdown-item value="paste">
-                        ${this.hass.localize(
-                          "ui.panel.lovelace.editor.edit_card.paste_condition"
-                        )}
-                        <ha-svg-icon
-                          slot="icon"
-                          .path=${mdiContentPaste}
-                        ></ha-svg-icon>
-                      </ha-dropdown-item>
-                    `
-                  : nothing
-              }
+              ${this._canPaste
+                ? html`
+                    <ha-dropdown-item value="paste">
+                      ${this.hass.localize(
+                        "ui.panel.lovelace.editor.edit_card.paste_condition"
+                      )}
+                      <ha-svg-icon
+                        slot="icon"
+                        .path=${mdiContentPaste}
+                      ></ha-svg-icon>
+                    </ha-dropdown-item>
+                  `
+                : nothing}
               ${this._availableConditions.map(
                 (condition) => html`
                   <ha-dropdown-item .value=${condition}>
-                    ${
-                      this.hass!.localize(
-                        `ui.panel.lovelace.editor.condition-editor.condition.${condition}.label`
-                      ) || condition
-                    }
+                    ${this.hass!.localize(
+                      `ui.panel.lovelace.editor.condition-editor.condition.${condition}.label`
+                    ) || condition}
                     <ha-svg-icon
                       slot="icon"
                       .path=${ICON_CONDITION[condition]}
