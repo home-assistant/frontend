f89b3c23c71127e8fe3c7fc2e32c02888c19e48c
Petar Petrov <MindFreeze@users.noreply.github.com>
2026-08-06 17:02:41 +0300

    Keep selected option visible in disabled select box (#53535)

--- src/components/ha-selector/ha-selector-select.ts
+++ src/components/ha-selector/ha-selector-select.ts
@@ -104,6 +104,7 @@ export class HaSelectSelector extends LitElement {
         <ha-select-box
           .options=${options}
           .value=${this.value as string | undefined}
+          .disabled=${this.disabled}
           @value-changed=${this._selectChanged}
           .maxColumns=${this.selector.select?.box_max_columns}
         ></ha-select-box>
