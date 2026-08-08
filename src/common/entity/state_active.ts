9c983b5d99832c1db0d18b860f89d969ce21c1c3
Aura Herrero Ruiz <aura.herreroruiz@outlook.es>
2026-08-08 16:14:44 +0000

    Use list of active states for camera domain

--- src/common/entity/state_active.ts
+++ src/common/entity/state_active.ts
@@ -52,7 +52,7 @@ export function stateActive(stateObj: HassEntity, state?: string): boolean {
     case "timer":
       return compareState === "active";
     case "camera":
-      return compareState !== "idle";
+      return ["streaming", "recording"].includes(compareState);
   }
 
   return true;
