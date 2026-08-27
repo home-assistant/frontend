c37fa57e12faf74b5ce6934320c743ffc3d3f65f
Paul Bottein <paul.bottein@gmail.com>
2026-08-27 16:06:53 +0200

    Fix weather forecast tabs in the more info dialog

--- src/dialogs/more-info/context.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from "@lit/context";
-
-export interface MoreInfoContext {
-  hash: URLSearchParams;
-  setHashParam: (key: string, value?: string) => void;
-}
-
-export const moreInfoContext =
-  createContext<MoreInfoContext>("more-info-context");
