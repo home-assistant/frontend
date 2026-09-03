3d83cce81559b69609d0ca8197e657ade56f5e2b
Aidan Timson <aidan@timmo.dev>
2026-09-03 15:29:35 +0100

    Format conditions editor

--- src/panels/lovelace/editor/conditions/ha-card-conditions-editor.ts
+++ src/panels/lovelace/editor/conditions/ha-card-conditions-editor.ts
@@ -160,9 +160,9 @@ export class HaCardConditionsEditor extends LitElement {
               >
                 <div
                   slot="drag-handle"
-                  class="handle ${this._rowSortSelected === idx
-                    ? "active"
-                    : ""}"
+                  class="handle ${
+                    this._rowSortSelected === idx ? "active" : ""
+                  }"
                   role="button"
                   tabindex="0"
                   aria-label=${this.hass.localize("ui.common.move")}
@@ -184,25 +184,29 @@ export class HaCardConditionsEditor extends LitElement {
                   "ui.panel.lovelace.editor.condition-editor.add"
                 )}
               </ha-button>
-              ${this._clipboard
-                ? html`
-                    <ha-dropdown-item value="paste">
-                      ${this.hass.localize(
+              ${
+                this._clipboard
+                  ? html`
+                      <ha-dropdown-item value="paste">
+                        ${this.hass.localize(
                         "ui.panel.lovelace.editor.edit_card.paste_condition"
                       )}
-                      <ha-svg-icon
-                        slot="icon"
-                        .path=${mdiContentPaste}
-                      ></ha-svg-icon>
-                    </ha-dropdown-item>
-                  `
-                : nothing}
+                        <ha-svg-icon
+                          slot="icon"
+                          .path=${mdiContentPaste}
+                        ></ha-svg-icon>
+                      </ha-dropdown-item>
+                    `
+                  : nothing
+              }
               ${UI_CONDITION.map(
                 (condition) => html`
                   <ha-dropdown-item .value=${condition}>
-                    ${this.hass!.localize(
-                      `ui.panel.lovelace.editor.condition-editor.condition.${condition}.label`
-                    ) || condition}
+                    ${
+                      this.hass!.localize(
+                        `ui.panel.lovelace.editor.condition-editor.condition.${condition}.label`
+                      ) || condition
+                    }
                     <ha-svg-icon
                       slot="icon"
                       .path=${ICON_CONDITION[condition]}
@@ -211,7 +215,7 @@ export class HaCardConditionsEditor extends LitElement {
                 `
               )}
             </ha-dropdown>
-        </div>
+          </div>
         </div>
       </ha-sortable>
     `;
