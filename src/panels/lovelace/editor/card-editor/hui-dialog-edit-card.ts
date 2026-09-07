d4d81722cd7de60e2d576f2d222bc25299da5b90
Aidan Timson <aidan@timmo.dev>
2026-09-07 08:51:58 +0100

    Fix drag border padding

--- src/panels/lovelace/editor/card-editor/hui-dialog-edit-card.ts
+++ src/panels/lovelace/editor/card-editor/hui-dialog-edit-card.ts
@@ -492,8 +492,8 @@ export class HuiDialogEditCard
           max-width: var(--ha-view-sections-column-max-width, 500px);
         }
         .content .element-editor {
+          padding-inline-start: var(--ha-space-1);
           padding-inline-end: var(--ha-space-2);
-          margin-inline-start: var(--ha-space-1);
           margin-bottom: 0;
         }
 
