c9887b55675aaae2eecc037f339aa671a68637a8
Aidan Timson <aidan@timmo.dev>
2026-09-08 10:47:28 +0100

    Stub read-only jsdom globals in Vitest tests

--- test/hassio/create_session.test.ts
+++ test/hassio/create_session.test.ts
@@ -1,4 +1,4 @@
-import { assert, describe, it } from "vitest";
+import { afterEach, assert, describe, it, vi } from "vitest";
 import { createHassioSession } from "../../src/data/hassio/ingress";
 import type { HomeAssistant } from "../../src/types";
 
@@ -9,35 +9,27 @@ describe("Create hassio session", () => {
     }),
   } as unknown as Pick<HomeAssistant, "callWS">;
 
+  afterEach(() => {
+    vi.unstubAllGlobals();
+  });
+
   it("Test create session without HTTPS", async () => {
-    // @ts-ignore
-    global.document = {};
-    // @ts-ignore
-    global.location = {};
+    vi.stubGlobal("document", {});
+    vi.stubGlobal("location", { protocol: "http:" });
     await createHassioSession(hass);
     assert.strictEqual(
-      // @ts-ignore
       global.document.cookie,
       "ingress_session=fhdsu73rh3io4h8f3irhjel8ousafehf8f3yh;path=/api/hassio_ingress/;SameSite=Strict"
     );
   });
   it("Test create session with HTTPS", async () => {
-    // @ts-ignore
-    global.document = {};
-    // @ts-ignore
-    global.location = { protocol: "https:" };
+    vi.stubGlobal("document", {});
+    vi.stubGlobal("location", { protocol: "https:" });
     await createHassioSession(hass);
     assert.strictEqual(
-      // @ts-ignore
       global.document.cookie,
       "ingress_session=fhdsu73rh3io4h8f3irhjel8ousafehf8f3yh;path=/api/hassio_ingress/;SameSite=Strict;Secure"
     );
-
-    // Clean up in case they will be used in other tests
-    // @ts-ignore
-    global.document = {};
-    // @ts-ignore
-    global.location = {};
   });
   it("Test fail to create", async () => {
     const createSessionPromise = createHassioSession({
