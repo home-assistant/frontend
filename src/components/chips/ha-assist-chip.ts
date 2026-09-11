434b2a1db2b5d8edfebf4d7ba374d67fa5ea6474
Aidan Timson <aidan@timmo.dev>
2026-09-11 14:56:56 +0100

    Migrate chips from Material to Web Awesome

--- src/components/chips/ha-assist-chip.ts
+++ src/components/chips/ha-assist-chip.ts
@@ -1,100 +1,69 @@
-import { AssistChip } from "@material/web/chips/internal/assist-chip";
-import { styles } from "@material/web/chips/internal/assist-styles.cssresult.js";
-
-import { styles as sharedStyles } from "@material/web/chips/internal/shared-styles.cssresult.js";
-import { styles as elevatedStyles } from "@material/web/chips/internal/elevated-styles.cssresult.js";
-
-import { css, html } from "lit";
+import type { CSSResultGroup } from "lit";
+import { css } from "lit";
 import { customElement, property } from "lit/decorators";
+import { HaChipBase } from "./ha-chip-base";
 
 @customElement("ha-assist-chip")
-// @ts-ignore
-export class HaAssistChip extends AssistChip {
+export class HaAssistChip extends HaChipBase {
   @property({ type: Boolean, reflect: true }) filled = false;
 
-  @property({ type: Boolean }) active = false;
+  @property({ type: Boolean, reflect: true }) active = false;
 
-  static override styles = [
-    sharedStyles,
-    elevatedStyles,
-    styles,
-    css`
-      :host {
-        --md-sys-color-primary: var(--primary-text-color);
-        --md-sys-color-on-surface: var(--primary-text-color);
-        --md-assist-chip-container-shape: var(
-          --ha-assist-chip-container-shape,
-          16px
-        );
-        --md-assist-chip-outline-color: var(--outline-color);
-        --md-assist-chip-label-text-weight: 400;
-      }
-      /** Material 3 doesn't have a filled chip, so we have to make our own **/
-      .filled {
-        display: flex;
-        pointer-events: none;
-        border-radius: inherit;
-        inset: 0;
-        position: absolute;
-        background-color: var(--ha-assist-chip-filled-container-color);
-      }
-      /** Set the size of mdc icons **/
-      ::slotted([slot="icon"]),
-      ::slotted([slot="trailing-icon"]) {
-        display: flex;
-        --mdc-icon-size: var(--md-input-chip-icon-size, 18px);
-        font-size: var(--_label-text-size) !important;
-      }
+  @property({ type: Boolean, reflect: true }) elevated = false;
 
-      .trailing.icon ::slotted(*),
-      .trailing.icon svg {
-        margin-inline-end: unset;
-        margin-inline-start: var(--_icon-label-space);
-      }
-      ::before {
-        background: var(--ha-assist-chip-container-color, transparent);
-        opacity: var(--ha-assist-chip-container-opacity, 1);
-      }
-      :where(.active)::before {
-        background: var(--ha-assist-chip-active-container-color);
-        opacity: var(--ha-assist-chip-active-container-opacity);
-      }
-      .label {
-        font-family: var(--ha-font-family-body);
-      }
-    `,
-  ];
-
-  protected override renderOutline() {
-    if (this.filled) {
-      return html`<span class="filled"></span>`;
-    }
-
-    return super.renderOutline();
-  }
-
-  protected override getContainerClasses() {
-    return {
-      ...super.getContainerClasses(),
-      active: this.active,
-    };
-  }
-
-  protected override renderPrimaryContent() {
-    return html`
-      <span class="leading icon" aria-hidden="true">
-        ${this.renderLeadingIcon()}
-      </span>
-      <span class="label">${this.label}</span>
-      <span class="touch"></span>
-      <span class="trailing leading icon" aria-hidden="true">
-        ${this.renderTrailingIcon()}
-      </span>
-    `;
-  }
-
-  protected renderTrailingIcon() {
-    return html`<slot name="trailing-icon"></slot>`;
+  static override get styles(): CSSResultGroup {
+    return [
+      super.styles,
+      css`
+        :host {
+          --ha-chip-label-color: var(
+            --md-assist-chip-label-text-color,
+            var(--md-sys-color-on-surface, var(--primary-text-color))
+          );
+          --ha-button-border-radius: var(
+            --ha-assist-chip-container-shape,
+            16px
+          );
+          --ha-chip-container-color: var(
+            --ha-assist-chip-container-color,
+            transparent
+          );
+          --ha-chip-container-opacity: var(
+            --ha-assist-chip-container-opacity,
+            1
+          );
+          --ha-chip-outline-color: var(
+            --md-assist-chip-outline-color,
+            var(--outline-color)
+          );
+          --ha-chip-icon-label-space: var(
+            --md-assist-chip-icon-label-space,
+            var(--ha-space-2)
+          );
+        }
+        :host([filled]) {
+          --ha-chip-container-color: var(
+            --ha-assist-chip-filled-container-color
+          );
+          --ha-chip-outline-color: transparent;
+        }
+        :host([active]) {
+          --ha-chip-container-color: var(
+            --ha-assist-chip-active-container-color
+          );
+          --ha-chip-container-opacity: var(
+            --ha-assist-chip-active-container-opacity,
+            1
+          );
+        }
+        .primary {
+          padding-inline-end: var(
+            --md-assist-chip-trailing-space,
+            var(--ha-space-4)
+          );
+        }
+      `,
+    ];
   }
 }
 
