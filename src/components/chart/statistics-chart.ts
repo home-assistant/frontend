7d421b8fa09ddeb0055e2bc1a9f0c7e64e6ba8d5
Yosi Levy <yosilevy@gmail.com>
2026-09-16 05:26:36 +0000

    Fix

--- src/components/chart/statistics-chart.ts
+++ src/components/chart/statistics-chart.ts
@@ -565,7 +565,8 @@ export class StatisticsChart extends LitElement {
       maximumFractionDigits: this._yAxisFractionDigits,
     });
 
-  private _formatYAxisNameLTR = (value: string) => "\u202A" + value + "\u202C";
+  private _formatYAxisNameLTR = (value?: string) =>
+    value === undefined ? undefined : "\u202A" + value + "\u202C";
 
   static styles = css`
     :host {
