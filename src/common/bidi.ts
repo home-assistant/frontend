c351fcfd7e91bb6e0b7024e382bd8d75fc653c5c
Yosi Levy <yosilevy@gmail.com>
2026-09-18 06:59:35 +0000

    Bidi fix

--- src/common/bidi.ts
+++ src/common/bidi.ts
@@ -1,5 +1,3 @@
-export function bidiIsolate(value: string): string;
-export function bidiIsolate(value: undefined): undefined;
 export function bidiIsolate(value: string | undefined): string | undefined {
   return value === undefined ? undefined : `\u2068${value}\u2069`;
 }
