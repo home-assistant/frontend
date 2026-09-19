f30f06e5462c1c16e593d74798aaae59554c8c91
Bruno Pantaleão <5808343+bgoncal@users.noreply.github.com>
2026-09-19 22:47:18 +0200

    Bleed the table itself so every row variant lines up

--- src/components/data-table/ha-data-table.ts
+++ src/components/data-table/ha-data-table.ts
@@ -1203,16 +1203,16 @@ export class HaDataTable extends LitElement {
         }
 
         :host([narrow]) .mdc-data-table {
-          overflow: visible;
-        }
-
-        :host([narrow]) .mdc-data-table__header-row {
           width: calc(
             100% + var(--safe-area-inset-left, 0px) +
               var(--safe-area-inset-right, 0px)
           );
           margin-left: calc(-1 * var(--safe-area-inset-left, 0px));
           margin-right: calc(-1 * var(--safe-area-inset-right, 0px));
+          overflow: visible;
+        }
+
+        :host([narrow]) .mdc-data-table__header-row {
           overflow: visible;
         }
 
@@ -1511,16 +1511,6 @@ export class HaDataTable extends LitElement {
         .scroller {
           height: calc(100% - 57px);
           overflow: overlay !important;
-        }
-
-        :host([narrow]) .scroller {
-          width: calc(
-            100% + var(--safe-area-inset-left, 0px) +
-              var(--safe-area-inset-right, 0px)
-          );
-          margin-left: calc(-1 * var(--safe-area-inset-left, 0px));
-          margin-right: calc(-1 * var(--safe-area-inset-right, 0px));
-          background-color: var(--data-table-background-color);
         }
 
         :host([narrow]) .mdc-data-table__row {
