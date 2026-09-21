80202cd6456665b1ed1d886810a9fe7c62621a70
c0ffeeca7 <38767475+c0ffeeca7@users.noreply.github.com>
2026-09-21 18:16:49 +0200

    Connectivity panel: add my link (#54296)
    Add my link for Connectivity panel

--- src/panels/my/ha-panel-my.ts
+++ src/panels/my/ha-panel-my.ts
@@ -174,6 +174,9 @@ export const getMyRedirects = (): Redirects => ({
     component: "bluetooth",
     redirect: "/config/bluetooth",
   },
+  config_connectivity: {
+    redirect: "/config/connectivity",
+  },
   config_dhcp: {
     component: "dhcp",
     redirect: "/config/dhcp",
