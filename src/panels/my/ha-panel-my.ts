1fc565b6023ba406b150f0e38f4762e61cbc7dec
c0ffeeca7 <38767475+c0ffeeca7@users.noreply.github.com>
2026-09-21 11:33:43 +0200

    Security dashboard: add my link (#54298)

--- src/panels/my/ha-panel-my.ts
+++ src/panels/my/ha-panel-my.ts
@@ -355,6 +355,9 @@ export const getMyRedirects = (): Redirects => ({
   overview: {
     redirect: "/home/overview",
   },
+  security: {
+    redirect: "/security",
+  },
   media_browser: {
     component: "media_source",
     redirect: "/media-browser",
