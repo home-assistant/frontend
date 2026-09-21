385f5f4f3ac45625e6462d5134b5f07d74fd0677
Aidan Timson <aidan@timmo.dev>
2026-09-21 12:52:37 +0100

    Share GitHub issue URLs and type integration queries

---
+++ src/common/url/github.ts
@@ -0,0 +1,5 @@
+export const GITHUB_CORE_ISSUES_URL =
+  "https://github.com/home-assistant/core/issues";
+
+export const GITHUB_FRONTEND_ISSUES_URL =
+  "https://github.com/home-assistant/frontend/issues";
