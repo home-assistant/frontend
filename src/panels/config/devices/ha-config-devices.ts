4c19694692dae20935e7d67b745a6cafade51efa
Aidan Timson <aidan@timmo.dev>
2026-09-23 10:23:11 +0100

    Load device config entries and manifests in parallel earlier

--- src/panels/config/devices/ha-config-devices.ts
+++ src/panels/config/devices/ha-config-devices.ts
@@ -35,9 +35,12 @@ class HaConfigDevices extends HassRouterPage {
 
   @state() private _manifests: IntegrationManifest[] = [];
 
-  protected firstUpdated(changedProps: PropertyValues<this>) {
-    super.firstUpdated(changedProps);
-    this._loadData();
+  protected willUpdate(changedProps: PropertyValues<this>) {
+    super.willUpdate(changedProps);
+
+    if (!this.hasUpdated) {
+      this._loadData();
+    }
   }
 
   protected updatePageEl(pageEl) {
@@ -55,8 +58,9 @@ class HaConfigDevices extends HassRouterPage {
   }
 
   private async _loadData() {
+    const manifests = fetchIntegrationManifests(this.hass);
     this._configEntries = await getConfigEntries(this.hass);
-    this._manifests = await fetchIntegrationManifests(this.hass);
+    this._manifests = await manifests;
   }
 }
 
